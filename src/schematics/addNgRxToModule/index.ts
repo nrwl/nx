5aa1bc309deebdae60ef813daf92476797babf85
vsavkin <avix1000@gmail.com>
2017-08-11 10:26:54 -0400

    set up testing

--- src/schematics/addNgRxToModule/index.ts
+++ src/schematics/addNgRxToModule/index.ts
@@ -2,8 +2,9 @@ import {apply, branchAndMerge, chain, mergeWith, Rule, template, Tree, url} from
 import { names } from "../name-utils";
 
 export default function (options: any): Rule {
+
   const templateSource = apply(url('./files'), [
-    template({...options,  tmpl: '', ...names(options.name)})
+    template({...options,  tmpl: '', ...names('some')})
   ]);
 
   return chain([
