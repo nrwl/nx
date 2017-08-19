c32770a71a4d40854ee8413d37da6a40b1659415
vsavkin <avix1000@gmail.com>
2017-08-19 10:15:48 -0400

    feat: add app schematic

---
+++ src/schematics/app/schema.d.ts
@@ -0,0 +1,13 @@
+export interface Schema {
+  name: string;
+  directory?: string;
+  sourceDir?: string;
+  inlineStyle?: boolean;
+  inlineTemplate?: boolean;
+  viewEncapsulation?: ("Emulated" | "Native" | "None");
+  changeDetection?: ("Default" | "OnPush");
+  prefix?: string;
+  style?: string;
+  skipTests?: boolean;
+  routing?: boolean;
+}
