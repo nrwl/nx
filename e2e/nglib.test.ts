a8cdc06eb10f43ca83466cdbab71c639bb781ea4
vsavkin <avix1000@gmail.com>
2017-08-19 17:14:20 -0400

    feat: add lib and nblib schematics

---
+++ e2e/nglib.test.ts
@@ -0,0 +1,26 @@
+import {
+  addNgRx, checkFilesExists, cleanup, newApp, readFile, runCLI, runCommand, runSchematic,
+  updateFile
+} from './utils';
+
+describe('angular library', () => {
+  beforeEach(cleanup);
+
+  it('creates a new  angularlibrary in a workspace', () => {
+    runSchematic('@nrwl/nx:application --name=proj');
+    runSchematic('@nrwl/nx:nglib --name=myLib', {projectName: 'proj'});
+
+    checkFilesExists(
+      `proj/tsconfig.json`,
+      `proj/WORKSPACE`,
+      `proj/BUILD.bazel`,
+      `proj/libs/my-lib/BUILD.bazel`,
+      `proj/libs/my-lib/index.ts`,
+      `proj/libs/my-lib/src/my-lib.module.ts`
+    );
+
+    const cliConfig = JSON.parse(readFile('proj/.angular-cli.json'));
+    expect(cliConfig.apps[0].name).toEqual('myLib');
+    expect(cliConfig.apps[0].root).toEqual('libs/my-lib/src');
+  });
+});
---
+++ src/schematics/lib/schema.d.ts
@@ -0,0 +1,5 @@
+export interface Schema {
+  name: string;
+  directory?: string;
+  sourceDir?: string;
+}
