0186fb48c95a09156d4d70175448712b6c1af483
Austin M. Matherne <AustinMatherne@ProtonMail.ch>
2017-08-30 22:56:03 -0500

    Create tests for testing helpers

---
+++ packages/nx/spec/testing-utils.spec.ts
@@ -0,0 +1,27 @@
+import {from} from 'rxjs/Observable/from'
+import {readAll, readFirst} from '../src/testing-utils';
+
+describe('TestingUtils', () => {
+  describe('readAll', () => {
+    it('should transform Observable<T> to Promise<Array<T>>', async (done) => {
+      const obs = from([1, 2, 3]);
+      const result = await readAll(obs);
+
+      expect(result).toEqual([1, 2, 3]);
+
+      done();
+    });
+  });
+
+  describe('readFirst', () => {
+    it('should transform first item emitted from Observable<T> to Promise<T>', async (done) => {
+      const obs = from([1, 2, 3]);
+      const result = await readFirst(obs);
+
+      expect(result).toBe(1);
+
+      done();
+    });
+  });
+});
+
