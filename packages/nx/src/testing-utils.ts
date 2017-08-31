6cfb6fa71600a274d4944cd341147ab54957783f
Austin M. Matherne <AustinMatherne@ProtonMail.ch>
2017-08-30 22:55:42 -0500

    Create readFirst testing helper

--- packages/nx/src/testing-utils.ts
+++ packages/nx/src/testing-utils.ts
@@ -1,7 +1,12 @@
 import {Observable} from 'rxjs/Observable';
+import {first} from 'rxjs/operator/first';
 import {toArray} from 'rxjs/operator/toArray';
 import {toPromise} from 'rxjs/operator/toPromise';
 
 export function readAll<T>(o: Observable<T>): Promise<T[]> {
   return toPromise.call(toArray.call(o));
+}
+
+export function readFirst<T>(o: Observable<T>): Promise<T> {
+  return toPromise.call(first.call(o));
 }
