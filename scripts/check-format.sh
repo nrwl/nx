ce1b0c08503eded58081382cd2f71eea0cf2c5f4
vsavkin <avix1000@gmail.com>
2017-10-03 15:40:39 -0400

    build: run check-format first

--- scripts/check-format.sh
+++ scripts/check-format.sh
@@ -3,13 +3,13 @@
 find packages/ -iname "*.ts" | xargs -n1 clang-format -output-replacements-xml | grep -c "<replacement " >/dev/null
 if [ $? -ne 1 ]
 then
-    echo "Please run `yarn format`"
+    echo "Please run yarn format"
     exit 1;
 fi
 
 find e2e/ -iname "*.ts" | xargs -n1 clang-format -output-replacements-xml | grep -c "<replacement " >/dev/null
 if [ $? -ne 1 ]
 then
-    echo "Please run `yarn format`"
+    echo "Please run yarn format"
     exit 1;
 fi
