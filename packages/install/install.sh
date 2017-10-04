04ef067bafa058e65f31bc303ea5468e8353478b
vsavkin <avix1000@gmail.com>
2017-10-04 14:58:30 -0400

    add install script

---
+++ packages/install/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]
+then
+    echo "Please provide project name"
+    exit 1;
+fi
+
+CWD=$PWD
+WORK_DIR=`mktemp -d`
+
+echo 'Creating a sandbox with the CLI and Nx Schematics...'
+touch $WORK_DIR/package.json
+echo '{"dependencies": {"@angular/cli": "nrwl/fix-cli-build", "@nrwl/schematics": "github:nrwl/schematics-build"}}' > $WORK_DIR/package.json
+
+cd $WORK_DIR
+npm install --silent
+
+echo 'Creating a new project...'
+cd $CWD
+$WORK_DIR/node_modules/.bin/ng new $1 --collection=@nrwl/schematics
+
+rm -rf $WORK_DIR
