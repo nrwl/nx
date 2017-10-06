94baa9479f3eeeab98f236bf531a4bc83632a01c
vsavkin <avix1000@gmail.com>
2017-10-06 11:31:48 -0400

    fix(schematics): revert to using angular 4.4.4

--- packages/schematics/src/utility/lib-versions.ts
+++ packages/schematics/src/utility/lib-versions.ts
@@ -1,5 +1,5 @@
 export const angularCliVersion = 'nrwl/fix-cli-build';
-export const angularVersion = '^5.0.0-rc.0';
+export const angularVersion = '^4.4.4';
 export const angularJsVersion = '1.6.6';
 export const ngrxVersion = '^4.0.0';
 export const nxVersion = 'nrwl/nx-build';
