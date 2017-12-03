9e9e7360e892b43f168f52ee494cf9744b995b8f
vsavkin <avix1000@gmail.com>
2017-12-03 15:09:39 -0500

    test(schematics): added an e2e test for migrator

--- e2e/schematics/migrator.test.ts
+++ e2e/schematics/migrator.test.ts
@@ -1,12 +1,29 @@
-import { newApp, newLib, newProject, readFile, runCLI, updateFile, runCommand } from '../utils';
+import { newProject, runCommand, updateFile } from '../utils';
+import { updateJsonFile } from '../../packages/schematics/src/collection/utility/fileutils';
 
 describe('Migrator', () => {
-  fit(
+  it(
     'should run migrations',
     () => {
       newProject();
-      console.log(runCommand('npm run nx-migrate'));
+      updateFile(
+        'node_modules/@nrwl/schematics/migrations/20200101-test-migration.js',
+        `
+        exports.default = {
+          description: 'Test migration',
+          run: function() {
+            console.log('Running test migration');
+          }
+        };
+      `
+      );
+      const out = runCommand('npm run nx-migrate');
+      expect(out).toContain('Test migration');
+      expect(out).toContain('Running test migration');
+      expect(out).toContain('All migrations run successfully');
+
+      expect(runCommand('npm run nx-migrate')).toContain('No migrations to run');
     },
-    100000
+    1000000
   );
 });
