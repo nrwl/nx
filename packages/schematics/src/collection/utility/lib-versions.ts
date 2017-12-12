201807e0d46e425e395552f0185e4ef1c0ac1560
vsavkin <avix1000@gmail.com>
2017-12-12 13:24:36 -0500

    feat(schematics): switch to angular 5.1 and CLI 1.6

--- packages/schematics/src/collection/utility/lib-versions.ts
+++ packages/schematics/src/collection/utility/lib-versions.ts
@@ -1,11 +1,13 @@
-export const angularCliVersion = 'nrwl/fix-cli-build';
-export const angularVersion = '5.0.0';
+export const angularCliVersion = 'nrwl/fix-cli-build#release16';
+export const angularVersion = '5.1.0';
 export const angularJsVersion = '1.6.6';
-export const ngrxVersion = '^4.1.0';
+export const ngrxVersion = '4.1.1';
 export const nxVersion = '*';
 export const schematicsVersion = '*';
-export const latestMigration = '20171211-create-tsconfigapp-per-app.ts';
+export const latestMigration = '20171211-create-tsconfigapp-per-app';
 export const prettierVersion = '1.8.2';
+export const typescriptVersion = '2.5.3';
+export const rxjsVersion = '5.5.2';
 
 export const libVersions = {
   angularVersion,
@@ -15,5 +17,7 @@ export const libVersions = {
   nxVersion,
   schematicsVersion,
   prettierVersion,
-  latestMigration
+  latestMigration,
+  typescriptVersion,
+  rxjsVersion
 };
