50295eaadb7b72a964de43a56f2364f77d5c4af3
Victor Savkin <vic.savkin@gmail.com>
2018-04-05 21:09:13 -0400

    cleanup(schematics): use relative imports

--- packages/schematics/migrations/20180328-add-nx-lint.ts
+++ packages/schematics/migrations/20180328-add-nx-lint.ts
@@ -1,4 +1,4 @@
-import { updateJsonFile } from '@nrwl/schematics/src/utils/fileutils';
+import { updateJsonFile } from '../src/utils/fileutils';
 
 export default {
   description: 'Run lint checks ensuring the integrity of the workspace',
