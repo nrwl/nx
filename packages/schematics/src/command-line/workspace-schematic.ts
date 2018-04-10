85a785e02ef3ce23f1b7a5a8ea7beef4272dba7b
Noel Mace <contact@noelmace.com>
2018-04-10 18:20:08 +0200

    refactor: rxjs v6 upgrade
    rxjs v6 breaking changes integration without rxjs-compact

--- packages/schematics/src/command-line/workspace-schematic.ts
+++ packages/schematics/src/command-line/workspace-schematic.ts
@@ -22,9 +22,8 @@ import {
   Tree,
   DryRunSink
 } from '@angular-devkit/schematics';
-import { of } from 'rxjs/observable/of';
 import { concat, concatMap, ignoreElements, map } from 'rxjs/operators';
-import { Observable } from 'rxjs/Observable';
+import { Observable, of } from 'rxjs';
 import { Url } from 'url';
 
 import * as yargsParser from 'yargs-parser';
