5a547ccadd2b314bebfc74930cbb69495b328413
Jason Jean <jasonjean1993@gmail.com>
2018-07-16 11:35:34 -0400

    feat(schematics): add schematics for jest

---
+++ packages/schematics/src/collection/jest/files/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular/preprocessor.js'
+  },
+  resolver: '@nrwl/builders/plugins/jest/resolver',
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  collectCoverage: true,
+  coverageReporters: ['html']
+};
