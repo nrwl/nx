de5c226a3afe359a8a537823f35a2e3d4d3f02c4
--get <jasonjean1993@gmail.com>
2018-09-21 16:19:11 -0400

    feat(schematics): update @angular/cli to 6.2.3 and ngrx 6.1.0

--- packages/schematics/src/lib-versions.ts
+++ packages/schematics/src/lib-versions.ts
@@ -1,16 +1,14 @@
-export const angularCliVersion = '6.1.2';
+export const angularCliVersion = '6.2.3';
 export const angularVersion = '^6.1.0';
-export const angularDevkitVersion = '~0.7.0';
+export const angularDevkitVersion = '~0.8.0';
 export const angularJsVersion = '1.6.6';
-export const ngrxVersion = '6.0.1';
+export const ngrxVersion = '6.1.0';
 export const ngrxStoreFreezeVersion = '0.2.4';
 export const nxVersion = '*';
 export const schematicsVersion = '*';
-export const angularCliSchema =
-  './node_modules/@nrwl/schematics/src/schema.json';
 export const latestMigration = '20180507-create-nx-json';
 export const prettierVersion = '1.13.7';
-export const typescriptVersion = '~2.7.2';
+export const typescriptVersion = '~2.9.2';
 export const rxjsVersion = '^6.0.0';
 export const jestVersion = '^23.0.0';
 export const jestPresetAngularVersion = '6.0.1';
@@ -26,7 +24,6 @@ export const libVersions = {
   nxVersion,
   schematicsVersion,
   prettierVersion,
-  angularCliSchema,
   latestMigration,
   typescriptVersion,
   rxjsVersion,
