b9ffb90ef9fe805c275bfa945792874c13f35a47
Leigh Caplan <texel1@gmail.com>
2018-10-04 10:16:36 -0700

    feat(nx): export data persistence operators
    DataPersistence methods have been refactored to use pipeable operators, but these operators weren't
    exported so they could not be used outside of the library. Now, users will be able to import the
    operators themselves.

--- packages/nx/index.ts
+++ packages/nx/index.ts
@@ -1,2 +1,8 @@
-export { DataPersistence } from './src/data-persistence';
+export {
+  DataPersistence,
+  fetch,
+  navigation,
+  optimisticUpdate,
+  pessimisticUpdate
+} from './src/data-persistence';
 export { NxModule } from './src/nx.module';
