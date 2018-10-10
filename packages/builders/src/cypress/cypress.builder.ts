f38bda025b8833923418db7bc61a636bcdf13a7a
ben <contact@benjamincabanes.com>
2018-10-10 17:18:19 -0400

    feat(schematics): add cypress e2e test runner
    This is enables the use of Cypress as e2e test runner for a new
    application in the Nx workspace.
    
    The Cypress command is hidden as a flag in the main application
    schematics. Meaning, the only thing needed to enable this feature is to
    add the `--e2eTestRunner=cypress` flag at your command when creating
    a new application.
    Example:
    ```shell
    $ ng generate application myApp --e2eTestRunner=cypress
    ```
    
    By default `ng e2e my-app-e2e` will start Cypress in the application
    mode. You will see the desktop application and will be able to check all
    the tests and run them as you which.
    
    If you want to run the Cypress tests in headless mode (while being on CI
    for example), you can do so by passing the `--headless` to the command.
      You will see all the test results live in the terminal.
      ```shell
      $ ng e2e my-app-e2e --headless
      ```
    
    The Cypress configuration files and folders are present in the new
    `my-app-e2e` folder created. Every file are written in typescript and
    will be compiled into the `/dist/apps/my-app-e2e` folder before starting
    Cypress.
    
    If you need to fine tunes the options of Cypress, you can do so by
    modifying directly the `cypress.json` file in the related project.
    
    The build steps are has follow:
    • compile typescript files into javascript inside the
    `/dist/app/my-app-e2e` folder
    • build a dev server (using the default AngularCLI dev target build)
    • run Cypress with the compiled e2e files
    
    Screenshots and Videos will be accessible respectively in
    `/dist/apps/homer-app-e2e/screenshots` and
    `/dist/apps/homer-app-e2e/videos`.

---
+++ packages/builders/src/cypress/cypress.builder.ts
@@ -0,0 +1,271 @@
+import {
+  Builder,
+  BuilderConfiguration,
+  BuilderContext,
+  BuildEvent
+} from '@angular-devkit/architect';
+import { Observable, of, Subscriber, noop, bindCallback } from 'rxjs';
+import { catchError, concatMap, tap, map, take } from 'rxjs/operators';
+import { ChildProcess, fork, spawn } from 'child_process';
+import { copySync } from 'fs-extra';
+import { fromPromise } from 'rxjs/internal-compatibility';
+import { DevServerBuilderOptions } from '@angular-devkit/build-angular';
+import { readFile } from '@angular-devkit/schematics/tools/file-system-utility';
+import * as path from 'path';
+import * as url from 'url';
+import * as treeKill from 'tree-kill';
+const Cypress = require('cypress'); // @NOTE: Importing via ES6 messes the whole test dependencies.
+
+export interface CypressBuilderOptions {
+  baseUrl: string;
+  cypressConfig: string;
+  devServerTarget: string;
+  headless: boolean;
+  tsConfig: string;
+  watch: boolean;
+}
+
+/**
+ * @whatItDoes Implementation of the Cypress Builder, compile Typescript files,
+ * build the devServer to serve the app then run Cypress e2e test runner.
+ * The builder needs some information from the `angular.json` file:
+ * @example:
+```
+ "my-app-e2e": {
+    "root": "apps/my-app-e2e/",
+    "projectType": "application",
+    "architect": {
+      "e2e": {
+        "builder": "@nrwl/builders:cypress",
+        "options": {
+          "cypressConfig": "apps/my-app-e2e/cypress.json",
+          "tsConfig": "apps/my-app-e2e/tsconfig.json",
+          "devServerTarget": "my-app:serve"
+      },
+      "configurations": {
+        "production": {
+          "devServerTarget": "my-app:serve:production"
+        }
+      }
+      }
+    }
+ }
+```
+ *
+ */
+export default class CypressBuilder implements Builder<CypressBuilderOptions> {
+  private computedCypressBaseUrl: string;
+  private tscProcess: ChildProcess = null;
+
+  constructor(public context: BuilderContext) {}
+
+  /**
+   * @whatItDoes This is the starting point of the builder.
+   * @param builderConfig
+   */
+  run(
+    builderConfig: BuilderConfiguration<CypressBuilderOptions>
+  ): Observable<BuildEvent> {
+    const options = builderConfig.options;
+
+    return this.compileTypescriptFiles(options.tsConfig, options.watch).pipe(
+      tap(() => this.copyCypressFixtures(options.tsConfig)),
+      concatMap(
+        () =>
+          !options.baseUrl && options.devServerTarget
+            ? this.startDevServer(options.devServerTarget, options.watch)
+            : of(null)
+      ),
+      concatMap(() =>
+        this.initCypress(
+          options.cypressConfig,
+          options.headless,
+          options.watch,
+          options.baseUrl
+        )
+      ),
+      options.watch ? tap(noop) : take(1),
+      catchError(error => {
+        throw new Error(error);
+      })
+    );
+  }
+
+  /**
+   * @whatItDoes Compile typescript spec files to be able to run Cypress.
+   * The compilation is done via executing the `tsc` command line/
+   * @param tsConfigPath
+   * @param isWatching
+   */
+  private compileTypescriptFiles(
+    tsConfigPath: string,
+    isWatching: boolean
+  ): Observable<BuildEvent> {
+    if (this.tscProcess) {
+      this.killProcess();
+    }
+    return Observable.create((subscriber: Subscriber<BuildEvent>) => {
+      try {
+        let args = ['-p', tsConfigPath];
+        if (isWatching) {
+          args.push('--watch');
+          this.tscProcess = fork(
+            `${path.resolve(
+              this.context.workspace.root
+            )}/node_modules/.bin/tsc`,
+            args,
+            { stdio: [0, 1, 2, 'ipc'] }
+          );
+          subscriber.next({ success: true });
+        } else {
+          this.tscProcess = spawn(
+            `${path.resolve(
+              this.context.workspace.root
+            )}/node_modules/.bin/tsc`,
+            args,
+            { stdio: [0, 1, 2] }
+          );
+          this.tscProcess.on('exit', code => {
+            subscriber.next({ success: code === 0 });
+            subscriber.complete();
+          });
+        }
+      } catch (error) {
+        if (this.tscProcess) {
+          this.killProcess();
+        }
+        subscriber.error(
+          new Error(`Could not compile Typescript files: \n ${error}`)
+        );
+      }
+    });
+  }
+
+  /**
+   * @whatItDoes Copy all the fixtures into the dist folder.
+   * This is done because `tsc` doesn't handle `json` files.
+   * @param tsConfigPath
+   */
+  private copyCypressFixtures(tsConfigPath: string) {
+    const tsconfigJson = JSON.parse(readFile(tsConfigPath));
+    copySync(
+      `${path.dirname(tsConfigPath)}/src/fixtures`,
+      `${path.resolve(
+        path.dirname(tsConfigPath),
+        tsconfigJson.compilerOptions.outDir
+      )}/fixtures`,
+      { overwrite: true }
+    );
+  }
+
+  /**
+   * @whatItDoes Initialize the Cypress test runner with the provided project configuration.
+   * If `headless` is `false`: open the Cypress application, the user will
+   * be able to interact directly with the application.
+   * If `headless` is `true`: Cypress will run in headless mode and will
+   * provide directly the results in the console output.
+   * @param cypressConfig
+   * @param headless
+   * @param baseUrl
+   * @param isWatching
+   */
+  private initCypress(
+    cypressConfig: string,
+    headless: boolean,
+    isWatching: boolean,
+    baseUrl: string
+  ): Observable<BuildEvent> {
+    // Cypress expects the folder where a `cypress.json` is present
+    const projectFolderPath = path.dirname(cypressConfig);
+    const options: any = {
+      project: projectFolderPath
+    };
+
+    // If not, will use the `baseUrl` normally from `cypress.json`
+    if (baseUrl || this.computedCypressBaseUrl) {
+      options.config = { baseUrl: baseUrl || this.computedCypressBaseUrl };
+    }
+
+    options.headed = !headless;
+
+    return fromPromise<any>(
+      !isWatching || headless ? Cypress.run(options) : Cypress.open(options)
+    ).pipe(
+      tap(() => (isWatching && !headless ? process.exit() : null)), // Forcing `cypress.open` to give back the terminal
+      map(result => ({
+        /**
+         * `cypress.open` is returning `0` and is not of the same type as `cypress.run`.
+         * `cypress.open` is the graphical UI, so it will be obvious to know what wasn't
+         * working. Forcing the build to success when `cypress.open` is used.
+         */
+        success: result.hasOwnProperty(`totalFailed`)
+          ? result.totalFailed === 0
+          : true
+      }))
+    );
+  }
+
+  /**
+   * @whatItDoes Compile the application using the webpack builder.
+   * @param devServerTarget
+   * @param isWatching
+   * @private
+   */
+  private startDevServer(
+    devServerTarget: string,
+    isWatching: boolean
+  ): Observable<BuildEvent> {
+    const architect = this.context.architect;
+    const [project, targetName, configuration] = devServerTarget.split(':');
+    // Overrides dev server watch setting.
+    const overrides: Partial<DevServerBuilderOptions> = { watch: isWatching };
+    const targetSpec = {
+      project,
+      target: targetName,
+      configuration,
+      overrides: overrides
+    };
+    const builderConfig = architect.getBuilderConfiguration<
+      DevServerBuilderOptions
+    >(targetSpec);
+
+    return architect.getBuilderDescription(builderConfig).pipe(
+      concatMap(devServerDescription =>
+        architect.validateBuilderOptions(builderConfig, devServerDescription)
+      ),
+      tap(builderConfig => {
+        if (devServerTarget && builderConfig.options.publicHost) {
+          let publicHost = builderConfig.options.publicHost;
+          if (!/^\w+:\/\//.test(publicHost)) {
+            publicHost = `${
+              builderConfig.options.ssl ? 'https' : 'http'
+            }://${publicHost}`;
+          }
+          const clientUrl = url.parse(publicHost);
+          this.computedCypressBaseUrl = url.format(clientUrl);
+        } else if (devServerTarget) {
+          this.computedCypressBaseUrl = url.format({
+            protocol: builderConfig.options.ssl ? 'https' : 'http',
+            hostname: builderConfig.options.host,
+            port: builderConfig.options.port.toString()
+          });
+        }
+      }),
+      concatMap(builderConfig => architect.run(builderConfig, this.context))
+    );
+  }
+
+  private killProcess(): void {
+    return treeKill(this.tscProcess.pid, 'SIGTERM', error => {
+      this.tscProcess = null;
+      if (error) {
+        if (Array.isArray(error) && error[0] && error[2]) {
+          const errorMessage = error[2];
+          this.context.logger.error(errorMessage);
+        } else if (error.message) {
+          this.context.logger.error(error.message);
+        }
+      }
+    });
+  }
+}
