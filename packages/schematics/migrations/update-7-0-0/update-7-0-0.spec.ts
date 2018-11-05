9fc3448396feb709011dd3748d8bbae148905365
--get <jasonjean1993@gmail.com>
2018-11-05 17:43:36 -0500

    fix(schematics): update ng packagr for ng7 migration

--- packages/schematics/migrations/update-7-0-0/update-7-0-0.spec.ts
+++ packages/schematics/migrations/update-7-0-0/update-7-0-0.spec.ts
@@ -3,6 +3,7 @@ import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import { serializeJson } from '../../src/utils/fileutils';
 
 import * as path from 'path';
+import { updateJsonInTree } from '@nrwl/schematics/src/utils/ast-utils';
 
 describe('Update 7.0.0', () => {
   let initialTree: Tree;
@@ -34,5 +35,29 @@ describe('Update 7.0.0', () => {
 
     expect(devDependencies.codelyzer).toEqual('~4.5.0');
     expect(devDependencies['jasmine-marbles']).toEqual('0.4.0');
+  });
+
+  it('should update ng-packagr dependencies', async () => {
+    initialTree = await schematicRunner
+      .callRule(
+        updateJsonInTree('package.json', json => {
+          json.devDependencies = {
+            ...json.devDependencies,
+            'ng-packagr': '^3.0.0'
+          };
+
+          return json;
+        }),
+        initialTree
+      )
+      .toPromise();
+    const result = await schematicRunner
+      .runSchematicAsync('update-7.0.0', {}, initialTree)
+      .toPromise();
+
+    const devDependencies = JSON.parse(result.readContent('package.json'))
+      .devDependencies;
+
+    expect(devDependencies['ng-packagr']).toEqual('^4.2.0');
   });
 });
