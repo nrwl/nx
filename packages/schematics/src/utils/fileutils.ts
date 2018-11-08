ace390e6f76a8472134f3485509c29c3b067fcd8
Emilio Martinez <e.marticor@gmail.com>
2018-11-07 19:12:35 -0800

    fix(schematics): ensure path to file before writing

--- packages/schematics/src/utils/fileutils.ts
+++ packages/schematics/src/utils/fileutils.ts
@@ -1,9 +1,11 @@
 import { statSync } from 'fs';
 import * as fs from 'fs';
 import * as path from 'path';
+import { ensureDirSync } from 'fs-extra';
 
-export function writeToFile(path: string, str: string) {
-  fs.writeFileSync(path, str);
+export function writeToFile(filePath: string, str: string) {
+  ensureDirSync(path.dirname(filePath));
+  fs.writeFileSync(filePath, str);
 }
 
 /**
