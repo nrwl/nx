973d33b8184c135f1ad1de8477d8c59438fd45b6
ben <contact@benjamincabanes.com>
2018-11-24 17:29:34 -0500

    fix(schematics): add forbidden properties when renaming
    Add a `forbiddenPropertyList` into the `replaceAppNameWithPath` function
    to not override some property's value that should stay pristine.
    
    closes #900

--- packages/schematics/src/utils/cli-config-utils.ts
+++ packages/schematics/src/utils/cli-config-utils.ts
@@ -58,8 +58,14 @@ export function replaceAppNameWithPath(
   } else if (Array.isArray(node)) {
     return node.map(j => replaceAppNameWithPath(j, appName, root));
   } else if (typeof node === 'object' && node) {
+    const forbiddenPropertyList: string[] = ['prefix']; // Some of the properties should not be renamed
     return Object.keys(node).reduce(
-      (m, c) => ((m[c] = replaceAppNameWithPath(node[c], appName, root)), m),
+      (m, c) => (
+        (m[c] = !forbiddenPropertyList.includes(c)
+          ? replaceAppNameWithPath(node[c], appName, root)
+          : node[c]),
+        m
+      ),
       {} as any
     );
   } else {
