6992fa7ea1e7ee33962b7b349c143f95b3b2e904
muradm <mail@muradm.net>
2018-12-11 03:22:46 +0400

    feat(builders): add mjs to extensions for node-app #944

--- packages/builders/src/node/build/webpack/config.spec.ts
+++ packages/builders/src/node/build/webpack/config.spec.ts
@@ -67,7 +67,7 @@ describe('getWebpackConfig', () => {
     it('should resolve typescript and javascript', () => {
       const result = getWebpackConfig(input);
 
-      expect(result.resolve.extensions).toEqual(['.ts', '.js']);
+      expect(result.resolve.extensions).toEqual(['.ts', '.mjs', '.js']);
     });
 
     it('should include module and main in mainFields', () => {
