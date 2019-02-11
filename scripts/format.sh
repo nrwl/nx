a324f74c66837f789f6e0907c8d34f2ef4ed963d
ben <contact@benjamincabanes.com>
2019-02-11 15:16:07 -0500

    chore(prettier): add end of line settings
    Remove `dos2unix` uses in the `format` command because Pretter is now
    handling the end of line options.

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,7 +2,3 @@
 
 echo "Prettier"
 prettier "**/*.{ts,js,json,css,scss,md}" "!**/{__name__,__directory__}/**" --write
-
-echo "Line endings"
-find packages -type f -print0 | xargs -0 dos2unix
-find scripts -type f -print0 | xargs -0 dos2unix
