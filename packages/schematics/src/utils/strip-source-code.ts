3cf84eca24ff6156d00618c1462036f32110c690
Jason Jean <jasonjean1993@gmail.com>
2019-03-15 17:12:17 -0400

    enhancement(nx): optimize affected to strip sourcecode down to imports/exports

---
+++ packages/schematics/src/utils/strip-source-code.ts
@@ -0,0 +1,65 @@
+import { Scanner, SyntaxKind } from 'typescript';
+
+export function stripSourceCode(scanner: Scanner, contents: string): string {
+  if (contents.indexOf('loadChildren') > -1) {
+    return contents;
+  }
+
+  scanner.setText(contents);
+  let token = scanner.scan();
+  const statements = [];
+  let start = null;
+  while (token !== SyntaxKind.EndOfFileToken) {
+    const potentialStart = scanner.getStartPos();
+    switch (token) {
+      case SyntaxKind.ImportKeyword: {
+        token = scanner.scan();
+        while (
+          token === SyntaxKind.WhitespaceTrivia ||
+          token === SyntaxKind.NewLineTrivia
+        ) {
+          token = scanner.scan();
+        }
+        if (token !== SyntaxKind.OpenParenToken) {
+          start = potentialStart;
+        }
+        break;
+      }
+
+      case SyntaxKind.ExportKeyword: {
+        token = scanner.scan();
+        while (
+          token === SyntaxKind.WhitespaceTrivia ||
+          token === SyntaxKind.NewLineTrivia
+        ) {
+          token = scanner.scan();
+        }
+        if (
+          token === SyntaxKind.OpenBraceToken ||
+          token === SyntaxKind.AsteriskToken
+        ) {
+          start = potentialStart;
+        }
+        break;
+      }
+
+      case SyntaxKind.StringLiteral: {
+        if (start !== null) {
+          token = scanner.scan();
+          const end = scanner.getStartPos();
+          statements.push(contents.substring(start, end));
+          start = null;
+        } else {
+          token = scanner.scan();
+        }
+        break;
+      }
+
+      default: {
+        token = scanner.scan();
+      }
+    }
+  }
+
+  return statements.join('\n');
+}
