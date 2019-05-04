60c515db8c7f21e292983e8c2bbb49c43d6ffdc8
Gustavo Perdomo <gperdomor@gmail.com>
2019-05-04 14:33:48 -0400

    feat(nest): update to nestjs 6

--- packages/nest/src/utils/versions.ts
+++ packages/nest/src/utils/versions.ts
@@ -1,4 +1,7 @@
 export const nxVersion = '*';
 
-export const nestJsVersion = '5.5.0';
-export const nestJsSchematicsVersion = '5.11.2';
+export const nestJsVersion = '^6.1.0';
+export const nestJsSchematicsVersion = '^6.2.0';
+
+export const expressTypingsVersion = '4.16.0';
+export const reflectMetadataVersion = '^0.1.12';
