c0008d97a7ef222cc30c7754f5cac8d82098d285
Jason Jean <jasonjean1993@gmail.com>
2019-05-26 12:09:10 -0400

    fix(frontend): fix react component for apps

--- packages/react/src/utils/testing.ts
+++ packages/react/src/utils/testing.ts
@@ -1,6 +1,8 @@
 import { join } from 'path';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import { Rule, Tree } from '@angular-devkit/schematics';
+import { names } from '@nrwl/workspace/src/utils/name-utils';
+import { updateWorkspace } from '@nrwl/workspace/src/utils/workspace';
 
 const testRunner = new SchematicTestRunner(
   '@nrwl/react',
@@ -13,4 +15,40 @@ export function runSchematic(schematicName: string, options: any, tree: Tree) {
 
 export function callRule(rule: Rule, tree: Tree) {
   return testRunner.callRule(rule, tree).toPromise();
+}
+
+export function createApp(tree: Tree, appName: string): Promise<Tree> {
+  const { fileName } = names(appName);
+
+  return callRule(
+    updateWorkspace(workspace => {
+      workspace.projects.add({
+        name: fileName,
+        root: `apps/${fileName}`,
+        projectType: 'application',
+        sourceRoot: `apps/${fileName}/src`,
+        targets: {}
+      });
+    }),
+    tree
+  );
+}
+
+export function createLib(tree: Tree, libName: string): Promise<Tree> {
+  const { fileName } = names(libName);
+
+  tree.create(`/libs/${fileName}/src/index.ts`, `\n`);
+
+  return callRule(
+    updateWorkspace(workspace => {
+      workspace.projects.add({
+        name: fileName,
+        root: `libs/${fileName}`,
+        projectType: 'library',
+        sourceRoot: `libs/${fileName}/src`,
+        targets: {}
+      });
+    }),
+    tree
+  );
 }
