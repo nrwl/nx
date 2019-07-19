98e5bbc1002bd911e5bf89b797e79cadcbc1fff1
Jason Jean <jasonjean1993@gmail.com>
2019-07-19 14:11:02 -0400

    fix(frontend): fix ngrx migration requiring angular cli 8.1

--- packages/angular/src/migrations/update-8-3-0/upgrade-ngrx-8-0.ts
+++ packages/angular/src/migrations/update-8-3-0/upgrade-ngrx-8-0.ts
@@ -1,18 +1,67 @@
-import { chain, Tree, noop } from '@angular-devkit/schematics';
-import { addUpdateTask, readJsonInTree, formatFiles } from '@nrwl/workspace';
+import { chain, Tree, noop, TaskId } from '@angular-devkit/schematics';
+import {
+  addUpdateTask,
+  readJsonInTree,
+  formatFiles,
+  updateJsonInTree
+} from '@nrwl/workspace';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
-const runNgrxUpdate = addUpdateTask('@ngrx/store', '8.1.0');
+function updateCLI() {
+  const tasks: TaskId[] = [];
+  const rule = chain([
+    updateJsonInTree('package.json', json => {
+      json.devDependencies = json.devDependencies || {};
+      const cliVersion = json.devDependencies['@angular/cli'];
 
-const updateNgrx = (host: Tree) => {
-  const { dependencies } = readJsonInTree(host, 'package.json');
+      if (
+        cliVersion &&
+        (cliVersion.startsWith('8.1') ||
+          cliVersion.startsWith('~8.1') ||
+          cliVersion.startsWith('^8.1'))
+      ) {
+        return json;
+      }
 
-  if (dependencies && dependencies['@ngrx/store']) {
-    return chain([runNgrxUpdate, formatFiles()]);
-  }
+      if (json['devDependencies']['@angular/cli']) {
+        json['devDependencies']['@angular/cli'] = '8.1.1';
+      }
 
-  return noop();
-};
+      if (json['devDependencies']['@angular-devkit/build-angular']) {
+        json['devDependencies']['@angular-devkit/build-angular'] = '^0.801.1';
+      }
+
+      if (json['devDependencies']['@angular-devkit/build-ng-packagr']) {
+        json['devDependencies']['@angular-devkit/build-ng-packagr'] =
+          '~0.801.1';
+      }
+
+      return json;
+    }),
+    (host, context) => {
+      tasks.push(context.addTask(new NodePackageInstallTask()));
+    }
+  ]);
+
+  return { rule, tasks };
+}
+
+function updateNgrx(updateDeps: TaskId[]) {
+  return (host: Tree) => {
+    const { dependencies } = readJsonInTree(host, 'package.json');
+
+    if (dependencies && dependencies['@ngrx/store']) {
+      return chain([
+        addUpdateTask('@ngrx/store', '8.1.0', updateDeps),
+        formatFiles()
+      ]);
+    }
+
+    return noop();
+  };
+}
 
 export default function() {
-  return chain([updateNgrx]);
+  const { rule: updateCLIRule, tasks } = updateCLI();
+  return chain([updateCLIRule, updateNgrx(tasks)]);
 }
