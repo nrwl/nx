3cd03a9a297ec5235c0c45b67a6d0040417fc0dc
mehrad-rafigh <mehrad@rafigh.de>
2019-08-23 01:40:27 +0200

    build(nx): update nestjs and ngrx to latest version

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -2,6 +2,6 @@ export const nxVersion = '*';
 export const angularVersion = '^8.0.0';
 export const angularDevkitVersion = '^0.800.1';
 export const angularJsVersion = '1.6.6';
-export const ngrxVersion = '8.1.0';
+export const ngrxVersion = '8.2.0';
 export const rxjsVersion = '~6.4.0';
 export const jestPresetAngularVersion = '7.0.0';
--- packages/nest/src/utils/versions.ts
+++ packages/nest/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = '*';
 
-export const nestJsVersion = '^6.2.4';
-export const nestJsSchematicsVersion = '^6.3.0';
+export const nestJsVersion = '^6.5.3';
+export const nestJsSchematicsVersion = '^6.4.3';
 
 export const reflectMetadataVersion = '^0.1.12';
