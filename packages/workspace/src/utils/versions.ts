87f4d45311acd3265951fb56a46d1727cb226077
victor savkin <vic.savkin@gmail.com>
2019-09-09 11:22:47 -0400

    Revert "chore(nx): add typescript eslint to 2.1.0"
    This reverts commit 12f62ece58c3132f04e331f7f49f39377b3afe43.

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -3,6 +3,6 @@ export const nxVersion = '*';
 export const angularCliVersion = '8.3.2';
 export const typescriptVersion = '~3.4.5';
 export const prettierVersion = '1.16.4';
-export const typescriptESLintVersion = '2.1.0';
+export const typescriptESLintVersion = '2.0.0-alpha.4';
 export const eslintVersion = '6.1.0';
 export const eslintConfigPrettierVersion = '6.0.0';
