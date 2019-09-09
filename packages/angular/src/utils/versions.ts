d7bb210e66c6025c934cfe2ac57f1315c4cbbd2b
Brandon Roberts <robertsbt@gmail.com>
2019-09-09 09:45:18 -0500

    chore(nx): bump Angular CLI deps to 8.3.3

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = '*';
 export const angularVersion = '^8.2.0';
-export const angularDevkitVersion = '^0.803.2';
+export const angularDevkitVersion = '^0.803.3';
 export const angularJsVersion = '1.6.6';
 export const ngrxVersion = '8.3.0';
 export const rxjsVersion = '~6.4.0';
--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = '*';
 
-export const angularCliVersion = '8.3.2';
+export const angularCliVersion = '8.3.3';
 export const typescriptVersion = '~3.4.5';
 export const prettierVersion = '1.16.4';
 export const typescriptESLintVersion = '2.1.0';
