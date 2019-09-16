5de142f69f3da617a971b2dc1db4887cc15a1fd7
Jo Pearce <jo@nrwl.io>
2019-09-16 16:20:49 +0100

    fix(testing): merge builder globals with jest.config.js globals rather than override them completely (#1827)
    Resolves #1059

--- e2e/jest.test.ts
+++ e2e/jest.test.ts
@@ -1,4 +1,12 @@
-import { runCLIAsync, ensureProject, uniq, runCLI, forEachCli } from './utils';
+import {
+  runCLIAsync,
+  ensureProject,
+  uniq,
+  runCLI,
+  forEachCli,
+  updateFile
+} from './utils';
+import { stripIndents } from '@angular-devkit/core/src/utils/literals';
 
 forEachCli(() => {
   describe('Jest', () => {
@@ -19,6 +27,44 @@ forEachCli(() => {
       expect(appResult.stderr).toContain('Test Suites: 3 passed, 3 total');
       const libResult = await runCLIAsync(`test ${mylib}`);
       expect(libResult.stderr).toContain('Test Suites: 3 passed, 3 total');
+      done();
+    }, 45000);
+
+    it('should merge with jest config globals', async done => {
+      ensureProject();
+      const testGlobal = `'My Test Global'`;
+      const mylib = uniq('mylib');
+      runCLI(`generate @nrwl/workspace:lib ${mylib} --unit-test-runner jest`);
+
+      updateFile(`libs/${mylib}/src/lib/${mylib}.ts`, `export class Test { }`);
+
+      updateFile(
+        `libs/${mylib}/src/lib/${mylib}.spec.ts`,
+        `
+          test('can access jest global', () => {
+            expect((global as any).testGlobal).toBe(${testGlobal});
+          });
+        `
+      );
+
+      updateFile(
+        `libs/${mylib}/jest.config.js`,
+        stripIndents`
+          module.exports = {
+            testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+            transform: {
+              '^.+\\.(ts|js|html)$': 'ts-jest'
+            },
+            resolver: '@nrwl/jest/plugins/resolver',
+            moduleFileExtensions: ['ts', 'js', 'html'],
+            coverageReporters: ['html'],
+            passWithNoTests: true,
+            globals: { testGlobal: ${testGlobal} }
+          };`
+      );
+
+      const appResult = await runCLIAsync(`test ${mylib} --no-watch`);
+      expect(appResult.stderr).toContain('Test Suites: 1 passed, 1 total');
       done();
     }, 45000);
   });
