da62415edf3aae474966864d2481c48e5d2f5ed7
Peter Blazejewicz <peterblazejewicz@users.noreply.github.com>
2019-09-30 22:42:58 +0200

    fix(nx): update ESLint types to stable version
    Closes #1833

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -3,6 +3,6 @@ export const nxVersion = '*';
 export const angularCliVersion = '8.3.3';
 export const typescriptVersion = '~3.4.5';
 export const prettierVersion = '1.18.2';
-export const typescriptESLintVersion = '2.0.0-alpha.4';
+export const typescriptESLintVersion = '2.3.2';
 export const eslintVersion = '6.1.0';
 export const eslintConfigPrettierVersion = '6.0.0';
