6908d9b70b32eb3a31a8c1110a21c70a954324cb
Isaac Mann <isaacplmann+git@gmail.com>
2019-10-21 16:45:49 -0400

    chore(nx): add storybook to e2e tests

--- scripts/e2e-ci2.sh
+++ scripts/e2e-ci2.sh
@@ -13,5 +13,6 @@ jest --maxWorkers=1 ./build/e2e/node.test.js
 jest --maxWorkers=1 ./build/e2e/print-affected.test.js
 jest --maxWorkers=1 ./build/e2e/react.test.js
 jest --maxWorkers=1 ./build/e2e/report.test.js
+jest --maxWorkers=1 ./build/e2e/storybook.test.js
 jest --maxWorkers=1 ./build/e2e/upgrade-module.test.js
 jest --maxWorkers=1 ./build/e2e/web.test.js
