6b32104fff9528dec2ab399b8f0466800368dfee
Brandon Roberts <robertsbt@gmail.com>
2019-11-07 11:19:21 -0600

    chore(nx): bump NgRx to 8.5.0 release

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -2,6 +2,6 @@ export const nxVersion = '*';
 export const angularVersion = '^8.2.0';
 export const angularDevkitVersion = '^0.803.14';
 export const angularJsVersion = '1.6.6';
-export const ngrxVersion = '8.4.0';
+export const ngrxVersion = '8.5.0';
 export const rxjsVersion = '~6.4.0';
 export const jestPresetAngularVersion = '7.0.0';
