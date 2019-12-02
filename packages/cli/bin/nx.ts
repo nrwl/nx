5b975dd0f503d5026e11d5f4aaa1abfedc427aff
Victor Savkin <mail@vsavkin.com>
2019-12-02 17:22:53 -0500

    fix(nx): polyfill rxjs to avoid version mismatch issues

--- packages/cli/bin/nx.ts
+++ packages/cli/bin/nx.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+// polyfill rxjs observable to avoid issues with multiple version fo Observable installed in node_modules
+// https://twitter.com/BenLesh/status/1192478226385428483?s=20
+(Symbol as any).observable = Symbol('observable polyfill');
 import { findWorkspaceRoot } from '../lib/find-workspace-root';
 import { initGlobal } from '../lib/init-global';
 import { initLocal } from '../lib/init-local';
