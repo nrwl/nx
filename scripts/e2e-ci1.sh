4a197e4047dbe2e2d73ecd7635f10ae08d8aabd6
Jason Jean <jasonjean1993@gmail.com>
2019-12-04 00:08:19 -0500

    fix(bazel): add bazel to e2e tests

--- scripts/e2e-ci1.sh
+++ scripts/e2e-ci1.sh
@@ -10,6 +10,7 @@ mkdir -p tmp/nx
 # This should be every file under e2e except for utils.js up to next.test.ts
 export SELECTED_CLI=$1
 jest --maxWorkers=1 ./build/e2e/affected.test.js &&
+jest --maxWorkers=1 ./build/e2e/bazel.test.js &&
 jest --maxWorkers=1 ./build/e2e/command-line.test.js &&
 jest --maxWorkers=1 ./build/e2e/cypress.test.js &&
 jest --maxWorkers=1 ./build/e2e/delegate-to-cli.test.js &&
