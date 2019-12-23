75c857c4345e2221d8ac0e41ce6b0e1997900269
Jack Hsu <jack.hsu@gmail.com>
2019-12-23 09:57:06 -0500

    chore(react): update React-related libs

--- packages/react/src/utils/versions.ts
+++ packages/react/src/utils/versions.ts
@@ -2,27 +2,27 @@ export const nxVersion = '*';
 
 export const reactVersion = '16.12.0';
 export const reactDomVersion = '16.12.0';
-export const typesReactVersion = '16.9.13';
+export const typesReactVersion = '16.9.17';
 export const typesReactDomVersion = '16.9.4';
 
 export const styledComponentsVersion = '4.4.1';
-export const typesStyledComponentsVersion = '4.4.0';
+export const typesStyledComponentsVersion = '4.4.1';
 
-export const emotionStyledVersion = '10.0.23';
-export const emotionCoreVersion = '10.0.22';
+export const emotionStyledVersion = '10.0.27';
+export const emotionCoreVersion = '10.0.27';
 
 export const reactRouterDomVersion = '5.1.2';
-export const typesReactRouterDomVersion = '5.1.2';
+export const typesReactRouterDomVersion = '5.1.3';
 
-export const testingLibraryReactVersion = '9.3.2';
+export const testingLibraryReactVersion = '9.4.0';
 
 export const babelPresetReactVersion = '7.7.4';
 
-export const reduxjsToolkitVersion = '1.0.4';
+export const reduxjsToolkitVersion = '1.1.0';
 export const reactReduxVersion = '7.1.3';
 export const typesReactReduxVersion = '7.1.5';
 
-export const eslintPluginImportVersion = '2.18.2';
+export const eslintPluginImportVersion = '2.19.1';
 export const eslintPluginJsxA11yVersion = '6.2.3';
-export const eslintPluginReactVersion = '7.16.0';
+export const eslintPluginReactVersion = '7.17.0';
 export const eslintPluginReactHooksVersion = '2.3.0';
