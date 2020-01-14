e518d109748f634db5c469bb0738792d96611e09
Jonathan Cammisuli <jon@cammisuli.ca>
2020-01-14 18:43:35 -0500

    feat(nx-plugin): update e2e tests

--- e2e/nx-plugin.test.ts
+++ e2e/nx-plugin.test.ts
@@ -5,8 +5,11 @@ import {
   runCLI,
   updateFile,
   expectTestsPass,
-  runCLIAsync
+  runCLIAsync,
+  checkFilesExist,
+  readJson
 } from './utils';
+import { readNxJson } from '@nrwl/workspace';
 
 forEachCli(currentCLIName => {
   const linter = currentCLIName === 'angular' ? 'tslint' : 'eslint';
@@ -16,12 +19,49 @@ forEachCli(currentCLIName => {
       ensureProject();
       const plugin = uniq('plugin');
 
-      runCLI(`generate @nrwl/nx-plugin:plugin ${plugin} --linter=${linter}`);
+      runCLI(
+        `generate @nrwl/nx-plugin:plugin ${plugin} --linter=${linter} --tags=e2etag,e2ePackage`
+      );
       const lintResults = runCLI(`lint ${plugin}`);
       expect(lintResults).toContain('All files pass linting.');
 
       expectTestsPass(await runCLIAsync(`test ${plugin}`));
+
+      const buildResults = runCLI(`build ${plugin}`);
+      expect(buildResults).toContain('Done compiling TypeScript files');
+      checkFilesExist(
+        `dist/libs/${plugin}/package.json`,
+        `dist/libs/${plugin}/collection.json`,
+        `dist/libs/${plugin}/builders.json`,
+        `dist/libs/${plugin}/src/index.js`,
+        `dist/libs/${plugin}/src/schematics/${plugin}/schema.json`,
+        `dist/libs/${plugin}/src/schematics/${plugin}/schema.d.ts`,
+        `dist/libs/${plugin}/src/schematics/${plugin}/schematic.js`,
+        `dist/libs/${plugin}/src/schematics/${plugin}/files/src/index.ts.template`,
+        `dist/libs/${plugin}/src/builders/${plugin}/builder.js`,
+        `dist/libs/${plugin}/src/builders/${plugin}/schema.d.ts`,
+        `dist/libs/${plugin}/src/builders/${plugin}/schema.json`
+      );
+      const nxJson = readJson('nx.json');
+      expect(nxJson).toMatchObject({
+        projects: expect.objectContaining({
+          [plugin]: {
+            tags: ['e2etag', 'e2ePackage']
+          },
+          [`${plugin}-e2e`]: {
+            tags: [],
+            implicitDependencies: [`${plugin}`]
+          }
+        })
+      });
       done();
     }, 45000);
+
+    //   it('should be able to build a Nx Plugin lib', async done => {
+    //   ensureProject();
+    //   const plugin = uniq('plugin');
+    //   runCLI(`generate @nrwl/nx-plugin:plugin ${plugin} --linter=${linter}`);
+    //   const buildResults = runCLI(`build ${plugin}`);
+    // });
   });
 });
