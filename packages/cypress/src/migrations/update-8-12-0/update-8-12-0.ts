6c10aa46106f38aad9d23173fe0fcd6b7d5820ef
Jason Jean <jasonjean1993@gmail.com>
2020-01-23 22:04:59 -0500

    feat(core): updating cypress version nrwl/cypress (#2370)
    Co-authored-by: Benjamin Cabanes <contact@benjamincabanes.com>

---
+++ packages/cypress/src/migrations/update-8-12-0/update-8-12-0.ts
@@ -0,0 +1,10 @@
+import { updatePackagesInPackageJson } from '@nrwl/workspace';
+
+import { join } from 'path';
+
+export default () => {
+  return updatePackagesInPackageJson(
+    join(__dirname, '../../../migrations.json'),
+    '8.12.0'
+  );
+};
