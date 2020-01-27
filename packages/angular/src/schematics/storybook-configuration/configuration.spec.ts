e73e6a7aa938dc7c1519d2e39bbcb5a3ca6e2041
Bucky Maler <maler.bucky@gmail.com>
2020-01-26 22:01:29 -0600

    fix(angular): storybook config schematic from generating stories when generateStories false
    ISSUES CLOSED: #2383

--- packages/angular/src/schematics/storybook-configuration/configuration.spec.ts
+++ packages/angular/src/schematics/storybook-configuration/configuration.spec.ts
@@ -10,6 +10,45 @@ describe('schematic:configuration', () => {
     appTree = await createTestUILib('test-ui-lib');
   });
 
+  it('should only configure storybook', async () => {
+    const tree = await runSchematic(
+      'storybook-configuration',
+      <StorybookConfigureSchema>{
+        name: 'test-ui-lib',
+        configureCypress: false,
+        generateCypressSpecs: false,
+        generateStories: false
+      },
+      appTree
+    );
+    expect(tree.exists('libs/test-ui-lib/.storybook/addons.js')).toBeTruthy();
+    expect(tree.exists('libs/test-ui-lib/.storybook/config.js')).toBeTruthy();
+    expect(
+      tree.exists('libs/test-ui-lib/.storybook/tsconfig.json')
+    ).toBeTruthy();
+    expect(tree.exists('apps/test-ui-lib-e2e/cypress.json')).toBeFalsy();
+    expect(
+      tree.exists(
+        'libs/test-ui-lib/src/lib/test-button/test-button.component.stories.ts'
+      )
+    ).toBeFalsy();
+    expect(
+      tree.exists(
+        'libs/test-ui-lib/src/lib/test-other/test-other.component.stories.ts'
+      )
+    ).toBeFalsy();
+    expect(
+      tree.exists(
+        'apps/test-ui-lib-e2e/src/integration/test-button/test-button.component.spec.ts'
+      )
+    ).toBeFalsy();
+    expect(
+      tree.exists(
+        'apps/test-ui-lib-e2e/src/integration/test-other/test-other.component.spec.ts'
+      )
+    ).toBeFalsy();
+  });
+
   it('should configure everything at once', async () => {
     const tree = await runSchematic(
       'storybook-configuration',
