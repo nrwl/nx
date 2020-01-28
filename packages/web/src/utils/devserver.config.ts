af19a4d2d7719156e0d351692dfd04aa762607c4
Krzysztof <czaplejewicz.krzysztof@gmail.com>
2020-01-28 15:45:32 +0100

    feat(react): add an options --maxWorkers and --memoryLimit to nx-serve.
    `nx serve —maxWorkers=1 —memoryLimit=1024`
    
    Closes #2366

--- packages/web/src/utils/devserver.config.ts
+++ packages/web/src/utils/devserver.config.ts
@@ -34,14 +34,17 @@ export function getDevServerConfig(
     serveOptions,
     buildOptions
   );
-  webpackConfig.plugins = [...webpackConfig.plugins,
+  webpackConfig.plugins = [
+    ...webpackConfig.plugins,
     new ForkTsCheckerWebpackPlugin({
-    memoryLimit:
-      serveOptions.memoryLimit ||
-      ForkTsCheckerWebpackPlugin.DEFAULT_MEMORY_LIMIT,
-    workers: serveOptions.maxWorkers || ForkTsCheckerWebpackPlugin.TWO_CPUS_FREE,
-    useTypescriptIncrementalApi: false
-  })];
+      memoryLimit:
+        serveOptions.memoryLimit ||
+        ForkTsCheckerWebpackPlugin.DEFAULT_MEMORY_LIMIT,
+      workers:
+        serveOptions.maxWorkers || ForkTsCheckerWebpackPlugin.TWO_CPUS_FREE,
+      useTypescriptIncrementalApi: false
+    })
+  ];
   if (serveOptions.liveReload) {
     webpackConfig.entry['main'].unshift(getLiveReloadEntry(serveOptions));
   }
