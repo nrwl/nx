86023dbbc6370031ebead2f8d9999702cd5236f9
Rares Matei <matei.rar@gmail.com>
2020-02-06 16:24:24 +0000

    fix(testing): revert changes to jest migration 9-0-0
    ISSUES CLOSED: #2345

--- packages/jest/src/migrations/update-9-0-0/update-9-0-0.spec.ts
+++ packages/jest/src/migrations/update-9-0-0/update-9-0-0.spec.ts
@@ -4,7 +4,6 @@ import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 import { createEmptyWorkspace } from '@nrwl/workspace/testing';
 import { serializeJson } from '@nrwl/workspace';
-import { readFileSync } from 'fs';
 
 describe('Update 9.0.0', () => {
   let initialTree: Tree;
@@ -26,7 +25,6 @@ describe('Update 9.0.0', () => {
     initialTree.overwrite(
       'workspace.json',
       serializeJson({
-        version: 1,
         projects: {
           'angular-one': {
             root: 'apps/angular-one/',
@@ -45,8 +43,7 @@ describe('Update 9.0.0', () => {
               test: {
                 builder: '@nrwl/jest:jest',
                 options: {
-                  jestConfig: 'apps/angular-two/jest.config.js',
-                  passWithNoTests: false
+                  jestConfig: 'apps/angular-two/jest.config.js'
                 }
               }
             }
@@ -59,20 +56,6 @@ describe('Update 9.0.0', () => {
                 options: {
                   jestConfig: 'apps/non-angular-one/jest.config.js'
                 }
-              }
-            }
-          },
-          other1: {
-            architect: {
-              'other-architect': {
-                builder: 'other',
-                options: {
-                  foo: 'bar'
-                }
-              },
-              test: {
-                builder: '@nrwl/jest:jest',
-                options: {}
               }
             }
           }
@@ -186,96 +169,5 @@ describe('Update 9.0.0', () => {
     expect(updateJestNonAngularOne).not.toContain(
       'jest-preset-angular/build/HTMLCommentSerializer.js'
     );
-  });
-
-  it('should remove passWithNoTests in jest.config.js', async () => {
-    initialTree.create(
-      'jest.config.js',
-      readFileSync(
-        path.join(__dirname, './test-files/jest.config.js')
-      ).toString()
-    );
-    const initialJestConfigFile = initialTree.read('jest.config.js').toString();
-    expect(initialJestConfigFile).toContain('passWithNoTests: true');
-
-    const result = await schematicRunner
-      .runSchematicAsync('update-9.0.0', {}, initialTree)
-      .toPromise();
-
-    const updatedJestConfigFile = result.readContent('jest.config.js');
-    expect(updatedJestConfigFile).not.toContain('passWithNoTests: true');
-    console.log(updatedJestConfigFile);
-  });
-
-  it('should add passWithNoTests to workspace.json where it does not exist', async () => {
-    await schematicRunner
-      .runSchematicAsync('update-9.0.0', {}, initialTree)
-      .toPromise();
-
-    const workspaceJson = readJsonInTree(initialTree, 'workspace.json');
-    expect(
-      workspaceJson.projects['angular-one'].architect.test.options
-        .passWithNoTests
-    ).toBeTruthy();
-    expect(
-      workspaceJson.projects['angular-two'].architect.test.options
-        .passWithNoTests
-    ).toBeFalsy();
-    expect(
-      workspaceJson.projects.other1.architect['other-architect'].options
-        .passWithNoTests
-    ).toBeUndefined();
-    expect(
-      workspaceJson.projects.other1.architect.test.options.passWithNoTests
-    ).toBeTruthy();
-  });
-
-  it('should add passWithNoTests to angular.json where it does not exist', async () => {
-    initialTree.create(
-      'angular.json',
-      JSON.stringify({
-        version: 1,
-        projects: {
-          frontend: {
-            architect: {
-              jest1: {
-                builder: '@nrwl/jest:jest',
-                options: {
-                  foo: 'bar'
-                }
-              },
-              jest2: {
-                builder: '@nrwl/jest:jest',
-                options: {
-                  foo: 'bar',
-                  passWithNoTests: false
-                }
-              },
-              other1: {
-                options: {
-                  foo: 'bar'
-                }
-              }
-            }
-          }
-        }
-      })
-    );
-
-    await schematicRunner
-      .runSchematicAsync('update-9.0.0', {}, initialTree)
-      .toPromise();
-
-    const angularJson = readJsonInTree(initialTree, 'angular.json');
-
-    expect(
-      angularJson.projects.frontend.architect.jest1.options.passWithNoTests
-    ).toBeTruthy();
-    expect(
-      angularJson.projects.frontend.architect.jest2.options.passWithNoTests
-    ).toBeFalsy();
-    expect(
-      angularJson.projects.frontend.architect.other1.options.passWithNoTests
-    ).toBeUndefined();
   });
 });
