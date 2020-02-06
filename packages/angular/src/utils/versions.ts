3c6d8c67f8e82110360a7672df04a054cb20c85e
Jason Jean <jasonjean1993@gmail.com>
2020-02-06 13:01:04 -0500

    feat(angular): update angular to 9-rc.14 (#2437)

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = '*';
-export const angularVersion = '9.0.0-rc.12';
-export const angularDevkitVersion = '0.900.0-rc.12';
+export const angularVersion = '9.0.0-rc.14';
+export const angularDevkitVersion = '0.900.0-rc.14';
 export const angularJsVersion = '1.6.6';
 export const ngrxVersion = '8.5.0';
 export const rxjsVersion = '~6.5.0';
