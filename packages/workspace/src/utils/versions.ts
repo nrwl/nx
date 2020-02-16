c62e1261078a81599fd36c900aef3bea280ac11f
Victor Savkin <mail@vsavkin.com>
2020-02-16 12:02:07 -0500

    fix(core): update ts eslint version to remove the warning

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -3,6 +3,6 @@ export const nxVersion = '*';
 export const angularCliVersion = '9.0.1';
 export const typescriptVersion = '~3.7.4';
 export const prettierVersion = '1.18.2';
-export const typescriptESLintVersion = '2.3.2';
+export const typescriptESLintVersion = '2.19.2';
 export const eslintVersion = '6.1.0';
 export const eslintConfigPrettierVersion = '6.0.0';
