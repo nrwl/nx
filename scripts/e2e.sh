30721d517c41a81ec6d22a936db7bdb6a3ada668
maxkoretskyi <max@nrwl.io>
2020-02-18 15:02:10 +0200

    fix(repo): fix `yarn e2e create-playground`
    Added a conditional check to execute the file in the correct directory

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -7,9 +7,14 @@ mkdir -p tmp/angular
 mkdir -p tmp/nx
 
 if [ -n "$1" ]; then
-  jest --maxWorkers=1 ./build/e2e/$1.test.js
+  TEST_FILE="./build/e2e/$1.test.js"
+  COMMAND_FILE="./build/e2e/commands/$1.test.js"
+
+  if [ -f "$TEST_FILE" ]; then
+    jest --maxWorkers=1 $TEST_FILE
+  else
+    jest --maxWorkers=1 $COMMAND_FILE
+  fi
 else
   jest --maxWorkers=1 ./build/e2e/*.test.js
 fi
-
-
