fc98d4a41829ed9d9acce9d3bc092259fd1fc1d1
Jonathan Cammisuli <jon@cammisuli.ca>
2020-02-20 09:11:03 -0500

    fix(node): set tsconfig target to es2015 for nest apps
    ISSUES CLOSED: #1283

--- packages/nest/src/schematics/application/application.spec.ts
+++ packages/nest/src/schematics/application/application.spec.ts
@@ -1,6 +1,7 @@
 import { Tree } from '@angular-devkit/schematics';
 import { createEmptyWorkspace } from '@nrwl/workspace/testing';
 import { runSchematic } from '../../utils/testing';
+import { readJsonInTree } from '@nrwl/workspace';
 
 describe('app', () => {
   let appTree: Tree;
@@ -16,5 +17,11 @@ describe('app', () => {
       `await NestFactory.create(AppModule);`
     );
     expect(tree.exists('apps/my-node-app/src/app/app.module.ts')).toBeTruthy();
+  });
+
+  it('should have es2015 as the tsconfig target', async () => {
+    const tree = await runSchematic('app', { name: 'myNodeApp' }, appTree);
+    const tsconfig = readJsonInTree(tree, 'apps/my-node-app/tsconfig.json');
+    expect(tsconfig.compilerOptions.target).toBe('es2015');
   });
 });
