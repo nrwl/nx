39ddf0cf0b54c8e4881ad83a614393603c885171
Victor Savkin <mail@vsavkin.com>
2020-02-26 11:17:08 -0500

    feat(core): memoize project source hashing

--- scripts/e2e-ci1.sh
+++ scripts/e2e-ci1.sh
@@ -6,12 +6,9 @@ rm -rf tmp
 mkdir -p tmp/angular
 mkdir -p tmp/nx
 
-# Please keep this in alphabetical order
-# This should be every file under e2e except for utils.js up to next.test.ts
 export SELECTED_CLI=$1
 jest --maxWorkers=1 ./build/e2e/new.test.js &&
 jest --maxWorkers=1 ./build/e2e/affected.test.js &&
 jest --maxWorkers=1 ./build/e2e/affected-git.test.js &&
-# jest --maxWorkers=1 ./build/e2e/bazel.test.js &&
 jest --maxWorkers=1 ./build/e2e/command-line.test.js &&
 jest --maxWorkers=1 ./build/e2e/cypress.test.js
--- scripts/e2e-ci2.sh
+++ scripts/e2e-ci2.sh
@@ -6,8 +6,6 @@ rm -rf tmp
 mkdir -p tmp/angular
 mkdir -p tmp/nx
 
-# Please keep this in alphabetical order
-# This should be every file under e2e except for utils.js after ng-add.test.ts
 export SELECTED_CLI=$1
 jest --maxWorkers=1 ./build/e2e/help.test.js &&
 jest --maxWorkers=1 ./build/e2e/jest.test.js &&
