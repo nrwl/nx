ff6b4345485d0da22578adbb447a13fd09b7242b
Victor Savkin <mail@vsavkin.com>
2020-02-27 17:12:05 -0500

    feat(core): add ability to run targets against project's deps

--- packages/workspace/src/command-line/run-one.ts
+++ packages/workspace/src/command-line/run-one.ts
@@ -1,26 +1,64 @@
 import { runCommand } from '../tasks-runner/run-command';
-import { createProjectGraph } from '../core/project-graph';
+import {
+  createProjectGraph,
+  onlyWorkspaceProjects,
+  ProjectGraph,
+  withDeps
+} from '../core/project-graph';
 import { readEnvironment } from '../core/file-utils';
 import { EmptyReporter } from '../tasks-runner/empty-reporter';
+import { splitArgsIntoNxArgsAndOverrides } from './utils';
+import { DefaultReporter } from '../tasks-runner/default-reporter';
 
 export function runOne(opts: {
   project: string;
   target: string;
-  skipNxCache: boolean;
   configuration: string;
-  overrides: any;
+  parsedArgs: any;
 }): void {
-  const projectGraph = createProjectGraph();
-  const projects = [projectGraph.nodes[opts.project]];
-  const env = readEnvironment(opts.target, {
-    [opts.project]: projectGraph.nodes[opts.project]
-  });
-  runCommand(
-    projects,
-    projectGraph,
-    env,
-    opts,
-    opts.overrides,
-    new EmptyReporter()
+  const { nxArgs, overrides } = splitArgsIntoNxArgsAndOverrides(
+    {
+      ...opts.parsedArgs,
+      configuration: opts.configuration,
+      target: opts.target,
+      _: []
+    },
+    'run-one'
   );
+
+  const projectGraph = createProjectGraph();
+  const { projects, projectsMap } = getProjects(
+    projectGraph,
+    nxArgs.withDeps,
+    opts.project
+  );
+  const env = readEnvironment(opts.target, projectsMap);
+  const reporter = nxArgs.withDeps
+    ? new DefaultReporter()
+    : new EmptyReporter();
+
+  runCommand(projects, projectGraph, env, nxArgs, overrides, reporter);
+}
+
+function getProjects(
+  projectGraph: ProjectGraph,
+  includeDeps: boolean,
+  project: string
+) {
+  let projects = [projectGraph.nodes[project]];
+  let projectsMap = {
+    [project]: projectGraph.nodes[project]
+  };
+
+  if (includeDeps) {
+    const projectWithDeps = onlyWorkspaceProjects(
+      withDeps(projectGraph, projects)
+    ).nodes;
+    return {
+      projects: Object.values(projectWithDeps),
+      projectsMap: projectWithDeps
+    };
+  } else {
+    return { projects, projectsMap };
+  }
 }
