6aa598eff7d0d39f14faf39e6faedf459e76fffb
Jason Jean <jasonjean1993@gmail.com>
2020-02-28 13:52:09 -0500

    feat(core): update prettier to 1.19.1

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -2,7 +2,7 @@ export const nxVersion = '*';
 
 export const angularCliVersion = '9.0.1';
 export const typescriptVersion = '~3.7.4';
-export const prettierVersion = '1.18.2';
+export const prettierVersion = '1.19.1';
 export const typescriptESLintVersion = '2.19.2';
 export const eslintVersion = '6.1.0';
 export const eslintConfigPrettierVersion = '6.0.0';
