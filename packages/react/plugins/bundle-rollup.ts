1e9186c542fa1fc4a2c66126f0df30eedabb7588
Jack Hsu <jack.hsu@gmail.com>
2020-03-04 09:11:26 -0500

    fix(react): fix rollup options extension to be compatible with output arrays

--- packages/react/plugins/bundle-rollup.ts
+++ packages/react/plugins/bundle-rollup.ts
@@ -1,11 +1,23 @@
-function getRollupOptions(options: any) {
-  const globals = options.output.globals || {};
-  globals.react = 'React';
-  globals['react-dom'] = 'ReactDOM';
-  globals['styled-components'] = 'styled';
-  globals['@emotion/core'] = 'emotionCore';
-  globals['@emotion/styled'] = 'emotionStyled';
-  options.output.globals = globals;
+import * as rollup from 'rollup';
+
+function getRollupOptions(options: rollup.RollupOptions) {
+  const extraGlobals = {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    'styled-components': 'styled',
+    '@emotion/core': 'emotionCore',
+    '@emotion/styled': 'emotionStyled'
+  };
+  if (Array.isArray(options.output)) {
+    options.output.forEach(o => {
+      o.globals = { ...o.globals, ...extraGlobals };
+    });
+  } else {
+    options.output = {
+      ...options.output,
+      ...extraGlobals
+    };
+  }
   return options;
 }
 
