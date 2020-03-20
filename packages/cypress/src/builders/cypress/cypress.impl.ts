417ff21aac1acd770d604a8308c4a14ee7246e0b
Florian Tischler <Florian.Tischler@gmx.net>
2020-03-20 20:10:19 +0100

    fix(testing): cypress cannot be started in chrome headless mode (#2683)

--- packages/cypress/src/builders/cypress/cypress.impl.ts
+++ packages/cypress/src/builders/cypress/cypress.impl.ts
@@ -155,7 +155,7 @@ function initCypress(
   }
 
   options.exit = exit;
-  options.headed = !headless;
+  options.headless = headless;
   options.record = record;
   options.key = key;
   options.parallel = parallel;
