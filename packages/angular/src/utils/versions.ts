37b14545bfdfc7e8bceccafc55e9e866242a004f
Jonathan Cammisuli <jon@cammisuli.ca>
2020-03-27 13:51:32 -0400

    feat(testing): update Jest and its dependencies to v25

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -4,4 +4,4 @@ export const angularDevkitVersion = '0.900.1';
 export const angularJsVersion = '1.6.6';
 export const ngrxVersion = '9.0.0';
 export const rxjsVersion = '~6.5.0';
-export const jestPresetAngularVersion = '8.0.0';
+export const jestPresetAngularVersion = '8.1.2';
