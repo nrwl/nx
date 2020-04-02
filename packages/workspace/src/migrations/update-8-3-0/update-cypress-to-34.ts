efae1694cc3f28aefda2bb5d0f2cb7acc7d767b2
Jason Jean <jasonjean1993@gmail.com>
2020-04-02 12:41:08 -0400

    cleanup(misc): refactor how installs are added during schematics (#2788)

--- packages/workspace/src/migrations/update-8-3-0/update-cypress-to-34.ts
+++ packages/workspace/src/migrations/update-8-3-0/update-cypress-to-34.ts
@@ -1,6 +1,5 @@
-import { formatFiles, updateJsonInTree } from '@nrwl/workspace';
-import { chain, SchematicContext } from '@angular-devkit/schematics';
-import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { addInstallTask, formatFiles, updateJsonInTree } from '@nrwl/workspace';
+import { chain } from '@angular-devkit/schematics';
 
 const updateCypress = updateJsonInTree('package.json', json => {
   json.devDependencies = json.devDependencies || {};
@@ -11,10 +10,6 @@ const updateCypress = updateJsonInTree('package.json', json => {
   return json;
 });
 
-const addInstall = (_: any, context: SchematicContext) => {
-  context.addTask(new NodePackageInstallTask());
-};
-
 export default function() {
-  return chain([updateCypress, addInstall, formatFiles()]);
+  return chain([updateCypress, addInstallTask(), formatFiles()]);
 }
