6f0d3f29c0d799bae4ab139627ae709afbd8f8ad
Benedikt D. Valdez Stefánsson <bvstefansson@ossur.com>
2020-05-13 10:16:58 +0000

    fix(linter): eslint version should match >= 6.1.0
    Closes #3001

--- packages/linter/src/builders/lint/lint.impl.ts
+++ packages/linter/src/builders/lint/lint.impl.ts
@@ -36,7 +36,7 @@ async function run(options: Schema, context: BuilderContext): Promise<any> {
     !version ||
     version.length < 2 ||
     Number(version[0]) < 6 ||
-    Number(version[1]) < 1
+    (Number(version[0]) === 6 && Number(version[1]) < 1)
   ) {
     throw new Error('ESLint must be version 6.1 or higher.');
   }
