a4fa80a351c20e289be1f67e359cd035d5b27c59
Hanna Walter <hanna.walter@bigtincan.com>
2020-05-26 10:08:04 -0600

    fix(core): remove matching implicitDependency when removing a project

--- packages/workspace/src/schematics/remove/lib/update-nx-json.ts
+++ packages/workspace/src/schematics/remove/lib/update-nx-json.ts
@@ -9,6 +9,15 @@ import { Schema } from '../schema';
 export function updateNxJson(schema: Schema) {
   return updateJsonInTree<NxJson>('nx.json', (json) => {
     delete json.projects[schema.projectName];
+
+    Object.values(json.projects).forEach((project) => {
+      if (project.implicitDependencies) {
+        project.implicitDependencies = project.implicitDependencies.filter(
+          (dep) => dep !== schema.projectName
+        );
+      }
+    });
+
     return json;
   });
 }
