1c599e47d4628be2c4d301e13a8f6860ac9d81e8
Jack Hsu <jack.hsu@gmail.com>
2020-06-02 14:14:44 -0400

    fix(react): sync up babel versions to avoid issues between @babel/preset-env 7.8.4-7.8.6

--- packages/jest/src/utils/versions.ts
+++ packages/jest/src/utils/versions.ts
@@ -3,8 +3,8 @@ export const jestVersion = '25.2.3';
 export const jestTypesVersion = '25.1.4';
 export const tsJestVersion = '25.2.1';
 
-export const babelCoreVersion = '7.8.4';
-export const babelPresetEnvVersion = '7.8.4';
+export const babelCoreVersion = '7.8.7';
+export const babelPresetEnvVersion = '7.8.7';
 export const babelPresetTypescriptVersion = '7.8.3';
 export const babelPresetReactVersion = '7.8.3';
 export const babelJestVersion = '25.1.0';
--- packages/storybook/src/utils/versions.ts
+++ packages/storybook/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxVersion = '*';
 export const storybookVersion = '5.3.9';
-export const babelCoreVersion = '7.8.3';
+export const babelCoreVersion = '7.8.7';
 export const babelLoaderVersion = '8.0.6';
 export const babelPresetTypescriptVersion = '7.8.3';
