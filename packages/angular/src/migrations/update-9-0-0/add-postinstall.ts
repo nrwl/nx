13280b528273915a4b1fd5d5cb1cb1823f6d8947
Victor Savkin <mail@vsavkin.com>
2020-06-10 15:04:53 -0400

    fix(angular): always append ngcc postinstall script

--- packages/angular/src/migrations/update-9-0-0/add-postinstall.ts
+++ packages/angular/src/migrations/update-9-0-0/add-postinstall.ts
@@ -1,26 +1,16 @@
 import { formatFiles, updateJsonInTree } from '@nrwl/workspace';
-import { stripIndents } from '@angular-devkit/core/src/utils/literals';
 import { chain } from '@angular-devkit/schematics';
 
 export default function () {
   return chain([
     updateJsonInTree('package.json', (json, context) => {
       json.scripts = json.scripts || {};
-      if (json.scripts.postinstall) {
-        context.logger.warn(
-          stripIndents`
-            ---------------------------------------------------------------------------------------
-            Angular Ivy requires you to run ngcc after every npm install.
-            The easiest way to accomplish this is to update your postinstall script to invoke ngcc.
-            ---------------------------------------------------------------------------------------
-          `
-        );
-      } else {
-        context.logger.info(
-          stripIndents`A "postinstall" script has been added to package.json to run ngcc.`
-        );
-        json.scripts.postinstall =
-          'ngcc --properties es2015 browser module main --first-only --create-ivy-entry-points';
+      const command =
+        'ngcc --properties es2015 browser module main --first-only --create-ivy-entry-points';
+      if (!json.scripts.postinstall) {
+        json.scripts.postinstall = command;
+      } else if (!json.scripts.postinstall.includes('ngcc')) {
+        json.scripts.postinstall = `${json.scripts.postinstall} && ${command}`;
       }
       return json;
     }),
