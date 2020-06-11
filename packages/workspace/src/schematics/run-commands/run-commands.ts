e46e93f496423128d124087313509c71093627ad
Victor Savkin <mail@vsavkin.com>
2020-06-11 11:36:03 -0400

    fix(misc): fix run-command schematic to not error when no architect section

--- packages/workspace/src/schematics/run-commands/run-commands.ts
+++ packages/workspace/src/schematics/run-commands/run-commands.ts
@@ -4,10 +4,12 @@ import { updateWorkspaceInTree } from '@nrwl/workspace';
 
 export default function (schema: Schema): Rule {
   return updateWorkspaceInTree((json) => {
-    if (!json.projects[schema.project]) {
+    const project = json.projects[schema.project];
+    if (!project) {
       throw new Error(`Invalid project name "${schema.project}"`);
     }
-    json.projects[schema.project].architect[schema.name] = {
+    project.architect = project.architect || {};
+    project.architect[schema.name] = {
       builder: '@nrwl/workspace:run-commands',
       options: {
         command: schema.command,
