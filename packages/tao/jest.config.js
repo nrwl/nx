dc0c52b31d3754399ab47e853a9c256378865da0
Victor Savkin <mail@vsavkin.com>
2020-07-16 14:46:02 -0400

    fix(repo): update the repo to nx 10

--- packages/tao/jest.config.js
+++ packages/tao/jest.config.js
@@ -5,4 +5,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
