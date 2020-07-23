8509ded7e4de1537c3af6606cdfcafc2f79f5a3d
Jonathan Cammisuli <jon@cammisuli.ca>
2020-07-23 11:23:34 -0400

    fix(angular): update tsconfig.lib.json to have "include" (#3389)

--- packages/angular/src/schematics/library/lib/update-tsconfig.ts
+++ packages/angular/src/schematics/library/lib/update-tsconfig.ts
@@ -1,33 +1,37 @@
-import {
-  chain,
-  Rule,
-  SchematicContext,
-  SchematicsException,
-  Tree,
-} from '@angular-devkit/schematics';
+import { chain, Rule, SchematicsException } from '@angular-devkit/schematics';
 import { updateJsonInTree } from '@nrwl/workspace';
 import { NormalizedSchema } from './normalized-schema';
 
+function updateRootConfig(options: NormalizedSchema) {
+  return updateJsonInTree('tsconfig.base.json', (json) => {
+    const c = json.compilerOptions;
+    c.paths = c.paths || {};
+    delete c.paths[options.name];
+
+    if (c.paths[options.importPath]) {
+      throw new SchematicsException(
+        `You already have a library using the import path "${options.importPath}". Make sure to specify a unique one.`
+      );
+    }
+
+    c.paths[options.importPath] = [
+      `libs/${options.projectDirectory}/src/index.ts`,
+    ];
+
+    return json;
+  });
+}
+
+function updateProjectConfig(options: NormalizedSchema) {
+  return updateJsonInTree(
+    `${options.projectRoot}/tsconfig.lib.json`,
+    (json) => {
+      json.include = ['**/*.ts'];
+      return json;
+    }
+  );
+}
+
 export function updateTsConfig(options: NormalizedSchema): Rule {
-  return chain([
-    (host: Tree, context: SchematicContext) => {
-      return updateJsonInTree('tsconfig.base.json', (json) => {
-        const c = json.compilerOptions;
-        c.paths = c.paths || {};
-        delete c.paths[options.name];
-
-        if (c.paths[options.importPath]) {
-          throw new SchematicsException(
-            `You already have a library using the import path "${options.importPath}". Make sure to specify a unique one.`
-          );
-        }
-
-        c.paths[options.importPath] = [
-          `libs/${options.projectDirectory}/src/index.ts`,
-        ];
-
-        return json;
-      })(host, context);
-    },
-  ]);
+  return chain([updateRootConfig(options), updateProjectConfig(options)]);
 }
