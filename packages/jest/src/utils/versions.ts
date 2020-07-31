8eb767fd53df15b679446773035a962e7c49ff40
Jonathan Cammisuli <jon@cammisuli.ca>
2020-07-31 16:30:47 -0400

    feat(testing): update jest to v26

--- packages/jest/src/utils/versions.ts
+++ packages/jest/src/utils/versions.ts
@@ -1,10 +1,10 @@
 export const nxVersion = '*';
-export const jestVersion = '25.2.3';
-export const jestTypesVersion = '25.1.4';
-export const tsJestVersion = '25.2.1';
+export const jestVersion = '26.2.2';
+export const jestTypesVersion = '26.0.8';
+export const tsJestVersion = '26.1.4';
 
 export const babelCoreVersion = '7.9.6';
 export const babelPresetEnvVersion = '7.9.6';
 export const babelPresetTypescriptVersion = '7.9.0';
 export const babelPresetReactVersion = '7.9.4';
-export const babelJestVersion = '25.1.0';
+export const babelJestVersion = '26.2.2';
