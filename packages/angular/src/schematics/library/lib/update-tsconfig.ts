d393b0af6172e9c56b7908d1cee850dd5f911389
Juri <juri.strumpflohner@gmail.com>
2020-08-03 10:55:15 +0200

    fix(core): accidental removal of libsDir(host)

--- packages/angular/src/schematics/library/lib/update-tsconfig.ts
+++ packages/angular/src/schematics/library/lib/update-tsconfig.ts
@@ -1,25 +1,27 @@
 import { chain, Rule, SchematicsException } from '@angular-devkit/schematics';
 import { updateJsonInTree } from '@nrwl/workspace';
 import { NormalizedSchema } from './normalized-schema';
+import { libsDir } from '@nrwl/workspace/src/utils/ast-utils';
 
 function updateRootConfig(options: NormalizedSchema) {
-  return updateJsonInTree('tsconfig.base.json', (json) => {
-    const c = json.compilerOptions;
-    c.paths = c.paths || {};
-    delete c.paths[options.name];
+  return (host) =>
+    updateJsonInTree('tsconfig.base.json', (json) => {
+      const c = json.compilerOptions;
+      c.paths = c.paths || {};
+      delete c.paths[options.name];
 
-    if (c.paths[options.importPath]) {
-      throw new SchematicsException(
-        `You already have a library using the import path "${options.importPath}". Make sure to specify a unique one.`
-      );
-    }
+      if (c.paths[options.importPath]) {
+        throw new SchematicsException(
+          `You already have a library using the import path "${options.importPath}". Make sure to specify a unique one.`
+        );
+      }
 
-    c.paths[options.importPath] = [
-      `libs/${options.projectDirectory}/src/index.ts`,
-    ];
+      c.paths[options.importPath] = [
+        `${libsDir(host)}/${options.projectDirectory}/src/index.ts`,
+      ];
 
-    return json;
-  });
+      return json;
+    });
 }
 
 function updateProjectConfig(options: NormalizedSchema) {
