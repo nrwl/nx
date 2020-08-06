8136b7ad4057cfa99b224a33de8fa4a381afb85e
Przemyslaw Figura <sherman@pf1gura.online>
2020-08-06 05:09:37 +0200

    fix(nest): add default linter for new nest projects (#3469)
    * fix(nest): add default linter for new nest projects
    
    Closes: #3063

--- packages/workspace/src/schematics/shared-new/shared-new.ts
+++ packages/workspace/src/schematics/shared-new/shared-new.ts
@@ -279,7 +279,10 @@ function setDefaultLinter(linter: string) {
     json.schematics['@nrwl/nx-plugin'] = {
       plugin: { linter },
     };
-    json.schematics['@nrwl/nest'] = { application: { linter } };
+    json.schematics['@nrwl/nest'] = {
+      application: { linter },
+      library: { linter },
+    };
     json.schematics['@nrwl/express'] = {
       application: { linter },
       library: { linter },
