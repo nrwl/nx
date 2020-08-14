2977b308854a92a9fe7a6fae54f9324a6fb5a422
Jeremy Forsythe <jdforsythe@gmail.com>
2020-08-14 19:42:21 -0400

    cleanup(misc): formatter changes

--- packages/workspace/src/schematics/remove/lib/update-workspace.ts
+++ packages/workspace/src/schematics/remove/lib/update-workspace.ts
@@ -8,13 +8,20 @@ import { updateWorkspaceInTree, getWorkspacePath } from '@nrwl/workspace';
  * @param schema The options provided to the schematic
  */
 export function updateWorkspace(schema: Schema) {
-  return updateWorkspaceInTree((workspace, context: SchematicContext, host: Tree) => {
-    delete workspace.projects[schema.projectName];
-    if (workspace.defaultProject && workspace.defaultProject === schema.projectName) {
-      delete workspace.defaultProject;
-      const workspacePath = getWorkspacePath(host);
-      context.logger.warn(`Default project was removed in ${workspacePath} because it was "${schema.projectName}". If you want a default project you should define a new one.`);
+  return updateWorkspaceInTree(
+    (workspace, context: SchematicContext, host: Tree) => {
+      delete workspace.projects[schema.projectName];
+      if (
+        workspace.defaultProject &&
+        workspace.defaultProject === schema.projectName
+      ) {
+        delete workspace.defaultProject;
+        const workspacePath = getWorkspacePath(host);
+        context.logger.warn(
+          `Default project was removed in ${workspacePath} because it was "${schema.projectName}". If you want a default project you should define a new one.`
+        );
+      }
+      return workspace;
     }
-    return workspace;
-  });
+  );
 }
