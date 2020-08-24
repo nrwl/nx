e3b21ab810b1cfcf0a80c10cf98a5f70428cdb56
Juri <juri.strumpflohner@gmail.com>
2020-08-24 14:50:19 +0200

    fix(storybook): linting migration

--- packages/storybook/src/migrations/update-10-0-12/update-10-0-12.ts
+++ packages/storybook/src/migrations/update-10-0-12/update-10-0-12.ts
@@ -27,98 +27,96 @@ interface ProjectDefinition {
 }
 
 export default function (tree: Tree, context: SchematicContext) {
-  return chain([update(tree, context), formatFiles()]);
+  return chain([update, formatFiles()]);
 }
 
 function update(tree: Tree, context: SchematicContext): Rule {
   return updateWorkspaceInTree((config, context, tree) => {
     Object.entries<ProjectDefinition>(config.projects).forEach(
       ([projectName, projectConfig]) => {
-        updateLintTarget(tree, { projectName, projectConfig });
+        updateLintTarget(tree, context, { projectName, projectConfig });
       }
     );
 
     return config;
   });
+}
 
-  function updateLintTarget(
-    tree: Tree,
-    options: {
-      projectName: string;
-      projectConfig: ProjectDefinition;
-    }
+function updateLintTarget(
+  tree: Tree,
+  context: SchematicContext,
+  options: {
+    projectName: string;
+    projectConfig: ProjectDefinition;
+  }
+) {
+  const architect = options.projectConfig.architect;
+
+  const paths = {
+    tsConfig: path.join(options.projectConfig.root, 'tsconfig.json'),
+    tsConfigLib: path.join(options.projectConfig.root, 'tsconfig.lib.json'),
+    tsConfigStorybook: path.join(
+      options.projectConfig.root,
+      '.storybook/tsconfig.json'
+    ),
+  };
+
+  const hasStorybookConfig =
+    architect.storybook && tree.exists(paths.tsConfigStorybook);
+
+  if (!hasStorybookConfig) {
+    context.logger.info(
+      `${options.projectName}: no storybook configured. skipping migration...`
+    );
+    return;
+  }
+
+  const isReactProject = isFramework('react', {
+    uiFramework: architect.storybook.options?.uiFramework as Parameters<
+      typeof isFramework
+    >[1]['uiFramework'],
+  });
+
+  const tsConfig = {
+    main: getTsConfigContent(tree, paths.tsConfig),
+    lib: getTsConfigContent(tree, paths.tsConfigLib),
+    storybook: getTsConfigContent(tree, paths.tsConfigStorybook),
+  };
+
+  if (isReactProject && Array.isArray(tsConfig.lib.exclude)) {
+    tsConfig.lib.exclude = uniqueArray([
+      ...tsConfig.lib.exclude,
+      '**/*.stories.jsx',
+      '**/*.stories.tsx',
+    ]);
+    tree.overwrite(paths.tsConfigLib, serializeJson(tsConfig.lib));
+  }
+
+  if (Array.isArray(tsConfig.main.references)) {
+    tsConfig.main.references.push({ path: './.storybook/tsconfig.json' });
+    tree.overwrite(paths.tsConfig, serializeJson(tsConfig.main));
+  }
+
+  if (isReactProject && Array.isArray(tsConfig.storybook.exclude)) {
+    tsConfig.storybook.exclude = uniqueArray([
+      ...tsConfig.storybook.exclude,
+      '../**/*.spec.js',
+      '../**/*.spec.tsx',
+      '../**/*.spec.jsx',
+    ]);
+    tree.overwrite(paths.tsConfigStorybook, serializeJson(tsConfig.storybook));
+  }
+
+  // update workspace
+  if (
+    architect.lint &&
+    architect.lint.options &&
+    Array.isArray(architect.lint.options.tsConfig)
   ) {
-    const architect = options.projectConfig.architect;
-
-    const paths = {
-      tsConfig: path.join(options.projectConfig.root, 'tsconfig.json'),
-      tsConfigLib: path.join(options.projectConfig.root, 'tsconfig.lib.json'),
-      tsConfigStorybook: path.join(
-        options.projectConfig.root,
-        '.storybook/tsconfig.json'
-      ),
-    };
-
-    const hasStorybookConfig =
-      architect.storybook && tree.exists(paths.tsConfigStorybook);
-
-    if (!hasStorybookConfig) {
-      context.logger.info(
-        `${options.projectName}: no storybook configured. skipping migration...`
-      );
-      return;
-    }
-
-    const isReactProject = isFramework('react', {
-      uiFramework: architect.storybook.options?.uiFramework as Parameters<
-        typeof isFramework
-      >[1]['uiFramework'],
-    });
-
-    const tsConfig = {
-      main: getTsConfigContent(tree, paths.tsConfig),
-      lib: getTsConfigContent(tree, paths.tsConfigLib),
-      storybook: getTsConfigContent(tree, paths.tsConfigStorybook),
-    };
-
-    if (isReactProject && Array.isArray(tsConfig.lib.exclude)) {
-      tsConfig.lib.exclude = uniqueArray([
-        ...tsConfig.lib.exclude,
-        '**/*.stories.jsx',
-        '**/*.stories.tsx',
-      ]);
-      tree.overwrite(paths.tsConfigLib, serializeJson(tsConfig.lib));
-    }
-
-    if (Array.isArray(tsConfig.main.references)) {
-      tsConfig.main.references.push({ path: './.storybook/tsconfig.json' });
-      tree.overwrite(paths.tsConfig, serializeJson(tsConfig.main));
-    }
-
-    if (isReactProject && Array.isArray(tsConfig.storybook.exclude)) {
-      tsConfig.storybook.exclude = uniqueArray([
-        ...tsConfig.storybook.exclude,
-        '../**/*.spec.js',
-        '../**/*.spec.tsx',
-        '../**/*.spec.jsx',
-      ]);
-      tree.overwrite(
-        paths.tsConfigStorybook,
-        serializeJson(tsConfig.storybook)
-      );
-    }
-
-    // update workspace
-    if (
-      architect.lint &&
-      architect.lint.options &&
-      Array.isArray(architect.lint.options.tsConfig)
-    ) {
-      architect.lint.options.tsConfig = uniqueArray([
-        ...architect.lint.options.tsConfig,
-        paths.tsConfigStorybook,
-      ]);
-    }
+    architect.lint.options.tsConfig = uniqueArray([
+      ...architect.lint.options.tsConfig,
+      paths.tsConfigStorybook,
+    ]);
   }
 }
 
