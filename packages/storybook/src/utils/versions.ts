97a3c28b6f70ec101dd802bc75868848615046b4
katerina <sk.katherine@gmail.com>
2020-08-28 18:24:33 +0300

    fix(storybook): wip - changed the version of storybook from rc to the current
    Just changed the version of storybook from the rc to the current one -  still wip

--- packages/storybook/src/utils/versions.ts
+++ packages/storybook/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxVersion = '*';
-export const storybookVersion = '6.0.0-rc.18';
+export const storybookVersion = '6.0.20';
 export const babelCoreVersion = '7.9.6';
 export const babelLoaderVersion = '8.1.0';
 export const babelPresetTypescriptVersion = '7.9.0';
