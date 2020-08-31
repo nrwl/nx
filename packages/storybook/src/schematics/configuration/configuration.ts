591d3e6ed3e94014ba01843b8db97bf4204fb0e5
katerina <sk.katherine@gmail.com>
2020-08-31 16:56:12 +0300

    notes about extra prompts of extra config files

--- packages/storybook/src/schematics/configuration/configuration.ts
+++ packages/storybook/src/schematics/configuration/configuration.ts
@@ -65,6 +65,56 @@ function createRootStorybookDir(projectName: string, js: boolean): Rule {
   return (tree: Tree, context: SchematicContext) => {
     context.logger.debug('adding .storybook folder to lib');
 
+    /**
+     * So, here we could check the configuration flag and see if we want to generate
+     * a storybook version 5 or version 6
+     * (or just look at which version we have installed, I guess)
+     * (so, is there a way to "get storybook version"?)
+     * 
+     * And have two folders, root-files-5 and root-files-6
+     * and then choose the correct one for the corresponding version
+     * 
+     * So, I guess the check should happen automatically (version of storybook)
+     * 
+     * Use case:
+     * A user has a huge app with Storybook v5 configured.
+     * User upgrades to Nx 10. All their storybooks are still v5. So, old config files, old stories.
+     * 
+     * They don't want their Storybook package to upgrade to Storybook v6 just yet, because
+     * it will break their builds. And they want the new stories they produce to be
+     * in version 5 still, for the same reason.
+     * When they have time to manually migrate all instances of Storybook v5 to Storybook v6, 
+     * then they will want their Storbook package to upgrade to v6.
+     * 
+     * All new Storybooks that are added or created should be in v6, if the package.json does
+     * not have Storybook.
+     */
+
+
+     /**
+      * Notes about manager.js
+      * 
+      * Potentially, we should add manager.js to the root folder, just an empty file.
+      * Let's discuss, but it should just be an empty file, which is then referenced
+      * in the lib-files, just to save the users from the trouble of creating
+      * that file themselves, in case they need it. 
+      * There's a change users will never use it, but some will. So, this 
+      * will save those the trouble of manually creating a manager.js file
+      * in root-files and then an importing one in all instances of storybook.
+      */
+
+      /**
+       * Notes about .html files (preview-head.html, preview-body.html, manager-head.html)
+       * 
+       * These files need to be created in each one separate instance of Storybook.
+       * We either do the same as we did with manager.js> just add them there, but most users
+       * will never use them, or let the users who want to add them, manually create them when and
+       * if they ever need them.
+       * 
+       * These files cannot be imported or referenced or required. There needs to be a one and 
+       * separate .html file in each one of the .storybook directories in the app, if it needs to be used
+       */
+
     return chain([
       applyWithSkipExisting(url('./root-files'), [js ? toJS() : noop()]),
     ])(tree, context);
@@ -77,6 +127,14 @@ function createLibStorybookDir(
   js: boolean
 ): Rule {
   return (tree: Tree, context: SchematicContext) => {
+
+    /**
+     * Here, same as above
+     * Check storybook version
+     * and use the correct folder
+     * lib-files-5 or lib-files-6
+     */
+
     context.logger.debug('adding .storybook folder to lib');
     const projectConfig = getProjectConfig(tree, projectName);
     return chain([
