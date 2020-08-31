270433fd3567533aeaf6817ca7bcf0dc94054fe1
Mehrad Rafigh <4339673+mehrad-rafigh@users.noreply.github.com>
2020-08-31 20:28:47 +0200

    feat(angular): update NgRx libraries to version 10.0.0 (#3543)
    Fixes: #3542

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -2,6 +2,6 @@ export const nxVersion = '*';
 export const angularVersion = '^10.0.0';
 export const angularDevkitVersion = '~0.1000.0';
 export const angularJsVersion = '1.7.9';
-export const ngrxVersion = '9.1.0';
+export const ngrxVersion = '10.0.0';
 export const rxjsVersion = '~6.5.5';
 export const jestPresetAngularVersion = '8.2.1';
