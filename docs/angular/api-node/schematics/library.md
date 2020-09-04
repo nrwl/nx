c8a79047f00a85727f3ec52ee5aecd3ecea59ecf
katerina <sk.katherine@gmail.com>
2020-09-04 20:43:50 +0300

    feat(node): rootDir flag updates workspace.json
    ISSUES CLOSED: #3625

--- docs/angular/api-node/schematics/library.md
+++ docs/angular/api-node/schematics/library.md
@@ -82,9 +82,11 @@ Create a publishable library.
 
 ### rootDir
 
+Alias(es): srcRootForCompilationRoot
+
 Type: `string`
 
-Sets the rootDir for TypeScript compilation. It's the equivalent of srcRootForCompilationRoot, but it updates tsconfig.lib.json directly. When not defined, it uses the project's root property, or srcRootForCompilationRoot if it is defined.
+Sets the rootDir for TypeScript compilation. When not defined, it uses the project's root property, or srcRootForCompilationRoot if it is defined.
 
 ### skipFormat
 
--- packages/node/src/schematics/library/library.spec.ts
+++ packages/node/src/schematics/library/library.spec.ts
@@ -39,9 +39,16 @@ describe('lib', () => {
     });
 
     it('adds srcRootForCompilationRoot in workspace.json', async () => {
-      const tree = await runSchematic('lib', { name: 'myLib', rootDir: './src' }, appTree);
+      const tree = await runSchematic(
+        'lib',
+        { name: 'myLib', rootDir: './src', buildable: true },
+        appTree
+      );
       const workspaceJson = readJsonInTree(tree, '/workspace.json');
-      expect(workspaceJson.projects['my-lib'].architect.build.options.srcRootForCompilationRoot).toEqual('./src');
+      expect(
+        workspaceJson.projects['my-lib'].architect.build.options
+          .srcRootForCompilationRoot
+      ).toEqual('./src');
     });
 
     it('should update nx.json', async () => {
