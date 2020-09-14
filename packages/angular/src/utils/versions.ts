722ed92245ff776049a22b61978fe58d1510bbe1
Mehrad Rafigh <mehrad@rafigh.de>
2020-09-14 18:05:49 +0200

    feat(angular): update jest-preset-angular library to v8.3.1

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -4,4 +4,4 @@ export const angularDevkitVersion = '~0.1000.0';
 export const angularJsVersion = '1.7.9';
 export const ngrxVersion = '10.0.0';
 export const rxjsVersion = '~6.5.5';
-export const jestPresetAngularVersion = '8.2.1';
+export const jestPresetAngularVersion = '8.3.1';
