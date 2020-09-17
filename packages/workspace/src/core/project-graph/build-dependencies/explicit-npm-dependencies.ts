a83ce36ff5eaa7dc8ba482c744885cf3e01e4d48
Jason Jean <jasonjean1993@gmail.com>
2020-09-17 13:30:58 -0400

    fix(core): improve performance of analyzing npm dependencies

--- packages/workspace/src/core/project-graph/build-dependencies/explicit-npm-dependencies.ts
+++ packages/workspace/src/core/project-graph/build-dependencies/explicit-npm-dependencies.ts
@@ -7,6 +7,7 @@ import {
 } from '../project-graph-models';
 import { TypeScriptImportLocator } from './typescript-import-locator';
 import { FileRead } from '../../file-utils';
+import { isRelativePath } from '../../../utils/fileutils';
 
 export function buildExplicitNpmDependencies(
   ctx: ProjectGraphContext,
@@ -19,17 +20,28 @@ export function buildExplicitNpmDependencies(
   const npmProjects = Object.values(nodes).filter(
     (node) => node.type === 'npm'
   ) as ProjectGraphNode<{ packageName: string; version: string }>[];
+  const cache = new Map<string, string>();
 
   Object.keys(ctx.fileMap).forEach((source) => {
     Object.values(ctx.fileMap[source]).forEach((f) => {
       importLocator.fromFile(
         f.file,
         (importExpr: string, filePath: string, type: DependencyType) => {
-          const pkg = npmProjects.find((pkg) =>
-            isNpmPackageImport(pkg.data.packageName, importExpr)
-          );
-          if (pkg) {
-            const target = nodes[pkg.name];
+          if (isRelativePath(importExpr)) {
+            return;
+          }
+
+          let pkgName: string;
+          if (cache.has(importExpr)) {
+            pkgName = cache.get(importExpr);
+          } else {
+            pkgName = npmProjects.find((pkg) =>
+              importExpr.startsWith(pkg.data.packageName)
+            )?.name;
+            cache.set(importExpr, pkgName);
+          }
+          if (pkgName) {
+            const target = nodes[pkgName];
             if (source && target) {
               addDependency(type, source, target.name);
             }
@@ -38,12 +50,4 @@ export function buildExplicitNpmDependencies(
       );
     });
   });
-}
-
-function isNpmPackageImport(p: string, e: string) {
-  const toMatch = e
-    .split(/[\/]/)
-    .slice(0, p.startsWith('@') ? 2 : 1)
-    .join('/');
-  return toMatch === p;
 }
