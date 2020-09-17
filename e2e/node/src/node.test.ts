784b39c1e4181919ac5b2e5507bed136bb97791e
Jonathan Cammisuli <jon@cammisuli.ca>
2020-09-17 10:32:21 -0400

    chore(node): add e2e test to ensure that typescript compilation will fail

--- e2e/node/src/node.test.ts
+++ e2e/node/src/node.test.ts
@@ -327,6 +327,21 @@ forEachCli((currentCLIName) => {
       runCLI(`build ${nodelib}`);
       checkFilesExist(`./dist/libs/${nodelib}/esm2015/index.js`);
     }, 60000);
+
+    it('should fail when trying to compile typescript files that are invalid', () => {
+      ensureProject();
+      const nodeLib = uniq('nodelib');
+      runCLI(
+        `generate @nrwl/node:lib ${nodeLib} --publishable --importPath=@proj/${nodeLib}`
+      );
+      updateFile(
+        `libs/${nodeLib}/src/index.ts`,
+        stripIndents`
+        const temp: number = 'should fail'
+        `
+      );
+      expect(() => runCLI(`build ${nodeLib}`)).toThrow();
+    });
   });
 
   describe('nest libraries', function () {
