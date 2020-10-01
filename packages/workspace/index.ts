30e59a7b5cfab85df6b0a5df79208619de85825a
James Henry <james@henry.sc>
2020-10-01 18:14:03 +0400

    feat(linter): add explicit file extension to config files

--- packages/workspace/index.ts
+++ packages/workspace/index.ts
@@ -71,6 +71,7 @@ export { formatFiles } from './src/utils/rules/format-files';
 export { deleteFile } from './src/utils/rules/deleteFile';
 export * from './src/utils/rules/ng-add';
 export { updateKarmaConf } from './src/utils/rules/update-karma-conf';
+export { visitNotIgnoredFiles } from './src/utils/rules/visit-not-ignored-files';
 export { setDefaultCollection } from './src/utils/rules/workspace';
 import * as strings from './src/utils/strings';
 export { checkAndCleanWithSemver } from './src/utils/version-utils';
