e9594b76db27776a8af5482ef80bbf84584f90ee
Jason Jean <jasonjean1993@gmail.com>
2020-10-02 12:02:13 -0400

    feat(repo): update to nx 10.3

--- e2e/cypress/jest.config.js
+++ e2e/cypress/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'cypress',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   maxWorkers: 1,
   globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'e2e-cypress',
 };
