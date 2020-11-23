c853b0f437e015096898e79f60a3637dc3d78051
Jonathan Cammisuli <jon@cammisuli.ca>
2020-11-23 08:32:16 -0500

    chore(core): revert wrapRule in run-commands schematic

--- packages/workspace/src/schematics/run-commands/run-commands.ts
+++ packages/workspace/src/schematics/run-commands/run-commands.ts
@@ -1,27 +1,26 @@
 import { Schema } from './schema';
 import { updateWorkspaceInTree } from '@nrwl/workspace';
-import { Tree, wrapRule } from '@nrwl/devkit';
+import { Tree } from '@nrwl/devkit';
 
 export default function (host: Tree, schema: Schema) {
-  wrapRule(
-    updateWorkspaceInTree((json) => {
-      const project = json.projects[schema.project];
-      if (!project) {
-        throw new Error(`Invalid project name "${schema.project}"`);
-      }
-      project.architect = project.architect || {};
-      project.architect[schema.name] = {
-        builder: '@nrwl/workspace:run-commands',
-        outputs: schema.outputs
-          ? schema.outputs.split(',').map((s) => s.trim())
-          : [],
-        options: {
-          command: schema.command,
-          cwd: schema.cwd,
-          envFile: schema.envFile,
-        },
-      };
-      return json;
-    })
-  )(host);
+  const fn = updateWorkspaceInTree((json) => {
+    const project = json.projects[schema.project];
+    if (!project) {
+      throw new Error(`Invalid project name "${schema.project}"`);
+    }
+    project.architect = project.architect || {};
+    project.architect[schema.name] = {
+      builder: '@nrwl/workspace:run-commands',
+      outputs: schema.outputs
+        ? schema.outputs.split(',').map((s) => s.trim())
+        : [],
+      options: {
+        command: schema.command,
+        cwd: schema.cwd,
+        envFile: schema.envFile,
+      },
+    };
+    return json;
+  }) as any;
+  fn(host);
 }
