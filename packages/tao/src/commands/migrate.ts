724b9b1f0336c040e4b8a5d00e48d92f01264282
Tasos Bekos <tbekos@gmail.com>
2020-11-26 11:51:10 +0200

    fix(core): migrate will tolerate backslashes in package names
    When migrate command is invoked in Windows, there is a chance that
    package names will contain backslashes due to an unexpected conversion.
    Since package names can only contain only URL-friendly characters, ie
    no backslashes, we try to fix it, by converting them back to forward slashes.

--- packages/tao/src/commands/migrate.ts
+++ packages/tao/src/commands/migrate.ts
@@ -69,6 +69,10 @@ export function normalizeVersion(version: string) {
   }
 }
 
+function slash(packageName) {
+  return packageName.replace(/\\/g, '/');
+}
+
 export class Migrator {
   private readonly versions: (p: string) => string;
   private readonly fetch: (p: string, v: string) => Promise<MigrationsJson>;
@@ -305,7 +309,7 @@ function versionOverrides(overrides: string, param: string) {
         `Incorrect '${param}' section. Use --${param}="package@version"`
       );
     }
-    res[selectedPackage] = normalizeVersionWithTagCheck(selectedVersion);
+    res[slash(selectedPackage)] = normalizeVersionWithTagCheck(selectedVersion);
   });
   return res;
 }
@@ -379,7 +383,7 @@ export function parseMigrationsOptions(
     );
     return {
       type: 'generateMigrations',
-      targetPackage,
+      targetPackage: slash(targetPackage),
       targetVersion,
       from,
       to,
