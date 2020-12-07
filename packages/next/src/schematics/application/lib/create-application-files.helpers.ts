0a5ee20e46f8f32941d12ed1d8bc8e41cd16f0fe
Jack Hsu <jack.hsu@gmail.com>
2020-12-07 13:34:12 -0500

    chore(react): update links for generated react apps (#4213)

--- packages/next/src/schematics/application/lib/create-application-files.helpers.ts
+++ packages/next/src/schematics/application/lib/create-application-files.helpers.ts
@@ -16,40 +16,29 @@ export function createAppJsx(name: string) {
       Here are some links to help you get started.
     </p>
     <ul className="resources">
-        <li className="col-span-2">
-          <a
-            className="resource flex"
-            href="https://connect.nrwl.io/app/courses/nx-workspaces/intro"
-          >
-            Nx video course
-          </a>
-        </li>
       <li className="col-span-2">
         <a
           className="resource flex"
-          href="https://nx.dev/react/getting-started/what-is-nx"
+          href="https://egghead.io/playlists/scale-react-development-with-nx-4038"
         >
-          Nx video tutorial
+          Scale React Development with Nx (Course)
         </a>
       </li>
       <li className="col-span-2">
         <a
           className="resource flex"
-          href="https://nx.dev/react/tutorial/01-create-application"
+          href="https://nx.dev/latest/react/tutorial/01-create-application"
         >
           Interactive tutorial
         </a>
       </li>
       <li className="col-span-2">
-        <a className="resource flex" href="https://connect.nrwl.io/">
-          <img
-            height="36"
-            alt="Nrwl Connect"
-            src="https://connect.nrwl.io/assets/img/CONNECT_ColorIcon.png"
-          />
-          <span  className="gutter-left">
-            Nrwl Connect
-          </span>
+        <a className="resource flex" href="https://nx.app/">
+          <svg width="36" height="36" viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M120 15V30C103.44 30 90 43.44 90 60C90 76.56 76.56 90 60 90C43.44 90 30 103.44 30 120H15C6.72 120 0 113.28 0 105V15C0 6.72 6.72 0 15 0H105C113.28 0 120 6.72 120 15Z" fill="#0E2039"/>
+            <path d="M120 30V105C120 113.28 113.28 120 105 120H30C30 103.44 43.44 90 60 90C76.56 90 90 76.56 90 60C90 43.44 103.44 30 120 30Z" fill="white"/>
+          </svg>
+          <span className="gutter-left">Nx Cloud</span>
         </a>
       </li>
     </ul>
