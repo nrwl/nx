b791ce3a0dcc78f19e849090c658011eaf60fcb7
Victor Savkin <vic.savkin@gmail.com>
2020-12-09 15:20:23 -0500

    fix(misc): update migration to work with older versions of tao

--- packages/workspace/src/migrations/update-11-0-0/update-decorate-angular-cli.ts
+++ packages/workspace/src/migrations/update-11-0-0/update-decorate-angular-cli.ts
@@ -1,19 +1,21 @@
 import { join as pathJoin } from 'path';
 import { readFileSync } from 'fs';
-import { Tree } from '@nrwl/devkit';
+import { Tree } from '@angular-devkit/schematics';
 
-export default function update(host: Tree) {
-  const decorateCli = readFileSync(
-    pathJoin(
-      __dirname as any,
-      '..',
-      '..',
-      'schematics',
-      'utils',
-      'decorate-angular-cli.js__tmpl__'
-    )
-  ).toString();
-  if (host.exists('/decorate-angular-cli.js')) {
-    host.write('/decorate-angular-cli.js', decorateCli);
-  }
+export default function update() {
+  return (host: Tree) => {
+    const decorateCli = readFileSync(
+      pathJoin(
+        __dirname as any,
+        '..',
+        '..',
+        'schematics',
+        'utils',
+        'decorate-angular-cli.js__tmpl__'
+      )
+    ).toString();
+    if (host.exists('/decorate-angular-cli.js')) {
+      host.overwrite('/decorate-angular-cli.js', decorateCli);
+    }
+  };
 }
