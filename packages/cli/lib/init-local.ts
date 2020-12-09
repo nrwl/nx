712fc5e307ac1f288697a4c218e65fdb96871ccc
Victor Savkin <vic.savkin@gmail.com>
2020-12-09 13:51:17 -0500

    feat(core): show a warning about using ng update insteda of nx migrate

--- packages/cli/lib/init-local.ts
+++ packages/cli/lib/init-local.ts
@@ -32,7 +32,25 @@ export function initLocal(workspace: Workspace) {
     if (workspace.type === 'nx') {
       loadCli(workspace, '@nrwl/tao/index.js');
     } else {
-      loadCli(workspace, '@angular/cli/lib/init.js');
+      if (process.argv[2] === 'update') {
+        console.log(
+          `Nx provides a much improved version of "ng update". It runs same migrations, but allows you to:`
+        );
+        console.log(`- rerun the same migration multiple times`);
+        console.log(`- reorder migrations`);
+        console.log(`- skip migrations`);
+        console.log(`- fix migrations that "almost work"`);
+        console.log(`- commit a partially migrated state`);
+        console.log(`- change versions of packages to match org requirements`);
+        console.log(
+          `And, in general, it is lot more reliable for non-trivial workspaces. Read more at: https://nx.dev/latest/angular/workspace/update`
+        );
+        console.log(
+          `Run "nx migrate latest" to update to the latest version of Nx.`
+        );
+      } else {
+        loadCli(workspace, '@angular/cli/lib/init.js');
+      }
     }
   }
 }
