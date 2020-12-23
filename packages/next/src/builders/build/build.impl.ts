3c30803569290e5e613644bb1c46191e7107ceba
Jack Hsu <jack.hsu@gmail.com>
2020-12-23 12:27:49 -0500

    fix(nextjs): remove duplicated public folder in dist

--- packages/next/src/builders/build/build.impl.ts
+++ packages/next/src/builders/build/build.impl.ts
@@ -6,13 +6,14 @@ import {
 import build from 'next/dist/build';
 import { PHASE_PRODUCTION_BUILD } from 'next/dist/next-server/lib/constants';
 import * as path from 'path';
-import { copyFileSync } from 'fs';
+import { copySync } from 'fs-extra';
 import { from, Observable } from 'rxjs';
-import { concatMap, map } from 'rxjs/operators';
+import { concatMap, map, tap } from 'rxjs/operators';
 import { prepareConfig } from '../../utils/config';
 import { NextBuildBuilderOptions } from '../../utils/types';
 import { createPackageJson } from './lib/create-package-json';
 import { createNextConfigFile } from './lib/create-next-config-file';
+import { join } from 'path';
 
 try {
   require('dotenv').config();
@@ -29,6 +30,9 @@ export function run(
   return from(build(root, config as any)).pipe(
     concatMap(() => from(createPackageJson(options, context))),
     concatMap(() => from(createNextConfigFile(options, context))),
+    tap(() => {
+      copySync(join(root, 'public'), join(options.outputPath, 'public'));
+    }),
     map(() => ({ success: true }))
   );
 }
