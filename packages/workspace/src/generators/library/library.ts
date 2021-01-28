55f6c11622fb8685315abe0a1a83da78d6d7457e
Jason Jean <jasonjean1993@gmail.com>
2021-01-27 20:20:18 -0500

    fix(core): properly set tsconfig path mapping on windows (#4624)

--- packages/workspace/src/generators/library/library.ts
+++ packages/workspace/src/generators/library/library.ts
@@ -80,7 +80,11 @@ function updateRootTsConfig(host: Tree, options: NormalizedSchema) {
     }
 
     c.paths[options.importPath] = [
-      join(options.projectRoot, './src', 'index.' + (options.js ? 'js' : 'ts')),
+      joinPathFragments(
+        options.projectRoot,
+        './src',
+        'index.' + (options.js ? 'js' : 'ts')
+      ),
     ];
 
     return json;
