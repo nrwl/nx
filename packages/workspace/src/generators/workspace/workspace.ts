613a6dfac8c31afa7bef29d03a39e7bd79375aad
Jason Jean <jasonjean1993@gmail.com>
2021-02-02 12:05:47 -0500

    fix(core): do not modify existing files during workspace creation

--- packages/workspace/src/generators/workspace/workspace.ts
+++ packages/workspace/src/generators/workspace/workspace.ts
@@ -15,21 +15,22 @@ import {
   typescriptVersion,
 } from '../../utils/versions';
 import { readFileSync } from 'fs';
-import { join as pathJoin } from 'path';
+import { join, join as pathJoin } from 'path';
+import { reformattedWorkspaceJsonOrNull } from '@nrwl/tao/src/shared/workspace';
 
 export const DEFAULT_NRWL_PRETTIER_CONFIG = {
   singleQuote: true,
 };
 
-const decorateAngularClI = (host: Tree) => {
+function decorateAngularClI(host: Tree, options: Schema) {
   const decorateCli = readFileSync(
     pathJoin(__dirname as any, '..', 'utils', 'decorate-angular-cli.js__tmpl__')
   ).toString();
-  host.write('decorate-angular-cli.js', decorateCli);
-};
+  host.write(join(options.directory, 'decorate-angular-cli.js'), decorateCli);
+}
 
 function setWorkspaceLayoutProperties(tree: Tree, options: Schema) {
-  updateJson(tree, 'nx.json', (json) => {
+  updateJson(tree, join(options.directory, 'nx.json'), (json) => {
     if (options.layout === 'packages') {
       json.workspaceLayout = {
         appsDir: 'packages',
@@ -42,17 +43,17 @@ function setWorkspaceLayoutProperties(tree: Tree, options: Schema) {
 
 function createAppsAndLibsFolders(host: Tree, options: Schema) {
   if (options.layout === 'packages') {
-    host.write('packages/.gitkeep', '');
+    host.write(join(options.directory, 'packages/.gitkeep'), '');
   } else {
-    host.write('apps/.gitkeep', '');
-    host.write('libs/.gitkeep', '');
+    host.write(join(options.directory, 'apps/.gitkeep'), '');
+    host.write(join(options.directory, 'libs/.gitkeep'), '');
   }
 }
 
 function createFiles(host: Tree, options: Schema) {
   const npmScope = options.npmScope ? options.npmScope : options.name;
   const formattedNames = names(options.name);
-  generateFiles(host, pathJoin(__dirname, './files'), '', {
+  generateFiles(host, pathJoin(__dirname, './files'), options.directory, {
     formattedNames,
     dot: '.',
     tmpl: '',
@@ -69,8 +70,31 @@ function createFiles(host: Tree, options: Schema) {
   });
 }
 
-function createPrettierrc(host: Tree) {
-  writeJson(host, '.prettierrc', DEFAULT_NRWL_PRETTIER_CONFIG);
+function createPrettierrc(host: Tree, options: Schema) {
+  writeJson(
+    host,
+    join(options.directory, '.prettierrc'),
+    DEFAULT_NRWL_PRETTIER_CONFIG
+  );
+}
+
+function formatWorkspaceJson(host: Tree, options: Schema) {
+  const path = join(
+    options.directory,
+    options.cli === 'angular' ? 'angular.json' : 'workspace.json'
+  );
+
+  try {
+    updateJson(host, path, (workspaceJson) => {
+      const reformatted = reformattedWorkspaceJsonOrNull(workspaceJson);
+      if (reformatted) {
+        host.write(path, JSON.stringify(reformatted, null, 2));
+      }
+    });
+  } catch (e) {
+    console.error(`Failed to format: ${path}`);
+    console.error(e);
+  }
 }
 
 export async function workspaceGenerator(host: Tree, options: Schema) {
@@ -78,14 +102,15 @@ export async function workspaceGenerator(host: Tree, options: Schema) {
     throw new Error(`Invalid options, "name" is required.`);
   }
   createFiles(host, options);
-  createPrettierrc(host);
+  createPrettierrc(host, options);
   if (options.cli === 'angular') {
-    decorateAngularClI(host);
+    decorateAngularClI(host, options);
   }
   setWorkspaceLayoutProperties(host, options);
   createAppsAndLibsFolders(host, options);
 
   await formatFiles(host);
+  formatWorkspaceJson(host, options);
 }
 
 export const workspaceSchematic = convertNxGenerator(workspaceGenerator);
