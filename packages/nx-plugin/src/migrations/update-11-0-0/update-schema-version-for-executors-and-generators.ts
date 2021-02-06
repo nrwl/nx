285879c1e2bd496598ac5827b245032b85fc768c
Victor Savkin <avix1000@gmail.com>
2021-02-06 14:27:37 -0500

    fix(misc): fix @nrwl/nx-plugin migrations

--- packages/nx-plugin/src/migrations/update-11-0-0/update-schema-version-for-executors-and-generators.ts
+++ packages/nx-plugin/src/migrations/update-11-0-0/update-schema-version-for-executors-and-generators.ts
@@ -1,11 +1,10 @@
-import { readJson } from '@nrwl/devkit';
 import { basename } from 'path';
-import { visitNotIgnoredFiles } from '@nrwl/workspace';
+import { visitNotIgnoredFiles, readJsonInTree } from '@nrwl/workspace';
 
 export default function () {
   return visitNotIgnoredFiles((file, host) => {
     if (basename(file) === 'schema.json') {
-      const p = readJson(host as any, file);
+      const p = readJsonInTree(host as any, file);
       if (
         p.$schema === 'https://json-schema.org/draft-07/schema' ||
         p.$schema === 'http://json-schema.org/draft-07/schema'
