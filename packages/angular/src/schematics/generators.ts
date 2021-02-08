7aebeb44871bc86c836af5eea1b925938f9859c3
Jason Jean <jasonjean1993@gmail.com>
2021-02-08 10:45:44 -0500

    feat(angular): export angular generators from @nrwl/angular/generators

---
+++ packages/angular/src/schematics/generators.ts
@@ -0,0 +1,5 @@
+export { applicationGenerator } from './application/application';
+export { libraryGenerator } from './library/library';
+export { moveGenerator } from './move/move';
+export { ngrxGenerator } from './ngrx/ngrx';
+export { storybookConfigurationGenerator } from './storybook-configuration/configuration';
