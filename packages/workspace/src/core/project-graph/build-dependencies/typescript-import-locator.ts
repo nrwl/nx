d82d4ab9030e0d4658234d4607fc9884a94e6400
duncan <duncangichimu@gmail.com>
2021-02-11 12:26:07 -0800

    feat(core): allow excluding imports using a special ignore comment called nx-import-ignore
    Placing a comment called nx-import-ignore above an import will exclude it from being used in the creation of the project dependency graph. This allows bypassing the circular dependency linting rule whenever needed.

--- packages/workspace/src/core/project-graph/build-dependencies/typescript-import-locator.ts
+++ packages/workspace/src/core/project-graph/build-dependencies/typescript-import-locator.ts
@@ -55,8 +55,10 @@ export class TypeScriptImportLocator {
       ts.isImportDeclaration(node) ||
       (ts.isExportDeclaration(node) && node.moduleSpecifier)
     ) {
-      const imp = this.getStringLiteralValue(node.moduleSpecifier);
-      visitor(imp, filePath, DependencyType.static);
+      if (!this.ignoreStatement(node)) {
+        const imp = this.getStringLiteralValue(node.moduleSpecifier);
+        visitor(imp, filePath, DependencyType.static);
+      }
       return; // stop traversing downwards
     }
 
@@ -66,8 +68,10 @@ export class TypeScriptImportLocator {
       node.arguments.length === 1 &&
       ts.isStringLiteral(node.arguments[0])
     ) {
-      const imp = this.getStringLiteralValue(node.arguments[0]);
-      visitor(imp, filePath, DependencyType.dynamic);
+      if (!this.ignoreStatement(node)) {
+        const imp = this.getStringLiteralValue(node.arguments[0]);
+        visitor(imp, filePath, DependencyType.dynamic);
+      }
       return;
     }
 
@@ -77,8 +81,10 @@ export class TypeScriptImportLocator {
       node.arguments.length === 1 &&
       ts.isStringLiteral(node.arguments[0])
     ) {
-      const imp = this.getStringLiteralValue(node.arguments[0]);
-      visitor(imp, filePath, DependencyType.static);
+      if (!this.ignoreStatement(node)) {
+        const imp = this.getStringLiteralValue(node.arguments[0]);
+        visitor(imp, filePath, DependencyType.static);
+      }
       return;
     }
 
@@ -88,7 +94,10 @@ export class TypeScriptImportLocator {
       );
       if (name === 'loadChildren') {
         const init = (node as ts.PropertyAssignment).initializer;
-        if (init.kind === ts.SyntaxKind.StringLiteral) {
+        if (
+          init.kind === ts.SyntaxKind.StringLiteral &&
+          !this.ignoreLoadChildrenDependency(node.getFullText())
+        ) {
           const childrenExpr = this.getStringLiteralValue(init);
           visitor(childrenExpr, filePath, DependencyType.dynamic);
           return; // stop traversing downwards
@@ -100,6 +109,35 @@ export class TypeScriptImportLocator {
      * Continue traversing down the AST from the current node
      */
     ts.forEachChild(node, (child) => this.fromNode(filePath, child, visitor));
+  }
+
+  private ignoreStatement(node: ts.Node) {
+    return stripSourceCode(this.scanner, node.getFullText()) === '';
+  }
+
+  private ignoreLoadChildrenDependency(contents: string): boolean {
+    this.scanner.setText(contents);
+    let token = this.scanner.scan();
+    while (token !== ts.SyntaxKind.EndOfFileToken) {
+      if (
+        token === ts.SyntaxKind.SingleLineCommentTrivia ||
+        token === ts.SyntaxKind.MultiLineCommentTrivia
+      ) {
+        const start = this.scanner.getStartPos() + 2;
+        token = this.scanner.scan();
+        const isMultiLineCommentTrivia =
+          token === ts.SyntaxKind.MultiLineCommentTrivia;
+        const end =
+          this.scanner.getStartPos() - (isMultiLineCommentTrivia ? 2 : 0);
+        const comment = contents.substring(start, end).trim();
+        if (comment === 'nx-ignore-next-line') {
+          return true;
+        }
+      } else {
+        token = this.scanner.scan();
+      }
+    }
+    return false;
   }
 
   private getPropertyAssignmentName(nameNode: ts.PropertyName) {
