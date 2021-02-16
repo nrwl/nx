e4faaf1ef4a11140c6e9cb0f9c3c27cb45fa2333
Brandon <robertsbt@gmail.com>
2021-02-16 09:38:11 -0600

    feat(angular): update Angular packages to 11.2.x release (#4788)
    * feat(angular): update Angular packages to 11.2.x release
    
    * chore(angular): fix storybook 5-6 default schematic unit test
    
    * fix(angular): fix Angular Devkit discrepancies

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = '*';
-export const angularVersion = '^11.0.0';
-export const angularDevkitVersion = '~0.1100.1';
+export const angularVersion = '^11.2.0';
+export const angularDevkitVersion = '~0.1102.0';
 export const angularJsVersion = '1.7.9';
 export const ngrxVersion = '^11.0.0';
 export const rxjsVersion = '~6.6.3';
