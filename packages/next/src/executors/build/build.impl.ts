c9a2b2c1c00d2993cd121326f4a10a7439f6b254
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2021-02-26 21:35:22 +0200

    feat(nextjs): allow custom distDir (#4885)

--- packages/next/src/executors/build/build.impl.ts
+++ packages/next/src/executors/build/build.impl.ts
@@ -4,12 +4,13 @@ import build from 'next/dist/build';
 import { PHASE_PRODUCTION_BUILD } from 'next/dist/next-server/lib/constants';
 
 import { join, resolve } from 'path';
-import { copySync } from 'fs-extra';
+import { copySync, mkdir } from 'fs-extra';
 
 import { prepareConfig } from '../../utils/config';
 import { NextBuildBuilderOptions } from '../../utils/types';
 import { createPackageJson } from './lib/create-package-json';
 import { createNextConfigFile } from './lib/create-next-config-file';
+import { directoryExists } from '@nrwl/workspace/src/utilities/fileutils';
 
 try {
   require('dotenv').config();
@@ -21,7 +22,13 @@ export default async function buildExecutor(
 ) {
   const root = resolve(context.root, options.root);
   const config = prepareConfig(PHASE_PRODUCTION_BUILD, options, context);
+
   await build(root, config as any);
+
+  if (!directoryExists(options.outputPath)) {
+    mkdir(options.outputPath);
+  }
+
   createPackageJson(options, context);
   createNextConfigFile(options, context);
 
