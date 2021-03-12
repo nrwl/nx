9d863a70731ae9ea45f1a9231638239af07d725e
kirjai <kirjaitube@gmail.com>
2021-03-12 13:10:07 +0200

    fix(nextjs): include peer dependencies in built package.json

--- packages/next/src/executors/build/lib/create-package-json.ts
+++ packages/next/src/executors/build/lib/create-package-json.ts
@@ -1,76 +1,32 @@
 import { ExecutorContext } from '@nrwl/devkit';
 
-import { writeFileSync } from 'fs';
-import { join } from 'path';
-
 import { createProjectGraph } from '@nrwl/workspace/src/core/project-graph';
-import { readJsonFile } from '@nrwl/workspace/src/utilities/fileutils';
-import { calculateProjectDependencies } from '@nrwl/workspace/src/utilities/buildable-libs-utils';
+import { writeJsonFile } from '@nrwl/workspace/src/utilities/fileutils';
+import { createPackageJson as generatePackageJson } from '@nrwl/workspace/src/utilities/create-package-json';
 
 import { NextBuildBuilderOptions } from '../../../utils/types';
-
-function getProjectDeps(context: ExecutorContext, rootPackageJson: any) {
-  const projGraph = createProjectGraph();
-  const { dependencies: deps } = calculateProjectDependencies(
-    projGraph,
-    context.root,
-    context.projectName,
-    context.targetName,
-    context.configurationName
-  );
-  const depNames = deps
-    .map((d) => d.node)
-    .filter((node) => node.type === 'npm')
-    .map((node) => node.data.packageName)
-    // Need to make sure @nrwl/workspace is installed
-    // It is only a peer dependency of @nrwl/next so does not get installed automatically
-    // See: https://github.com/nrwl/nx/issues/4336
-    .concat('@nrwl/workspace');
-  const dependencies: string[] = depNames
-    .filter((packageName) => packageName in rootPackageJson.dependencies)
-    .reduce((deps, pkgName) => {
-      return { ...deps, [pkgName]: rootPackageJson.dependencies[pkgName] };
-    }, {});
-  const devDependencies = depNames
-    .filter((packageName) => packageName in rootPackageJson.devDependencies)
-    .reduce((deps, pkgName) => {
-      return { ...deps, [pkgName]: rootPackageJson.devDependencies[pkgName] };
-    }, {});
-  return {
-    dependencies,
-    devDependencies,
-  };
-}
 
 export function createPackageJson(
   options: NextBuildBuilderOptions,
   context: ExecutorContext
 ) {
-  const rootPackageJson = readJsonFile(join(context.root, 'package.json'));
-  const { dependencies, devDependencies } = getProjectDeps(
-    context,
-    rootPackageJson
-  );
+  const depGraph = createProjectGraph();
+  const packageJson = generatePackageJson(context.projectName, depGraph, {
+    root: context.root,
+    projectRoot: context.workspace.projects[context.projectName].sourceRoot,
+  });
+  if (!packageJson.scripts) {
+    packageJson.scripts = {};
+  }
+  packageJson.scripts.start = 'next start';
+  if (!packageJson.devDependencies) {
+    packageJson.devDependencies = {};
+  }
+  const nrwlWorkspaceNode = depGraph.nodes['npm:@nrwl/workspace'];
 
-  const outPackageJson = {
-    name: context.projectName,
-    version: '0.0.1',
-    scripts: {
-      start: 'next start',
-    },
-    dependencies: {
-      ...dependencies,
-      // peer deps of next, so we need to add them here
-      react: rootPackageJson.dependencies['react'],
-      'react-dom': rootPackageJson.dependencies['react-dom'],
-      next: rootPackageJson.dependencies['next'],
-    },
-    // needed for the next.config.js file
-    devDependencies,
-  };
-
-  writeFileSync(
-    join(options.outputPath, 'package.json'),
-    JSON.stringify(outPackageJson, null, 2)
-  );
+  if (nrwlWorkspaceNode) {
+    packageJson.devDependencies['@nrwl/workspace'] =
+      nrwlWorkspaceNode.data.version;
+  }
+  writeJsonFile(`${options.outputPath}/package.json`, packageJson);
 }
