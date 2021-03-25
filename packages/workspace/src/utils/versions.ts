c6916d12e869a309c95c23e367053988e458abdf
Jack Hsu <jack.hsu@gmail.com>
2021-03-25 10:47:37 -0400

    feat(core): update typescript to 4.1

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -2,9 +2,9 @@ export const nxVersion = '*';
 
 export const angularCliVersion = '~11.0.0';
 export const buildAngularVersion = '~0.1102.0';
-export const typescriptVersion = '~4.0.3';
+export const typescriptVersion = '~4.1.4';
 export const prettierVersion = '2.2.1';
-export const typescriptESLintVersion = '4.3.0';
+export const typescriptESLintVersion = '4.19.0';
 export const tslintVersion = '~6.1.0';
-export const eslintVersion = '7.10.0';
+export const eslintVersion = '7.22.0';
 export const eslintConfigPrettierVersion = '8.1.0';
