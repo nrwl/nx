9348eae236dd71d6876c0b086dcf37354004ec5c
Jason Jean <jasonjean1993@gmail.com>
2021-03-25 13:03:15 -0400

    feat(linter): update tslint-to-eslint-config to ^2.2.0 (#5117)

--- packages/linter/src/utils/versions.ts
+++ packages/linter/src/utils/versions.ts
@@ -1,7 +1,7 @@
 export const nxVersion = '*';
 
 export const tslintVersion = '~6.1.0';
-export const tslintToEslintConfigVersion = '2.2.0';
+export const tslintToEslintConfigVersion = '^2.2.0';
 export const buildAngularVersion = '~0.1102.0';
 
 export const typescriptESLintVersion = '4.3.0';
