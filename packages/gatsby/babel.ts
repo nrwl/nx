f51b1dbc9b6ce06cbe8b1d16bd6ab12e085fd7f0
Jack Hsu <jack.hsu@gmail.com>
2021-03-30 10:51:20 -0400

    feat(gatsby): update to Gatsby v3

--- packages/gatsby/babel.ts
+++ packages/gatsby/babel.ts
@@ -1,12 +1,31 @@
 /*
  * Babel preset to provide Gatsby support for Nx.
  */
-module.exports = function (api, options) {
+
+interface GatsbyBabelOptions {
+  reactRuntime?: string;
+  reactImportSource?: string;
+}
+
+module.exports = function (api: any, presetOptions: GatsbyBabelOptions) {
   api.assertVersion(7);
   return {
     presets: [
       '@nrwl/web/babel',
-      [require.resolve('babel-preset-gatsby'), { useBuiltIns: true }],
+      [
+        require.resolve('babel-preset-gatsby'),
+        getGatsbyBabelOptions({ presetOptions }),
+      ],
     ],
   };
 };
+
+function getGatsbyBabelOptions({ presetOptions }) {
+  const gatsbyPresetOptions: Record<string, string | boolean> = {
+    reactRuntime: presetOptions.reactRuntime ?? 'automatic',
+  };
+  if (presetOptions.reactImportSource) {
+    gatsbyPresetOptions.reactImportSource = presetOptions.reactImportSource;
+  }
+  return gatsbyPresetOptions;
+}
