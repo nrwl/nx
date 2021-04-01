c84bbb3c93fed7d6fa8853583b3e2c55a3b3c76a
Jack Hsu <jack.hsu@gmail.com>
2021-04-01 09:46:12 -0400

    feat(react): add support for new jsx transform (#5131)

--- packages/react/babel.ts
+++ packages/react/babel.ts
@@ -2,12 +2,44 @@
  * Babel preset to provide React support for Nx.
  */
 
-module.exports = function (api: any, options: {}) {
+interface ReactBabelOptions {
+  runtime?: string;
+  importSource?: string;
+}
+
+module.exports = function (api: any, options: ReactBabelOptions) {
   api.assertVersion(7);
+  const env = api.env();
+
   return {
     presets: [
       '@nrwl/web/babel',
-      [require.resolve('@babel/preset-react'), { useBuiltIns: true }],
+      [
+        require.resolve('@babel/preset-react'),
+        getReactPresetOptions({
+          presetOptions: options,
+          env,
+        }),
+      ],
     ],
   };
 };
+
+function getReactPresetOptions({ presetOptions, env }) {
+  const reactPresetOptions: Record<string, string | boolean> = {
+    runtime: presetOptions.runtime ?? 'automatic',
+    development: env !== 'production',
+  };
+
+  // JSX spread is transformed into object spread in `@babel/plugin-transform-react-jsx`
+  // `useBuiltIns` will be removed in Babel 8.
+  if (reactPresetOptions.runtime === 'automatic') {
+    reactPresetOptions.useBuiltIns = true;
+  }
+
+  if (presetOptions.importSource) {
+    reactPresetOptions.importSource = presetOptions.importSource;
+  }
+
+  return reactPresetOptions;
+}
