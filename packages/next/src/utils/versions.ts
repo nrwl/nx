0e016d976f5395ac4f4c5b120384cab0f882cc06
Jack Hsu <jack.hsu@gmail.com>
2021-04-06 14:05:19 -0400

    feat(nextjs): update Next.js to 10.1.3

--- packages/next/src/utils/versions.ts
+++ packages/next/src/utils/versions.ts
@@ -1,9 +1,7 @@
 export const nxVersion = '*';
 
-export const nextVersion = '10.1.2';
-export const zeitNextCss = '1.0.1';
-export const zeitNextSass = '1.0.1';
-export const nodeSass = '4.14.1';
+export const nextVersion = '10.1.3';
+export const nodeSass = '5.0.0';
 export const zeitNextLess = '1.0.1';
 export const zeitNextStylus = '1.0.1';
 export const emotionServerVersion = '11.0.0';
