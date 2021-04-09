7f096d75d858bee6710b818857e126d3d4a81ef4
ben <contact@benjamincabanes.com>
2021-04-09 18:02:59 -0400

    feat(nxdev): setup tailwindcss jit

--- nx-dev/nx-dev/pages/_app.tsx
+++ nx-dev/nx-dev/pages/_app.tsx
@@ -2,29 +2,12 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Header, Footer } from '@nrwl/nx-dev/ui/common';
-import './styles.css';
+import 'tailwindcss/tailwind.css';
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/* ONLY TEMPORARY */}
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/tailwindcss@^2.0/dist/base.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/tailwindcss@^2.0/dist/components.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/@tailwindcss/typography@0.3.x/dist/typography.min.css"
-        />
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/tailwindcss@^2.0/dist/utilities.min.css"
-        />
         <title>Welcome to documentation!</title>
       </Head>
       <div className="documentation-app text-gray-500 antialiased bg-white">
