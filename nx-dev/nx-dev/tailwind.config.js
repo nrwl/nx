76723b5ce62ca4a5897fb839bb9685a03a35283f
Benjamin Cabanes <contact@benjamincabanes.com>
2021-04-09 20:18:21 -0400

    feat(nxdev): setup tailwindcss jit (#5301)

---
+++ nx-dev/nx-dev/tailwind.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const nxJson = require('@nrwl/workspace').readNxJson();
+const workspaceJson = require('@nrwl/workspace').readWorkspaceJson();
+
+function getProjectNameWithTag(projectsJson, tag) {
+  return Object.keys(projectsJson.projects)
+    .map((projectName) => ({
+      ...projectsJson.projects[projectName],
+      name: projectName,
+    }))
+    .filter((project) => project.tags.includes(tag))
+    .map((project) => project.name);
+}
+
+function getNxDevPurgePathList(nxJson, workspace) {
+  return getProjectNameWithTag(nxJson, 'scope:nx-dev')
+    .filter((projectName) => !['nx-dev', 'nx-dev-e2e'].includes(projectName))
+    .map((projectName) => workspace.projects[projectName])
+    .map((project) => path.join(project.sourceRoot, 'lib'))
+    .map((projectLib) => projectLib.concat('/**/*.{js,ts,jsx,tsx}'));
+}
+
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './nx-dev/nx-dev/pages/**/*.{js,ts,jsx,tsx}',
+    ...getNxDevPurgePathList(nxJson, workspaceJson),
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
