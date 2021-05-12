7352fd0c1ee8109461d6b9b94b94bba6da5b9b6d
Jason Jean <jasonjean1993@gmail.com>
2021-05-12 19:30:23 -0400

    feat(angular): update to angular 12 stable (#5643)

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -1,7 +1,7 @@
 export const nxVersion = '*';
-export const angularVersion = '^12.0.0-rc.3';
+export const angularVersion = '12.0.0';
 export const angularJsVersion = '1.7.9';
-export const ngrxVersion = '^12.0.0-rc.0';
+export const ngrxVersion = '^12.0.0';
 export const rxjsVersion = '~6.6.0';
 export const jestPresetAngularVersion = '8.4.0';
 export const angularEslintVersion = '~12.0.0-alpha.0';
--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = '*';
 
-export const angularCliVersion = '^12.0.0-rc.3';
+export const angularCliVersion = '12.0.0';
 export const typescriptVersion = '~4.2.4';
 export const prettierVersion = '2.2.1';
 export const typescriptESLintVersion = '4.19.0';
