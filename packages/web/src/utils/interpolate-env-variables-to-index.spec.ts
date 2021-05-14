857559e48c1ed2fcafa97f269c93291e8022a2a1
Zack DeRose <zack.derose@gmail.com>
2021-05-14 10:50:42 -0700

    chore(web): adding test case for NX_ option not present

--- packages/web/src/utils/interpolate-env-variables-to-index.spec.ts
+++ packages/web/src/utils/interpolate-env-variables-to-index.spec.ts
@@ -56,4 +56,21 @@ describe('interpolateEnvironmentVariablesToIndex()', () => {
 `;
     expect(interpolateEnvironmentVariablesToIndex(content)).toBe(expected);
   });
+
+  test('NX_ prefixed option present in index.html, but not present in process.env', () => {
+    delete process.env.PUBLIC_URL;
+    const content = `
+<div>Nx Variable: %NX_VARIABLE%</div>
+<div>Some other variable: %SOME_OTHER_VARIABLE%</div>
+<div>Some other nx_variable: %NX_SOME_OTHER_VARIABLE%</div>
+<div>Public Url: %PUBLIC_URL%</div>
+`;
+    const expected = `
+<div>Nx Variable: foo</div>
+<div>Some other variable: %SOME_OTHER_VARIABLE%</div>
+<div>Some other nx_variable: %NX_SOME_OTHER_VARIABLE%</div>
+<div>Public Url: </div>
+`;
+    expect(interpolateEnvironmentVariablesToIndex(content)).toBe(expected);
+  });
 });
