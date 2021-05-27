038185ef6c83752c3c9862aa19489f9f59699931
Benjamin Cabanes <contact@benjamincabanes.com>
2021-05-27 10:23:49 -0400

    docs(core): dedicated node screen (#5746)

---
+++ nx-dev/ui/common/src/lib/nx-users-showcase.tsx
@@ -0,0 +1,128 @@
+export function NxUsersShowcase() {
+  return (
+    <div className="max-w-screen xl:max-w-screen-xl mx-auto px-5 py-32">
+      <div className="flex sm:flex-row flex-col justify-center">
+        <div className="w-full py-6 mt-8">
+          <h2 className="text-xl sm:text-2xl lg:text-3xl leading-none font-extrabold text-gray-900 tracking-tight mb-4 text-center">
+            Who's using Nx?
+          </h2>
+          <p className="sm:w-2/3 sm:mx-auto text-center sm:text-lg">
+            The world’s leading companies use and love Nx.
+          </p>
+        </div>
+      </div>
+      <div className="grid items-center justify-items-center grid-cols-3 md:grid-cols-6 gap-8">
+        <img
+          className="opacity-60"
+          src="/images/nx-users/Accenture.png"
+          width={130}
+          height={45}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/AmericanAirlines.png"
+          width={140}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/AmericanTireDistributors.png"
+          width={80}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/Anthem.png"
+          width={95}
+          height={80}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/Audi.png"
+          width={60}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/BlackRock.png"
+          width={100}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/CapitalOne.png"
+          width={95}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/Cigna.png"
+          width={100}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/Cisco.png"
+          width={70}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/Datasite.png"
+          width={100}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/FedEx.png"
+          width={95}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/FICO.png"
+          width={65}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/GM.png"
+          width={40}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/Microsoft.png"
+          width={85}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/RedHat.png"
+          width={110}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/SatcomDirect.png"
+          width={75}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/TMobile.png"
+          width={125}
+          height={50}
+        />
+        <img
+          className="opacity-60"
+          src="/images/nx-users/Viacom.png"
+          width={95}
+          height={50}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default NxUsersShowcase;
