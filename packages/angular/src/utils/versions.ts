a6772c4346e0b141ce13a2273b9c90360be26e14
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2021-06-01 17:52:38 +0100

    fix(angular): update angular deps to use the latest minor versions (#5858)

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxVersion = '*';
-export const angularVersion = '12.0.0';
+export const angularVersion = '^12.0.0';
 export const angularJsVersion = '1.7.9';
 export const ngrxVersion = '^12.0.0';
 export const rxjsVersion = '~6.6.0';
