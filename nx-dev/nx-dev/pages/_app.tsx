a68be781f8b10d06628ca2ee4d4eee9968cde991
ben <contact@benjamincabanes.com>
2021-06-04 16:31:11 -0400

    docs(core): add seo meta

--- nx-dev/nx-dev/pages/_app.tsx
+++ nx-dev/nx-dev/pages/_app.tsx
@@ -8,6 +8,35 @@ export default function CustomApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Nx: Smart, Extensible Build Framework</title>
+        <meta charSet="utf-8" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@nxdevtools" />
+        <meta name="twitter:creator" content="@nrwl_io" />
+        <meta
+          name="twitter:title"
+          content="Nx: Smart, Extensible Build Framework"
+        />
+        <meta
+          name="twitter:description"
+          content="With Nx, you can develop multiple full-stack applications holistically and share code between them all in the same workspace. Add Cypress, Jest, Prettier, and Storybook into your dev workflow."
+        />
+        <meta name="twitter:image" content="/images/nx-media.jpg" />
+        <meta
+          name="twitter:image:alt"
+          content="Nx: Smart, Extensible Build Framework"
+        />
+        <meta property="og:url" content="https://nx.dev" />
+        <meta
+          property="og:description"
+          content="With Nx, you can develop multiple full-stack applications holistically and share code between them all in the same workspace. Add Cypress, Jest, Prettier, and Storybook into your dev workflow."
+        />
+        <meta
+          property="og:title"
+          content="Nx: Smart, Extensible Build Framework"
+        />
+        <meta property="og:image" content="/images/nx-media.jpg" />
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="300" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
