c9b8a18d76806e0fb2daad97d1dce031c8bf7d2d
Zack DeRose <zack.derose@gmail.com>
2021-06-04 17:03:48 -0700

    feat(testing): accomodating for getJestConfig() when adding new project

--- packages/jest/src/generators/jest-project/lib/update-jestconfig.ts
+++ packages/jest/src/generators/jest-project/lib/update-jestconfig.ts
@@ -2,7 +2,15 @@ import { JestProjectSchema } from '../schema';
 import { addPropertyToJestConfig } from '../../../utils/config/update-config';
 import { readProjectConfiguration, Tree } from '@nrwl/devkit';
 
+function usingUtilityFunction(host: Tree) {
+  // TODO: too naive implementation?
+  return host.read('jest.config.js').toString().includes('getJestProjects()');
+}
+
 export function updateJestConfig(host: Tree, options: JestProjectSchema) {
+  if (usingUtilityFunction(host)) {
+    return;
+  }
   const project = readProjectConfiguration(host, options.project);
   addPropertyToJestConfig(
     host,
