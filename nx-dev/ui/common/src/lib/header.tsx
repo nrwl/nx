c39a56ca2e01a8d0c0aebda9d8434a306ec1ad4a
Brandon <robertsbt@gmail.com>
2021-06-15 14:56:41 -0500

    docs(core): update files for generators/executors and fix plugins link (#6020)

--- nx-dev/ui/common/src/lib/header.tsx
+++ nx-dev/ui/common/src/lib/header.tsx
@@ -66,7 +66,7 @@ export function Header({ flavor, showSearch, version }: HeaderProps) {
               </a>
             </Link>
             <Link
-              href={`/${version.value}/${flavor.value}/core-concepts/plugins`}
+              href={`/${version.value}/${flavor.value}/core-concepts/nx-devkit`}
             >
               <a className="px-3 py-2 hidden md:inline-flex text-white leading-tight">
                 Plugins
