e182700f78152cb05df73a68454d43164c647968
Juri Strumpflohner <juri.strumpflohner@gmail.com>
2021-06-15 16:11:05 +0200

    feat(core): expose utilities for retrieving source dirs of dependent projects (#5976)
    * feat(core): expose utilities for retrieving source dirs of dependent projects
    
    * feat(nextjs): utility to generate glob patterns from dependent projects

--- packages/workspace/src/utilities/project-graph-utils.ts
+++ packages/workspace/src/utilities/project-graph-utils.ts
@@ -1,4 +1,5 @@
-import type { ProjectGraphNode } from '@nrwl/devkit';
+import type { ProjectGraph, ProjectGraphNode } from '@nrwl/devkit';
+import { createProjectGraph } from '../core/project-graph';
 
 export function projectHasTarget(project: ProjectGraphNode, target: string) {
   return project.data && project.data.targets && project.data.targets[target];
@@ -14,4 +15,70 @@ export function projectHasTargetAndConfiguration(
     project.data.targets[target].configurations &&
     project.data.targets[target].configurations[configuration]
   );
+}
+
+export function getSourceDirOfDependentProjects(
+  projectName: string,
+  projectGraph = createProjectGraph()
+): string[] {
+  const dependencyNodeNames = findAllProjectNodeDependencies(
+    projectName,
+    projectGraph
+  );
+
+  return dependencyNodeNames.map(
+    (nodeName) => projectGraph.nodes[nodeName].data.sourceRoot
+  );
+}
+
+/**
+ * Takes a filename and figures out the belonging app and from there
+ * collects all dependent
+ * @param filename name of a file in some workspace app / lib
+ */
+function findAllProjectNodeDependencies(
+  parentNodeName: string,
+  projectGraph = createProjectGraph()
+): string[] {
+  const dependencyNodeNames = new Set<string>();
+
+  collectDependentProjectNodesNames(
+    projectGraph,
+    dependencyNodeNames,
+    parentNodeName
+  );
+
+  return Array.from(dependencyNodeNames);
+}
+
+// Recursively get all the dependencies of the node
+function collectDependentProjectNodesNames(
+  nxDeps: ProjectGraph,
+  dependencyNodeNames: Set<string>,
+  parentNodeName: string
+) {
+  const dependencies = nxDeps.dependencies[parentNodeName];
+  if (!dependencies) {
+    // no dependencies for the given node, so silently return,
+    // as we probably wouldn't want to throw here
+    return;
+  }
+
+  for (const dependency of dependencies) {
+    const dependencyName = dependency.target;
+
+    // we're only intersted in project dependencies, not npm
+    if (dependencyName.startsWith('npm:')) {
+      continue;
+    }
+
+    dependencyNodeNames.add(dependencyName);
+
+    // Get the dependencies of the dependencies
+    collectDependentProjectNodesNames(
+      nxDeps,
+      dependencyNodeNames,
+      dependencyName
+    );
+  }
 }
