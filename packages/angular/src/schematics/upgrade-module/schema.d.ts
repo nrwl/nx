60382f56c18d08ad785b40dcdc0109ed5cc3c78d
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2021-06-21 14:46:41 +0100

    cleanup(angular): migrate downgrade-module and upgrade-module generators to nx devkit

--- packages/angular/src/schematics/upgrade-module/schema.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Schema {
-  project: string;
-  angularJsImport: string;
-  angularJsCmpSelector: string;
-  name: string;
-  skipFormat: boolean;
-  skipPackageJson: boolean;
-  router: boolean;
-}
