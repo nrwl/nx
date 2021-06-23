c8140023841b60c4355c0d64d1537295c2a8bae7
Miroslav Jonaš <meeroslav@users.noreply.github.com>
2021-06-23 22:47:28 +0200

    chore(repo): fix storybook to 6.2.9 before 6.3.0 support is ready (#6108)

--- packages/storybook/src/utils/versions.ts
+++ packages/storybook/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxVersion = '*';
-export const storybookVersion = '^6.2.7';
+export const storybookVersion = '6.2.9';
 export const babelCoreVersion = '7.12.13';
 export const babelLoaderVersion = '8.1.0';
 export const babelPresetTypescriptVersion = '7.12.13';
