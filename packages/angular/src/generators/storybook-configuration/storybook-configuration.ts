2e4d00f8bed0baeca3e1719e5b4ab52eb7c512f3
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2021-06-24 09:51:11 +0100

    fix(angular): fix storybook-configuration not installing deps (#6109)

--- packages/angular/src/generators/storybook-configuration/storybook-configuration.ts
+++ packages/angular/src/generators/storybook-configuration/storybook-configuration.ts
@@ -1,4 +1,4 @@
-import type { Tree } from '@nrwl/devkit';
+import type { GeneratorCallback, Tree } from '@nrwl/devkit';
 import { assertCompatibleStorybookVersion } from './lib/assert-compatible-storybook-version';
 import { generateStories } from './lib/generate-stories';
 import { generateStorybookConfiguration } from './lib/generate-storybook-configuration';
@@ -8,14 +8,16 @@ import type { StorybookConfigurationOptions } from './schema';
 export async function storybookConfigurationGenerator(
   tree: Tree,
   options: StorybookConfigurationOptions
-) {
+): Promise<GeneratorCallback> {
   assertCompatibleStorybookVersion();
   validateOptions(options);
-  await generateStorybookConfiguration(tree, options);
+  const installTask = await generateStorybookConfiguration(tree, options);
 
   if (options.generateStories) {
-    await generateStories(tree, options);
+    generateStories(tree, options);
   }
+
+  return installTask;
 }
 
 export default storybookConfigurationGenerator;
