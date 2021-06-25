73835e3f4aa607b215a80fc42645b99977c3b697
Juri Strumpflohner <juri.strumpflohner@gmail.com>
2021-06-25 17:12:01 +0200

    fix(storybook): make sure compilation errors get logged to the console (#6129)

--- packages/storybook/src/executors/storybook/storybook.impl.ts
+++ packages/storybook/src/executors/storybook/storybook.impl.ts
@@ -5,7 +5,7 @@ import { constants, copyFileSync, mkdtempSync, statSync } from 'fs';
 import { buildDevStandalone } from '@storybook/core/server';
 
 import { getStorybookFrameworkPath, setStorybookAppProject } from '../utils';
-import { ExecutorContext } from '@nrwl/devkit';
+import { ExecutorContext, logger } from '@nrwl/devkit';
 
 export interface StorybookConfig {
   configFolder?: string;
@@ -50,8 +50,47 @@ export default async function* storybookExecutor(
 }
 
 function runInstance(options: StorybookExecutorOptions) {
-  process.env.NODE_ENV = process.env.NODE_ENV ?? 'development';
-  return buildDevStandalone({ ...options, ci: true } as any);
+  const env = process.env.NODE_ENV ?? 'development';
+  process.env.NODE_ENV = env;
+  return buildDevStandalone({
+    ...options,
+    ci: true,
+    configType: env.toUpperCase(),
+  } as any).catch((error) => {
+    // TODO(juri): find a better cleaner way to handle these. Taken from:
+    // https://github.com/storybookjs/storybook/blob/dea23e5e9a3e7f5bb25cb6520d3011cc710796c8/lib/core-server/src/build-dev.ts#L138-L166
+    if (error instanceof Error) {
+      if ((error as any).error) {
+        logger.error((error as any).error);
+      } else if (
+        (error as any).stats &&
+        (error as any).stats.compilation.errors
+      ) {
+        (error as any).stats.compilation.errors.forEach((e: any) =>
+          logger.log(e)
+        );
+      } else {
+        logger.error(error as any);
+      }
+    } else if (error.compilation?.errors) {
+      error.compilation.errors.forEach((e: any) => logger.log(e));
+    }
+
+    logger.log('');
+    logger.warn(
+      error.close
+        ? `
+          FATAL broken build!, will close the process,
+          Fix the error below and restart storybook.
+        `
+        : `
+          Broken build, fix the error above.
+          You may need to refresh the browser.
+        `
+    );
+
+    process.exit(1);
+  });
 }
 
 function storybookOptionMapper(
