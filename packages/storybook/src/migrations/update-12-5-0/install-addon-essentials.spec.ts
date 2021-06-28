08c575ebd0449a2cfbb3cda5fd2249107f87a078
katerina <sk.katherine@gmail.com>
2021-06-28 18:12:12 +0300

    fix(storybook): change main-js without executing it in migrator

--- packages/storybook/src/migrations/update-12-5-0/install-addon-essentials.spec.ts
+++ packages/storybook/src/migrations/update-12-5-0/install-addon-essentials.spec.ts
@@ -35,4 +35,89 @@ describe('Add the @storybook/addon-essentials package to package.json', () => {
       ]
     ).toBeFalsy();
   });
+
+  describe('Add the @storybook/addon-essentials addon in the addons array in root main.js', () => {
+    beforeEach(() => {
+      writeJson(tree, 'package.json', {
+        devDependencies: {},
+      });
+    });
+    it('should add the addon if other addons exist in the addons array', async () => {
+      tree.write(
+        '.storybook/main.js',
+        `
+        module.exports = {
+          stories: [],
+          addons: ['@storybook/addon-something'],
+        };
+        `
+      );
+
+      await installAddonEssentials(tree);
+      expect(tree.read('.storybook/main.js', 'utf-8')).toBe(`
+        module.exports = {
+          stories: [],
+          addons: ['@storybook/addon-essentials', '@storybook/addon-something'],
+        };
+        `);
+    });
+
+    it('should add the addon if addons array is empty', async () => {
+      tree.write(
+        '.storybook/main.js',
+        `
+        module.exports = {
+          stories: [],
+          addons: [],
+        };
+        `
+      );
+
+      await installAddonEssentials(tree);
+      expect(tree.read('.storybook/main.js', 'utf-8')).toBe(`
+        module.exports = {
+          stories: [],
+          addons: ['@storybook/addon-essentials', ],
+        };
+        `);
+    });
+
+    it('should add the addon if addons array does not exist', async () => {
+      tree.write(
+        '.storybook/main.js',
+        `
+        module.exports = {
+          stories: [],
+        };
+        `
+      );
+
+      await installAddonEssentials(tree);
+      expect(tree.read('.storybook/main.js', 'utf-8')).toBe(`
+        module.exports = {
+          addons: ['@storybook/addon-essentials'], stories: [],
+        };
+        `);
+    });
+
+    it('should not edit the file if @storybook/addon-essentials already exists', async () => {
+      tree.write(
+        '.storybook/main.js',
+        `
+        module.exports = {
+          stories: [],
+          addons: ['@storybook/addon-essentials'],
+        };
+        `
+      );
+
+      await installAddonEssentials(tree);
+      expect(tree.read('.storybook/main.js', 'utf-8')).toBe(`
+        module.exports = {
+          stories: [],
+          addons: ['@storybook/addon-essentials'],
+        };
+        `);
+    });
+  });
 });
