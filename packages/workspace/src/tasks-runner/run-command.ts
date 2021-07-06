4c206349378bf41617342451d32ac0f3e0222f75
Jason Jean <jasonjean1993@gmail.com>
2021-07-06 14:25:51 -0400

    Revert "fix(core): include dependencies of projects which do not have a target (#6245)" (#6270)
    This reverts commit cbc0160953d48778e9dbfe01ef5c5f5e247d2b37.

--- packages/workspace/src/tasks-runner/run-command.ts
+++ packages/workspace/src/tasks-runner/run-command.ts
@@ -305,18 +305,6 @@ function addTasksForProjectDependencyConfig(
           tasksMap,
           [...path, targetIdentifier]
         );
-      } else {
-        for (const dep of dependencies) {
-          addTasksForProjectDependencyConfig(
-            projectGraph.nodes[dep.target],
-            { target, configuration },
-            dependencyConfig,
-            defaultDependencyConfigs,
-            projectGraph,
-            tasksMap,
-            path
-          );
-        }
       }
     }
   } else {
