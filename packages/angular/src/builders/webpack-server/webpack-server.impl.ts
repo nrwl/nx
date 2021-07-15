ec414e5840a7076554fbe1a541d2e9ff26daeef9
Colum Ferry <cferry09@gmail.com>
2021-07-15 11:09:59 +0100

    feat(angular): add executor to allow custom webpack config with serve (#6359)
    Add a builder to allow the user to pass a custom webpack config with serve. It should fetch the
    custom webpack config from the build target.

---
+++ packages/angular/src/builders/webpack-server/webpack-server.impl.ts
@@ -0,0 +1,58 @@
+import { BuilderContext, createBuilder } from '@angular-devkit/architect';
+import { JsonObject } from '@angular-devkit/core';
+import type { Schema } from './schema';
+
+import { parseTargetString, joinPathFragments } from '@nrwl/devkit';
+import { Workspaces } from '@nrwl/tao/src/shared/workspace';
+import {
+  DevServerBuilderOptions,
+  serveWebpackBrowser,
+} from '@angular-devkit/build-angular/src/dev-server';
+import { existsSync } from 'fs';
+import { merge } from 'webpack-merge';
+import { normalizeOptions } from './lib';
+
+export function webpackServer(schema: Schema, context: BuilderContext) {
+  const options = normalizeOptions(schema);
+  const workspaceConfig = new Workspaces(
+    context.workspaceRoot
+  ).readWorkspaceConfiguration();
+
+  const parsedBrowserTarget = parseTargetString(options.browserTarget);
+  const buildTarget =
+    workspaceConfig.projects[parsedBrowserTarget.project].targets[
+      parsedBrowserTarget.target
+    ];
+
+  const selectedConfiguration = parsedBrowserTarget.configuration
+    ? buildTarget.configurations[parsedBrowserTarget.configuration]
+    : buildTarget.configurations[buildTarget.defaultConfiguration];
+
+  const customWebpackConfig: { path: string } =
+    selectedConfiguration.customWebpackConfig ??
+    buildTarget.options.customWebpackConfig;
+
+  if (customWebpackConfig && customWebpackConfig.path) {
+    const pathToWebpackConfig = joinPathFragments(
+      context.workspaceRoot,
+      customWebpackConfig.path
+    );
+
+    if (existsSync(pathToWebpackConfig)) {
+      return serveWebpackBrowser(options as DevServerBuilderOptions, context, {
+        webpackConfiguration: (baseWebpackConfig) => {
+          const customWebpackConfiguration = require(pathToWebpackConfig);
+          return merge(baseWebpackConfig, customWebpackConfiguration);
+        },
+      });
+    } else {
+      throw new Error(
+        `Custom Webpack Config File Not Found!\nTo use a custom webpack config, please ensure the path to the custom webpack file is correct: \n${pathToWebpackConfig}`
+      );
+    }
+  }
+
+  return serveWebpackBrowser(options as DevServerBuilderOptions, context);
+}
+
+export default createBuilder<JsonObject & Schema>(webpackServer);
