e7bfd9b217bab06eb664179f2246de57d555d6ec
James Henry <james@henry.sc>
2021-07-15 21:38:53 +0400

    chore(core): whitespace

--- packages/angular/tailwind.ts
+++ packages/angular/tailwind.ts
@@ -15,7 +15,7 @@ export function createGlobPatternsForDependencies(
     /**
      * It should not be possible to reach this point when the utility is invoked as part of the normal
      * lifecycle of Nx executors. However, other tooling, such as the VSCode Tailwind IntelliSense plugin
-     * or JetBrains editors such as  WebStorm, may execute the tailwind.config.js file in order to provide
+     * or JetBrains editors such as WebStorm, may execute the tailwind.config.js file in order to provide
      * autocomplete features, for example.
      *
      * In order to best support that use-case, we therefore do not hard error when the ProjectGraph is
