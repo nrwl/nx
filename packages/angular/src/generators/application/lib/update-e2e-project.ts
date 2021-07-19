cdaf3ddefce0a22a394871c4c9a6188a6a51a294
Jason Jean <jasonjean1993@gmail.com>
2021-07-19 17:36:00 -0400

    fix(angular): fix generating apps with protractor

--- packages/angular/src/generators/application/lib/update-e2e-project.ts
+++ packages/angular/src/generators/application/lib/update-e2e-project.ts
@@ -1,13 +1,20 @@
-import type { Tree } from '@nrwl/devkit';
+import type { ProjectConfiguration, Tree } from '@nrwl/devkit';
 import type { NormalizedSchema } from './normalized-schema';
 
-import { getWorkspacePath, offsetFromRoot, updateJson } from '@nrwl/devkit';
+import {
+  offsetFromRoot,
+  updateJson,
+  addProjectConfiguration,
+  readProjectConfiguration,
+  updateProjectConfiguration,
+  ProjectType,
+} from '@nrwl/devkit';
 import { Linter } from '@nrwl/linter';
 
-export function updateE2eProject(host: Tree, options: NormalizedSchema) {
+export function updateE2eProject(tree: Tree, options: NormalizedSchema) {
   const spec = `${options.e2eProjectRoot}/src/app.e2e-spec.ts`;
-  const content = host.read(spec, 'utf-8');
-  host.write(
+  const content = tree.read(spec, 'utf-8');
+  tree.write(
     spec,
     content.replace(
       `${options.name} app is running!`,
@@ -16,41 +23,39 @@ export function updateE2eProject(host: Tree, options: NormalizedSchema) {
   );
 
   const page = `${options.e2eProjectRoot}/src/app.po.ts`;
-  const pageContent = host.read(page, 'utf-8');
-  host.write(page, pageContent.replace(`.content span`, `header h1`));
+  const pageContent = tree.read(page, 'utf-8');
+  tree.write(page, pageContent.replace(`.content span`, `header h1`));
 
-  // update workspace.json / angular.json
-  updateJson(host, getWorkspacePath(host), (json) => {
-    const project = {
-      root: options.e2eProjectRoot,
-      projectType: 'application',
-      targets: {
-        e2e: json.projects[options.name].architect.e2e,
-        lint:
-          options.linter === Linter.None
-            ? undefined
-            : {
-                builder: '@nrwl/linter:eslint',
-                options: {
-                  lintFilePatterns: [`${options.e2eProjectRoot}/**/*.ts`],
-                },
+  const proj = readProjectConfiguration(tree, options.name);
+  const project: ProjectConfiguration = {
+    root: options.e2eProjectRoot,
+    projectType: 'application',
+    targets: {
+      e2e: proj.targets.e2e,
+      lint:
+        options.linter === Linter.None
+          ? undefined
+          : {
+              executor: '@nrwl/linter:eslint',
+              options: {
+                lintFilePatterns: [`${options.e2eProjectRoot}/**/*.ts`],
               },
-      },
-    };
+            },
+    },
+  };
+  project.targets.e2e.options.protractorConfig = `${options.e2eProjectRoot}/protractor.conf.js`;
+  // update workspace.json / angular.json
+  addProjectConfiguration(tree, options.e2eProjectName, project);
 
-    project.targets.e2e.options.protractorConfig = `${options.e2eProjectRoot}/protractor.conf.js`;
-
-    json.projects[options.e2eProjectName] = project;
-    delete json.projects[options.name].architect.e2e;
-    return json;
-  });
+  delete proj.targets.e2e;
+  updateProjectConfiguration(tree, options.name, proj);
 
   // update tsconfig e2e
-  if (!host.exists(`${options.e2eProjectRoot}/tsconfig.e2e.json`)) {
-    host.write(`${options.e2eProjectRoot}/tsconfig.e2e.json`, '{}');
+  if (!tree.exists(`${options.e2eProjectRoot}/tsconfig.e2e.json`)) {
+    tree.write(`${options.e2eProjectRoot}/tsconfig.e2e.json`, '{}');
   }
 
-  updateJson(host, `${options.e2eProjectRoot}/tsconfig.e2e.json`, (json) => {
+  updateJson(tree, `${options.e2eProjectRoot}/tsconfig.e2e.json`, (json) => {
     return {
       ...json,
       extends: `./tsconfig.json`,
@@ -62,7 +67,7 @@ export function updateE2eProject(host: Tree, options: NormalizedSchema) {
   });
 
   // update tsconfig
-  updateJson(host, `${options.e2eProjectRoot}/tsconfig.json`, (json) => {
+  updateJson(tree, `${options.e2eProjectRoot}/tsconfig.json`, (json) => {
     return {
       ...json,
       extends: `${offsetFromRoot(options.e2eProjectRoot)}tsconfig.base.json`,
