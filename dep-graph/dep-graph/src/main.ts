1e79dafb23aee68c4e69c916325551a8abca1c52
Philip Fulcher <philip@nrwl.io>
2021-08-02 09:13:57 -0600

    fix(misc): fix affected:dep-graph file output

--- dep-graph/dep-graph/src/main.ts
+++ dep-graph/dep-graph/src/main.ts
@@ -1,14 +1,6 @@
 import { AppComponent } from './app/app';
 import { LocalProjectGraphService } from './app/local-project-graph-service';
 import { environment } from './environments/environment';
-import { projectGraphs } from './graphs';
-
-if (environment.environment !== 'release') {
-  window.affected = [];
-  window.exclude = [];
-  window.projectGraphList = projectGraphs;
-  window.selectedProjectGraph = projectGraphs[0].id;
-}
 
 if (environment.environment === 'dev-watch') {
   window.watch = true;
