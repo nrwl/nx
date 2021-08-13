4b91487c8f0683aca74c4dc4db4a387379e7f9b5
Kirils L <9858620+kirjai@users.noreply.github.com>
2021-08-13 18:08:00 +0100

    Update packages/next/src/utils/versions.ts

--- packages/next/src/utils/versions.ts
+++ packages/next/src/utils/versions.ts
@@ -1,7 +1,7 @@
 export const nxVersion = '*';
 
 export const nextVersion = '11.1.0';
-export const eslintConfigNextVersion = '11.0.1';
+export const eslintConfigNextVersion = '11.1.0';
 export const nodeSass = '5.0.0';
 export const zeitNextLess = '1.0.1';
 export const zeitNextStylus = '1.0.1';
