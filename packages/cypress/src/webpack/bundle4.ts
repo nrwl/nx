9d8431d567bef397630ae9e862b44d7695adf860
Jack Hsu <jack.hsu@gmail.com>
2021-08-20 14:08:55 -0400

    fix(testing): remove webpack dependency since it is causing issues with MFE (#6794)

--- packages/cypress/src/webpack/bundle4.ts
+++ packages/cypress/src/webpack/bundle4.ts
@@ -1,11 +1,5 @@
 module.exports = function (useShim = true) {
-  const webpack = require('webpack');
-  webpack.webpack = webpack;
-  webpack.WebpackError = require('webpack/lib/WebpackError');
-
   return {
-    webpack,
     nodeExternals: require('webpack-node-externals'),
-    version: require('webpack/package.json').version,
   };
 };
