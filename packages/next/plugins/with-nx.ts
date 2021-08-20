e539a381bf55bd254d0e960a1043682cccd636fc
Jason Jean <jasonjean1993@gmail.com>
2021-08-20 18:31:38 -0400

    chore(repo): update nx to 12.8.0-beta.1

--- packages/next/plugins/with-nx.ts
+++ packages/next/plugins/with-nx.ts
@@ -1,6 +1,4 @@
-// ignoring while we support both Next 11.1.0 and versions before it
-// @ts-ignore
-import type { NextConfig } from 'next/dist/next-server/server/config';
+import type { NextConfig } from 'next/types';
 import { WebpackConfigOptions } from '../src/utils/types';
 
 const { join } = require('path');
