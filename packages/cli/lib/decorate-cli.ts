d876acd77bccbe8b37261cfdee8cd80a63092914
Vivek 🧐 <vivekmore@noreply.users.github.com>
2021-09-06 12:43:47 -0400

    cleanup(core): simplify code

--- packages/cli/lib/decorate-cli.ts
+++ packages/cli/lib/decorate-cli.ts
@@ -4,16 +4,13 @@ export function decorateCli() {
   const path = 'node_modules/@angular/cli/lib/cli/index.js';
   const angularCLIInit = readFileSync(path, 'utf-8');
   const start = angularCLIInit.indexOf(`(options) {`) + 11;
-  const end = angularCLIInit.lastIndexOf(`}`) - 2;
 
   const newContent = `${angularCLIInit.substr(0, start)}
   if (!process.env['NX_CLI_SET']) {
     require('@nrwl/cli/bin/nx');
     return new Promise(function(res, rej) {});
-  } else {
-    ${angularCLIInit.substring(start, end)}
   }
-${angularCLIInit.substring(end)}
+  ${angularCLIInit.substring(start)}
 `;
   writeFileSync(path, newContent);
 }
