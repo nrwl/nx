d79bba36307be40c261940d7c32dd2b7aca22668
Victor Savkin <avix1000@gmail.com>
2021-09-17 11:50:44 -0400

    chore(repo): update to nx 12.10

--- dep-graph/dep-graph-e2e/src/plugins/index.js
+++ dep-graph/dep-graph-e2e/src/plugins/index.js
@@ -16,7 +16,5 @@ const { preprocessTypescript } = require('@nrwl/cypress/plugins/preprocessor');
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-
   // Preprocess Typescript file using Nx helper
-  on('file:preprocessor', preprocessTypescript(config));
 };
