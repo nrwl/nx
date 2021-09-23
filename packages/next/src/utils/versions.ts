fec1c39be0177133face9b641c9f36b1da5f0b6e
Jack Hsu <jack.hsu@gmail.com>
2021-09-23 12:01:38 -0400

    feat(react): add migration to replace deprecated node-sass with sass (#7105)

--- packages/next/src/utils/versions.ts
+++ packages/next/src/utils/versions.ts
@@ -2,7 +2,7 @@ export const nxVersion = '*';
 
 export const nextVersion = '11.1.0';
 export const eslintConfigNextVersion = '11.1.0';
-export const nodeSass = '5.0.0';
+export const sassVersion = '1.42.1';
 export const zeitNextLess = '1.0.1';
 export const zeitNextStylus = '1.0.1';
 export const emotionServerVersion = '11.0.0';
