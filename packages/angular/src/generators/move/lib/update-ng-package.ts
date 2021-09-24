36f75fe5c879d2ef99b11ba34d24930e63ae6d1d
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2021-09-24 16:56:47 +0100

    fix(angular): update ng-package.json dest property when moving a library

---
+++ packages/angular/src/generators/move/lib/update-ng-package.ts
@@ -0,0 +1,30 @@
+import { readProjectConfiguration, Tree, updateJson } from '@nrwl/devkit';
+import { appRootPath } from '@nrwl/tao/src/utils/app-root';
+import { getNewProjectName } from '@nrwl/workspace/src/generators/move/lib/utils';
+import { join, relative } from 'path';
+import { Schema } from '../schema';
+
+export function updateNgPackage(tree: Tree, schema: Schema): void {
+  const newProjectName = getNewProjectName(schema.destination);
+  const project = readProjectConfiguration(tree, newProjectName);
+
+  if (project.projectType === 'application') {
+    return;
+  }
+
+  const ngPackagePath = `${project.root}/ng-package.json`;
+  if (!tree.exists(ngPackagePath)) {
+    return;
+  }
+
+  const rootOffset = relative(join(appRootPath, project.root), appRootPath);
+  let output = `dist/${project.root}`;
+  if (project.targets?.build?.outputs?.length > 0) {
+    output = project.targets.build.outputs[0];
+  }
+
+  updateJson(tree, ngPackagePath, (json) => {
+    json.dest = `${rootOffset}/${output}`;
+    return json;
+  });
+}
