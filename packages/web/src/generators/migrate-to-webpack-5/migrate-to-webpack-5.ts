81448ea4ae845e90e5bd0d039327eae29428e296
Katerina Skroumpelou <sk.katherine@gmail.com>
2021-10-01 13:03:43 +0300

    feat(storybook): adding storybook webpack 5 migration to web migrator

--- packages/web/src/generators/migrate-to-webpack-5/migrate-to-webpack-5.ts
+++ packages/web/src/generators/migrate-to-webpack-5/migrate-to-webpack-5.ts
@@ -1,11 +1,17 @@
-import type { GeneratorCallback, Tree } from '@nrwl/devkit';
+import { GeneratorCallback, getProjects, Tree } from '@nrwl/devkit';
 import {
   addDependenciesToPackageJson,
   convertNxGenerator,
   logger,
   removeDependenciesFromPackageJson,
+  formatFiles,
+  applyChangesToString,
+  ChangeType,
 } from '@nrwl/devkit';
+import { readPackageJson } from '@nrwl/workspace/src/core/file-utils';
 import { runTasksInSerial } from '@nrwl/workspace/src/utilities/run-tasks-in-serial';
+import ts = require('typescript');
+import { findNodes } from '@nrwl/workspace/src/utilities/typescript/find-nodes';
 
 const basePackages = {
   'copy-webpack-plugin': '^9.0.1',
@@ -27,6 +33,7 @@ const webPackages = {
 export async function webMigrateToWebpack5Generator(tree: Tree, schema: {}) {
   const packages = { ...basePackages, ...webPackages };
   const tasks: GeneratorCallback[] = [];
+  const packageJson = readPackageJson();
 
   logger.info(`NX Adding webpack 5 to workspace.`);
 
@@ -35,9 +42,194 @@ export async function webMigrateToWebpack5Generator(tree: Tree, schema: {}) {
     removeDependenciesFromPackageJson(tree, [], Object.keys(packages))
   );
 
+  // Here, if our workspace has Storybook for react, we add the Storybook webpack 5 dependencies
+  if (workspaceHasStorybookForReact(packageJson)) {
+    packages['@storybook/builder-webpack5'] =
+      workspaceHasStorybookForReact(packageJson);
+    packages['@storybook/manager-webpack5'] =
+      workspaceHasStorybookForReact(packageJson);
+
+    await migrateStorybookToWebPack5(tree);
+  }
+
   tasks.push(addDependenciesToPackageJson(tree, {}, packages));
 
   return runTasksInSerial(...tasks);
+}
+
+function workspaceHasStorybookForReact(packageJson: any): string | undefined {
+  return (
+    packageJson.dependencies['@storybook/react'] ||
+    packageJson.devDependencies['@storybook/react']
+  );
+}
+
+async function migrateStorybookToWebPack5(tree: Tree) {
+  allReactProjectsWithStorybookConfiguration(tree).forEach((project) => {
+    editProjectMainJs(
+      tree,
+      `${project.storybookConfigPath}/main.js`,
+      project.projectName
+    );
+  });
+  await formatFiles(tree);
+}
+
+function allReactProjectsWithStorybookConfiguration(tree: Tree): {
+  projectName: string;
+  storybookConfigPath: string;
+}[] {
+  const projects = getProjects(tree);
+  const reactProjectsThatHaveStorybookConfiguration: {
+    projectName: string;
+    storybookConfigPath: string;
+  }[] = [...projects.entries()]
+    ?.filter(
+      ([_, projectConfig]) =>
+        projectConfig.targets &&
+        projectConfig.targets.storybook &&
+        projectConfig.targets.storybook.options
+    )
+    ?.map(([projectName, projectConfig]) => {
+      if (
+        projectConfig.targets &&
+        projectConfig.targets.storybook &&
+        projectConfig.targets.storybook.options?.config?.configFolder &&
+        projectConfig.targets.storybook.options?.uiFramework ===
+          '@storybook/react'
+      ) {
+        return {
+          projectName,
+          storybookConfigPath:
+            projectConfig.targets.storybook.options.config.configFolder,
+        };
+      }
+    });
+  return reactProjectsThatHaveStorybookConfiguration;
+}
+
+function editProjectMainJs(
+  tree: Tree,
+  projectMainJsFile: string,
+  projectName: string
+) {
+  let newContents: string;
+  let moduleExportsIsEmptyOrNonExistentOrInvalid = false;
+  let alreadyHasBuilder: any;
+  const rootMainJsExists = tree.exists(projectMainJsFile);
+  if (rootMainJsExists) {
+    const file = getTsSourceFile(tree, projectMainJsFile);
+    const appFileContent = tree.read(projectMainJsFile, 'utf-8');
+    newContents = appFileContent;
+    const moduleExportsFull = findNodes(file, [
+      ts.SyntaxKind.ExpressionStatement,
+    ]);
+
+    if (moduleExportsFull && moduleExportsFull[0]) {
+      const moduleExports = moduleExportsFull[0];
+      const listOfStatements = findNodes(moduleExports, [
+        ts.SyntaxKind.SyntaxList,
+      ]);
+
+      /**
+       * Keep the index of the stories node
+       * to put the core object before it
+       * if it does not exist already
+       */
+
+      let indexOfStoriesNode = -1;
+
+      const hasCoreObject = listOfStatements[0]?.getChildren()?.find((node) => {
+        if (
+          node &&
+          node.getText().length > 0 &&
+          indexOfStoriesNode < 0 &&
+          node?.getText().startsWith('stories')
+        ) {
+          indexOfStoriesNode = node.getStart();
+        }
+        return (
+          node?.kind === ts.SyntaxKind.PropertyAssignment &&
+          node?.getText().startsWith('core')
+        );
+      });
+
+      if (hasCoreObject) {
+        const contentsOfCoreNode = hasCoreObject.getChildren().find((node) => {
+          return node.kind === ts.SyntaxKind.ObjectLiteralExpression;
+        });
+        const everyAttributeInsideCoreNode = contentsOfCoreNode
+          .getChildren()
+          .find((node) => node.kind === ts.SyntaxKind.SyntaxList);
+
+        alreadyHasBuilder = everyAttributeInsideCoreNode
+          .getChildren()
+          .find((node) => node.getText() === "builder: 'webpack5'");
+
+        if (!alreadyHasBuilder) {
+          newContents = applyChangesToString(newContents, [
+            {
+              type: ChangeType.Insert,
+              index: contentsOfCoreNode.getEnd() - 1,
+              text: ", builder: 'webpack5'",
+            },
+          ]);
+        }
+      } else if (indexOfStoriesNode >= 0) {
+        /**
+         * Does not have core object,
+         * so just write one, at the start.
+         */
+        newContents = applyChangesToString(newContents, [
+          {
+            type: ChangeType.Insert,
+            index: indexOfStoriesNode - 1,
+            text: "core: { ...rootMain.core, builder: 'webpack5' }, ",
+          },
+        ]);
+      } else {
+        /**
+         * Module exports is empty or does not
+         * contain stories - most probably invalid
+         */
+        moduleExportsIsEmptyOrNonExistentOrInvalid = true;
+      }
+    } else {
+      /**
+       * module.exports does not exist
+       */
+      moduleExportsIsEmptyOrNonExistentOrInvalid = true;
+    }
+  } else {
+    moduleExportsIsEmptyOrNonExistentOrInvalid = true;
+  }
+
+  if (moduleExportsIsEmptyOrNonExistentOrInvalid) {
+    logger.info(
+      `Please configure Storybook for project "${projectName}"", since it has not been configured properly.`
+    );
+    return;
+  }
+
+  if (!alreadyHasBuilder) {
+    tree.write(projectMainJsFile, newContents);
+  }
+}
+
+function getTsSourceFile(host: Tree, path: string): ts.SourceFile {
+  const buffer = host.read(path);
+  if (!buffer) {
+    throw new Error(`Could not read TS file (${path}).`);
+  }
+  const content = buffer.toString();
+  const source = ts.createSourceFile(
+    path,
+    content,
+    ts.ScriptTarget.Latest,
+    true
+  );
+
+  return source;
 }
 
 export default webMigrateToWebpack5Generator;
