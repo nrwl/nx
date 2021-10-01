259ac84c63a6d8a333289074009b221a5bde5b49
Minijus L <3633549+minijus@users.noreply.github.com>
2021-10-01 23:47:52 +0300

    fix(angular): update jest-preset-angular to 10.0.1 (#7139)

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -4,5 +4,5 @@ export const angularDevkitVersion = '~12.2.0';
 export const angularJsVersion = '1.7.9';
 export const ngrxVersion = '~12.4.0';
 export const rxjsVersion = '~6.6.0';
-export const jestPresetAngularVersion = '9.0.7';
+export const jestPresetAngularVersion = '10.0.1';
 export const angularEslintVersion = '~12.3.0';
