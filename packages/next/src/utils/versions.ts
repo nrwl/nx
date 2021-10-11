e5c63c1e3a90d7b2bd97f09a4f5972a3ab9fdd2d
kirjai <9858620+kirjai@users.noreply.github.com>
2021-10-11 13:00:05 +0100

    feat(nextjs): use next 11.1.2

--- packages/next/src/utils/versions.ts
+++ packages/next/src/utils/versions.ts
@@ -1,7 +1,7 @@
 export const nxVersion = '*';
 
-export const nextVersion = '11.1.0';
-export const eslintConfigNextVersion = '11.1.0';
+export const nextVersion = '11.1.2';
+export const eslintConfigNextVersion = '11.1.2';
 export const sassVersion = '1.42.1';
 export const zeitNextLess = '1.0.1';
 export const zeitNextStylus = '1.0.1';
