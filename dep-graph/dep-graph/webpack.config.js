7d4c350b78b9e6c831803a1740f3d73d7a9ff309
Jason Jean <jasonjean1993@gmail.com>
2021-10-14 14:12:40 -0400

    chore(repo): update nx to 13.0.0-beta.8 (#7361)

--- dep-graph/dep-graph/webpack.config.js
+++
@@ -1,6 +0,0 @@
-// This is a workaround for a problem in Nx 13.0.0-beta.7
-// TODO(jack): Can remove this after we patch Nx in 13.0.0 beta.8 or final release.
-module.exports = (config) => {
-  config.optimization.providedExports = true;
-  return config;
-};
