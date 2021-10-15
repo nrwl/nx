fa9b9c41ddfb0514c1bda460f38fc9c08b93f6f4
ben <contact@benjamincabanes.com>
2021-10-15 15:31:22 -0400

    docs(nxdev): remove unused components

--- nx-dev/ui/common/src/index.ts
+++ nx-dev/ui/common/src/index.ts
@@ -3,6 +3,5 @@ export * from './lib/feature-list';
 export * from './lib/footer';
 export * from './lib/selector';
 export * from './lib/inline-command';
-export * from './lib/interactive-sections';
 export * from './lib/nx-users-showcase';
 export * from './lib/plugin-card';
