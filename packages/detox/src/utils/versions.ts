50006ae2e0bf28b3091ced68c75a4927c1cf3352
Emily Xiong <xiongemi@gmail.com>
2021-10-15 15:15:51 -0400

    feat(react-native): upgrade @types/react-native versions - upgrade @type/react-native, detox and @react-native-community/cli

--- packages/detox/src/utils/versions.ts
+++ packages/detox/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxVersion = '*';
 
-export const detoxVersion = '18.22.1';
-export const typesDetoxVersion = '17.14.2';
+export const detoxVersion = '18.22.2';
+export const typesDetoxVersion = '17.14.3';
 export const testingLibraryJestDom = '5.14.1';
--- packages/react-native/src/utils/versions.ts
+++ packages/react-native/src/utils/versions.ts
@@ -1,20 +1,17 @@
 export const nxVersion = '*';
 
-export const reactVersion = '17.0.2';
-export const typesReactVersion = '17.0.19';
-
-export const reactNativeVersion = '0.66.0';
-export const typesReactNativeVersion = '0.65.3';
+export const reactNativeVersion = '0.66.1';
+export const typesReactNativeVersion = '0.65.8';
 
 export const metroVersion = '0.66.2';
 
-export const reactNativeCommunityCli = '6.0.0';
-export const reactNativeCommunityCliIos = '6.0.0';
-export const reactNativeCommunityCliAndroid = '6.0.0';
+export const reactNativeCommunityCli = '6.1.0';
+export const reactNativeCommunityCliIos = '6.1.0';
+export const reactNativeCommunityCliAndroid = '6.1.0';
 
 export const metroReactNativeBabelPresetVersion = '0.66.2';
 
-export const testingLibraryReactNativeVersion = '8.0.0-rc.0';
+export const testingLibraryReactNativeVersion = '8.0.0';
 export const testingLibraryJestNativeVersion = '4.0.2';
 
 export const jestReactNativeVersion = '18.0.0';
