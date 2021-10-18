b599b037383f948d3794e569b2585aec93191f30
Miroslav Jonas <missing.manual@gmail.com>
2021-10-18 09:53:36 +0200

    fix(linter): add eslint version to package.json

--- packages/linter/src/utils/versions.ts
+++ packages/linter/src/utils/versions.ts
@@ -4,6 +4,6 @@ export const tslintVersion = '~6.1.0';
 export const tslintToEslintConfigVersion = '^2.4.0';
 export const buildAngularVersion = '~12.2.0';
 
-export const typescriptESLintVersion = '~4.31.1';
-export const eslintVersion = '7.22.0';
+export const typescriptESLintVersion = '~4.33.0';
+export const eslintVersion = '7.32.0';
 export const eslintConfigPrettierVersion = '8.1.0';
