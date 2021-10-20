25eee5bd643e59f060fc9bf34a4567616b192208
Jack Hsu <jack.hsu@gmail.com>
2021-10-19 21:24:53 -0400

    chore(react): update react packages

--- packages/next/src/utils/versions.ts
+++ packages/next/src/utils/versions.ts
@@ -2,8 +2,8 @@ export const nxVersion = '*';
 
 export const nextVersion = '11.1.2';
 export const eslintConfigNextVersion = '11.1.2';
-export const sassVersion = '1.42.1';
+export const sassVersion = '1.43.2';
 export const nextWithLess = '1.0.1';
 export const stylusLoader = '6.2.0';
-export const emotionServerVersion = '11.0.0';
+export const emotionServerVersion = '11.4.0';
 export const babelPluginStyledComponentsVersion = '1.10.7';
--- packages/react/src/utils/versions.ts
+++ packages/react/src/utils/versions.ts
@@ -3,32 +3,32 @@ export const nxVersion = '*';
 export const reactVersion = '17.0.2';
 export const reactDomVersion = '17.0.2';
 export const reactIsVersion = '17.0.2';
-export const typesReactVersion = '17.0.3';
-export const typesReactDomVersion = '17.0.3';
-export const typesReactIsVersion = '17.0.0';
+export const typesReactVersion = '17.0.30';
+export const typesReactDomVersion = '17.0.9';
+export const typesReactIsVersion = '17.0.2';
 
-export const styledComponentsVersion = '5.2.1';
-export const typesStyledComponentsVersion = '5.1.9';
+export const styledComponentsVersion = '5.3.3';
+export const typesStyledComponentsVersion = '5.1.15';
 
-export const emotionStyledVersion = '11.1.5';
-export const emotionReactVersion = '11.1.5';
-export const emotionBabelPlugin = '11.2.0';
+export const emotionStyledVersion = '11.3.0';
+export const emotionReactVersion = '11.5.0';
+export const emotionBabelPlugin = '11.3.0';
 
-export const styledJsxVersion = '3.4.4';
-export const typesStyledJsxVersion = '2.2.8';
+export const styledJsxVersion = '4.0.1';
+export const typesStyledJsxVersion = '3.4.4';
 
-export const reactRouterDomVersion = '5.2.0';
-export const typesReactRouterDomVersion = '5.1.7';
+export const reactRouterDomVersion = '5.3.0';
+export const typesReactRouterDomVersion = '5.3.1';
 
-export const testingLibraryReactVersion = '11.2.6';
-export const testingLibraryReactHooksVersion = '7.0.1';
+export const testingLibraryReactVersion = '12.1.2';
+export const testingLibraryReactHooksVersion = '7.0.2';
 
-export const reduxjsToolkitVersion = '1.5.1';
-export const reactReduxVersion = '7.2.3';
+export const reduxjsToolkitVersion = '1.6.2';
+export const reactReduxVersion = '7.2.5';
 
-export const eslintPluginImportVersion = '2.22.1';
+export const eslintPluginImportVersion = '2.25.2';
 export const eslintPluginJsxA11yVersion = '6.4.1';
-export const eslintPluginReactVersion = '7.23.1';
+export const eslintPluginReactVersion = '7.26.1';
 export const eslintPluginReactHooksVersion = '4.2.0';
 
 export const babelPluginStyledComponentsVersion = '1.10.7';
