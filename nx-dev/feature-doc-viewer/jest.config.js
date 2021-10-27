4b4266a17ee48fcdf239c2d103dc4714812a05b4
Miroslav Jonaš <meeroslav@users.noreply.github.com>
2021-10-27 07:33:00 -0500

    fix(nxdev): fix broken test setup (#7534)

--- nx-dev/feature-doc-viewer/jest.config.js
+++ nx-dev/feature-doc-viewer/jest.config.js
@@ -7,5 +7,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/nx-dev/feature-doc-viewer',
-  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
 };
