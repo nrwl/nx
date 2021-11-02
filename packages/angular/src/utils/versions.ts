78116905efdbea95f42759bcbae0f9bfa22a4068
Colum Ferry <cferry09@gmail.com>
2021-11-02 15:58:31 +0000

    chore(angular): rxjs7

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -3,6 +3,6 @@ export const angularVersion = '^13.0.0-rc.2';
 export const angularDevkitVersion = '~13.0.0-rc.2';
 export const angularJsVersion = '1.7.9';
 export const ngrxVersion = '~12.5.0';
-export const rxjsVersion = '~6.6.0';
+export const rxjsVersion = '~7.4.0';
 export const jestPresetAngularVersion = '10.1.0';
 export const angularEslintVersion = '~12.6.0';
