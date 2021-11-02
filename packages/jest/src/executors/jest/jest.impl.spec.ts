74611cb75d71758fddbd522756a7e22714e2fdfa
Caleb Ukle <caleb@nrwl.io>
2021-11-02 09:27:20 -0500

    fix(testing): fix filesystem issue with jest tests
    mock readConfig to always return an basic config to prevent issues of not finding a config in non
    linux environments, aka missing /root

--- packages/jest/src/executors/jest/jest.impl.spec.ts
+++ packages/jest/src/executors/jest/jest.impl.spec.ts
@@ -1,19 +1,20 @@
 import { ExecutorContext } from '@nrwl/tao/src/shared/workspace';
-import * as fs from 'fs';
 
 let runCLI = jest.fn();
+let readConfig = jest.fn(() =>
+  Promise.resolve({
+    projectConfig: {
+      displayName: 'something',
+    },
+  })
+);
+
 jest.mock('jest', () => ({
   runCLI,
 }));
 
-// Mocking these 2 functions is required for jest-config
-// Otherwise, jest throws with "Can't find a root directory while resolving a config file path"
-jest.mock('fs', () => ({
-  ...(jest.requireActual('fs') as typeof fs),
-  existsSync: () => true,
-  lstatSync: () => ({
-    isDirectory: () => false,
-  }),
+jest.mock('jest-config', () => ({
+  readConfig,
 }));
 
 import { jestExecutor } from './jest.impl';
@@ -60,6 +61,10 @@ describe('Jest Executor', () => {
   });
 
   afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(() => {
     jest.resetAllMocks();
   });
 
