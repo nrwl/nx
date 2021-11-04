b6aaeeff35992f22ed934dbbf41bed46c448dea6
Tuan Dang <tuandang@i3-automation.com>
2021-11-04 15:17:36 +0700

    fix(web): option --no-runtimeChunk is not working

--- packages/web/src/executors/webpack/webpack.impl.ts
+++ packages/web/src/executors/webpack/webpack.impl.ts
@@ -50,6 +50,7 @@ export interface WebWebpackExecutorOptions extends BuildBuilderOptions {
 
   vendorChunk?: boolean;
   commonChunk?: boolean;
+  runtimeChunk?: boolean;
 
   namedChunks?: boolean;
 
