0ca5a88bf16fbaa14c0d6d7ce75978f1456520fa
Caleb Ukle <caleb.ukle+github@pm.me>
2021-11-05 14:25:25 -0500

    fix(testing) generated jest ts configs mismatch (#7556)
    * fix(testing): add .test. files to spec tsconfig template
    
    add support for .test.ts and with supportTsx .test.{js|tsx|jsx} files
    in the tsconfig.spec.json template.
    
    Note each package that excludes testing files will need to be updated to
    exclude the new files (coming in future commits)
    
    * fix(angular): add .test.ts support in tsconfigs
    
    add .test.ts in angular jest template
    update angular generators to exclude test files
    
    * fix(gatsby): add .test. file prefixes to tsconfig ignore
    
    ignore the new .test.{js|ts|jsx|tsx} files added to jest configs
    
    * fix(react): add .test. file prefix to app/lib tsconfigs
    
    add .test.{ts|js|tsx|jsx} files to be excluded by app/lib tsconfig
    
    * fix(node): add .test. file prefix to app/lib tsconfig
    
    add .test.{ts|js|tsx|jsx} files to be excluded by app/lib tsconfig
    
    ISSUES CLOSED: #7263
    
    * fix(nest): update tests for new .test.ts files in tsconfig
    
    update tests to account for changes in node generators which add .test.ts to the exclude of
    tsconfigs
    
    * fix(express): update tests to account for new tsconfig changes
    
    updated snapshots
    added new patterns for tsconfig
    
    * fix(linter): update snapshots for new tsconfig changes
    
    snapshot updated to include new .test.ts file changes for tsconfig
    
    * fix(node): update testing to account for spec tsconfig changes
    
    add .test.ts files in e2e tests
    
    * fix(testing): add migration for tsconfig changes
    
    initial migration and test for tsconfig changes
    
    * chore(testing): add to migrations collection
    
    add the update-ts-config-for-test-filenames to migrations
    
    * fix(testing): fix filesystem issue with jest tests
    
    mock readConfig to always return an basic config to prevent issues of not finding a config in non
    linux environments, aka missing /root
    
    * cleanup(testing): update error message
    
    update message logged when tsconfig isn't found to mention what was trying to be accompished so it
    can be manually updated if desired.

---
+++ packages/jest/src/migrations/update-13-1-2/update-tsconfigs-for-tests.spec.ts
@@ -0,0 +1,198 @@
+import { addProjectConfiguration, Tree } from '@nrwl/devkit';
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import update from './update-tsconfigs-for-tests';
+
+const reactTsConfigs = {
+  app: {
+    extends: './tsconfig.json',
+    compilerOptions: {
+      outDir: '../../dist/out-tsc',
+      types: ['node'],
+    },
+    files: [
+      '../../node_modules/@nrwl/react/typings/cssmodule.d.ts',
+      '../../node_modules/@nrwl/react/typings/image.d.ts',
+    ],
+    exclude: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
+    include: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+  },
+  lib: {
+    extends: './tsconfig.json',
+    compilerOptions: {
+      outDir: '../../dist/out-tsc',
+      types: ['node'],
+    },
+    files: [
+      '../../node_modules/@nrwl/react/typings/cssmodule.d.ts',
+      '../../node_modules/@nrwl/react/typings/image.d.ts',
+    ],
+    exclude: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
+    include: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+  },
+  spec: {
+    extends: './tsconfig.json',
+    compilerOptions: {
+      outDir: '../../dist/out-tsc',
+      module: 'commonjs',
+      types: ['jest', 'node'],
+    },
+    include: [
+      '**/*.spec.ts',
+      '**/*.spec.tsx',
+      '**/*.spec.js',
+      '**/*.spec.jsx',
+      '**/*.d.ts',
+    ],
+    files: [
+      '../../node_modules/@nrwl/react/typings/cssmodule.d.ts',
+      '../../node_modules/@nrwl/react/typings/image.d.ts',
+    ],
+  },
+  expectedFilesToContain: [
+    '**/*.spec.ts',
+    '**/*.test.ts',
+    '**/*.spec.tsx',
+    '**/*.test.tsx',
+    '**/*.spec.js',
+    '**/*.test.js',
+    '**/*.spec.jsx',
+    '**/*.test.jsx',
+  ],
+};
+const angularTsConfigs = {
+  app: {
+    extends: './tsconfig.json',
+    compilerOptions: {
+      outDir: '../../dist/out-tsc',
+      types: [],
+    },
+    files: ['src/main.ts', 'src/polyfills.ts'],
+    include: ['src/**/*.d.ts'],
+    exclude: ['**/*.spec.ts'],
+  },
+  lib: {
+    extends: './tsconfig.json',
+    compilerOptions: {
+      outDir: '../../dist/out-tsc',
+      target: 'es2015',
+      declaration: true,
+      declarationMap: true,
+      inlineSources: true,
+      types: [],
+      lib: ['dom', 'es2018'],
+    },
+    exclude: ['src/test-setup.ts', '**/*.spec.ts'],
+    include: ['**/*.ts'],
+  },
+  spec: {
+    extends: './tsconfig.json',
+    compilerOptions: {
+      outDir: '../../dist/out-tsc',
+      module: 'commonjs',
+      types: ['jest', 'node'],
+    },
+    files: ['src/test-setup.ts'],
+    include: ['**/*.spec.ts', '**/*.d.ts'],
+  },
+  expectedFilesToContain: ['**/*.spec.ts', '**/*.test.ts'],
+};
+[
+  // test TSX/JSX support
+  { name: 'React App', configs: reactTsConfigs },
+  // test non TSX/JSX support
+  { name: 'Angular App', configs: angularTsConfigs },
+].forEach(({ name, configs }) => {
+  describe(`Jest Migration (v13.1.2): ${name}`, () => {
+    let tree: Tree;
+
+    beforeEach(() => {
+      tree = createTreeWithEmptyWorkspace();
+      tree.write(
+        'apps/project-one/tsconfig.app.json',
+        String.raw`${JSON.stringify(configs.app, null, 2)}`
+      );
+      tree.write(
+        'apps/project-one/tsconfig.spec.json',
+        String.raw`${JSON.stringify(configs.spec, null, 2)}`
+      );
+      tree.write(
+        'libs/lib-one/tsconfig.lib.json',
+        String.raw`${JSON.stringify(configs.lib, null, 2)}`
+      );
+      tree.write(
+        'libs/lib-one/tsconfig.spec.json',
+        String.raw`${JSON.stringify(configs.spec, null, 2)}`
+      );
+
+      addProjectConfiguration(tree, 'lib-one', {
+        root: 'libs/lib-one',
+        sourceRoot: 'libs/lib-one/src',
+        projectType: 'library',
+        targets: {
+          build: {
+            executor: '@nrwl/web:build',
+          },
+          test: {
+            executor: '@nrwl/jest:jest',
+            options: {
+              jestConfig: 'libs/lib-one/jest.config.js',
+              passWithNoTests: true,
+            },
+          },
+        },
+      });
+      addProjectConfiguration(tree, 'project-one', {
+        root: 'apps/project-one',
+        sourceRoot: 'apps/project-one/src',
+        projectType: 'application',
+        targets: {
+          build: {
+            executor: '@nrwl/web:build',
+          },
+          test: {
+            executor: '@nrwl/jest:jest',
+            options: {
+              jestConfig: 'apps/project-one/jest.config.js',
+              passWithNoTests: true,
+            },
+          },
+        },
+      });
+    });
+
+    it('should update tsconfig.spec.json to include .test. files', async () => {
+      await update(tree);
+      const tsAppSpecConfig = JSON.parse(
+        tree.read('apps/project-one/tsconfig.spec.json', 'utf-8')
+      );
+      const tsLibSpecConfig = JSON.parse(
+        tree.read('apps/project-one/tsconfig.spec.json', 'utf-8')
+      );
+
+      expect(tsAppSpecConfig.include).toEqual(
+        expect.arrayContaining(configs.expectedFilesToContain)
+      );
+
+      expect(tsLibSpecConfig.include).toEqual(
+        expect.arrayContaining(configs.expectedFilesToContain)
+      );
+    });
+
+    it('should update tsconfig.{lib|app}.json to exclude .test. files', async () => {
+      await update(tree);
+      const tsAppConfig = JSON.parse(
+        tree.read('apps/project-one/tsconfig.app.json', 'utf-8')
+      );
+      const tsLibConfig = JSON.parse(
+        tree.read('apps/project-one/tsconfig.app.json', 'utf-8')
+      );
+
+      expect(tsAppConfig.exclude).toEqual(
+        expect.arrayContaining(configs.expectedFilesToContain)
+      );
+      expect(tsLibConfig.exclude).toEqual(
+        expect.arrayContaining(configs.expectedFilesToContain)
+      );
+    });
+  });
+});
