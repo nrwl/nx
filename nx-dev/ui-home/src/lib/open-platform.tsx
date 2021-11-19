dfa9933ca5ac1b70634490b53aeb82c0301a31db
ben <3447705+bcabanes@users.noreply.github.com>
2021-11-19 10:35:29 -0500

    docs(nxdev): faster animation appearance

--- nx-dev/ui-home/src/lib/open-platform.tsx
+++ nx-dev/ui-home/src/lib/open-platform.tsx
@@ -74,7 +74,7 @@ export function OpenPlatform(): ReactComponentElement<any> {
     },
   };
   const controls = useAnimation();
-  const [ref, inView] = useInView({ threshold: 0.5, triggerOnce: true });
+  const [ref, inView] = useInView({ triggerOnce: true });
 
   useEffect(() => {
     if (!inView) return;
--- nx-dev/ui-home/src/lib/performance.tsx
+++ nx-dev/ui-home/src/lib/performance.tsx
@@ -54,7 +54,7 @@ export function Performance(): ReactComponentElement<any> {
     },
   };
   const controls = useAnimation();
-  const [ref, inView] = useInView({ threshold: 0.5, triggerOnce: true });
+  const [ref, inView] = useInView({ triggerOnce: true });
 
   useEffect(() => {
     if (!inView) return;
