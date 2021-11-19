99bd24fc8de0961585c6f1d1fbfeddabfe291c9f
“JamesHenry” <james@henry.sc>
2021-11-19 16:28:08 +0000

    fix(core): align version of eslint across all packages

--- packages/linter/src/utils/versions.ts
+++ packages/linter/src/utils/versions.ts
@@ -5,5 +5,5 @@ export const tslintToEslintConfigVersion = '^2.4.0';
 export const buildAngularVersion = '~13.0.0';
 
 export const typescriptESLintVersion = '~5.3.0';
-export const eslintVersion = '8.1.0';
+export const eslintVersion = '8.2.0';
 export const eslintConfigPrettierVersion = '8.1.0';
