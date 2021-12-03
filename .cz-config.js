5359ce9ba56f025bba8d29c067b5db547cbca044
ben <3447705+bcabanes@users.noreply.github.com>
2021-12-03 17:18:23 -0500

    fix(detox): test assertion on starter page

--- .cz-config.js
+++ .cz-config.js
@@ -26,6 +26,7 @@ module.exports = {
     { name: 'nx-plugin', description: 'anything Nx Plugin specific' },
     { name: 'react', description: 'anything React specific' },
     { name: 'react-native', description: 'anything React Native specific' },
+    { name: 'detox', description: 'anything Detox specific' },
     { name: 'web', description: 'anything Web specific' },
     { name: 'linter', description: 'anything Linter specific' },
     { name: 'storybook', description: 'anything Storybook specific' },
