31fa41b2d1dfe4a3582176f641565415c349a1ef
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2021-12-08 14:14:30 +0000

    fix(angular): fix @nrwl/angular package build to produce the right type definitions and package entry points

--- scripts/build-angular.js
+++ scripts/build-angular.js
@@ -4,6 +4,5 @@ const fs = require('fs-extra');
 childProcess.execSync(`npx ng-packagr -p packages/angular/ng-package.json`, {
   stdio: [0, 1, 2],
 });
-fs.removeSync('packages/angular/dist/src');
 fs.copySync('packages/angular/dist/', 'build/packages/angular/');
 fs.removeSync('packages/angular/dist/');
