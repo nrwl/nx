c6059df65d2581c5f59e34cb9685525ee01461f1
Benjamin Cabanes <3447705+bcabanes@users.noreply.github.com>
2021-12-09 17:53:06 -0500

    docs(nxdev): dialog path cleaner update (#8092)

--- nx-dev/feature-flavor-selection/src/lib/utils.ts
+++ nx-dev/feature-flavor-selection/src/lib/utils.ts
@@ -8,18 +8,17 @@ export function pathCleaner(
   flavors: FlavorMetadata[]
 ) {
   return (path: string): string => {
-    const myPath = path
-      .split('/')
-      .filter(
-        (segment: string) =>
-          !versions.find((v) => [v.alias, v.id].includes(segment))
-      )
-      .filter(
-        (segment: string) =>
-          !flavors.find((f) => [f.alias, f.id].includes(segment))
-      )
-      .join('/');
+    const [first, second, ...others] = path.split('/').filter(Boolean);
+    const cleanPath = [];
 
-    return myPath;
+    if (!versions.find((v) => [v.alias, v.id].includes(first))) {
+      cleanPath.push(first);
+    }
+
+    if (!flavors.find((f) => [f.alias, f.id].includes(second))) {
+      cleanPath.push(second);
+    }
+
+    return '/' + (others ? cleanPath.concat(...others) : cleanPath).join('/');
   };
 }
