ba6e9c3dee54804920ff8b5321a98a1d7f51679f
Philipp Kirchniawy <bestellungen.philipp@gmail.com>
2021-12-14 13:26:34 +0100

    fix(storybook): added migration for missing outDir property for existing Angular Storybook configurations
    ISSUES CLOSED: #7825

---
+++ packages/storybook/src/migrations/update-13.3.0/update-angular-storybook-tsconfig-outdir.spec.ts
@@ -0,0 +1,57 @@
+import { readJson, Tree, writeJson } from '@nrwl/devkit';
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import addOutDirToAngularStorybookTsconfig from './update-angular-storybook-tsconfig-outdir';
+
+describe('Add outdir to Storybook tsconfig when uiFramework is angular migration', () => {
+  let tree: Tree;
+
+  beforeEach(async () => {
+    tree = createTreeWithEmptyWorkspace();
+
+    writeJson(tree, 'workspace.json', {
+      projects: {
+        ['home-ui-angular']: {
+          projectType: 'application',
+          root: 'apps/myapps/ui-angular',
+          sourceRoot: 'apps/myapps/ui-angular/src',
+          targets: {
+            storybook: {
+              builder: '@nrwl/storybook:storybook',
+              options: {
+                uiFramework: '@storybook/angular',
+                port: 4400,
+                config: {
+                  configFolder: 'apps/myapps/ui-angular/.storybook',
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+
+    writeJson(tree, 'apps/myapps/ui-angular/.storybook/tsconfig.json', {
+      extends: '../tsconfig.json',
+      compilerOptions: {
+        emitDecoratorMetadata: true,
+        // apps/myapps/ui-angular/.storybook -> should be 4 directories up to root
+        outDir: '../../../../dist/out-tsc'
+      },
+      
+      exclude: [
+        '../**/*.spec.ts',        
+      ],
+      include: ['../src/**/*', '*.js'],
+    });
+  });
+
+  it(`should add outDir compilerOption`, async () => {
+    await addOutDirToAngularStorybookTsconfig(tree);
+
+    const tsConfigContent = readJson(
+      tree,
+      'apps/myapps/ui-angular/.storybook/tsconfig.json'
+    );
+    expect(tsConfigContent).toMatchSnapshot();
+  });
+});
---
+++ packages/storybook/src/migrations/update-13.3.0/update-angular-storybook-tsconfig-outdir.ts
@@ -0,0 +1,72 @@
+import {
+  formatFiles,
+  getProjects,
+  joinPathFragments,
+  logger,
+  offsetFromRoot,
+  readJson,
+  Tree,
+  writeJson,
+} from '@nrwl/devkit';
+import * as path from 'path';
+import { isFramework, TsConfig } from '../../utils/utilities';
+
+/**
+ * Migration that adds the outDir property to the compilerOptions in the Storybook configuration.
+ * It will look up all Storybook Projects and update only the one using Angular as uiFramework.
+ * @param tree virtual file system tree representing the physical file system we want to modify
+ */
+export default async function addOutDirToAngularStorybookTsconfig(tree: Tree) {
+  let changesMade = false;
+  const projects = getProjects(tree);
+
+  projects.forEach((projectConfig, projectName) => {
+    const targets = projectConfig.targets;
+    const storybookRoot = path.join(projectConfig.root, '.storybook');
+
+    const paths = {
+      tsConfigStorybook: joinPathFragments(
+        projectConfig.root,
+        '.storybook/tsconfig.json'
+      ),
+    };
+
+    const storybookExecutor =
+      targets &&
+      Object.keys(targets).find(
+        (x) => targets[x].executor === '@nrwl/storybook:storybook'
+      );
+
+    const hasStorybookConfig =
+      storybookExecutor && tree.exists(paths.tsConfigStorybook);
+
+    if (!hasStorybookConfig) {
+      logger.info(
+        `${projectName}: no storybook configured. skipping migration...`
+      );
+      return;
+    }
+
+    const isAngularProject = isFramework('angular', {
+      uiFramework: targets[storybookExecutor].options
+        ?.uiFramework as Parameters<typeof isFramework>[1]['uiFramework'],
+    });
+
+    if (isAngularProject) {
+      const tsConfig = {
+        storybook: readJson<TsConfig>(tree, paths.tsConfigStorybook),
+      };
+
+      tsConfig.storybook.compilerOptions.outDir = `${offsetFromRoot(
+        storybookRoot
+      )}dist/out-tsc`
+
+      writeJson(tree, paths.tsConfigStorybook, tsConfig.storybook);
+      changesMade = true;
+    }
+  });
+
+  if (changesMade) {
+    await formatFiles(tree);
+  }
+}
