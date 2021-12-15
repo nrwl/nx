10c0e76db9dee1b1ffa9745b9ab10a1337941b35
Colum Ferry <cferry09@gmail.com>
2021-12-15 09:44:58 +0000

    feat(angular): upgrade to angular 13.1

--- packages/linter/src/utils/versions.ts
+++ packages/linter/src/utils/versions.ts
@@ -2,7 +2,7 @@ export const nxVersion = '*';
 
 export const tslintVersion = '~6.1.0';
 export const tslintToEslintConfigVersion = '^2.4.0';
-export const buildAngularVersion = '~13.0.0';
+export const buildAngularVersion = '~13.1.0';
 
 export const typescriptESLintVersion = '~5.3.0';
 export const eslintVersion = '8.2.0';
