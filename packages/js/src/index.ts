9ef3798c5e6ccee58193439cfab401f6b8aa1bf9
jaybell <jaycooperbell@gmail.com>
2021-12-21 16:53:52 -0800

    feat(js): export lib and app generator functions

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -1,2 +1,4 @@
 export * from './utils/typescript/print-diagnostics';
 export * from './utils/typescript/run-type-check';
+export { libraryGenerator } from './generators/library/library';
+export { applicationGenerator } from './generators/application/application';
