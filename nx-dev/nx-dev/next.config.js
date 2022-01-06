54a5eb989df46d9768d4bf1cd060cd7160bfd0c8
Benjamin Cabanes <3447705+bcabanes@users.noreply.github.com>
2022-01-06 14:20:11 -0500

    docs(nxdev): ci-overview redirect rule (#8412)

--- nx-dev/nx-dev/next.config.js
+++ nx-dev/nx-dev/next.config.js
@@ -5,12 +5,12 @@ const redirects = {
   '/core-concepts/configuration': '/configuration/projectjson',
   '/core-concepts/mental-model': '/using-nx/mental-model',
   '/core-concepts/updating-nx': '/using-nx/updating-nx',
+  '/core-concepts/ci-overview': '/using-nx/ci-overview',
   '/using-nx/nx-devkit': '/getting-started/nx-devkit',
   '/getting-started/nx-cli': '/using-nx/nx-cli',
   '/getting-started/console': '/using-nx/console',
   '/core-extended/affected': '/using-nx/affected',
   '/core-extended/computation-caching': '/using-nx/caching',
-  '/using-nx/ci-overview': '/using-nx/ci-overview',
 };
 
 module.exports = withNx({
