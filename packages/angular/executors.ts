a7a7c55ff55c913d0c1ce707880a92a0ab5427a7
Jordan <kkoo95@gmail.com>
2022-01-13 13:19:46 +0100

    fix(angular): expose executors sources (#8368)
    * fix(angular): expose executors sources
    
    when having custom builder relying on yours, explicit access to executors directory is now required with ng13. Otherwise calls such as `require("@nrwl/angular/src/executors/package/package.impl");` do not work anymore
    
    * feat(angular): expose only executor functions
    
    Co-authored-by: Colum Ferry <cferry09@gmail.com>

---
+++ packages/angular/executors.ts
@@ -0,0 +1,3 @@
+export * from './src/executors/delegate-build/delegate-build.impl';
+export * from './src/executors/ng-packagr-lite/ng-packagr-lite.impl';
+export * from './src/executors/package/package.impl';
