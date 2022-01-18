ead308cb47fdf64130a88887048c1c9d68928aca
Phillip Barta <barta.phillip@gmail.com>
2022-01-18 16:52:58 +0100

    feat(core): migrate to prettier ^2.5.1

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -2,7 +2,7 @@ export const nxVersion = '*';
 
 export const angularCliVersion = '~13.1.0';
 export const typescriptVersion = '~4.4.3';
-export const prettierVersion = '^2.3.1';
+export const prettierVersion = '^2.5.1';
 export const tslintVersion = '~6.1.0';
 export const typescriptESLintVersion = '~5.3.0';
 export const eslintVersion = '8.2.0';
