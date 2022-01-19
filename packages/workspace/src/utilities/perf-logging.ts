3ed436b52cc13a9772b30a37a472f5da0c497825
Victor Savkin <mail@vsavkin.com>
2022-01-19 10:13:42 -0500

    fix(core): fix perf logging

--- packages/workspace/src/utilities/perf-logging.ts
+++ packages/workspace/src/utilities/perf-logging.ts
@@ -2,8 +2,9 @@ import { PerformanceObserver } from 'perf_hooks';
 
 if (process.env.NX_PERF_LOGGING) {
   const obs = new PerformanceObserver((list) => {
-    const entry = list.getEntries()[0];
-    console.log(`Time for '${entry.name}'`, entry.duration);
+    for (const entry of list.getEntries()) {
+      console.log(`Time for '${entry.name}'`, entry.duration);
+    }
   });
   obs.observe({ entryTypes: ['measure'] });
 }
