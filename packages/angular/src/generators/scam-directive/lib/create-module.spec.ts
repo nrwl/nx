ac66fc1a17b6e37490fdd60c3523732ed7e85330
Colum Ferry <cferry09@gmail.com>
2022-01-24 15:24:29 +0000

    feat(angular): add --export flag to scam-directive (#8673)

--- packages/angular/src/generators/scam-directive/lib/create-module.spec.ts
+++ packages/angular/src/generators/scam-directive/lib/create-module.spec.ts
@@ -1,4 +1,4 @@
-import { addProjectConfiguration } from '@nrwl/devkit';
+import { addProjectConfiguration, logger } from '@nrwl/devkit';
 import { wrapAngularDevkitSchematic } from '@nrwl/devkit/ngcli-adapter';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 import { createScamDirective } from './create-module';
@@ -59,6 +59,106 @@ describe('Create module in the tree', () => {
       })
       export class ExampleDirectiveModule {}"
     `);
+  });
+
+  it('should create the scam directive and export it correctly', async () => {
+    // ARRANGE
+    const tree = createTreeWithEmptyWorkspace(2);
+    addProjectConfiguration(tree, 'lib1', {
+      projectType: 'library',
+      sourceRoot: 'lib1/lib1/src',
+      root: 'lib1/lib1',
+    });
+
+    const angularDirectiveSchematic = wrapAngularDevkitSchematic(
+      '@schematics/angular',
+      'directive'
+    );
+    await angularDirectiveSchematic(tree, {
+      name: 'example',
+      project: 'lib1',
+      skipImport: true,
+      export: false,
+      flat: false,
+    });
+
+    // ACT
+    createScamDirective(tree, {
+      name: 'example',
+      project: 'lib1',
+      inlineScam: true,
+      flat: false,
+      export: true,
+    });
+
+    // ASSERT
+    const directiveSource = tree.read(
+      'lib1/lib1/src/lib/example/example.directive.ts',
+      'utf-8'
+    );
+
+    expect(directiveSource).toMatchInlineSnapshot(`
+      "import { Directive, NgModule } from '@angular/core';
+      import { CommonModule } from '@angular/common';
+
+      @Directive({
+        selector: '[example]'
+      })
+      export class ExampleDirective {
+
+        constructor() { }
+
+      }
+
+      @NgModule({
+        imports: [CommonModule],
+        declarations: [ExampleDirective],
+        exports: [ExampleDirective],
+      })
+      export class ExampleDirectiveModule {}"
+    `);
+
+    const entryPointSource = tree.read('lib1/lib1/src/index.ts', 'utf-8');
+
+    expect(entryPointSource).toMatchInlineSnapshot(`null`);
+  });
+
+  it('should warn if export=true and project is application', async () => {
+    // ARRANGE
+    const tree = createTreeWithEmptyWorkspace(2);
+    addProjectConfiguration(tree, 'app1', {
+      projectType: 'application',
+      sourceRoot: 'apps/app1/src',
+      root: 'apps/app1',
+    });
+
+    const angularDirectiveSchematic = wrapAngularDevkitSchematic(
+      '@schematics/angular',
+      'directive'
+    );
+    await angularDirectiveSchematic(tree, {
+      name: 'example',
+      project: 'app1',
+      skipImport: true,
+      export: false,
+      flat: false,
+    });
+
+    const mockLoggerWarn = jest.spyOn(logger, 'warn');
+
+    // ACT
+    createScamDirective(tree, {
+      name: 'example',
+      project: 'app1',
+      inlineScam: true,
+      flat: false,
+      export: true,
+    });
+
+    // ASSERT
+    expect(mockLoggerWarn).toHaveBeenCalledWith(
+      '--export=true was ignored as the project the SCAM is being generated in is not a library.'
+    );
   });
 
   it('should create the scam directive separately correctly', async () => {
