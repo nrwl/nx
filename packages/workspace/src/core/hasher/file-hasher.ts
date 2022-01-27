86a1a6c8259451c4d1d66c482522e8cbc9d82952
Victor Savkin <vic.savkin@gmail.com>
2022-01-27 11:13:07 -0500

    fix(core): better detection whether the workspace uses git (#8720)

--- packages/workspace/src/core/hasher/file-hasher.ts
+++ packages/workspace/src/core/hasher/file-hasher.ts
@@ -4,13 +4,24 @@ import { appRootPath } from '@nrwl/tao/src/utils/app-root';
 import { NodeBasedFileHasher } from './node-based-file-hasher';
 import { FileHasherBase } from './file-hasher-base';
 import { statSync } from 'fs';
+import { execSync } from 'child_process';
 
 function createFileHasher(): FileHasherBase {
+  // special case for unit tests
+  if (appRootPath === '/root') {
+    return new NodeBasedFileHasher();
+  }
   try {
+    // checking the folder first, cause it is faster
     statSync(joinPathFragments(appRootPath, '.git')).isDirectory();
     return new GitBasedFileHasher();
   } catch (err) {
-    return new NodeBasedFileHasher();
+    try {
+      execSync('git rev-parse --is-inside-work-tree', { stdio: 'ignore' });
+      return new GitBasedFileHasher();
+    } catch {
+      return new NodeBasedFileHasher();
+    }
   }
 }
 
