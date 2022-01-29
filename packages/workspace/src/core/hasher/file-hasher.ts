5e3f1d47bfa725e19a6d06fe7c049dd41c41a077
Victor Savkin <mail@vsavkin.com>
2022-01-28 21:04:19 -0500

    chore(repo): update repo to 13.7.1

--- packages/workspace/src/core/hasher/file-hasher.ts
+++ packages/workspace/src/core/hasher/file-hasher.ts
@@ -1,16 +1,16 @@
-import { GitBasedFileHasher } from "./git-based-file-hasher";
-import { appRootPath } from "@nrwl/tao/src/utils/app-root";
-import { NodeBasedFileHasher } from "./node-based-file-hasher";
-import { FileHasherBase } from "./file-hasher-base";
-import { execSync } from "child_process";
+import { GitBasedFileHasher } from './git-based-file-hasher';
+import { appRootPath } from '@nrwl/tao/src/utils/app-root';
+import { NodeBasedFileHasher } from './node-based-file-hasher';
+import { FileHasherBase } from './file-hasher-base';
+import { execSync } from 'child_process';
 
 function createFileHasher(): FileHasherBase {
   // special case for unit tests
-  if (appRootPath === "/root") {
+  if (appRootPath === '/root') {
     return new NodeBasedFileHasher();
   }
   try {
-    execSync("git rev-parse --is-inside-work-tree", { stdio: "ignore" });
+    execSync('git rev-parse --is-inside-work-tree', { stdio: 'ignore' });
     return new GitBasedFileHasher();
   } catch {
     return new NodeBasedFileHasher();
