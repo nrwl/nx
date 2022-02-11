e5804afbc19e86af5457e5c20e63acda22f6c3c1
Benjamin Cabanes <3447705+bcabanes@users.noreply.github.com>
2022-02-11 10:07:54 -0500

    docs(nxdev): reskin community page (#8927)

--- nx-dev/ui-common/src/lib/plugin-card.tsx
+++ nx-dev/ui-common/src/lib/plugin-card.tsx
@@ -1,6 +1,3 @@
-import React, { useEffect, useState } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-
 /* eslint-disable-next-line */
 export interface PluginCardProps {
   name: string;
@@ -9,28 +6,10 @@ export interface PluginCardProps {
 }
 
 export function PluginCard({ name, description, url }: PluginCardProps) {
-  const [copied, setCopied] = useState(false);
-  useEffect(() => {
-    let t: NodeJS.Timeout;
-    if (copied) {
-      t = setTimeout(() => {
-        setCopied(false);
-      }, 3000);
-    }
-    return () => {
-      t && clearTimeout(t);
-    };
-  }, [copied]);
   return (
-    <div className="flex w-full flex-col rounded-md border border-gray-100 py-8 px-6">
-      <h3 className="mb-4 text-lg font-semibold leading-tight">
-        <a
-          href={url}
-          target="_blank"
-          rel="nofollow"
-          title="Github repository"
-          className="flex items-center"
-        >
+    <div className="focus-within:ring-blue-nx-base relative flex w-full overflow-hidden rounded-lg border border-gray-300 bg-white shadow-sm transition focus-within:ring-2 focus-within:ring-offset-2 hover:bg-gray-50">
+      <div className="flex w-full flex-col px-4 py-3">
+        <h3 className="mb-4 flex text-lg font-semibold leading-tight">
           <svg
             className="mr-3 h-5 w-5 text-gray-800"
             role="img"
@@ -41,9 +20,18 @@ export function PluginCard({ name, description, url }: PluginCardProps) {
             <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
           </svg>{' '}
           {name}
+        </h3>
+        <a
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+          title="Github repository"
+          className="focus:outline-none"
+        >
+          <span className="absolute inset-0" aria-hidden="true"></span>
+          <p className="mb-6 sm:text-sm">{description}</p>
         </a>
-      </h3>
-      <p className="mb-6 sm:text-lg">{description}</p>
+      </div>
     </div>
   );
 }
---
+++ nx-dev/ui-community/src/lib/plugin-directory.tsx
@@ -0,0 +1,64 @@
+import { ReactComponentElement } from 'react';
+import { useState } from 'react';
+import { PluginCard } from '@nrwl/nx-dev/ui-common';
+import { SearchIcon } from '@heroicons/react/solid';
+
+interface Plugin {
+  description: string;
+  name: string;
+  url: string;
+}
+
+export function PluginDirectory({
+  pluginList,
+}: {
+  pluginList: Plugin[];
+}): ReactComponentElement<any> {
+  const [searchTerm, setSearchTerm] = useState('');
+  return (
+    <div
+      id="plugin-directory"
+      className="max-w-screen mx-auto p-4 lg:max-w-7xl"
+    >
+      <div className="flex w-full flex-col justify-between gap-8 md:flex-row ">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+          Nx Plugin Directory
+        </h2>
+        <div>
+          <label htmlFor="search" className="sr-only">
+            Quick search
+          </label>
+          <div className="relative">
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+              <SearchIcon className="h-5 w-5 text-gray-400" />
+            </div>
+            <input
+              id="search"
+              name="search"
+              className="focus:border-blue-nx-base focus:ring-blue-nx-base block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:text-gray-900 focus:placeholder-gray-400 focus:outline-none focus:ring-1 sm:text-sm"
+              placeholder="Quick search"
+              onChange={(event) => setSearchTerm(event.target.value)}
+              type="search"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="my-12 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+        {pluginList
+          .filter((plugin) =>
+            !!searchTerm ? plugin.name.includes(searchTerm) : true
+          )
+          .map((plugin) => (
+            <PluginCard
+              key={plugin.name}
+              name={plugin.name}
+              description={plugin.description}
+              url={plugin.url}
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default PluginDirectory;
