ccb45605bcc6fd576b3bf1adeb2013e36bcf9ab3
ben <3447705+bcabanes@users.noreply.github.com>
2022-02-10 19:00:14 -0500

    docs(nxdev): reskin community page

---
+++ nx-dev/ui-community/README.md
@@ -0,0 +1,7 @@
+# nx-dev-ui-community
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test nx-dev-ui-community` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ nx-dev/ui-community/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'nx-dev-ui-community',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/nx-dev/ui-community',
+};
