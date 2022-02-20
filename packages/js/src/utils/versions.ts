af04709f0b261b8d15ea9f2cf53677331b09e1d5
Chau Tran <ctran@Chaus-MacBook-Pro.local>
2022-02-20 09:17:00 -0600

    fix(js): bump swc deps versions

--- packages/js/src/utils/versions.ts
+++ packages/js/src/utils/versions.ts
@@ -1,7 +1,5 @@
 export const nxVersion = '*';
 
-export const swcCoreVersion = '1.2.118';
-
-export const swcCliVersion = '~0.1.52';
-
-export const swcHelpersVersion = '~0.2.14';
+export const swcCoreVersion = '~1.2.143';
+export const swcCliVersion = '~0.1.55';
+export const swcHelpersVersion = '~0.3.3';
