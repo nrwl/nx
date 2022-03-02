3a8349438ff8fb1922d60e90e3f49e75d76a7922
wscats <enoyao@tencent.com>
2022-03-02 15:13:50 +0800

    feat(core): feat(core): feat(nx): add color option

--- packages/workspace/src/executors/run-script/run-script.impl.ts
+++ packages/workspace/src/executors/run-script/run-script.impl.ts
@@ -2,9 +2,11 @@ import { execSync } from 'child_process';
 import { getPackageManagerCommand } from '@nrwl/devkit';
 import type { ExecutorContext } from '@nrwl/devkit';
 import * as path from 'path';
+import { env as appendLocalEnv } from 'npm-run-path';
 
 export interface RunScriptOptions {
   script: string;
+  color?: boolean;
 }
 
 export default async function (
@@ -13,7 +15,9 @@ export default async function (
 ) {
   const pm = getPackageManagerCommand();
   const script = options.script;
+  const color = options.color;
   delete options.script;
+  delete options.color;
 
   const args = [];
   Object.keys(options).forEach((r) => {
@@ -22,6 +26,7 @@ export default async function (
 
   try {
     execSync(pm.run(script, args.join(' ')), {
+      env: processEnv(color),
       stdio: ['inherit', 'inherit', 'inherit'],
       cwd: path.join(
         context.root,
@@ -32,4 +37,16 @@ export default async function (
   } catch (e) {
     return { success: false };
   }
+}
+
+function processEnv(color: boolean) {
+  const env = {
+    ...process.env,
+    ...appendLocalEnv(),
+  };
+
+  if (color) {
+    env.FORCE_COLOR = `${color}`;
+  }
+  return env;
 }
