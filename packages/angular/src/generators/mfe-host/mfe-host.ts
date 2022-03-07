538c4de702cd9f8966135eab6007933eb50e958c
Colum Ferry <cferry09@gmail.com>
2022-03-07 10:13:43 +0000

    feat(angular): add mfe-host generator

---
+++ packages/angular/src/generators/mfe-host/mfe-host.ts
@@ -0,0 +1,30 @@
+import type { Tree } from '@nrwl/devkit';
+import type { Schema } from './schema';
+
+import { getProjects } from '@nrwl/devkit';
+import applicationGenerator from '../application/application';
+
+export default async function mfeHost(tree: Tree, options: Schema) {
+  const projects = getProjects(tree);
+
+  if (options.remotes && options.remotes.length > 0) {
+    options.remotes.forEach((remote) => {
+      if (!projects.has(remote)) {
+        throw new Error(
+          `Could not find specified remote application (${remote})`
+        );
+      }
+    });
+  }
+
+  const installTask = await applicationGenerator(tree, {
+    name: options.name,
+    mfe: true,
+    mfeType: 'host',
+    routing: true,
+    remotes: options.remotes ?? [],
+    port: 4200,
+  });
+
+  return installTask;
+}
---
+++ packages/angular/src/generators/mfe-host/schema.d.ts
@@ -0,0 +1,4 @@
+export interface Schema {
+  name: string;
+  remotes?: string[];
+}
