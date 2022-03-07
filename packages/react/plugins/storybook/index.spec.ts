a2c5010678312e904e0782c38b4ff76a30473284
Miroslav Jonaš <meeroslav@users.noreply.github.com>
2022-03-08 00:22:13 +0100

    chore(react): fix storybook test failing on local (#9224)

--- packages/react/plugins/storybook/index.spec.ts
+++ packages/react/plugins/storybook/index.spec.ts
@@ -1,6 +1,6 @@
 import { webpack } from './index';
 import { join } from 'path';
-import { readJsonFile, writeJsonFile } from '@nrwl/tao/src/utils/fileutils';
+import { writeJsonFile } from '@nrwl/tao/src/utils/fileutils';
 import { appRootPath } from '@nrwl/tao/src/utils/app-root';
 jest.mock('@nrwl/web/src/utils/web.config', () => {
   return {
@@ -10,10 +10,7 @@ jest.mock('@nrwl/web/src/utils/web.config', () => {
 
 describe('Storybook webpack config', () => {
   it('should skip type checking', async () => {
-    // package.json does not exist in appRootPath when running tests in CI
-    if (process.env.CI) {
-      writeJsonFile(join(appRootPath, 'package.json'), {});
-    }
+    writeJsonFile(join(appRootPath, 'package.json'), {});
 
     const config = await webpack(
       {
