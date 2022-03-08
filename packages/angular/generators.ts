744203057d3fc9d0b75901b9fe0a1308491ca35e
Colum Ferry <cferry09@gmail.com>
2022-03-08 13:30:24 +0000

    feat(angular): add mfe-host generator (#9218)

--- packages/angular/generators.ts
+++ packages/angular/generators.ts
@@ -15,6 +15,7 @@ export * from './src/generators/storybook-migrate-defaults-5-to-6/storybook-migr
 export * from './src/generators/storybook-migrate-stories-to-6-2/migrate-stories-to-6-2';
 export * from './src/generators/upgrade-module/upgrade-module';
 export * from './src/generators/setup-mfe/setup-mfe';
+export * from './src/generators/mfe-host/mfe-host';
 export * from './src/generators/scam/scam';
 export * from './src/generators/scam-directive/scam-directive';
 export * from './src/generators/scam-pipe/scam-pipe';
