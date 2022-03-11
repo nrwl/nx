e650ecce8648f7b87b32af5e0936c26977b82cce
Jack Hsu <jack.hsu@gmail.com>
2022-03-11 10:19:29 -0500

    feat(react): update packages

--- packages/react/src/utils/versions.ts
+++ packages/react/src/utils/versions.ts
@@ -3,32 +3,32 @@ export const nxVersion = '*';
 export const reactVersion = '17.0.2';
 export const reactDomVersion = '17.0.2';
 export const reactIsVersion = '17.0.2';
-export const typesReactVersion = '17.0.30';
-export const typesReactDomVersion = '17.0.9';
-export const typesReactIsVersion = '17.0.2';
+export const typesReactVersion = '17.0.40';
+export const typesReactDomVersion = '17.0.13';
+export const typesReactIsVersion = '17.0.3';
 
 export const styledComponentsVersion = '5.3.3';
-export const typesStyledComponentsVersion = '5.1.15';
+export const typesStyledComponentsVersion = '5.1.24';
 
-export const emotionStyledVersion = '11.3.0';
-export const emotionReactVersion = '11.5.0';
-export const emotionBabelPlugin = '11.3.0';
+export const emotionStyledVersion = '11.8.1';
+export const emotionReactVersion = '11.8.2';
+export const emotionBabelPlugin = '11.7.2';
 
-export const styledJsxVersion = '4.0.1';
+export const styledJsxVersion = '5.0.0';
 
 export const reactRouterDomVersion = '5.3.0';
-export const typesReactRouterDomVersion = '5.3.1';
+export const typesReactRouterDomVersion = '5.3.3';
 
-export const testingLibraryReactVersion = '12.1.2';
+export const testingLibraryReactVersion = '12.1.4';
 export const testingLibraryReactHooksVersion = '7.0.2';
 
-export const reduxjsToolkitVersion = '1.6.2';
-export const reactReduxVersion = '7.2.5';
+export const reduxjsToolkitVersion = '1.8.0';
+export const reactReduxVersion = '7.2.6';
 export const reactTestRendererVersion = '17.0.2';
 
-export const eslintPluginImportVersion = '2.25.2';
+export const eslintPluginImportVersion = '2.25.4';
 export const eslintPluginJsxA11yVersion = '6.5.1';
-export const eslintPluginReactVersion = '7.28.0';
+export const eslintPluginReactVersion = '7.29.3';
 export const eslintPluginReactHooksVersion = '4.3.0';
 
 export const babelPluginStyledComponentsVersion = '1.10.7';
