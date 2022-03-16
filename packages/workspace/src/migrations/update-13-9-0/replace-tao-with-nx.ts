f99e1d33b70de23f098e7be959feced54f0dc058
Jason Jean <jasonjean1993@gmail.com>
2022-03-16 17:24:36 -0400

    fix(core): undo removal of @nrwl/cli and add warning when it is imported (#9370)

--- packages/workspace/src/migrations/update-13-9-0/replace-tao-with-nx.ts
+++ packages/workspace/src/migrations/update-13-9-0/replace-tao-with-nx.ts
@@ -1,22 +1,21 @@
 import { Tree, updateJson } from '@nrwl/devkit';
 
-export function replaceTaoAndCLIWithNx(host: Tree) {
+export function replaceTaoWithNx(host: Tree) {
   updateJson(host, 'package.json', (json: any) => {
     if (json.dependencies['@nrwl/workspace']) {
       json.dependencies['nx'] = json.dependencies['@nrwl/workspace'];
     } else if (json.devDependencies['@nrwl/workspace']) {
       json.devDependencies['nx'] = json.devDependencies['@nrwl/workspace'];
     }
-    removeTaoAndCLI(json.dependencies);
-    removeTaoAndCLI(json.devDependencies);
+    removeTao(json.dependencies);
+    removeTao(json.devDependencies);
     return json;
   });
 }
 
-function removeTaoAndCLI(json: any) {
+function removeTao(json: any) {
   if (!json) return;
   json['@nrwl/tao'] = undefined;
-  json['@nrwl/cli'] = undefined;
 }
 
-export default replaceTaoAndCLIWithNx;
+export default replaceTaoWithNx;
