025ed95514d5b52ebba4fa1ac0a750b1fb78b6d4
Craigory Coppola <craigorycoppola@gmail.com>
2022-03-18 13:37:44 -0400

    chore(misc): update add-nx ci

--- e2e/add-nx-to-monorepo/src/add-nx-to-monorepo.test.ts
+++ e2e/add-nx-to-monorepo/src/add-nx-to-monorepo.test.ts
@@ -4,52 +4,66 @@ import {
   runCommand,
   tmpProjPath,
   updateFile,
+  getPackageManagerCommand,
+  getSelectedPackageManager,
 } from '@nrwl/e2e/utils';
 import { Workspaces } from 'nx/src/shared/workspace';
 
-describe.each(['npx', 'pnpx --yes'])('%s add-nx-to-monorepo', (command) => {
-  it('should not throw', () => {
-    // Arrange
-    createNonNxProjectDirectory();
-    updateFile(
-      'packages/package-a/package.json',
-      JSON.stringify({
-        name: 'package-a',
-      })
-    );
-    updateFile(
-      'packages/package-b/package.json',
-      JSON.stringify({
-        name: 'package-b',
-      })
-    );
+describe('add-nx-to-monorepo', () => {
+  const packageManagerCommand = getPackageManagerCommand({
+    packageManager: getSelectedPackageManager(),
+  }).runUninstalledPackage;
 
-    // Act
-    const output = runCommand(`${command} add-nx-to-monorepo --nx-cloud false`);
-    // Assert
-    expect(output).toContain('🎉 Done!');
-    expect(readWorkspaceConfig().projects['package-a']).toBeTruthy();
-    expect(readWorkspaceConfig().projects['package-b']).toBeTruthy();
+  it('should not throw', () => {
+    if (packageManagerCommand) {
+      // Arrange
+      createNonNxProjectDirectory();
+      updateFile(
+        'packages/package-a/package.json',
+        JSON.stringify({
+          name: 'package-a',
+        })
+      );
+      updateFile(
+        'packages/package-b/package.json',
+        JSON.stringify({
+          name: 'package-b',
+        })
+      );
+
+      // Act
+      const output = runCommand(
+        `${packageManagerCommand} add-nx-to-monorepo --nx-cloud false`
+      );
+      // Assert
+      expect(output).toContain('🎉 Done!');
+      expect(readWorkspaceConfig().projects['package-a']).toBeTruthy();
+      expect(readWorkspaceConfig().projects['package-b']).toBeTruthy();
+    }
   });
 
   it('should build', () => {
-    // Arrange
-    createNonNxProjectDirectory();
-    updateFile(
-      'packages/package-a/package.json',
-      JSON.stringify({
-        name: 'package-a',
-        scripts: {
-          build: 'echo "build successful"',
-        },
-      })
-    );
+    if (packageManagerCommand) {
+      // Arrange
+      createNonNxProjectDirectory();
+      updateFile(
+        'packages/package-a/package.json',
+        JSON.stringify({
+          name: 'package-a',
+          scripts: {
+            build: 'echo "build successful"',
+          },
+        })
+      );
 
-    // Act
-    runCommand(`${command} add-nx-to-monorepo --nx-cloud false`);
-    const output = runCLI('build package-a');
-    // Assert
-    expect(output).toContain('build successful');
+      // Act
+      runCommand(
+        `${packageManagerCommand} add-nx-to-monorepo --nx-cloud false`
+      );
+      const output = runCLI('build package-a');
+      // Assert
+      expect(output).toContain('build successful');
+    }
   });
 });
 
