b722fe082a7938f2c510e74c5af24384c469f391
ben <3447705+bcabanes@users.noreply.github.com>
2022-03-22 12:55:51 -0400

    docs(nxdev): remove unused versions & flavors feature

--- nx-dev/feature-versions-and-flavors/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'nx-dev-feature-versions-and-flavors',
-  preset: '../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage//nx-dev/feature-versions-and-flavors',
-};
