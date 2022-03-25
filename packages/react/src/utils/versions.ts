7b17020fb89e6e5a15a6a97297b318f8b60a196e
Jack Hsu <jack.hsu@gmail.com>
2022-03-25 15:57:56 -0400

    feat(react): update packages

--- packages/react/src/utils/versions.ts
+++ packages/react/src/utils/versions.ts
@@ -3,18 +3,18 @@ export const nxVersion = '*';
 export const reactVersion = '17.0.2';
 export const reactDomVersion = '17.0.2';
 export const reactIsVersion = '17.0.2';
-export const typesReactVersion = '17.0.40';
-export const typesReactDomVersion = '17.0.13';
+export const typesReactVersion = '17.0.43';
+export const typesReactDomVersion = '17.0.14';
 export const typesReactIsVersion = '17.0.3';
 
-export const styledComponentsVersion = '5.3.3';
+export const styledComponentsVersion = '5.3.5';
 export const typesStyledComponentsVersion = '5.1.24';
 
 export const emotionStyledVersion = '11.8.1';
 export const emotionReactVersion = '11.8.2';
 export const emotionBabelPlugin = '11.7.2';
 
-export const styledJsxVersion = '5.0.0';
+export const styledJsxVersion = '5.0.1';
 
 export const reactRouterDomVersion = '5.3.0';
 export const typesReactRouterDomVersion = '5.3.3';
@@ -28,7 +28,7 @@ export const reactTestRendererVersion = '17.0.2';
 
 export const eslintPluginImportVersion = '2.25.4';
 export const eslintPluginJsxA11yVersion = '6.5.1';
-export const eslintPluginReactVersion = '7.29.3';
+export const eslintPluginReactVersion = '7.29.4';
 export const eslintPluginReactHooksVersion = '4.3.0';
 
 export const babelPluginStyledComponentsVersion = '1.10.7';
