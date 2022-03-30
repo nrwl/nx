8888274080c6a328ad0eb3e6cb0156cd965d8f97
Jack Hsu <jack.hsu@gmail.com>
2022-03-30 09:12:25 -0400

    feat(react): update to react 18

--- packages/react-native/src/generators/init/init.ts
+++ packages/react-native/src/generators/init/init.ts
@@ -4,6 +4,8 @@ import {
   convertNxGenerator,
   detectPackageManager,
   formatFiles,
+  logger,
+  readJson,
   removeDependenciesFromPackageJson,
   Tree,
 } from '@nrwl/devkit';
@@ -11,10 +13,7 @@ import { Schema } from './schema';
 import { runTasksInSerial } from '@nrwl/workspace/src/utilities/run-tasks-in-serial';
 import { jestInitGenerator } from '@nrwl/jest';
 import { detoxInitGenerator } from '@nrwl/detox';
-import {
-  reactVersion,
-  typesReactVersion,
-} from '@nrwl/react/src/utils/versions';
+import { typesReactVersion } from '@nrwl/react/src/utils/versions';
 
 import {
   babelRuntimeVersion,
@@ -31,6 +30,7 @@ import {
   reactNativeSvgVersion,
   reactNativeVersion,
   reactTestRendererVersion,
+  reactVersion,
   testingLibraryJestNativeVersion,
   testingLibraryReactNativeVersion,
   typesReactNativeVersion,
@@ -65,6 +65,13 @@ export async function reactNativeInitGenerator(host: Tree, schema: Schema) {
 
 export function updateDependencies(host: Tree) {
   const isPnpm = detectPackageManager(host.root) === 'pnpm';
+  const { dependencies = {} } = readJson(host, 'package.json');
+  // TODO(jack): Remove this once React Native 0.68.0 is out.
+  if (dependencies['react']?.match(/[\^~]?18/)) {
+    logger.warn(
+      `React version ${dependencies['react']} is incompatible with React Native version ${reactNativeVersion}. Nx will downgrade the version to ${reactVersion}.`
+    );
+  }
   return addDependenciesToPackageJson(
     host,
     {
--- packages/react-native/src/utils/versions.ts
+++ packages/react-native/src/utils/versions.ts
@@ -1,9 +1,14 @@
 export const nxVersion = '*';
 
-export const reactNativeVersion = '0.67.3';
+export const reactNativeVersion = '0.67.4';
 export const typesReactNativeVersion = '0.67.3';
 
-export const metroVersion = '0.69.1';
+export const metroVersion = '0.70.0';
+
+// TODO(jack): Remove this once react-native 0.68.0 is released.
+// v18 is only supported on the New Architecture
+// See: https://reactnative.dev/blog/2022/03/15/an-update-on-the-new-architecture-rollout
+export const reactVersion = '17.0.2';
 
 export const reactNativeCommunityCli = '7.0.1';
 export const reactNativeCommunityCliIos = '7.0.1';
@@ -12,9 +17,9 @@ export const reactNativeCommunityCliAndroid = '7.0.1';
 export const reactNativeConfigVersion = '1.4.5';
 export const reactNativeAsyncStorageAsyncStorageVersion = '1.16.3';
 
-export const metroReactNativeBabelPresetVersion = '0.69.1';
+export const metroReactNativeBabelPresetVersion = '0.70.0';
 
-export const testingLibraryReactNativeVersion = '9.0.0';
+export const testingLibraryReactNativeVersion = '9.1.0';
 export const testingLibraryJestNativeVersion = '4.0.4';
 
 export const jestReactNativeVersion = '18.0.0';
@@ -24,4 +29,4 @@ export const reactTestRendererVersion = '17.0.2';
 export const reactNativeSvgTransformerVersion = '1.0.0';
 export const reactNativeSvgVersion = '12.3.0';
 
-export const babelRuntimeVersion = '7.17.7';
+export const babelRuntimeVersion = '7.17.8';
