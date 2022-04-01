028752b407795344541c74f2ddcc7ff2aea94491
Kalarrs Topham <k-dawg-klark@hotmail.com>
2022-04-01 15:48:31 -0700

    feat(node): add support for async webpack configuration (#9641)

--- packages/node/src/executors/webpack/webpack.impl.ts
+++ packages/node/src/executors/webpack/webpack.impl.ts
@@ -1,44 +1,44 @@
-import 'dotenv/config';
-import { ExecutorContext } from '@nrwl/devkit';
+import 'dotenv/config'
+import { ExecutorContext } from '@nrwl/devkit'
 
-import { readCachedProjectGraph } from '@nrwl/devkit';
+import { readCachedProjectGraph } from '@nrwl/devkit'
 import {
   calculateProjectDependencies,
   checkDependentProjectsHaveBeenBuilt,
   createTmpTsConfig,
-} from '@nrwl/workspace/src/utilities/buildable-libs-utils';
-import { getRootTsConfigPath } from '@nrwl/workspace/src/utilities/typescript';
+} from '@nrwl/workspace/src/utilities/buildable-libs-utils'
+import { getRootTsConfigPath } from '@nrwl/workspace/src/utilities/typescript'
 
-import { map, mergeMap, tap } from 'rxjs/operators';
-import { eachValueFrom } from 'rxjs-for-await';
-import { resolve } from 'path';
-import { register } from 'ts-node';
+import { map, mergeMap, tap } from 'rxjs/operators'
+import { eachValueFrom } from 'rxjs-for-await'
+import { resolve } from 'path'
+import { register } from 'ts-node'
 
-import { getNodeWebpackConfig } from '../../utils/node.config';
-import { BuildNodeBuilderOptions } from '../../utils/types';
-import { normalizeBuildOptions } from '../../utils/normalize';
-import { generatePackageJson } from '../../utils/generate-package-json';
-import { runWebpack } from '../../utils/run-webpack';
-import { from, of } from 'rxjs';
-import { Configuration } from 'webpack';
+import { getNodeWebpackConfig } from '../../utils/node.config'
+import { BuildNodeBuilderOptions } from '../../utils/types'
+import { normalizeBuildOptions } from '../../utils/normalize'
+import { generatePackageJson } from '../../utils/generate-package-json'
+import { runWebpack } from '../../utils/run-webpack'
+import { from, of } from 'rxjs'
+import { Configuration } from 'webpack'
 
 export type NodeBuildEvent = {
-  outfile: string;
-  success: boolean;
-};
+  outfile: string
+  success: boolean
+}
 
 export async function* webpackExecutor(
   rawOptions: BuildNodeBuilderOptions,
   context: ExecutorContext
 ) {
-  const { sourceRoot, root } = context.workspace.projects[context.projectName];
+  const { sourceRoot, root } = context.workspace.projects[context.projectName]
 
   if (!sourceRoot) {
-    throw new Error(`${context.projectName} does not have a sourceRoot.`);
+    throw new Error(`${context.projectName} does not have a sourceRoot.`)
   }
 
   if (!root) {
-    throw new Error(`${context.projectName} does not have a root.`);
+    throw new Error(`${context.projectName} does not have a root.`)
   }
 
   const options = normalizeBuildOptions(
@@ -46,13 +46,13 @@ export async function* webpackExecutor(
     context.root,
     sourceRoot,
     root
-  );
+  )
 
   if (options.webpackConfig.some((x) => x.endsWith('.ts'))) {
-    registerTsNode();
+    registerTsNode()
   }
 
-  const projGraph = readCachedProjectGraph();
+  const projGraph = readCachedProjectGraph()
   if (!options.buildLibsFromSource) {
     const { target, dependencies } = calculateProjectDependencies(
       projGraph,
@@ -60,13 +60,13 @@ export async function* webpackExecutor(
       context.projectName,
       context.targetName,
       context.configurationName
-    );
+    )
     options.tsConfig = createTmpTsConfig(
       options.tsConfig,
       context.root,
       target.data.root,
       dependencies
-    );
+    )
 
     if (
       !checkDependentProjectsHaveBeenBuilt(
@@ -76,31 +76,31 @@ export async function* webpackExecutor(
         dependencies
       )
     ) {
-      return { success: false } as any;
+      return { success: false } as any
     }
   }
 
   if (options.generatePackageJson) {
-    generatePackageJson(context.projectName, projGraph, options);
+    generatePackageJson(context.projectName, projGraph, options)
   }
-  const config: Configuration | Configuration[] = options.webpackConfig.reduce(
-    (currentConfig, plugin) => {
-      return require(plugin)(currentConfig, {
+  const config: Configuration | Configuration[] = await options.webpackConfig.reduce(
+    async (currentConfig, plugin) => {
+      return require(plugin)(await currentConfig, {
         options,
         configuration: context.configurationName,
-      });
+      })
     },
-    getNodeWebpackConfig(options)
-  );
+    Promise.resolve(getNodeWebpackConfig(options))
+  )
 
   return yield* eachValueFrom(
     (Array.isArray(config) ? from(config) : of(config)).pipe(
       mergeMap((conf) => {
         return runWebpack(conf).pipe(
           tap((stats) => {
-            console.info(stats.toString(conf.stats));
+            console.info(stats.toString(conf.stats))
           })
-        );
+        )
       }),
       map((stats) => {
         return {
@@ -110,17 +110,17 @@ export async function* webpackExecutor(
             options.outputPath,
             options.outputFileName
           ),
-        } as NodeBuildEvent;
+        } as NodeBuildEvent
       })
     )
-  );
+  )
 }
 
 function registerTsNode() {
-  const rootTsConfig = getRootTsConfigPath();
+  const rootTsConfig = getRootTsConfigPath()
   register({
     ...(rootTsConfig ? { project: rootTsConfig } : null),
-  });
+  })
 }
 
-export default webpackExecutor;
+export default webpackExecutor
