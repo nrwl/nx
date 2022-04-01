edc34e14f6a5a4ae2006328540083942f0c32b7a
Chau Tran <nartc7789@gmail.com>
2022-04-01 10:31:15 -0500

    feat(repo): use js:swc for nx repo

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -1,3 +1,4 @@
 export * from './utils/typescript/print-diagnostics';
 export * from './utils/typescript/run-type-check';
 export { libraryGenerator } from './generators/library/library';
+export { convertToSwcGenerator } from './generators/convert-to-swc/convert-to-swc';
