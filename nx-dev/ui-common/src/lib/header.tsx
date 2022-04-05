0efe5b16de6d77046c02fb969ab949ef4417981a
ben <3447705+bcabanes@users.noreply.github.com>
2022-04-05 16:41:48 -0400

    docs(nxdev): hide nxcloud button on smaller screen

--- nx-dev/ui-common/src/lib/header.tsx
+++ nx-dev/ui-common/src/lib/header.tsx
@@ -106,7 +106,7 @@ export function Header(props: HeaderProps) {
               href="https://nx.app/?utm_source=nx.dev"
               target="_blank"
               rel="noreferrer"
-              className="group relative px-3 py-2 leading-tight text-white lg:inline-flex"
+              className="group relative hidden px-3 py-2 leading-tight text-white lg:inline-flex"
             >
               Nx Cloud
             </a>
