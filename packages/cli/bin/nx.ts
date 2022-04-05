5491870a4b9e66f4fd06767210d9bfc3d6a30e30
Jason Jean <jasonjean1993@gmail.com>
2022-04-05 17:51:05 -0400

    fix(core): remove warning for out of date global version

--- packages/cli/bin/nx.ts
+++ packages/cli/bin/nx.ts
@@ -1,9 +1,3 @@
 #!/usr/bin/env node
 
-import { logger } from 'nx/src/utils/logger';
-import { getPackageManagerCommand } from 'nx/src/utils/package-manager';
-
-logger.warn('Please update your global install of Nx');
-logger.warn(`- ${getPackageManagerCommand().addGlobal} nx`);
-
 require('nx/bin/nx');
