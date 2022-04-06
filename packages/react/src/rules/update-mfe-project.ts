170a62686c06f67397a78d57a6150e34fa3e52ac
Jack Hsu <jack.hsu@gmail.com>
2022-04-05 20:42:57 -0400

    feat(react): update production setup for MFE host app

--- packages/react/src/rules/update-mfe-project.ts
+++ packages/react/src/rules/update-mfe-project.ts
@@ -1,16 +1,22 @@
 import { Tree } from 'nx/src/shared/tree';
-import { NormalizedSchema } from '../schema';
 import {
   readProjectConfiguration,
   updateProjectConfiguration,
 } from '@nrwl/devkit';
 
-export function updateMfeProject(host: Tree, options: NormalizedSchema) {
+export function updateMfeProject(
+  host: Tree,
+  options: { name: string; appProjectRoot: string; devServerPort?: number }
+) {
   let projectConfig = readProjectConfiguration(host, options.name);
   projectConfig.targets.build.options = {
     ...projectConfig.targets.build.options,
     main: `${options.appProjectRoot}/src/main.ts`,
     webpackConfig: `${options.appProjectRoot}/webpack.config.js`,
+  };
+  projectConfig.targets.build.configurations.production = {
+    ...projectConfig.targets.build.configurations.production,
+    webpackConfig: `${options.appProjectRoot}/webpack.config.prod.js`,
   };
   projectConfig.targets.serve.executor = '@nrwl/react:mfe-dev-server';
   projectConfig.targets.serve.options.port = options.devServerPort;
