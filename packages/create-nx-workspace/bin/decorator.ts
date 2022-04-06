b99a349d7fbc31532d225519d304b8b523ba6493
Miroslav Jonas <missing.manual@gmail.com>
2022-04-06 12:06:27 +0200

    feat(core): add colors to create-nx-workspace

--- packages/create-nx-workspace/bin/decorator.ts
+++ packages/create-nx-workspace/bin/decorator.ts
@@ -1,6 +1,14 @@
 import * as chalk from 'chalk';
 
 export const yargsDecorator = {
-  'Options:': `${chalk.bold('Options')}:`,
-  'Positionals:': `${chalk.bold('Positionals')}:`,
+  'Options:': `${chalk.green`Options`}:`,
+  'Examples:': `${chalk.green`Examples`}:`,
+  boolean: `${chalk.blue`boolean`}`,
+  count: `${chalk.blue`count`}`,
+  string: `${chalk.blue`string`}`,
+  array: `${chalk.blue`array`}`,
+  required: `${chalk.blue`required`}`,
+  'default:': `${chalk.blue`default`}:`,
+  'choices:': `${chalk.blue`choices`}:`,
+  'aliases:': `${chalk.blue`aliases`}:`,
 };
