7f773f3996738e200fcec8569fe7b00260b7b114
Jack Hsu <jack.hsu@gmail.com>
2022-04-09 10:12:16 -0400

    feat(react): update @types/react and other packages

--- packages/react/src/utils/versions.ts
+++ packages/react/src/utils/versions.ts
@@ -3,15 +3,15 @@ export const nxVersion = '*';
 export const reactVersion = '18.0.0';
 export const reactDomVersion = '18.0.0';
 export const reactIsVersion = '18.0.0';
-export const typesReactVersion = '17.0.43';
-export const typesReactDomVersion = '17.0.14';
+export const typesReactVersion = '18.0.0';
+export const typesReactDomVersion = '18.0.0';
 export const typesReactIsVersion = '17.0.3';
 
 export const styledComponentsVersion = '5.3.5';
-export const typesStyledComponentsVersion = '5.1.24';
+export const typesStyledComponentsVersion = '5.1.25';
 
 export const emotionStyledVersion = '11.8.1';
-export const emotionReactVersion = '11.8.2';
+export const emotionReactVersion = '11.9.0';
 export const emotionBabelPlugin = '11.7.2';
 
 export const styledJsxVersion = '5.0.2';
@@ -19,14 +19,14 @@ export const styledJsxVersion = '5.0.2';
 export const reactRouterDomVersion = '5.3.0';
 export const typesReactRouterDomVersion = '5.3.3';
 
-export const testingLibraryReactVersion = '12.1.4';
+export const testingLibraryReactVersion = '13.0.0';
 export const testingLibraryReactHooksVersion = '7.0.2';
 
-export const reduxjsToolkitVersion = '1.8.0';
-export const reactReduxVersion = '7.2.6';
+export const reduxjsToolkitVersion = '1.8.1';
+export const reactReduxVersion = '7.2.8';
 export const reactTestRendererVersion = '18.0.0';
 
-export const eslintPluginImportVersion = '2.25.4';
+export const eslintPluginImportVersion = '2.26.0';
 export const eslintPluginJsxA11yVersion = '6.5.1';
 export const eslintPluginReactVersion = '7.29.4';
 export const eslintPluginReactHooksVersion = '4.4.0';
