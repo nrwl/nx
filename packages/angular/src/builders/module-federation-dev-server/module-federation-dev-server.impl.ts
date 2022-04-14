34742b6e7f00d20dbdd895408fe76e5a8edda9bc
Colum Ferry <cferry09@gmail.com>
2022-04-14 11:12:08 +0100

    feat(angular): add module-federation-dev-server builder

---
+++ packages/angular/src/builders/module-federation-dev-server/module-federation-dev-server.impl.ts
@@ -0,0 +1,50 @@
+import type { Schema } from './schema';
+import { Workspaces } from '@nrwl/devkit';
+import { scheduleTarget } from 'nx/src/adapter/ngcli-adapter';
+import { BuilderContext, createBuilder } from '@angular-devkit/architect';
+import { JsonObject } from '@angular-devkit/core';
+import { join } from 'path';
+import { webpackServer } from '../webpack-server/webpack-server.impl';
+
+export function moduleFederationDevServer(
+  schema: Schema,
+  context: BuilderContext
+) {
+  const workspaces = new Workspaces(context.workspaceRoot);
+  const workspaceConfig = workspaces.readWorkspaceConfiguration();
+
+  const p = workspaceConfig.projects[context.target.project];
+
+  const mfeConfigPath = join(context.workspaceRoot, p.root, 'mfe.config.js');
+
+  let mfeConfig: { remotes: string[] };
+  try {
+    mfeConfig = require(mfeConfigPath);
+  } catch {
+    throw new Error(
+      `Could not load ${mfeConfigPath}. Was this project generated with "@nrwl/angular:host"?`
+    );
+  }
+
+  const { ...options } = schema;
+  const unparsedRemotes = mfeConfig.remotes.length > 0 ? mfeConfig.remotes : [];
+  const remotes = unparsedRemotes.map((a) => (Array.isArray(a) ? a[0] : a));
+
+  for (const remote of remotes) {
+    scheduleTarget(
+      context.workspaceRoot,
+      {
+        project: remote,
+        target: 'serve',
+        configuration: context.target.configuration,
+        runOptions: {},
+        executor: context.builder.builderName,
+      },
+      options.verbose
+    );
+  }
+
+  return webpackServer(options, context);
+}
+
+export default createBuilder<JsonObject & Schema>(moduleFederationDevServer);
