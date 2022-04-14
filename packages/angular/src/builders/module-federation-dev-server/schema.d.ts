4b846e8dc9a665ce37b48f58169701aa439779f0
Colum Ferry <cferry09@gmail.com>
2022-04-14 20:39:15 +0100

    feat(angular): add module-federation-dev-server builder (#9843)

---
+++ packages/angular/src/builders/module-federation-dev-server/schema.d.ts
@@ -0,0 +1,20 @@
+export interface Schema {
+  browserTarget: string;
+  port: number;
+  host: string;
+  proxyConfig?: string;
+  ssl: boolean;
+  sslKey?: string;
+  sslCert?: string;
+  headers?: Record<string, string>;
+  open: boolean;
+  verbose?: boolean;
+  liveReload: boolean;
+  publicHost?: string;
+  allowedHosts?: string[];
+  servePath?: string;
+  disableHostCheck?: boolean;
+  hmr?: boolean;
+  watch?: boolean;
+  poll?: number;
+}
