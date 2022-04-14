2b045e055b17b9637a1638bddd4bce7cce237d3f
Caleb Ukle <caleb@nrwl.io>
2022-04-14 09:50:09 -0500

    fix(core): allow parsing dot notation cli args (#9812)
    parse dot notation cli args for env args passed to cypress executor to override cypress.env.json
    values.
    i.e. nx e2e project --env.API_URL=localhost:1234
    will now correctly give cypress e2e args to
    override the cypress.env.json values
    
    ISSUES CLOSED: #9764

---
+++ packages/nx/src/command-line/nx-commands.spec.ts
@@ -0,0 +1,24 @@
+import { commandsObject } from 'nx/src/command-line/nx-commands';
+
+describe('nx-commands', () => {
+  it('should parse dot notion cli args', () => {
+    const actual = commandsObject.parse(
+      'nx e2e project-e2e --env.NX_API_URL=http://localhost:4200 --abc.123.xyx=false --a.b=3'
+    );
+    expect(actual).toEqual(
+      expect.objectContaining({
+        abc: {
+          '123': {
+            xyx: 'false',
+          },
+        },
+        a: {
+          b: 3,
+        },
+        env: {
+          NX_API_URL: 'http://localhost:4200',
+        },
+      })
+    );
+  });
+});
