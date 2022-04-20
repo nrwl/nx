991efef5e51187cd08dd8ac3bbd554c1477fa7cb
Colum Ferry <cferry09@gmail.com>
2022-04-20 22:49:26 +0100

    feat(angular): serve static for remotes (#9911)

---
+++ packages/angular/src/executors/file-server/schema.d.ts
@@ -0,0 +1,14 @@
+export interface Schema {
+  host: string;
+  port: number;
+  ssl: boolean;
+  sslKey?: string;
+  sslCert?: string;
+  proxyUrl?: string;
+  buildTarget: string;
+  parallel: boolean;
+  maxParallel?: number;
+  withDeps: boolean;
+  proxyOptions?: object;
+  watch?: boolean;
+}
