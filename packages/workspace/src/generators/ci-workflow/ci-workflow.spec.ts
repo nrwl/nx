ea9fbd545445526de0b7c8769357de581b283596
Miroslav Jonaš <meeroslav@users.noreply.github.com>
2022-04-21 00:26:59 +0200

    feat(core): throw error on ci generator if nx cloud not set (#9914)

--- packages/workspace/src/generators/ci-workflow/ci-workflow.spec.ts
+++ packages/workspace/src/generators/ci-workflow/ci-workflow.spec.ts
@@ -1,4 +1,4 @@
-import { Tree } from '@nrwl/devkit';
+import { NxJsonConfiguration, Tree, updateJson } from '@nrwl/devkit';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 import { ciWorkflowGenerator } from './ci-workflow';
 
@@ -10,24 +10,28 @@ describe('lib', () => {
   });
 
   it('should generate github CI config', async () => {
+    setNxCloud(tree);
     await ciWorkflowGenerator(tree, { ci: 'github' });
 
     expect(tree.read('.github/workflows/build.yml', 'utf-8')).toMatchSnapshot();
   });
 
   it('should generate circleci CI config', async () => {
+    setNxCloud(tree);
     await ciWorkflowGenerator(tree, { ci: 'circleci' });
 
     expect(tree.read('.circleci/config.yml', 'utf-8')).toMatchSnapshot();
   });
 
   it('should generate azure CI config', async () => {
+    setNxCloud(tree);
     await ciWorkflowGenerator(tree, { ci: 'azure' });
 
     expect(tree.read('azure-pipelines.yml', 'utf-8')).toMatchSnapshot();
   });
 
   it('should generate github CI config with custom name', async () => {
+    setNxCloud(tree);
     await ciWorkflowGenerator(tree, {
       ci: 'github',
       name: 'My custom-workflow',
@@ -37,4 +41,25 @@ describe('lib', () => {
       tree.read('.github/workflows/my-custom-workflow.yml', 'utf-8')
     ).toMatchSnapshot();
   });
+
+  it('should throw error is nx cloud is not set', async () => {
+    await expect(
+      ciWorkflowGenerator(tree, {
+        ci: 'github',
+      })
+    ).rejects.toThrowErrorMatchingSnapshot();
+  });
 });
+
+function setNxCloud(tree: Tree) {
+  updateJson<NxJsonConfiguration>(tree, 'nx.json', (json) => {
+    return {
+      ...json,
+      tasksRunnerOptions: {
+        default: {
+          runner: '@nrwl/nx-cloud',
+        },
+      },
+    };
+  });
+}
