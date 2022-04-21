d6323374ae8bc5a0eb1f3a04ea58d07e36c88cf2
Benjamin Cabanes <3447705+bcabanes@users.noreply.github.com>
2022-04-21 11:51:02 -0400

    docs(nxdev): fix mobile announcement banner alignment (#9930)

--- nx-dev/ui-common/src/lib/announcement-banner.tsx
+++ nx-dev/ui-common/src/lib/announcement-banner.tsx
@@ -4,7 +4,7 @@ export function AnnouncementBanner() {
   return (
     <div className="bg-slate-800">
       <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
-        <div className="pr-16 text-center sm:px-16">
+        <div className="text-center sm:px-16 sm:pr-16">
           <p className="text-sm font-medium text-white">
             <span className="md:hidden">
               <Link href="/conf">
@@ -17,7 +17,7 @@ export function AnnouncementBanner() {
               To your agenda! Nx Conf Lite is on 4/29, a free to attend
               conference that you don't want to miss!
             </span>
-            <span className="inline-block sm:ml-2">
+            <span className="ml-2 inline-block">
               <Link href="/conf">
                 <a className="font-bold text-white underline">
                   {' '}
