4030d472444f7efdb6d1db2ff3bc9b0e9dee9064
Jason Jean <jasonjean1993@gmail.com>
2022-04-22 14:59:57 -0400

    fix(core): fix creating new workspaces with not normalized names

--- packages/create-nx-workspace/bin/utils.ts
+++ packages/create-nx-workspace/bin/utils.ts
@@ -1,3 +1,10 @@
 export function stringifyCollection(items: string[]): string {
   return items.map((item) => `"${item}"`).join(', ');
 }
+
+export function getFileName(name: string) {
+  return name
+    .replace(/([a-z\d])([A-Z])/g, '$1_$2')
+    .toLowerCase()
+    .replace(/[ _]/g, '-');
+}
