2ef452440fc6234f3e33290212e1a3cb29dc1d95
wSedlacek <william.sedlacek@icloud.com>
2022-04-21 18:33:20 -0700

    fix(core): calculate dependencies of mixed targets

--- packages/nx/src/tasks-runner/run-command.ts
+++ packages/nx/src/tasks-runner/run-command.ts
@@ -371,7 +371,7 @@ function addTasksForProjectDependencyConfig(
   };
 
   const newPath = [...path, pathFragment];
-  seenSet.add(project.name);
+  seenSet.add(targetIdentifier);
 
   if (tasksMap.has(targetIdentifier)) {
     return;
@@ -391,11 +391,11 @@ function addTasksForProjectDependencyConfig(
         ) {
           const depTargetId = getId({
             project: depProject.name,
-            target,
+            target: dependencyConfig.target,
             configuration: configuration,
           });
           exitOnCircularDep(newPath, depTargetId);
-          if (seenSet.has(dep.target)) {
+          if (seenSet.has(depTargetId)) {
             continue;
           }
 
@@ -416,18 +416,17 @@ function addTasksForProjectDependencyConfig(
           );
         } else {
           if (!depProject) {
-            seenSet.add(dep.target);
             continue;
           }
           const depTargetId = getId({
             project: depProject.name,
-            target,
+            target: dependencyConfig.target,
             configuration: configuration,
           });
 
           exitOnCircularDep(newPath, depTargetId);
 
-          if (seenSet.has(dep.target)) {
+          if (seenSet.has(depTargetId)) {
             continue;
           }
 
