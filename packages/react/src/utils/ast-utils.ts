fa1bc7b28982c7bebdaed6c7ef3f9cdf10346b13
Juri Strumpflohner <juri.strumpflohner@gmail.com>
2022-04-22 17:40:48 +0200

    fix(react): use Routes component wrapper for react router v6 (#9936)

--- packages/react/src/utils/ast-utils.ts
+++ packages/react/src/utils/ast-utils.ts
@@ -256,24 +256,29 @@ export function addInitialRoutes(
         <li><Link to="/page-2">Page 2</Link></li>
       </ul>
     </div>
-    <Route
-      path="/"
-      element={
-        <div>This is the generated root route. <Link to="/page-2">Click here for page 2.</Link></div>
-      }
-    />
-    <Route
-      path="/page-2"
-      element={
-        <div><Link to="/">Click here to go back to root page.</Link></div>
-      }
-    />
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div>This is the generated root route. <Link to="/page-2">Click here for page 2.</Link></div>
+        }
+      />
+      <Route
+        path="/page-2"
+        element={
+          <div><Link to="/">Click here to go back to root page.</Link></div>
+        }
+      />
+    </Routes>
     {/* END: routes */}
     `,
   };
 
   return [
-    ...addImport(source, `import { Route, Link } from 'react-router-dom';`),
+    ...addImport(
+      source,
+      `import { Route, Routes, Link } from 'react-router-dom';`
+    ),
     insertRoutes,
   ];
 }
