bef82408b4ae7608fdca4aa68d7506ceb8ba30c3
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2022-04-29 19:15:08 +0300

    fix(core): refine move and remove generators for projects with a target name (#10063)
    Co-authored-by: Jack Hsu <jack.hsu@gmail.com>

--- packages/workspace/src/generators/move/lib/update-build-targets.ts
+++ packages/workspace/src/generators/move/lib/update-build-targets.ts
@@ -1,4 +1,9 @@
-import { getProjects, Tree, updateProjectConfiguration } from '@nrwl/devkit';
+import {
+  getProjects,
+  TargetConfiguration,
+  Tree,
+  updateProjectConfiguration,
+} from '@nrwl/devkit';
 import { NormalizedSchema } from '../schema';
 
 /**
@@ -8,16 +13,40 @@ export function updateBuildTargets(tree: Tree, schema: NormalizedSchema) {
   getProjects(tree).forEach((projectConfig, project) => {
     Object.entries(projectConfig.targets || {}).forEach(
       ([target, targetConfig]) => {
-        const configString = JSON.stringify(targetConfig);
-        const updated = JSON.parse(
-          configString.replace(
-            new RegExp(`${schema.projectName}:`, 'g'),
-            `${schema.newProjectName}:`
-          )
-        );
-        projectConfig.targets[target] = updated;
+        updateJsonValue(targetConfig, (value) => {
+          const [project, target, configuration] = value.split(':');
+          if (project === schema.projectName && target) {
+            return configuration
+              ? `${schema.newProjectName}:${target}:${configuration}`
+              : `${schema.newProjectName}:${target}`;
+          }
+        });
       }
     );
     updateProjectConfiguration(tree, project, projectConfig);
+  });
+}
+
+function updateJsonValue(
+  config: TargetConfiguration,
+  callback: (x: string) => void | string
+) {
+  function recur(obj, key, value) {
+    if (typeof value === 'string') {
+      const result = callback(value);
+      if (result) {
+        obj[key] = result;
+      }
+    } else if (Array.isArray(value)) {
+      value.forEach((x, idx) => recur(value, idx, x));
+    } else if (typeof value === 'object') {
+      Object.entries(value).forEach(([k, v]) => {
+        recur(value, k, v);
+      });
+    }
+  }
+
+  Object.entries(config).forEach(([k, v]) => {
+    recur(config, k, v);
   });
 }
