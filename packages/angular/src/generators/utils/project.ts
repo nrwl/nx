d4c507b678ac7e18141340351ac4f3c63fa39900
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-05-03 15:15:35 +0100

    fix(angular): normalize project name and don't default to npmScope as prefix when is not a valid html selector

--- packages/angular/src/generators/utils/project.ts
+++ packages/angular/src/generators/utils/project.ts
@@ -1,13 +1,33 @@
 import { names } from '@nrwl/devkit';
 
-export function normalizeDirectory(appName: string, directoryName: string) {
+export function normalizeDirectory(
+  appName: string,
+  directoryName: string
+): string {
   return directoryName
     ? `${names(directoryName).fileName}/${names(appName).fileName}`
     : names(appName).fileName;
 }
 
-export function normalizeProjectName(appName: string, directoryName: string) {
-  return normalizeDirectory(appName, directoryName)
-    .replace(new RegExp('/', 'g'), '-')
-    .replace(/-\d+/g, '');
+export function normalizeProjectName(
+  appName: string,
+  directoryName: string
+): string {
+  return normalizeDirectory(appName, directoryName).replace(/\//g, '-');
+}
+
+export function normalizePrefix(
+  prefix: string | undefined,
+  npmScope: string | undefined
+): string {
+  if (prefix) {
+    return prefix;
+  }
+
+  // Prefix needs to be a valid html selector, if npmScope it's not valid, we don't default
+  // to it and let it fall through to the Angular schematic to handle it
+  // https://github.com/angular/angular-cli/blob/1c634cd327e5a850553b258aa2d5e6a6b2c75c65/packages/schematics/angular/component/index.ts#L130
+  const htmlSelectorRegex =
+    /^[a-zA-Z][.0-9a-zA-Z]*(:?-[a-zA-Z][.0-9a-zA-Z]*)*$/;
+  return npmScope && htmlSelectorRegex.test(npmScope) ? npmScope : undefined;
 }
