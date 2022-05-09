de59f0d4f54bb2273fa9bcb506647223ad7d1254
skrtheboss <denisfrenademetz97@gmail.com>
2022-05-09 23:19:27 +0200

    fix(node): use webpack compilation hooks to emit package.json
    Use the webpack plugin system to create the package.json, this allows for developers to use plugins like
    CleanWebpackPlugin which clears the dist before the compilation starts. If this plugin is used in
    combination with the old method of generating the package.json, then it is deleted on compilation start.

--- packages/node/src/utils/node.config.spec.ts
+++ packages/node/src/utils/node.config.spec.ts
@@ -1,6 +1,9 @@
+import { ProjectGraph, ExecutorContext } from '@nrwl/devkit';
 import { join } from 'path';
-import { getNodeWebpackConfig } from './node.config';
 import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+
+import { GeneratePackageJsonWebpackPlugin } from './generate-package-json-webpack-plugin';
+import { getNodeWebpackConfig } from './node.config';
 import { BuildNodeBuilderOptions } from './types';
 
 jest.mock('tsconfig-paths-webpack-plugin');
@@ -11,8 +14,19 @@ jest.mock('@nrwl/devkit', () => ({
 }));
 
 describe('getNodePartial', () => {
+  let context: ExecutorContext;
+  let projectGraph: ProjectGraph;
   let input: BuildNodeBuilderOptions;
   beforeEach(() => {
+    context = {
+      projectName: 'sample-project',
+      root: '/root',
+      cwd: '',
+      target: {} as any,
+      isVerbose: false,
+      workspace: {} as any,
+    };
+    projectGraph = {} as any;
     input = {
       main: 'main.ts',
       outputPath: 'dist',
@@ -28,18 +42,18 @@ describe('getNodePartial', () => {
 
   describe('unconditionally', () => {
     it('should target commonjs', () => {
-      const result = getNodeWebpackConfig(input);
+      const result = getNodeWebpackConfig(context, projectGraph, input);
       expect(result.output.libraryTarget).toEqual('commonjs');
     });
 
     it('should target node', () => {
-      const result = getNodeWebpackConfig(input);
+      const result = getNodeWebpackConfig(context, projectGraph, input);
 
       expect(result.target).toEqual('node');
     });
 
     it('should not polyfill node apis', () => {
-      const result = getNodeWebpackConfig(input);
+      const result = getNodeWebpackConfig(context, projectGraph, input);
 
       expect(result.node).toEqual(false);
     });
@@ -47,7 +61,7 @@ describe('getNodePartial', () => {
 
   describe('the optimization option when true', () => {
     it('should minify', () => {
-      const result = getNodeWebpackConfig({
+      const result = getNodeWebpackConfig(context, projectGraph, {
         ...input,
         optimization: true,
       });
@@ -57,7 +71,7 @@ describe('getNodePartial', () => {
     });
 
     it('should concatenate modules', () => {
-      const result = getNodeWebpackConfig({
+      const result = getNodeWebpackConfig(context, projectGraph, {
         ...input,
         optimization: true,
       });
@@ -68,14 +82,14 @@ describe('getNodePartial', () => {
 
   describe('the externalDependencies option', () => {
     it('should change all node_modules to commonjs imports', () => {
-      const result = getNodeWebpackConfig(input);
+      const result = getNodeWebpackConfig(context, projectGraph, input);
       const callback = jest.fn();
       result.externals[0](null, '@nestjs/core', callback);
       expect(callback).toHaveBeenCalledWith(null, 'commonjs @nestjs/core');
     });
 
     it('should change given module names to commonjs imports but not others', () => {
-      const result = getNodeWebpackConfig({
+      const result = getNodeWebpackConfig(context, projectGraph, {
         ...input,
         externalDependencies: ['module1'],
       });
@@ -87,12 +101,37 @@ describe('getNodePartial', () => {
     });
 
     it('should not change any modules to commonjs imports', () => {
-      const result = getNodeWebpackConfig({
+      const result = getNodeWebpackConfig(context, projectGraph, {
         ...input,
         externalDependencies: 'none',
       });
 
       expect(result.externals).not.toBeDefined();
+    });
+  });
+
+  describe('the generatePackageJson option', () => {
+    it('should add the GeneratePackageJsonWebpackPlugin plugin', () => {
+      const result = getNodeWebpackConfig(context, projectGraph, {
+        ...input,
+        generatePackageJson: true,
+      });
+      expect(
+        result.plugins.find(
+          (plugin) => plugin instanceof GeneratePackageJsonWebpackPlugin
+        )
+      ).toBeTruthy();
+    });
+    it('should not add the GeneratePackageJsonWebpackPlugin plugin', () => {
+      const result = getNodeWebpackConfig(context, projectGraph, {
+        ...input,
+        generatePackageJson: false,
+      });
+      expect(
+        result.plugins.find(
+          (plugin) => plugin instanceof GeneratePackageJsonWebpackPlugin
+        )
+      ).toBeFalsy();
     });
   });
 });
