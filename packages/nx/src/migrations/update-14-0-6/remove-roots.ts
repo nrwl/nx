9cdcf1720ac79e980a6802d8e3dc43317865de1a
Jason Jean <jasonjean1993@gmail.com>
2022-05-11 16:57:33 -0400

    fix(core): only migrate projects with project.json (#10251)

--- packages/nx/src/migrations/update-14-0-6/remove-roots.ts
+++ packages/nx/src/migrations/update-14-0-6/remove-roots.ts
@@ -4,11 +4,14 @@ import {
   updateProjectConfiguration,
 } from '../../generators/utils/project-configuration';
 import { formatChangedFilesWithPrettierIfAvailable } from '../../generators/internal-utils/format-changed-files-with-prettier-if-available';
+import { join } from 'path';
 
 export default async function (tree: Tree) {
   // This looks like it does nothing, but this will actually effectively migrate over all the configs that need to be moved over, but won't touch configs that don't need to be moved
   for (const [projName, projConfig] of getProjects(tree)) {
-    updateProjectConfiguration(tree, projName, projConfig);
+    if (tree.exists(join(projConfig.root, 'project.json'))) {
+      updateProjectConfiguration(tree, projName, projConfig);
+    }
   }
 
   await formatChangedFilesWithPrettierIfAvailable(tree);
