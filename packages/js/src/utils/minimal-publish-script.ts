56908f84408e7a52893f2226c27f48fdb6f084a4
James <5511220+Zamiell@users.noreply.github.com>
2022-05-21 16:11:15 -0400

    fix(js): semver regex update

--- packages/js/src/utils/minimal-publish-script.ts
+++ packages/js/src/utils/minimal-publish-script.ts
@@ -27,7 +27,7 @@ function invariant(condition, message) {
 const [, , name, version, tag = 'next'] = process.argv;
 
 // A simple SemVer validation to validate the version
-const validVersion = /^\\d+\\.\\d+\\.\\d(-\\w+\\.\\d+)?/;
+const validVersion = /^\\d+\\.\\d+\\.\\d+(-\\w+\\.\\d+)?/;
 invariant(
   version && validVersion.test(version),
   \`No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got \${version}.\`
