7897f5fbbf783554dbfc51ba1f08dc31c4b80a0c
Phillip Barta <barta.phillip@gmail.com>
2022-05-28 10:44:34 +0200

    feat(core): migrate to prettier ^2.6.2

--- packages/nx/src/utils/versions.ts
+++ packages/nx/src/utils/versions.ts
@@ -2,7 +2,7 @@ export const nxVersion = require('../../package.json').version;
 
 export const angularCliVersion = '~14.0.0';
 export const typescriptVersion = '~4.7.2';
-export const prettierVersion = '^2.5.1';
+export const prettierVersion = '^2.6.2';
 export const tslintVersion = '~6.1.0';
 export const typescriptESLintVersion = '~5.24.0';
 export const eslintVersion = '~8.15.0';
