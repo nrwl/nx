52848641c863703aa0297dada68c05e433def094
Victor Savkin <mail@vsavkin.com>
2022-06-07 10:17:10 -0400

    cleanup(core): speed up code loading by not loading the info about the daemon process

--- packages/nx/src/daemon/client/generate-help-output.ts
+++ packages/nx/src/daemon/client/generate-help-output.ts
@@ -2,13 +2,7 @@ import { spawnSync } from 'child_process';
 import { getDaemonProcessId } from '../cache';
 import { DAEMON_OUTPUT_LOG_FILE } from '../tmp-dir';
 
-export function generateDaemonHelpOutput(
-  isGenerateDocsProcess = false
-): string {
-  if (isGenerateDocsProcess) {
-    return `The Nx Daemon is a local server which runs in the background in order to intelligently cache information about the workspace's project graph.`;
-  }
-
+export function generateDaemonHelpOutput(): string {
   /**
    * A workaround for cases such as yargs output where we need to synchronously
    * get the value of this async operation.
