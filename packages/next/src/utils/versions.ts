db8bf61bf4f5e9ae6ba15c531534c1acf3188164
Jack Hsu <jack.hsu@gmail.com>
2022-06-09 09:28:38 -0400

    feat(nextjs): update package versions

--- packages/next/src/utils/versions.ts
+++ packages/next/src/utils/versions.ts
@@ -1,9 +1,9 @@
 export const nxVersion = require('../../package.json').version;
 
-export const nextVersion = '12.1.5';
-export const eslintConfigNextVersion = '12.1.5';
-export const sassVersion = '1.43.2';
-export const lessLoader = '10.2.0';
-export const stylusLoader = '6.2.0';
+export const nextVersion = '12.1.6';
+export const eslintConfigNextVersion = '12.1.6';
+export const sassVersion = '1.52.3';
+export const lessLoader = '11.0.0';
+export const stylusLoader = '7.0.0';
 export const emotionServerVersion = '11.4.0';
 export const babelPluginStyledComponentsVersion = '1.10.7';
