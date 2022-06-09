d4c04b5f6053b3b53910f301e7cec659ab1f1ada
Jack Hsu <jack.hsu@gmail.com>
2022-06-09 09:57:09 -0400

    feat(react): update package versions (#10657)

--- packages/react/src/utils/versions.ts
+++ packages/react/src/utils/versions.ts
@@ -3,8 +3,8 @@ export const nxVersion = require('../../package.json').version;
 export const reactVersion = '18.1.0';
 export const reactDomVersion = '18.1.0';
 export const reactIsVersion = '18.1.0';
-export const typesReactVersion = '18.0.8';
-export const typesReactDomVersion = '18.0.3';
+export const typesReactVersion = '18.0.12';
+export const typesReactDomVersion = '18.0.5';
 export const typesReactIsVersion = '17.0.3';
 
 export const typesNodeVersion = '16.11.7';
@@ -17,20 +17,20 @@ export const emotionReactVersion = '11.9.0';
 export const emotionBabelPlugin = '11.9.2';
 
 // WARNING: This needs to be in sync with Next.js' dependency or else there might be issues.
-export const styledJsxVersion = '5.0.1';
+export const styledJsxVersion = '5.0.2';
 
 export const reactRouterDomVersion = '6.3.0';
 export const typesReactRouterDomVersion = '5.3.3';
 
-export const testingLibraryReactVersion = '13.1.1';
+export const testingLibraryReactVersion = '13.3.0';
 
-export const reduxjsToolkitVersion = '1.8.1';
-export const reactReduxVersion = '8.0.1';
+export const reduxjsToolkitVersion = '1.8.2';
+export const reactReduxVersion = '8.0.2';
 export const reactTestRendererVersion = '18.1.0';
 
 export const eslintPluginImportVersion = '2.26.0';
 export const eslintPluginJsxA11yVersion = '6.5.1';
-export const eslintPluginReactVersion = '7.29.4';
+export const eslintPluginReactVersion = '7.30.0';
 export const eslintPluginReactHooksVersion = '4.5.0';
 
 export const babelPluginStyledComponentsVersion = '1.10.7';
