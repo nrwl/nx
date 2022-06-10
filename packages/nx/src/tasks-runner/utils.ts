3b3888a2ecd680b16bcad4f91127cfeba65dca9f
Victor Savkin <mail@vsavkin.com>
2022-06-10 14:47:35 -0400

    chore(repo): add unparse back as other package deep import it

--- packages/nx/src/tasks-runner/utils.ts
+++ packages/nx/src/tasks-runner/utils.ts
@@ -279,3 +279,45 @@ function longRunningTask(task: Task) {
     !!task.overrides['watch'] || t === 'serve' || t === 'dev' || t === 'start'
   );
 }
+
+// TODO: vsavkin remove when nx-cloud doesn't depend on it
+export function unparse(options: Object): string[] {
+  const unparsed = [];
+  for (const key of Object.keys(options)) {
+    const value = options[key];
+    unparseOption(key, value, unparsed);
+  }
+
+  return unparsed;
+}
+
+function unparseOption(key: string, value: any, unparsed: string[]) {
+  if (value === true) {
+    unparsed.push(`--${key}`);
+  } else if (value === false) {
+    unparsed.push(`--no-${key}`);
+  } else if (Array.isArray(value)) {
+    value.forEach((item) => unparseOption(key, item, unparsed));
+  } else if (Object.prototype.toString.call(value) === '[object Object]') {
+    const flattened = flatten<any, any>(value, { safe: true });
+    for (const flattenedKey in flattened) {
+      unparseOption(
+        `${key}.${flattenedKey}`,
+        flattened[flattenedKey],
+        unparsed
+      );
+    }
+  } else if (
+    typeof value === 'string' &&
+    stringShouldBeWrappedIntoQuotes(value)
+  ) {
+    const sanitized = value.replace(/"/g, String.raw`\"`);
+    unparsed.push(`--${key}="${sanitized}"`);
+  } else if (value != null) {
+    unparsed.push(`--${key}=${value}`);
+  }
+}
+
+function stringShouldBeWrappedIntoQuotes(str: string) {
+  return str.includes(' ') || str.includes('{') || str.includes('"');
+}
