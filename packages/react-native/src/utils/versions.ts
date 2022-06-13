cce43db1f9866861de0a917fecb9df4094d48fd2
Emily Xiong <xiongemi@gmail.com>
2022-06-13 12:57:22 -0400

    feat(react-native): upgrade metro to 0.71.1 and react-native-config to 1.4.6 (#10700)

--- packages/react-native/src/utils/versions.ts
+++ packages/react-native/src/utils/versions.ts
@@ -5,14 +5,14 @@ export const typesReactNativeVersion = '0.67.8';
 
 export const typesNodeVersion = '16.11.7';
 
-export const metroVersion = '0.71.0';
+export const metroVersion = '0.71.1';
 
 export const reactNativeCommunityCli = '7.0.3';
 export const reactNativeCommunityCliIos = '7.0.1';
 export const reactNativeCommunityCliAndroid = '7.0.1';
 
-export const reactNativeConfigVersion = '1.4.5';
-export const reactNativeAsyncStorageAsyncStorageVersion = '1.17.5';
+export const reactNativeConfigVersion = '1.4.6';
+export const reactNativeAsyncStorageAsyncStorageVersion = '1.17.6';
 
 export const testingLibraryReactNativeVersion = '9.1.0';
 export const testingLibraryJestNativeVersion = '4.0.5';
