9ad7bd56d42d9e08e16abc970ca388e5932aad0e
Jon Hammerskov <jon@hammerskov.dk>
2022-06-14 07:36:38 +0200

    fix(core): prevent race condition when compiling tools/generators
    when executing workspace generators the tools/generators are first compiled to dist/out-tsc
    and the
    compilation will conflict if generators are invoked in run-commands with parallel execution.
    with
    this change a lock file is used to serialize access to dist/out-tsc
    and in addition to this
    tools/generator/* are hashed so that
    compilation is only invoked when there is some change since
    last compilation
    
    ISSUES CLOSED: #8887

--- packages/nx/src/command-line/workspace-generators.ts
+++ packages/nx/src/command-line/workspace-generators.ts
@@ -1,18 +1,26 @@
 import * as chalk from 'chalk';
 import { execSync } from 'child_process';
-import { readdirSync, existsSync } from 'fs';
-import { copySync, removeSync } from 'fs-extra';
+import { existsSync, lstatSync, readdirSync, readFileSync } from 'fs';
+import {
+  copySync,
+  emptyDirSync,
+  ensureDirSync,
+  removeSync,
+  writeFileSync,
+} from 'fs-extra';
+import { lock, unlock } from 'lockfile';
+import { sha1 } from 'object-hash';
 import * as path from 'path';
-import * as yargsParser from 'yargs-parser';
-import { workspaceRoot } from '../utils/workspace-root';
-import { fileExists } from '../utils/fileutils';
-import { output } from '../utils/output';
 import type { CompilerOptions } from 'typescript';
-import { generate } from './generate';
-import { readJsonFile, writeJsonFile } from '../utils/fileutils';
+import * as yargsParser from 'yargs-parser';
+import { getFileHashes, getGitHashForFiles } from '../hasher/git-hasher';
+import { fileExists, readJsonFile, writeJsonFile } from '../utils/fileutils';
 import { logger } from '../utils/logger';
+import { output } from '../utils/output';
 import { getPackageManagerCommand } from '../utils/package-manager';
 import { normalizePath } from '../utils/path';
+import { workspaceRoot } from '../utils/workspace-root';
+import { generate } from './generate';
 import { parserConfiguration } from './nx-commands';
 
 const rootDirectory = workspaceRoot;
@@ -29,30 +37,120 @@ type TsConfig = {
   references?: Array<{ path: string }>;
 };
 
-export async function workspaceGenerators(args: string[]) {
-  const outDir = compileTools();
+export async function workspaceGenerators(args: string[]): Promise<void> {
+  const outDir = await compileTools();
   const collectionFile = path.join(outDir, 'workspace-generators.json');
   const parsedArgs = parseOptions(args, outDir, collectionFile);
   if (parsedArgs.listGenerators) {
-    return listGenerators(collectionFile);
+    listGenerators(collectionFile);
   } else {
     process.exitCode = await generate(process.cwd(), parsedArgs);
   }
 }
 
-// compile tools
-function compileTools() {
-  const toolsOutDir = getToolsOutDir();
-  removeSync(toolsOutDir);
-  compileToolsDir(toolsOutDir);
-
-  const generatorsOutDir = path.join(toolsOutDir, 'generators');
-  const collectionData = constructCollection();
-  writeJsonFile(
-    path.join(generatorsOutDir, 'workspace-generators.json'),
-    collectionData
+async function takeLock(lockFile: string): Promise<boolean> {
+  let resolve: (value: boolean) => void;
+  const result = new Promise<boolean>((_resolve, _reject) => {
+    resolve = _resolve;
+  });
+  lock(
+    lockFile,
+    { retries: 60, retryWait: 1000, stale: 3 * 60 * 1000 },
+    (err: any) => {
+      resolve(!err);
+    }
   );
-  return generatorsOutDir;
+  return result;
+}
+
+async function releaseLock(lockFile: string): Promise<boolean> {
+  let resolve: (value: boolean) => void;
+  const result = new Promise<boolean>((_resolve, _reject) => {
+    resolve = _resolve;
+  });
+  unlock(lockFile, (err: any) => {
+    resolve(!err);
+  });
+  return result;
+}
+
+function globalFiles() {
+  const files = [];
+  const candidates = ['package.json', 'package-lock.json', 'yarn.lock'];
+  for (const file of candidates) {
+    if (lstatSync(file, { throwIfNoEntry: false })) {
+      files.push(file);
+    }
+  }
+  return files;
+}
+
+async function generatorsDirHash(): Promise<string> {
+  const hashes = new Map<string, string>();
+  const generatorsHashes = (await getFileHashes(generatorsDir)).allFiles;
+  const globalHashes = (await getGitHashForFiles(globalFiles(), rootDirectory))
+    .hashes;
+  generatorsHashes.forEach((hash: string, filename: string) => {
+    hashes.set(filename, hash);
+  });
+  globalHashes.forEach((hash: string, filename: string) => {
+    hashes.set(filename, hash);
+  });
+  return sha1(hashes);
+}
+
+// compile tools
+async function compileTools() {
+  const toolsOutDir = getToolsOutDir();
+  const generatorsOutDir = path.join(toolsOutDir, 'generators');
+  const hashPath = path.join(toolsOutDir, '.hash');
+  const toolsLockFile = path.join(
+    path.dirname(toolsOutDir),
+    `.${path.basename(toolsOutDir)}.lock`
+  );
+  // if toolsOutDir is dist/out-tsc/tools
+  // then we use dist/out-tsc/.tools.lock as a lock file
+  // to ensure only one thread is building the toolsdir at a time
+  let hashHasChanged = true;
+  let locked = false;
+  const currentHash = await generatorsDirHash();
+
+  try {
+    try {
+      ensureDirSync(path.dirname(toolsOutDir));
+      locked = await takeLock(toolsLockFile);
+      if (locked) {
+        // OK we got the lock so trust no-one else will
+        // change the .hash file
+        const storedHash = readFileSync(hashPath, 'ascii');
+        hashHasChanged = storedHash !== currentHash;
+      }
+    } catch (_err: any) {}
+
+    if (hashHasChanged) {
+      // either we got the lock (locked) and the hash differed
+      // or we did not get the lock, so to be on the safe side
+      // just compile the generators
+      emptyDirSync(toolsOutDir);
+      compileToolsDir(toolsOutDir);
+
+      const collectionData = constructCollection();
+      writeJsonFile(
+        path.join(generatorsOutDir, 'workspace-generators.json'),
+        collectionData
+      );
+      writeFileSync(hashPath, currentHash);
+    }
+  } finally {
+    // if we took the lock then release it again
+    // if we did not get the lock it will become stale soon
+    // and next time takeLock() will take over the stale
+    // lock file and then it will be released
+    if (locked) {
+      await releaseLock(toolsLockFile);
+    }
+  }
+  return Promise.resolve(generatorsOutDir);
 }
 
 function getToolsOutDir() {
