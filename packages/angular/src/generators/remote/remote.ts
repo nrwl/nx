b7ce906677551d93719db1c0327ba8a5a07ee8a1
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-06-15 12:28:36 +0100

    fix(angular): remove unnecessary unused entry module import from new mf remote apps

--- packages/angular/src/generators/remote/remote.ts
+++ packages/angular/src/generators/remote/remote.ts
@@ -86,17 +86,10 @@ export class AppComponent {}`;
 
   tree.write(
     joinPathFragments(project.sourceRoot, 'app/app.module.ts'),
-    `/*
-* This RemoteEntryModule is imported here to allow TS to find the Module during
-* compilation, allowing it to be included in the built bundle. This is required
-* for the Module Federation Plugin to expose the Module correctly.
-* */
-import { RemoteEntryModule } from './remote-entry/entry.module';
-import { NgModule } from '@angular/core';
+    `import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 @NgModule({
  declarations: [AppComponent],
