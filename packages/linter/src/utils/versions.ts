a7888552b8fde086e5cfc0d783dcfdda02c3d36a
Sicarius <73046273+sicarius97@users.noreply.github.com>
2022-06-21 05:33:42 -0500

    fix(linter): update @typescript-eslint/* deps
    Co-authored-by: Miroslav Jonas <missing.manual@gmail.com>

--- packages/linter/src/utils/versions.ts
+++ packages/linter/src/utils/versions.ts
@@ -4,6 +4,6 @@ export const tslintVersion = '~6.1.0';
 export const tslintToEslintConfigVersion = '^2.13.0';
 export const buildAngularVersion = '~14.0.0';
 
-export const typescriptESLintVersion = '~5.24.0';
+export const typescriptESLintVersion = '^5.29.0';
 export const eslintVersion = '~8.15.0';
 export const eslintConfigPrettierVersion = '8.1.0';
