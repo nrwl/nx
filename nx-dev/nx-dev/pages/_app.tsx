6d8ed4babaed679a1a4eb11b27263d74ad4fda1e
Benjamin Cabanes <3447705+bcabanes@users.noreply.github.com>
2022-06-30 11:28:13 -0400

    docs(nxdev): set hotjar tag script to afterInteractive (#10962)

--- nx-dev/nx-dev/pages/_app.tsx
+++ nx-dev/nx-dev/pages/_app.tsx
@@ -90,7 +90,7 @@ export default function CustomApp({ Component, pageProps }: AppProps) {
       {/* Hotjar Analytics */}
       <Script
         id="hotjar-script-loader"
-        strategy="worker"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
           (function(h,o,t,j,a,r){
