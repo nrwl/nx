b715fa82355f680b5c002c7c049ea88189f67854
Emily Xiong <xiongemi@gmail.com>
2022-07-05 13:48:46 -0400

    feat(react-native): upgrade react-native to 0.69.1

--- packages/react-native/src/utils/versions.ts
+++ packages/react-native/src/utils/versions.ts
@@ -1,20 +1,20 @@
 export const nxVersion = require('../../package.json').version;
 
-export const reactNativeVersion = '0.68.2';
-export const typesReactNativeVersion = '0.67.8';
+export const reactNativeVersion = '0.69.1';
+export const typesReactNativeVersion = '0.69.2';
 
 export const typesNodeVersion = '16.11.7';
 
-export const metroVersion = '0.71.1';
+export const metroVersion = '0.71.2';
 
-export const reactNativeCommunityCli = '7.0.3';
-export const reactNativeCommunityCliIos = '7.0.1';
-export const reactNativeCommunityCliAndroid = '7.0.1';
+export const reactNativeCommunityCli = '8.0.2';
+export const reactNativeCommunityCliIos = '8.0.2';
+export const reactNativeCommunityCliAndroid = '8.0.2';
 
 export const reactNativeConfigVersion = '1.4.6';
-export const reactNativeAsyncStorageAsyncStorageVersion = '1.17.6';
+export const reactNativeAsyncStorageAsyncStorageVersion = '1.17.7';
 
-export const testingLibraryReactNativeVersion = '9.1.0';
+export const testingLibraryReactNativeVersion = '10.1.1';
 export const testingLibraryJestNativeVersion = '4.0.5';
 
 export const jestReactNativeVersion = '18.0.0';
@@ -22,4 +22,4 @@ export const jestReactNativeVersion = '18.0.0';
 export const reactNativeSvgTransformerVersion = '1.0.0';
 export const reactNativeSvgVersion = '12.3.0';
 
-export const babelRuntimeVersion = '7.18.3';
+export const babelRuntimeVersion = '7.18.6';
