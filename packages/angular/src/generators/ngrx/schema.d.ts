99af08edcacca7abbda9d951590b97b54e60fbcd
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-07-18 14:58:13 +0100

    feat(angular): deprecate syntax option from the ngrx generator

--- packages/angular/src/generators/ngrx/schema.d.ts
+++ packages/angular/src/generators/ngrx/schema.d.ts
@@ -3,12 +3,20 @@ export interface NgRxGeneratorOptions {
   minimal: boolean;
   module: string;
   name: string;
-  useDataPersistence: boolean;
   barrels?: boolean;
   facade?: boolean;
   root?: boolean;
   skipFormat?: boolean;
   skipImport?: boolean;
   skipPackageJson?: boolean;
+  /**
+   * @deprecated This option is deprecated and will be removed in v15.
+   * Using the individual operators is recommended.
+   */
+  useDataPersistence?: boolean;
+  /**
+   * @deprecated Always use the `creators` value. The `classes` syntax is
+   * deprecated and this option will be removed in v15.
+   */
   syntax?: 'classes' | 'creators';
 }
