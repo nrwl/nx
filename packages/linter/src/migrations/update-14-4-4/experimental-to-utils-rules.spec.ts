af258aad2583187a388951df2cd276dc17edb428
Miroslav Jonas <missing.manual@gmail.com>
2022-07-20 23:32:12 +0200

    feat(linter): update migration version and remove limitations of the updates

--- packages/linter/src/migrations/update-14-4-4/experimental-to-utils-rules.spec.ts
+++ packages/linter/src/migrations/update-14-4-4/experimental-to-utils-rules.spec.ts
@@ -11,11 +11,11 @@ describe('experimentalToUtilsUpdate()', () => {
       `tools/eslint-rules/rules/existing-rule.ts`,
       `
         import { ESLintUtils } from '@typescript-eslint/experimental-utils';
-        import { rule, RULE_NAME } from './existing-rule'; 
+        import { rule, RULE_NAME } from './existing-rule';
 
         // NOTE: The rule will be available in ESLint configs as "@nrwl/nx/workspace/existing-rule"
         export const RULE_NAME = 'existing-rule';
-  
+
         export const rule = ESLintUtils.RuleCreator(() => __filename)({
           name: RULE_NAME,
           meta: {
@@ -51,9 +51,20 @@ describe('experimentalToUtilsUpdate()', () => {
       });
       `
     );
+
+    tree.write(
+      `tools/eslint-rules/rules/multi-import.ts`,
+      `
+        import { ESLintUtils } from '@typescript-eslint/experimental-utils';
+        import { TSESLint } from '@typescript-eslint/experimental-utils';
+        import { rule, RULE_NAME } from './existing-rule';
+
+        // NOTE: remaining code is irrelevant for this test
+      `
+    );
   });
 
-  it('should switch import from @typescript-eslint/experimental-utils to @typescript-eslint/utils in rule file', async () => {
+  it('should switch import from @typescript-eslint/experimental-utils to @typescript-eslint/utils in rule and spec file', async () => {
     await experimentalToUtilsUpdate(tree);
 
     expect(
@@ -61,11 +72,11 @@ describe('experimentalToUtilsUpdate()', () => {
     ).toMatchInlineSnapshot(`
     "
             import { ESLintUtils } from '@typescript-eslint/utils';
-            import { rule, RULE_NAME } from './existing-rule'; 
+            import { rule, RULE_NAME } from './existing-rule';
 
             // NOTE: The rule will be available in ESLint configs as \\"@nrwl/nx/workspace/existing-rule\\"
             export const RULE_NAME = 'existing-rule';
-      
+
             export const rule = ESLintUtils.RuleCreator(() => __filename)({
               name: RULE_NAME,
               meta: {
@@ -84,10 +95,6 @@ describe('experimentalToUtilsUpdate()', () => {
             });
           "
         `);
-  });
-
-  it('should switch import from @typescript-eslint/experimental-utils to @typescript-eslint/utils in spec file', async () => {
-    await experimentalToUtilsUpdate(tree);
 
     expect(
       tree
@@ -108,5 +115,21 @@ describe('experimentalToUtilsUpdate()', () => {
             });
             "
     `);
+  });
+
+  it('should switch import from @typescript-eslint/experimental-utils to @typescript-eslint/utils on multiple imports', async () => {
+    await experimentalToUtilsUpdate(tree);
+
+    expect(
+      tree.read(`tools/eslint-rules/rules/multi-import.ts`).toString('utf-8')
+    ).toMatchInlineSnapshot(`
+    "
+            import { ESLintUtils } from '@typescript-eslint/utils';
+            import { TSESLint } from '@typescript-eslint/utils';
+            import { rule, RULE_NAME } from './existing-rule';
+
+            // NOTE: remaining code is irrelevant for this test
+          "
+        `);
   });
 });
