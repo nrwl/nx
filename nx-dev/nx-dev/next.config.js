f0b350a84c3e1392c80d498c6a13cd5db46c443d
ben <3447705+bcabanes@users.noreply.github.com>
2022-07-21 10:25:21 -0400

    docs(nxdev): add security headers

--- nx-dev/nx-dev/next.config.js
+++ nx-dev/nx-dev/next.config.js
@@ -21,6 +21,24 @@ module.exports = withNx({
   env: {
     VERCEL: process.env.VERCEL,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-DNS-Prefetch-Control', value: 'on' },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'Referrer-Policy', value: 'no-referrer' },
+        ],
+      },
+    ];
+  },
   async redirects() {
     const rules = [];
 
