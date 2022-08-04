f7d2a4ab012f5a517c3bf5fc0fa7e8fe9c9e10b3
Caleb Ukle <caleb@nrwl.io>
2022-08-04 15:34:30 -0500

    fix(testing): force babel-jest preset in transformer for project transforms

--- packages/react/src/utils/jest-utils.ts
+++ packages/react/src/utils/jest-utils.ts
@@ -1,6 +1,11 @@
 export function updateJestConfigContent(content: string) {
-  return content.replace(
-    'transform: {',
-    "transform: {\n    '^(?!.*\\\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',"
-  );
+  return content
+    .replace(
+      'transform: {',
+      "transform: {\n    '^(?!.*\\\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',"
+    )
+    .replace(
+      `'babel-jest'`,
+      `['babel-jest', { presets: ['@nrwl/react/babel'] }]`
+    );
 }
