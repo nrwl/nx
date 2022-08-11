40487af36d668e4b1499f5b85edec4b3933d9db3
Jack Hsu <jack.hsu@gmail.com>
2022-08-11 09:52:07 -0400

    feat(node): update @types/node to 18.7.1

--- packages/node/src/utils/versions.ts
+++ packages/node/src/utils/versions.ts
@@ -2,4 +2,4 @@ export const nxVersion = require('../../package.json').version;
 
 export const tslibVersion = '^2.3.0';
 
-export const typesNodeVersion = '18.0.0';
+export const typesNodeVersion = '18.7.1';
