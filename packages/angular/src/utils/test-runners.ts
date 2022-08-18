28c85a804ea28e1eac5ddb0fcad6fdd6e0870db6
Colum Ferry <cferry09@gmail.com>
2022-08-18 10:29:23 +0100

    feat(angular): export test runners from package

--- packages/angular/src/utils/test-runners.ts
+++ packages/angular/src/utils/test-runners.ts
@@ -1,9 +1,9 @@
-export const enum UnitTestRunner {
+export enum UnitTestRunner {
   Karma = 'karma',
   Jest = 'jest',
   None = 'none',
 }
-export const enum E2eTestRunner {
+export enum E2eTestRunner {
   /**
    * @deprecated Protractor is no longer maintained. Support for generating
    * E2E tests with it will be removed in v15.
