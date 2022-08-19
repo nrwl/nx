6866f55c4a03b5283125565d21509b64e5e6050b
Caleb Ukle <caleb@nrwl.io>
2022-08-19 15:56:52 -0500

    chore(repo): run jest 28 migrations to fix package sync issue (#11650)

--- packages/cypress/src/plugins/preprocessor.spec.ts
+++ packages/cypress/src/plugins/preprocessor.spec.ts
@@ -1,4 +1,4 @@
-import { mocked } from 'ts-jest/utils';
+import { mocked } from 'jest-mock';
 import { getWebpackConfig, preprocessTypescript } from './preprocessor';
 jest.mock('@cypress/webpack-preprocessor', () => {
   return jest.fn(
