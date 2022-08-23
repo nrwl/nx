1bf6d2854ccbe49d1495a54911f6aec5b6952112
ben <3447705+bcabanes@users.noreply.github.com>
2022-08-23 10:51:48 -0400

    docs(nxdev): add announcement banner for nx conf

--- nx-dev/ui-common/src/lib/announcement-banner.tsx
+++ nx-dev/ui-common/src/lib/announcement-banner.tsx
@@ -1,34 +1,32 @@
+import Link from 'next/link';
+
 export function AnnouncementBanner(): JSX.Element {
   return (
-    <div className="bg-slate-800">
+    <div className="border-b border-slate-900/50 bg-slate-800">
       <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
         <div className="text-center sm:px-16 sm:pr-16">
           <p className="text-sm font-medium text-white">
             <span className="md:hidden">
-              <a
-                href="https://ti.to/nrwl/nx-workshop-7-2022/?utm_source=nxdev-announcement-banner"
-                rel="noreferrer"
-                target="_blank"
-                className="text-white underline"
-              >
-                A new Nx workshop is coming on July 7th and 8th!
-              </a>
+              <Link href="/conf?utm_source=announcement-banner">
+                <a className="text-white underline">
+                  Nx Conf on October 17th at Phoenix AZ!
+                </a>
+              </Link>
             </span>
             <span className="hidden md:inline">
-              To your agenda! A new Nx workshop will happen on July 7th and 8th,
-              an online workshop to know how to Develop at Scale with Nx
-              Monorepos!
+              <span className="font-black">
+                Nx Conf on October 17th at Phoenix AZ
+              </span>
+              , do not miss out!
             </span>
             <span className="ml-2 inline-block">
-              <a
-                href="https://ti.to/nrwl/nx-workshop-7-2022/?utm_source=nxdev-announcement-banner"
-                rel="noreferrer"
-                target="_blank"
-                className="font-bold text-white underline"
-              >
-                {' '}
-                Schedule and more info <span aria-hidden="true">&rarr;</span>
-              </a>
+              <Link href="/conf?utm_source=announcement-banner">
+                <a className="font-semibold text-white underline">
+                  {' '}
+                  Tickets, Speakers, Schedule and more{' '}
+                  <span aria-hidden="true">&rarr;</span>
+                </a>
+              </Link>
             </span>
           </p>
         </div>
