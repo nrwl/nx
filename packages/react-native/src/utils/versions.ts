07e524c2e305d790545822ee05ff929f515d529d
Emily Xiong <xiongemi@gmail.com>
2022-08-23 15:37:57 -0400

    feat(react-native): upgrade metro to 0.72.1

--- packages/react-native/src/utils/versions.ts
+++ packages/react-native/src/utils/versions.ts
@@ -5,11 +5,11 @@ export const typesReactNativeVersion = '0.69.5';
 
 export const typesNodeVersion = '16.11.7';
 
-export const metroVersion = '0.72.0';
+export const metroVersion = '0.72.1';
 
-export const reactNativeCommunityCli = '8.0.5';
-export const reactNativeCommunityCliIos = '8.0.4';
-export const reactNativeCommunityCliAndroid = '8.0.5';
+export const reactNativeCommunityCli = '9.0.0';
+export const reactNativeCommunityCliIos = '9.0.0';
+export const reactNativeCommunityCliAndroid = '9.0.0';
 
 export const reactNativeConfigVersion = '1.4.6';
 export const reactNativeAsyncStorageAsyncStorageVersion = '1.17.7';
@@ -20,6 +20,6 @@ export const testingLibraryJestNativeVersion = '4.0.11';
 export const jestReactNativeVersion = '18.0.0';
 
 export const reactNativeSvgTransformerVersion = '1.0.0';
-export const reactNativeSvgVersion = '12.4.4';
+export const reactNativeSvgVersion = '13.0.0';
 
 export const babelRuntimeVersion = '7.18.9';
