f53d856e5fa5f958a52c02c8bb6c80a6627f1f3b
ben <3447705+bcabanes@users.noreply.github.com>
2022-08-23 10:31:26 -0400

    docs(nxdev): add pulse dot animation on nxconf header menu

--- nx-dev/ui-common/src/lib/header.tsx
+++ nx-dev/ui-common/src/lib/header.tsx
@@ -116,10 +116,10 @@ export function Header(props: HeaderProps): JSX.Element {
                 title="Check Nx conference"
                 className="relative hidden px-3 py-2 leading-tight text-white md:inline-flex"
               >
-                {/*<span className="absolute top-0 right-0 -mt-1 -mr-1 flex h-3 w-3">*/}
-                {/*  <span className="bg-green-nx-base absolute inline-flex h-full w-full animate-ping rounded-full opacity-75" />*/}
-                {/*  <span className="bg-green-nx-base relative inline-flex h-3 w-3 rounded-full" />*/}
-                {/*</span>*/}
+                <span className="absolute top-0 right-0 -mt-1 -mr-1 flex h-3 w-3">
+                  <span className="bg-green-nx-base absolute inline-flex h-full w-full animate-ping rounded-full opacity-75" />
+                  <span className="bg-green-nx-base relative inline-flex h-3 w-3 rounded-full" />
+                </span>
                 Nx Conf
               </a>
             </Link>
