ef5334bc3cdd5edf30671f4d8953a31b0560549a
Jack Hsu <jack.hsu@gmail.com>
2022-08-26 12:16:55 -0400

    Revert "feat(core): support reading cli args from env (#11721)" (#11738)
    This reverts commit d9b9bd138ae2e38334b0e1a3fa13b61d4dfea695.

--- packages/nx/src/command-line/nx-commands.spec.ts
+++ packages/nx/src/command-line/nx-commands.spec.ts
@@ -1,16 +1,6 @@
 import { commandsObject } from 'nx/src/command-line/nx-commands';
 
 describe('nx-commands', () => {
-  const OLD_ENV = process.env;
-
-  beforeEach(() => {
-    jest.resetModules();
-  });
-
-  afterAll(() => {
-    process.env = OLD_ENV;
-  });
-
   it('should parse dot notion cli args', () => {
     const actual = commandsObject.parse(
       'nx e2e project-e2e --env.NX_API_URL=http://localhost:4200 --abc.123.xyx=false --a.b=3'
@@ -28,28 +18,6 @@ describe('nx-commands', () => {
         env: {
           NX_API_URL: 'http://localhost:4200',
         },
-      })
-    );
-  });
-
-  it('should support args from the environment', () => {
-    process.env.NX_DRY_RUN = 'true';
-    process.env.NX_SKIP_NX_CACHE = 'false';
-    const actual = commandsObject.parse('nx run project-e2e');
-    expect(actual).toEqual(
-      expect.objectContaining({
-        dryRun: 'true',
-        skipNxCache: 'false',
-      })
-    );
-  });
-
-  it('CLI args should take precedence over args from the environment', () => {
-    process.env.NX_DRY_RUN = 'true';
-    const actual = commandsObject.parse('nx run project-e2e --dryRun=false');
-    expect(actual).toEqual(
-      expect.objectContaining({
-        dryRun: 'false',
       })
     );
   });
