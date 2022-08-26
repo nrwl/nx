1daaf9592d3ca906fc2139b049a44bd44e21dd8d
AgentEnder <craigorycoppola@gmail.com>
2022-08-26 09:47:30 -0400

    fix(devkit): split-create-tree should use nx cli
    fixes: #11710

--- packages/devkit/migrations.spec.ts
+++ packages/devkit/migrations.spec.ts
@@ -1,7 +1,7 @@
 import path = require('path');
 import json = require('./migrations.json');
 
-const migrations = Object.entries(json.schematics);
+const migrations = Object.entries(json.generators);
 
 describe('devkit migrations', () => {
   it.each(migrations)('should have valid path: %s', (key, m) => {
