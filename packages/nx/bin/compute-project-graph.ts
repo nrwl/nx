a3126116826b4bc8d492341e2b2b02daa4b2195e
Victor Savkin <mail@vsavkin.com>
2022-08-29 17:10:30 -0400

    feat(misc): add task hashes to the output of print affected (#11763)
    * feat(misc): add task hashes to the output of print affected
    
    * bump up version
    
    * fix(core): postinstall should build project graph without daemon

--- packages/nx/bin/compute-project-graph.ts
+++ packages/nx/bin/compute-project-graph.ts
@@ -1,12 +1,16 @@
-import { createProjectGraphAsync } from '../src/project-graph/project-graph';
+import { buildProjectGraphWithoutDaemon } from '../src/project-graph/project-graph';
 import { workspaceRoot } from '../src/utils/workspace-root';
 import { fileExists } from '../src/utils/fileutils';
 import { join } from 'path';
+import { isServerAvailable, stop } from '../src/daemon/client/client';
 
 (async () => {
   try {
     if (fileExists(join(workspaceRoot, 'nx.json'))) {
-      await createProjectGraphAsync();
+      if (await isServerAvailable()) {
+        await stop();
+      }
+      await buildProjectGraphWithoutDaemon();
     }
   } catch (e) {
     // Do not error since this runs in a postinstall
