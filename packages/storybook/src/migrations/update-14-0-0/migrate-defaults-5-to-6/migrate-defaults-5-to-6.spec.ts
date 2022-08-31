835d0ef7090031bd92c9e9a0f6b252aa3e71a182
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2022-08-31 16:21:30 +0300

    fix(storybook): ignore addon-postcss from version update (#11801)
    ISSUES CLOSED: #11465

--- packages/storybook/src/migrations/update-14-0-0/migrate-defaults-5-to-6/migrate-defaults-5-to-6.spec.ts
+++ packages/storybook/src/migrations/update-14-0-0/migrate-defaults-5-to-6/migrate-defaults-5-to-6.spec.ts
@@ -23,6 +23,7 @@ describe('migrate-defaults-5-to-6 Generator', () => {
           '@storybook/addon-knobs': '^5.3.8',
           '@storybook/angular': '^5.3.8',
           '@storybook/addon-notes': '5.3.21',
+          '@storybook/addon-postcss': '2.0.0',
         },
       };
     });
@@ -73,6 +74,9 @@ describe('migrate-defaults-5-to-6 Generator', () => {
     );
     expect(packageJson.devDependencies['@storybook/addon-notes']).toEqual(
       '5.3.21'
+    );
+    expect(packageJson.devDependencies['@storybook/addon-postcss']).toEqual(
+      '2.0.0'
     );
   });
 
