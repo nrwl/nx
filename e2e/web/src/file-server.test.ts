bcbf76b8b82c5e1e0a55851782a55ddaab2d340b
Miroslav Jonaš <meeroslav@users.noreply.github.com>
2022-09-19 21:19:35 +0200

    fix(web): add cleanup on e2e tests (#12098)

--- e2e/web/src/file-server.test.ts
+++ e2e/web/src/file-server.test.ts
@@ -1,4 +1,5 @@
 import {
+  cleanupProject,
   killPorts,
   newProject,
   promisifiedTreeKill,
@@ -9,6 +10,8 @@ import {
 } from '@nrwl/e2e/utils';
 
 describe('file-server', () => {
+  afterEach(() => cleanupProject());
+
   it('should serve folder of files', async () => {
     newProject({ name: uniq('fileserver') });
     const appName = uniq('app');
