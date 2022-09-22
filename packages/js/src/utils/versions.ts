3287d5f1a76c98cd0bf92056b653018382d7471d
Jack Hsu <jack.hsu@gmail.com>
2022-09-22 10:03:19 -0400

    fix(js): update library generator for webpack and rollup bundler

--- packages/js/src/utils/versions.ts
+++ packages/js/src/utils/versions.ts
@@ -4,3 +4,4 @@ export const nxVersion = require('../../package.json').version;
 
 export const swcCliVersion = '~0.1.55';
 export const swcHelpersVersion = '~0.3.3';
+export const typesNodeVersion = '18.7.1';
