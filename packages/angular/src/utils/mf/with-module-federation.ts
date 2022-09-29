nrwl/nx

commit a29946992501aba7c3a70b7e411d511aa51e4dfc

feat(angular): support alternate remote entry filenames #10206 (#10432)

packages/react/src/module-federation/with-module-federation.ts
@@ -23,8 +23,9 @@ import {
   SharedFunction,
   SharedLibraryConfig,
 } from './models';
-import ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 import { readRootPackageJson } from './package-json';
+import { extname, join } from 'path';
+import ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 function collectDependencies(
   projectGraph: ProjectGraph,
@@ -131,14 +132,10 @@ function mapRemotes(remotes: Remotes) {
   for (const remote of remotes) {
     if (Array.isArray(remote)) {
       let [remoteName, remoteLocation] = remote;
-      if (!remoteLocation.match(/remoteEntry\.(js|mjs)$/)) {
-        remoteLocation = `${
-          remoteLocation.endsWith('/')
-            ? remoteLocation.slice(0, -1)
-            : remoteLocation
-        }/remoteEntry.js`;
-      }
-      mappedRemotes[remoteName] = remoteLocation;
+      const remoteLocationExt = extname(remoteLocation);
+      mappedRemotes[remoteName] = ['.js', '.mjs'].includes(remoteLocationExt)
+        ? remoteLocation
+        : join(remoteLocation, 'remoteEntry.js');
     } else if (typeof remote === 'string') {
       mappedRemotes[remote] = determineRemoteUrl(remote);
     }

packages/angular/src/utils/mfe/with-module-federation.ts
@@ -18,6 +18,7 @@ import {
 import { ParsedCommandLine } from 'typescript';
 import { readWorkspaceJson } from 'nx/src/project-graph/file-utils';
 import { readRootPackageJson } from './utils';
+import { extname, join } from 'path';
 import ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 export type MFERemotes = string[] | [remoteName: string, remoteUrl: string][];
@@ -142,12 +143,11 @@ function mapRemotes(remotes: MFERemotes) {
 
   for (const remote of remotes) {
     if (Array.isArray(remote)) {
-      const remoteLocation = remote[1].match(/remoteEntry\.(js|mjs)/)
-        ? remote[1]
-        : `${
-            remote[1].endsWith('/') ? remote[1].slice(0, -1) : remote[1]
-          }/remoteEntry.mjs`;
-      mappedRemotes[remote[0]] = remoteLocation;
+      const [remoteName, remoteLocation] = remote;
+      const remoteLocationExt = extname(remoteLocation);
+      mappedRemotes[remoteName] = ['.js', '.mjs'].includes(remoteLocationExt)
+        ? remoteLocation
+        : join(remoteLocation, 'remoteEntry.mjs');
     } else if (typeof remote === 'string') {
       mappedRemotes[remote] = determineRemoteUrl(remote);
     }

packages/react/src/executors/module-federation-dev-server/module-federation-dev-server.impl.ts
@@ -44,12 +44,13 @@ export default async function* moduleFederationDevServer(
     : [options.devRemotes];
 
   for (const app of knownRemotes) {
-    const isDev = devServeApps.includes(app);
+    const [appName] = Array.isArray(app) ? app : [app];
+    const isDev = devServeApps.includes(appName);
     iter = combineAsyncIterators(
       iter,
       await runExecutor(
         {
-          project: app,
+          project: appName,
           target: isDev ? 'serve' : 'serve-static',
           configuration: context.configurationName,
         },

commit 754ed15d3fdcd7a56ca7c18a8616907c9003e3ae

fix(webpack): regression from #10432 to allow `//` prefix in remote

packages/angular/src/utils/mf/with-module-federation.ts
@@ -4,7 +4,7 @@ import {
   readCachedProjectGraph,
 } from '@nrwl/devkit';
 import { readCachedProjectConfiguration } from 'nx/src/project-graph/project-graph';
-import { extname, join } from 'path';
+import { extname } from 'path';
 import {
   getNpmPackageSharedConfig,
   SharedLibraryConfig,
@@ -59,7 +59,11 @@ function mapRemotes(remotes: MFRemotes) {
       const remoteLocationExt = extname(remoteLocation);
       mappedRemotes[remoteName] = ['.js', '.mjs'].includes(remoteLocationExt)
         ? remoteLocation
-        : join(remoteLocation, 'remoteEntry.mjs');
+        : `${
+            remoteLocation.endsWith('/')
+              ? remoteLocation.slice(0, -1)
+              : remoteLocation
+          }/remoteEntry.mjs`;
     } else if (typeof remote === 'string') {
       mappedRemotes[remote] = determineRemoteUrl(remote);
     }

packages/react/src/module-federation/with-module-federation.ts
@@ -22,7 +22,7 @@ import {
   SharedLibraryConfig,
 } from './models';
 import { readRootPackageJson } from './package-json';
-import { extname, join } from 'path';
+import { extname } from 'path';
 import ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 function collectDependencies(
@@ -133,7 +133,11 @@ function mapRemotes(remotes: Remotes, projectGraph: ProjectGraph) {
       const remoteLocationExt = extname(remoteLocation);
       mappedRemotes[remoteName] = ['.js', '.mjs'].includes(remoteLocationExt)
         ? remoteLocation
-        : join(remoteLocation, 'remoteEntry.js');
+        : `${
+            remoteLocation.endsWith('/')
+              ? remoteLocation.slice(0, -1)
+              : remoteLocation
+          }/remoteEntry.js`;
     } else if (typeof remote === 'string') {
       mappedRemotes[remote] = determineRemoteUrl(remote, projectGraph);
     }