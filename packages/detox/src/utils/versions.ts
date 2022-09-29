9ce0e24ca906ed70586992e3a2f80c024ce17f81
Emily Xiong <xiongemi@gmail.com>
2022-09-29 15:50:40 -0400

    feat(detox): upgrade detox to 19.2.5

--- packages/detox/src/utils/versions.ts
+++ packages/detox/src/utils/versions.ts
@@ -1,4 +1,4 @@
 export const nxVersion = require('../../package.json').version;
 
-export const detoxVersion = '19.12.1';
+export const detoxVersion = '19.12.5';
 export const testingLibraryJestDom = '5.16.5';
