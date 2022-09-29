27037cbaeddf43aa584e3d9db9408e3b3452a03a
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2022-09-29 12:35:27 +0300

    fix(storybook): check root mainjs for builder (#12308)

--- packages/storybook/src/executors/utils.spec.ts
+++ packages/storybook/src/executors/utils.spec.ts
@@ -1,15 +1,11 @@
 import * as ts from 'typescript';
 import { stripIndents } from '@nrwl/devkit';
-import { findBuilderInMainJsTs } from './utils';
-import { logger } from '@nrwl/devkit';
+import { builderIsWebpackButNotWebpack5 } from './utils';
 
 describe('testing utilities', () => {
-  beforeEach(() => {
-    jest.spyOn(logger, 'warn');
-  });
-
-  it('should not log the webpack5 warning if builder is webpack5', () => {
-    const sourceCode = stripIndents`
+  describe('builderIsWebpackButNotWebpack5', () => {
+    it('should return false if builder is webpack5', () => {
+      const sourceCode = stripIndents`
     const rootMain = require('../../../.storybook/main');
 
     module.exports = {
@@ -18,19 +14,18 @@ describe('testing utilities', () => {
     };    
     `;
 
-    const source = ts.createSourceFile(
-      '.storybook/main.js',
-      sourceCode,
-      ts.ScriptTarget.Latest,
-      true
-    );
+      const source = ts.createSourceFile(
+        '.storybook/main.js',
+        sourceCode,
+        ts.ScriptTarget.Latest,
+        true
+      );
 
-    findBuilderInMainJsTs(source);
-    expect(logger.warn).not.toHaveBeenCalled();
-  });
+      expect(builderIsWebpackButNotWebpack5(source)).toBeFalsy();
+    });
 
-  it('should not log the webpack5 warning if builder is @storybook/webpack5', () => {
-    const sourceCode = stripIndents`
+    it('should return false if builder is @storybook/webpack5', () => {
+      const sourceCode = stripIndents`
     const rootMain = require('../../../.storybook/main');
 
     module.exports = {
@@ -39,19 +34,18 @@ describe('testing utilities', () => {
     };    
     `;
 
-    const source = ts.createSourceFile(
-      '.storybook/main.js',
-      sourceCode,
-      ts.ScriptTarget.Latest,
-      true
-    );
+      const source = ts.createSourceFile(
+        '.storybook/main.js',
+        sourceCode,
+        ts.ScriptTarget.Latest,
+        true
+      );
 
-    findBuilderInMainJsTs(source);
-    expect(logger.warn).not.toHaveBeenCalled();
-  });
+      expect(builderIsWebpackButNotWebpack5(source)).toBeFalsy();
+    });
 
-  it('should not log the webpack5 warning if builder exists but does not contain webpack', () => {
-    const sourceCode = stripIndents`
+    it('should return false if builder exists but does not contain webpack', () => {
+      const sourceCode = stripIndents`
     const rootMain = require('../../../.storybook/main');
 
     module.exports = {
@@ -60,19 +54,18 @@ describe('testing utilities', () => {
     };    
     `;
 
-    const source = ts.createSourceFile(
-      '.storybook/main.js',
-      sourceCode,
-      ts.ScriptTarget.Latest,
-      true
-    );
+      const source = ts.createSourceFile(
+        '.storybook/main.js',
+        sourceCode,
+        ts.ScriptTarget.Latest,
+        true
+      );
 
-    findBuilderInMainJsTs(source);
-    expect(logger.warn).not.toHaveBeenCalled();
-  });
+      expect(builderIsWebpackButNotWebpack5(source)).toBeFalsy();
+    });
 
-  it('should log the webpack5 warning if builder is webpack4', () => {
-    const sourceCode = stripIndents`
+    it('should return true if builder is webpack4', () => {
+      const sourceCode = stripIndents`
     const rootMain = require('../../../.storybook/main');
 
     module.exports = {
@@ -81,19 +74,18 @@ describe('testing utilities', () => {
     };    
     `;
 
-    const source = ts.createSourceFile(
-      '.storybook/main.js',
-      sourceCode,
-      ts.ScriptTarget.Latest,
-      true
-    );
+      const source = ts.createSourceFile(
+        '.storybook/main.js',
+        sourceCode,
+        ts.ScriptTarget.Latest,
+        true
+      );
 
-    findBuilderInMainJsTs(source);
-    expect(logger.warn).toHaveBeenCalled();
-  });
+      expect(builderIsWebpackButNotWebpack5(source)).toBeTruthy();
+    });
 
-  it('should log the webpack5 warning if builder does not exist', () => {
-    const sourceCode = stripIndents`
+    it('should return true if builder does not exist because default is webpack', () => {
+      const sourceCode = stripIndents`
     const rootMain = require('../../../.storybook/main');
 
     module.exports = {
@@ -101,14 +93,14 @@ describe('testing utilities', () => {
     };    
     `;
 
-    const source = ts.createSourceFile(
-      '.storybook/main.js',
-      sourceCode,
-      ts.ScriptTarget.Latest,
-      true
-    );
+      const source = ts.createSourceFile(
+        '.storybook/main.js',
+        sourceCode,
+        ts.ScriptTarget.Latest,
+        true
+      );
 
-    findBuilderInMainJsTs(source);
-    expect(logger.warn).toHaveBeenCalled();
+      expect(builderIsWebpackButNotWebpack5(source)).toBeFalsy();
+    });
   });
 });
