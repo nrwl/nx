e2fdb1f4a204de199f24f6b5b807b53b6b6a70cc
Juri <juri.strumpflohner@gmail.com>
2022-09-30 18:26:53 +0200

    docs(nxdev): update conf schedule timing

--- nx-dev/ui-conference/src/lib/conf-schedule-short.tsx
+++ nx-dev/ui-conference/src/lib/conf-schedule-short.tsx
@@ -42,7 +42,7 @@ export function ConfScheduleShort(): JSX.Element {
   const scheduleItemsForMonday17: ScheduleItem[] = [
     {
       type: 'event',
-      time: '10:00 - 10:30am (UTC-07:00)',
+      time: '10:00 - 10:25am (UTC-07:00)',
       title: 'Keynote - Nx and Lerna: Integrated vs Package-Based Monorepos',
       description:
         'The JS community has two main styles of monorepos: the “package-based” approach (popularized by Lerna) and the “integrated” style (popularized by Nx, used by Google, Facebook etc.). While one is lightweight and easy to adopt, the other gives much more flexibility, scales better, and provides better dev ergonomics but requires a mind shift.\n' +
@@ -52,7 +52,7 @@ export function ConfScheduleShort(): JSX.Element {
     },
     {
       type: 'event',
-      time: '10:30 - 11:00am (UTC-07:00)',
+      time: '10:30 - 10:55am (UTC-07:00)',
       title: 'Re-thinking CI bottlenecks with Distributed Execution',
       description:
         'Nx’s powerful caching algorithms and dependency graph knowledge allow you to build simple CI pipelines that scale as your workspace grows. But caching and affected work best in the average case: when only some projects are changed in a PR, you can skip anything that can’t be affected by that change, and from the remaining ones the caching algorithms will help you replay all the work that has been done before. But after a certain point you have to start splitting the work across multiple agents if you want your workspace to scale.      In this talk we’ll explore why your CI can slow down in big workspaces, and how you can unlock horizontal scaling by distributing your tasks across multiple machines. We’ll look at some of the issues with task distribution and how you can use NxCloud workflows to implement scalable CI configs that optimally splits the work across your matrix of agents.',
@@ -61,7 +61,7 @@ export function ConfScheduleShort(): JSX.Element {
     },
     {
       type: 'event',
-      time: '11:00 - 11:30am (UTC-07:00)',
+      time: '11:00 - 11:25am (UTC-07:00)',
       title: 'Lerna Reborn',
       description:
         "Lerna blazed the original trail in the open-source JavaScript monorepo space, and the whole ecosystem has benefitted immensely from that experience. It remains extremely popular for versioning/publishing and full monorepo workspace management, and now that the Nrwl Team has taken over stewardship of the project, we want to share some of the great updates we've made over the last few months as well as what we have planned for the near future.",
@@ -70,7 +70,7 @@ export function ConfScheduleShort(): JSX.Element {
     },
     {
       type: 'break',
-      time: '11:30 - 11:45am (UTC-07:00)',
+      time: '11:30 - 11:40am (UTC-07:00)',
       title: 'Break',
       description: '',
       speakers: [],
@@ -78,7 +78,7 @@ export function ConfScheduleShort(): JSX.Element {
     },
     {
       type: 'event',
-      time: '11:45 - 12:15pm (UTC-07:00)',
+      time: '11:45 - 12:10pm (UTC-07:00)',
       title: 'Nx + Cypress Like Peanut Butter & Jelly',
       description:
         "Name a more iconic duo than Nx & Cypress... I'll wait. In this talk I will highlight some of ways in which Nx integrates seamlessly with Cypress and how it can speed up both your development and overall developer experience. We will also take a look at Cypress Component Testing and how you can leverage Nx in your testing. By the end you will see why Nx and Cypress are the peanut butter and jelly of your developer toolbox.",
@@ -122,7 +122,7 @@ export function ConfScheduleShort(): JSX.Element {
     },
     {
       type: 'event',
-      time: '2:00 - 2:30pm (UTC-07:00)',
+      time: '2:00 - 2:25pm (UTC-07:00)',
       title: 'Extending Nx: Polyglot Superpowers',
       description:
         "Extensibility is in Nx's DNA, and having a tool that you can extend and customize to your monorepo setup is crucial. Sharing different languages in a monorepo is extremely common yet toolchains usually don't play well together. Fortunately, Nx allows you to extend its capabilities to integrate your whole stack. Use Nx's graph on your Go or Python code and see what is affected, or generate Go code via Nx. Make Nx polyglot to take advantage of affected tests and remote caching for all your tech stack.",
@@ -131,7 +131,7 @@ export function ConfScheduleShort(): JSX.Element {
     },
     {
       type: 'event',
-      time: '2:30 - 3:00pm (UTC-07:00)',
+      time: '2:30 - 2:55pm (UTC-07:00)',
       title: 'Nx, Ionic, Appflow: Navigating the Wild West of Hybrid Apps',
       description:
         'These days there are a lot of ways to build mobile apps. But what if you want to share your types across the frontend and backend, or share components and logic across your mobile and web app, and maybe an admin panel? At this point, your options get pretty limited, and things only get more complicated once you start thinking about how to automate building and deploying your native apps to the app store. In this talk, we’ll explore how to develop Ionic applications in an Nx workspace and how to configure Appflow for automated app store deployments.',
@@ -140,7 +140,7 @@ export function ConfScheduleShort(): JSX.Element {
     },
     {
       type: 'event',
-      time: '3:00 - 3:30pm (UTC-07:00)',
+      time: '3:00 - 3:25pm (UTC-07:00)',
       title: 'Nx at Celonis',
       description:
         'Celonis adopted AngularJS in 2011, 5 years later in 2016 we had fewer applications to manage, and individual teams were responsible for each application, soon we found there are many issues with each team owning the code, we are duplicating some code, some teams dont have frontend developers and are sometimes dependent on each other is delaying the development workflow. Celonis decided to introduce the project "One Frontend" and we chose Nx and use Module Federation, join us to learn about the journey of Celonis migrating to Nx, why we chose Nx, and how CLI made our life easier, and what Nx\'s future is at Celonis.',
@@ -149,7 +149,7 @@ export function ConfScheduleShort(): JSX.Element {
     },
     {
       type: 'event',
-      time: '3:30 - 4:00pm (UTC-07:00)',
+      time: '3:30 - 3:55pm (UTC-07:00)',
       title: 'Make Nx Work for You with Custom Generators',
       description:
         'There is a lot the Nx can do for your application right out of the box, but did you know you can write custom workspace generators specific to YOUR application to help automate the various tasks your team performs on a daily basis? Why write yet another confluence page documenting all of the little things that have to be done after generating a library when you can write a workspace generator that handles all of the little details for you? In this talk we will cover everything you need to know to create your own workspace generator to help reduce manual tasks, enforce architectural patterns, and generate more consistent predictable code.',
@@ -166,7 +166,7 @@ export function ConfScheduleShort(): JSX.Element {
     },
     {
       type: 'event',
-      time: '4:15 - 4:45pm (UTC-07:00)',
+      time: '4:15 - 4:40pm (UTC-07:00)',
       title: 'The power of Nx Console',
       description:
         'Are you tired of searching the docs for the exact name of that Nx command flag? Or do you want to explore what’s possible but don’t know where to start? Nx isn’t just fast! We deeply care about the developer experience too! Every command, with all its options, running tasks, and the Nx graph. Just fingertips away so you can keep focused and remain in the flow. In this talk, we’re going deep into how to augment your Nx experience in VSCode! Not a VSCode user? Then be all ears: we might have a surprise.',
