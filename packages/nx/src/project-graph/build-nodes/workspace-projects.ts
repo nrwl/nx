nrwl/nx

commit 9b63ce167acb08faaf55bdf899055ae04a87036f

feat(core): don't generate workspace.json for v2 workspaces (#12127)

packages/nx/src/project-graph/build-nodes/workspace-projects.ts
@@ -50,7 +50,7 @@ export function buildWorkspaceProjectNodes(
     p.targets = mergePluginTargetsWithNxTargets(
       p.root,
       p.targets,
-      loadNxPlugins(ctx.workspace.plugins)
+      loadNxPlugins(ctx.workspace.plugins, [p.root], p.root)
     );
 
     const projectType =

commit ea540853a35c4abcab5446f5d53950aba693f43d

fix(core): js plugin files should work
JS file plugins were broken by #12127

Fixes #12340

packages/nx/src/project-graph/build-nodes/workspace-projects.ts
@@ -50,7 +50,7 @@ export function buildWorkspaceProjectNodes(
     p.targets = mergePluginTargetsWithNxTargets(
       p.root,
       p.targets,
-      loadNxPlugins(ctx.workspace.plugins, [p.root], p.root)
+      loadNxPlugins(ctx.workspace.plugins)
     );
 
     const projectType =