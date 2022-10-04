09e524a01a0859900f51808d35a2c58e6c6f370d
Jack Hsu <jack.hsu@gmail.com>
2022-10-04 12:32:43 -0400

    cleanup(bundling): clean up webpack root exports (#12403)

--- packages/webpack/index.ts
+++ packages/webpack/index.ts
@@ -1,7 +1,12 @@
 export * from './src/utils/config';
 export * from './src/generators/webpack-project/webpack-project';
-export * from './src/executors/dev-server/schema';
+export type { WebDevServerOptions } from './src/executors/dev-server/schema';
 export * from './src/executors/dev-server/dev-server.impl';
 export * from './src/executors/webpack/lib/normalize-options';
-export * from './src/executors/webpack/schema';
+export type {
+  WebpackExecutorOptions,
+  NormalizedWebpackExecutorOptions,
+  AssetGlobPattern,
+  FileReplacement,
+} from './src/executors/webpack/schema';
 export * from './src/executors/webpack/webpack.impl';
