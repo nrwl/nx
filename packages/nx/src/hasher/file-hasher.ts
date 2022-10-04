e5f559a39f57ea9df7922b4f08c087a3b6a6440a
Victor Savkin <mail@vsavkin.com>
2022-10-04 10:05:06 -0400

    fix(core): use node based hasher in git repos with submodules

--- packages/nx/src/hasher/file-hasher.ts
+++ packages/nx/src/hasher/file-hasher.ts
@@ -3,6 +3,8 @@ import { workspaceRoot } from '../utils/workspace-root';
 import { NodeBasedFileHasher } from './node-based-file-hasher';
 import { FileHasherBase } from './file-hasher-base';
 import { execSync } from 'child_process';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 function createFileHasher(): FileHasherBase {
   // special case for unit tests
@@ -11,7 +13,12 @@ function createFileHasher(): FileHasherBase {
   }
   try {
     execSync('git rev-parse --is-inside-work-tree', { stdio: 'ignore' });
-    return new GitBasedFileHasher();
+    // we don't use git based hasher when the repo uses git submodules
+    if (!existsSync(join(workspaceRoot, '.git', 'modules'))) {
+      return new GitBasedFileHasher();
+    } else {
+      return new NodeBasedFileHasher();
+    }
   } catch {
     return new NodeBasedFileHasher();
   }
