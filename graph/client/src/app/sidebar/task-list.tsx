080a8961d88456b39857d0d23ac8b51f8e0fda5e
Philip Fulcher <philip@nrwl.io>
2022-10-08 15:58:32 -0600

    chore(graph): add components to support task graph (#12472)

---
+++ graph/client/src/app/sidebar/task-list.tsx
@@ -0,0 +1,277 @@
+import { DocumentMagnifyingGlassIcon } from '@heroicons/react/24/solid';
+// nx-ignore-next-line
+import type { ProjectGraphNode, Task } from '@nrwl/devkit';
+import { parseParentDirectoriesFromFilePath } from '../util';
+import { WorkspaceLayout } from '../interfaces';
+import Tag from '../ui-components/tag';
+
+function getProjectsByType(type: string, projects: ProjectGraphNode[]) {
+  return projects
+    .filter((project) => project.type === type)
+    .sort((a, b) => a.name.localeCompare(b.name));
+}
+
+interface SidebarTarget {
+  targetName: string;
+  configurations: Array<{
+    name: string;
+    isSelected: boolean;
+    default: boolean;
+  }>;
+}
+
+interface SidebarProjectWithTargets {
+  projectGraphNode: ProjectGraphNode;
+  targets: SidebarTarget[];
+}
+
+function groupProjectsByDirectory(
+  projects: ProjectGraphNode[],
+  workspaceLayout: { appsDir: string; libsDir: string }
+): Record<string, ProjectGraphNode[]> {
+  let groups: Record<string, ProjectGraphNode[]> = {};
+
+  projects.forEach((project) => {
+    const workspaceRoot =
+      project.type === 'app' || project.type === 'e2e'
+        ? workspaceLayout.appsDir
+        : workspaceLayout.libsDir;
+    const directories = parseParentDirectoriesFromFilePath(
+      project.data.root,
+      workspaceRoot
+    );
+
+    const directory = directories.join('/');
+
+    if (!groups.hasOwnProperty(directory)) {
+      groups[directory] = [];
+    }
+    groups[directory].push(project);
+  });
+
+  return groups;
+}
+
+function ProjectListItem({
+  project,
+  selectTask,
+}: {
+  project: SidebarProjectWithTargets;
+  selectTask: (
+    projectName: string,
+    targetName: string,
+    configurationName: string
+  ) => void;
+}) {
+  return (
+    <li className="relative block cursor-default select-none py-1 pl-2 pr-6 text-xs text-slate-600 dark:text-slate-400">
+      <strong>{project.projectGraphNode.name}</strong>
+      <br />
+      {project.targets.map((target) => (
+        <>
+          <strong>{target.targetName}</strong>
+          <br />
+          {target.configurations.map((configuration) => (
+            <div className="flex items-center">
+              <button
+                data-cy={`focus-button-${configuration.name}`}
+                type="button"
+                className="mr-1 flex items-center rounded-md border-slate-300 bg-white p-1 font-medium text-slate-500 shadow-sm ring-1 ring-slate-200 transition hover:bg-slate-50 dark:border-slate-600 dark:bg-slate-800 dark:text-slate-400 dark:ring-slate-600 hover:dark:bg-slate-700"
+                title="Focus on this library"
+                onClick={() =>
+                  selectTask(
+                    project.projectGraphNode.name,
+                    target.targetName,
+                    configuration.name
+                  )
+                }
+              >
+                <DocumentMagnifyingGlassIcon className="h-5 w-5" />
+              </button>
+
+              <label
+                className="ml-2 block w-full cursor-pointer truncate rounded-md p-2 font-mono font-normal transition hover:bg-slate-50 hover:dark:bg-slate-700"
+                data-project={configuration.name}
+                title={configuration.name}
+                data-active={configuration.isSelected.toString()}
+              >
+                {configuration.name}
+              </label>
+
+              {configuration.default ? <Tag>default</Tag> : null}
+            </div>
+          ))}
+        </>
+      ))}
+    </li>
+  );
+}
+
+function SubProjectList({
+  headerText = '',
+  projects,
+  selectTask,
+}: {
+  headerText: string;
+  projects: SidebarProjectWithTargets[];
+  selectTask: (
+    projectName: string,
+    targetName: string,
+    configurationName: string
+  ) => void;
+}) {
+  let sortedProjects = [...projects];
+  sortedProjects.sort((a, b) => {
+    return a.projectGraphNode.name.localeCompare(b.projectGraphNode.name);
+  });
+
+  return (
+    <>
+      {headerText !== '' ? (
+        <h3 className="mt-4 cursor-text py-2 text-sm font-semibold uppercase tracking-wide text-slate-800 dark:text-slate-200 lg:text-xs">
+          {headerText}
+        </h3>
+      ) : null}
+      <ul className="mt-2 -ml-3">
+        {sortedProjects.map((project) => {
+          return (
+            <ProjectListItem
+              key={project.projectGraphNode.name}
+              project={project}
+              selectTask={selectTask}
+            ></ProjectListItem>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+function mapToSidebarProjectWithTasks(
+  project: ProjectGraphNode,
+  selectedTask: string
+): SidebarProjectWithTargets {
+  const targets: SidebarTarget[] = [];
+
+  for (const targetName in project.data?.targets) {
+    const target: SidebarTarget = {
+      targetName,
+      configurations: [],
+    };
+
+    for (const configuration in project.data?.targets?.[targetName]
+      ?.configurations) {
+      target.configurations.push({
+        name: configuration,
+        isSelected: configuration === selectedTask,
+        default:
+          configuration ===
+          project.data?.targets?.[targetName]?.defaultConfiguration,
+      });
+    }
+
+    targets.push(target);
+  }
+
+  return {
+    projectGraphNode: project,
+    targets,
+  };
+}
+
+export interface TaskListProps {
+  projects: ProjectGraphNode[];
+  taskGraphs: Record<string, Record<string, Record<string, Task>>>;
+  workspaceLayout: WorkspaceLayout;
+  selectedTask: string;
+  selectTask: (
+    projectName: string,
+    targetName: string,
+    configurationName: string
+  ) => void;
+}
+
+export function TaskList({
+  projects,
+  workspaceLayout,
+  selectedTask,
+  selectTask,
+}: TaskListProps) {
+  const appProjects = getProjectsByType('app', projects);
+  const libProjects = getProjectsByType('lib', projects);
+  const e2eProjects = getProjectsByType('e2e', projects);
+
+  const appDirectoryGroups = groupProjectsByDirectory(
+    appProjects,
+    workspaceLayout
+  );
+  const libDirectoryGroups = groupProjectsByDirectory(
+    libProjects,
+    workspaceLayout
+  );
+  const e2eDirectoryGroups = groupProjectsByDirectory(
+    e2eProjects,
+    workspaceLayout
+  );
+
+  const sortedAppDirectories = Object.keys(appDirectoryGroups).sort();
+  const sortedLibDirectories = Object.keys(libDirectoryGroups).sort();
+  const sortedE2EDirectories = Object.keys(e2eDirectoryGroups).sort();
+
+  return (
+    <div id="project-lists" className="mt-8 border-t border-slate-400/10 px-4">
+      <h2 className="mt-8 border-b border-solid border-slate-200/10 text-lg font-light text-slate-400 dark:text-slate-500">
+        app projects
+      </h2>
+
+      {sortedAppDirectories.map((directoryName) => {
+        return (
+          <SubProjectList
+            key={'app-' + directoryName}
+            headerText={directoryName}
+            projects={appDirectoryGroups[directoryName].map((project) =>
+              mapToSidebarProjectWithTasks(project, selectedTask)
+            )}
+            selectTask={selectTask}
+          ></SubProjectList>
+        );
+      })}
+
+      <h2 className="mt-8 border-b border-solid border-slate-200/10 text-lg font-light">
+        e2e projects
+      </h2>
+
+      {sortedE2EDirectories.map((directoryName) => {
+        return (
+          <SubProjectList
+            key={'e2e-' + directoryName}
+            headerText={directoryName}
+            projects={e2eDirectoryGroups[directoryName].map((project) =>
+              mapToSidebarProjectWithTasks(project, selectedTask)
+            )}
+            selectTask={selectTask}
+          ></SubProjectList>
+        );
+      })}
+
+      <h2 className="mt-8 border-b border-solid border-slate-200/10 text-lg font-light">
+        lib projects
+      </h2>
+
+      {sortedLibDirectories.map((directoryName) => {
+        return (
+          <SubProjectList
+            key={'lib-' + directoryName}
+            headerText={directoryName}
+            projects={libDirectoryGroups[directoryName].map((project) =>
+              mapToSidebarProjectWithTasks(project, selectedTask)
+            )}
+            selectTask={selectTask}
+          ></SubProjectList>
+        );
+      })}
+    </div>
+  );
+}
+
+export default TaskList;
--- graph/client/src/app/sidebar/text-filter-panel.tsx
+++ graph/client/src/app/sidebar/text-filter-panel.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import type { KeyboardEvent } from 'react';
 import { useDebounce } from '../hooks/use-debounce';
 import { BackspaceIcon, FunnelIcon } from '@heroicons/react/24/outline';
+import DebouncedTextInput from '../ui-components/debounced-text-input';
 
 export interface TextFilterPanelProps {
   textFilter: string;
@@ -18,72 +19,15 @@ export function TextFilterPanel({
   toggleIncludeLibsInPathChange,
   includePath,
 }: TextFilterPanelProps) {
-  const [currentTextFilter, setCurrentTextFilter] = useState('');
-
-  const [debouncedValue, setDebouncedValue] = useDebounce(
-    currentTextFilter,
-    500
-  );
-
-  function onTextFilterKeyUp(event: KeyboardEvent<HTMLInputElement>) {
-    if (event.key === 'Enter') {
-      updateTextFilter(event.currentTarget.value);
-    }
-  }
-
-  function onTextInputChange(change: string) {
-    if (change === '') {
-      setCurrentTextFilter('');
-      setDebouncedValue('');
-
-      resetTextFilter();
-    } else {
-      setCurrentTextFilter(change);
-    }
-  }
-
-  function resetClicked() {
-    setCurrentTextFilter('');
-    setDebouncedValue('');
-
-    resetTextFilter();
-  }
-
-  useEffect(() => {
-    updateTextFilter(debouncedValue);
-  }, [debouncedValue, updateTextFilter]);
-
   return (
     <div>
       <div className="mt-10 px-4">
-        <form
-          className="group relative flex rounded-md shadow-sm"
-          onSubmit={(event) => event.preventDefault()}
-        >
-          <span className="inline-flex items-center rounded-l-md border border-r-0 border-slate-300 bg-slate-50 p-2 dark:border-slate-900 dark:bg-slate-800">
-            <FunnelIcon className="h-4 w-4"></FunnelIcon>
-          </span>
-          <input
-            type="text"
-            className={`block w-full flex-1 rounded-none rounded-r-md border border-slate-300 bg-white p-1.5 font-light text-slate-400 placeholder:font-light placeholder:text-slate-400 dark:border-slate-900 dark:bg-slate-800 dark:text-white dark:hover:bg-slate-700`}
-            placeholder="lib name, other lib name"
-            data-cy="textFilterInput"
-            name="filter"
-            value={currentTextFilter}
-            onKeyUp={onTextFilterKeyUp}
-            onChange={(event) => onTextInputChange(event.currentTarget.value)}
-          ></input>
-          {currentTextFilter.length > 0 ? (
-            <button
-              data-cy="textFilterReset"
-              type="reset"
-              onClick={resetClicked}
-              className="absolute top-1 right-1 inline-block rounded-md bg-slate-50 p-1 text-slate-400 dark:bg-slate-800"
-            >
-              <BackspaceIcon className="h-5 w-5"></BackspaceIcon>
-            </button>
-          ) : null}
-        </form>
+        <DebouncedTextInput
+          resetTextFilter={resetTextFilter}
+          updateTextFilter={updateTextFilter}
+          initialText={''}
+          placeholderText={'lib name, other lib name'}
+        ></DebouncedTextInput>
       </div>
 
       <div className="mt-4 px-4">
