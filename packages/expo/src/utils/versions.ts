d6997d5a995eaff67f569a93b19d6e590d8f4158
Emily Xiong <xiongemi@gmail.com>
2022-10-12 13:30:53 -0400

    feat(expo): upgrade expo to 46.0.13 (#12326)

--- packages/expo/src/utils/versions.ts
+++ packages/expo/src/utils/versions.ts
@@ -1,15 +1,15 @@
 export const nxVersion = '*';
 
-export const expoVersion = '46.0.10';
+export const expoVersion = '46.0.13';
 export const expoMetroConfigVersion = '0.3.22';
 export const expoSplashScreenVersion = '~0.16.2';
 export const expoStatusBarVersion = '~1.4.0';
 export const jestExpoVersion = '46.0.1';
-export const expoCliVersion = '6.0.5';
+export const expoCliVersion = '6.0.6';
 export const easCliVersion = '2.1.0';
 export const babelPresetExpoVersion = '~9.2.0';
 
-export const reactNativeVersion = '0.69.5';
+export const reactNativeVersion = '0.69.6';
 export const typesReactNativeVersion = '0.69.8';
 export const reactNativeWebVersion = '~0.18.7';
 
