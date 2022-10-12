bc53592ad6424e4a48cd905b821421ad2693a6a0
Jason Jean <jasonjean1993@gmail.com>
2022-10-12 17:30:11 -0400

    Revert "feat(node): remove library generator in favor of @nrwl/js:library (#12550)" (#12560)
    This reverts commit e74a54d13973b7a2ad5e5fb311d975ecd174705c.

--- packages/node/index.ts
+++ packages/node/index.ts
@@ -1,8 +1,3 @@
-import { libraryGenerator } from '@nrwl/js';
-
-// backwards compat
-// TODO(jack): Remove in Nx 16
-export { libraryGenerator };
-
 export { applicationGenerator } from './src/generators/application/application';
+export { libraryGenerator } from './src/generators/library/library';
 export { initGenerator } from './src/generators/init/init';
