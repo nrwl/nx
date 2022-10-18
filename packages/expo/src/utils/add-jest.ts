dd9ecc42be7e930225f016b96957926b1b4b82f9
Emily Xiong <xiongemi@gmail.com>
2022-10-18 13:53:58 -0700

    feat(expo): change expo jest config preset to react-native

--- packages/expo/src/utils/add-jest.ts
+++ packages/expo/src/utils/add-jest.ts
@@ -23,18 +23,18 @@ export async function addJest(
   // overwrite the jest.config.ts file because react native needs to have special transform property
   const configPath = `${appProjectRoot}/jest.config.${js ? 'js' : 'ts'}`;
   const content = `module.exports = {
-  displayName: '${projectName}',
-  resolver: '@nrwl/jest/plugins/resolver',
-  preset: 'jest-expo',
-  transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
-  ],
-  moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
-  setupFilesAfterEnv: ['<rootDir>/test-setup.${js ? 'js' : 'ts'}'],
-  moduleNameMapper: {
-    '\\.svg': '@nrwl/expo/plugins/jest/svg-mock'
-  }
-};`;
+    displayName: '${projectName}',
+    resolver: '@nrwl/jest/plugins/resolver',
+    preset: 'react-native',
+    transformIgnorePatterns: [
+      'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+    ],
+    moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
+    setupFilesAfterEnv: ['<rootDir>/test-setup.${js ? 'js' : 'ts'}'],
+    moduleNameMapper: {
+      '\\.svg': '@nrwl/expo/plugins/jest/svg-mock'
+    }
+  };`;
   host.write(configPath, content);
 
   return jestTask;
--- packages/expo/src/utils/versions.ts
+++ packages/expo/src/utils/versions.ts
@@ -4,7 +4,6 @@ export const expoVersion = '46.0.13';
 export const expoMetroConfigVersion = '0.3.22';
 export const expoSplashScreenVersion = '~0.16.2';
 export const expoStatusBarVersion = '~1.4.0';
-export const jestExpoVersion = '46.0.1';
 export const expoCliVersion = '6.0.6';
 export const easCliVersion = '2.1.0';
 export const babelPresetExpoVersion = '~9.2.0';
@@ -19,5 +18,5 @@ export const svgrWebpackVersion = '^6.3.1';
 
 export const metroVersion = '0.72.3';
 
-export const testingLibraryReactNativeVersion = '11.2.0';
-export const testingLibraryJestNativeVersion = '4.0.13';
+export const testingLibraryReactNativeVersion = '11.3.0';
+export const testingLibraryJestNativeVersion = '5.0.0';
