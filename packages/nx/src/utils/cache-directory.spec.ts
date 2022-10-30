69ea2dacb93d46ddf67fd2c946b111668859c225
Jon Hammerskov <jon@hammerskov.dk>
2022-10-30 15:53:53 +0100

    feat(core): add nx.json property projectGraphCacheDirectory
    The nx.json tasksRunnerOptions.default.options.projectGraphCacheDirectory
    will allow specifying the
    placement of the cache of nx project graph
    and daemon logs to be placed in a place different from
    the cache of
    target artefacts.
    This was allready possible using the environment
    variable
    NX_PROJECT_GRAPH_CACHE_DIRECTORY but as it is not advisable
    to have a shared directory for
    this purpose it is preferable
    to allow a specification like
    \"projectGraphCacheDirectory\":
    \"node_module/.cache/nxproject\"
    to ensure that anyone - developer or CI - that will use
    a shared
    cache for the target artefacs via the environment
    variable NX_CACHE_DIRECTORY=~/.nxcache will not
    inadvertently

---
+++ packages/nx/src/utils/cache-directory.spec.ts
@@ -0,0 +1,133 @@
+import { join } from 'path';
+import { NxJsonConfiguration } from './../config/nx-json';
+import { cacheDirectory } from './cache-directory';
+
+describe.only('cache-directory', () => {
+  const emptyNxJSon = () =>
+    ({
+      tasksRunnerOptions: {
+        default: {
+          options: {},
+        },
+      },
+    } as unknown as NxJsonConfiguration);
+  const emptyEnv = () => ({});
+  const root = () => '/Users/BlaBla/repos/some-repo';
+
+  const cacheDirSpec = () => ({
+    envName: 'NX_CACHE_DIRECTORY',
+    propertyName: 'cacheDirectory',
+    defaultDirectory: join('node_modules', '.cache', 'nx'),
+  });
+
+  const projectGraphCacheDirSpec = (cacheDir: string) => ({
+    envName: 'NX_PROJECT_GRAPH_CACHE_DIRECTORY',
+    propertyName: 'projectGraphCacheDirectory',
+    defaultDirectory: cacheDir,
+  });
+
+  it('cacheDir should default to node_modules/.cache/nx', () => {
+    const spec = cacheDirSpec();
+    const dir = cacheDirectory(root(), emptyEnv(), emptyNxJSon(), spec);
+    expect(dir).toBe(join(root(), spec.defaultDirectory));
+  });
+
+  it('cacheDirectory in nx.json should work with relative path', () => {
+    const spec = cacheDirSpec();
+    const nxJson = emptyNxJSon();
+    nxJson.tasksRunnerOptions.default.options['cacheDirectory'] = '.nxcache';
+
+    const dir = cacheDirectory(root(), emptyEnv(), nxJson, spec);
+    expect(dir).toBe(join(root(), '.nxcache'));
+  });
+
+  it('cacheDirectory in nx.json should work with absolute path', () => {
+    const spec = cacheDirSpec();
+    const nxJson = emptyNxJSon();
+    nxJson.tasksRunnerOptions.default.options['cacheDirectory'] =
+      '/tmp/.nxcache';
+
+    const dir = cacheDirectory(root(), emptyEnv(), nxJson, spec);
+    expect(dir).toBe('/tmp/.nxcache');
+  });
+
+  it('env NX_CACHE_DIRECTORY should work with relative path', () => {
+    const spec = cacheDirSpec();
+    const env = { NX_CACHE_DIRECTORY: 'local-cache' };
+    const dir = cacheDirectory(root(), env, emptyNxJSon(), spec);
+    expect(dir).toBe(join(root(), 'local-cache'));
+  });
+
+  it('env NX_CACHE_DIRECTORY should work with aboslute path', () => {
+    const spec = cacheDirSpec();
+    const env = { NX_CACHE_DIRECTORY: '/Users/BlaBla/.cache' };
+    const dir = cacheDirectory(root(), env, emptyNxJSon(), spec);
+    expect(dir).toBe('/Users/BlaBla/.cache');
+  });
+
+  it('env NX_CACHE_DIRECTORY should have priority over nx.json', () => {
+    const spec = cacheDirSpec();
+    const env = { NX_CACHE_DIRECTORY: 'local-cache' };
+    const nxJson = emptyNxJSon();
+    nxJson.tasksRunnerOptions.default.options['cacheDirectory'] = '.nxcache';
+    const dir = cacheDirectory(root(), env, nxJson, spec);
+    expect(dir).toBe(join(root(), 'local-cache'));
+  });
+
+  it('projectGraphCacheDirectory should default to what cacheDir is', () => {
+    const cacheDir = cacheDirectory(
+      root(),
+      emptyEnv(),
+      emptyNxJSon(),
+      cacheDirSpec()
+    );
+    const spec = projectGraphCacheDirSpec(cacheDir);
+    const dir = cacheDirectory(root(), emptyEnv(), emptyNxJSon(), spec);
+    expect(dir).toBe(cacheDir);
+  });
+
+  it('projectGraphCacheDirectory in nx.json should work with relative path', () => {
+    const spec = projectGraphCacheDirSpec('not/a/directory/we/should/see');
+    const nxJson = emptyNxJSon();
+    nxJson.tasksRunnerOptions.default.options['projectGraphCacheDirectory'] =
+      '.nxproject';
+    const dir = cacheDirectory(root(), emptyEnv(), nxJson, spec);
+    expect(dir).toBe(join(root(), '.nxproject'));
+  });
+
+  it('projectGraphCacheDirectory in nx.json should work with absolute path', () => {
+    const spec = projectGraphCacheDirSpec('not/a/directory/we/should/see');
+    const nxJson = emptyNxJSon();
+    nxJson.tasksRunnerOptions.default.options['projectGraphCacheDirectory'] =
+      '/tmp/.nxproject';
+    const dir = cacheDirectory(root(), emptyEnv(), nxJson, spec);
+    expect(dir).toBe(join('/tmp/.nxproject'));
+  });
+
+  it('env NX_PROJECT_GRAPH_CACHE_DIRECTORY should work with relative path', () => {
+    const spec = projectGraphCacheDirSpec('not/a/directory/we/should/see');
+    const env = { NX_PROJECT_GRAPH_CACHE_DIRECTORY: 'nx-project-graph' };
+    const dir = cacheDirectory(root(), env, emptyNxJSon(), spec);
+    expect(dir).toBe(join(root(), 'nx-project-graph'));
+  });
+
+  it('env NX_PROJECT_GRAPH_CACHE_DIRECTORY should work with absolute path', () => {
+    const spec = projectGraphCacheDirSpec('not/a/directory/we/should/see');
+    const env = {
+      NX_PROJECT_GRAPH_CACHE_DIRECTORY: '/usr/home/bla/nx-project-graph',
+    };
+    const dir = cacheDirectory(root(), env, emptyNxJSon(), spec);
+    expect(dir).toBe('/usr/home/bla/nx-project-graph');
+  });
+
+  it('env NX_PROJECT_GRAPH_CACHE_DIRECTORY should have priority over nx.json', () => {
+    const spec = projectGraphCacheDirSpec('not/a/directory/we/should/see');
+    const env = { NX_PROJECT_GRAPH_CACHE_DIRECTORY: 'nx-project-graph' };
+    const nxJson = emptyNxJSon();
+    nxJson.tasksRunnerOptions.default.options['projectGraphCacheDirectory'] =
+      '.nxproject';
+    nxJson.tasksRunnerOptions.default.options['cacheDirectory'] = '.nxcache';
+    const dir = cacheDirectory(root(), env, nxJson, spec);
+    expect(dir).toBe(join(root(), 'nx-project-graph'));
+  });
+});
--- packages/nx/src/utils/cache-directory.ts
+++ packages/nx/src/utils/cache-directory.ts
@@ -1,40 +1,106 @@
-import { join, isAbsolute } from 'path';
-import { workspaceRoot } from './workspace-root';
-import { readJsonFile } from './fileutils';
+import { isAbsolute, join } from 'path';
 import { NxJsonConfiguration } from '../config/nx-json';
+import { readJsonFile } from './fileutils';
+import { workspaceRoot } from './workspace-root';
 
-function readCacheDirectoryProperty(root: string): string | undefined {
-  try {
-    const nxJson = readJsonFile<NxJsonConfiguration>(join(root, 'nx.json'));
-    return nxJson.tasksRunnerOptions.default.options.cacheDirectory;
-  } catch {
-    return undefined;
-  }
+export interface CacheDirectorySpec {
+  envName: string;
+  propertyName: string;
+  defaultDirectory?: string;
 }
 
-function cacheDirectory(root: string, cacheDirectory: string) {
-  const cacheDirFromEnv = process.env.NX_CACHE_DIRECTORY;
-  if (cacheDirFromEnv) {
-    cacheDirectory = cacheDirFromEnv;
+/**
+ * Calculate path to cache directory
+ * The path is calculated by looking at these values in order of precedense:
+ * env[cache.envName]
+ * nxConfig.tasksRunnerOptions.default.options[cache.propertyName]
+ * cache.defaultDirectory
+ *
+ * if the resulting path from the above rule is not an absolute path
+ * it will be prefixed with root
+ *
+ * @param root : the root of the workspace
+ * @param env : the environment - e.g. process.env
+ * @param nxConfig : the content of the workspace nx.json
+ * @param cache : the specification of the cache :
+ * -              envName: the property in env that may define the cache
+ * -              propertyName: the property in
+ * -                            nxConfig.tasksRunnerOptions.default.options
+ * -                            that may define the cache
+ * -              defaultDirectory: the cache directory path if none of the
+ * -                                above defines the path
+ * @returns
+ */
+export function cacheDirectory(
+  root: string,
+  env: { [key: string]: string },
+  nxConfig: NxJsonConfiguration,
+  cache: CacheDirectorySpec
+): string {
+  let directory = env[cache.envName];
+  if (!directory) {
+    directory =
+      nxConfig.tasksRunnerOptions?.default?.options[cache.propertyName];
   }
-  if (cacheDirectory) {
-    if (isAbsolute(cacheDirectory)) {
-      return cacheDirectory;
+  if (!directory) {
+    directory = cache.defaultDirectory;
+  }
+
+  if (directory) {
+    if (isAbsolute(directory)) {
+      return directory;
     } else {
-      return join(root, cacheDirectory);
+      return join(root, directory);
     }
-  } else {
-    return join(root, 'node_modules', '.cache', 'nx');
   }
+  return '';
+}
+
+let nxJson: NxJsonConfiguration;
+//temporarily keep a value of workspace nx.json
+try {
+  nxJson = readJsonFile<NxJsonConfiguration>(join(workspaceRoot, 'nx.json'));
+} catch (_err: any) {
+  nxJson = {};
 }
 
 /**
  * Path to the directory where Nx stores its cache and daemon-related files.
+ * It is defined in order of precedence:
+ * environment variable NX_CACHE_DIRECTORY
+ * nx.json tasksRunnerOptions.default.options.cacheDirectory
+ * node_modules/.cache/nx
+ *
+ * if the resulting path from the above rule is not an absolute path
+ * it will be prefixed with the workspace root path
  */
-export const cacheDir = cacheDirectory(
+export const cacheDir = cacheDirectory(workspaceRoot, process.env, nxJson, {
+  envName: 'NX_CACHE_DIRECTORY',
+  propertyName: 'cacheDirectory',
+  defaultDirectory: join('node_modules', '.cache', 'nx'),
+});
+
+/**
+ * Path to the directory where Nx stores its cache of project dependencies and
+ * logs from the Nx Daemon in case it is active.
+ * It is defined in order of precedence:
+ * environment variable NX_PROJECT_GRAPH_CACHE_DIRECTORY
+ * nx.json tasksRunnerOptions.default.options.projectGraphCacheDirectory
+ * cacheDir -- see export above this
+ *
+ * if the resulting path from the above rule is not an absolute path
+ * it will be prefixed with the workspace root path
+ */
+export const projectGraphCacheDirectory = cacheDirectory(
   workspaceRoot,
-  readCacheDirectoryProperty(workspaceRoot)
+  process.env,
+  nxJson,
+  {
+    envName: 'NX_PROJECT_GRAPH_CACHE_DIRECTORY',
+    propertyName: 'projectGraphCacheDirectory',
+    defaultDirectory: cacheDir,
+  }
 );
 
-export const projectGraphCacheDirectory =
-  process.env.NX_PROJECT_GRAPH_CACHE_DIRECTORY ?? cacheDir;
+//now release nxJson to be garbage collected
+nxJson = undefined;
