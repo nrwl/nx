2c7977752962ebf176bd0f9b0b61838590184b9f
Victor Savkin <mail@vsavkin.com>
2022-11-01 13:08:12 -0400

    feat(misc): show a hint message on how to upload last logs to cloud

--- packages/nx/src/command-line/nx-commands.spec.ts
+++ packages/nx/src/command-line/nx-commands.spec.ts
@@ -5,6 +5,7 @@ describe('nx-commands', () => {
     const actual = commandsObject.parse(
       'nx e2e project-e2e --env.NX_API_URL=http://localhost:4200 --abc.123.xyx=false --a.b=3'
     );
+
     expect(actual).toEqual(
       expect.objectContaining({
         abc: {
