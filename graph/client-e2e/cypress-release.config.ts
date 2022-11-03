7120f22699df848c7cde9c0f18d94d38a466047a
Philip Fulcher <philip@nrwl.io>
2022-11-03 14:37:12 -0600

    chore(graph): upgrade to Cypress 10 (#12980)

---
+++ graph/client-e2e/cypress-release.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'cypress';
+import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+import setupNodeEvents from './src/plugins/index';
+
+const cypressJsonConfig = {
+  fileServerFolder: '.',
+  fixturesFolder: './src/fixtures',
+  video: true,
+  videosFolder: '../../dist/cypress/graph/client-e2e/videos',
+  screenshotsFolder: '../../dist/cypress/graph/client-e2e/screenshots',
+  chromeWebSecurity: false,
+  specPattern: './src/release-integration/**/*.cy.{js,jsx,ts,tsx}',
+  supportFile: 'src/support/e2e.ts',
+};
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    ...cypressJsonConfig,
+    setupNodeEvents,
+  },
+});
