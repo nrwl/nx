c9c0edd37e72b0978ca2e939e0448940615d9d9a
Philip Fulcher <philip@nrwl.io>
2022-11-03 08:09:02 -0600

    chore(graph): upgrade to Cypress 10

---
+++ nx-dev/nx-dev-e2e/cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress';
+import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+
+const cypressJsonConfig = {
+  fileServerFolder: '.',
+  fixturesFolder: './src/fixtures',
+  video: true,
+  videosFolder: '../../dist/cypress/apps/nx-dev-e2e/videos',
+  screenshotsFolder: '../../dist/cypress/apps/nx-dev-e2e/screenshots',
+  chromeWebSecurity: false,
+  specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  supportFile: 'src/support/e2e.ts',
+};
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    ...cypressJsonConfig,
+  },
+});
