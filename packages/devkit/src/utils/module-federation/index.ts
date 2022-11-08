7fb9b550bf137cd8285e35ca1ae81ca3d573f016
Colum Ferry <cferry09@gmail.com>
2022-11-08 10:12:54 +0000

    feat(devkit): add module federation utils

---
+++ packages/devkit/src/utils/module-federation/index.ts
@@ -0,0 +1,4 @@
+export * from './share';
+export * from './dependencies';
+export * from './package-json';
+export * from './models';
---
+++ packages/devkit/src/utils/module-federation/typescript.ts
@@ -0,0 +1,49 @@
+import { existsSync } from 'fs';
+import { ParsedCommandLine } from 'typescript';
+import { getRootTsConfigPath } from 'nx/src/utils/typescript';
+import { dirname } from 'path';
+
+let tsConfig: ParsedCommandLine;
+let tsPathMappings: ParsedCommandLine['options']['paths'];
+
+export function readTsPathMappings(
+  tsConfigPath: string = process.env.NX_TSCONFIG_PATH ?? getRootTsConfigPath()
+): ParsedCommandLine['options']['paths'] {
+  if (tsPathMappings) {
+    return tsPathMappings;
+  }
+
+  tsConfig ??= readTsConfiguration(tsConfigPath);
+  tsPathMappings = {};
+  Object.entries(tsConfig.options?.paths ?? {}).forEach(([alias, paths]) => {
+    tsPathMappings[alias] = paths.map((path) => path.replace(/^\.\//, ''));
+  });
+
+  return tsPathMappings;
+}
+
+function readTsConfiguration(tsConfigPath: string): ParsedCommandLine {
+  if (!existsSync(tsConfigPath)) {
+    throw new Error(
+      `NX MF: TsConfig Path for workspace libraries does not exist! (${tsConfigPath}).`
+    );
+  }
+
+  return readTsConfig(tsConfigPath);
+}
+
+let tsModule: typeof import('typescript');
+export function readTsConfig(tsConfigPath: string): ParsedCommandLine {
+  if (!tsModule) {
+    tsModule = require('typescript');
+  }
+  const readResult = tsModule.readConfigFile(
+    tsConfigPath,
+    tsModule.sys.readFile
+  );
+  return tsModule.parseJsonConfigFileContent(
+    readResult.config,
+    tsModule.sys,
+    dirname(tsConfigPath)
+  );
+}
