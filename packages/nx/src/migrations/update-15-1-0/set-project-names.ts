098903f7f761cc80d8f578241b2c1f088392de4b
Victor Savkin <mail@vsavkin.com>
2022-11-08 14:57:34 -0500

    fix(misc): set project names used the old logic including dirs
    (cherry picked from commit 4ba4bd024ab70f6baaef98260728d712d3fadb7d)

--- packages/nx/src/migrations/update-15-1-0/set-project-names.ts
+++ packages/nx/src/migrations/update-15-1-0/set-project-names.ts
@@ -1,6 +1,7 @@
 import { Tree } from '../../generators/tree';
 import {
   getProjects,
+  readNxJson,
   updateProjectConfiguration,
 } from '../../generators/utils/project-configuration';
 import { formatChangedFilesWithPrettierIfAvailable } from '../../generators/internal-utils/format-changed-files-with-prettier-if-available';
@@ -10,10 +11,23 @@ export default async function (tree: Tree) {
   // This looks like it does nothing, but this will actually effectively migrate over all the configs that need to be moved over, but won't touch configs that don't need to be moved
   for (const [projName, projConfig] of getProjects(tree)) {
     if (tree.exists(join(projConfig.root, 'project.json'))) {
-      projConfig.name ??= projName;
+      if (!projConfig.name) {
+        projConfig.name = toProjectName(projConfig.root, readNxJson(tree));
+      }
       updateProjectConfiguration(tree, projName, projConfig);
     }
   }
 
   await formatChangedFilesWithPrettierIfAvailable(tree);
+}
+
+function toProjectName(directory: string, nxJson: any): string {
+  let { appsDir, libsDir } = nxJson?.workspaceLayout || {};
+  appsDir ??= 'apps';
+  libsDir ??= 'libs';
+  const parts = directory.split(/[\/\\]/g);
+  if ([appsDir, libsDir].includes(parts[0])) {
+    parts.splice(0, 1);
+  }
+  return parts.join('-').toLowerCase();
 }
