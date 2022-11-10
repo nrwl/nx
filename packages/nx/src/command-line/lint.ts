f572574fae3b5fe6378a864e6eb0b7c9e55fea49
Jason Jean <jasonjean1993@gmail.com>
2022-11-10 17:08:00 -0500

    fix(misc): read the version of nrwl packages from project graph (#13118)

--- packages/nx/src/command-line/lint.ts
+++ packages/nx/src/command-line/lint.ts
@@ -4,15 +4,12 @@ import { workspaceLayout } from '../config/configuration';
 import { output } from '../utils/output';
 import * as path from 'path';
 import { createProjectGraphAsync } from '../project-graph/project-graph';
-import { pruneExternalNodes } from '../project-graph/operators';
 
 export async function workspaceLint(): Promise<void> {
   const graph = await createProjectGraphAsync({ exitOnError: true });
   const allWorkspaceFiles = graph.allWorkspaceFiles;
-  const projectGraph = pruneExternalNodes(graph);
-
   const integrityMessages = new WorkspaceIntegrityChecks(
-    projectGraph,
+    graph,
     readAllFilesFromAppsAndLibs(allWorkspaceFiles)
   ).run();
 
