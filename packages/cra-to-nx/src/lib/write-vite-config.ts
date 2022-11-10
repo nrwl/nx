83646895c38d1cdeb0baa0311641a0e08a6a5416
Jack Hsu <jack.hsu@gmail.com>
2022-11-10 10:41:05 -0500

    cleanup(react): add e2e tests for cra-to-nx package (#13107)

--- packages/cra-to-nx/src/lib/write-vite-config.ts
+++ packages/cra-to-nx/src/lib/write-vite-config.ts
@@ -9,7 +9,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../../dist/apps/cra-app'
+    outDir: '../../dist/apps/${appName}'
   },
   server: {
     open: true,
