40cd7ac1742cf31564468163c1b0022ed126f47b
nodegin <10@nodeg.in>
2022-11-19 04:04:58 +0900

    Add comments back

--- packages/vite/plugins/rollup-replace-files.plugin.ts
+++ packages/vite/plugins/rollup-replace-files.plugin.ts
@@ -15,10 +15,19 @@ export default function replaceFiles(replacements: FileReplacement[]) {
     name: 'rollup-plugin-replace-files',
     enforce: 'pre',
     async load(id) {
+      /**
+       * The reason we're using endsWith here is because the resolved id
+       * will be the absolute path to the file. We want to check if the
+       * file ends with the file we're trying to replace, which will be essentially
+       * the path from the root of our workspace.
+       */
       const foundReplace = replacements.find((replacement)=>{
         return id.endsWith(replacement.replace)
       });
       if (foundReplace) {
+        console.info(
+          `replace "${foundReplace.replace}" with "${foundReplace.with}"`
+        );
         return `export * from "${foundReplace.with}"; export { default } from "${foundReplace.with}";`;
       }
       return null
