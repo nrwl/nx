ade666806059beca5f2320704e916362afe19fec
Jack Hsu <jack.hsu@gmail.com>
2022-11-21 16:58:34 -0500

    feat(react): update package versions (#13300)

--- packages/react/src/utils/versions.ts
+++ packages/react/src/utils/versions.ts
@@ -3,44 +3,44 @@ export const nxVersion = require('../../package.json').version;
 export const reactVersion = '18.2.0';
 export const reactDomVersion = '18.2.0';
 export const reactIsVersion = '18.2.0';
-export const typesReactVersion = '18.0.20';
-export const typesReactDomVersion = '18.0.6';
+export const typesReactVersion = '18.0.25';
+export const typesReactDomVersion = '18.0.9';
 export const typesReactIsVersion = '17.0.3';
 
-export const typesNodeVersion = '18.7.18';
+export const typesNodeVersion = '18.11.9';
 
-export const styledComponentsVersion = '5.3.5';
+export const styledComponentsVersion = '5.3.6';
 export const typesStyledComponentsVersion = '5.1.26';
 
-export const emotionStyledVersion = '11.10.4';
-export const emotionReactVersion = '11.10.4';
-export const emotionBabelPlugin = '11.10.2';
+export const emotionStyledVersion = '11.10.5';
+export const emotionReactVersion = '11.10.5';
+export const emotionBabelPlugin = '11.10.5';
 
 // WARNING: This needs to be in sync with Next.js' dependency or else there might be issues.
-export const styledJsxVersion = '5.0.7';
+export const styledJsxVersion = '5.1.0';
 
-export const reactRouterDomVersion = '6.4.1';
+export const reactRouterDomVersion = '6.4.3';
 export const typesReactRouterDomVersion = '5.3.3';
 
 export const testingLibraryReactVersion = '13.4.0';
 
-export const reduxjsToolkitVersion = '1.8.5';
-export const reactReduxVersion = '8.0.2';
+export const reduxjsToolkitVersion = '1.9.0';
+export const reactReduxVersion = '8.0.5';
 export const reactTestRendererVersion = '18.2.0';
 
 export const eslintPluginImportVersion = '2.26.0';
 export const eslintPluginJsxA11yVersion = '6.6.1';
-export const eslintPluginReactVersion = '7.31.8';
+export const eslintPluginReactVersion = '7.31.11';
 export const eslintPluginReactHooksVersion = '4.6.0';
 
 export const babelPluginStyledComponentsVersion = '1.10.7';
 
 export const tsLibVersion = '^2.3.0';
 
-export const postcssVersion = '8.4.16';
-export const tailwindcssVersion = '3.1.8';
-export const autoprefixerVersion = '10.4.12';
+export const postcssVersion = '8.4.19';
+export const tailwindcssVersion = '3.2.4';
+export const autoprefixerVersion = '10.4.13';
 
 export const expressVersion = '^4.18.1';
-export const typesExpressVersion = '4.17.13';
+export const typesExpressVersion = '4.17.14';
 export const isbotVersion = '^3.6.5';
