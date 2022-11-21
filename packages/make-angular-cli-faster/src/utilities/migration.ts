6328913dda8810b3077deb4bf9934e453c09fc34
Phillip Barta <barta.phillip@gmail.com>
2022-11-21 23:24:55 +0100

    cleanup(misc): refactor `make-angular-cli-faster` to reduce install size and make it faster (#13039)

--- packages/make-angular-cli-faster/src/utilities/migration.ts
+++ packages/make-angular-cli-faster/src/utilities/migration.ts
@@ -1,8 +1,9 @@
-import { getPackageManagerCommand, output } from '@nrwl/devkit';
 import { execSync } from 'child_process';
 import { prompt } from 'enquirer';
-import { readModulePackageJson } from 'nx/src/utils/package-json';
 import { lt, lte, major, satisfies } from 'semver';
+import { output } from 'nx/src/utils/output';
+import { readModulePackageJson } from 'nx/src/utils/package-json';
+import { PackageManagerCommands } from 'nx/src/utils/package-manager';
 import { resolvePackageVersion } from './package-manager';
 import { MigrationDefinition } from './types';
 
@@ -84,7 +85,10 @@ export async function determineMigration(
   };
 }
 
-export function migrateWorkspace(migration: MigrationDefinition): void {
+export function migrateWorkspace(
+  migration: MigrationDefinition,
+  pmc: PackageManagerCommands
+): void {
   const preserveAngularCliLayoutFlag = lte(
     migration.version,
     latestVersionWithOldFlag
@@ -92,9 +96,7 @@ export function migrateWorkspace(migration: MigrationDefinition): void {
     ? '--preserveAngularCLILayout'
     : '--preserve-angular-cli-layout';
   execSync(
-    `${getPackageManagerCommand().exec} nx g ${
-      migration.packageName
-    }:ng-add ${preserveAngularCliLayoutFlag}`,
+    `${pmc.exec} nx g ${migration.packageName}:ng-add ${preserveAngularCliLayoutFlag}`,
     { stdio: [0, 1, 2] }
   );
 }
