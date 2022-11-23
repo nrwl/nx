80eca769f6d6d1c33053f9fecde6946b3e89bc82
Colum Ferry <cferry09@gmail.com>
2022-11-23 11:04:42 +0000

    fix(angular): handle workspaces that still have old style polyfills

--- packages/angular/src/generators/karma-project/lib/update-tsconfig.ts
+++ packages/angular/src/generators/karma-project/lib/update-tsconfig.ts
@@ -8,7 +8,7 @@ import {
 export function updateTsConfigs(tree: Tree, project: string): void {
   const projectConfig = readProjectConfiguration(tree, project);
 
-  return updateJson(
+  updateJson(
     tree,
     joinPathFragments(projectConfig.root, 'tsconfig.json'),
     (json) => {
@@ -20,6 +20,30 @@ export function updateTsConfigs(tree: Tree, project: string): void {
             path: './tsconfig.spec.json',
           },
         ],
+      };
+    }
+  );
+
+  let extraFiles: string[] = [];
+  if (
+    projectConfig.projectType == 'application' &&
+    projectConfig.targets.build?.options?.polyfills &&
+    typeof projectConfig.targets.build.options.polyfills === 'string'
+  ) {
+    let polyfillsPath = projectConfig.targets.build.options.polyfills;
+    polyfillsPath = polyfillsPath.startsWith(projectConfig.root)
+      ? polyfillsPath.replace(`${projectConfig.root}/`, '')
+      : polyfillsPath;
+    extraFiles = [polyfillsPath];
+  }
+
+  return updateJson(
+    tree,
+    joinPathFragments(projectConfig.root, 'tsconfig.spec.json'),
+    (json) => {
+      return {
+        ...json,
+        files: [...json.files, ...extraFiles],
       };
     }
   );
