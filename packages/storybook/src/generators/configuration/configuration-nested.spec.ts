2614452840baf982cb8a29fa848256439a26d7af
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2022-11-24 19:18:06 +0200

    feat(storybook): for nested projects (#13314)

---
+++ packages/storybook/src/generators/configuration/configuration-nested.spec.ts
@@ -0,0 +1,110 @@
+import { NxJsonConfiguration, Tree, updateJson, writeJson } from '@nrwl/devkit';
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+
+import configurationGenerator from './configuration';
+import * as rootProjectConfiguration from './test-configs/root-project-configuration.json';
+import * as workspaceConfiguration from './test-configs/root-workspace-configuration.json';
+
+describe('@nrwl/storybook:configuration for workspaces with Root project', () => {
+  describe('basic functionalities', () => {
+    let tree: Tree;
+
+    beforeEach(async () => {
+      tree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
+      updateJson<NxJsonConfiguration>(tree, 'nx.json', (json) => {
+        json.namedInputs = {
+          production: ['default'],
+        };
+        return json;
+      });
+
+      writeJson(tree, 'project.json', rootProjectConfiguration);
+      writeJson(tree, 'tsconfig.json', {
+        extends: './tsconfig.base.json',
+        compilerOptions: {
+          jsx: 'react-jsx',
+          allowJs: false,
+          esModuleInterop: false,
+          allowSyntheticDefaultImports: true,
+          forceConsistentCasingInFileNames: true,
+          isolatedModules: true,
+          lib: ['DOM', 'DOM.Iterable', 'ESNext'],
+          module: 'ESNext',
+          moduleResolution: 'Node',
+          noEmit: true,
+          resolveJsonModule: true,
+          skipLibCheck: true,
+          strict: true,
+          target: 'ESNext',
+          types: ['vite/client'],
+          useDefineForClassFields: true,
+          noImplicitOverride: true,
+          noPropertyAccessFromIndexSignature: true,
+          noImplicitReturns: true,
+          noFallthroughCasesInSwitch: true,
+        },
+        files: [],
+        include: [],
+        references: [
+          {
+            path: './tsconfig.app.json',
+          },
+          {
+            path: './tsconfig.spec.json',
+          },
+          {
+            path: './.storybook/tsconfig.json',
+          },
+        ],
+      });
+      writeJson(tree, 'workspace.json', workspaceConfiguration);
+      writeJson(tree, 'package.json', {
+        devDependencies: {
+          '@storybook/addon-essentials': '~6.2.9',
+          '@storybook/react': '~6.2.9',
+        },
+      });
+    });
+
+    it('should generate files for root app', async () => {
+      await configurationGenerator(tree, {
+        name: 'web',
+        uiFramework: '@storybook/react',
+        standaloneConfig: false,
+      });
+
+      expect(tree.exists('.storybook/main.js')).toBeTruthy();
+      expect(tree.exists('.storybook/main.root.js')).toBeTruthy();
+      expect(tree.exists('.storybook/tsconfig.json')).toBeTruthy();
+      expect(tree.exists('.storybook/preview.js')).toBeTruthy();
+    });
+
+    it('should generate Storybook files for nested first - then for root', async () => {
+      writeJson(tree, 'apps/reapp/tsconfig.json', {});
+
+      await configurationGenerator(tree, {
+        name: 'reapp',
+        uiFramework: '@storybook/react',
+        tsConfiguration: true,
+      });
+
+      expect(tree.exists('.storybook/main.ts')).toBeFalsy();
+      expect(tree.exists('.storybook/main.root.ts')).toBeTruthy();
+      expect(tree.exists('.storybook/tsconfig.json')).toBeFalsy();
+      expect(tree.exists('.storybook/preview.ts')).toBeFalsy();
+
+      expect(tree.exists('apps/reapp/.storybook/main.ts')).toBeTruthy();
+      expect(tree.exists('apps/reapp/.storybook/tsconfig.json')).toBeTruthy();
+      expect(tree.exists('apps/reapp/.storybook/preview.ts')).toBeTruthy();
+
+      await configurationGenerator(tree, {
+        name: 'web',
+        uiFramework: '@storybook/react',
+      });
+
+      expect(tree.exists('.storybook/main.ts')).toBeTruthy();
+      expect(tree.exists('.storybook/tsconfig.json')).toBeTruthy();
+      expect(tree.exists('.storybook/preview.ts')).toBeTruthy();
+    });
+  });
+});
--- packages/storybook/src/generators/init/init.ts
+++ packages/storybook/src/generators/init/init.ts
@@ -99,7 +99,7 @@ function checkDependenciesInstalled(host: Tree, schema: Schema) {
   return addDependenciesToPackageJson(host, dependencies, devDependencies);
 }
 
-export function addCacheableOperation(tree: Tree) {
+function addCacheableOperation(tree: Tree) {
   const workspace = readWorkspaceConfiguration(tree);
   if (
     !workspace.tasksRunnerOptions ||
@@ -143,9 +143,39 @@ function moveToDevDependencies(tree: Tree) {
   });
 }
 
+/**
+ * This is a temporary fix for Storybook to support TypeScript configuration files.
+ * The issue is that if there is a root tsconfig.json file, Storybook will use it, and
+ * ignore the tsconfig.json file in the .storybook folder. This results in module being set
+ * to esnext, and Storybook does not recognise the main.ts code as a module.
+ */
+function editRootTsConfig(tree: Tree) {
+  if (tree.exists('tsconfig.json')) {
+    updateJson(tree, 'tsconfig.json', (json) => {
+      if (json['ts-node']) {
+        json['ts-node'] = {
+          ...json['ts-node'],
+          compilerOptions: {
+            ...(json['ts-node'].compilerOptions ?? {}),
+            module: 'commonjs',
+          },
+        };
+      } else {
+        json['ts-node'] = {
+          compilerOptions: {
+            module: 'commonjs',
+          },
+        };
+      }
+      return json;
+    });
+  }
+}
+
 export function initGenerator(tree: Tree, schema: Schema) {
   const installTask = checkDependenciesInstalled(tree, schema);
   moveToDevDependencies(tree);
+  editRootTsConfig(tree);
   addCacheableOperation(tree);
   return installTask;
 }
