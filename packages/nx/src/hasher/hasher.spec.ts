dd1ba53c645d3d0791a56ad86bc893da654f581f
Victor Savkin <mail@vsavkin.com>
2022-11-30 10:31:53 -0500

    fix(core): negative patterns do not work for root project

--- packages/nx/src/hasher/hasher.spec.ts
+++ packages/nx/src/hasher/hasher.spec.ts
@@ -845,56 +845,71 @@ describe('Hasher', () => {
   describe('filterUsingGlobPatterns', () => {
     it('should OR all positive patterns and AND all negative patterns (when positive and negative patterns)', () => {
       const filtered = filterUsingGlobPatterns(
-        '/root',
+        'root',
         [
-          { file: '/root/a.ts' },
-          { file: '/root/b.js' },
-          { file: '/root/c.spec.ts' },
-          { file: '/root/d.md' },
+          { file: 'root/a.ts' },
+          { file: 'root/b.js' },
+          { file: 'root/c.spec.ts' },
+          { file: 'root/d.md' },
         ] as any,
         [
-          '/root/**/*.ts',
-          '/root/**/*.js',
-          '!/root/**/*.spec.ts',
-          '!/root/**/*.md',
+          '{projectRoot}/**/*.ts',
+          '{projectRoot}/**/*.js',
+          '!{projectRoot}/**/*.spec.ts',
+          '!{projectRoot}/**/*.md',
         ]
       );
 
-      expect(filtered.map((f) => f.file)).toEqual(['/root/a.ts', '/root/b.js']);
+      expect(filtered.map((f) => f.file)).toEqual(['root/a.ts', 'root/b.js']);
     });
 
     it('should OR all positive patterns and AND all negative patterns (when negative patterns)', () => {
       const filtered = filterUsingGlobPatterns(
-        '/root',
+        'root',
         [
-          { file: '/root/a.ts' },
-          { file: '/root/b.js' },
-          { file: '/root/c.spec.ts' },
-          { file: '/root/d.md' },
+          { file: 'root/a.ts' },
+          { file: 'root/b.js' },
+          { file: 'root/c.spec.ts' },
+          { file: 'root/d.md' },
         ] as any,
-        ['!/root/**/*.spec.ts', '!/root/**/*.md']
+        ['!{projectRoot}/**/*.spec.ts', '!{projectRoot}/**/*.md']
       );
 
-      expect(filtered.map((f) => f.file)).toEqual(['/root/a.ts', '/root/b.js']);
+      expect(filtered.map((f) => f.file)).toEqual(['root/a.ts', 'root/b.js']);
     });
 
     it('should OR all positive patterns and AND all negative patterns (when positive patterns)', () => {
       const filtered = filterUsingGlobPatterns(
-        '/root',
+        'root',
         [
-          { file: '/root/a.ts' },
-          { file: '/root/b.js' },
-          { file: '/root/c.spec.ts' },
-          { file: '/root/d.md' },
+          { file: 'root/a.ts' },
+          { file: 'root/b.js' },
+          { file: 'root/c.spec.ts' },
+          { file: 'root/d.md' },
         ] as any,
-        ['/root/**/*.ts', '/root/**/*.js']
+        ['{projectRoot}/**/*.ts', '{projectRoot}/**/*.js']
       );
 
       expect(filtered.map((f) => f.file)).toEqual([
-        '/root/a.ts',
-        '/root/b.js',
-        '/root/c.spec.ts',
+        'root/a.ts',
+        'root/b.js',
+        'root/c.spec.ts',
       ]);
+    });
+
+    it('should handle projects with the root set to .', () => {
+      const filtered = filterUsingGlobPatterns(
+        '.',
+        [
+          { file: 'a.ts' },
+          { file: 'b.md' },
+          { file: 'dir/a.ts' },
+          { file: 'dir/b.md' },
+        ] as any,
+        ['{projectRoot}/**/*.ts', '!{projectRoot}/**/*.md']
+      );
+
+      expect(filtered.map((f) => f.file)).toEqual(['a.ts', 'dir/a.ts']);
     });
   });
 });
