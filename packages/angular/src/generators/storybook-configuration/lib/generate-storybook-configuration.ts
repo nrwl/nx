c60d52af9bcb71eef5f885725a2a50f1c0be0440
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2022-12-02 17:38:59 +0200

    cleanup(misc): remove storybook dependency from other packages (#13571)

--- packages/angular/src/generators/storybook-configuration/lib/generate-storybook-configuration.ts
+++ packages/angular/src/generators/storybook-configuration/lib/generate-storybook-configuration.ts
@@ -1,11 +1,13 @@
-import type { GeneratorCallback, Tree } from '@nrwl/devkit';
-import { configurationGenerator } from '@nrwl/storybook';
+import { GeneratorCallback, Tree, ensurePackage } from '@nrwl/devkit';
 import type { StorybookConfigurationOptions } from '../schema';
+import { nxVersion } from '../../../utils/versions';
 
 export async function generateStorybookConfiguration(
   tree: Tree,
   options: StorybookConfigurationOptions
 ): Promise<GeneratorCallback> {
+  await ensurePackage(tree, '@nrwl/storybook', nxVersion);
+  const { configurationGenerator } = await import('@nrwl/storybook');
   return await configurationGenerator(tree, {
     name: options.name,
     uiFramework: '@storybook/angular',
