d3b8e1f547a678b8c594c41585f16101bd6c6799
Jonathan Cammisuli <jon@cammisuli.ca>
2022-12-06 17:24:15 -0500

    chore(core): add test for building watch command

--- packages/nx/src/command-line/watch.ts
+++ packages/nx/src/command-line/watch.ts
@@ -156,7 +156,7 @@ export async function watch(args: WatchArguments) {
   );
 }
 
-function buildCommands(
+export function buildCommands(
   command: string,
   changedFiles: ChangedFile[],
   changedProjects: string[]
