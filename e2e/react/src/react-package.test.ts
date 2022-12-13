acc67802ec1e90a03d0f1d0559fd8b184dea3221
Emily Xiong <xiongemi@gmail.com>
2022-12-12 20:14:36 -0500

    feat(react): add unit test examples when inSourceTests is passed

--- e2e/react/src/react-package.test.ts
+++ e2e/react/src/react-package.test.ts
@@ -260,6 +260,88 @@ describe('Build React applications and libraries with Vite', () => {
     proj = newProject();
   });
 
+  it('should test and lint app with bundler=vite', async () => {
+    const viteApp = uniq('viteapp');
+
+    runCLI(
+      `generate @nrwl/react:app ${viteApp} --bundler=vite --unitTestRunner=vitest --no-interactive`
+    );
+
+    const appTestResults = await runCLIAsync(`test ${viteApp}`);
+    expect(appTestResults.combinedOutput).toContain(
+      'Successfully ran target test'
+    );
+
+    const appLintResults = await runCLIAsync(`lint ${viteApp}`);
+    expect(appLintResults.combinedOutput).toContain(
+      'Successfully ran target lint'
+    );
+
+    await runCLIAsync(`build ${viteApp}`);
+    checkFilesExist(`dist/apps/${viteApp}/index.html`);
+  }, 300_000);
+
+  it('should test and lint app with bundler=vite and inSourceTests', async () => {
+    const viteApp = uniq('viteapp');
+    const viteLib = uniq('vitelib');
+
+    runCLI(
+      `generate @nrwl/react:app ${viteApp} --bundler=vite --unitTestRunner=vitest --inSourceTests --no-interactive`
+    );
+    expect(() => {
+      checkFilesExist(`apps/${viteApp}/src/app/app.spec.tsx`);
+    }).toThrow();
+
+    const appTestResults = await runCLIAsync(`test ${viteApp}`);
+    expect(appTestResults.combinedOutput).toContain(
+      'Successfully ran target test'
+    );
+
+    const appLintResults = await runCLIAsync(`lint ${viteApp}`);
+    expect(appLintResults.combinedOutput).toContain(
+      'Successfully ran target lint'
+    );
+
+    await runCLIAsync(`build ${viteApp}`);
+    checkFilesExist(`dist/apps/${viteApp}/index.html`);
+
+    runCLI(
+      `generate @nrwl/react:lib ${viteLib} --bundler=vite --inSourceTests --no-interactive`
+    );
+    expect(() => {
+      checkFilesExist(`libs/${viteLib}/src/lib/${viteLib}.spec.tsx`);
+    }).toThrow();
+
+    runCLI(
+      `generate @nrwl/react:component comp1 --inSourceTests --export --project=${viteLib} --no-interactive`
+    );
+    expect(() => {
+      checkFilesExist(`libs/${viteLib}/src/lib/comp1/comp1.spec.tsx`);
+    }).toThrow();
+
+    runCLI(
+      `generate @nrwl/react:component comp2 --export --project=${viteLib} --no-interactive`
+    );
+    checkFilesExist(`libs/${viteLib}/src/lib/comp2/comp2.spec.tsx`);
+
+    const libTestResults = await runCLIAsync(`test ${viteLib}`);
+    expect(libTestResults.combinedOutput).toContain(
+      'Successfully ran target test'
+    );
+
+    const libLintResults = await runCLIAsync(`lint ${viteLib}`);
+    expect(libLintResults.combinedOutput).toContain(
+      'Successfully ran target lint'
+    );
+
+    await runCLIAsync(`build ${viteLib}`);
+    checkFilesExist(
+      `dist/libs/${viteLib}/index.d.ts`,
+      `dist/libs/${viteLib}/index.js`,
+      `dist/libs/${viteLib}/index.mjs`
+    );
+  }, 300_000);
+
   it('should support bundling with Vite', async () => {
     const viteLib = uniq('vitelib');
 
