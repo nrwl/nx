e2f362cb5c09e0970a60d6db664c972a29590b2e
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-12-16 14:44:53 +0100

    feat(angular): skip invalid projects or targets instead of erroring when migrating from angular cli to nx monorepo

--- packages/angular/src/generators/ng-add/utilities/index.ts
+++ packages/angular/src/generators/ng-add/utilities/index.ts
@@ -4,6 +4,5 @@ export * from './get-all-projects';
 export * from './logger';
 export * from './normalize-options';
 export * from './types';
-export * from './validate-projects';
 export * from './validation-logging';
 export * from './workspace';
