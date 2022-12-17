ff3661538c117f5f249ba4bdefdbf112f805f632
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-12-17 13:15:36 +0100

    fix(misc): set schema when converting a project to standalone configuration

--- packages/workspace/src/generators/convert-to-nx-project/convert-to-nx-project.spec.ts
+++ packages/workspace/src/generators/convert-to-nx-project/convert-to-nx-project.spec.ts
@@ -1,20 +1,17 @@
+import * as devkit from '@nrwl/devkit';
 import {
   ProjectConfiguration,
   readJson,
   readProjectConfiguration,
 } from '@nrwl/devkit';
-import {
-  createTreeWithEmptyV1Workspace,
-  createTreeWithEmptyWorkspace,
-} from '@nrwl/devkit/testing';
-import enquirer = require('enquirer');
+import { createTreeWithEmptyV1Workspace } from '@nrwl/devkit/testing';
+import { getRelativeProjectJsonSchemaPath } from 'nx/src/generators/utils/project-configuration';
 import { libraryGenerator } from '../library/library';
-import * as devkit from '@nrwl/devkit';
-
 import convertToNxProject, {
   SCHEMA_OPTIONS_ARE_MUTUALLY_EXCLUSIVE,
 } from './convert-to-nx-project';
 import { getProjectConfigurationPath } from './utils/get-project-configuration-path';
+import enquirer = require('enquirer');
 
 jest.mock('fs-extra', () => ({
   ...jest.requireActual<any>('fs-extra'),
@@ -88,7 +85,11 @@ describe('convert-to-nx-project', () => {
       getProjectConfigurationPath(config)
     );
 
+    expect(newConfigFile.$schema).toBe(
+      getRelativeProjectJsonSchemaPath(tree, config)
+    );
     delete config.root;
+    delete newConfigFile.$schema;
     expect(config).toEqual(newConfigFile);
   });
 
@@ -116,7 +117,11 @@ describe('convert-to-nx-project', () => {
         tree,
         getProjectConfigurationPath(config)
       );
+      expect(newConfigFile.$schema).toBe(
+        getRelativeProjectJsonSchemaPath(tree, config)
+      );
       delete config.root;
+      delete newConfigFile.$schema;
       expect(config).toEqual(newConfigFile);
     }
   });
