7f6237795c40825d9052e2970e995ab1b0f307ef
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2022-12-17 12:04:30 +0100

    fix(angular): handle not provided path when generating a component without the project option

--- packages/angular/src/generators/component/lib/normalize-options.ts
+++ packages/angular/src/generators/component/lib/normalize-options.ts
@@ -2,7 +2,6 @@ import type { Tree } from '@nrwl/devkit';
 import {
   createProjectGraphAsync,
   joinPathFragments,
-  readCachedProjectGraph,
   readProjectConfiguration,
   readWorkspaceConfiguration,
 } from '@nrwl/devkit';
@@ -15,7 +14,10 @@ import {
 async function findProjectFromOptions(options: Schema) {
   const projectGraph = await createProjectGraphAsync();
   const projectRootMappings = createProjectRootMappings(projectGraph.nodes);
-  return findProjectForPath(options.path, projectRootMappings);
+
+  // path can be undefined when running on the root of the workspace, we default to the root
+  // to handle standalone layouts
+  return findProjectForPath(options.path || '', projectRootMappings);
 }
 
 export async function normalizeOptions(
@@ -26,6 +28,22 @@ export async function normalizeOptions(
     options.project ??
     (await findProjectFromOptions(options)) ??
     readWorkspaceConfiguration(tree).defaultProject;
+
+  if (!project) {
+    // path is hidden, so if not provided we don't suggest setting it
+    if (!options.path) {
+      throw new Error(
+        'No "project" was specified and "defaultProject" is not set in the workspace configuration. Please provide the "project" option and try again.'
+      );
+    }
+
+    // path was provided, so it's wrong and we should mention it
+    throw new Error(
+      'The provided "path" is wrong and no "project" was specified and "defaultProject" is not set in the workspace configuration. ' +
+        'Please provide a correct "path" or provide the "project" option instead and try again.'
+    );
+  }
+
   const { projectType, root, sourceRoot } = readProjectConfiguration(
     tree,
     project
