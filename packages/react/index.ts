1380ddf91f7ff2da5db7456b41759ee56335191d
Nicolas Beaussart <nic.beaussart@gmail.com>
2022-12-27 18:16:49 +0100

    feat(react): expose setupTailwindGenerator from @nrwl/react (#14029)

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -20,4 +20,5 @@ export { hostGenerator } from './src/generators/host/host';
 export { remoteGenerator } from './src/generators/remote/remote';
 export { cypressComponentConfigGenerator } from './src/generators/cypress-component-configuration/cypress-component-configuration';
 export { componentTestGenerator } from './src/generators/component-test/component-test';
+export { setupTailwindGenerator } from './src/generators/setup-tailwind/setup-tailwind';
 export type { SupportedStyles } from './typings/style';
