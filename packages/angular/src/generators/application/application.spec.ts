e9056bffd2cc539508fc044e95f03eeb59c85664
Dirk Odendaal <dirko.odendaal@rabobank.nl>
2022-12-28 11:58:25 +0100

    feat(misc): update unittest to original snapshot check

--- packages/angular/src/generators/application/application.spec.ts
+++ packages/angular/src/generators/application/application.spec.ts
@@ -1046,20 +1046,15 @@ describe('app', () => {
     // ASSERT
     expect(appTree.read('apps/myapp/src/main.ts', 'utf-8'))
       .toMatchInlineSnapshot(`
-        "import { enableProdMode } from '@angular/core';
-        import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+      "import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-        import { AppModule } from './app/app.module';
-        import { environment } from './environments/environment';
+      import { AppModule } from './app/app.module';
 
-        if (environment.production) {
-          enableProdMode();
-        }
 
-        platformBrowserDynamic().bootstrapModule(AppModule)
-          .catch(err => console.error(err));
-        "
-      `);
+      platformBrowserDynamic().bootstrapModule(AppModule)
+        .catch(err => console.error(err));
+      "
+    `);
   });
 
   describe('--skipStarterTemplate', () => {
