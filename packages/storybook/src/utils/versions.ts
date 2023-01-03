00db9e3a22318c342c7396054e7339298603d80a
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2023-01-03 18:56:16 +0200

    fix(storybook): update storybook to latest 6 (#14110)

--- packages/storybook/src/utils/versions.ts
+++ packages/storybook/src/utils/versions.ts
@@ -1,12 +1,12 @@
 export const nxVersion = require('../../package.json').version;
-export const storybookVersion = '~6.5.9';
+export const storybookVersion = '^6.5.15';
 export const babelCoreVersion = '7.12.13';
 export const babelLoaderVersion = '8.1.0';
 export const babelPresetTypescriptVersion = '7.12.13';
 export const svgrVersion = '^5.4.0';
 export const urlLoaderVersion = '^3.0.0';
 export const webpack5Version = '^5.64.0';
-export const viteBuilderVeresion = '0.2.5';
+export const viteBuilderVersion = '^0.2.6';
 export const storybookReactNativeVersion = '^6.0.1-beta.9';
 export const reactNativeStorybookLoader = '^2.0.5';
 export const storybookSwcAddonVersion = '^1.1.7';
