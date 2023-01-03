16a0891d30c39f241603a38ff0d012a804746c43
Colum Ferry <cferry09@gmail.com>
2023-01-03 13:35:53 +0000

    feat(angular): add parent flag to ngrx generator (#14105)

--- e2e/angular-extensions/src/ngrx.test.ts
+++ e2e/angular-extensions/src/ngrx.test.ts
@@ -27,7 +27,7 @@ describe('Angular Package', () => {
 
       // Generate root ngrx state management
       runCLI(
-        `generate @nrwl/angular:ngrx users --module=apps/${myapp}/src/app/app.module.ts --root --minimal=false`
+        `generate @nrwl/angular:ngrx users --parent=apps/${myapp}/src/app/app.module.ts --root --minimal=false`
       );
       const packageJson = readJson('package.json');
       expect(packageJson.dependencies['@ngrx/store']).toBeDefined();
@@ -39,7 +39,7 @@ describe('Angular Package', () => {
       // Generate feature library and ngrx state within that library
       runCLI(`g @nrwl/angular:lib ${mylib} --prefix=fl`);
       runCLI(
-        `generate @nrwl/angular:ngrx flights --module=libs/${mylib}/src/lib/${mylib}.module.ts --facade`
+        `generate @nrwl/angular:ngrx flights --parent=libs/${mylib}/src/lib/${mylib}.module.ts --facade`
       );
 
       expect(runCLI(`build ${myapp}`)).toMatch(/main\.[a-z0-9]+\.js/);
@@ -56,7 +56,40 @@ describe('Angular Package', () => {
 
       // Generate root ngrx state management
       runCLI(
-        `generate @nrwl/angular:ngrx users --module=apps/${myapp}/src/app/app.module.ts --root`
+        `generate @nrwl/angular:ngrx users --parent=apps/${myapp}/src/app/app.module.ts --root`
+      );
+      const packageJson = readJson('package.json');
+      expect(packageJson.dependencies['@ngrx/entity']).toBeDefined();
+      expect(packageJson.dependencies['@ngrx/store']).toBeDefined();
+      expect(packageJson.dependencies['@ngrx/effects']).toBeDefined();
+      expect(packageJson.dependencies['@ngrx/router-store']).toBeDefined();
+      expect(packageJson.devDependencies['@ngrx/schematics']).toBeDefined();
+      expect(packageJson.devDependencies['@ngrx/store-devtools']).toBeDefined();
+
+      const mylib = uniq('mylib');
+      // Generate feature library and ngrx state within that library
+      runCLI(`g @nrwl/angular:lib ${mylib} --prefix=fl`);
+
+      const flags = `--facade --barrels`;
+      runCLI(
+        `generate @nrwl/angular:ngrx flights --parent=libs/${mylib}/src/lib/${mylib}.module.ts ${flags}`
+      );
+
+      expect(runCLI(`build ${myapp}`)).toMatch(/main\.[a-z0-9]+\.js/);
+      expectTestsPass(await runCLIAsync(`test ${myapp} --no-watch`));
+      // TODO: remove this condition
+      if (getSelectedPackageManager() !== 'pnpm') {
+        expectTestsPass(await runCLIAsync(`test ${mylib} --no-watch`));
+      }
+    }, 1000000);
+
+    it('should work with creators using --module', async () => {
+      const myapp = uniq('myapp');
+      runCLI(`generate @nrwl/angular:app ${myapp} --routing --no-interactive`);
+
+      // Generate root ngrx state management
+      runCLI(
+        `generate @nrwl/angular:ngrx users --parent=apps/${myapp}/src/app/app.module.ts --root`
       );
       const packageJson = readJson('package.json');
       expect(packageJson.dependencies['@ngrx/entity']).toBeDefined();
--- packages/angular/src/generators/ngrx/schema.d.ts
+++ packages/angular/src/generators/ngrx/schema.d.ts
@@ -1,7 +1,8 @@
 export interface NgRxGeneratorOptions {
   directory: string;
   minimal: boolean;
-  module: string;
+  module?: string;
+  parent?: string;
   name: string;
   barrels?: boolean;
   facade?: boolean;
