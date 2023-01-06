88f11a47ab32a6a09c30a5fb8d98645892fc5b8b
Jack Hsu <jack.hsu@gmail.com>
2023-01-06 09:41:11 -0500

    feat(nextjs): update to Next.js 13.1 (#14186)

--- packages/next/src/utils/versions.ts
+++ packages/next/src/utils/versions.ts
@@ -1,7 +1,7 @@
 export const nxVersion = require('../../package.json').version;
 
-export const nextVersion = '13.0.0';
-export const eslintConfigNextVersion = '13.0.0';
+export const nextVersion = '13.1.1';
+export const eslintConfigNextVersion = '13.1.1';
 export const sassVersion = '1.55.0';
 export const lessLoader = '11.1.0';
 export const stylusLoader = '7.1.0';
