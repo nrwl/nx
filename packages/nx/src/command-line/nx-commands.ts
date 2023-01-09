d6b7d782eb8a1e8de355af8a1585e4a3fd73f691
AgentEnder <craigorycoppola@gmail.com>
2023-01-09 12:34:07 -0500

    fix(core): `exec` should not log usage info when inner command fails

--- packages/nx/src/command-line/nx-commands.ts
+++ packages/nx/src/command-line/nx-commands.ts
@@ -378,8 +378,12 @@ export const commandsObject = yargs
     describe: 'Executes any command as if it was a target on the project',
     builder: (yargs) => withRunOneOptions(yargs),
     handler: async (args) => {
-      await (await import('./exec')).nxExecCommand(withOverrides(args));
-      process.exit(0);
+      try {
+        await (await import('./exec')).nxExecCommand(withOverrides(args));
+        process.exit(0);
+      } catch (e) {
+        process.exit(1);
+      }
     },
   })
   .command({
