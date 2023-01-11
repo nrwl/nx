0bc93ee83d710fc15d705cbdc5dae71abe321452
Caleb Ukle <caleb@nrwl.io>
2023-01-10 18:48:29 -0600

    feat(testing): Cypress 12 Support (#14058)

--- packages/cypress/src/utils/versions.ts
+++ packages/cypress/src/utils/versions.ts
@@ -1,8 +1,8 @@
 export const nxVersion = require('../../package.json').version;
 export const eslintPluginCypressVersion = '^2.10.3';
 export const typesNodeVersion = '16.11.7';
-export const cypressVersion = '^11.0.0';
 export const cypressViteDevServerVersion = '^2.2.1';
+export const cypressVersion = '^12.2.0';
 export const cypressWebpackVersion = '^2.0.0';
 export const webpackHttpPluginVersion = '^5.5.0';
 export const viteVersion = '^4.0.1';
