7a16543f0c66c735bf8c842027b9817977292012
Sushruth Shastry <2114206+sushruth@users.noreply.github.com>
2023-01-12 20:57:55 +0000

    cleanup(core): removing a missed unused import

--- packages/nx/src/lock-file/yarn.ts
+++ packages/nx/src/lock-file/yarn.ts
@@ -9,7 +9,6 @@ import { isRootVersion, TransitiveLookupFunctionInput } from './utils/mapping';
 import { generatePrunnedHash, hashString } from './utils/hashing';
 import { PackageJsonDeps } from './utils/pruning';
 import { sortObjectByKeys } from '../utils/object-sort';
-import { execSync } from 'child_process';
 
 type LockFileDependencies = Record<
   string,
