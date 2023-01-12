59de87b0067713d85ed635900f521c95a4ef912e
Dmitriy Stepanenko <33101123+dmitry-stepanenko@users.noreply.github.com>
2023-01-13 00:15:16 +0200

    fix(vite): expose "init" generator (#14316)

--- packages/vite/index.ts
+++ packages/vite/index.ts
@@ -2,3 +2,4 @@ export * from './src/utils/versions';
 export * from './src/utils/generator-utils';
 export { viteConfigurationGenerator } from './src/generators/configuration/configuration';
 export { vitestGenerator } from './src/generators/vitest/vitest-generator';
+export { initGenerator } from './src/generators/init/init';
