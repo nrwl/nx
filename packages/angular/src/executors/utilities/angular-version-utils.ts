fd5dffefd6ee0292cd169a6c2470e709316ce06c
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-01-16 12:21:09 +0100

    cleanup(angular): reduce sourcemap loading and decoding in ng-packagr-lite executor (#14360)
    (cherry picked from commit 90ca4c949a82dffdd81c1fea87fae62a2227cc5e)

--- packages/angular/src/executors/utilities/angular-version-utils.ts
+++ packages/angular/src/executors/utilities/angular-version-utils.ts
@@ -1,13 +1,19 @@
 import { readModulePackageJson } from 'nx/src/utils/package-json';
 import { major } from 'semver';
 
-type AngularVersionInfo = { major: number; version: string };
+type VersionInfo = { major: number; version: string };
 
-export function getInstalledAngularVersionInfo(): AngularVersionInfo | null {
+export function getInstalledAngularVersionInfo(): VersionInfo | null {
+  return getInstalledPackageVersionInfo('@angular/core');
+}
+
+export function getInstalledPackageVersionInfo(
+  pkgName: string
+): VersionInfo | null {
   try {
     const {
       packageJson: { version },
-    } = readModulePackageJson('@angular/core');
+    } = readModulePackageJson(pkgName);
 
     return { major: major(version), version };
   } catch {
