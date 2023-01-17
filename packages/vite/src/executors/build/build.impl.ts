18421f6f3dc3fe2a973caff0bdb1ed7727547789
Victor Berchet <victor@suumit.com>
2023-01-16 19:28:44 -0800

    feat(vite): enable watch mode for preview

--- packages/vite/src/executors/build/build.impl.ts
+++ packages/vite/src/executors/build/build.impl.ts
@@ -10,7 +10,7 @@ import { copyAssets } from '@nrwl/js';
 import { existsSync } from 'fs';
 import { resolve } from 'path';
 
-export default async function viteBuildExecutor(
+export default async function* viteBuildExecutor(
   options: ViteBuildExecutorOptions,
   context: ExecutorContext
 ) {
@@ -24,7 +24,7 @@ export default async function viteBuildExecutor(
     }
   );
 
-  await runInstance(buildConfig);
+  const watcherOrOutput = await runInstance(buildConfig);
 
   const libraryPackageJson = resolve(projectRoot, 'package.json');
   const rootPackageJson = resolve(context.root, 'package.json');
@@ -49,11 +49,56 @@ export default async function viteBuildExecutor(
     );
   }
 
-  return { success: true };
+  if ('on' in watcherOrOutput) {
+    // watcherOrOutput is a RollupWatcher.
+    // event is a RollupWatcherEvent.
+    const emitter = makeEmitter();
+    let success = true;
+    watcherOrOutput.on('event', (event: any) => {
+      if (event.code === 'START') {
+        success = true;
+      } else if (event.code === 'ERROR') {
+        success = false;
+      } else if (event.code === 'END') {
+        emitter.push({ success });
+      }
+      // result must be closed when present.
+      // see https://rollupjs.org/guide/en/#rollupwatch
+      event.result?.close();
+    });
+    yield* emitter;
+  } else {
+    yield { success: true };
+  }
 }
 
 function runInstance(options: InlineConfig) {
   return build({
     ...options,
   });
+}
+
+/**
+ * Helper to create an async iterator.
+ * Calling push on the returned object emits the value.
+ */
+function makeEmitter() {
+  const events = [];
+  let resolve: (value: unknown) => void | null;
+
+  return {
+    push: (event) => {
+      events.push(event);
+      resolve?.(event);
+      resolve = null;
+    },
+    [Symbol.asyncIterator]: () => ({
+      next: async () => {
+        if (events.length == 0) {
+          await new Promise((r) => (resolve = r));
+        }
+        return { value: events.shift(), done: false };
+      },
+    }),
+  };
 }
