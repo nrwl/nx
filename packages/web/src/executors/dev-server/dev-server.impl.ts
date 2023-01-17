00336d85f5931c765d4a347cf652e37c1accfc51
Jack Hsu <jack.hsu@gmail.com>
2023-01-17 08:05:48 -0500

    fix(web): remove hard dependencies to @nrwl/vite and @nrwl/webpack (#14408)
    (cherry picked from commit e0bd3382253a9840415b007e9c56b5ff06d9865c)

--- packages/web/src/executors/dev-server/dev-server.impl.ts
+++ packages/web/src/executors/dev-server/dev-server.impl.ts
@@ -2,7 +2,19 @@
  * This is here for backwards-compat.
  * TODO(jack): remove in Nx 16.
  */
-import { devServerExecutor, WebDevServerOptions } from '@nrwl/webpack';
+import { ExecutorContext } from '@nrwl/devkit';
 
-export { devServerExecutor, WebDevServerOptions };
+export async function* devServerExecutor(
+  options: any,
+  context: ExecutorContext
+) {
+  const { devServerExecutor: baseDevServerExecutor } = require('@nrwl/webpack');
+  yield* baseDevServerExecutor(
+    {
+      ...options,
+      target: 'web',
+    },
+    context
+  );
+}
 export default devServerExecutor;
