944882fb52d41882b725fbe815cb5ade30f43270
Jonathan Cammisuli <jon@cammisuli.ca>
2023-01-23 14:08:27 -0500

    feat(core): always copy the local native bin to the dist folder

--- scripts/copy-local-native.js
+++ scripts/copy-local-native.js
@@ -4,11 +4,6 @@ const glob = require('fast-glob');
 
 const p = process.argv[2];
 
-// Only copy the .node binaries when it is a local release
-if (!process.env.LOCAL_RELEASE) {
-  process.exit(0);
-}
-
 const nativeFiles = glob.sync(`packages/${p}/**/*.node`);
 
 nativeFiles.forEach((file) => {
