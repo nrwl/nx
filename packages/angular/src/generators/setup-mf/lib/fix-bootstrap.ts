954729b3e0fb33b9443507ca94ea0092275d3e12
4javier <4javier4@gmail.com>
2023-01-26 13:33:32 +0100

    fix(angular): remove import of missing environment file
    angular 15 doesn't generate environment.ts files by default anymore
    because ngDevMode is disabled by optimization flag in builder
    
    closed #14510

--- packages/angular/src/generators/setup-mf/lib/fix-bootstrap.ts
+++ packages/angular/src/generators/setup-mf/lib/fix-bootstrap.ts
@@ -28,16 +28,11 @@ export function fixBootstrap(tree: Tree, appRoot: string, options: Schema) {
   );
 }
 
-const standaloneBootstrapCode = `import {environment} from "./environments/environment";
-import {enableProdMode, importProvidersFrom} from "@angular/core";
+const standaloneBootstrapCode = `import {importProvidersFrom} from "@angular/core";
 import {bootstrapApplication} from "@angular/platform-browser";
 import {RouterModule} from "@angular/router";
 import {RemoteEntryComponent} from "./app/remote-entry/entry.component";
 import {appRoutes} from "./app/app.routes";
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(RemoteEntryComponent, {
   providers: [
