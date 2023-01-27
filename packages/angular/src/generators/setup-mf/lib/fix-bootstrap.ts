c3dd001b8da12fc437ed853e9ffe0bd54cce3261
Colum Ferry <cferry09@gmail.com>
2023-01-27 16:18:35 +0000

    feat(angular): add support for ng14 for standalone mf

--- packages/angular/src/generators/setup-mf/lib/fix-bootstrap.ts
+++ packages/angular/src/generators/setup-mf/lib/fix-bootstrap.ts
@@ -1,12 +1,17 @@
 import type { Tree } from '@nrwl/devkit';
 import { joinPathFragments } from '@nrwl/devkit';
 import type { Schema } from '../schema';
+import { getInstalledAngularMajorVersion } from '../../utils/version-utils';
 
 export function fixBootstrap(tree: Tree, appRoot: string, options: Schema) {
   const mainFilePath = joinPathFragments(appRoot, 'src/main.ts');
   const bootstrapCode = tree.read(mainFilePath, 'utf-8');
+  const installedAngularMajor = getInstalledAngularMajorVersion(tree);
   if (options.standalone) {
-    tree.write(`${appRoot}/src/bootstrap.ts`, standaloneBootstrapCode);
+    tree.write(
+      `${appRoot}/src/bootstrap.ts`,
+      standaloneBootstrapCode(installedAngularMajor === 14)
+    );
   } else {
     tree.write(joinPathFragments(appRoot, 'src/bootstrap.ts'), bootstrapCode);
   }
@@ -28,12 +33,23 @@ export function fixBootstrap(tree: Tree, appRoot: string, options: Schema) {
   );
 }
 
-const standaloneBootstrapCode = `import {importProvidersFrom} from "@angular/core";
+const standaloneBootstrapCode = (
+  includeEnvironments: boolean = false
+) => `import {importProvidersFrom} from "@angular/core";
 import {bootstrapApplication} from "@angular/platform-browser";
 import {RouterModule} from "@angular/router";
 import {RemoteEntryComponent} from "./app/remote-entry/entry.component";
 import {appRoutes} from "./app/app.routes";
-
+${
+  includeEnvironments
+    ? `import {enableProdMode} from '@angular/core';
+import {environment} from './environments/environment';
+if(environment.production) {
+  enableProdMode();
+}
+`
+    : ``
+}
 bootstrapApplication(RemoteEntryComponent, {
   providers: [
     importProvidersFrom(
