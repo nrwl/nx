1bb8ba95495efecb456503a24741e8cc831ce795
Jack Hsu <jack.hsu@gmail.com>
2023-01-27 08:12:24 -0500

    fix(react): install @nrwl/web when module federation is used

--- packages/react/src/generators/remote/remote.spec.ts
+++ packages/react/src/generators/remote/remote.spec.ts
@@ -1,12 +1,27 @@
-import { readNxJson } from '@nrwl/devkit';
+import { readJson, readNxJson } from '@nrwl/devkit';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
 import { Linter } from '@nrwl/linter';
 import remote from './remote';
 
 describe('remote generator', () => {
+  it('should install @nrwl/web for the file-server executor', async () => {
+    const tree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
+    await remote(tree, {
+      name: 'test',
+      devServerPort: 4201,
+      e2eTestRunner: 'cypress',
+      linter: Linter.EsLint,
+      skipFormat: false,
+      style: 'css',
+      unitTestRunner: 'jest',
+    });
+
+    const packageJson = readJson(tree, 'package.json');
+    expect(packageJson.devDependencies['@nrwl/web']).toBeDefined();
+  });
+
   it('should not set the remote as the default project', async () => {
     const tree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
-
     await remote(tree, {
       name: 'test',
       devServerPort: 4201,
