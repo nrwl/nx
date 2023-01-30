de8dc4d996354c647c3df14aa6fca4379eb0a34c
Jonathan Cammisuli <jon@cammisuli.ca>
2023-01-30 11:40:28 -0500

    fix(core): add tests for native hasher

--- packages/nx/src/native/native_hasher.rs
+++ packages/nx/src/native/native_hasher.rs
@@ -82,3 +82,86 @@ fn hash_files(workspace_root: String) -> HashMap<String, String> {
     drop(sender);
     receiver_thread.join().unwrap()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_fs::prelude::*;
+    use assert_fs::TempDir;
+
+    ///
+    /// Setup a temporary directory to do testing in
+    ///
+    fn setup_fs() -> TempDir {
+        let temp = TempDir::new().unwrap();
+        temp.child("test.txt").write_str("content").unwrap();
+        temp.child("foo.txt").write_str("content1").unwrap();
+        temp.child("bar.txt").write_str("content2").unwrap();
+        temp.child("baz")
+            .child("qux.txt")
+            .write_str("content@qux")
+            .unwrap();
+        temp.child("node_modules")
+            .child("node-module-dep")
+            .write_str("content")
+            .unwrap();
+        temp
+    }
+
+    #[test]
+    fn it_hashes_a_file() {
+        // handle non existent files
+        let content = hash_file("".into());
+        assert!(content.is_none());
+
+        let temp_dir = setup_fs();
+
+        let test_file_path = temp_dir.display().to_string() + "/test.txt";
+        let content = hash_file(test_file_path);
+
+        assert_eq!(content.unwrap().hash, "6193209363630369380");
+    }
+
+    #[test]
+    fn it_hashes_a_directory() {
+        // handle empty workspaces
+        let content = hash_files("/does/not/exist".into());
+        assert!(content.is_empty());
+
+        let temp_dir = setup_fs();
+
+        let content = hash_files(temp_dir.display().to_string());
+        // println!("{:?}", content);
+        assert_eq!(
+            content,
+            HashMap::from([
+                ("baz/qux.txt".into(), "8039819779822413286".into()),
+                ("foo.txt".into(), "8455857314690418558".into()),
+                ("test.txt".into(), "6193209363630369380".into()),
+                ("bar.txt".into(), "1707056588989152788".into()),
+            ])
+        );
+    }
+
+    #[test]
+    fn handles_nx_ignore() {
+        let temp_dir = setup_fs();
+
+        // add nxignore file with baz/
+        temp_dir.child(".nxignore").write_str("baz/").unwrap();
+
+        let content = hash_files(temp_dir.display().to_string());
+        assert_eq!(
+            content,
+            HashMap::from([
+                ("foo.txt".into(), "8455857314690418558".into()),
+                ("test.txt".into(), "6193209363630369380".into()),
+                ("bar.txt".into(), "1707056588989152788".into()),
+                (".nxignore".into(), "5786346484289078730".into())
+            ])
+        );
+    }
+}
+//
+
+//
--- packages/nx/src/native/tests/native.spec.ts
+++ packages/nx/src/native/tests/native.spec.ts
@@ -1,10 +1,18 @@
 import { hashFile } from '../index';
 
-// TODO(cammisuli): write tests
+import { tmpdir } from 'os';
+import { mkdtemp, writeFile } from 'fs-extra';
+import { join } from 'path';
+
 describe('native', () => {
-  it('should hash', () => {
+  it('should hash', async () => {
     expect(hashFile).toBeDefined();
-    // expect(hash('Test')).toMatchInlineSnapshot(`"12967476824633224542"`);
+
+    const tempDirPath = await mkdtemp(join(tmpdir(), 'native-test'));
+    const tempFilePath = join(tempDirPath, 'temp.txt');
+    await writeFile(tempFilePath, 'content');
+
+    expect(hashFile(tempFilePath).hash).toBe('6193209363630369380');
   });
 
   it('should create an instance of NativeHasher', () => {
