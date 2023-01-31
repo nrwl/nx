825972f318b9a3cb95a3cc70b9226398b4b32b82
Jonathan Cammisuli <jon@cammisuli.ca>
2023-01-31 14:14:13 -0500

    fix(core): add preinstall script to check for node and cargo

---
+++ scripts/preinstall.js
@@ -0,0 +1,28 @@
+/*
+This preinstall script will check that the necessary dependencies are installed
+Checks for:
+    * Node 18+
+    * Cargo
+ */
+
+const semver = require('semver');
+const childProcess = require('child_process');
+
+// Check node version
+const nodeVersion = semver.parse(process.version);
+if (nodeVersion.major < 18) {
+  console.error(
+    'Please make sure that your installed Node version is greater than v18'
+  );
+  process.exit(1);
+}
+
+// Check for cargo
+try {
+  childProcess.execSync('cargo --version');
+} catch {
+  console.error(
+    'Could not find Cargo. Please make sure that Cargo and Rust is installed with https://rustup.rs'
+  );
+  process.exit(1);
+}
