4ff59ff3cb5cf4cac6297ebebe2c113eadaa9557
Jack Hsu <jack.hsu@gmail.com>
2023-02-03 11:49:39 -0500

    chore(repo): inline native module requires so jest CLI runs successfully (#14796)

--- packages/nx/src/hasher/native-file-hasher.ts
+++ packages/nx/src/hasher/native-file-hasher.ts
@@ -1,11 +1,12 @@
 import { FileHasherBase } from './file-hasher-base';
 import { performance } from 'perf_hooks';
-import { hashFile, hashFiles } from '../native';
 import { workspaceRoot } from '../utils/app-root';
 
 export class NativeFileHasher extends FileHasherBase {
   async init(): Promise<void> {
     performance.mark('init hashing:start');
+    // Import as needed. There is also an issue running unit tests in Nx repo if this is a top-level import.
+    const { hashFiles } = require('../native');
     this.clear();
     const filesObject = hashFiles(workspaceRoot);
     this.fileHashes = new Map(Object.entries(filesObject));
@@ -29,6 +30,8 @@ export class NativeFileHasher extends FileHasherBase {
   }
 
   hashFile(path: string): string {
+    // Import as needed. There is also an issue running unit tests in Nx repo if this is a top-level import.
+    const { hashFile } = require('../native');
     return hashFile(path).hash;
   }
 }
