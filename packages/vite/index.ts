4095b953cead8059d03972a336872b993ed950f1
Dmitriy Stepanenko <33101123+dmitry-stepanenko@users.noreply.github.com>
2023-02-16 10:25:42 +0200

    fix(vite): explicitly export type instead of a value from d.ts files (#15029)
    (cherry picked from commit 8ce0b3cdc79619885acc94e802557006f501e305)

--- packages/vite/index.ts
+++ packages/vite/index.ts
@@ -1,8 +1,8 @@
 export * from './src/utils/versions';
 export * from './src/utils/generator-utils';
-export { ViteConfigurationGeneratorSchema } from './src/generators/configuration/schema';
+export { type ViteConfigurationGeneratorSchema } from './src/generators/configuration/schema';
 export { viteConfigurationGenerator } from './src/generators/configuration/configuration';
-export { VitestGeneratorSchema } from './src/generators/vitest/schema';
+export { type VitestGeneratorSchema } from './src/generators/vitest/schema';
 export { vitestGenerator } from './src/generators/vitest/vitest-generator';
-export { InitGeneratorSchema } from './src/generators/init/schema';
+export { type InitGeneratorSchema } from './src/generators/init/schema';
 export { initGenerator } from './src/generators/init/init';
