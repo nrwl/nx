3a98c6d3ab02f44054f077da7a521b1bf9376fa1
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-02-17 17:23:05 +0000

    feat(misc): support ts 4.9 (#14899)

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -1,7 +1,7 @@
 export const nxVersion = require('../../package.json').version;
 
 export const angularCliVersion = '~15.1.0';
-export const typescriptVersion = '~4.8.2';
+export const typescriptVersion = '~4.9.5';
 export const prettierVersion = '^2.6.2';
 export const typescriptESLintVersion = '^5.36.1';
 export const eslintVersion = '~8.15.0';
