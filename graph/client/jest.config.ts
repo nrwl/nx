a676c26c86c979f5f150ea56bd343c6af7d7dfe6
FrozenPandaz <jasonjean1993@gmail.com>
2023-02-24 15:09:11 -0500

    chore(repo): update nx to 15.8.0-beta.7

--- graph/client/jest.config.ts
+++ graph/client/jest.config.ts
@@ -16,4 +16,14 @@ export default {
   modulePathIgnorePatterns: [
     '/graph/client/src/app/machines/match-media-mock.spec.ts',
   ],
+  /* TODO: Update to latest Jest snapshotFormat
+   * By default Nx has kept the older style of Jest Snapshot formats
+   * to prevent breaking of any existing tests with snapshots.
+   * It's recommend you update to the latest format.
+   * You can do this by removing snapshotFormat property
+   * and running tests with --update-snapshot flag.
+   * Example: From within the project directory, run "nx test --update-snapshot"
+   * More info: https://jestjs.io/docs/upgrading-to-jest29#snapshot-format
+   */
+  snapshotFormat: { escapeString: true, printBasicPrototype: true },
 };
--- nx-dev/models-menu/jest.config.ts
+++ nx-dev/models-menu/jest.config.ts
@@ -1,14 +1,15 @@
 /* eslint-disable */
 export default {
   displayName: 'nx-dev-models-menu',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/nx-dev/models-menu',
