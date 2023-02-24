1a9517deb23302471608fa6a439aa066775ca267
Caleb Ukle <caleb.ukle+github@pm.me>
2023-02-24 15:21:30 -0600

    chore(testing): clean up angular CT e2e tests

--- e2e/angular-extensions/src/cypress-component-tests.test.ts
+++ e2e/angular-extensions/src/cypress-component-tests.test.ts
@@ -19,154 +19,15 @@ describe('Angular Cypress Component Tests', () => {
 
   beforeAll(async () => {
     projectName = newProject({ name: uniq('cy-ng') });
-    runCLI(`generate @nrwl/angular:app ${appName} --no-interactive`);
-    runCLI(
-      `generate @nrwl/angular:component fancy-component --project=${appName} --no-interactive`
-    );
-    runCLI(`generate @nrwl/angular:lib ${usedInAppLibName} --no-interactive`);
-    runCLI(
-      `generate @nrwl/angular:component btn --project=${usedInAppLibName} --inlineTemplate --inlineStyle --export --no-interactive`
-    );
-    runCLI(
-      `generate @nrwl/angular:component btn-standalone --project=${usedInAppLibName} --inlineTemplate --inlineStyle --export --standalone --no-interactive`
-    );
-    updateFile(
-      `libs/${usedInAppLibName}/src/lib/btn/btn.component.ts`,
-      `
-import { Component, Input } from '@angular/core';
 
-@Component({
-  selector: '${projectName}-btn',
-  template: '<button class="text-green-500">{{text}}</button>',
-  styles: []
-})
-export class BtnComponent {
-  @Input() text = 'something';
-}
-`
-    );
-    updateFile(
-      `libs/${usedInAppLibName}/src/lib/btn-standalone/btn-standalone.component.ts`,
-      `
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-@Component({
-  selector: '${projectName}-btn-standalone',
-  standalone: true,
-  imports: [CommonModule],
-  template: '<button class="text-green-500">standlone-{{text}}</button>',
-  styles: [],
-})
-export class BtnStandaloneComponent {
-  @Input() text = 'something';
-}
-`
-    );
-    // use lib in the app
-    createFile(
-      `apps/${appName}/src/app/app.component.html`,
-      `
-<${projectName}-btn></${projectName}-btn>
-<${projectName}-btn-standalone></${projectName}-btn-standalone>
-<${projectName}-nx-welcome></${projectName}-nx-welcome>
-`
-    );
-    const btnModuleName = names(usedInAppLibName).className;
-    updateFile(
-      `apps/${appName}/src/app/app.component.scss`,
-      `
-@use 'styleguide' as *;
+    createApp(appName);
 
-h1 {
-  @include headline;
-}`
-    );
-    updateFile(
-      `apps/${appName}/src/app/app.module.ts`,
-      `
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import {${btnModuleName}Module} from "@${projectName}/${usedInAppLibName}";
+    createLib(projectName, appName, usedInAppLibName);
+    useLibInApp(projectName, appName, usedInAppLibName);
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+    createBuildableLib(projectName, buildableLibName);
 
-@NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, ${btnModuleName}Module],
-  providers: [],
-  bootstrap: [AppComponent],
-})
-export class AppModule {}
-`
-    );
-
-    runCLI(
-      `generate @nrwl/angular:lib ${buildableLibName} --buildable --no-interactive`
-    );
-    runCLI(
-      `generate @nrwl/angular:component input --project=${buildableLibName} --inlineTemplate --inlineStyle --export --no-interactive`
-    );
-    runCLI(
-      `generate @nrwl/angular:component input-standalone --project=${buildableLibName} --inlineTemplate --inlineStyle --export --standalone --no-interactive`
-    );
-    updateFile(
-      `libs/${buildableLibName}/src/lib/input/input.component.ts`,
-      `
-import {Component, Input} from '@angular/core';
-
-@Component({
-  selector: '${projectName}-input',
-  template: \`<label class="text-green-500">Email: <input class="border-blue-500" type="email" [readOnly]="readOnly"></label>\`,
-    styles  : []
-  })
-  export class InputComponent{
-    @Input() readOnly = false;
-  }
-  `
-    );
-    updateFile(
-      `libs/${buildableLibName}/src/lib/input-standalone/input-standalone.component.ts`,
-      `
-import {Component, Input} from '@angular/core';
-import {CommonModule} from '@angular/common';
-@Component({
-  selector: '${projectName}-input-standalone',
-  standalone: true,
-  imports: [CommonModule],
-  template: \`<label class="text-green-500">Email: <input class="border-blue-500" type="email" [readOnly]="readOnly"></label>\`,
-    styles  : []
-  })
-  export class InputStandaloneComponent{
-    @Input() readOnly = false;
-  }
-  `
-    );
-
-    // make sure assets from the workspace root work.
-    createFile('libs/assets/data.json', JSON.stringify({ data: 'data' }));
-    createFile(
-      'assets/styles/styleguide.scss',
-      `
-    @mixin headline {
-    font-weight: bold;
-    color: darkkhaki;
-    background: lightcoral;
-    font-weight: 24px;
-  }
-  `
-    );
-    updateProjectConfig(appName, (config) => {
-      config.targets['build'].options.stylePreprocessorOptions = {
-        includePaths: ['assets/styles'],
-      };
-      config.targets['build'].options.assets.push({
-        glob: '**/*',
-        input: 'libs/assets',
-        output: 'assets',
-      });
-      return config;
-    });
+    useWorkspaceAssetsInApp(appName);
   });
 
   afterAll(() => cleanupProject());
@@ -200,9 +61,211 @@ import {CommonModule} from '@angular/common';
         `generate @nrwl/angular:cypress-component-configuration --project=${buildableLibName} --generate-tests --no-interactive`
       );
     }).toThrow();
-    createFile(
+
+    updateTestToAssertTailwindIsNotApplied(buildableLibName);
+
+    runCLI(
+      `generate @nrwl/angular:cypress-component-configuration --project=${buildableLibName} --generate-tests --build-target=${appName}:build --no-interactive`
+    );
+    if (runCypressTests()) {
+      expect(runCLI(`component-test ${buildableLibName} --no-watch`)).toContain(
+        'All specs passed!'
+      );
+    }
+    // add tailwind
+    runCLI(
+      `generate @nrwl/angular:setup-tailwind --project=${buildableLibName}`
+    );
+    updateFile(
       `libs/${buildableLibName}/src/lib/input/input.component.cy.ts`,
-      `
+      (content) => {
+        // text-green-500 should now apply
+        return content.replace('rgb(0, 0, 0)', 'rgb(34, 197, 94)');
+      }
+    );
+    updateFile(
+      `libs/${buildableLibName}/src/lib/input-standalone/input-standalone.component.cy.ts`,
+      (content) => {
+        // text-green-500 should now apply
+        return content.replace('rgb(0, 0, 0)', 'rgb(34, 197, 94)');
+      }
+    );
+
+    if (runCypressTests()) {
+      expect(runCLI(`component-test ${buildableLibName} --no-watch`)).toContain(
+        'All specs passed!'
+      );
+      checkFilesDoNotExist(`tmp/libs/${buildableLibName}/ct-styles.css`);
+    }
+  }, 300_000);
+});
+
+function createApp(appName: string) {
+  runCLI(`generate @nrwl/angular:app ${appName} --no-interactive`);
+  runCLI(
+    `generate @nrwl/angular:component fancy-component --project=${appName} --no-interactive`
+  );
+}
+
+function createLib(projectName: string, appName: string, libName: string) {
+  runCLI(`generate @nrwl/angular:lib ${libName} --no-interactive`);
+  runCLI(
+    `generate @nrwl/angular:component btn --project=${libName} --inlineTemplate --inlineStyle --export --no-interactive`
+  );
+  runCLI(
+    `generate @nrwl/angular:component btn-standalone --project=${libName} --inlineTemplate --inlineStyle --export --standalone --no-interactive`
+  );
+  updateFile(
+    `libs/${libName}/src/lib/btn/btn.component.ts`,
+    `
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: '${projectName}-btn',
+  template: '<button class="text-green-500">{{text}}</button>',
+  styles: []
+})
+export class BtnComponent {
+  @Input() text = 'something';
+}
+`
+  );
+  updateFile(
+    `libs/${libName}/src/lib/btn-standalone/btn-standalone.component.ts`,
+    `
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: '${projectName}-btn-standalone',
+  standalone: true,
+  imports: [CommonModule],
+  template: '<button class="text-green-500">standlone-{{text}}</button>',
+  styles: [],
+})
+export class BtnStandaloneComponent {
+  @Input() text = 'something';
+}
+`
+  );
+}
+
+function createBuildableLib(projectName: string, libName: string) {
+  // create lib
+  runCLI(`generate @nrwl/angular:lib ${libName} --buildable --no-interactive`);
+  // create cmp for lib
+  runCLI(
+    `generate @nrwl/angular:component input --project=${libName} --inlineTemplate --inlineStyle --export --no-interactive`
+  );
+  // create standlone cmp for lib
+  runCLI(
+    `generate @nrwl/angular:component input-standalone --project=${libName} --inlineTemplate --inlineStyle --export --standalone --no-interactive`
+  );
+  // update cmp implmentation to use tailwind clasasserting in tests
+  updateFile(
+    `libs/${libName}/src/lib/input/input.component.ts`,
+    `
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: '${projectName}-input',
+  template: \`<label class="text-green-500">Email: <input class="border-blue-500" type="email" [readOnly]="readOnly"></label>\`,
+    styles  : []
+  })
+  export class InputComponent{
+    @Input() readOnly = false;
+  }
+  `
+  );
+  updateFile(
+    `libs/${libName}/src/lib/input-standalone/input-standalone.component.ts`,
+    `
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+@Component({
+  selector: '${projectName}-input-standalone',
+  standalone: true,
+  imports: [CommonModule],
+  template: \`<label class="text-green-500">Email: <input class="border-blue-500" type="email" [readOnly]="readOnly"></label>\`,
+    styles  : []
+  })
+  export class InputStandaloneComponent{
+    @Input() readOnly = false;
+  }
+  `
+  );
+}
+
+function useLibInApp(projectName: string, appName: string, libName: string) {
+  createFile(
+    `apps/${appName}/src/app/app.component.html`,
+    `
+<${projectName}-btn></${projectName}-btn>
+<${projectName}-btn-standalone></${projectName}-btn-standalone>
+<${projectName}-nx-welcome></${projectName}-nx-welcome>
+`
+  );
+  const btnModuleName = names(libName).className;
+  updateFile(
+    `apps/${appName}/src/app/app.component.scss`,
+    `
+@use 'styleguide' as *;
+
+h1 {
+  @include headline;
+}`
+  );
+  updateFile(
+    `apps/${appName}/src/app/app.module.ts`,
+    `
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {${btnModuleName}Module} from "@${projectName}/${libName}";
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent],
+  imports: [BrowserModule, ${btnModuleName}Module],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+`
+  );
+}
+
+function useWorkspaceAssetsInApp(appName: string) {
+  // make sure assets from the workspace root work.
+  createFile('libs/assets/data.json', JSON.stringify({ data: 'data' }));
+  createFile(
+    'assets/styles/styleguide.scss',
+    `
+    @mixin headline {
+    font-weight: bold;
+    color: darkkhaki;
+    background: lightcoral;
+    font-weight: 24px;
+  }
+  `
+  );
+  updateProjectConfig(appName, (config) => {
+    config.targets['build'].options.stylePreprocessorOptions = {
+      includePaths: ['assets/styles'],
+    };
+    config.targets['build'].options.assets.push({
+      glob: '**/*',
+      input: 'libs/assets',
+      output: 'assets',
+    });
+    return config;
+  });
+}
+
+function updateTestToAssertTailwindIsNotApplied(libName: string) {
+  createFile(
+    `libs/${libName}/src/lib/input/input.component.cy.ts`,
+    `
 import { MountConfig } from 'cypress/angular';
 import { InputComponent } from './input.component';
 
@@ -229,11 +292,11 @@ describe(InputComponent.name, () => {
   });
 });
 `
-    );
+  );
 
-    createFile(
-      `libs/${buildableLibName}/src/lib/input-standalone/input-standalone.component.cy.ts`,
-      `
+  createFile(
+    `libs/${libName}/src/lib/input-standalone/input-standalone.component.cy.ts`,
+    `
 import { MountConfig } from 'cypress/angular';
 import { InputStandaloneComponent } from './input-standalone.component';
 
@@ -260,39 +323,5 @@ describe(InputStandaloneComponent.name, () => {
   });
 });
 `
-    );
-
-    runCLI(
-      `generate @nrwl/angular:cypress-component-configuration --project=${buildableLibName} --generate-tests --build-target=${appName}:build --no-interactive`
-    );
-    if (runCypressTests()) {
-      expect(runCLI(`component-test ${buildableLibName} --no-watch`)).toContain(
-        'All specs passed!'
-      );
-    }
-
-    // add tailwind
-    runCLI(
-      `generate @nrwl/angular:setup-tailwind --project=${buildableLibName}`
-    );
-    updateFile(
-      `libs/${buildableLibName}/src/lib/input/input.component.cy.ts`,
-      (content) => {
-        // text-green-500 should now apply
-        return content.replace('rgb(0, 0, 0)', 'rgb(34, 197, 94)');
-      }
-    );
-    updateFile(
-      `libs/${buildableLibName}/src/lib/input-standalone/input-standalone.component.cy.ts`,
-      (content) => {
-        // text-green-500 should now apply
-        return content.replace('rgb(0, 0, 0)', 'rgb(34, 197, 94)');
-      }
-    );
-
-    expect(runCLI(`component-test ${buildableLibName} --no-watch`)).toContain(
-      'All specs passed!'
-    );
-    checkFilesDoNotExist(`tmp/libs/${buildableLibName}/ct-styles.css`);
-  }, 300_000);
-});
+  );
+}
