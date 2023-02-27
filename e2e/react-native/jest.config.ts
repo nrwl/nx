92610fe4b636aa97333b17da2040039242316c1a
Jason Jean <jasonjean1993@gmail.com>
2023-02-27 17:24:06 -0500

    chore(repo): update nx to 15.8.0-beta.7 (#15293)

--- e2e/react-native/jest.config.ts
+++ e2e/react-native/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   maxWorkers: 1,
-  globals: { 'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' } },
+  globals: {},
   displayName: 'e2e-react-native',
   testTimeout: 600000,
   preset: '../../jest.preset.js',
--- e2e/storybook-angular/jest.config.ts
+++ e2e/storybook-angular/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   maxWorkers: 1,
-  globals: { 'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' } },
+  globals: {},
   displayName: 'e2e-storybook-angular',
   preset: '../../jest.preset.js',
 };
