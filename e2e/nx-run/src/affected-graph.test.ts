f111580f284914cfda8515d916829c507df80d51
Jason Jean <jasonjean1993@gmail.com>
2023-03-01 18:59:36 -0500

    fix(core): handle file renames properly for affected (#15340)
    (cherry picked from commit 7aac67c7833c82b129024204aa0f8c8b52beb62a)

--- e2e/nx-run/src/affected-graph.test.ts
+++ e2e/nx-run/src/affected-graph.test.ts
@@ -180,8 +180,6 @@ describe('Nx Affected and Graph Tests', () => {
       mylib = uniq('mylib');
       const nxJson: NxJsonConfiguration = readJson('nx.json');
 
-      delete nxJson.implicitDependencies;
-
       updateFile('nx.json', JSON.stringify(nxJson));
       runCommand(`git init`);
       runCommand(`git config user.email "test@test.com"`);
@@ -265,6 +263,24 @@ describe('Nx Affected and Graph Tests', () => {
         ...config,
         implicitDependencies: [],
       }));
+    });
+
+    it('should handle file renames', () => {
+      generateAll();
+
+      // Move file
+      updateFile(
+        `apps/${myapp2}/src/index.html`,
+        readFile(`apps/${myapp}/src/index.html`)
+      );
+      removeFile(`apps/${myapp}/src/index.html`);
+
+      const affectedProjects = runCLI(
+        'print-affected --uncommitted --select projects'
+      ).split(', ');
+
+      expect(affectedProjects).toContain(myapp);
+      expect(affectedProjects).toContain(myapp2);
     });
   });
 
