0aff9dcd5bd3e62175c44e9165311c766e512360
Craigory Coppola <craigorycoppola@gmail.com>
2023-03-02 10:39:58 -0500

    fix(core): last project should be visible in `nx show projects` (#15335)

--- packages/nx/src/command-line/show.ts
+++ packages/nx/src/command-line/show.ts
@@ -3,7 +3,10 @@ import { createProjectGraphAsync } from '../project-graph/project-graph';
 export async function show(args: { object: 'projects' }): Promise<void> {
   if (args.object == 'projects') {
     const graph = await createProjectGraphAsync();
-    process.stdout.write(Object.keys(graph.nodes).join('\n'));
+    const projects = Object.keys(graph.nodes).join('\n');
+    if (projects.length) {
+      console.log(projects);
+    }
   } else {
     throw new Error(`Unrecognized option: ${args.object}`);
   }
