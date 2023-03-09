435bad8ae11120739563900e186912bb4c6ac1e9
Isaac Mann <isaacplmann@gmail.com>
2023-03-09 14:33:11 -0500

    docs(core): add nx champions page

--- nx-dev/nx-dev/pages/community.tsx
+++ nx-dev/nx-dev/pages/community.tsx
@@ -1,7 +1,11 @@
 import {
   BeakerIcon,
+  BookOpenIcon,
   ChatBubbleLeftEllipsisIcon,
   ClipboardIcon,
+  MicrophoneIcon,
+  TrophyIcon,
+  VideoCameraIcon,
 } from '@heroicons/react/24/solid';
 import { Footer, Header, SectionHeading } from '@nx/nx-dev/ui-common';
 import {
@@ -91,12 +95,11 @@ export default function Community(props: CommunityProps): JSX.Element {
           >
             <header className="mx-auto max-w-prose px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8">
               <div>
-                <SectionHeading as="h1" variant="title" id="community">
-                  <span className="sr-only">Nx has </span> A strong and dynamic
-                  community
+                <SectionHeading as="p" variant="title" id="community">
+                  What are people saying about Nx?
                 </SectionHeading>
-                <SectionHeading as="p" variant="display" className="mt-4">
-                  It's always better when we're together.
+                <SectionHeading as="h1" variant="display" className="mt-4">
+                  Community Voices
                 </SectionHeading>
               </div>
             </header>
@@ -104,48 +107,53 @@ export default function Community(props: CommunityProps): JSX.Element {
             <div className="mx-auto px-4 py-16 lg:px-8 lg:py-32 xl:max-w-7xl">
               <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
                 <div className="relative rounded-lg border border-slate-200 bg-white/60 p-5 shadow-sm transition hover:border-green-500 hover:bg-white dark:border-slate-800/40 dark:bg-slate-800/60 dark:hover:border-green-500 dark:hover:bg-slate-800">
-                  <ChatBubbleLeftEllipsisIcon className="mb-5 inline-block h-10 w-10 text-green-500" />
-                  <h4 className="mb-2 text-lg font-bold dark:text-slate-300">
-                    Community
+                  <BookOpenIcon className="mb-5 inline-block h-10 w-10 text-green-500" />
+                  <h4 className="ml-2 inline-block align-super text-lg font-bold dark:text-slate-300">
+                    Articles
                   </h4>
-                  <a className="focus:outline-none" href="#community">
+                  <h4 className="mb-2 text-lg font-bold dark:text-slate-300">
+                    Learn to Scale React Development with Nx
+                  </h4>
+                  <img
+                    src="https://res.cloudinary.com/practicaldev/image/fetch/s--9qrpxIAn--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0p7aemoattr29ah6wc8c.png"
+                    alt="Article banner"
+                  />
+                  <a
+                    className="focus:outline-none"
+                    href="https://dev.to/nothanii/learn-to-scale-react-development-with-nx-1i3k"
+                  >
                     <span className="absolute inset-0" aria-hidden="true" />
-                    <p className="leading-relaxed">
-                      There are many ways you can connect with the open-source
-                      Nx community: Slack, Youtube, Twitter and email newsletter
-                      are available to keep you on top of all the Nx things!
-                    </p>
                   </a>
                 </div>
                 <div className="relative rounded-lg border border-slate-200 bg-white/60 p-5 shadow-sm transition hover:border-blue-500 hover:bg-white dark:border-slate-800/40 dark:bg-slate-800/60 dark:hover:border-sky-500 dark:hover:bg-slate-800">
-                  <BeakerIcon className="mb-5 inline-block h-10 w-10 text-blue-500 dark:text-sky-500" />
-                  <h4 className="mb-2 text-lg font-bold">
-                    Create and Share your own{' '}
-                    <span className="sr-only">Nx plugin</span>
+                  <VideoCameraIcon className="mb-5 inline-block h-10 w-10 text-blue-500 dark:text-sky-500" />
+                  <h4 className="ml-2 inline-block align-super text-lg font-bold dark:text-slate-300">
+                    Videos
                   </h4>
-                  <a className="focus:outline-none" href="#create-nx-plugin">
-                    <span className="absolute inset-0" aria-hidden="true" />
-                    <p className="leading-relaxed">
-                      Official Nx plugins are created and maintained by the Nx
-                      team but you can easily create your own! Follow our
-                      documentation on how to create your own plugin.
-                    </p>
-                  </a>
+                  <iframe
+                    title="Migrate from Lerna to Nx in 1 minute"
+                    loading="lazy"
+                    className="max-w-screen-sm rounded-lg shadow-lg"
+                    width="100%"
+                    src="https://www.youtube.com/embed/hEBWdy5gwrM"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                  />
                 </div>
                 <div className="relative rounded-lg border border-slate-200 bg-white/60 p-5 shadow-sm transition hover:border-pink-500 hover:bg-white dark:border-slate-800/40 dark:bg-slate-800/60 dark:hover:border-pink-500 dark:hover:bg-slate-800">
-                  <ClipboardIcon className="mb-5 inline-block h-10 w-10 text-pink-500" />
-                  <h4 className="mb-2 text-lg font-bold">
-                    Browse the community plugin directory
+                  <MicrophoneIcon className="float-left mb-5 inline-block h-10 w-10 text-pink-500" />
+                  <h4 className="ml-2 inline-block align-super text-lg font-bold dark:text-slate-300">
+                    Conference Talks
                   </h4>
-                  <a className="focus:outline-none" href="#plugin-directory">
-                    <span className="absolute inset-0" aria-hidden="true" />
-                    <p className="leading-relaxed">
-                      Check all the community plugins available for Nx! These
-                      plugins have been approved by the Nx core team, are well
-                      maintained and regularly updated to make sure they work
-                      with the latest versions.
-                    </p>
-                  </a>
+                  <iframe
+                    title="Managing Monorepos with Nx"
+                    loading="lazy"
+                    className="max-w-screen-sm rounded-lg shadow-lg"
+                    width="100%"
+                    src="https://www.youtube.com/embed/pPCJZhiZcEw"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                  />
                 </div>
               </div>
             </div>
@@ -155,13 +163,179 @@ export default function Community(props: CommunityProps): JSX.Element {
             <ConnectWithUs />
           </div>
 
-          <div id="create-nx-plugin" className="py-28">
-            <CreateNxPlugin />
-          </div>
+          <article
+            id="champions"
+            className="relative pt-28 pt-16 sm:pt-24 lg:pt-32"
+          >
+            <header className="mx-auto max-w-prose px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8">
+              <div>
+                <SectionHeading as="p" variant="title" id="community">
+                  Get to know our
+                </SectionHeading>
+                <SectionHeading as="h1" variant="display" className="mt-4">
+                  <TrophyIcon className="mb-2 inline-block h-10 w-10 text-yellow-500" />{' '}
+                  Nx Champions{' '}
+                  <TrophyIcon className="mb-2 inline-block h-10 w-10 text-yellow-500" />
+                </SectionHeading>
+              </div>
+            </header>
+            <ul className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 py-16 text-center sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-4">
+              <li>
+                <div className="space-y-4">
+                  <img
+                    className="mx-auto h-20 w-20 rounded-full lg:h-24 lg:w-24"
+                    src="/images/testimonials/michael-bromley.jpg"
+                    alt="Altan stalker"
+                  />
+                  <div className="space-y-2">
+                    <div className="text-xs font-medium lg:text-sm">
+                      <h3 className="mb-2">Altan Stalker</h3>
+                      <p className="ml-6 text-left -indent-6 text-slate-400 dark:text-slate-600">
+                        🗺️ Atlanta, Georgia, USA
+                      </p>
+                      <p className="ml-6 text-left -indent-6 text-slate-400 dark:text-slate-600">
+                        <a href="https://twitter.com/StalkAltan">
+                          🐥 @StalkAltan
+                        </a>
+                      </p>
+                      <p className=" ml-6 text-left -indent-6">
+                        💬 I don't often use a monorepo, but when I do, I use
+                        Nx.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className="space-y-4">
+                  <img
+                    className="mx-auto h-20 w-20 rounded-full lg:h-24 lg:w-24"
+                    src="/images/testimonials/michael-bromley.jpg"
+                    alt="Austin Fahsl"
+                  />
+                  <div className="space-y-2">
+                    <div className="text-xs font-medium lg:text-sm">
+                      <h3>Austin Fahsl</h3>
+                      <p className="text-slate-400 dark:text-slate-600">
+                        Senior Engineer
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className="space-y-4">
+                  <img
+                    className="mx-auto h-20 w-20 rounded-full lg:h-24 lg:w-24"
+                    src="/images/testimonials/michael-bromley.jpg"
+                    alt="Benjamin Cabanes"
+                  />
+                  <div className="space-y-2">
+                    <div className="text-xs font-medium lg:text-sm">
+                      <h3>Benjamin Cabanes</h3>
+                      <p className="text-slate-400 dark:text-slate-600">
+                        Architect
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className="space-y-4">
+                  <img
+                    className="mx-auto h-20 w-20 rounded-full lg:h-24 lg:w-24"
+                    src="/images/testimonials/michael-bromley.jpg"
+                    alt="Caitlin Cashin"
+                  />
+                  <div className="space-y-2">
+                    <div className="text-xs font-medium lg:text-sm">
+                      <h3>Caitlin Cashin</h3>
+                      <p className="text-slate-400 dark:text-slate-600">
+                        Developer Marketing Manager
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className="space-y-4">
+                  <img
+                    className="mx-auto h-20 w-20 rounded-full lg:h-24 lg:w-24"
+                    src="/images/testimonials/michael-bromley.jpg"
+                    alt="Caleb Ukle"
+                  />
+                  <div className="space-y-2">
+                    <div className="text-xs font-medium lg:text-sm">
+                      <h3>Caleb Ukle</h3>
+                      <p className="text-slate-400 dark:text-slate-600">
+                        Senior Engineer
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className="space-y-4">
+                  <img
+                    className="mx-auto h-20 w-20 rounded-full lg:h-24 lg:w-24"
+                    src="/images/testimonials/michael-bromley.jpg"
+                    alt="Chau Tran"
+                  />
+                  <div className="space-y-2">
+                    <div className="text-xs font-medium lg:text-sm">
+                      <h3>Chau Tran</h3>
+                      <p className="text-slate-400 dark:text-slate-600">
+                        Senior Engineer
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className="space-y-4">
+                  <img
+                    className="mx-auto h-20 w-20 rounded-full lg:h-24 lg:w-24"
+                    src="/images/testimonials/michael-bromley.jpg"
+                    alt="Chelsea Durso"
+                  />
+                  <div className="space-y-2">
+                    <div className="text-xs font-medium lg:text-sm">
+                      <h3>Chelsea Durso</h3>
+                      <p className="text-slate-400 dark:text-slate-600">
+                        Design Director
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className="space-y-4">
+                  <img
+                    className="mx-auto h-20 w-20 rounded-full lg:h-24 lg:w-24"
+                    src="/images/testimonials/michael-bromley.jpg"
+                    alt="Colum Ferry"
+                  />
+                  <div className="space-y-2">
+                    <div className="text-xs font-medium lg:text-sm">
+                      <h3>Colum Ferry</h3>
+                      <p className="text-slate-400 dark:text-slate-600">
+                        Senior Engineer
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </li>{' '}
+            </ul>
 
-          <div id="plugin-directory" className="py-28">
-            <PluginDirectory pluginList={props.pluginList} />
-          </div>
+            <div className="pb-16">
+              <p className="text-center">
+                Interested in joining the Nx Champions program yourself?{' '}
+                <a className="underline" href="/champions">
+                  Find out more about the program
+                </a>
+              </p>
+            </div>
+          </article>
         </div>
       </main>
       <Footer />
