a91513d9619624f573ecbd603a0299f0d554608d
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-03-20 09:43:27 +0000

    fix(angular): do not set angularCompilerOptions in e2e tsconfig when generating an app (#15729)

--- packages/angular/src/generators/application/lib/enable-strict-type-checking.ts
+++ packages/angular/src/generators/application/lib/enable-strict-type-checking.ts
@@ -1,45 +1,39 @@
 import type { Tree } from '@nrwl/devkit';
-import type { NormalizedSchema } from './normalized-schema';
-
 import { updateJson } from '@nrwl/devkit';
+import type { NormalizedSchema } from './normalized-schema';
 
 export function enableStrictTypeChecking(
   host: Tree,
   options: NormalizedSchema
-) {
-  const configFiles = [
-    `${options.appProjectRoot}/tsconfig.json`,
-    `${options.e2eProjectRoot}/tsconfig.json`,
-  ];
+): void {
+  // This matches the settings defined by the Angular CLI https://angular.io/guide/strict-mode
+  const compilerOptions = {
+    forceConsistentCasingInFileNames: true,
+    strict: true,
+    noImplicitOverride: true,
+    noPropertyAccessFromIndexSignature: true,
+    noImplicitReturns: true,
+    noFallthroughCasesInSwitch: true,
+  };
 
-  for (const configFile of configFiles) {
-    if (!host.exists(configFile)) {
-      continue;
-    }
-
-    // Update the settings in the tsconfig.app.json to enable strict type checking.
-    // This matches the settings defined by the Angular CLI https://angular.io/guide/strict-mode
-    updateJson(host, configFile, (json) => {
-      // update the TypeScript settings
-      json.compilerOptions = {
-        ...(json.compilerOptions ?? {}),
-        forceConsistentCasingInFileNames: true,
-        strict: true,
-        noImplicitOverride: true,
-        noPropertyAccessFromIndexSignature: true,
-        noImplicitReturns: true,
-        noFallthroughCasesInSwitch: true,
-      };
-
-      // update Angular Template Settings
+  const appTsConfigPath = `${options.appProjectRoot}/tsconfig.json`;
+  if (host.exists(appTsConfigPath)) {
+    updateJson(host, appTsConfigPath, (json) => {
+      json.compilerOptions = { ...json.compilerOptions, ...compilerOptions };
       json.angularCompilerOptions = {
-        ...(json.angularCompilerOptions ?? {}),
         enableI18nLegacyMessageIdFormat: false,
         strictInjectionParameters: true,
         strictInputAccessModifiers: true,
         strictTemplates: true,
       };
+      return json;
+    });
+  }
 
+  const e2eTsConfigPath = `${options.e2eProjectRoot}/tsconfig.json`;
+  if (host.exists(e2eTsConfigPath)) {
+    updateJson(host, e2eTsConfigPath, (json) => {
+      json.compilerOptions = { ...json.compilerOptions, ...compilerOptions };
       return json;
     });
   }
