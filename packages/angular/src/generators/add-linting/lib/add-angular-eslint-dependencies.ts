4d71406e6dedd898457a94f2cac8f235dca9dac1
Colum Ferry <cferry09@gmail.com>
2023-03-22 10:57:28 +0000

    cleanup(angular): remove duplicated code for add-linting

--- packages/angular/src/generators/add-linting/lib/add-angular-eslint-dependencies.ts
+++ packages/angular/src/generators/add-linting/lib/add-angular-eslint-dependencies.ts
@@ -1,15 +1,22 @@
 import type { GeneratorCallback, Tree } from '@nrwl/devkit';
 import { addDependenciesToPackageJson } from '@nrwl/devkit';
 import { angularEslintVersion } from '../../../utils/versions';
+import { getInstalledAngularVersionInfo } from '../../utils/version-utils';
+import { backwardCompatibleVersions } from '../../../utils/backward-compatible-versions';
 
 export function addAngularEsLintDependencies(tree: Tree): GeneratorCallback {
+  const installedAngularVersionInfo = getInstalledAngularVersionInfo(tree);
+  const angularEslintVersionToInstall =
+    installedAngularVersionInfo.major === 14
+      ? backwardCompatibleVersions.angularV14.angularEslintVersion
+      : angularEslintVersion;
   return addDependenciesToPackageJson(
     tree,
     {},
     {
-      '@angular-eslint/eslint-plugin': angularEslintVersion,
-      '@angular-eslint/eslint-plugin-template': angularEslintVersion,
-      '@angular-eslint/template-parser': angularEslintVersion,
+      '@angular-eslint/eslint-plugin': angularEslintVersionToInstall,
+      '@angular-eslint/eslint-plugin-template': angularEslintVersionToInstall,
+      '@angular-eslint/template-parser': angularEslintVersionToInstall,
     }
   );
 }
