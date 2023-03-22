d3a85e5450634bc66c75482c0cd24238ed99be21
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2023-03-22 17:02:39 +0200

    feat(vite): ts-node register paths in vite (#15629)

--- packages/vite/src/executors/build/build.impl.ts
+++ packages/vite/src/executors/build/build.impl.ts
@@ -11,13 +11,18 @@ import { existsSync } from 'fs';
 import { resolve } from 'path';
 import { createAsyncIterable } from '@nrwl/devkit/src/utils/async-iterable';
 
+import { registerTsConfigPaths } from 'nx/src/utils/register';
+
 export async function* viteBuildExecutor(
   options: ViteBuildExecutorOptions,
   context: ExecutorContext
 ) {
-  const normalizedOptions = normalizeOptions(options);
   const projectRoot =
     context.projectsConfigurations.projects[context.projectName].root;
+
+  registerTsConfigPaths(resolve(projectRoot, 'tsconfig.json'));
+
+  const normalizedOptions = normalizeOptions(options);
 
   const buildConfig = mergeConfig(
     getViteSharedConfig(normalizedOptions, false, context),
