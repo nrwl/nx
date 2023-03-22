e904e0461192e9fc962d40a705ba6c05f1b778a1
Jack Hsu <jack.hsu@gmail.com>
2023-03-22 18:05:18 -0400

    fix(nextjs): add missing style deps for less and stylus (#15839)

--- packages/next/plugins/with-stylus.ts
+++ packages/next/plugins/with-stylus.ts
@@ -1,5 +1,6 @@
 // Adapted from https://raw.githubusercontent.com/elado/next-with-less/main/src/index.js
 import { merge } from 'webpack-merge';
+import { NextConfigFn } from './with-nx';
 
 const addStylusToRegExp = (rx) =>
   new RegExp(rx.source.replace('|sass', '|sass|styl'), rx.flags);
@@ -12,85 +13,91 @@ function patchNextCSSWithStylus(
 
 patchNextCSSWithStylus();
 
-export function withStylus({ stylusLoaderOptions = {}, ...nextConfig }: any) {
-  return Object.assign({}, nextConfig, {
-    webpack(config, opts) {
-      // there are 2 relevant sass rules in next.js - css modules and global css
-      let sassModuleRule;
-      // global sass rule (does not exist in server builds)
-      let sassGlobalRule;
+export function withStylus(configFn: NextConfigFn): NextConfigFn {
+  return async (phase: string) => {
+    const { stylusLoaderOptions = {}, ...nextConfig } = await configFn(phase);
 
-      const cssRule = config.module.rules.find((rule) =>
-        rule.oneOf?.find((r) => r?.[Symbol.for('__next_css_remove')])
-      );
+    return Object.assign({}, nextConfig, {
+      webpack(config, opts) {
+        // there are 2 relevant sass rules in next.js - css modules and global css
+        let sassModuleRule;
+        // global sass rule (does not exist in server builds)
+        let sassGlobalRule;
 
-      const addStylusRuleToTest = (test) => {
-        if (Array.isArray(test)) {
-          return test.map((rx) => addStylusToRegExp(rx));
-        } else {
-          return addStylusToRegExp(test);
-        }
-      };
-
-      cssRule.oneOf.forEach((rule) => {
-        if (rule.options?.__next_css_remove) return;
-
-        if (rule.use?.loader === 'error-loader') {
-          rule.test = addStylusRuleToTest(rule.test);
-        } else if (rule.use?.loader?.includes('file-loader')) {
-          rule.issuer = addStylusRuleToTest(rule.issuer);
-        } else if (rule.use?.includes?.('ignore-loader')) {
-          rule.test = addStylusRuleToTest(rule.test);
-        } else if (rule.test?.source === '\\.module\\.(scss|sass)$') {
-          sassModuleRule = rule;
-        } else if (rule.test?.source === '(?<!\\.module)\\.(scss|sass)$') {
-          sassGlobalRule = rule;
-        }
-      });
-
-      const stylusLoader = {
-        loader: 'stylus-loader',
-        options: {
-          ...stylusLoaderOptions,
-          stylusOptions: {
-            javascriptEnabled: true,
-            ...stylusLoaderOptions.stylusOptions,
-          },
-        },
-      };
-
-      let stylusModuleRule = merge({}, sassModuleRule);
-
-      const configureStylusRule = (rule) => {
-        rule.test = new RegExp(rule.test.source.replace('(scss|sass)', 'styl'));
-        // replace sass-loader (last entry) with stylus-loader
-        rule.use.splice(-1, 1, stylusLoader);
-      };
-
-      configureStylusRule(stylusModuleRule);
-      cssRule.oneOf.splice(
-        cssRule.oneOf.indexOf(sassModuleRule) + 1,
-        0,
-        stylusModuleRule
-      );
-
-      if (sassGlobalRule) {
-        let stylusGlobalRule = merge({}, sassGlobalRule);
-        configureStylusRule(stylusGlobalRule);
-        cssRule.oneOf.splice(
-          cssRule.oneOf.indexOf(sassGlobalRule) + 1,
-          0,
-          stylusGlobalRule
+        const cssRule = config.module.rules.find((rule) =>
+          rule.oneOf?.find((r) => r?.[Symbol.for('__next_css_remove')])
         );
-      }
 
-      if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, opts);
-      }
+        const addStylusRuleToTest = (test) => {
+          if (Array.isArray(test)) {
+            return test.map((rx) => addStylusToRegExp(rx));
+          } else {
+            return addStylusToRegExp(test);
+          }
+        };
 
-      return config;
-    },
-  });
+        cssRule.oneOf.forEach((rule) => {
+          if (rule.options?.__next_css_remove) return;
+
+          if (rule.use?.loader === 'error-loader') {
+            rule.test = addStylusRuleToTest(rule.test);
+          } else if (rule.use?.loader?.includes('file-loader')) {
+            rule.issuer = addStylusRuleToTest(rule.issuer);
+          } else if (rule.use?.includes?.('ignore-loader')) {
+            rule.test = addStylusRuleToTest(rule.test);
+          } else if (rule.test?.source === '\\.module\\.(scss|sass)$') {
+            sassModuleRule = rule;
+          } else if (rule.test?.source === '(?<!\\.module)\\.(scss|sass)$') {
+            sassGlobalRule = rule;
+          }
+        });
+
+        const stylusLoader = {
+          loader: 'stylus-loader',
+          options: {
+            ...stylusLoaderOptions,
+            stylusOptions: {
+              javascriptEnabled: true,
+              ...stylusLoaderOptions.stylusOptions,
+            },
+          },
+        };
+
+        let stylusModuleRule = merge({}, sassModuleRule);
+
+        const configureStylusRule = (rule) => {
+          rule.test = new RegExp(
+            rule.test.source.replace('(scss|sass)', 'styl')
+          );
+          // replace sass-loader (last entry) with stylus-loader
+          rule.use.splice(-1, 1, stylusLoader);
+        };
+
+        configureStylusRule(stylusModuleRule);
+        cssRule.oneOf.splice(
+          cssRule.oneOf.indexOf(sassModuleRule) + 1,
+          0,
+          stylusModuleRule
+        );
+
+        if (sassGlobalRule) {
+          let stylusGlobalRule = merge({}, sassGlobalRule);
+          configureStylusRule(stylusGlobalRule);
+          cssRule.oneOf.splice(
+            cssRule.oneOf.indexOf(sassGlobalRule) + 1,
+            0,
+            stylusGlobalRule
+          );
+        }
+
+        if (typeof nextConfig.webpack === 'function') {
+          return nextConfig.webpack(config, opts);
+        }
+
+        return config;
+      },
+    });
+  };
 }
 
 module.exports = withStylus;
--- packages/next/src/utils/styles.ts
+++ packages/next/src/utils/styles.ts
@@ -5,6 +5,7 @@ import {
   updateJson,
 } from '@nrwl/devkit';
 
+import { lessVersion, stylusVersion } from '@nrwl/react/src/utils/versions';
 import { CSS_IN_JS_DEPENDENCIES } from '@nrwl/react/src/utils/styled';
 import {
   babelPluginStyledComponentsVersion,
@@ -40,11 +41,13 @@ export const NEXT_SPECIFIC_STYLE_DEPENDENCIES = {
   less: {
     dependencies: {},
     devDependencies: {
+      less: lessVersion,
       'less-loader': lessLoader,
     },
   },
   styl: {
     dependencies: {
+      stylus: stylusVersion,
       'stylus-loader': stylusLoader,
     },
     devDependencies: {},
