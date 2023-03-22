d062dc6179b42fcf222c313a437421f24bb8e8b8
Colum Ferry <cferry09@gmail.com>
2023-03-22 12:41:24 +0000

    cleanup(angular): remove duplicated v14 code for application

--- packages/angular/src/generators/application/lib/create-project.ts
+++ packages/angular/src/generators/application/lib/create-project.ts
@@ -1,8 +1,10 @@
 import { NormalizedSchema } from './normalized-schema';
 import type { ProjectConfiguration, Tree } from '@nrwl/devkit';
 import { addProjectConfiguration } from '@nrwl/devkit';
+import { getInstalledAngularVersionInfo } from '../../utils/version-utils';
 
 export function createProject(tree: Tree, options: NormalizedSchema) {
+  const installedAngularInfo = getInstalledAngularVersionInfo(tree);
   const project: ProjectConfiguration & { prefix: string } = {
     name: options.name,
     projectType: 'application',
@@ -20,7 +22,10 @@ export function createProject(tree: Tree, options: NormalizedSchema) {
           }`,
           index: `${options.appProjectRoot}/src/index.html`,
           main: `${options.appProjectRoot}/src/main.ts`,
-          polyfills: ['zone.js'],
+          polyfills:
+            installedAngularInfo.major === 14
+              ? `${options.appProjectRoot}/src/polyfills.ts`
+              : ['zone.js'],
           tsConfig: `${options.appProjectRoot}/tsconfig.app.json`,
           assets: [
             `${options.appProjectRoot}/src/favicon.ico`,
@@ -43,6 +48,15 @@ export function createProject(tree: Tree, options: NormalizedSchema) {
                 maximumError: '4kb',
               },
             ],
+            fileReplacements:
+              installedAngularInfo.major === 14
+                ? [
+                    {
+                      replace: `${options.appProjectRoot}/src/environments/environment.ts`,
+                      with: `${options.appProjectRoot}/src/environments/environment.prod.ts`,
+                    },
+                  ]
+                : undefined,
             outputHashing: 'all',
           },
           development: {
