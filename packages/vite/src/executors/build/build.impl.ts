64017a27bafcf4b13d576dbae1317f673d95c610
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2023-03-23 14:13:25 +0200

    feat(vite): add outfile for build executor (#15002) (#15845)
    Co-authored-by: Leng, Errong <eleng@bestbuycanada.ca>

--- packages/vite/src/executors/build/build.impl.ts
+++ packages/vite/src/executors/build/build.impl.ts
@@ -76,7 +76,10 @@ export async function* viteBuildExecutor(
     });
     yield* iterable;
   } else {
-    yield { success: true };
+    const output = watcherOrOutput?.['output'] || watcherOrOutput?.[0]?.output;
+    const fileName = output?.[0]?.fileName || 'main.cjs';
+    const outfile = resolve(normalizedOptions.outputPath, fileName);
+    yield { success: true, outfile };
   }
 }
 
