cf6a1226077a30287103dc7f60c99c129050c93c
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2023-03-30 15:11:39 +0300

    fix(rspack): add withWeb if web app (#200)

--- packages/rspack/src/generators/configuration/configuration.ts
+++ packages/rspack/src/generators/configuration/configuration.ts
@@ -2,9 +2,11 @@ import {
   formatFiles,
   joinPathFragments,
   readProjectConfiguration,
+  TargetConfiguration,
   Tree,
   updateProjectConfiguration,
 } from '@nrwl/devkit';
+import { tsquery } from '@phenomnomnominal/tsquery';
 import { RspackExecutorSchema } from '../../executors/rspack/schema';
 import rspackInitGenerator from '../init/init';
 import { ConfigurationSchema } from './schema';
@@ -14,10 +16,18 @@ export async function configurationGenerator(
   options: ConfigurationSchema
 ) {
   const task = await rspackInitGenerator(tree, options);
-
-  const { targets } = readProjectConfiguration(tree, options.project);
+  const { targets, root } = readProjectConfiguration(tree, options.project);
   const foundStylePreprocessorOptions: { includePaths?: string[] } | undefined =
     targets?.build?.options?.stylePreprocessorOptions;
+
+  const { target, uiFramework } = determineFrameworkAndTarget(
+    tree,
+    options,
+    root,
+    targets
+  );
+  options.uiFramework = uiFramework;
+  options.target = target;
 
   addBuildTarget(tree, options);
   if (options.uiFramework !== 'none' || options.devServer) {
@@ -26,6 +36,57 @@ export async function configurationGenerator(
   writeRspackConfigFile(tree, options, foundStylePreprocessorOptions);
   await formatFiles(tree);
   return task;
+}
+
+function determineFrameworkAndTarget(
+  tree: Tree,
+  options: ConfigurationSchema,
+  projectRoot: string,
+  targets: {
+    [targetName: string]: TargetConfiguration<any>;
+  }
+): { target: 'node' | 'web'; uiFramework?: 'none' | 'react' | 'web' } {
+  // First try to infer if the target is node
+  if (options.target !== 'node') {
+    // Try to infer from jest config if the env is node
+    let jestConfigPath: string;
+    if (
+      targets?.test?.executor !== '@nrwl/jest:jest' &&
+      targets?.test?.options?.jestConfig
+    ) {
+      jestConfigPath = targets?.test?.options?.jestConfig;
+    } else {
+      jestConfigPath = joinPathFragments(projectRoot, 'jest.config.ts');
+    }
+
+    if (!tree.exists(jestConfigPath)) {
+      return { target: options.target, uiFramework: options.uiFramework };
+    }
+    const appFileContent = tree.read(jestConfigPath, 'utf-8');
+    const file = tsquery.ast(appFileContent);
+    // find testEnvironment: 'node' in jest config
+    const testEnvironment = tsquery(
+      file,
+      `PropertyAssignment:has(Identifier[name="testEnvironment"]) > StringLiteral[value="node"]`
+    );
+    if (testEnvironment.length > 0) {
+      return { target: 'node', uiFramework: options.uiFramework };
+    }
+  }
+
+  if (options.uiFramework !== 'react' && options.target === 'web') {
+    // Look if React is used in the project
+    let tsConfigPath = joinPathFragments(projectRoot, 'tsconfig.json');
+    if (!tree.exists(tsConfigPath)) {
+      tsConfigPath = determineTsConfig(tree, options);
+    }
+    const tsConfig = JSON.parse(tree.read(tsConfigPath).toString());
+    if (tsConfig?.compilerOptions?.jsx?.includes('react')) {
+      return { target: 'web', uiFramework: 'react' };
+    } else {
+      return { target: options.target, uiFramework: options.uiFramework };
+    }
+  }
 }
 
 function determineMain(tree: Tree, options: ConfigurationSchema) {
@@ -55,6 +116,18 @@ function determineTsConfig(tree: Tree, options: ConfigurationSchema) {
 
 function addBuildTarget(tree: Tree, options: ConfigurationSchema) {
   const project = readProjectConfiguration(tree, options.project);
+
+  const assets = [];
+  if (
+    options.target === 'web' &&
+    tree.exists(joinPathFragments(project.root, 'src/favicon.ico'))
+  ) {
+    assets.push(joinPathFragments(project.root, 'src/favicon.ico'));
+  }
+  if (tree.exists(joinPathFragments(project.root, 'src/assets'))) {
+    assets.push(joinPathFragments(project.root, 'src/assets'));
+  }
+
   const buildOptions: RspackExecutorSchema = {
     target: options.target ?? 'web',
     outputPath: joinPathFragments(
@@ -65,10 +138,7 @@ function addBuildTarget(tree: Tree, options: ConfigurationSchema) {
     main: determineMain(tree, options),
     tsConfig: determineTsConfig(tree, options),
     rspackConfig: joinPathFragments(project.root, 'rspack.config.js'),
-    assets: [
-      joinPathFragments(project.root, 'src/favicon.ico'),
-      joinPathFragments(project.root, 'src/assets'),
-    ],
+    assets,
   };
 
   updateProjectConfiguration(tree, options.project, {
@@ -156,7 +226,7 @@ function createConfig(
         return config;
       });
     `;
-  } else if (options.uiFramework === 'web') {
+  } else if (options.uiFramework === 'web' || options.target === 'web') {
     return `
       const { composePlugins, withNx, withWeb } = require('@nrwl/rspack');
 
