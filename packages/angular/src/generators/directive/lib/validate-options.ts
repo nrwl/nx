d6b80b32395b3ec876b72a2d974ebcdd835344ad
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-04-03 12:01:20 +0100

    fix(angular): validate standalone option in the directive generator (#16051)
    (cherry picked from commit bf9542a150a603d5c9379ab4dd923fb21c36e29a)

--- packages/angular/src/generators/directive/lib/validate-options.ts
+++ packages/angular/src/generators/directive/lib/validate-options.ts
@@ -1,6 +1,8 @@
 import type { Tree } from '@nrwl/devkit';
-import { getProjects } from '@nrwl/devkit';
+import { getProjects, stripIndents } from '@nrwl/devkit';
+import { lt } from 'semver';
 import { checkPathUnderProjectRoot } from '../../utils/path';
+import { getInstalledAngularVersionInfo } from '../../utils/version-utils';
 import type { Schema } from '../schema';
 
 export function validateOptions(tree: Tree, options: Schema): void {
@@ -10,4 +12,10 @@ export function validateOptions(tree: Tree, options: Schema): void {
   }
 
   checkPathUnderProjectRoot(tree, options.project, options.path);
+
+  const installedAngularVersionInfo = getInstalledAngularVersionInfo(tree);
+  if (lt(installedAngularVersionInfo.version, '14.1.0') && options.standalone) {
+    throw new Error(stripIndents`The "standalone" option is only supported in Angular >= 14.1.0. You are currently using "${installedAngularVersionInfo.version}".
+    You can resolve this error by removing the "standalone" option or by migrating to Angular 14.1.0.`);
+  }
 }
