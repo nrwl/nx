3895696f8a6593bbf583ffeaf8b7412e1bd0e5a4
Colum Ferry <cferry09@gmail.com>
2023-04-03 11:55:50 +0100

    feat(angular): export some utils as public api #15669

---
+++ packages/angular/src/utils/public-api.ts
@@ -0,0 +1,12 @@
+export {
+  isStandalone,
+  addImportToComponent,
+  addImportToDirective,
+  addImportToPipe,
+  addImportToModule,
+  addProviderToBootstrapApplication,
+  addProviderToComponent,
+  addProviderToModule,
+} from './nx-devkit/ast-utils';
+
+export { addRoute, addProviderToRoute } from './nx-devkit/route-utils';
