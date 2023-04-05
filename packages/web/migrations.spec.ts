b51ae51e4fcd9c5ebc9c9eb79ad2a6f7435f8068
FrozenPandaz <jasonjean1993@gmail.com>
2023-04-05 13:30:02 -0400

    fix(repo): fix migration specs

--- packages/web/migrations.spec.ts
+++ packages/web/migrations.spec.ts
@@ -1,11 +1,14 @@
 import path = require('path');
 import json = require('./migrations.json');
+import { MigrationsJsonEntry } from 'nx/src/config/misc-interfaces';
 
 describe('Web migrations', () => {
   it('should have valid paths', () => {
-    Object.values(json.generators).forEach((m) => {
+    Object.values(json.generators).forEach((m: MigrationsJsonEntry) => {
       expect(() =>
-        require.resolve(path.join(__dirname, `${m.factory}.ts`))
+        require.resolve(
+          path.join(__dirname, `${m.factory ?? m.implementation}.ts`)
+        )
       ).not.toThrow();
     });
   });
