nrwl/nx

commit e2a27039c0a9bd0854e18bdf399027648f6b80f0

fix(misc): filter out deps early based on shared config from module federation (#15654)

packages/angular/src/utils/mf/utils.ts
@@ -51,6 +51,16 @@ export async function getModuleFederationConfig(
     projectGraph,
     mfConfig.name
   );
+
+  if (mfConfig.shared) {
+    dependencies.workspaceLibraries = dependencies.workspaceLibraries.filter(
+      (lib) => mfConfig.shared(lib.name, {})
+    );
+    dependencies.npmPackages = dependencies.npmPackages.filter((pkg) =>
+      mfConfig.shared(pkg, {})
+    );
+  }
+
   const sharedLibraries = shareWorkspaceLibraries(
     dependencies.workspaceLibraries
   );

packages/react/src/module-federation/utils.ts
@@ -37,6 +37,16 @@ export async function getModuleFederationConfig(
     projectGraph,
     mfConfig.name
   );
+
+  if (mfConfig.shared) {
+    dependencies.workspaceLibraries = dependencies.workspaceLibraries.filter(
+      (lib) => mfConfig.shared(lib.name, {})
+    );
+    dependencies.npmPackages = dependencies.npmPackages.filter((pkg) =>
+      mfConfig.shared(pkg, {})
+    );
+  }
+
   const sharedLibraries = shareWorkspaceLibraries(
     dependencies.workspaceLibraries
   );

commit 8ca24cbf1fcbde09797fda4da0650bc016a3c5e1

fix(misc): shared deps that rely on default configuration are filtered out
The issue was introduced by #15654. It then filters out and omits shared deps that rely on default
configuration when return value from `shared` function is `undefined`.

closed #15811

packages/angular/src/utils/mf/utils.ts
@@ -60,10 +60,10 @@ export async function getModuleFederationConfig(
 
   if (mfConfig.shared) {
     dependencies.workspaceLibraries = dependencies.workspaceLibraries.filter(
-      (lib) => mfConfig.shared(lib.importKey, {})
+      (lib) => mfConfig.shared(lib.importKey, {}) !== false
     );
-    dependencies.npmPackages = dependencies.npmPackages.filter((pkg) =>
-      mfConfig.shared(pkg, {})
+    dependencies.npmPackages = dependencies.npmPackages.filter(
+      (pkg) => mfConfig.shared(pkg, {}) !== false
     );
   }
 

packages/react/src/module-federation/utils.ts
@@ -40,10 +40,10 @@ export async function getModuleFederationConfig(
 
   if (mfConfig.shared) {
     dependencies.workspaceLibraries = dependencies.workspaceLibraries.filter(
-      (lib) => mfConfig.shared(lib.importKey, {})
+      (lib) => mfConfig.shared(lib.importKey, {}) !== false
     );
-    dependencies.npmPackages = dependencies.npmPackages.filter((pkg) =>
-      mfConfig.shared(pkg, {})
+    dependencies.npmPackages = dependencies.npmPackages.filter(
+      (pkg) => mfConfig.shared(pkg, {}) !== false
     );
   }
 