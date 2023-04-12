cdc09061321711944ddfefa431e6efb1aa7c50fa
Miroslav Jonaš <meeroslav@users.noreply.github.com>
2023-04-12 17:40:40 +0200

    feat(linter): update @typescript-eslint/* to 5.58.0 (#16250)

--- packages/linter/src/utils/versions.ts
+++ packages/linter/src/utils/versions.ts
@@ -2,6 +2,6 @@ export const nxVersion = require('../../package.json').version;
 
 export const tslintToEslintConfigVersion = '^2.13.0';
 
-export const typescriptESLintVersion = '^5.36.1';
+export const typescriptESLintVersion = '^5.58.0';
 export const eslintVersion = '~8.15.0';
 export const eslintConfigPrettierVersion = '8.1.0';
