b4dfb445560df28eeec48dc2943742a5707962dc
Miroslav Jonas <missing.manual@gmail.com>
2023-04-12 15:29:25 +0200

    feat(linter): add test

--- tools/eslint-rules/rules/restrict-js-plugin-deep-import.spec.ts
+++ tools/eslint-rules/rules/restrict-js-plugin-deep-import.spec.ts
@@ -5,7 +5,32 @@ const ruleTester = new TSESLint.RuleTester({
   parser: require.resolve('@typescript-eslint/parser'),
 });
 
+jest.mock('@nrwl/devkit', () => ({
+  ...jest.requireActual<any>('@nrwl/devkit'),
+  workspaceRoot: '/root',
+}));
+
 ruleTester.run(RULE_NAME, rule, {
-  valid: [`const example = true;`],
-  invalid: [],
+  valid: [
+    {
+      code: `import { createLockFile } from 'nx/src/plugins/js';`,
+      filename: '/root/packages/devkit/src/path/to.ts',
+    },
+    {
+      code: `import { createLockFile } from '../plugins/js';`,
+      filename: '/root/packages/nx/src/path/to.ts',
+    },
+  ],
+  invalid: [
+    {
+      errors: [{ messageId: 'noDeepImport' }],
+      code: `import { createLockFile } from 'nx/src/plugins/js/lock-file/lock-file';`,
+      filename: '/root/packages/devkit/src/path/to.ts',
+    },
+    {
+      errors: [{ messageId: 'noDeepRelativeImport' }],
+      code: `import { createLockFile } from '../plugins/js/lock-file/lock-file';`,
+      filename: '/root/packages/nx/src/path/to.ts',
+    },
+  ],
 });
