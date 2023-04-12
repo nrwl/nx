cb359a2d1e90fc7e6d917a827a53e1a4ddf85496
Caleb Ukle <caleb.ukle+github@pm.me>
2023-04-12 11:51:08 -0500

    chore(testing): update jest to use new snapshot format

--- packages/jest/src/migrations/update-15-0-0/add-jest-inputs.spec.ts
+++ packages/jest/src/migrations/update-15-0-0/add-jest-inputs.spec.ts
@@ -44,30 +44,30 @@ describe('15.0.0 migration (add-jest-inputs)', () => {
 
     const updated = readNxJson(tree);
     expect(updated).toMatchInlineSnapshot(`
-      Object {
-        "namedInputs": Object {
-          "default": Array [
+      {
+        "namedInputs": {
+          "default": [
             "{projectRoot}/**/*",
             "sharedGlobals",
           ],
-          "production": Array [
+          "production": [
             "default",
             "!{projectRoot}/**/?(*.)+(spec|test).[jt]s?(x)?(.snap)",
             "!{projectRoot}/tsconfig.spec.json",
             "!{projectRoot}/jest.config.[jt]s",
           ],
-          "sharedGlobals": Array [],
+          "sharedGlobals": [],
         },
-        "targetDefaults": Object {
-          "test": Object {
-            "inputs": Array [
+        "targetDefaults": {
+          "test": {
+            "inputs": [
               "default",
               "^production",
               "{workspaceRoot}/jest.preset.js",
             ],
           },
-          "test2": Object {
-            "inputs": Array [
+          "test2": {
+            "inputs": [
               "default",
               "^production",
               "{workspaceRoot}/jest.preset.js",
