afccd0e693405c5d2480d334db9c79f017c9680d
Craigory Coppola <craigorycoppola@gmail.com>
2023-04-13 15:54:55 -0400

    chore(repo): move our migrations to 'generators' rather than 'schematics'

--- packages/detox/migrations.spec.ts
+++ packages/detox/migrations.spec.ts
@@ -3,7 +3,7 @@ import json = require('./migrations.json');
 
 describe('Detox migrations', () => {
   it('should have valid paths', () => {
-    Object.values((json as any)?.schematics || {}).forEach((m: any) => {
+    Object.values(json.generators).forEach((m: any) => {
       expect(() =>
         require.resolve(path.join(__dirname, `${m.factory}.ts`))
       ).not.toThrow();
--- packages/node/migrations.spec.ts
+++ packages/node/migrations.spec.ts
@@ -3,7 +3,7 @@ import json = require('./migrations.json');
 
 describe('Node migrations', () => {
   it('should have valid paths', () => {
-    Object.values(json.schematics).forEach((m) => {
+    Object.values(json.generators).forEach((m) => {
       expect(() =>
         require.resolve(path.join(__dirname, `${m.factory}.ts`))
       ).not.toThrow();
