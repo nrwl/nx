2ddf806fa1b5b026d18435f08d29ba289830fe71
Craigory Coppola <craigorycoppola@gmail.com>
2023-04-14 15:25:58 -0400

    chore(repo): move our migrations to 'generators' rather than 'schematics' (#16291)

--- packages/workspace/migrations.spec.ts
+++ packages/workspace/migrations.spec.ts
@@ -3,7 +3,7 @@ import json = require('./migrations.json');
 
 describe('Workspace migrations', () => {
   it('should have valid paths', () => {
-    Object.values(json.schematics).forEach((m: any) => {
+    Object.values(json.generators).forEach((m: any) => {
       expect(() =>
         require.resolve(
           path.join(__dirname, `${m.implementation || m.factory}.ts`)
