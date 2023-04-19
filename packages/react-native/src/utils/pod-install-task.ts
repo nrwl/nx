16e115fd7b8edb9db62cb9b40451d5f9833e1483
Emily Xiong <xiongemi@gmail.com>
2023-04-19 15:03:36 -0400

    fix(react-native): log errors for pod install (#16386)

--- packages/react-native/src/utils/pod-install-task.ts
+++ packages/react-native/src/utils/pod-install-task.ts
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process';
+import { exec } from 'child_process';
 import { platform } from 'os';
 import * as chalk from 'chalk';
 import { GeneratorCallback, logger } from '@nx/devkit';
@@ -48,21 +48,34 @@ export function podInstall(
   buildFolder?: string
 ): Promise<void> {
   return new Promise((resolve, reject) => {
-    const result = execSync('pod install', {
-      cwd: iosDirectory,
-    });
-    logger.info(result.toString());
-    if (result.toString().includes('Pod installation complete')) {
-      // Remove build folder after pod install
-      if (buildFolder) {
-        buildFolder = join(iosDirectory, buildFolder);
-        if (existsSync(buildFolder)) {
-          rmdirSync(buildFolder, { recursive: true });
+    exec(
+      'pod install',
+      {
+        cwd: iosDirectory,
+      },
+      (error, stdout, stderr) => {
+        if (error) {
+          logger.error(error.message);
+          reject(new Error(podInstallErrorMessage));
+        }
+        if (stderr) {
+          logger.error(stderr);
+          reject(new Error(podInstallErrorMessage));
+        }
+        logger.info(stdout);
+        if (stdout.includes('Pod installation complete')) {
+          // Remove build folder after pod install
+          if (buildFolder) {
+            buildFolder = join(iosDirectory, buildFolder);
+            if (existsSync(buildFolder)) {
+              rmdirSync(buildFolder, { recursive: true });
+            }
+          }
+          resolve();
+        } else {
+          reject(new Error(podInstallErrorMessage));
         }
       }
-      resolve();
-    } else {
-      reject(new Error(podInstallErrorMessage));
-    }
+    );
   });
 }
