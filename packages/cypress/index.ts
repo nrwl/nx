1f7efc37921573ec3081ad4290cac04231c835dd
Dominik Pieper <dominik@pieper.io>
2023-04-19 10:51:28 +0200

    feat(testing): export cypressE2EConfigurationGenerator

--- packages/cypress/index.ts
+++ packages/cypress/index.ts
@@ -3,3 +3,4 @@ export { cypressInitGenerator } from './src/generators/init/init';
 export { conversionGenerator } from './src/generators/convert-tslint-to-eslint/convert-tslint-to-eslint';
 export { cypressComponentProject } from './src/generators/cypress-component-project/cypress-component-project';
 export { migrateCypressProject } from './src/generators/migrate-to-cypress-11/migrate-to-cypress-11';
+export { cypressE2EConfigurationGenerator } from './src/generators/cypress-e2e-configuration/cypress-e2e-configuration';
