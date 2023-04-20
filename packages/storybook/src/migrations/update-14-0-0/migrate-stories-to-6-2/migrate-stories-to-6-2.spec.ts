8a92d2e77ca8a9f5a1570fd25e03ce9c55635af6
Miroslav Jonas <missing.manual@gmail.com>
2023-04-20 17:26:51 +0200

    chore(repo): regenerate pnpm lock file

--- packages/storybook/src/migrations/update-14-0-0/migrate-stories-to-6-2/migrate-stories-to-6-2.spec.ts
+++ packages/storybook/src/migrations/update-14-0-0/migrate-stories-to-6-2/migrate-stories-to-6-2.spec.ts
@@ -45,7 +45,7 @@ describe('migrate-stories-to-6-2 schematic', () => {
   describe('angular project', () => {
     beforeEach(async () => {
       overrideCollectionResolutionForTesting({
-        '@nrwl/storybook': joinPathFragments(
+        '@nx/storybook': joinPathFragments(
           __dirname,
           '../../../../generators.json'
         ),
@@ -64,7 +64,7 @@ describe('migrate-stories-to-6-2 schematic', () => {
 
       writeJson(appTree, 'package.json', {
         devDependencies: {
-          '@nrwl/storybook': nxVersion,
+          '@nx/storybook': nxVersion,
           '@storybook/addon-knobs': storybookVersion,
           '@storybook/angular': storybookVersion,
         },
