cba3c48e6c11ffc62ae2c3b9129adf0146611f9c
Jason Jean <jasonjean1993@gmail.com>
2023-04-25 15:31:25 -0400

    fix(core): do not replace legacy package mentions in binary files (#16547)

---
+++ packages/devkit/src/utils/binary-extensions.ts
@@ -0,0 +1,48 @@
+import { extname } from 'path';
+
+const binaryExtensions = new Set([
+  // // Image types originally from https://github.com/sindresorhus/image-type/blob/5541b6a/index.js
+  '.jpg',
+  '.jpeg',
+  '.png',
+  '.gif',
+  '.webp',
+  '.flif',
+  '.cr2',
+  '.tif',
+  '.bmp',
+  '.jxr',
+  '.psd',
+  '.ico',
+  '.bpg',
+  '.jp2',
+  '.jpm',
+  '.jpx',
+  '.heic',
+  '.cur',
+  '.avif',
+  '.dcm',
+
+  // Compressed files
+  '.tgz',
+  '.gz',
+  '.zip',
+
+  // Documents
+  '.pdf',
+
+  // Java files
+  '.jar',
+  '.keystore',
+
+  // Font files
+  '.ttf',
+  '.otf',
+  '.woff',
+  '.woff2',
+  '.eot',
+]);
+
+export function isBinaryPath(path: string): boolean {
+  return binaryExtensions.has(extname(path));
+}
