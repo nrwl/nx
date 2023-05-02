ec0ff791c10b94505598d279b982dd2be38c99af
Emily Xiong <xiongemi@gmail.com>
2023-05-02 11:51:34 -0400

    feat(expo): upgrade expo to 48.0.15

--- packages/expo/src/utils/versions.ts
+++ packages/expo/src/utils/versions.ts
@@ -1,11 +1,11 @@
 export const nxVersion = require('../../package.json').version;
 
-export const expoVersion = '48.0.11';
+export const expoVersion = '48.0.15';
 export const expoMetroConfigVersion = '0.7.1';
-export const expoSplashScreenVersion = '~0.18.1';
+export const expoSplashScreenVersion = '~0.18.2';
 export const expoStatusBarVersion = '~1.4.4';
-export const expoCliVersion = '~0.7.0'; // @expo/cli
-export const easCliVersion = '~3.10.0';
+export const expoCliVersion = '~0.7.1'; // @expo/cli
+export const easCliVersion = '~3.10.2';
 export const babelPresetExpoVersion = '~9.3.2';
 
 export const reactVersion = '18.2.0';
@@ -13,7 +13,7 @@ export const reactDomVersion = '18.2.0';
 export const reactTestRendererVersion = '18.2.0';
 export const typesReactVersion = '18.0.28';
 
-export const reactNativeVersion = '0.71.6';
+export const reactNativeVersion = '0.71.7';
 export const typesReactNativeVersion = '0.71.6';
 export const reactNativeWebVersion = '~0.18.12';
 
