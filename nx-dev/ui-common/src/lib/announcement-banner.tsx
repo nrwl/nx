ee963922d54518576a2d364a0d03faaba595d0d1
Juri <juri.strumpflohner@gmail.com>
2023-05-04 12:31:22 +0200

    docs(nx-dev): update banner to point to Nx 16 release

--- nx-dev/ui-common/src/lib/announcement-banner.tsx
+++ nx-dev/ui-common/src/lib/announcement-banner.tsx
@@ -11,23 +11,21 @@ export function AnnouncementBanner(): JSX.Element {
                 href="https://www.producthunt.com/posts/nx-cloud#nx-cloud"
                 className="underline"
               >
-                Nx Cloud is on Product Hunt!
+                Nx 16 is out!!
               </Link>
             </span>
             <span className="hidden md:inline">
-              <span className="font-semibold">
-                Nx Cloud is on Product Hunt today!
-              </span>
+              <span className="font-semibold">Nx 16 is out!!</span>
             </span>
             <span className="ml-2 inline-block">
               <Link
-                href="https://www.producthunt.com/posts/nx-cloud#nx-cloud"
+                href="https://blog.nrwl.io/nx-16-is-here-69584ec87053?source=friends_link&sk=e62d25031428743ad9ffc08f2ccc3b06"
                 rel="noreferrer"
                 target="_blank"
                 className="font-semibold text-blue-500 underline dark:text-sky-500"
               >
                 <span className="absolute inset-0" aria-hidden="true" />
-                Leave us a review
+                Learn about all the details
                 <span aria-hidden="true">&rarr;</span>
               </Link>
             </span>
