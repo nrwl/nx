67a5e8e8235f00e7212a6f8b72ef3890bb8f1c81
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-05-08 18:38:49 +0100

    feat(angular): update jest-preset-angular to support angular v16 (#16852)

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -25,6 +25,6 @@ export const postcssUrlVersion = '~10.1.3';
 export const autoprefixerVersion = '^10.4.0';
 export const tsNodeVersion = '10.9.1';
 
-export const jestPresetAngularVersion = '~13.0.0';
+export const jestPresetAngularVersion = '~13.1.0';
 export const typesNodeVersion = '16.11.7';
 export const jasmineMarblesVersion = '^0.9.2';
