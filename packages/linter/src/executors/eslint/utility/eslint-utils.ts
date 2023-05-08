7920fb6a201ee9e82df4907328ae63a3cbf43afe
James Henry <james@henry.sc>
2023-05-08 15:56:48 +0100

    feat(linter): allow ESLint Flat Config to be opted into manually (#16858)

--- packages/linter/src/executors/eslint/utility/eslint-utils.ts
+++ packages/linter/src/executors/eslint/utility/eslint-utils.ts
@@ -1,36 +1,43 @@
-import { ESLint } from 'eslint';
+import type { ESLint } from 'eslint';
 import type { Schema } from '../schema';
 
-export async function loadESLint() {
-  let eslint;
+async function resolveESLintClass(
+  useFlatConfig = false
+): Promise<typeof ESLint> {
   try {
-    eslint = await import('eslint');
-    return eslint;
+    if (!useFlatConfig) {
+      return (await import('eslint')).ESLint;
+    }
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const { FlatESLint } = require('eslint/use-at-your-own-risk');
+    return FlatESLint;
   } catch {
     throw new Error('Unable to find ESLint. Ensure ESLint is installed.');
   }
 }
 
-export async function lint(
+export async function resolveAndInstantiateESLint(
   eslintConfigPath: string | undefined,
-  options: Schema
-): Promise<ESLint.LintResult[]> {
-  const projectESLint: { ESLint: typeof ESLint } = await loadESLint();
+  options: Schema,
+  useFlatConfig = false
+) {
+  if (
+    useFlatConfig &&
+    eslintConfigPath &&
+    !eslintConfigPath?.endsWith('eslint.config.js')
+  ) {
+    throw new Error(
+      'When using the new Flat Config with ESLint, all configs must be named eslint.config.js and .eslintrc files may not be used. See https://eslint.org/docs/latest/use/configure/configuration-files-new'
+    );
+  }
+  const ESLint = await resolveESLintClass(useFlatConfig);
 
-  const eslint = new projectESLint.ESLint({
-    /**
-     * If "noEslintrc" is set to `true` (and therefore here "useEslintrc" will be `false`), then ESLint will not
-     * merge the provided config with others it finds automatically.
-     */
-    useEslintrc: !options.noEslintrc,
+  const eslintOptions: ESLint.Options = {
     overrideConfigFile: eslintConfigPath,
-    ignorePath: options.ignorePath || undefined,
     fix: !!options.fix,
     cache: !!options.cache,
     cacheLocation: options.cacheLocation || undefined,
     cacheStrategy: options.cacheStrategy || undefined,
-    resolvePluginsRelativeTo: options.resolvePluginsRelativeTo || undefined,
-    rulePaths: options.rulesdir || [],
     /**
      * Default is `true` and if not overridden the eslint.lintFiles() method will throw an error
      * when no target files are found.
@@ -38,13 +45,46 @@ export async function lint(
      * We don't want ESLint to throw an error if a user has only just created
      * a project and therefore doesn't necessarily have matching files, for example.
      *
-     * An angular generator creates lint pattern for `html` files, but there may
-     * not be any html files in the project, so keeping it true would break linting everytime
+     * Also, the angular generator creates a lint pattern for `html` files, but there may
+     * not be any html files in the project, so keeping it true would break linting every time
      */
     errorOnUnmatchedPattern: false,
     reportUnusedDisableDirectives:
       options.reportUnusedDisableDirectives || undefined,
-  });
+  };
 
-  return await eslint.lintFiles(options.lintFilePatterns);
+  if (useFlatConfig) {
+    if (typeof options.useEslintrc !== 'undefined') {
+      throw new Error(
+        'For Flat Config, the `useEslintrc` option is not applicable. See https://eslint.org/docs/latest/use/configure/configuration-files-new'
+      );
+    }
+    if (options.resolvePluginsRelativeTo !== undefined) {
+      throw new Error(
+        'For Flat Config, ESLint removed `resolvePluginsRelativeTo` and so it is not supported as an option. See https://eslint.org/docs/latest/use/configure/configuration-files-new'
+      );
+    }
+    if (options.ignorePath !== undefined) {
+      throw new Error(
+        'For Flat Config, ESLint removed `ignorePath` and so it is not supported as an option. See https://eslint.org/docs/latest/use/configure/configuration-files-new'
+      );
+    }
+  } else {
+    eslintOptions.rulePaths = options.rulesdir || [];
+    eslintOptions.resolvePluginsRelativeTo =
+      options.resolvePluginsRelativeTo || undefined;
+    eslintOptions.ignorePath = options.ignorePath || undefined;
+    /**
+     * If "noEslintrc" is set to `true` (and therefore here "useEslintrc" will be `false`), then ESLint will not
+     * merge the provided config with others it finds automatically.
+     */
+    eslintOptions.useEslintrc = !options.noEslintrc;
+  }
+
+  const eslint = new ESLint(eslintOptions);
+
+  return {
+    ESLint,
+    eslint,
+  };
 }
