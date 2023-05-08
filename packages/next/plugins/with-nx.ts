nrwl/nx

commit 88615152fb0ff010743f194b2d3a493e28fdbd2b

fix(nextjs): enable Next.js 13.4 support (#16819)

packages/next/plugins/with-nx.ts
@@ -156,6 +156,15 @@ function withNx(
   _nextConfig = {} as WithNxOptions,
   context: WithNxContext = getWithNxContext()
 ): NextConfigFn {
+  // If this is not set user will see compile errors in Next.js 13.4.
+  // See: https://github.com/nrwl/nx/issues/16692, https://github.com/vercel/next.js/issues/49169
+  // TODO(jack): Remove this once Nx is refactored to invoke CLI directly.
+  forNextVersion('>=13.4.0', () => {
+    process.env['__NEXT_PRIVATE_PREBUNDLED_REACT'] =
+      // Not in Next 13.3 or earlier, so need to access config via string
+      _nextConfig.experimental['serverActions'] ? 'experimental' : 'next';
+  });
+
   return async (phase: string) => {
     const { PHASE_PRODUCTION_SERVER } = await import('next/constants');
     if (phase === PHASE_PRODUCTION_SERVER) {

commit d6bfeb9272ad9bbf0aa56666c6ad26b62beecabe

fix(nextjs): error caused by undefined 'serverActions'
This commit addresses a bug introduced in the PR https://github.com/nrwl/nx/pull/16819, which caused
a TypeError when running NextJS 13.4 with the '_nextConfig.experimental' object missing the
'serverActions' property. The fix involves adding checks to ensure that both the
'_nextConfig.experimental' object and 'serverActions' property are defined before attempting to
access them. If 'serverActions' is undefined, the code defaults to using 'next'.

closed 16845

packages/next/plugins/with-nx.ts
@@ -162,7 +162,9 @@ function withNx(
   forNextVersion('>=13.4.0', () => {
     process.env['__NEXT_PRIVATE_PREBUNDLED_REACT'] =
       // Not in Next 13.3 or earlier, so need to access config via string
-      _nextConfig.experimental['serverActions'] ? 'experimental' : 'next';
+      _nextConfig.experimental && _nextConfig.experimental['serverActions']
+        ? 'experimental'
+        : 'next';
   });
 
   return async (phase: string) => {