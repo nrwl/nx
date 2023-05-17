8a0dcafe2bc704f8344d97600d2f9eae2f0eebdd
Victor Savkin <mail@vsavkin.com>
2023-05-17 14:53:33 -0400

    chore(repo): update to 16.3.0-beta.0

--- nx-dev/nx-dev/lib/changelog.api.ts
+++ nx-dev/nx-dev/lib/changelog.api.ts
@@ -2,5 +2,5 @@ import { ChangelogApi } from '@nx/nx-dev/data-access-documents/node-only';
 
 export const changeLogApi = new ChangelogApi({
   id: 'changelog',
-  changelogRoot: 'nx-dev/nx-dev/public/documentation/changelog',
+  changelogRoot: 'public/documentation/changelog',
 });
