6d53832f9034b3a780acb5843fcbb7916e507916
Nicholas Cunningham <ndcunningham@gmail.com>
2023-06-02 11:44:16 -0600

    fix(nextjs): Update Next.js page generator to support app/ router
    closes: #17352

--- packages/next/src/generators/page/page.ts
+++ packages/next/src/generators/page/page.ts
@@ -16,25 +16,23 @@ import { Schema } from './schema';
  * extra dependencies for css, sass, less, styl style options, and make sure
  * it is under `pages` folder.
  */
-export async function pageGenerator(host: Tree, options: Schema) {
-  const project = readProjectConfiguration(host, options.project);
-  const directory = options.directory ? `pages/${options.directory}` : 'pages';
+export async function pageGenerator(host: Tree, schema: Schema) {
+  const options = normalizeOptions(host, schema);
   const componentTask = await reactComponentGenerator(host, {
     ...options,
-    directory,
+    project: schema.project,
     pascalCaseFiles: false,
     export: false,
     classComponent: false,
     routing: false,
     skipTests: !options.withTests,
     flat: !!options.flat,
-    fileName: !options.flat ? 'index' : undefined,
     skipFormat: true,
   });
 
   const styledTask = addStyleDependencies(host, {
     style: options.style,
-    swc: !host.exists(joinPathFragments(project.root, '.babelrc')),
+    swc: !host.exists(joinPathFragments(options.project.root, '.babelrc')),
   });
 
   if (!options.skipFormat) {
@@ -42,6 +40,19 @@ export async function pageGenerator(host: Tree, options: Schema) {
   }
 
   return runTasksInSerial(componentTask, styledTask);
+}
+
+function normalizeOptions(host: Tree, options: Schema) {
+  const project = readProjectConfiguration(host, options.project);
+
+  // app/ is a reserved folder in nextjs so it is safe to check it's existence
+  const isAppRouter = host.exists(`${project.root}/app`);
+  const routerDirectory = isAppRouter ? 'app' : 'pages';
+  const directory = options.directory
+    ? `${routerDirectory}/${options.directory}`
+    : `${routerDirectory}`;
+  const fileName = isAppRouter ? 'page' : !options.flat ? 'index' : undefined;
+  return { ...options, project, directory, fileName };
 }
 
 export default pageGenerator;
