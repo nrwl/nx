8855fa8577185485961e4e6edc0114bcaff4eed9
Emily Xiong <xiongemi@gmail.com>
2023-06-08 11:42:40 -0400

    feat(vite): upgrade vite attempt to fix windows issue

--- packages/vite/src/utils/versions.ts
+++ packages/vite/src/utils/versions.ts
@@ -1,16 +1,14 @@
 export const nxVersion = require('../../package.json').version;
-export const viteVersion = '^4.3.4';
+export const viteVersion = '^4.3.9';
 export const vitePluginEslintVersion = '^1.8.1';
 export const vitestVersion = '^0.32.0';
 export const vitestUiVersion = '^0.32.0';
 export const vitePluginReactVersion = '^4.0.0';
 export const vitePluginReactSwcVersion = '^3.3.2';
-export const vitePluginVueVersion = '^4.2.3';
-export const vitePluginVueJsxVersion = '^2.1.1';
 export const viteTsConfigPathsVersion = '^4.2.0';
 export const jsdomVersion = '~22.1.0';
 export const vitePluginDtsVersion = '~2.3.0';
 
 // Coverage providers
-export const vitestCoverageC8Version = '^0.31.0';
-export const vitestCoverageIstanbulVersion = '^0.31.0';
+export const vitestCoverageC8Version = '^0.32.0';
+export const vitestCoverageIstanbulVersion = '^0.32.0';
