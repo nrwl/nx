e2217a9350a5d3b403c33b4aae5384fb7960bf32
Juri <juri.strumpflohner@gmail.com>
2023-06-10 14:58:33 +0200

    docs(core): fix typos

--- packages/nx/src/command-line/init/implementation/react/check-for-uncommitted-changes.ts
+++ packages/nx/src/command-line/init/implementation/react/check-for-uncommitted-changes.ts
@@ -4,7 +4,7 @@ export function checkForUncommittedChanges() {
   const gitResult = execSync(`git status --porcelain`);
   if (gitResult.length > 0) {
     console.log('❗️ Careful!');
-    console.log('You have uncommited changes in your repository.');
+    console.log('You have uncommitted changes in your repository.');
     console.log('');
     console.log(gitResult.toString());
     console.log('Please commit your changes before running the migrator!');
