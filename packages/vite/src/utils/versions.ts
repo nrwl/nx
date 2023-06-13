d9f610b8ed4c34a7433c2469d1be6937a0159096
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-06-13 12:15:45 +0300

    feat(vite): update some deps

--- packages/vite/src/utils/versions.ts
+++ packages/vite/src/utils/versions.ts
@@ -1,15 +1,15 @@
 export const nxVersion = require('../../package.json').version;
 export const viteVersion = '^4.3.4';
 export const vitePluginEslintVersion = '^1.8.1';
-export const vitestVersion = '^0.31.0';
-export const vitestUiVersion = '^0.31.0';
-export const vitePluginReactVersion = '^3.0.0';
-export const vitePluginReactSwcVersion = '^3.3.1';
-export const vitePluginVueVersion = '^3.2.0';
+export const vitestVersion = '^0.32.0';
+export const vitestUiVersion = '^0.32.0';
+export const vitePluginReactVersion = '^4.0.0';
+export const vitePluginReactSwcVersion = '^3.3.2';
+export const vitePluginVueVersion = '^4.2.3';
 export const vitePluginVueJsxVersion = '^2.1.1';
-export const viteTsConfigPathsVersion = '^4.0.2';
+export const viteTsConfigPathsVersion = '^4.2.0';
 export const jsdomVersion = '~20.0.3';
-export const vitePluginDtsVersion = '~1.7.1';
+export const vitePluginDtsVersion = '~2.3.0';
 
 // Coverage providers
 export const vitestCoverageC8Version = '^0.31.0';
