a545090b1ab2fb85a66b766e0e7f97214415a820
Emily Xiong <xiongemi@gmail.com>
2023-06-14 13:44:31 -0400

    feat(vite): upgrade vite to 4.3.9 (#17579)

--- packages/vite/src/utils/versions.ts
+++ packages/vite/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxVersion = require('../../package.json').version;
-export const viteVersion = '^4.3.4';
+export const viteVersion = '^4.3.9';
 export const vitestVersion = '^0.32.0';
 export const vitestUiVersion = '^0.32.0';
 export const vitePluginReactVersion = '^4.0.0';
@@ -11,5 +11,5 @@ export const happyDomVersion = '~9.20.3';
 export const edgeRuntimeVmVersion = '~3.0.2';
 
 // Coverage providers
-export const vitestCoverageC8Version = '^0.31.0';
-export const vitestCoverageIstanbulVersion = '^0.31.0';
+export const vitestCoverageC8Version = '^0.32.0';
+export const vitestCoverageIstanbulVersion = '^0.32.0';
