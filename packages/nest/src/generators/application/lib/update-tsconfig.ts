6cb159e9f7942332343f9d016ed77594f4e7db7b
Gustavo Perdomo <gperdomor@gmail.com>
2023-06-22 17:38:54 -0400

    feat(nest): added es2021 support for nest generators (#17726)
    Co-authored-by: Caleb Ukle <caleb.ukle+github@pm.me>

--- packages/nest/src/generators/application/lib/update-tsconfig.ts
+++ packages/nest/src/generators/application/lib/update-tsconfig.ts
@@ -8,7 +8,7 @@ export function updateTsConfig(tree: Tree, options: NormalizedOptions): void {
     joinPathFragments(options.appProjectRoot, 'tsconfig.app.json'),
     (json) => {
       json.compilerOptions.emitDecoratorMetadata = true;
-      json.compilerOptions.target = 'es2015';
+      json.compilerOptions.target = 'es2021';
       if (options.strict) {
         json.compilerOptions = {
           ...json.compilerOptions,
