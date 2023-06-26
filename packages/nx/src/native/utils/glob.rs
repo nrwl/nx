6acf28e86caed258cec194e7fb93d039c8feac82
Jason Jean <jasonjean1993@gmail.com>
2023-06-26 16:40:26 -0400

    fix(core): fix globbing in rust (#17803)
    (cherry picked from commit 38101fdbf04755a8283eac739fb33090fc81f652)

--- packages/nx/src/native/utils/glob.rs
+++ packages/nx/src/native/utils/glob.rs
@@ -1,10 +1,44 @@
-use globset::{Glob, GlobSet, GlobSetBuilder};
+use globset::{GlobBuilder, GlobSet, GlobSetBuilder};
 
 pub(crate) fn build_glob_set(globs: Vec<String>) -> anyhow::Result<GlobSet> {
-    let mut glob_builder = GlobSetBuilder::new();
+    let mut glob_set_builder = GlobSetBuilder::new();
     for glob in globs {
-        glob_builder.add(Glob::new(&glob).map_err(anyhow::Error::from)?);
+        let glob = GlobBuilder::new(&glob)
+            .literal_separator(true)
+            .build()
+            .map_err(anyhow::Error::from)?;
+        glob_set_builder.add(glob);
     }
 
-    glob_builder.build().map_err(anyhow::Error::from)
+    glob_set_builder.build().map_err(anyhow::Error::from)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn should_detect_package_json() {
+        let glob_set = build_glob_set(vec![String::from("packages/*/package.json")]).unwrap();
+        assert!(glob_set.is_match("packages/nx/package.json"))
+    }
+
+    #[test]
+    fn should_not_detect_deeply_nested_package_json() {
+        let glob_set = build_glob_set(vec![String::from("packages/*/package.json")]).unwrap();
+        assert!(!glob_set.is_match("packages/nx/test-files/package.json"))
+    }
+
+    #[test]
+    fn should_detect_deeply_nested_package_json() {
+        let glob_set = build_glob_set(vec![String::from("packages/**/package.json")]).unwrap();
+        assert!(glob_set.is_match("packages/nx/test-files/package.json"))
+    }
+
+    #[test]
+    fn should_detect_node_modules() {
+        let glob_set = build_glob_set(vec![String::from("**/node_modules")]).unwrap();
+        assert!(glob_set.is_match("node_modules"));
+        assert!(glob_set.is_match("packages/nx/node_modules"));
+    }
 }
