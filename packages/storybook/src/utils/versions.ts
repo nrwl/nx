7bd9932b4a12635d166a17761012dae89d09650c
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-06-27 14:28:03 +0100

    feat(storybook): update storybook version to ^7.0.24

--- packages/storybook/src/utils/versions.ts
+++ packages/storybook/src/utils/versions.ts
@@ -5,6 +5,6 @@ export const storybookTestRunnerVersion = '^0.7.2';
 export const litVersion = '^2.6.1';
 export const tsNodeVersion = '10.9.1';
 
-export const storybookVersion = '^7.0.9';
+export const storybookVersion = '^7.0.24';
 export const reactVersion = '^18.2.0';
 export const viteVersion = '^4.3.4';
