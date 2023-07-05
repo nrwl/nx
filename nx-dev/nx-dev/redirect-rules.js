91aa32161befb3099ca7b8904f182a0ca52421c8
Rares Matei <matei.rar@gmail.com>
2023-07-05 13:29:35 +0100

    docs(nx-cloud): ami deplopyment docs (#17865)

--- nx-dev/nx-dev/redirect-rules.js
+++ nx-dev/nx-dev/redirect-rules.js
@@ -360,6 +360,9 @@ const nxCloudUrls = {
   '/nx-cloud/set-up/add-nx-cloud': '/core-features/share-your-cache',
   '/nx-cloud/set-up/set-up-caching': '/core-features/share-your-cache',
   '/nx-cloud/set-up/set-up-dte': '/core-features/distribute-task-execution',
+  '/nx-cloud/private-cloud/standalone': '/nx-cloud/private-cloud/ami-setup',
+  '/nx-cloud/private-cloud/kubernetes-setup':
+    '/nx-cloud/private-cloud/get-started',
 };
 
 /**
