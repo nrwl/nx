c04053b4e931176d8be6cd364841f3e0990710d2
Jason Jean <jasonjean1993@gmail.com>
2023-07-07 16:06:44 -0400

    fix(core): parse project configs only in js (#18009)

--- packages/nx/src/native/workspace/types.rs
+++ packages/nx/src/native/workspace/types.rs
@@ -1,10 +1,3 @@
-use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-pub(crate) struct ProjectConfiguration {
-    pub name: Option<String>,
-}
-
 #[derive(Debug, Eq, PartialEq)]
 pub enum FileLocation {
     Global,
