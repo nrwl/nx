3c91bff38d7dce8e0f7ed43a400e6c544bb1b0a4
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-07-07 17:00:42 +0300

    chore(nx-dev): broke it

--- nx-dev/nx-dev/pages/ai/index.tsx
+++ nx-dev/nx-dev/pages/ai/index.tsx
@@ -6,15 +6,28 @@ import { useState } from 'react';
 import { useNavToggle } from '../../lib/navigation-toggle.effect';
 import { renderMarkdown } from '@nx/nx-dev/ui-markdoc';
 import { DocumentationHeader } from '@nx/nx-dev/ui-common';
-
-// console.log(nxDevDataAccessAi('Hello'));
-console.log(getStringFromStream(nxDevDataAccessAi('Hello')));
+import { logger } from '@nx/devkit';
 
 export default function Ai(): JSX.Element {
   const { toggleNav, navIsOpen } = useNavToggle();
 
   const [finalResult, setFinalResult] = useState('');
   const [error, setError] = useState(null);
+
+  const handleSubmit = async () => {
+    // setFinalResult('loading...');
+    try {
+      const response = await nxDevDataAccessAi(
+        'What is the best way to set up inputs in my nx.json?'
+      );
+      // const completeText = (await getStringFromStream(response)) as string;
+      // setFinalResult(completeText);
+    } catch (error) {
+      logger.error(error);
+      setError(error);
+    }
+  };
+
   return (
     <div id="shell" className="flex h-full flex-col">
       <div className="w-full flex-shrink-0">
@@ -25,7 +38,7 @@ export default function Ai(): JSX.Element {
         role="main"
         className="flex h-full flex-1 overflow-y-hidden"
       >
-        {/* <button onClick={() => handleSubmit()}>Click me</button> */}
+        <button onClick={() => handleSubmit()}>Click me</button>
         {finalResult && !error ? (
           <div>
             <h3>Answer:</h3>
