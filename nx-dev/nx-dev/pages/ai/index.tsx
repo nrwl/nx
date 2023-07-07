6f9cb4b21500d02b5520f26f49b5e02935a24e52
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-07-07 15:34:51 +0300

    chore(nx-dev): works now

--- nx-dev/nx-dev/pages/ai/index.tsx
+++ nx-dev/nx-dev/pages/ai/index.tsx
@@ -1,12 +1,14 @@
 import { nxDevDataAccessAi } from '@nx/nx-dev/data-access-ai';
 import { Footer, Header } from '@nx/nx-dev/ui-common';
 
+console.log(nxDevDataAccessAi('Hello'));
+
 export default function Ai(): JSX.Element {
   return (
     <>
       <Header />
       <main id="main" role="main">
-        Hello {nxDevDataAccessAi()}
+        Hello kat
       </main>
       <Footer />
     </>
