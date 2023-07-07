3fdc66dd691c8385ac2f88eb156936449378ae89
Jack Hsu <jack.hsu@gmail.com>
2023-07-07 03:47:03 -0400

    feat(vite): pin Vite version range to patch releases only to avoid conflicts with Angular (#18010)

--- packages/vite/src/utils/versions.ts
+++ packages/vite/src/utils/versions.ts
@@ -1,15 +1,15 @@
 export const nxVersion = require('../../package.json').version;
-export const viteVersion = '^4.3.9';
-export const vitestVersion = '^0.32.0';
-export const vitestUiVersion = '^0.32.0';
-export const vitePluginReactVersion = '^4.0.0';
-export const vitePluginReactSwcVersion = '^3.3.2';
-export const viteTsConfigPathsVersion = '^4.2.0';
+export const viteVersion = '~4.3.9';
+export const vitestVersion = '~0.32.0';
+export const vitestUiVersion = '~0.32.0';
+export const vitePluginReactVersion = '~4.0.0';
+export const vitePluginReactSwcVersion = '~3.3.2';
+export const viteTsConfigPathsVersion = '~4.2.0';
 export const jsdomVersion = '~22.1.0';
 export const vitePluginDtsVersion = '~2.3.0';
 export const happyDomVersion = '~9.20.3';
 export const edgeRuntimeVmVersion = '~3.0.2';
 
 // Coverage providers
-export const vitestCoverageC8Version = '^0.32.0';
-export const vitestCoverageIstanbulVersion = '^0.32.0';
+export const vitestCoverageC8Version = '~0.32.0';
+export const vitestCoverageIstanbulVersion = '~0.32.0';
