e2f1c92e034ec07b3cd5bb98070e835b763ae624
Jason Jean <jasonjean1993@gmail.com>
2023-07-18 18:26:43 -0400

    feat(js): introduce rust-based typescript dependency processor (#17976)
    Co-authored-by: Jack Hsu <jack.hsu@gmail.com>

--- packages/nx/src/native/index.d.ts
+++ packages/nx/src/native/index.d.ts
@@ -14,6 +14,7 @@ export function hashArray(input: Array<string>): string
 export function hashFile(file: string): FileData | null
 export function hashFiles(workspaceRoot: string): Record<string, string>
 export function hashFilesMatchingGlobs(directory: string, globPatterns: Array<string>): string | null
+export function findImports(projectFileMap: Record<string, Array<string>>): Array<ImportResult>
 export interface FileData {
   file: string
   hash: string
@@ -44,6 +45,12 @@ export interface NxWorkspaceFiles {
   projectConfigurations: Record<string, object>
 }
 export function getWorkspaceFilesNative(workspaceRoot: string, globs: Array<string>, parseConfigurations: (arg0: Array<string>) => Record<string, object>): NxWorkspaceFiles
+export class ImportResult {
+  file: string
+  sourceProject: string
+  dynamicImportExpressions: Array<string>
+  staticImportExpressions: Array<string>
+}
 export class Watcher {
   origin: string
   /**
