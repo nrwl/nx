d222caaceb828da40af256b194888be177f2657b
Caleb Ukle <caleb.ukle+github@pm.me>
2023-07-19 11:31:55 -0500

    feat(testing): add playwright e2e preset

--- packages/playwright/src/index.ts
+++ packages/playwright/src/index.ts
@@ -4,3 +4,4 @@ export {
 } from './executors/playwright/playwright';
 export { initGenerator } from './generators/init/init';
 export { configurationGenerator } from './generators/configuration/configuration';
+export { nxE2EPreset, NxPlaywrightOptions } from './utils/preset';
