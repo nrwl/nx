aa2c722dea98c1295a8c74bad8235602c7ae1f70
Caleb Ukle <caleb.ukle+github@pm.me>
2023-07-19 15:34:44 -0500

    fix(testing): add preset secondary entrypoint

--- packages/playwright/src/index.ts
+++
@@ -1,7 +0,0 @@
-export {
-  playwrightExecutor,
-  PlaywrightExecutorSchema,
-} from './executors/playwright/playwright';
-export { initGenerator } from './generators/init/init';
-export { configurationGenerator } from './generators/configuration/configuration';
-export { nxE2EPreset, NxPlaywrightOptions } from './utils/preset';
