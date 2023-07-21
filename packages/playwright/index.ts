9199444f48e117498f4323767fdf84a44e0e989a
Caleb Ukle <caleb@nrwl.io>
2023-07-21 16:10:11 -0500

    fix(testing): update playwright tsconfig.base.json/entrypoint (#18227)

--- packages/playwright/index.ts
+++ packages/playwright/index.ts
@@ -4,4 +4,3 @@ export {
 } from './src/executors/playwright/playwright';
 export { initGenerator } from './src/generators/init/init';
 export { configurationGenerator } from './src/generators/configuration/configuration';
-export { nxE2EPreset, NxPlaywrightOptions } from './src/utils/preset';
