0cc297fb919f4a1460305c33454de40a224a9243
Miroslav Jonas <missing.manual@gmail.com>
2023-07-24 16:25:06 +0200

    fix(nx-dev): fix wrong link

--- nx-dev/nx-dev/lib/plugins.api.ts
+++ nx-dev/nx-dev/lib/plugins.api.ts
@@ -1,5 +1,5 @@
 import { DocumentsApi } from '@nx/nx-dev/data-access-documents/node-only';
-import documents from '../public/documentation/generated/manifests/plugins.json';
+import documents from '../public/documentation/generated/manifests/extending-nx.json';
 import { tagsApi } from './tags.api';
 
 export const nxPluginsApi = new DocumentsApi({
