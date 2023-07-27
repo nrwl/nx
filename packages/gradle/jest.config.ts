ceba261ee114faab05096c884c025f1b8e6dcf0f
Emily Xiong <xiongemi@gmail.com>
2023-07-27 15:02:00 -0400

    feat(core): add empty gradle package

---
+++ packages/gradle/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'gradle',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/gradle',
+};
