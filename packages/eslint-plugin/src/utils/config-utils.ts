2fc2a16935cd0b0572eee8a1966ea0979315dd43
Miroslav Jonas <missing.manual@gmail.com>
2023-08-03 09:21:01 +0200

    feat(linter): use require.resolve to detect version

--- packages/eslint-plugin/src/utils/config-utils.ts
+++ packages/eslint-plugin/src/utils/config-utils.ts
@@ -1,26 +1,13 @@
-import { detectPackageManager } from '@nx/devkit';
-import { execSync } from 'child_process';
-import { accessSync } from 'fs';
-
 /**
  * Checks if package is available
  * @param name name of the package
  * @returns
  */
 export function packageExists(name: string): boolean {
-  const pm = detectPackageManager();
-  if (pm === 'yarn') {
-    try {
-      execSync(`yarn info ${name}`);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
   try {
-    accessSync(`./node_modules/.bin/${name}`);
+    require.resolve(name);
     return true;
-  } catch (e) {
+  } catch {
     return false;
   }
 }
