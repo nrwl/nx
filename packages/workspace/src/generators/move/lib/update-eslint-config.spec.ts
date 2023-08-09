634e384cb01507e41c081f4a7256a46ea6e6a6bc
Miroslav Jonas <missing.manual@gmail.com>
2023-08-10 00:05:52 +0200

    fix(linter): add support for move command

--- packages/workspace/src/generators/move/lib/update-eslint-config.spec.ts
+++ packages/workspace/src/generators/move/lib/update-eslint-config.spec.ts
@@ -1,4 +1,6 @@
 import {
+  joinPathFragments,
+  offsetFromRoot,
   readJson,
   readProjectConfiguration,
   Tree,
@@ -230,3 +232,222 @@ describe('updateEslint', () => {
     ).toEqual({ project: `libs/shared/my-destination/${storybookProject}` });
   });
 });
+
+describe('updateEslint (flat config)', () => {
+  let tree: Tree;
+  let schema: NormalizedSchema;
+
+  beforeEach(async () => {
+    schema = {
+      projectName: 'my-lib',
+      destination: 'shared/my-destination',
+      importPath: '@proj/shared-my-destination',
+      updateImportPath: true,
+      newProjectName: 'shared-my-destination',
+      relativeToRootDestination: 'libs/shared/my-destination',
+    };
+
+    tree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
+  });
+
+  it('should handle config not existing', async () => {
+    await libraryGenerator(tree, {
+      name: 'my-lib',
+      linter: Linter.None,
+    });
+
+    const projectConfig = readProjectConfiguration(tree, 'my-lib');
+
+    expect(() => {
+      updateEslintConfig(tree, schema, projectConfig);
+    }).not.toThrow();
+  });
+
+  it('should update config extends path when project is moved to subdirectory', async () => {
+    await libraryGenerator(tree, {
+      name: 'my-lib',
+      linter: Linter.EsLint,
+    });
+    convertToFlat(tree, 'libs/my-lib');
+    // This step is usually handled elsewhere
+    tree.rename(
+      'libs/my-lib/eslint.config.js',
+      'libs/shared/my-destination/eslint.config.js'
+    );
+    const projectConfig = readProjectConfiguration(tree, 'my-lib');
+
+    updateEslintConfig(tree, schema, projectConfig);
+
+    expect(
+      tree.read('libs/shared/my-destination/eslint.config.js', 'utf-8')
+    ).toEqual(expect.stringContaining(`require('../../../eslint.config.js')`));
+  });
+
+  it('should update config extends path when project is moved from subdirectory', async () => {
+    await libraryGenerator(tree, {
+      name: 'test',
+      directory: 'api',
+      linter: Linter.EsLint,
+    });
+    convertToFlat(tree, 'libs/api/test');
+    // This step is usually handled elsewhere
+    tree.rename('libs/api/test/eslint.config.js', 'libs/test/eslint.config.js');
+
+    const projectConfig = readProjectConfiguration(tree, 'api-test');
+
+    const newSchema = {
+      projectName: 'api-test',
+      destination: 'test',
+      importPath: '@proj/test',
+      updateImportPath: true,
+      newProjectName: 'test',
+      relativeToRootDestination: 'libs/test',
+    };
+
+    updateEslintConfig(tree, newSchema, projectConfig);
+
+    expect(tree.read('libs/test/eslint.config.js', 'utf-8')).toEqual(
+      expect.stringContaining(`require('../../eslint.config.js')`)
+    );
+  });
+
+  it('should update config overrides parser project when project is moved', async () => {
+    await libraryGenerator(tree, {
+      name: 'my-lib',
+      linter: Linter.EsLint,
+      setParserOptionsProject: true,
+    });
+    convertToFlat(tree, 'libs/my-lib', { hasParser: true });
+    // This step is usually handled elsewhere
+    tree.rename(
+      'libs/my-lib/eslint.config.js',
+      'libs/shared/my-destination/eslint.config.js'
+    );
+    const projectConfig = readProjectConfiguration(tree, 'my-lib');
+
+    updateEslintConfig(tree, schema, projectConfig);
+
+    expect(
+      tree.read('libs/shared/my-destination/eslint.config.js', 'utf-8')
+    ).toEqual(
+      expect.stringContaining(
+        `project: ["libs/shared/my-destination/tsconfig.*?.json"]`
+      )
+    );
+  });
+
+  it('should update multiple config overrides parser project when project is moved', async () => {
+    await libraryGenerator(tree, {
+      name: 'my-lib',
+      linter: Linter.EsLint,
+      setParserOptionsProject: true,
+    });
+    // Add another parser project to eslint.json
+    const storybookProject = '.storybook/tsconfig.json';
+    convertToFlat(tree, 'libs/my-lib', {
+      hasParser: true,
+      anotherProject: storybookProject,
+    });
+    // This step is usually handled elsewhere
+    tree.rename(
+      'libs/my-lib/eslint.config.js',
+      'libs/shared/my-destination/eslint.config.js'
+    );
+    const projectConfig = readProjectConfiguration(tree, 'my-lib');
+
+    updateEslintConfig(tree, schema, projectConfig);
+
+    expect(
+      tree.read('libs/shared/my-destination/eslint.config.js', 'utf-8')
+    ).toEqual(
+      expect.stringContaining(
+        `project: ["libs/shared/my-destination/tsconfig.*?.json", "libs/shared/my-destination/${storybookProject}"]`
+      )
+    );
+  });
+
+  it('should update config parserOptions.project as a string', async () => {
+    await libraryGenerator(tree, {
+      name: 'my-lib',
+      linter: Linter.EsLint,
+      setParserOptionsProject: true,
+    });
+
+    convertToFlat(tree, 'libs/my-lib', { hasParser: true, isString: true });
+    // This step is usually handled elsewhere
+    tree.rename(
+      'libs/my-lib/eslint.config.js',
+      'libs/shared/my-destination/eslint.config.js'
+    );
+    const projectConfig = readProjectConfiguration(tree, 'my-lib');
+
+    updateEslintConfig(tree, schema, projectConfig);
+
+    expect(
+      tree.read('libs/shared/my-destination/eslint.config.js', 'utf-8')
+    ).toEqual(
+      expect.stringContaining(
+        `project: "libs/shared/my-destination/tsconfig.*?.json"`
+      )
+    );
+  });
+});
+
+function convertToFlat(
+  tree: Tree,
+  path: string,
+  options: {
+    hasParser?: boolean;
+    anotherProject?: string;
+    isString?: boolean;
+  } = {}
+) {
+  const offset = offsetFromRoot(path);
+  tree.delete(joinPathFragments(path, '.eslintrc.json'));
+
+  let parserOptions = '';
+  if (options.hasParser) {
+    const paths = options.anotherProject
+      ? `["${path}/tsconfig.*?.json", "${path}/${options.anotherProject}"]`
+      : options.isString
+      ? `"${path}/tsconfig.*?.json"`
+      : `["${path}/tsconfig.*?.json"]`;
+    parserOptions = `languageOptions: {
+      parserOptions: {
+        project: ${paths}
+      }
+    },
+    `;
+  }
+
+  tree.write(
+    joinPathFragments(path, 'eslint.config.js'),
+    `const { FlatCompat } = require("@eslint/eslintrc");
+  const baseConfig = require("${offset}eslint.config.js");
+  const js = require("@eslint/js");
+  const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+  });
+  module.exports = [
+    ...baseConfig,
+    {
+      files: ["${path}/**/*.ts", "${path}/**/*.tsx", "${path}/**/*.js", "${path}/**/*.jsx"],
+      ${parserOptions}rules: {}
+    },
+    {
+      files: ["${path}/**/*.ts", "${path}/**/*.tsx"],
+      rules: {}
+    },
+    {
+      files: ["${path}/**/*.js", "${path}/**/*.jsx"],
+      rules: {}
+    },
+    ...compat.config({ parser: "jsonc-eslint-parser" }).map(config => ({
+      ...config,
+      files: ["${path}/**/*.json"],
+      rules: { "@nx/dependency-checks": "error" }
+    }))
+  ];`
+  );
+}
--- packages/workspace/src/generators/move/lib/update-eslint-config.ts
+++ packages/workspace/src/generators/move/lib/update-eslint-config.ts
@@ -43,41 +43,96 @@ export function updateEslintConfig(
   project: ProjectConfiguration
 ) {
   const offset = offsetFromRoot(schema.relativeToRootDestination);
+  const eslintJsonPath = join(
+    schema.relativeToRootDestination,
+    '.eslintrc.json'
+  );
 
-  const eslintRcPath = join(schema.relativeToRootDestination, '.eslintrc.json');
-
-  if (!tree.exists(eslintRcPath)) {
-    // no .eslintrc found. nothing to do
-    return;
-  }
-
-  updateJson<PartialEsLintRcJson>(tree, eslintRcPath, (eslintRcJson) => {
-    if (typeof eslintRcJson.extends === 'string') {
-      eslintRcJson.extends = offsetFilePath(
-        project,
-        eslintRcJson.extends,
-        offset
-      );
-    } else if (eslintRcJson.extends) {
-      eslintRcJson.extends = eslintRcJson.extends.map((extend: string) =>
-        offsetFilePath(project, extend, offset)
-      );
-    }
-
-    eslintRcJson.overrides?.forEach(
-      (o: { parserOptions?: { project?: string | string[] } }) => {
-        if (o.parserOptions?.project) {
-          o.parserOptions.project = Array.isArray(o.parserOptions.project)
-            ? o.parserOptions.project.map((p) =>
-                p.replace(project.root, schema.relativeToRootDestination)
-              )
-            : o.parserOptions.project.replace(
-                project.root,
-                schema.relativeToRootDestination
-              );
+  if (tree.exists(eslintJsonPath)) {
+    return updateJson<PartialEsLintRcJson>(
+      tree,
+      eslintJsonPath,
+      (eslintRcJson) => {
+        if (typeof eslintRcJson.extends === 'string') {
+          eslintRcJson.extends = offsetFilePath(
+            project,
+            eslintRcJson.extends,
+            offset
+          );
+        } else if (eslintRcJson.extends) {
+          eslintRcJson.extends = eslintRcJson.extends.map((extend: string) =>
+            offsetFilePath(project, extend, offset)
+          );
         }
+
+        eslintRcJson.overrides?.forEach(
+          (o: { parserOptions?: { project?: string | string[] } }) => {
+            if (o.parserOptions?.project) {
+              o.parserOptions.project = Array.isArray(o.parserOptions.project)
+                ? o.parserOptions.project.map((p) =>
+                    p.replace(project.root, schema.relativeToRootDestination)
+                  )
+                : o.parserOptions.project.replace(
+                    project.root,
+                    schema.relativeToRootDestination
+                  );
+            }
+          }
+        );
+        return eslintRcJson;
       }
     );
-    return eslintRcJson;
-  });
+  }
+
+  const eslintFlatPath = join(
+    schema.relativeToRootDestination,
+    'eslint.config.js'
+  );
+  if (tree.exists(eslintFlatPath)) {
+    const config = tree.read(eslintFlatPath, 'utf-8');
+    tree.write(
+      eslintFlatPath,
+      replaceFlatConfigPaths(
+        config,
+        project,
+        offset,
+        schema.relativeToRootDestination
+      )
+      // config.replace(
+      //   /require(['"](.*)['"])/g,
+      //   `require('` + offsetFilePath(project, `$1`, offset) + `')`
+      // )
+    );
+  }
+}
+
+function replaceFlatConfigPaths(
+  config: string,
+  project: ProjectConfiguration,
+  offset: string,
+  pathToDestination: string
+): string {
+  let match;
+  let newConfig = config;
+
+  // replace requires
+  const requireRegex = RegExp(/require\(['"](.*)['"]\)/g);
+  while ((match = requireRegex.exec(newConfig)) !== null) {
+    const newPath = offsetFilePath(project, match[1], offset);
+    newConfig =
+      newConfig.slice(0, match.index) +
+      `require('${newPath}')` +
+      newConfig.slice(match.index + match[0].length);
+  }
+  // replace projects
+  const projectRegex = RegExp(/project:\s?\[?['"](.*)['"]\]?/g);
+  while ((match = projectRegex.exec(newConfig)) !== null) {
+    const newProjectDef = match[0].replaceAll(project.root, pathToDestination);
+    newConfig =
+      newConfig.slice(0, match.index) +
+      newProjectDef +
+      newConfig.slice(match.index + match[0].length);
+  }
+
+  return newConfig;
 }
