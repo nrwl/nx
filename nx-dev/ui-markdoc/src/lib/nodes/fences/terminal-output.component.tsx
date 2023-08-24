67d9e7da0e8847155262f675ee666bba183eaf42
Isaac Mann <isaacplmann@gmail.com>
2023-08-24 10:06:55 -0400

    docs(core): fix shell component

--- nx-dev/ui-markdoc/src/lib/nodes/fences/terminal-output.component.tsx
+++ nx-dev/ui-markdoc/src/lib/nodes/fences/terminal-output.component.tsx
@@ -12,19 +12,28 @@ export function TerminalOutput({
   isMessageBelow: boolean;
   path: string;
 }): JSX.Element {
+  const commandLines = command.split('\n').filter(Boolean);
   return (
     <TerminalShellWrapper isMessageBelow={isMessageBelow}>
       <div className="p-4 pt-2 overflow-x-auto">
-        <div className="flex items-center">
-          <p className="mt-0.5">
-            {path && (
-              <span className="text-purple-600 dark:text-fuchsia-500">
-                {path}
-              </span>
-            )}
-            <span className="mx-1 text-green-600 dark:text-green-400">❯</span>
-          </p>
-          <p className="typing mt-0.5 flex-1 pl-2">{command}</p>
+        <div className="flex flex-col items-left">
+          {commandLines.map((line, index) => {
+            return (
+              <div key={index} className="flex items-center">
+                <p className="mt-0.5">
+                  {path && (
+                    <span className="text-purple-600 dark:text-fuchsia-500">
+                      {path}
+                    </span>
+                  )}
+                  <span className="mx-1 text-green-600 dark:text-green-400">
+                    ❯
+                  </span>
+                </p>
+                <p className="typing mt-0.5 flex-1 pl-2">{line}</p>
+              </div>
+            );
+          })}
         </div>
         <div className="flex not-prose">{content}</div>
       </div>
