99eebfb5ea7fac2dba1aab341811ba3b724251da
Miroslav Jonaš <meeroslav@users.noreply.github.com>
2023-09-05 11:48:51 +0200

    chore(repo): disable flaky storybook test (#18997)

--- e2e/storybook-angular/src/storybook-angular.test.ts
+++ e2e/storybook-angular/src/storybook-angular.test.ts
@@ -38,7 +38,8 @@ describe('Storybook executors for Angular', () => {
     }, 200_000);
 
     // Increased timeout because 92% sealing asset processing TerserPlugin
-    it('shoud build an Angular based storybook', () => {
+    // TODO(meeroslav) this test is still flaky and breaks the PR runs. We need to investigate why.
+    xit('shoud build an Angular based storybook', () => {
       runCLI(`run ${angularStorybookLib}:build-storybook`);
       checkFilesExist(`dist/storybook/${angularStorybookLib}/index.html`);
     }, 1_000_000);
