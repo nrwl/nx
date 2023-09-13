54223a78e8dfd7b057b13f9f51e20a734e01322c
Juri <juri.strumpflohner@gmail.com>
2023-09-13 22:59:32 +0200

    docs(angular): update Nx and Angular CLI page with more details and video embed

--- packages/playwright/src/utils/versions.ts
+++ packages/playwright/src/utils/versions.ts
@@ -1,3 +1,3 @@
 export const nxVersion = require('../../package.json').version;
-export const playwrightVersion = '^1.36.0';
-export const eslintPluginPlaywrightVersion = '^0.15.3';
+export const playwrightVersion = '^1.38.0';
+export const eslintPluginPlaywrightVersion = '^0.16.0';
