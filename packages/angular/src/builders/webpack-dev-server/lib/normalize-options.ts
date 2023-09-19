b15173f8f0fdd157348bfe2ce0fb73e946972618
titan-ron <30556071+titan-ron@users.noreply.github.com>
2023-09-20 01:21:57 +0300

    feat(angular): support `~` path for **webpack-dev-server** ssl

--- packages/angular/src/builders/webpack-dev-server/lib/normalize-options.ts
+++ packages/angular/src/builders/webpack-dev-server/lib/normalize-options.ts
@@ -1,12 +1,17 @@
+import { homedir } from 'node:os';
 import type { Schema } from '../schema';
 
 export function normalizeOptions(schema: Schema): Schema {
+  const { ssl, sslCert, sslKey } = schema;
+
   return {
     host: 'localhost',
     port: 4200,
     liveReload: true,
     open: false,
-    ssl: false,
     ...schema,
+    ssl: ssl ?? false,
+    ...(sslCert ? { sslCert: sslCert.replace('~', homedir()) } : {}),
+    ...(sslKey ? { sslKey: sslKey.replace('~', homedir()) } : {}),
   };
 }
