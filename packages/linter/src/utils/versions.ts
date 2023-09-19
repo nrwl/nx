030385809ba22cadae0b2467f0604dcce245a7fb
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2023-09-19 13:41:04 +0000

    chore(core): update eslint-config-prettier to v9

--- packages/linter/src/utils/versions.ts
+++ packages/linter/src/utils/versions.ts
@@ -2,5 +2,5 @@ export const nxVersion = require('../../package.json').version;
 
 export const eslintVersion = '~8.46.0';
 export const eslintrcVersion = '^2.1.1';
-export const eslintConfigPrettierVersion = '8.1.0';
+export const eslintConfigPrettierVersion = '9.0.0';
 export const typescriptESLintVersion = '^5.60.1';
