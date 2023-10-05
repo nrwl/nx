361a263f8a223165c02a4c0549cb3f8deb2c0428
Colum Ferry <cferry09@gmail.com>
2023-10-05 11:49:46 +0100

    fix(react): get correct path to tsconfig

--- e2e/react-core/src/react-module-federation.test.ts
+++ e2e/react-core/src/react-module-federation.test.ts
@@ -64,9 +64,9 @@ describe('React Module Federation', () => {
       stripIndents`
         import { composePlugins, withNx, ModuleFederationConfig } from '@nx/webpack';
         import { withReact } from '@nx/react';
-        import { withModuleFederation } from '@nx/react/module-federation');
+        import { withModuleFederation } from '@nx/react/module-federation';
         
-        const baseConfig = require('./module-federation.config');
+        import baseConfig from './module-federation.config';
         
         const config: ModuleFederationConfig = {
           ...baseConfig,
--- packages/webpack/src/utils/webpack/custom-webpack.ts
+++ packages/webpack/src/utils/webpack/custom-webpack.ts
@@ -2,15 +2,24 @@ import { registerTsProject } from '@nx/js/src/internal';
 
 export function resolveCustomWebpackConfig(path: string, tsConfig: string) {
   const cleanupTranspiler = registerTsProject(tsConfig);
-  const customWebpackConfig = require(path);
+  const maybeCustomWebpackConfig = require(path);
   cleanupTranspiler();
 
   // If the user provides a configuration in TS file
-  // then there are 2 cases for exporing an object. The first one is:
+  // then there are 3 cases for exporing an object. The first one is:
   // `module.exports = { ... }`. And the second one is:
   // `export default { ... }`. The ESM format is compiled into:
   // `{ default: { ... } }`
-  return customWebpackConfig.default || customWebpackConfig;
+  // There is also a case of
+  // `{ default: { default: { ... } }`
+  const customWebpackConfig =
+    'default' in maybeCustomWebpackConfig
+      ? 'default' in maybeCustomWebpackConfig.default
+        ? maybeCustomWebpackConfig.default.default
+        : maybeCustomWebpackConfig.default
+      : maybeCustomWebpackConfig;
+
+  return customWebpackConfig;
 }
 
 export function isRegistered() {
