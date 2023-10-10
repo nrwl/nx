66daa9fb80825cc713261f572811959afe563b63
Miroslav Jonas <missing.manual@gmail.com>
2023-10-10 21:19:33 +0200

    feat(linter): add migration for executor and package

--- packages/eslint/src/migrations/update-16-0-0-add-nx-packages/update-16-0-0-add-nx-packages.spec.ts
+++ packages/eslint/src/migrations/update-16-0-0-add-nx-packages/update-16-0-0-add-nx-packages.spec.ts
@@ -24,13 +24,13 @@ describe('update-16-0-0-add-nx-packages', () => {
     ).not.toBeDefined();
   });
 
-  it('should add a dependency on @nx/eslint', async () => {
+  it('should add a dependency on @nx/linter', async () => {
     await replacePackage(tree);
 
     const packageJson = readJson(tree, 'package.json');
     const newDependencyVersion =
-      packageJson.devDependencies['@nx/eslint'] ??
-      packageJson.dependencies['@nx/eslint'];
+      packageJson.devDependencies['@nx/linter'] ??
+      packageJson.dependencies['@nx/linter'];
 
     expect(newDependencyVersion).toBeDefined();
   });
