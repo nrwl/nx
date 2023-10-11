47e9fc8ed6140b576913de18e66c9a3470d77c19
Jason Jean <jasonjean1993@gmail.com>
2023-10-11 10:46:17 -0400

    feat(misc): remove migrations for v14 and under (#19552)

--- packages/plugin/src/migrations/update-14-1-9/add-swc-deps-if-needed.ts
+++
@@ -1,8 +0,0 @@
-import { formatFiles, installPackagesTask, Tree } from '@nx/devkit';
-import { addSwcRegisterDependencies } from '@nx/js/src/utils/swc/add-swc-dependencies';
-
-export default async function addSwcNodeIfNeeded(tree: Tree) {
-  addSwcRegisterDependencies(tree);
-  await formatFiles(tree);
-  return installPackagesTask(tree);
-}
