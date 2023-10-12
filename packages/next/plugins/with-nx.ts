3bf599d3d91ce8892bbcb7e76cd916bbb5ee0a8c
Nicholas Cunningham <ndcunningham@gmail.com>
2023-10-12 12:04:58 -0600

    fix(nextjs): Add missing environment variables for Nx 18 environment variables
    closes: #19492

--- packages/next/plugins/with-nx.ts
+++ packages/next/plugins/with-nx.ts
@@ -45,6 +45,9 @@ const baseNXEnvironmentVariables = [
   'NX_NEXT_OUTPUT_PATH',
   'NX_E2E_RUN_E2E',
   'NX_E2E_CI_CACHE_KEY',
+  'NX_MAPPINGS',
+  'NX_FILE_TO_RUN',
+  'NX_NEXT_PUBLIC_DIR',
 ];
 
 export interface WithNxOptions extends NextConfig {
