5b8f95b26d7e77dee2464bc959eb5207e3089525
Colum Ferry <cferry09@gmail.com>
2023-10-13 15:07:12 +0100

    feat(angular): use consistent artifact generation for generators

--- packages/angular/src/generators/scam-directive/lib/normalize-options.ts
+++ packages/angular/src/generators/scam-directive/lib/normalize-options.ts
@@ -1,28 +1,43 @@
 import type { Tree } from '@nx/devkit';
 import { normalizeNameAndPaths } from '../../utils/path';
 import type { NormalizedSchema, Schema } from '../schema';
+import { determineArtifactNameAndDirectoryOptions } from '@nx/devkit/src/generators/artifact-name-and-directory-utils';
+import { names } from '@nx/devkit';
 
-export function normalizeOptions(
+export async function normalizeOptions(
   tree: Tree,
   options: Schema
-): NormalizedSchema {
-  const { directory, fileName, filePath, name, path } = normalizeNameAndPaths(
-    tree,
-    {
-      ...options,
-      type: 'directive',
-    }
-  );
+): Promise<NormalizedSchema> {
+  const {
+    artifactName: name,
+    directory,
+    fileName,
+    filePath,
+    project,
+  } = await determineArtifactNameAndDirectoryOptions(tree, {
+    artifactType: 'directive',
+    callingGenerator: '@nx/angular:scam-directive',
+    name: options.name,
+    directory: options.directory ?? options.path,
+    flat: options.flat,
+    nameAndDirectoryFormat: options.nameAndDirectoryFormat,
+    project: options.project,
+    suffix: 'directive',
+  });
+
+  const { className } = names(name);
+  const { className: suffixClassName } = names('directive');
+  const symbolName = `${className}${suffixClassName}`;
 
   return {
     ...options,
     export: options.export ?? true,
-    flat: options.flat ?? true,
     inlineScam: options.inlineScam ?? true,
     directory,
     fileName,
     filePath,
-    path,
     name,
+    symbolName,
+    project,
   };
 }
