5d0c7832d4501f8eb735ea22b711813081e86dbb
Jack Hsu <jack.hsu@gmail.com>
2023-10-17 16:02:45 -0400

    feat(linter): update @typescript-eslint/* versions

--- packages/eslint/src/utils/versions.ts
+++ packages/eslint/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = require('../../package.json').version;
 
-export const eslintVersion = '~8.46.0';
+export const eslintVersion = '~8.51.0';
 export const eslintrcVersion = '^2.1.1';
 export const eslintConfigPrettierVersion = '9.0.0';
-export const typescriptESLintVersion = '^5.60.1';
+export const typescriptESLintVersion = '^6.8.0';
