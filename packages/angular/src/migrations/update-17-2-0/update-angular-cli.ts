018ade03abe35636011d86e5b9ad29790d06e11c
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-10-18 12:22:57 +0200

    feat(angular): support angular 17.0.0-next.9

--- packages/angular/src/migrations/update-17-2-0/update-angular-cli.ts
+++ packages/angular/src/migrations/update-17-2-0/update-angular-cli.ts
@@ -1,6 +1,6 @@
 import { formatFiles, Tree, updateJson } from '@nx/devkit';
 
-export const angularCliVersion = '~17.0.0-next.7';
+export const angularCliVersion = '~17.0.0-next.9';
 
 export default async function (tree: Tree) {
   let shouldFormat = false;
