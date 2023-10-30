d6085c511567b147eb3ada7356ed91b147fa2d13
Emily Xiong <xiongemi@gmail.com>
2023-10-30 10:40:06 -0400

    feat(js): allow tsc batch to have task without tsc

--- packages/js/src/executors/tsc/lib/batch/normalize-tasks-options.ts
+++ packages/js/src/executors/tsc/lib/batch/normalize-tasks-options.ts
@@ -8,14 +8,15 @@ import { normalizeOptions } from '../normalize-options';
 
 export function normalizeTasksOptions(
   inputs: Record<string, ExecutorOptions>,
-  context: ExecutorContext
+  context: ExecutorContext,
+  overrides: ExecutorOptions
 ): Record<string, NormalizedExecutorOptions> {
   return Object.entries(inputs).reduce((tasksOptions, [taskName, options]) => {
     const { project } = parseTargetString(taskName, context);
     const { sourceRoot, root } =
       context.projectsConfigurations.projects[project];
     tasksOptions[taskName] = normalizeOptions(
-      options,
+      { ...options, ...overrides },
       context.root,
       sourceRoot,
       root
