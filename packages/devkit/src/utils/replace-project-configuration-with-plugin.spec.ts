bdde0ddc9835fa971e9aae69faf7248367288344
Jason Jean <jasonjean1993@gmail.com>
2023-10-31 14:21:17 -0400

    chore(devkit): add util for migrating to a plugin (#19942)

---
+++ packages/devkit/src/utils/replace-project-configuration-with-plugin.spec.ts
@@ -0,0 +1,447 @@
+import { Tree } from 'nx/src/generators/tree';
+import { CreateNodes } from 'nx/src/utils/nx-plugin';
+import { createTreeWithEmptyWorkspace } from 'nx/src/generators/testing-utils/create-tree-with-empty-workspace';
+import {
+  addProjectConfiguration,
+  readProjectConfiguration,
+} from 'nx/src/generators/utils/project-configuration';
+
+import { replaceProjectConfigurationsWithPlugin } from './replace-project-configuration-with-plugin';
+
+describe('replaceProjectConfigurationsWithPlugin', () => {
+  let tree: Tree;
+  let createNodes: CreateNodes;
+
+  beforeEach(async () => {
+    tree = createTreeWithEmptyWorkspace();
+    tree.write('proj/file.txt', '');
+    createNodes = [
+      'proj/file.txt',
+      () => ({
+        projects: {
+          proj: {
+            root: 'proj',
+            targets: {
+              build: {
+                executor: 'nx:run-commands',
+                dependsOn: ['^build-base'],
+                inputs: ['default', '^default'],
+                outputs: ['{options.output}', '{projectRoot}/outputs'],
+                options: {
+                  configFile: 'file.txt',
+                },
+                configurations: {
+                  production: {
+                    configFile: 'file.prod.txt',
+                  },
+                },
+              },
+            },
+          },
+        },
+      }),
+    ];
+  });
+
+  it('should not update the target when it uses a different executor', async () => {
+    const buildTarget = {
+      executor: 'nx:run-script',
+      inputs: ['default', '^default'],
+      outputs: ['{options.output}', '{projectRoot}/outputs'],
+      options: {
+        configFile: 'file.txt',
+      },
+    };
+    addProjectConfiguration(tree, 'proj', {
+      root: 'proj',
+      targets: {
+        build: buildTarget,
+      },
+    });
+
+    replaceProjectConfigurationsWithPlugin(
+      tree,
+      new Map([['proj', 'proj']]),
+      'plugin-path',
+      createNodes,
+      {}
+    );
+
+    expect(readProjectConfiguration(tree, 'proj').targets.build).toEqual(
+      buildTarget
+    );
+  });
+
+  describe('options', () => {
+    it('should be removed when there are no other options', async () => {
+      addProjectConfiguration(tree, 'proj', {
+        root: 'proj',
+        targets: {
+          build: {
+            executor: 'nx:run-commands',
+            inputs: ['default', '^default'],
+            outputs: ['{options.output}', '{projectRoot}/outputs'],
+            options: {
+              configFile: 'file.txt',
+            },
+          },
+        },
+      });
+
+      replaceProjectConfigurationsWithPlugin(
+        tree,
+        new Map([['proj', 'proj']]),
+        'plugin-path',
+        createNodes,
+        {}
+      );
+
+      expect(
+        readProjectConfiguration(tree, 'proj').targets.build
+      ).toBeUndefined();
+    });
+
+    it('should not be removed when there are other options', async () => {
+      addProjectConfiguration(tree, 'proj', {
+        root: 'proj',
+        targets: {
+          build: {
+            executor: 'nx:run-commands',
+            inputs: ['default', '^default'],
+            outputs: ['{options.output}', '{projectRoot}/outputs'],
+            options: {
+              configFile: 'file.txt',
+              watch: false,
+            },
+          },
+        },
+      });
+
+      replaceProjectConfigurationsWithPlugin(
+        tree,
+        new Map([['proj', 'proj']]),
+        'plugin-path',
+        createNodes,
+        {}
+      );
+
+      expect(readProjectConfiguration(tree, 'proj').targets.build).toEqual({
+        options: {
+          watch: false,
+        },
+      });
+    });
+  });
+
+  describe('inputs', () => {
+    it('should not be removed if there are additional inputs', () => {
+      addProjectConfiguration(tree, 'proj', {
+        root: 'proj',
+        targets: {
+          build: {
+            executor: 'nx:run-commands',
+            inputs: ['default', '^default', '{workspaceRoot}/file.txt'],
+            outputs: ['{options.output}', '{projectRoot}/outputs'],
+            options: {
+              configFile: 'file.txt',
+            },
+          },
+        },
+      });
+
+      replaceProjectConfigurationsWithPlugin(
+        tree,
+        new Map([['proj', 'proj']]),
+        'plugin-path',
+        createNodes,
+        {}
+      );
+
+      expect(readProjectConfiguration(tree, 'proj').targets.build).toEqual({
+        inputs: ['default', '^default', '{workspaceRoot}/file.txt'],
+      });
+    });
+
+    it('should not be removed if there are additional inputs which are objects', () => {
+      addProjectConfiguration(tree, 'proj', {
+        root: 'proj',
+        targets: {
+          build: {
+            executor: 'nx:run-commands',
+            inputs: [
+              'default',
+              '^default',
+              {
+                env: 'HOME',
+              },
+            ],
+            outputs: ['{options.output}', '{projectRoot}/outputs'],
+            options: {
+              configFile: 'file.txt',
+            },
+          },
+        },
+      });
+
+      replaceProjectConfigurationsWithPlugin(
+        tree,
+        new Map([['proj', 'proj']]),
+        'plugin-path',
+        createNodes,
+        {}
+      );
+
+      expect(readProjectConfiguration(tree, 'proj').targets.build).toEqual({
+        inputs: [
+          'default',
+          '^default',
+          {
+            env: 'HOME',
+          },
+        ],
+      });
+    });
+
+    it('should not be removed if there are less inputs', () => {
+      addProjectConfiguration(tree, 'proj', {
+        root: 'proj',
+        targets: {
+          build: {
+            executor: 'nx:run-commands',
+            inputs: ['default'],
+            outputs: ['{options.output}', '{projectRoot}/outputs'],
+            options: {
+              configFile: 'file.txt',
+            },
+          },
+        },
+      });
+
+      replaceProjectConfigurationsWithPlugin(
+        tree,
+        new Map([['proj', 'proj']]),
+        'plugin-path',
+        createNodes,
+        {}
+      );
+
+      expect(readProjectConfiguration(tree, 'proj').targets.build).toEqual({
+        inputs: ['default'],
+      });
+    });
+  });
+
+  describe('outputs', () => {
+    it('should not be removed if there are additional outputs', () => {
+      addProjectConfiguration(tree, 'proj', {
+        root: 'proj',
+        targets: {
+          build: {
+            executor: 'nx:run-commands',
+            inputs: ['default', '^default'],
+            outputs: [
+              '{options.output}',
+              '{projectRoot}/outputs',
+              '{projectRoot}/more-outputs',
+            ],
+            options: {
+              configFile: 'file.txt',
+            },
+          },
+        },
+      });
+
+      replaceProjectConfigurationsWithPlugin(
+        tree,
+        new Map([['proj', 'proj']]),
+        'plugin-path',
+        createNodes,
+        {}
+      );
+
+      expect(readProjectConfiguration(tree, 'proj').targets.build).toEqual({
+        outputs: [
+          '{options.output}',
+          '{projectRoot}/outputs',
+          '{projectRoot}/more-outputs',
+        ],
+      });
+    });
+
+    it('should not be removed if there are less outputs', () => {
+      addProjectConfiguration(tree, 'proj', {
+        root: 'proj',
+        targets: {
+          build: {
+            executor: 'nx:run-commands',
+            outputs: ['{options.output}'],
+            options: {
+              configFile: 'file.txt',
+            },
+          },
+        },
+      });
+
+      replaceProjectConfigurationsWithPlugin(
+        tree,
+        new Map([['proj', 'proj']]),
+        'plugin-path',
+        createNodes,
+        {}
+      );
+
+      expect(readProjectConfiguration(tree, 'proj').targets.build).toEqual({
+        outputs: ['{options.output}'],
+      });
+    });
+  });
+
+  describe('dependsOn', () => {
+    it('should be removed when it is the same', () => {
+      addProjectConfiguration(tree, 'proj', {
+        root: 'proj',
+        targets: {
+          build: {
+            executor: 'nx:run-commands',
+            dependsOn: ['^build-base'],
+            options: {
+              configFile: 'file.txt',
+            },
+          },
+        },
+      });
+
+      replaceProjectConfigurationsWithPlugin(
+        tree,
+        new Map([['proj', 'proj']]),
+        'plugin-path',
+        createNodes,
+        {}
+      );
+
+      expect(
+        readProjectConfiguration(tree, 'proj').targets.build
+      ).toBeUndefined();
+    });
+
+    it('should not be removed when there are more dependent tasks', () => {
+      addProjectConfiguration(tree, 'proj', {
+        root: 'proj',
+        targets: {
+          build: {
+            executor: 'nx:run-commands',
+            dependsOn: ['^build-base', 'prebuild'],
+            options: {
+              configFile: 'file.txt',
+            },
+          },
+        },
+      });
+
+      replaceProjectConfigurationsWithPlugin(
+        tree,
+        new Map([['proj', 'proj']]),
+        'plugin-path',
+        createNodes,
+        {}
+      );
+
+      expect(readProjectConfiguration(tree, 'proj').targets.build).toEqual({
+        dependsOn: ['^build-base', 'prebuild'],
+      });
+    });
+
+    it('should not be removed when there are less dependent tasks', () => {
+      addProjectConfiguration(tree, 'proj', {
+        root: 'proj',
+        targets: {
+          build: {
+            executor: 'nx:run-commands',
+            dependsOn: [],
+            options: {
+              configFile: 'file.txt',
+            },
+          },
+        },
+      });
+
+      replaceProjectConfigurationsWithPlugin(
+        tree,
+        new Map([['proj', 'proj']]),
+        'plugin-path',
+        createNodes,
+        {}
+      );
+
+      expect(readProjectConfiguration(tree, 'proj').targets.build).toEqual({
+        dependsOn: [],
+      });
+    });
+  });
+
+  describe('defaultConfiguration', () => {
+    it('should not be removed when the defaultConfiguration is different', () => {
+      addProjectConfiguration(tree, 'proj', {
+        root: 'proj',
+        targets: {
+          build: {
+            executor: 'nx:run-commands',
+            options: {
+              configFile: 'file.txt',
+            },
+            defaultConfiguration: 'other',
+          },
+        },
+      });
+
+      replaceProjectConfigurationsWithPlugin(
+        tree,
+        new Map([['proj', 'proj']]),
+        'plugin-path',
+        createNodes,
+        {}
+      );
+
+      expect(readProjectConfiguration(tree, 'proj').targets.build).toEqual({
+        defaultConfiguration: 'other',
+      });
+    });
+  });
+
+  describe('configurations', () => {
+    it('should not be removed when an additional configuration is defined', () => {
+      addProjectConfiguration(tree, 'proj', {
+        root: 'proj',
+        targets: {
+          build: {
+            executor: 'nx:run-commands',
+            options: {
+              configFile: 'file.txt',
+            },
+            configurations: {
+              other: {
+                configFile: 'other-file.txt',
+              },
+            },
+          },
+        },
+      });
+
+      replaceProjectConfigurationsWithPlugin(
+        tree,
+        new Map([['proj', 'proj']]),
+        'plugin-path',
+        createNodes,
+        {}
+      );
+
+      expect(readProjectConfiguration(tree, 'proj').targets.build).toEqual({
+        configurations: {
+          other: {
+            configFile: 'other-file.txt',
+          },
+        },
+      });
+    });
+  });
+});
