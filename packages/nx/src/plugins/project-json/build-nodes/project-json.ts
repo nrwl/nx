4347bd3f6b326852cca4aaae5b9e6e2789059c38
FrozenPandaz <jasonjean1993@gmail.com>
2023-11-03 15:23:00 -0400

    chore(core): split package.json scripts from project.json plugin

--- packages/nx/src/plugins/project-json/build-nodes/project-json.ts
+++ packages/nx/src/plugins/project-json/build-nodes/project-json.ts
@@ -1,17 +1,9 @@
 import { dirname, join } from 'node:path';
-import { existsSync } from 'node:fs';
 
-import {
-  ProjectConfiguration,
-  TargetConfiguration,
-} from '../../../config/workspace-json-project-json';
+import { ProjectConfiguration } from '../../../config/workspace-json-project-json';
 import { toProjectName } from '../../../config/workspaces';
 import { readJsonFile } from '../../../utils/fileutils';
 import { NxPluginV2 } from '../../../utils/nx-plugin';
-import {
-  PackageJson,
-  readTargetsFromPackageJson,
-} from '../../../utils/package-json';
 
 export const CreateProjectJsonProjectsPlugin: NxPluginV2 = {
   name: 'nx-core-build-project-json-nodes',
@@ -21,7 +13,6 @@ export const CreateProjectJsonProjectsPlugin: NxPluginV2 = {
       const root = context.workspaceRoot;
       const json = readJsonFile<ProjectConfiguration>(join(root, file));
       const project = buildProjectFromProjectJson(json, file);
-      mergePackageJsonConfigurationWithProjectJson(project, root);
       return {
         projects: {
           [project.name]: project,
@@ -40,47 +31,4 @@ export function buildProjectFromProjectJson(
     root: dirname(path),
     ...json,
   };
-}
-
-export function mergePackageJsonConfigurationWithProjectJson(
-  p: ProjectConfiguration,
-  root: string
-) {
-  if (existsSync(join(root, p.root, 'package.json'))) {
-    try {
-      const packageJson: PackageJson = readJsonFile(
-        join(root, p.root, 'package.json')
-      );
-
-      p.targets = mergeNpmScriptsWithTargets(packageJson, p.targets);
-
-      const { nx } = packageJson;
-
-      if (nx?.tags) {
-        p.tags = [...(p.tags || []), ...nx.tags];
-      }
-      if (nx?.implicitDependencies) {
-        p.implicitDependencies = [
-          ...(p.implicitDependencies || []),
-          ...nx.implicitDependencies,
-        ];
-      }
-      if (nx?.namedInputs) {
-        p.namedInputs = { ...(p.namedInputs || {}), ...nx.namedInputs };
-      }
-    } catch (e) {
-      // ignore json parser errors
-    }
-  }
-}
-
-export function mergeNpmScriptsWithTargets(
-  packageJson: PackageJson,
-  targets: Record<string, TargetConfiguration>
-): Record<string, TargetConfiguration> {
-  try {
-    return { ...readTargetsFromPackageJson(packageJson), ...(targets || {}) };
-  } catch (e) {
-    return targets;
-  }
 }
