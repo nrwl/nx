e52311094a50bd39cdaafc40ac92759f79fbf017
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-11-06 17:28:30 +0100

    fix(storybook): bump supported version to 7.5.3

--- packages/storybook/src/utils/versions.ts
+++ packages/storybook/src/utils/versions.ts
@@ -7,7 +7,7 @@ export const storybookJestVersion = '~0.1.0';
 export const litVersion = '^2.6.1';
 export const tsNodeVersion = '10.9.1';
 
-export const storybookVersion = '^7.5.1';
+export const storybookVersion = '^7.5.3';
 export const reactVersion = '^18.2.0';
 export const viteVersion = '~4.3.9';
 
