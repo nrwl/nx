ff29d51c953dc8103437747250445704384f5f36
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-11-07 09:57:29 +0100

    feat(angular): support angular 17.0.0-rc.4

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -1,7 +1,7 @@
 export const nxVersion = require('../../package.json').version;
 
-export const angularVersion = '~17.0.0-rc.2';
-export const angularDevkitVersion = '~17.0.0-rc.3';
+export const angularVersion = '~17.0.0-rc.3';
+export const angularDevkitVersion = '~17.0.0-rc.4';
 export const ngPackagrVersion = '~17.0.0-rc.1';
 export const ngrxVersion = '~16.0.0';
 export const rxjsVersion = '~7.8.0';
