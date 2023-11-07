26a8ba6044dd433a52bccdadbc8e77758394b91b
Jack Hsu <jack.hsu@gmail.com>
2023-11-07 09:28:32 -0500

    fix(react): component generator should work without sourceRoot

--- packages/react/src/generators/component/component.spec.ts
+++ packages/react/src/generators/component/component.spec.ts
@@ -1,5 +1,11 @@
 import { installedCypressVersion } from '@nx/cypress/src/utils/cypress-version';
-import { logger, readJson, Tree } from '@nx/devkit';
+import {
+  logger,
+  readJson,
+  readProjectConfiguration,
+  Tree,
+  updateProjectConfiguration,
+} from '@nx/devkit';
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
 import { createApp, createLib } from '../../utils/testing-generators';
 import { componentGenerator } from './component';
@@ -133,6 +139,22 @@ describe('component', () => {
         name: 'hello',
         style: 'css',
         project: 'my-app',
+        export: true,
+      });
+
+      const indexContent = appTree.read('my-lib/src/index.ts', 'utf-8');
+
+      expect(indexContent).not.toMatch(/lib\/hello/);
+    });
+
+    it('should work for projects without sourceRoot', async () => {
+      const projectConfig = readProjectConfiguration(appTree, 'my-lib');
+      delete projectConfig.sourceRoot;
+      updateProjectConfiguration(appTree, 'my-lib', projectConfig);
+
+      await componentGenerator(appTree, {
+        name: 'my-lib/src/lib/hello',
+        style: 'css',
         export: true,
       });
 
