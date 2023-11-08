395eb70336cb1346548705d7c41d896b4a6c6cbb
Jack Hsu <jack.hsu@gmail.com>
2023-11-08 11:03:34 -0500

    feat(webpack): add NxWebpackPlugin that works with normal Webpack configuration (#19984)

--- packages/webpack/src/plugins/nx-webpack-plugin/nx-webpack-plugin-options.ts
+++ packages/webpack/src/plugins/nx-webpack-plugin/nx-webpack-plugin-options.ts
@@ -1,3 +1,4 @@
+import { ProjectGraph } from '@nx/devkit';
 import { AssetGlob } from '@nx/js/src/utils/assets/assets';
 
 export interface AssetGlobPattern {
@@ -36,58 +37,64 @@ export interface OptimizationOptions {
   styles: boolean;
 }
 
-export interface WebpackExecutorOptions {
+export interface NxWebpackPluginOptions {
+  // Required options
+  main: string;
+  outputPath: string;
+  tsConfig: string;
+
+  // Optional options
   additionalEntryPoints?: AdditionalEntryPoint[];
   assets?: Array<AssetGlob | string>;
-  buildLibsFromSource?: boolean;
+  babelConfig?: string;
+  babelUpwardRootMode?: boolean;
+  baseHref?: string;
   commonChunk?: boolean;
   compiler?: 'babel' | 'swc' | 'tsc';
+  crossOrigin?: 'none' | 'anonymous' | 'use-credentials';
   deleteOutputPath?: boolean;
+  deployUrl?: string;
   externalDependencies?: 'all' | 'none' | string[];
+  extractCss?: boolean;
   extractLicenses?: boolean;
   fileReplacements?: FileReplacement[];
+  generateIndexHtml?: boolean;
   generatePackageJson?: boolean;
-  isolatedConfig?: boolean;
-  main: string;
+  index?: string;
   memoryLimit?: number;
   namedChunks?: boolean;
   optimization?: boolean | OptimizationOptions;
   outputFileName?: string;
   outputHashing?: any;
-  outputPath: string;
   poll?: number;
   polyfills?: string;
+  postcssConfig?: string;
   progress?: boolean;
   runtimeChunk?: boolean;
-  sourceMap?: boolean | 'hidden';
-  statsJson?: boolean;
-  target?: 'node' | 'web' | 'webworker';
-  transformers?: TransformerEntry[];
-  tsConfig: string;
-  vendorChunk?: boolean;
-  verbose?: boolean;
-  watch?: boolean;
-  webpackConfig?: string;
-  babelConfig?: string;
-  babelUpwardRootMode?: boolean;
-  baseHref?: string;
-  crossOrigin?: 'none' | 'anonymous' | 'use-credentials';
-  deployUrl?: string;
-  extractCss?: boolean;
-  generateIndexHtml?: boolean;
-  index?: string;
-  postcssConfig?: string;
   scripts?: Array<ExtraEntryPointClass | string>;
+  skipTypeChecking?: boolean;
+  sourceMap?: boolean | 'hidden';
+  ssr?: boolean;
+  statsJson?: boolean;
   stylePreprocessorOptions?: any;
   styles?: Array<ExtraEntryPointClass | string>;
   subresourceIntegrity?: boolean;
+  target?: string | string[];
+  transformers?: TransformerEntry[];
+  vendorChunk?: boolean;
+  verbose?: boolean;
+  watch?: boolean;
 }
 
-export interface NormalizedWebpackExecutorOptions
-  extends WebpackExecutorOptions {
-  outputFileName: string;
-  assets: AssetGlobPattern[];
+export interface NormalizedNxWebpackPluginOptions
+  extends NxWebpackPluginOptions {
+  projectName: string;
   root: string;
   projectRoot: string;
   sourceRoot: string;
+  configurationName: string;
+  targetName: string;
+  projectGraph: ProjectGraph;
+  outputFileName: string;
+  assets: AssetGlobPattern[];
 }
