fb0f0cc2dfa44cfd4c9a04f22a3d0dac9c74c2bb
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2023-11-08 11:49:55 +0100

    cleanup(angular): remove unneeded stubbing for performance.mark in jest setup file

--- packages/angular/src/generators/application/lib/add-unit-test-runner.ts
+++ packages/angular/src/generators/application/lib/add-unit-test-runner.ts
@@ -1,7 +1,6 @@
 import { Tree, joinPathFragments } from '@nx/devkit';
 import { configurationGenerator } from '@nx/jest';
 import { UnitTestRunner } from '../../../utils/test-runners';
-import { getInstalledAngularVersionInfo } from '../../utils/version-utils';
 import type { NormalizedSchema } from './normalized-schema';
 
 export async function addUnitTestRunner(host: Tree, options: NormalizedSchema) {
@@ -19,7 +18,6 @@ export async function addUnitTestRunner(host: Tree, options: NormalizedSchema) {
       'src',
       'test-setup.ts'
     );
-    const { major: angularMajorVersion } = getInstalledAngularVersionInfo(host);
     if (options.strict && host.exists(setupFile)) {
       const contents = host.read(setupFile, 'utf-8');
       host.write(
@@ -31,17 +29,7 @@ globalThis.ngJest = {
     errorOnUnknownProperties: true,
   },
 };
-${contents}${
-          angularMajorVersion >= 17
-            ? `
-/**
- * Angular uses performance.mark() which is not supported by jsdom. Stub it out
- * to avoid errors.
- */
-global.performance.mark = jest.fn();
-`
-            : ''
-        }`
+${contents}`
       );
     }
   }
