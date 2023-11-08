b537686b0fc2e664bf1f2d2df50abe9fb637cbac
Remco <info@remcokrams.nl>
2023-11-08 21:47:13 +0100

    fix(module-federation): re-add removal of js files

--- packages/react/src/generators/remote/remote.ts
+++ packages/react/src/generators/remote/remote.ts
@@ -50,6 +50,23 @@ export function addModuleFederationFiles(
     options.appProjectRoot,
     templateVariables
   );
+
+  if (options.typescriptConfiguration) {
+    const pathToWebpackConfig = joinPathFragments(
+      options.appProjectRoot,
+      'webpack.config.js'
+    );
+    const pathToWebpackProdConfig = joinPathFragments(
+      options.appProjectRoot,
+      'webpack.config.prod.js'
+    );
+    if (host.exists(pathToWebpackConfig)) {
+      host.delete(pathToWebpackConfig);
+    }
+    if (host.exists(pathToWebpackProdConfig)) {
+      host.delete(pathToWebpackProdConfig);
+    }
+  }
 }
 
 export async function remoteGenerator(host: Tree, schema: Schema) {
