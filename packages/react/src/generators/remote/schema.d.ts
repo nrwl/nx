cd074e0b37d7fc8b6e467b8d88bbe69200bbd1ab
Miloš Lajtman <mlajtman@slido.com>
2023-11-14 16:37:17 +0100

    fix(react): SupportedStyles import paths for generator schema

--- packages/react/src/generators/remote/schema.d.ts
+++ packages/react/src/generators/remote/schema.d.ts
@@ -1,6 +1,6 @@
 import type { ProjectNameAndRootFormat } from '@nx/devkit/src/generators/project-name-and-root-utils';
 import type { Linter } from '@nx/eslint';
-import type { SupportedStyles } from '../../../typings';
+import type { SupportedStyles } from '../../../typings/style';
 import type { NormalizedSchema as ApplicationNormalizedSchema } from '../application/schema';
 
 export interface Schema {
