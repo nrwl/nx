448e9151738344c6ca49512cb8ede1166d045241
Juri <juri.strumpflohner@gmail.com>
2023-11-28 10:14:02 +0100

    docs(nx-dev): adds banner for state of js

--- nx-dev/ui-common/src/lib/announcement-banner.tsx
+++ nx-dev/ui-common/src/lib/announcement-banner.tsx
@@ -8,24 +8,26 @@ export function AnnouncementBanner(): JSX.Element {
           <p className="text-sm font-medium">
             <span className="md:hidden">
               <Link
-                href="https://www.producthunt.com/posts/nx-cloud#nx-cloud"
+                href="https://survey.devographics.com/en-US/survey/state-of-js/2023"
                 className="underline"
               >
-                Missed Nx Conf? Watch the recording!
+                The State of JS survey is live, featuring a monorepo tools
+                section
               </Link>
             </span>
             <span className="hidden md:inline">
               <span className="font-semibold">
-                Missed Nx Conf? Watch the recording!
+                The State of JS survey is live, featuring a monorepo tools
+                section
               </span>
             </span>
             <span className="ml-2 inline-block">
               <Link
-                href="/conf"
+                href="https://survey.devographics.com/en-US/survey/state-of-js/2023"
                 className="font-semibold text-blue-500 underline dark:text-sky-500"
               >
                 <span className="absolute inset-0" aria-hidden="true" />
-                Details here
+                Vote for us
                 <span aria-hidden="true">&rarr;</span>
               </Link>
             </span>
