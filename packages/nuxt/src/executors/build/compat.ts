468de36ad1045a9b44181449c327bacce03320a7
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2023-11-30 20:35:02 +0200

    feat(nuxt): nodes for build, serve, test targets (#20145)

--- packages/nuxt/src/executors/build/compat.ts
+++
@@ -1,4 +0,0 @@
-import { convertNxExecutor } from '@nx/devkit';
-import nuxtBuildExecutor from './build.impl';
-
-export default convertNxExecutor(nuxtBuildExecutor);
--- packages/nuxt/src/executors/serve/compat.ts
+++
@@ -1,4 +0,0 @@
-import { convertNxExecutor } from '@nx/devkit';
-import nuxtServeExecutor from './serve.impl';
-
-export default convertNxExecutor(nuxtServeExecutor);
