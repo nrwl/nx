83db767b27cdf08f6b99788e461261aafba5a7c1
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2023-11-30 22:56:16 +0200

    feat(vite): nodes for build, serve, test, preview targets (#20086)

--- packages/vite/migrations.spec.ts
+++ packages/vite/migrations.spec.ts
@@ -3,6 +3,16 @@ import json = require('./migrations.json');
 import { assertValidMigrationPaths } from '@nx/devkit/internal-testing-utils';
 import { MigrationsJson } from '@nx/devkit';
 
+jest.mock('vite', () => ({
+  loadConfigFromFile: jest.fn().mockImplementation(() => {
+    return Promise.resolve({
+      path: 'vite.config.ts',
+      config: {},
+      dependencies: [],
+    });
+  }),
+}));
+
 describe('vite migrations', () => {
   assertValidMigrationPaths(json as MigrationsJson, __dirname);
 });
