96a7f4db4c63fcb7d0ca87023e95952b7b6dab9a
Benjamin Cabanes <3447705+bcabanes@users.noreply.github.com>
2023-12-07 13:03:50 -0500

    feat(nx-dev): add animation

--- nx-dev/ui-home/src/lib/nx-with-ci.tsx
+++ nx-dev/ui-home/src/lib/nx-with-ci.tsx
@@ -3,7 +3,7 @@ import {
   CogIcon,
   ServerStackIcon,
 } from '@heroicons/react/24/outline';
-import { SectionHeading } from '@nx/nx-dev/ui-common';
+import { ButtonLink, SectionHeading } from '@nx/nx-dev/ui-common';
 import {
   animate,
   motion,
@@ -72,7 +72,7 @@ export function NxWithCi(): JSX.Element {
         <div>
           <header>
             <SectionHeading as="h1" variant="title" id="nx-is-fast">
-              Speed up your CI from 90 mins to 10 mins with 1 flag
+              From 90 to 10 minutes
             </SectionHeading>
             <SectionHeading
               as="p"
@@ -92,134 +92,115 @@ export function NxWithCi(): JSX.Element {
             </p>
           </div>
         </div>
-        <div className="flex flex-row content-between gap-6 lg:gap-28">
-          <div className="flex flex-col justify-center">
-            <div className="text-lg text-slate-400">Most workspaces see</div>
-            <div
-              aria-hidden="true"
-              className="my-2 text-4xl font-semibold text-slate-800 drop-shadow-sm dark:text-slate-100 dark:drop-shadow-[0_1px_1px_rgba(255,255,255,0.35)] xl:text-6xl"
-            >
-              <Counter from={0} to={3} round={0} progress={progress} />{' '}
-              <span className="-ml-2 text-lg xl:-ml-3 xl:text-3xl">x</span>
-              <span className="mx-4 text-2xl xl:text-5xl">to</span>
-              <Counter from={0} to={14} round={0} progress={progress} />{' '}
-              <span className="-ml-2 text-lg xl:-ml-3 xl:text-3xl">x</span>
+        <div className="flex items-end">
+          <div className="w-full">
+            <div className="flex">
+              <div className="shrink-0 w-28 border-r border-slate-200 dark:border-slate-500 py-3 text-slate-700 dark:text-slate-400">
+                Average CI
+              </div>
+              <div className="flex-grow py-1">
+                <motion.div
+                  initial={{ display: 'none', width: 0 }}
+                  variants={{ visible: { display: 'flex', width: '90%' } }}
+                  transition={{ type: 'tween', duration: 1 }}
+                  className="flex-grow items-center justify-end text-slate-600 dark:text-slate-400 bg-slate-200 dark:bg-slate-700 py-2 px-4 rounded-r-lg"
+                >
+                  <Counter from={0} to={90} round={0} progress={progress} />
+                  <span className="ml-1">minutes</span>
+                </motion.div>
+              </div>
             </div>
-            <div className="text-sm text-slate-500">
-              <span className="sr-only">3 times </span>reduction in CI time
+            <div className="flex">
+              <div className="shrink-0 font-medium w-28 border-r border-slate-200 dark:border-slate-500 py-3 text-slate-700 dark:text-slate-400">
+                CI with Nx
+              </div>
+              <div className="flex-grow flex gap-4 py-1 font-medium">
+                <motion.div
+                  initial={{ display: 'none', width: 0 }}
+                  variants={{ visible: { display: 'flex', width: '10%' } }}
+                  transition={{ type: 'tween', duration: 1 }}
+                  className="bg-blue-500 dark:bg-sky-500 rounded-r-lg"
+                />
+                <div className="flex items-center text-slate-700 dark:text-slate-400">
+                  <Counter from={0} to={10} round={0} progress={progress} />
+                  <span className="ml-1">minutes</span>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="flex flex-col justify-center">
-            <div className="text-lg text-slate-400">Average</div>
-            <div
-              aria-hidden="true"
-              className="my-2 text-4xl font-bold text-slate-800 drop-shadow-sm dark:text-slate-100 dark:drop-shadow-[0_1px_1px_rgba(255,255,255,0.35)] xl:text-6xl"
-            >
-              <Counter from={0} to={2.5} round={1} progress={progress} />{' '}
-              <span className="-ml-2 text-lg xl:text-3xl">x</span>
-            </div>
-            <div className="text-sm text-slate-500">
-              <span className="sr-only">2.5 times </span>reduction in
-              computation time
+            <div className="flex w-full justify-end">
+              <a
+                href="https://www.youtube.com/watch?v=KPCMg_Dn0Eo"
+                target="_blank"
+                className="hover:underline text-sm font-medium"
+                title="Find out how to reduce CI time with Nx"
+              >
+                Find out how
+              </a>
             </div>
           </div>
         </div>
       </motion.div>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-8 lg:pb-16">
-        <dl className="grid grid-cols-1 gap-16 sm:grid-cols-3 items-stretch">
-          <div className="relative group">
-            <div className="absolute -inset-1 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
-            <div className="relative flex flex-col-reverse rounded-lg border border-slate-200 bg-white p-4 text-sm shadow-sm  transition focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 dark:border-slate-800/40 dark:bg-slate-800">
-              <dd className="mt-4 text-slate-400 dark:text-slate-500 sr-only">
-                <p>DESCRIPTION HERE - CHANGE TEXT</p>
-              </dd>
-              <dt className="py-8">
-                <div className="flex items-center text-slate-800 dark:text-slate-200 gap-4">
-                  <CloudArrowDownIcon
-                    className="h-8 w-8 flex-none "
-                    aria-hidden="true"
-                  />
-                  <div className="font-semibold text-lg">
-                    <a
-                      target="_blank"
-                      rel="noreferrer"
-                      href="/ci/features/nx-cache"
-                    >
-                      <span className="absolute inset-0"></span>Nx Cache
-                    </a>
-                  </div>
+        <dl className="grid grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-3">
+          <div className="relative">
+            <dt className="text-base font-semibold leading-7 text-slate-900 dark:text-slate-100">
+              <div className="relative group">
+                <div className="absolute -inset-1 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg blur-sm opacity-25 group-hover:opacity-90 transition duration-1000 group-hover:duration-200"></div>
+                <div className="relative flex gap-4 items-center rounded-lg border border-slate-200 bg-white p-4 text-lg shadow-sm transition focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 dark:border-slate-800/40 dark:bg-slate-800">
+                  <CloudArrowDownIcon className="h-8 w-8" aria-hidden="true" />
+                  <a href="/ci/features/remote-cache" title="Discover Nx Cache">
+                    <span className="absolute inset-0"></span>Nx Cache
+                  </a>
                 </div>
-                <div className="mt-2 text-base text-slate-600 dark:text-slate-400">
-                  Built-in local and remote caching to speed up your tasks and
-                  save you time and money.
-                </div>
-              </dt>
-            </div>
+              </div>
+            </dt>
+            <dd className="mt-4 text-base leading-7 text-slate-700 dark:text-slate-400">
+              Built-in local and remote caching to speed up your tasks and save
+              you time and money.
+            </dd>
           </div>
-          <div className="relative group">
-            <div className="absolute -inset-1 bg-gradient-to-r from-orange-500 to-rose-500 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
-            <div className="relative flex flex-col-reverse rounded-lg border border-slate-200 bg-white p-4 text-sm shadow-sm  transition focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 dark:border-slate-800/40 dark:bg-slate-800">
-              <dd className="mt-4 text-slate-400 dark:text-slate-500 sr-only">
-                <p>DESCRIPTION HERE - CHANGE TEXT</p>
-              </dd>
-              <dt className="flex items-center space-x-4 py-8">
-                <ServerStackIcon
-                  className="h-8 w-8 flex-none text-slate-800 dark:text-slate-200"
-                  aria-hidden="true"
-                />
-                <div className="flex-auto">
-                  <div className="font-semibold text-slate-800 dark:text-slate-200">
-                    <a href="/ci/features/nx-agents" title="Discover Nx Agents">
-                      <span className="absolute inset-0"></span>Nx Agents{' '}
-                      <span className="absolute top-2 right-2 dark:bg-slate-400/10 dark:text-slate-400 dark:ring-slate-400/20 inline-flex items-center rounded-md bg-slate-50 px-2 py-1 text-xs font-medium text-slate-600 ring-1 ring-inset ring-slate-500/10">
-                        Early access
-                      </span>
-                    </a>
-                  </div>
-                  <div className="mt-0.5 text-xs text-slate-600 dark:text-slate-400">
-                    A single flag to enable distributed computation, across
-                    multiple machines. Fully managed agents, dynamically
-                    allocated based on PR size.
-                  </div>
-                  {/* <div className="mt-0.5 text-xs text-slate-600 dark:text-slate-400">
-                    A single flag to enable powerful task distribution, on any CI provider.
-
-
-                    Streamline CI with dynamic task distribution across
-                    machines, managed by a single flag. Works seamlessly across
-                    different CI providers.
-                  </div> */}
+          <div className="relative">
+            <dt className="text-base font-semibold leading-7 text-slate-900 dark:text-slate-100">
+              <div className="relative group">
+                <div className="absolute -inset-1 bg-gradient-to-r from-orange-500 to-rose-500 rounded-lg blur-sm opacity-25 group-hover:opacity-90 transition duration-1000 group-hover:duration-200"></div>
+                <div className="relative flex gap-4 items-center rounded-lg border border-slate-200 bg-white p-4 text-lg shadow-sm transition focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 dark:border-slate-800/40 dark:bg-slate-800">
+                  <ServerStackIcon className="h-8 w-8" aria-hidden="true" />
+                  <a href="/ci/features/nx-agents" title="Discover Nx Agents">
+                    <span className="absolute inset-0"></span>Nx Agents
+                  </a>
+                  <div className="flex-grow" />
+                  <span className="dark:bg-slate-400/10 dark:text-slate-400 dark:ring-slate-400/20 inline-flex items-center rounded-md bg-slate-50 px-2 py-1 text-xs font-medium text-slate-600 ring-1 ring-inset ring-slate-500/10">
+                    Early access
+                  </span>
                 </div>
-              </dt>
-            </div>
+              </div>
+            </dt>
+            <dd className="mt-4 text-base leading-7 text-slate-700 dark:text-slate-400">
+              A single line to enable distributed computation, across multiple
+              machines. Fully managed agents, dynamically allocated based on PR
+              size.
+            </dd>
           </div>
-          <div className="relative group opacity-50">
-            <div className="absolute -inset-1 bg-gradient-to-r from-pink-500 to-fuchsia-500 rounded-lg blur opacity-25 transition duration-1000 group-hover:duration-200"></div>
-            <div className="relative flex flex-col-reverse rounded-lg border border-slate-200 bg-white p-4 text-sm shadow-sm  transition focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 dark:border-slate-800/40 dark:bg-slate-800">
-              <dd className="mt-4 text-slate-400 dark:text-slate-500 sr-only">
-                <p>DESCRIPTION HERE - CHANGE TEXT</p>
-              </dd>
-              <dt className="flex items-center space-x-4 py-8">
-                <CogIcon
-                  className="h-8 w-8 flex-none text-slate-800 dark:text-slate-200"
-                  aria-hidden="true"
-                />
-                <div className="flex-auto">
-                  <div className="font-semibold text-slate-800 dark:text-slate-200">
-                    {/*<a target="_blank" rel="noreferrer" href="#">*/}
-                    <span className="absolute inset-0"></span>Nx Workflows
-                    <span className="absolute top-2 right-2 dark:bg-slate-400/10 dark:text-slate-400 dark:ring-slate-400/20 inline-flex items-center rounded-md bg-slate-50 px-2 py-1 text-xs font-medium text-slate-600 ring-1 ring-inset ring-slate-500/10">
-                      Coming soon
-                    </span>
-                    {/*</a>*/}
-                  </div>
-                  <div className="mt-0.5 text-xs text-slate-600 dark:text-slate-400">
-                    Next generation, fully managed CI solution with distribution
-                    at its core, designed from the ground up for monorepos.
-                  </div>
+          <div className="relative">
+            <dt className="text-base font-semibold leading-7 text-slate-900 dark:text-slate-100">
+              <div className="relative group opacity-70">
+                <div className="absolute -inset-1 bg-gradient-to-r from-pink-500 to-fuchsia-500 rounded-lg blur-sm opacity-25"></div>
+                <div className="relative flex gap-4 items-center rounded-lg border border-slate-200 bg-white p-4 text-lg shadow-sm transition focus-within:ring-2 focus-within:ring-blue-500 focus-within:ring-offset-2 dark:border-slate-800/40 dark:bg-slate-800">
+                  <CogIcon className="h-8 w-8" aria-hidden="true" />
+                  {/*<a href="/ci/features/nx-workflows" title="Discover Nx Workflows">*/}
+                  <span className="absolute inset-0"></span>Nx Workflows
+                  {/*</a>*/}
+                  <div className="flex-grow" />
+                  <span className="dark:bg-slate-400/10 dark:text-slate-400 dark:ring-slate-400/20 inline-flex items-center rounded-md bg-slate-50 px-2 py-1 text-xs font-medium text-slate-600 ring-1 ring-inset ring-slate-500/10">
+                    Coming soon
+                  </span>
                 </div>
-              </dt>
-            </div>
+              </div>
+            </dt>
+            <dd className="mt-4 text-base leading-7 text-slate-700 dark:text-slate-400">
+              Next generation, fully managed CI solution with distribution at
+              its core, designed from the ground up for monorepos.
+            </dd>
           </div>
         </dl>
       </div>
