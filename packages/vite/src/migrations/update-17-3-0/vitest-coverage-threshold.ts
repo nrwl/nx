e44f17b18ab94be903f0e3a65c5afe54c615e4f4
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-12-14 16:31:34 +0200

    fix(vite): migration for thresholds

---
+++ packages/vite/src/migrations/update-17-3-0/vitest-coverage-threshold.ts
@@ -0,0 +1,45 @@
+import {
+  ProjectConfiguration,
+  Tree,
+  formatFiles,
+  getProjects,
+  joinPathFragments,
+} from '@nx/devkit';
+import { forEachExecutorOptions } from '@nx/devkit/src/generators/executor-options-utils';
+import { fixCoverage } from './lib/fix-coverage';
+
+export default async function fixCoverageThreshold(tree: Tree) {
+  const projects = getProjects(tree);
+  forEachExecutorOptions(tree, '@nx/vite:test', (_options, projectName) => {
+    const projectConfig: ProjectConfiguration = projects.get(
+      projectName
+    ) as ProjectConfiguration;
+    const configPath = findViteConfig(tree, projectConfig.root);
+    if (!configPath || !tree.exists(configPath)) {
+      return;
+    }
+    const configContents = tree.read(configPath, 'utf-8') as string;
+
+    const updatedConfigContents = fixCoverage(configContents);
+
+    if (updatedConfigContents) {
+      tree.write(configPath, updatedConfigContents);
+    }
+  });
+
+  await formatFiles(tree);
+}
+
+function findViteConfig(tree: Tree, searchRoot: string) {
+  const allowsExt = ['js', 'mjs', 'ts', 'cjs', 'mts', 'cts'];
+
+  for (const ext of allowsExt) {
+    if (tree.exists(joinPathFragments(searchRoot, `vite.config.${ext}`))) {
+      return joinPathFragments(searchRoot, `vite.config.${ext}`);
+    } else if (
+      tree.exists(joinPathFragments(searchRoot, `vitest.config.${ext}`))
+    ) {
+      return joinPathFragments(searchRoot, `vitest.config.${ext}`);
+    }
+  }
+}
