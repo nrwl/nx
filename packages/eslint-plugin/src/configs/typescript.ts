64e20fb7c49879d6c025c97e8a6bc29a63d23081
Steven Rathbauer <gh.rathpc@gmail.com>
2024-01-09 15:05:03 -0500

    fix(linter): update eslint plugins for @typescript-eslint v6 naming
    This updates the plugins used to map correctly to the new
    configuration naming that @typescript-eslint v6 introduces.
    
    Resolves #20966

--- packages/eslint-plugin/src/configs/typescript.ts
+++ packages/eslint-plugin/src/configs/typescript.ts
@@ -10,6 +10,9 @@ const isPrettierAvailable =
  *
  * It should therefore NOT contain any rules or plugins which are specific
  * to one ecosystem, such as React, Angular, Node etc.
+ *
+ * TODO: Reevaluate stylistic plugin and added specific rules in the next major
+ * version of Nx. Related to https://github.com/nrwl/nx/pull/21066
  */
 export default {
   parser: '@typescript-eslint/parser',
@@ -23,12 +26,32 @@ export default {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    /**
+     * This was added to preserve the linting rules that were previously
+     * defined v5 of @typescript-eslint. v6 of @typescript-eslint changed
+     * how configurations are defined. This should be reevaluated in the
+     * next major version of Nx.
+     */
+    'plugin:@typescript-eslint/stylistic',
     ...(isPrettierAvailable ? ['prettier'] : []),
   ],
   rules: {
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    /**
+     * These 3 rules:
+     *   `no-extra-semi`
+     *   `@typescript-eslint/no-extra-semi`
+     *   `@typescript-eslint/no-non-null-assertion`
+     * were added to preserve the linting rules that were
+     * previously defined v5 of `@typescript-eslint`. v6 of `@typescript-eslint`
+     * changed how configurations are defined. This should be reevaluated in
+     * the next major version of Nx.
+     */
+    'no-extra-semi': 'off',
+    '@typescript-eslint/no-extra-semi': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
     '@typescript-eslint/no-parameter-properties': 'off',
   },
 };
