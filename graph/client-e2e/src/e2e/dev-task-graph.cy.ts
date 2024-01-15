d794c710eb4ff5a60ba362287cb3e3f33a5c6394
Colum Ferry <cferry09@gmail.com>
2024-01-15 11:24:11 +0000

    fix(graph): e2es require sourceMapUrl and remove deprecated tests (#21138)

--- graph/client-e2e/src/e2e/dev-task-graph.cy.ts
+++ graph/client-e2e/src/e2e/dev-task-graph.cy.ts
@@ -198,6 +198,7 @@ describe('dev mode - task graph', () => {
           // Load the fixture data and find the property based on the query parameter
 
           const expandedInputs = nxExamplesTaskInputs[taskId];
+          console.log(expandedInputs);
 
           // Reply with the selected property
           req.reply({
