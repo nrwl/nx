28438bb8100b12906fbe8567adea16edc4258233
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-01-17 21:22:16 +0100

    feat(core): add the "add" cli command (#20976)

--- packages/nx/src/migrations/update-17-3-0/update-nxw.ts
+++ packages/nx/src/migrations/update-17-3-0/update-nxw.ts
@@ -1,4 +1,4 @@
-import { Tree } from '../../generators/tree';
+import type { Tree } from '../../generators/tree';
 import { updateNxw } from '../../utils/update-nxw';
 
 export default async function (tree: Tree) {
