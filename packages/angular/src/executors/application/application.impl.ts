b2ebf7b452cec3b0cb15524fc1ca6077abf09efb
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-01-17 12:55:08 +0100

    feat(angular): add option to application executor to transform the index.html file (#21026)

--- packages/angular/src/executors/application/application.impl.ts
+++ packages/angular/src/executors/application/application.impl.ts
@@ -1,8 +1,13 @@
 import type { ExecutorContext } from '@nx/devkit';
 import type { DependentBuildableProjectNode } from '@nx/js/src/utils/buildable-libs-utils';
 import { createBuilderContext } from 'nx/src/adapter/ngcli-adapter';
+import { gte } from 'semver';
+import { getInstalledAngularVersionInfo } from '../utilities/angular-version-utils';
 import { createTmpTsConfigForBuildableLibs } from '../utilities/buildable-libs';
-import { loadPlugins } from '../utilities/esbuild-extensions';
+import {
+  loadIndexHtmlTransformer,
+  loadPlugins,
+} from '../utilities/esbuild-extensions';
 import type { ApplicationExecutorOptions } from './schema';
 import { validateOptions } from './utils/validate-options';
 
@@ -31,6 +36,12 @@ export default async function* applicationExecutor(
   }
 
   const plugins = await loadPlugins(pluginPaths, options.tsConfig);
+  const indexHtmlTransformer = options.indexHtmlTransformer
+    ? await loadIndexHtmlTransformer(
+        options.indexHtmlTransformer,
+        options.tsConfig
+      )
+    : undefined;
 
   const { buildApplication } = await import('@angular-devkit/build-angular');
   const builderContext = await createBuilderContext(
@@ -41,6 +52,14 @@ export default async function* applicationExecutor(
     },
     context
   );
+
+  const { version: angularVersion } = getInstalledAngularVersionInfo();
+  if (gte(angularVersion, '17.1.0')) {
+    return yield* buildApplication(delegateExecutorOptions, builderContext, {
+      codePlugins: plugins,
+      indexHtmlTransformer,
+    });
+  }
 
   return yield* buildApplication(
     delegateExecutorOptions,
--- packages/angular/src/executors/application/schema.d.ts
+++ packages/angular/src/executors/application/schema.d.ts
@@ -3,5 +3,6 @@ import type { PluginSpec } from '../utilities/esbuild-extensions';
 
 export interface ApplicationExecutorOptions extends ApplicationBuilderOptions {
   buildLibsFromSource?: boolean;
+  indexHtmlTransformer?: string;
   plugins?: string[] | PluginSpec[];
 }
