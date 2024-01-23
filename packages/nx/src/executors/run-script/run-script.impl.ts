28e5fd77e113b487054386c0dcf4c8163dad5cb3
Emily Xiong <xiongemi@gmail.com>
2024-01-23 18:07:29 -0500

    feat(core): use runCommand for runScript

--- packages/nx/src/executors/run-script/run-script.impl.ts
+++ packages/nx/src/executors/run-script/run-script.impl.ts
@@ -1,8 +1,8 @@
-import { execSync } from 'child_process';
 import { getPackageManagerCommand } from '../../utils/package-manager';
 import type { ExecutorContext } from '../../config/misc-interfaces';
 import * as path from 'path';
 import { env as appendLocalEnv } from 'npm-run-path';
+import { runCommand } from '../../native';
 
 export interface RunScriptOptions {
   script: string;
@@ -15,16 +15,25 @@ export default async function (
 ) {
   const pm = getPackageManagerCommand();
   try {
-    execSync(pm.run(options.script, options.__unparsed__.join(' ')), {
-      stdio: ['inherit', 'inherit', 'inherit'],
-      cwd: path.join(
-        context.root,
-        context.projectsConfigurations.projects[context.projectName].root
-      ),
-      env: {
-        ...process.env,
-        ...appendLocalEnv(),
-      },
+    await new Promise<void>((res, rej) => {
+      const cp = runCommand(
+        pm.run(options.script, options.__unparsed__.join(' ')),
+        path.join(
+          context.root,
+          context.projectsConfigurations.projects[context.projectName].root
+        ),
+        {
+          ...process.env,
+          ...appendLocalEnv(),
+        }
+      );
+      cp.onExit((code) => {
+        if (code === 0) {
+          res();
+        } else {
+          rej();
+        }
+      });
     });
     return { success: true };
   } catch (e) {
