5cabf1c7e8f3af7931cbe2446ddf3f0549c4afdf
Matt Lewis <mattlewis92@users.noreply.github.com>
2024-01-30 10:38:29 +0000

    Update packages/nx/src/plugins/js/utils/register.ts
    Co-authored-by: Miroslav Jonaš <missing.manual@gmail.com>

--- packages/nx/src/plugins/js/utils/register.ts
+++ packages/nx/src/plugins/js/utils/register.ts
@@ -99,14 +99,8 @@ export function getTranspiler(compilerOptions: CompilerOptions) {
   }
 
   compilerOptions.lib = ['es2021'];
-  // Don't override module if it's set to node16 or nodenext
-  // Otherwise custom workspace eslint rules will fail with typescript 5.2
-  const preserveModuleOption =
-    compilerOptions.module === ts.ModuleKind.Node16 ||
-    compilerOptions.module === ts.ModuleKind.NodeNext;
-  compilerOptions.module = preserveModuleOption
-    ? compilerOptions.module
-    : ts.ModuleKind.CommonJS;
+  compilerOptions.module = compilerOptions.module ||
+    ts.ModuleKind.CommonJS;
   compilerOptions.target = ts.ScriptTarget.ES2021;
   compilerOptions.inlineSourceMap = true;
   compilerOptions.skipLibCheck = true;
