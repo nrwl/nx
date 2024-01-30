df47c9d155a172c332fc0578229ef51840778959
Max Kless <maxk@nrwl.io>
2024-01-30 15:21:33 -0500

    fix(graph): don't show open config button on nx.dev & refactor tooltips

--- graph/ui-tooltips/src/lib/task-node-tooltip.tsx
+++ graph/ui-tooltips/src/lib/task-node-tooltip.tsx
@@ -12,9 +12,8 @@ export interface TaskNodeTooltipProps {
   runTaskCallback?: () => void;
   description?: string;
   inputs?: Record<string, string[]>;
-  isNxConsole?: boolean;
   openConfigCallback?: () => void;
-
+  renderMode?: 'nx-console' | 'nx-docs';
   children?: ReactNode | ReactNode[];
 }
 
@@ -22,8 +21,8 @@ export function TaskNodeTooltip({
   id,
   executor,
   description,
-  runTaskCallback: runTargetCallback,
-  isNxConsole,
+  renderMode,
+  runTaskCallback,
   openConfigCallback,
   children,
 }: TaskNodeTooltipProps) {
@@ -33,33 +32,35 @@ export function TaskNodeTooltip({
         <div className="flex grow items-center justify-between">
           <div className="flex items-center">
             <Tag className="mr-3">{executor}</Tag>
-            <span className="font-mono">{id}</span>
+            <span className="font-mono mr-3">{id}</span>
           </div>
-          <button
-            className=" flex items-center rounded-md border-slate-300 bg-white p-1 font-medium text-slate-500 shadow-sm ring-1 transition hover:bg-slate-50 dark:border-slate-600 dark:bg-slate-800 dark:text-slate-400 dark:ring-slate-600 hover:dark:bg-slate-700"
-            title={
-              isNxConsole
-                ? 'Open project details in editor'
-                : 'Open project details'
-            }
-            onClick={openConfigCallback}
-          >
-            {isNxConsole ? (
-              <PencilSquareIcon className="h-5 w-5" />
-            ) : (
-              <DocumentMagnifyingGlassIcon className="h-5 w-5" />
-            )}
-          </button>
+          {openConfigCallback && (
+            <button
+              className=" flex items-center rounded-md border-slate-300 bg-white p-1 font-medium text-slate-500 shadow-sm ring-1 transition hover:bg-slate-50 dark:border-slate-600 dark:bg-slate-800 dark:text-slate-400 dark:ring-slate-600 hover:dark:bg-slate-700"
+              title={
+                renderMode === 'nx-console'
+                  ? 'Open project details in editor'
+                  : 'Open project details'
+              }
+              onClick={openConfigCallback}
+            >
+              {renderMode === 'nx-console' ? (
+                <PencilSquareIcon className="h-5 w-5" />
+              ) : (
+                <DocumentMagnifyingGlassIcon className="h-5 w-5" />
+              )}
+            </button>
+          )}
         </div>
-        {runTargetCallback ? (
+        {runTaskCallback && (
           <button
             className=" flex items-center rounded-md border-slate-300 bg-white p-1 font-medium text-slate-500 shadow-sm ring-1 transition hover:bg-slate-50 dark:border-slate-600 dark:bg-slate-800 dark:text-slate-400 dark:ring-slate-600 hover:dark:bg-slate-700"
             title="Run Task"
-            onClick={runTargetCallback}
+            onClick={runTaskCallback}
           >
             <PlayIcon className="h-5 w-5" />
           </button>
-        ) : undefined}
+        )}
       </h4>
       {description ? <p className="mt-4">{description}</p> : null}
       {children}
