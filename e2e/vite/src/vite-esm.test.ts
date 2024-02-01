4be691d187d31cbd517b087ed95c38361f1d9369
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-02-01 12:52:03 +0200

    fix(vite): specify project root format

--- e2e/vite/src/vite-esm.test.ts
+++ e2e/vite/src/vite-esm.test.ts
@@ -19,7 +19,9 @@ describe('Vite ESM tests', () => {
 
   it('should build with Vite when it is ESM-only', async () => {
     const appName = uniq('viteapp');
-    runCLI(`generate @nx/react:app ${appName} --bundler=vite`);
+    runCLI(
+      `generate @nx/react:app ${appName} --bundler=vite --project-name-and-root-format=as-provided`
+    );
 
     // .mts file is needed because Nx will transpile .ts files as CJS
     renameFile(`${appName}/vite.config.ts`, `${appName}/vite.config.mts`);
