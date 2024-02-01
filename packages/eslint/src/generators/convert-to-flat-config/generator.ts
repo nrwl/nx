9dda57bb0cdfd94ae789f3dc87a743e168b4967f
Miroslav Jonas <missing.manual@gmail.com>
2024-02-01 14:24:04 +0100

    fix(linter): improve the command check

--- packages/eslint/src/generators/convert-to-flat-config/generator.ts
+++ packages/eslint/src/generators/convert-to-flat-config/generator.ts
@@ -90,7 +90,7 @@ function convertProjectToFlatConfig(
       const eslintTargets = Object.keys(projectConfig.targets || {}).filter(
         (t) =>
           projectConfig.targets[t].executor === '@nx/eslint:lint' ||
-          projectConfig.targets[t].command.startsWith('eslint')
+          projectConfig.targets[t].command.includes('eslint ')
       );
       let ignorePath: string | undefined;
       for (const target of eslintTargets) {
@@ -107,7 +107,8 @@ function convertProjectToFlatConfig(
       const nxHasEsLintTargets = Object.keys(nxJson.targetDefaults || {}).some(
         (t) =>
           (t === '@nx/eslint:lint' ||
-            nxJson.targetDefaults[t].executor === '@nx/eslint:lint') &&
+            nxJson.targetDefaults[t].executor === '@nx/eslint:lint' ||
+            nxJson.targetDefaults[t].command.includes('eslint ')) &&
           projectConfig.targets?.[t]
       );
       const nxHasEsLintPlugin = (nxJson.plugins || []).some((p) =>
