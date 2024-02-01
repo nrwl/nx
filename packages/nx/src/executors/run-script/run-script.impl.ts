341bd1bd066d9e2f956b3c18fbdfa696e290878a
Jonathan Cammisuli <jon@cammisuli.ca>
2024-02-01 14:22:58 -0500

    fix(core): do not run pty in run-script when we're not TTY (#21496)
    (cherry picked from commit 09abd39ecc1e11ef94b404d4512fca3526d20d31)

--- packages/nx/src/executors/run-script/run-script.impl.ts
+++ packages/nx/src/executors/run-script/run-script.impl.ts
@@ -3,6 +3,7 @@ import type { ExecutorContext } from '../../config/misc-interfaces';
 import { runCommand } from '../../native';
 import { PseudoTtyProcess } from '../../utils/child-process';
 import { getPackageManagerCommand } from '../../utils/package-manager';
+import { execSync } from 'child_process';
 
 export interface RunScriptOptions {
   script: string;
@@ -15,29 +16,50 @@ export default async function (
 ) {
   const pm = getPackageManagerCommand();
   try {
-    await new Promise<void>((res, rej) => {
-      const cp = new PseudoTtyProcess(
-        runCommand(
-          pm.run(options.script, options.__unparsed__.join(' ')),
-          path.join(
-            context.root,
-            context.projectsConfigurations.projects[context.projectName].root
-          ),
-          process.env
-        )
-      );
-      cp.onExit((code) => {
-        if (code === 0) {
-          res();
-        } else if (code >= 128) {
-          process.exit(code);
-        } else {
-          rej();
-        }
-      });
-    });
+    let command = pm.run(options.script, options.__unparsed__.join(' '));
+    let cwd = path.join(
+      context.root,
+      context.projectsConfigurations.projects[context.projectName].root
+    );
+    let env = process.env;
+    if (process.stdout.isTTY) {
+      await ptyProcess(command, cwd, env);
+    } else {
+      nodeProcess(command, cwd, env);
+    }
     return { success: true };
   } catch (e) {
     return { success: false };
   }
+}
+
+function nodeProcess(
+  command: string,
+  cwd: string,
+  env: Record<string, string>
+) {
+  execSync(command, {
+    stdio: ['inherit', 'inherit', 'inherit'],
+    cwd,
+    env,
+  });
+}
+
+async function ptyProcess(
+  command: string,
+  cwd: string,
+  env: Record<string, string>
+) {
+  return new Promise<void>((res, rej) => {
+    const cp = new PseudoTtyProcess(runCommand(command, cwd, env));
+    cp.onExit((code) => {
+      if (code === 0) {
+        res();
+      } else if (code >= 128) {
+        process.exit(code);
+      } else {
+        rej();
+      }
+    });
+  });
 }
