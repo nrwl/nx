8a76e6717c4a33b80d047eab23f9aa9fc3d16bf8
Jack Hsu <jack.hsu@gmail.com>
2024-02-05 15:07:58 -0500

    wip

--- graph/client/webpack.config.js
+++ graph/client/webpack.config.js
@@ -1,20 +1,68 @@
+const { join } = require('path');
 // nx-ignore-next-line
-const { withNx, composePlugins } = require('@nx/webpack');
+const { NxWebpackPlugin } = require('@nx/webpack');
 // nx-ignore-next-line
-const { withReact } = require('@nx/react');
+const { NxReactWebpackPlugin } = require('@nx/react');
 
-module.exports = composePlugins(withNx(), withReact(), (config, context) => {
-  return {
-    ...config,
-    resolve: {
-      ...config.resolve,
-      alias: {
-        ...config.resolve.alias,
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat', // Must be below test-utils
-        'react/jsx-runtime': 'preact/jsx-runtime',
-      },
-    },
-  };
-});
+module.exports = {
+  output: {
+    path: join(__dirname, '../../build/apps/graph'),
+  },
+  devServer: { port: 4201 },
+  plugins: [
+    new NxWebpackPlugin({
+      maxWorkers: 8,
+      index: './src/index.html',
+      main: './src/main.tsx',
+      polyfills: './src/polyfills.ts',
+      tsConfig: './tsconfig.app.json',
+      styles: ['./src/styles.css'],
+      scripts: [],
+      assets: [
+        './src/favicon.ico',
+        './src/assets/project-graphs/',
+        './src/assets/task-graphs/',
+        './src/assets/generated-project-graphs/',
+        './src/assets/generated-task-graphs/',
+        './src/assets/generated-task-inputs/',
+        './src/assets/generated-source-maps/',
+        {
+          input: 'graph/client/src/assets/dev',
+          output: '/',
+          glob: 'environment.js',
+        },
+      ],
+      webpackConfig: './webpack.config.js',
+      optimization: false,
+      outputHashing: 'none',
+      sourceMap: true,
+      extractCss: true,
+      namedChunks: false,
+      extractLicenses: false,
+      vendorChunk: true,
+      budgets: [
+        {
+          type: 'initial',
+          maximumWarning: '2mb',
+          maximumError: '5mb',
+        },
+      ],
+      babelUpwardRootMode: true,
+    }),
+    new NxReactWebpackPlugin(),
+  ],
+};
+//   return {
+//     ...config,
+//     resolve: {
+//       ...config.resolve,
+//       alias: {
+//         ...config.resolve.alias,
+//         react: 'preact/compat',
+//         'react-dom/test-utils': 'preact/test-utils',
+//         'react-dom': 'preact/compat', // Must be below test-utils
+//         'react/jsx-runtime': 'preact/jsx-runtime',
+//       },
+//     },
+//   };
+// });
