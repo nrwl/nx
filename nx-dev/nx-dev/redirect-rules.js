81d73e3512bf38fb93342e3cc69ea9d142fa6d1c
Isaac Mann <isaacplmann@users.noreply.github.com>
2024-02-05 12:55:07 -0500

    docs(core): redirect for nx-agents (#21617)
    (cherry picked from commit 19fc320e201cee307750177532d264fed6fd1334)

--- nx-dev/nx-dev/redirect-rules.js
+++ nx-dev/nx-dev/redirect-rules.js
@@ -452,7 +452,9 @@ const nxCloudUrls = {
   '/nx-cloud/concepts/scenarios': '/ci/concepts/cache-security',
   '/nx-cloud/account/encryption': '/ci/recipes/security/encryption',
   '/nx-cloud/concepts/encryption': '/ci/recipes/security/encryption',
-  '/nx-cloud/features/nx-cloud-workflows': '/ci/features/nx-agents',
+  '/nx-cloud/features/nx-cloud-workflows':
+    '/ci/features/distribute-task-execution',
+  '/ci/features/nx-agents': '/ci/features/distribute-task-execution',
   '/ci': '/ci/intro/ci-with-nx',
   '/nx-cloud/:path*': '/ci/:path*',
 };
