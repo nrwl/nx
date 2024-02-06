b7599e8ec4ec47c3905498f6c1b28b53e60e0b5b
Zack DeRose <zack.derose@gmail.com>
2024-02-06 09:57:16 -0500

    docs(misc): launch nx tuesday announcement

--- nx-dev/ui-conference/src/lib/launch-week/announcements.tsx
+++ nx-dev/ui-conference/src/lib/launch-week/announcements.tsx
@@ -73,6 +73,75 @@ export function LaunchWeekAnnouncements(): JSX.Element {
             </div>
           </article>
         </div>
+
+        {/* TUESDAY */}
+        <div>
+          <article className="relative overflow-hidden pt-4 mx-auto max-w-screen-lg xl:max-w-screen-xl">
+            <div className="px-5 py-12 grid sm:grid-cols-2 sm:gap-8 lg:py-16">
+              <div>
+                <header>
+                  <SectionHeading as="h2" variant="title" id="monday">
+                    Tuesday
+                  </SectionHeading>
+                  <SectionHeading as="p" variant="display" className="mt-4">
+                    NEW PLUGIN: <a href="nx-api/nuxt">@nx/nuxt</a>
+                  </SectionHeading>
+                </header>
+                <div className="mt-8 flex gap-16 font-normal">
+                  <p className="max-w-xl text-lg text-slate-700 dark:text-slate-400">
+                    Checkout the newest Nx Plugin: @nx/nuxt. We're excited to
+                    collaborate closer with the Vue community, and have been
+                    looking forward to launching this plugin since we first
+                    announce Vue support last year!
+                    <br />
+                    <br />
+                    This is the first plugin to be created with Project Crystal
+                    from day 1! Using this plugin - you can expect enhanced
+                    support for the Nuxt framework in Nx. It's a game changer
+                    for Nuxt developers who want to take advantage of Nx's
+                    powerful monorepo capabilities.
+                  </p>
+                </div>
+                <div className="action my-6 flex space-x-2">
+                  <ButtonLink
+                    variant="primary"
+                    size="default"
+                    href="https://blog.nrwl.io/introducing-nx-nuxt-enhanced-nuxt-js-support-in-nx-01eac78034fc?source=friends_link&sk=91582cdbd0719dc23375338ad92afa9b"
+                    title="Unveiling Project Crystal"
+                  >
+                    Read the blog post
+                  </ButtonLink>
+                  <ButtonLink
+                    variant="primary"
+                    size="default"
+                    href="https://www.youtube.com/watch?v=1L-bDvEemoc&ab_channel=Nx-SmartMonorepos-FastCI"
+                    title="NEW Nx Plugin: @nx/nuxt - Youtube Video"
+                  >
+                    Watch the video
+                  </ButtonLink>
+                  <ButtonLink
+                    variant="primary"
+                    size="default"
+                    href="https://www.youtube.com/watch?v=uHwUxFYX2DY&ab_channel=Nx-SmartMonorepos-FastCI"
+                    title="Nx Live: @nx/nuxt w/ Nuxt Maintinaer: Daniel Roe - Youtube Livestream"
+                  >
+                    Catch the Livestream
+                  </ButtonLink>
+                </div>
+              </div>
+              <div
+                aria-hidden="true"
+                className="order-first sm:order-last pb-8 relative flex flex-col items-center"
+              >
+                <img
+                  className="rounded-lg"
+                  src="/images/launch-nx/new-plugin-nx-nuxt.jpg"
+                  alt="Nx Project Crystal"
+                />
+              </div>
+            </div>
+          </article>
+        </div>
       </section>
     </div>
   );
