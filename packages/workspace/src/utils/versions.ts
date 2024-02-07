b44020a0308ab208c4d9e912d0d86f2d7e523ebb
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-02-07 13:59:56 +0100

    feat(angular): support angular 17.2.0-rc.0 (#21741)

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -4,4 +4,4 @@ export const typescriptVersion = '~5.3.2';
 
 // TODO: remove when preset generation is reworked and
 // deps are not installed from workspace
-export const angularCliVersion = '~17.1.0';
+export const angularCliVersion = '~17.2.0-rc.0';
