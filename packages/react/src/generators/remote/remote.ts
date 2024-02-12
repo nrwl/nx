c87884ad6ed95eabab9970fb472eff5212123c73
Remco Krams <info@remcokrams.nl>
2024-02-12 21:42:11 +0100

    fix(react): generate correctly when --js is used for module federation host/remote (#20119)
    Co-authored-by: Jack Hsu <jack.hsu@gmail.com>

--- packages/react/src/generators/remote/remote.ts
+++ packages/react/src/generators/remote/remote.ts
@@ -22,6 +22,7 @@ import { setupSsrForRemote } from './lib/setup-ssr-for-remote';
 import { setupTspathForRemote } from './lib/setup-tspath-for-remote';
 import { addRemoteToDynamicHost } from './lib/add-remote-to-dynamic-host';
 import { addMfEnvToTargetDefaultInputs } from '../../utils/add-mf-env-to-inputs';
+import { maybeJs } from '../../utils/maybe-js';
 
 export function addModuleFederationFiles(
   host: Tree,
@@ -32,6 +33,13 @@ export function addModuleFederationFiles(
     ...options,
     tmpl: '',
   };
+
+  generateFiles(
+    host,
+    join(__dirname, `./files/${options.js ? 'common' : 'common-ts'}`),
+    options.appProjectRoot,
+    templateVariables
+  );
 
   const pathToModuleFederationFiles = options.typescriptConfiguration
     ? 'module-federation-ts'
@@ -73,7 +81,11 @@ export async function remoteGeneratorInternal(host: Tree, schema: Schema) {
   const tasks: GeneratorCallback[] = [];
   const options: NormalizedSchema<Schema> = {
     ...(await normalizeOptions<Schema>(host, schema, '@nx/react:remote')),
-    typescriptConfiguration: schema.typescriptConfiguration ?? false,
+    // when js is set to true, we want to use the js configuration
+    js: schema.js ?? false,
+    typescriptConfiguration: schema.js
+      ? false
+      : schema.typescriptConfiguration ?? true,
     dynamic: schema.dynamic ?? false,
     // TODO(colum): remove when MF works with Crystal
     addPlugin: false,
@@ -94,8 +106,8 @@ export async function remoteGeneratorInternal(host: Tree, schema: Schema) {
   // Renaming original entry file so we can use `import(./bootstrap)` in
   // new entry file.
   host.rename(
-    join(options.appProjectRoot, 'src/main.tsx'),
-    join(options.appProjectRoot, 'src/bootstrap.tsx')
+    join(options.appProjectRoot, maybeJs(options, 'src/main.tsx')),
+    join(options.appProjectRoot, maybeJs(options, 'src/bootstrap.tsx'))
   );
 
   addModuleFederationFiles(host, options);
