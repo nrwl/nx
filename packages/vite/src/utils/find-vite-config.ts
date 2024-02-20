fe17fc3287be64bc764dbe3281bc37bda4562fd6
Katerina Skroumpelou <mandarini@users.noreply.github.com>
2024-02-20 16:38:12 +0200

    fix(vite): project conversion generator (#21646)

--- packages/vite/src/utils/find-vite-config.ts
+++ packages/vite/src/utils/find-vite-config.ts
@@ -9,3 +9,13 @@ export function findViteConfig(tree: Tree, searchRoot: string) {
     }
   }
 }
+
+export function findWebpackConfig(tree: Tree, searchRoot: string) {
+  const allowsExt = ['js', 'ts', 'mjs', 'cjs'];
+
+  for (const ext of allowsExt) {
+    if (tree.exists(joinPathFragments(searchRoot, `webpack.config.${ext}`))) {
+      return joinPathFragments(searchRoot, `webpack.config.${ext}`);
+    }
+  }
+}
