729e0a15ce91c2be63c5ce831499d5b4fa714b2b
Colum Ferry <cferry09@gmail.com>
2024-02-29 18:56:09 +0000

    feat(bundling): crystalize rollup (#22045)

--- e2e/rollup/src/rollup.test.ts
+++ e2e/rollup/src/rollup.test.ts
@@ -161,4 +161,53 @@ describe('Rollup Plugin', () => {
     expect(() => runCLI(`build ${jsLib}`)).not.toThrow();
     checkFilesExist(`dist/test/index.mjs.js`);
   });
+
+  it('should build correctly with crystal', () => {
+    // ARRANGE
+    updateFile(
+      `libs/test/src/index.ts`,
+      `export function helloWorld() {
+      console.log("hello world");
+    }`
+    );
+    updateFile(`libs/test/package.json`, JSON.stringify({ name: 'test' }));
+    updateFile(
+      `libs/test/rollup.config.js`,
+      `import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript2 from 'rollup-plugin-typescript2';
+
+const config = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/bundle.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: 'dist/bundle.es.js',
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    typescript2(),
+    babel({ babelHelpers: 'bundled' }),
+    commonjs(),
+  ]
+};
+
+export default config;
+`
+    );
+    // ACT
+    runCLI(`generate @nx/rollup:init --no-interactive`);
+    const output = runCLI(`build test`);
+
+    // ASSERT
+    expect(output).toContain('Successfully ran target build for project test');
+    checkFilesExist(`libs/test/dist/bundle.js`);
+    checkFilesExist(`libs/test/dist/bundle.es.js`);
+  });
 });
