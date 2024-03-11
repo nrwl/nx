ce65c25dd4ead0afd60c750e7f066b96e4b9321a
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-03-11 12:55:49 +0100

    feat(misc): add support for typescript 5.4

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = require('../../package.json').version;
 
-export const typescriptVersion = '~5.3.2';
+export const typescriptVersion = '~5.4.2';
 
 // TODO: remove when preset generation is reworked and
 // deps are not installed from workspace
