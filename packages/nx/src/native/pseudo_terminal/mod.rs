86c5b3d2aa8d3c3e72f760624c1e287d182dea2c
Jason Jean <jasonjean1993@gmail.com>
2024-03-15 12:34:27 -0400

    cleanup(core): rename file with rust patterns (#22330)

--- packages/nx/src/native/pseudo_terminal/mod.rs
+++ packages/nx/src/native/pseudo_terminal/mod.rs
@@ -7,5 +7,5 @@ mod pseudo_terminal;
 pub mod child_process;
 
 #[cfg_attr(target_os = "macos", path = "mac.rs")]
-#[cfg_attr(not(target_os = "macos"), path = "non-mac.rs")]
+#[cfg_attr(not(target_os = "macos"), path = "non_mac.rs")]
 pub mod rust_pseudo_terminal;
