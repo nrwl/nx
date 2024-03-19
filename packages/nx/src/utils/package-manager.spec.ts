1f9951a30efd95992441bf93d74a7644c145ffc4
Layne Faler <laynefaler@gmail.com>
2024-03-19 09:55:50 -0700

    fix(js): Updated tests

--- packages/nx/src/utils/package-manager.spec.ts
+++ packages/nx/src/utils/package-manager.spec.ts
@@ -106,7 +106,7 @@ describe('package-manager', () => {
       });
       const packageManager = detectPackageManager();
       expect(packageManager).toEqual('npm');
-      expect(fs.existsSync).toHaveBeenCalledTimes(8300);
+      expect(fs.existsSync).toHaveBeenCalledTimes(10084);
     });
   });
 
