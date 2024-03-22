fb70a9e1bba6075183eacc0fd6012f3c91719426
Jack Hsu <jack.hsu@gmail.com>
2024-03-21 22:09:56 -0400

    fix(js): update babel preset to specify minor version of core-js for better optimization (#22433)

--- packages/rollup/src/utils/versions.ts
+++ packages/rollup/src/utils/versions.ts
@@ -1,3 +1,5 @@
 export const nxVersion = require('../../package.json').version;
 export const swcLoaderVersion = '0.1.15';
 export const tsLibVersion = '^2.3.0';
+
+export const coreJsVersion = '^3.36.1';
