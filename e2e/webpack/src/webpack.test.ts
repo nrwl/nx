ee7b2274acdbadf99602b38d34ccc4b8f5569218
Jack Hsu <jack.hsu@gmail.com>
2024-03-25 12:49:13 -0400

    fix(js): match core-js version with babel options (#22493)

--- e2e/webpack/src/webpack.test.ts
+++ e2e/webpack/src/webpack.test.ts
@@ -2,6 +2,7 @@ import {
   cleanupProject,
   listFiles,
   newProject,
+  packageInstall,
   readFile,
   rmDist,
   runCLI,
@@ -188,4 +189,38 @@ describe('Webpack Plugin', () => {
     expect(content).not.toMatch(/secret/);
     expect(content).toMatch(/foobar/);
   });
+
+  it('should support babel + core-js to polyfill JS features', async () => {
+    const appName = uniq('app');
+    runCLI(
+      `generate @nx/web:app ${appName} --bundler webpack --compiler=babel`
+    );
+    packageInstall('core-js', undefined, '3.26.1', 'prod');
+    updateFile(
+      `apps/${appName}/src/main.ts`,
+      `
+      import 'core-js/stable';
+      async function main() {
+        const result = await Promise.resolve('foobar')
+        console.log(result);
+      }
+      main();
+    `
+    );
+
+    // Modern browser
+    updateFile(`apps/${appName}/.browserslistrc`, `last 1 Chrome version\n`);
+    runCLI(`build ${appName}`);
+    expect(readMainFile(`dist/apps/${appName}`)).toMatch(`await Promise`);
+
+    // Legacy browser
+    updateFile(`apps/${appName}/.browserslistrc`, `IE 11\n`);
+    runCLI(`build ${appName}`);
+    expect(readMainFile(`dist/apps/${appName}`)).not.toMatch(`await Promise`);
+  });
 });
+
+function readMainFile(dir: string): string {
+  const main = listFiles(dir).find((f) => f.startsWith('main.'));
+  return readFile(`${dir}/${main}`);
+}
