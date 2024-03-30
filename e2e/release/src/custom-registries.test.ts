a7be3e4e6d51acad7c8e5520990aaca312365dbe
Austin Fahsl <austin@fahsl.io>
2024-03-30 17:09:40 -0600

    chore(release): refactor e2e to be more readable

--- e2e/release/src/custom-registries.test.ts
+++ e2e/release/src/custom-registries.test.ts
@@ -13,183 +13,15 @@ import {
 import { execSync } from 'child_process';
 import type { PackageJson } from 'nx/src/utils/package-json';
 
-expect.addSnapshotSerializer({
-  serialize(str: string) {
-    return (
-      str
-        // Remove all output unique to specific projects to ensure deterministic snapshots
-        .replaceAll(/my-pkg-\d+/g, '{project-name}')
-        .replaceAll(
-          /integrity:\s*.*/g,
-          'integrity: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
-        )
-        .replaceAll(/\b[0-9a-f]{40}\b/g, '{SHASUM}')
-        .replaceAll(/\d*B  index\.js/g, 'XXB  index.js')
-        .replaceAll(/\d*B  project\.json/g, 'XXB  project.json')
-        .replaceAll(/\d*B package\.json/g, 'XXXB package.json')
-        .replaceAll(/size:\s*\d*\s?B/g, 'size: XXXB')
-        .replaceAll(/\d*\.\d*\s?kB/g, 'XXX.XXX kb')
-        .replaceAll(/[a-fA-F0-9]{7}/g, '{COMMIT_SHA}')
-        .replaceAll(/Test @[\w\d]+/g, 'Test @{COMMIT_AUTHOR}')
-        // Normalize the version title date.
-        .replaceAll(/\(\d{4}-\d{2}-\d{2}\)/g, '(YYYY-MM-DD)')
-        // We trim each line to reduce the chances of snapshot flakiness
-        .split('\n')
-        .map((r) => r.trim())
-        .join('\n')
-    );
-  },
-  test(val: string) {
-    return val != null && typeof val === 'string';
-  },
-});
-
 describe('nx release - custom npm registries', () => {
-  let pkg1: string;
-  let pkg2: string;
-  let pkg3: string;
-  let pkg4: string;
-  let pkg5: string;
-  let pkg6: string;
   const verdaccioPort = 7191;
   const customRegistryUrl = `http://localhost:${verdaccioPort}`;
+  const scope = 'scope';
 
   beforeAll(async () => {
     newProject({
       unsetProjectNameAndRootFormat: false,
       packages: ['@nx/js'],
-    });
-
-    pkg1 = uniq('my-pkg-1');
-    runCLI(`generate @nx/workspace:npm-package ${pkg1}`);
-
-    pkg2 = uniq('my-pkg-2');
-    runCLI(`generate @nx/workspace:npm-package ${pkg2}`);
-
-    pkg3 = uniq('my-pkg-3');
-    runCLI(`generate @nx/workspace:npm-package ${pkg3}`);
-
-    pkg4 = uniq('my-pkg-4');
-    runCLI(`generate @nx/workspace:npm-package ${pkg4}`);
-
-    pkg5 = uniq('my-pkg-5');
-    runCLI(`generate @nx/workspace:npm-package ${pkg5}`);
-
-    pkg6 = uniq('my-pkg-6');
-    runCLI(`generate @nx/workspace:npm-package ${pkg6}`);
-
-    updateJson<PackageJson>(`${pkg1}/package.json`, (json) => {
-      json.name = `@proj/${pkg1}`;
-
-      json.publishConfig = {
-        registry: 'http://localhost:4002',
-      };
-      return json;
-    });
-
-    updateJson<ProjectConfiguration>(`${pkg1}/project.json`, (json) => {
-      json.release = {
-        version: {
-          generatorOptions: {
-            currentVersionResolver: 'registry',
-          },
-        },
-      };
-      return json;
-    });
-
-    updateJson<PackageJson>(`${pkg2}/package.json`, (json) => {
-      json.name = `@proj/${pkg2}`;
-
-      json.publishConfig = {
-        registry: 'http://localhost:4002',
-      };
-      return json;
-    });
-
-    updateJson<ProjectConfiguration>(`${pkg2}/project.json`, (json) => {
-      json.targets = {
-        'nx-release-publish': {
-          options: {
-            registry: 'http://localhost:4020',
-          },
-        },
-      };
-
-      return json;
-    });
-
-    updateJson<PackageJson>(`${pkg3}/package.json`, (json) => {
-      json.name = `@proj/${pkg3}`;
-      return json;
-    });
-
-    updateJson<PackageJson>(`${pkg4}/package.json`, (json) => {
-      json.name = pkg4;
-
-      json.publishConfig = {
-        registry: 'http://publish-config-registry.com',
-      };
-
-      return json;
-    });
-
-    updateJson<ProjectConfiguration>(`${pkg4}/project.json`, (json) => {
-      json.release = {
-        version: {
-          generatorOptions: {
-            currentVersionResolver: 'registry',
-          },
-        },
-      };
-      json.targets = {
-        ...json.targets,
-        'nx-release-publish': {
-          options: {
-            registry: 'http://project-config-registry.com',
-          },
-        },
-      };
-      return json;
-    });
-
-    updateJson<PackageJson>(`${pkg5}/package.json`, (json) => {
-      json.name = pkg5;
-
-      json.publishConfig = {
-        registry: 'http://publish-config-registry.com',
-      };
-
-      return json;
-    });
-
-    updateJson<ProjectConfiguration>(`${pkg5}/project.json`, (json) => {
-      json.targets = {
-        'nx-release-publish': {
-          options: {
-            registry: customRegistryUrl,
-            tag: 'beta',
-          },
-        },
-      };
-      json.release = {
-        version: {
-          generatorOptions: {
-            currentVersionResolver: 'registry',
-            currentVersionResolverMetadata: {
-              registry: customRegistryUrl,
-              tag: 'beta',
-            },
-          },
-        },
-      };
-
-      return json;
-    });
-
-    updateJson<PackageJson>(`${pkg6}/package.json`, (json) => {
-      json.name = pkg6;
-      return json;
     });
   }, 60000);
   afterAll(() => cleanupProject());
@@ -206,47 +38,91 @@ describe('nx release - custom npm registries', () => {
       .toString()
       .trim();
 
-    const npmrcEntries = ['@proj:registry=http://scoped-registry.com'];
+    const npmrcEntries = [
+      `@${scope}:registry=http://scoped-registry.com`,
+      `registry=${e2eRegistryUrl}`,
+      'tag=next',
+    ];
     createFile('.npmrc', npmrcEntries.join('\n'));
 
+    const scopedWithPublishConfig = newPackage('pkg1', {
+      scoped: true,
+      publishConfig: {
+        registry: 'http://ignored-registry.com',
+      },
+    });
+
     const publishResultScopedWithPublishConfig = runCLI(
-      `release publish -p ${pkg1} --dry-run`
+      `release publish -p ${scopedWithPublishConfig} --dry-run`
     );
     expect(publishResultScopedWithPublishConfig).toContain(
-      'Would publish to http://scoped-registry.com with tag "latest"'
+      'Would publish to http://scoped-registry.com with tag "next"'
     );
+
+    const scopedWithPublishConfigAndProjectConfig = newPackage('pkg2', {
+      scoped: true,
+      publishConfig: {
+        registry: 'http://ignored-registry.com',
+      },
+      projectConfig: {
+        registry: 'http://ignored-registry.com',
+        tag: 'alpha',
+      },
+    });
 
     const publishResultScopedWithPublishConfigAndProjectConfig = runCLI(
-      `release publish -p ${pkg2} --dry-run`
+      `release publish -p ${scopedWithPublishConfigAndProjectConfig} --dry-run`
     );
     expect(publishResultScopedWithPublishConfigAndProjectConfig).toContain(
-      'Would publish to http://scoped-registry.com with tag "latest"'
+      'Would publish to http://scoped-registry.com with tag "alpha"'
     );
 
+    const publishResultScopedWithPublishConfigAndProjectConfigAndArg = runCLI(
+      `release publish -p ${scopedWithPublishConfigAndProjectConfig} --dry-run --registry=http://ignored-registry.com`
+    );
+    expect(
+      publishResultScopedWithPublishConfigAndProjectConfigAndArg
+    ).toContain('Would publish to http://scoped-registry.com with tag "alpha"');
+
+    const scopedNoOtherConfig = newPackage('pkg3', {
+      scoped: true,
+    });
+
     const publishResultScopedNoOtherConfig = runCLI(
-      `release publish -p ${pkg3} --dry-run`
+      `release publish -p ${scopedNoOtherConfig} --dry-run`
     );
     expect(publishResultScopedNoOtherConfig).toContain(
-      'Would publish to http://scoped-registry.com with tag "latest"'
+      'Would publish to http://scoped-registry.com with tag "next"'
     );
 
     const publishResultScopedWithRegistryArg = runCLI(
-      `release publish -p ${pkg3} --dry-run --registry=${customRegistryUrl}`
+      `release publish -p ${scopedNoOtherConfig} --dry-run --registry=http://ignored-registry.com`
     );
     expect(publishResultScopedWithRegistryArg).toContain(
-      'Would publish to http://scoped-registry.com with tag "latest"'
+      'Would publish to http://scoped-registry.com with tag "next"'
     );
+
+    const noScopeWithPublishConfigAndProjectConfig = newPackage('pkg4', {
+      publishConfig: {
+        registry: 'http://publish-config-registry.com',
+      },
+      projectConfig: {
+        registry: 'http://ignored-registry.com',
+        tag: 'alpha',
+      },
+    });
 
     const publishResultNoScopeWithPublishConfigAndProjectConfig = runCLI(
-      `release publish -p ${pkg4} --dry-run`
+      `release publish -p ${noScopeWithPublishConfigAndProjectConfig} --dry-run`
     );
     expect(publishResultNoScopeWithPublishConfigAndProjectConfig).toContain(
-      'Would publish to http://publish-config-registry.com with tag "latest"'
+      'Would publish to http://publish-config-registry.com with tag "alpha"'
     );
 
-    // custom registry url will be ignored because publishConfig takes precedence
+    // custom registry url will be ignored because publishConfig takes precedence,
+    // but tag arg will properly override that from the project config
     const publishResultNoScopeWithPublishConfigAndProjectConfigAndArg = runCLI(
-      `release publish -p ${pkg5} --dry-run --registry=http://ignored-registry.com`
+      `release publish -p ${noScopeWithPublishConfigAndProjectConfig} --dry-run --registry=http://ignored-registry.com --tag=beta`
     );
     expect(
       publishResultNoScopeWithPublishConfigAndProjectConfigAndArg
@@ -254,16 +130,20 @@ describe('nx release - custom npm registries', () => {
       `Would publish to http://publish-config-registry.com with tag "beta"`
     );
 
-    const publishResultNoScope = runCLI(`release publish -p ${pkg6} --dry-run`);
+    const noScopeNoOtherConfig = newPackage('pkg6', {});
+
+    const publishResultNoScope = runCLI(
+      `release publish -p ${noScopeNoOtherConfig} --dry-run`
+    );
     expect(publishResultNoScope).toContain(
-      `Would publish to ${e2eRegistryUrl} with tag "latest"`
+      `Would publish to ${e2eRegistryUrl} with tag "next"`
     );
 
     const publishResultNoScopeWithRegistryArg = runCLI(
-      `release publish -p ${pkg6} --dry-run --registry=${customRegistryUrl}`
+      `release publish -p ${noScopeNoOtherConfig} --dry-run --registry=${customRegistryUrl} --tag=alpha`
     );
     expect(publishResultNoScopeWithRegistryArg).toContain(
-      `Would publish to ${customRegistryUrl} with tag "latest"`
+      `Would publish to ${customRegistryUrl} with tag "alpha"`
     );
 
     runCLI(`generate setup-verdaccio`);
@@ -273,39 +153,62 @@ describe('nx release - custom npm registries', () => {
       (output) => output.includes(`warn --- http address`)
     );
 
-    updateFile('.npmrc', `@proj:registry=${customRegistryUrl}\ntag=next`);
+    const npmrcEntries2 = [
+      `@${scope}:registry=${customRegistryUrl}`,
+      `registry=http://ignored-registry.com`,
+      'tag=next',
+    ];
+    updateFile('.npmrc', npmrcEntries2.join('\n'));
 
-    const actualPublishResultScoped = runCLI(`release publish -p ${pkg1}`);
+    const actualScopedWithPublishConfigAndProjectConfig = newPackage('pkg7', {
+      scoped: true,
+      publishConfig: {
+        registry: 'http://ignored-registry.com',
+      },
+      projectConfig: {
+        registry: 'http://ignored-registry.com',
+        tag: 'beta',
+        version: {
+          tag: 'alpha', // alpha tag will be passed via publish CLI arg to override the above 'beta'
+        },
+      },
+    });
 
-    updateJson<PackageJson>(`${pkg4}/package.json`, (json) => {
-      json.publishConfig = {
+    const actualPublishResultScoped = runCLI(
+      `release publish -p ${actualScopedWithPublishConfigAndProjectConfig} --registry=http://ignored-registry.com --tag=alpha`
+    );
+
+    const actualNoScopeWithPublishConfig = newPackage('pkg8', {
+      publishConfig: {
         registry: customRegistryUrl,
-      };
-      return json;
+      },
+      projectConfig: {}, // this will still set the current version resolver to 'registry', it just won't set the registry url
     });
 
     const actualPublishResultNoScopeWithPublishConfig = runCLI(
-      `release publish -p ${pkg4}`
+      `release publish -p ${actualNoScopeWithPublishConfig} --registry=http://ignored-registry.com`
     );
 
-    updateJson<PackageJson>(`${pkg5}/package.json`, (json) => {
-      delete json.publishConfig;
-      return json;
+    const actualNoScopeWithProjectConfig = newPackage('pkg9', {
+      projectConfig: {
+        registry: customRegistryUrl,
+        tag: 'beta',
+      },
     });
 
     const actualPublishResultNoScopeWithProjectConfig = runCLI(
-      `release publish -p ${pkg5}`
+      `release publish -p ${actualNoScopeWithProjectConfig}`
     );
 
-    const versionResultPkgs145 = runCLI(
-      `release version 999.9.9 -p ${pkg1},${pkg4},${pkg5} --dry-run`,
-      { silenceError: true }
+    const versionResult = runCLI(
+      `release version 999.9.9 -p ${actualScopedWithPublishConfigAndProjectConfig},${actualNoScopeWithPublishConfig},${actualNoScopeWithProjectConfig} --dry-run`,
+      { silenceError: true } // don't error on this command because the verdaccio process needs to be killed regardless before the test exits
     );
 
     await killProcessAndPorts(process.pid, verdaccioPort);
 
     expect(actualPublishResultScoped).toContain(
-      `Published to ${customRegistryUrl} with tag "next"`
+      `Published to ${customRegistryUrl} with tag "alpha"`
     );
 
     expect(actualPublishResultNoScopeWithPublishConfig).toContain(
@@ -317,21 +220,126 @@ describe('nx release - custom npm registries', () => {
     );
 
     expect(
-      versionResultPkgs145.match(
+      versionResult.match(
+        new RegExp(
+          `Resolved the current version as 0.0.0 for tag "alpha" from registry ${customRegistryUrl}`,
+          'g'
+        )
+      ).length
+    ).toBe(1);
+
+    expect(
+      versionResult.match(
         new RegExp(
           `Resolved the current version as 0.0.0 for tag "next" from registry ${customRegistryUrl}`,
           'g'
         )
       ).length
-    ).toBe(2);
+    ).toBe(1);
 
     expect(
-      versionResultPkgs145.match(
+      versionResult.match(
         new RegExp(
           `Resolved the current version as 0.0.0 for tag "beta" from registry ${customRegistryUrl}`,
           'g'
         )
       ).length
     ).toBe(1);
-  });
+  }, 600000);
+
+  function newPackage(
+    name: string,
+    options: {
+      scoped?: true;
+      publishConfig?: {
+        registry?: string;
+      };
+      projectConfig?: {
+        registry?: string;
+        tag?: string;
+        version?: {
+          registry?: string;
+          tag?: string;
+        };
+        publish?: {
+          registry?: string;
+          tag?: string;
+        };
+      };
+    }
+  ): string {
+    const projectName = uniq(name);
+    runCLI(`generate @nx/workspace:npm-package ${projectName}`);
+
+    let packageName = projectName;
+    if (options.scoped) {
+      packageName = `@${scope}/${projectName}`;
+    }
+
+    updateJson<PackageJson>(`${projectName}/package.json`, (json) => {
+      json.name = packageName;
+      if (options.publishConfig) {
+        json.publishConfig = options.publishConfig;
+      }
+      return json;
+    });
+
+    updateJson<ProjectConfiguration>(`${projectName}/project.json`, (json) => {
+      if (options.projectConfig) {
+        json.release = {
+          version: {
+            generatorOptions: {
+              currentVersionResolver: 'registry',
+              currentVersionResolverMetadata: {},
+            },
+          },
+        };
+        json.targets = {
+          ...json.targets,
+          'nx-release-publish': {
+            options: {},
+          },
+        };
+      }
+      if (options.projectConfig?.registry) {
+        json.targets['nx-release-publish'].options.registry =
+          options.projectConfig.registry;
+        (
+          json.release.version.generatorOptions
+            .currentVersionResolverMetadata as Record<string, string>
+        ).registry = options.projectConfig.registry;
+      }
+      if (options.projectConfig?.tag) {
+        json.targets['nx-release-publish'].options.tag =
+          options.projectConfig.tag;
+        (
+          json.release.version.generatorOptions
+            .currentVersionResolverMetadata as Record<string, string>
+        ).tag = options.projectConfig.tag;
+      }
+      if (options.projectConfig?.publish?.registry) {
+        json.targets['nx-release-publish'].options.registry =
+          options.projectConfig.publish.registry;
+      }
+      if (options.projectConfig?.publish?.tag) {
+        json.targets['nx-release-publish'].options.tag =
+          options.projectConfig.publish.tag;
+      }
+      if (options.projectConfig?.version?.registry) {
+        (
+          json.release.version.generatorOptions
+            .currentVersionResolverMetadata as Record<string, string>
+        ).registry = options.projectConfig.version.registry;
+      }
+      if (options.projectConfig?.version?.tag) {
+        (
+          json.release.version.generatorOptions
+            .currentVersionResolverMetadata as Record<string, string>
+        ).tag = options.projectConfig.version.tag;
+      }
+      return json;
+    });
+
+    return projectName;
+  }
 });
