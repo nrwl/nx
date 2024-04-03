ad0bf1923d9da5628da1894b2ca2635ebacd493e
Nicholas Cunningham <ndcunningham@gmail.com>
2024-04-03 15:35:10 -0600

    fix(nx-dev): Update urls that are 404 (#22653)

--- nx-dev/nx-dev/redirect-rules.js
+++ nx-dev/nx-dev/redirect-rules.js
@@ -186,7 +186,7 @@ const guideUrls = {
   '/migration/lerna-and-nx': 'https://lerna.js.org',
   '/cypress/v10-migration-guide': '/cypress/v11-migration-guide',
   '/cypress/generators/migrate-to-cypress-10':
-    '/cypress/generators/migrate-to-cypress-11',
+    '/packages/cypress/generators/migrate-to-cypress-11',
 };
 
 /**
