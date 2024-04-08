50d89c7d5ae407d79494567e1f2aa4fb9562624f
Nicholas Cunningham <ndcunningham@gmail.com>
2024-04-08 08:44:20 -0600

    fix(nextjs): Adding tailwind should work when creating an app OOTB (#22709)

--- packages/next/src/generators/application/lib/normalize-options.ts
+++ packages/next/src/generators/application/lib/normalize-options.ts
@@ -94,7 +94,7 @@ export async function normalizeOptions(
   const appDir = options.appDir ?? true;
   const src = options.src ?? true;
 
-  const styledModule = /^(css|scss|less)$/.test(options.style)
+  const styledModule = /^(css|scss|less|tailwind)$/.test(options.style)
     ? null
     : options.style;
 
