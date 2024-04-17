701c513fd7ccbc52a6ee3c0aed3cbf342007e5f9
Jack Hsu <jack.hsu@gmail.com>
2024-04-17 14:18:10 -0400

    feat(js): add swc cli options --strip-leading-paths (#22856)

--- packages/js/src/executors/swc/swc.impl.ts
+++ packages/js/src/executors/swc/swc.impl.ts
@@ -17,6 +17,7 @@ import {
 import { copyPackageJson } from '../../utils/package-json';
 import {
   NormalizedSwcExecutorOptions,
+  SwcCliOptions,
   SwcExecutorOptions,
 } from '../../utils/schema';
 import { compileSwc, compileSwcWatch } from '../../utils/swc/compile-swc';
@@ -59,20 +60,16 @@ function normalizeOptions(
     outputPath
   );
 
-  const projectRootParts = projectRoot.split('/');
-  // We pop the last part of the `projectRoot` to pass
-  // the last part (projectDir) and the remainder (projectRootParts) to swc
-  const projectDir = projectRootParts.pop();
-  // default to current directory if projectRootParts is [].
-  // Eg: when a project is at the root level, outside of layout dir
-  const swcCwd = projectRootParts.join('/') || '.';
+  // Always execute from root of project, same as with SWC CLI.
+  const swcCwd = join(root, projectRoot);
   const { swcrcPath, tmpSwcrcPath } = getSwcrcPath(options, root, projectRoot);
 
   const swcCliOptions = {
-    srcPath: projectDir,
-    destPath: relative(join(root, swcCwd), outputPath),
+    srcPath: projectRoot,
+    destPath: relative(swcCwd, outputPath),
     swcCwd,
     swcrcPath,
+    stripLeadingPaths: Boolean(options.stripLeadingPaths),
   };
 
   return {
@@ -127,11 +124,16 @@ export async function* swcExecutor(
   );
 
   if (!isInlineGraphEmpty(inlineProjectGraph)) {
+    if (options.stripLeadingPaths) {
+      throw new Error(`Cannot use --strip-leading-paths with inlining.`);
+    }
+
     options.projectRoot = '.'; // set to root of workspace to include other libs for type check
 
     // remap paths for SWC compilation
-    options.swcCliOptions.srcPath = options.swcCliOptions.swcCwd;
+    options.inline = true;
     options.swcCliOptions.swcCwd = '.';
+    options.swcCliOptions.srcPath = options.swcCliOptions.swcCwd;
     options.swcCliOptions.destPath = join(
       options.swcCliOptions.destPath.split(normalize('../')).at(-1),
       options.swcCliOptions.srcPath
