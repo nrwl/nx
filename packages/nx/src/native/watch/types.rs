a93a98bf13c3056d456021d6800d629a51c9a684
FrozenPandaz <jasonjean1993@gmail.com>
2024-04-24 12:26:57 -0400

    fix(core): handle created directories when watching on linux

--- packages/nx/src/native/watch/types.rs
+++ packages/nx/src/native/watch/types.rs
@@ -1,7 +1,11 @@
 use napi::bindgen_prelude::*;
+use std::fs::read_dir;
 
-use std::path::PathBuf;
+use crate::native::utils::Normalize;
+use crate::native::walker::nx_walker_sync;
+use std::path::{Path, PathBuf};
 use tracing::trace;
+use watchexec_events::filekind::CreateKind;
 use watchexec_events::{Event, Tag};
 
 use crate::native::watch::utils::transform_event;
@@ -28,7 +32,7 @@ impl From<&WatchEventInternal> for WatchEvent {
     fn from(value: &WatchEventInternal) -> Self {
         let path = value
             .path
-            .strip_prefix(value.origin.as_ref().expect("origin is available"))
+            .strip_prefix(&value.origin)
             .unwrap_or(&value.path)
             .display()
             .to_string();
@@ -47,86 +51,135 @@ impl From<&WatchEventInternal> for WatchEvent {
 pub(super) struct WatchEventInternal {
     pub path: PathBuf,
     pub r#type: EventType,
-    pub origin: Option<String>,
+    pub origin: String,
 }
 
+pub fn transform_event_to_watch_events(
+    value: &Event,
+    origin: &str,
+) -> anyhow::Result<Vec<WatchEventInternal>> {
+    let transformed = transform_event(value);
+    let value = transformed.as_ref().unwrap_or(value);
 
+    let Some(path) = value.paths().next() else {
+        let error_msg = "unable to get path from the event";
+        trace!(?value, error_msg);
+        anyhow::bail!(error_msg)
+    };
 
-impl TryFrom<&Event> for WatchEventInternal {
-    type Error = anyhow::Error;
+    let Some(event_kind) = value.tags.iter().find_map(|t| match t {
+        Tag::FileEventKind(event_kind) => Some(event_kind),
+        _ => None,
+    }) else {
+        let error_msg = "unable to get the file event kind";
+        trace!(?value, error_msg);
+        anyhow::bail!(error_msg)
+    };
 
-    fn try_from(value: &Event) -> std::result::Result<Self, Self::Error> {
-        let transformed = transform_event(value);
-        let value = transformed.as_ref().unwrap_or(value);
+    let path_ref = path.0;
+    if path.1.is_none() && !path_ref.exists() {
+        Ok(vec![WatchEventInternal {
+            path: path_ref.into(),
+            r#type: EventType::delete,
+            origin: origin.to_owned(),
+        }])
+    } else {
+        #[cfg(target_os = "macos")]
+        {
+            use std::fs;
+            use std::os::macos::fs::MetadataExt;
 
-        let Some(path) =  value.paths().next() else {
-             let error_msg = "unable to get path from the event";
-             trace!(?value, error_msg);
-             anyhow::bail!(error_msg)
-        };
+            let origin = origin.to_owned();
+            let t = fs::metadata(path_ref);
+            match t {
+                Err(_) => Ok(vec![WatchEventInternal {
+                    path: path_ref.into(),
+                    r#type: EventType::delete,
+                    origin,
+                }]),
+                Ok(t) => {
+                    let modified_time = t.st_mtime();
+                    let birth_time = t.st_birthtime();
 
-        let Some( event_kind ) = value
-            .tags
-            .iter()
-            .find_map(|t| match t {
-                Tag::FileEventKind(event_kind) => Some(event_kind),
-                _ => None,
-            }) else {
-            let error_msg = "unable to get the file event kind";
-            trace!(?value, error_msg);
-            anyhow::bail!(error_msg)
-        };
-
-        let path_ref = path.0;
-        let event_type = if path.1.is_none() && !path_ref.exists() {
-            EventType::delete
-        } else {
-            #[cfg(target_os = "macos")]
-            {
-                use std::fs;
-                use std::os::macos::fs::MetadataExt;
-
-                let t = fs::metadata(path_ref);
-                match t {
-                    Err(_) => EventType::delete,
-                    Ok(t) => {
-                        let modified_time = t.st_mtime();
-                        let birth_time = t.st_birthtime();
-
-                        // if a file is created and updated near the same time, we always get a create event
-                        // so we need to check the timestamps to see if it was created or updated
-                        // if the modified time is the same as birth_time then it was created
-                        if modified_time == birth_time {
-                            EventType::create
-                        } else {
-                            EventType::update
-                        }
+                    // if a file is created and updated near the same time, we always get a create event
+                    // so we need to check the timestamps to see if it was created or updated
+                    // if the modified time is the same as birth_time then it was created
+                    if modified_time == birth_time {
+                        Ok(vec![WatchEventInternal {
+                            path: path_ref.into(),
+                            r#type: EventType::create,
+                            origin,
+                        }])
+                    } else {
+                        Ok(vec![WatchEventInternal {
+                            path: path_ref.into(),
+                            r#type: EventType::update,
+                            origin,
+                        }])
                     }
                 }
             }
+        }
 
-            #[cfg(not(target_os = "macos"))]
-            {
-                use watchexec_events::filekind::FileEventKind;
-                use watchexec_events::filekind::ModifyKind::Name;
-                use watchexec_events::filekind::RenameMode;
+        #[cfg(target_os = "windows")]
+        {
+            use watchexec_events::filekind::FileEventKind;
+            use watchexec_events::filekind::ModifyKind::Name;
+            use watchexec_events::filekind::RenameMode;
 
-                match event_kind {
-                    FileEventKind::Create(_) => EventType::create,
+            let event_kind = match event_kind {
+                FileEventKind::Create(CreateKind::File) => EventType::create,
+                FileEventKind::Modify(Name(RenameMode::To)) => EventType::create,
+                FileEventKind::Modify(Name(RenameMode::From)) => EventType::delete,
+                FileEventKind::Modify(_) => EventType::update,
+                _ => EventType::update,
+            };
+
+            Ok(vec![WatchEventInternal {
+                path: path_ref.into(),
+                r#type: event_kind,
+                origin: origin.to_owned(),
+            }])
+        }
+
+        #[cfg(all(not(target_os = "macos"), not(target_os = "windows")))]
+        {
+            use watchexec_events::filekind::FileEventKind;
+            use watchexec_events::filekind::ModifyKind::Name;
+            use watchexec_events::filekind::RenameMode;
+
+            if matches!(event_kind, FileEventKind::Create(CreateKind::Folder)) {
+                let mut result = vec![];
+
+                for path in nx_walker_sync(path_ref, None) {
+                    let path = path_ref.join(path);
+                    if !path.is_dir() {
+                        result.push(WatchEventInternal {
+                            path,
+                            r#type: EventType::create,
+                            origin: origin.to_owned(),
+                        });
+                    }
+                }
+
+                Ok(result)
+            } else {
+                let event_kind = match event_kind {
+                    FileEventKind::Create(CreateKind::File) => EventType::create,
                     FileEventKind::Modify(Name(RenameMode::To)) => EventType::create,
                     FileEventKind::Modify(Name(RenameMode::From)) => EventType::delete,
                     FileEventKind::Modify(_) => EventType::update,
                     _ => EventType::update,
-                }
+                };
+
+                Ok(vec![WatchEventInternal {
+                    path: path_ref.into(),
+                    r#type: event_kind,
+                    origin: origin.to_owned(),
+                }])
             }
-        };
-
-        trace!(?path, ?event_kind, ?event_type, "event kind -> event type");
-
-        Ok(WatchEventInternal {
-            path: path.0.into(),
-            r#type: event_type,
-            origin: None,
-        })
+        }
     }
+
+    // trace!(?path, ?event_kind, ?event_type, "event kind -> event type");
 }
