2f1188c81a256e1fc1d73fced466ee5bd2b63f98
Mikael Pettersson <evanion@icloud.com>
2024-04-27 22:04:51 +0200

    chore(nest): update nest.js dependencies
    Updates nest plugin dependencies to nest.js 10.
    No migration needed as there already is a migration for the breaking change in nest 10 included in
    nx 16

--- packages/nest/src/utils/versions.ts
+++ packages/nest/src/utils/versions.ts
@@ -1,7 +1,7 @@
 export const nxVersion = require('../../package.json').version;
 
-export const nestJsVersion = '^10.0.2';
-export const nestJsSchematicsVersion = '^10.0.1';
-export const rxjsVersion = '^7.8.0';
-export const reflectMetadataVersion = '^0.1.13';
-export const tsLibVersion = '^2.3.0';
+export const nestJsVersion = '^10.3.8';
+export const nestJsSchematicsVersion = '^10.1.1';
+export const rxjsVersion = '^7.8.1';
+export const reflectMetadataVersion = '^0.2.2';
+export const tsLibVersion = '^2.6.2';
