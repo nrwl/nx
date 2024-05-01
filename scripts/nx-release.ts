ed8f96a9f24b6e286412be2f61491a1dcc8d1589
Katerina Skroumpelou <sk.katherine@gmail.com>
2024-05-01 11:57:55 +0300

    chore(misc): add a log

--- scripts/nx-release.ts
+++ scripts/nx-release.ts
@@ -133,7 +133,7 @@ const VALID_AUTHORS_FOR_LATEST = [
   console.log('KATERINA THE TAG IS', distTag);
 
   if (options.dryRun) {
-    console.warn('Not Publishing because --dryRun was passed');
+    console.warn('Not Publishing because --dryRun was passed', distTag);
   } else {
     // If publishing locally, force all projects to not be private first
     if (options.local) {
