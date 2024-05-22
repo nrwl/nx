b99e5ec79810879721a01141fca5dc1e468bed14
Max Kless <maxk@nrwl.io>
2024-05-22 12:21:27 +0200

    fix(core): use current user when hashing native file & enable setting its directory via env

--- packages/nx/src/native/native-file-cache-location.ts
+++ packages/nx/src/native/native-file-cache-location.ts
@@ -1,10 +1,20 @@
-import { tmpdir } from 'os';
+import { tmpdir, userInfo } from 'os';
 import { join } from 'path';
 import { createHash } from 'crypto';
 import { workspaceRoot } from '../utils/workspace-root';
 
 export const nativeFileCacheLocation = join(
-  tmpdir(),
-  'nx-native-file-cache',
-  createHash('sha256').update(workspaceRoot).digest('hex')
+  getNativeFileCacheBaseFolder(),
+  createHash('sha256')
+    .update(workspaceRoot)
+    .update(userInfo().username)
+    .digest('hex')
 );
+
+function getNativeFileCacheBaseFolder() {
+  if (process.env.NX_NATIVE_FILE_CACHE_DIRECTORY) {
+    return process.env.NX_NATIVE_FILE_CACHE_DIRECTORY;
+  } else {
+    return join(tmpdir(), 'nx-native-file-cache');
+  }
+}
