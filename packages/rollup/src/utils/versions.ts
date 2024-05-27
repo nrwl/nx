b4e7ea303942502b6fae99c3b2b9f7bc566d1f67
Jack Hsu <jack.hsu@gmail.com>
2024-05-27 16:03:39 -0400

    feat(bundling): extract rollup plugins into withNx function for use with run-commands

--- packages/rollup/src/utils/versions.ts
+++ packages/rollup/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxVersion = require('../../package.json').version;
+export const coreJsVersion = '^3.36.1';
+export const rollupVersion = '^4.14.0';
 export const swcLoaderVersion = '0.1.15';
 export const tsLibVersion = '^2.3.0';
-
-export const coreJsVersion = '^3.36.1';
