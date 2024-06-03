e8ba85b5e56aeffbb9133591febfca32b04bb430
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-06-03 15:17:40 +0200

    cleanup(devkit): prevent mutating targetDefaults in migration to crystal helper (#26316)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    ## Current Behavior
    <!-- This is the behavior we have today -->
    
    ## Expected Behavior
    <!-- This is the behavior we should expect with the changes in this PR
    -->
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #
    
    (cherry picked from commit 030ede2c957bd4eb2089b07e00d432e3db692788)

--- packages/devkit/src/generators/plugin-migrations/executor-to-plugin-migrator.ts
+++ packages/devkit/src/generators/plugin-migrations/executor-to-plugin-migrator.ts
@@ -343,8 +343,9 @@ class ExecutorToPluginMigrator<T> {
   }
 
   #getTargetDefaultsForExecutor() {
-    this.#targetDefaultsForExecutor =
-      this.#nxJson.targetDefaults?.[this.#executor];
+    this.#targetDefaultsForExecutor = structuredClone(
+      this.#nxJson.targetDefaults?.[this.#executor]
+    );
   }
 
   #getCreatedTargetForProjectRoot(targetName: string, projectRoot: string) {
