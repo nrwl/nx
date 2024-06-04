d2d06cdbab10b272854126defa6819cbef336d9c
Nicholas Cunningham <ndcunningham@gmail.com>
2024-06-04 10:23:42 -0600

    chore(nx-dev): Replace duplicate references to discord icon

--- nx-dev/nx-dev/pages/advent-of-code/index.tsx
+++ nx-dev/nx-dev/pages/advent-of-code/index.tsx
@@ -5,6 +5,7 @@ import {
 } from '@heroicons/react/24/outline';
 import {
   ButtonLink,
+  DiscordIcon,
   Footer,
   Header,
   SectionHeading,
@@ -50,7 +51,7 @@ export default function AdventOfCode(): JSX.Element {
           >
             <article
               id="advent-of-code"
-              className="mx-auto flex max-w-7xl flex-col space-y-12 py-12 px-4 sm:px-6 lg:flex-row lg:items-center lg:space-y-0 lg:space-x-20 lg:py-16 lg:px-8"
+              className="mx-auto flex max-w-7xl flex-col space-y-12 px-4 py-12 sm:px-6 lg:flex-row lg:items-center lg:space-x-20 lg:space-y-0 lg:px-8 lg:py-16"
             >
               <header className="space-y-10 md:py-12 lg:w-5/12 xl:w-5/12">
                 <div>
@@ -66,7 +67,7 @@ export default function AdventOfCode(): JSX.Element {
                     together! We have a convenient TypeScript starter kit
                     available to help you get started right away.
                   </p>
-                  <div className="flex mt-8 gap-4">
+                  <div className="mt-8 flex gap-4">
                     <ButtonLink
                       href="https://github.com/nrwl/ts-aoc-starter#running-the-puzzles"
                       variant="primary"
@@ -84,9 +85,9 @@ export default function AdventOfCode(): JSX.Element {
                     >
                       <button
                         title="Use Nx to setup your Advent of Code workspace"
-                        className="group relative flex w-full items-center rounded-lg border border-slate-200 bg-white py-3 px-6 text-md font-semibold leading-6 transition hover:bg-slate-100 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white dark:border-slate-700 dark:bg-slate-800 dark:text-white dark:hover:bg-slate-700 sm:w-auto"
+                        className="text-md group relative flex w-full items-center rounded-lg border border-slate-200 bg-white px-6 py-3 font-semibold leading-6 transition hover:bg-slate-100 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white sm:w-auto dark:border-slate-700 dark:bg-slate-800 dark:text-white dark:hover:bg-slate-700"
                       >
-                        <span className="absolute top-1 right-1 flex opacity-0 transition-opacity group-hover:opacity-100">
+                        <span className="absolute right-1 top-1 flex opacity-0 transition-opacity group-hover:opacity-100">
                           {copied ? (
                             <ClipboardDocumentCheckIcon className="h-4 w-4" />
                           ) : (
@@ -108,7 +109,7 @@ export default function AdventOfCode(): JSX.Element {
                 <div className="video-responsive flex items-center justify-center">
                   <iframe
                     src={`https://www.youtube.com/embed/cOHIO5UXqMw`}
-                    className="aspect-video w-full rounded-lg shadow-lg mb-1"
+                    className="mb-1 aspect-video w-full rounded-lg shadow-lg"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                     title="Nx Advent of Code"
@@ -122,7 +123,7 @@ export default function AdventOfCode(): JSX.Element {
             {/* <div className="max-w-screen-lg mx-auto"> */}
             <div className="mx-auto max-w-7xl">
               <article className="mx-auto px-4 sm:px-6 lg:items-center lg:px-8">
-                <main className="grid grid-cols-1 gap-5 md:grid-cols-3 pt-10 pb-24">
+                <main className="grid grid-cols-1 gap-5 pb-24 pt-10 md:grid-cols-3">
                   <div className="group relative rounded-lg border border-slate-200 bg-white/60 p-5 transition duration-200 ease-out hover:border-slate-300 dark:border-slate-800/40 dark:bg-slate-800/60 dark:hover:border-slate-900 dark:hover:bg-slate-800">
                     <div className="relative m-2 mb-6 inline-flex h-10 w-10 items-center justify-center">
                       <div className="absolute inset-0 -m-2 rotate-6 transform rounded-3xl bg-slate-300 transition duration-200 ease-out group-hover:-rotate-3 group-hover:scale-105 dark:bg-slate-800" />
@@ -157,16 +158,10 @@ export default function AdventOfCode(): JSX.Element {
                     <div className="relative m-2 mb-6 inline-flex h-10 w-10 items-center justify-center">
                       <div className="absolute inset-0 -m-2 rotate-6 transform rounded-3xl bg-indigo-300 transition duration-200 ease-out group-hover:-rotate-3 group-hover:scale-105 dark:bg-indigo-900" />
                       <div className="absolute inset-0 -rotate-6 transform rounded-2xl bg-[#5865F2] bg-opacity-75 shadow-inner transition duration-200 ease-out group-hover:rotate-2 group-hover:scale-105" />
-                      <svg
-                        fill="currentColor"
+                      <DiscordIcon
+                        showTitle={true}
                         className="relative inline-block h-5 w-5 transform text-white transition duration-200 ease-out group-hover:scale-110"
-                        role="img"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <title>Discord</title>
-                        <path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z" />
-                      </svg>
+                      />
                     </div>
                     <h4 className="mb-2 text-lg font-bold">
                       Join us on Discord
