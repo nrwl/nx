1170b4e73ab0cbb2bea1595192701ff6f5371f6b
FrozenPandaz <jasonjean1993@gmail.com>
2024-06-05 18:50:50 -0400

    chore(repo): debug

--- e2e/utils/create-project-utils.ts
+++ e2e/utils/create-project-utils.ts
@@ -104,14 +104,6 @@ export function newProject({
         createNxWorkspaceEnd.name
       );
 
-      if (unsetProjectNameAndRootFormat) {
-        console.warn(
-          'ATTENTION: The workspace generated for this e2e test does not use the new as-provided project name/root format. Please update this test'
-        );
-        createFile('apps/.gitkeep');
-        createFile('libs/.gitkeep');
-      }
-
       // Temporary hack to prevent installing with `--frozen-lockfile`
       if (isCI && packageManager === 'pnpm') {
         updateFile(
@@ -120,19 +112,6 @@ export function newProject({
         );
       }
 
-      if (!packages) {
-        console.warn(
-          'ATTENTION: All packages are installed into the new workspace. To make this test faster, please pass the subset of packages that this test needs by passing a packages array in the options'
-        );
-      }
-      const packageInstallStart = performance.mark('packageInstall:start');
-      packageInstall((packages ?? nxPackages).join(` `), projScope);
-      const packageInstallEnd = performance.mark('packageInstall:end');
-      packageInstallMeasure = performance.measure(
-        'packageInstall',
-        packageInstallStart.name,
-        packageInstallEnd.name
-      );
       // stop the daemon
       execSync(`${getPackageManagerCommand().runNx} reset`, {
         cwd: `${e2eCwd}/proj`,
@@ -145,6 +124,21 @@ export function newProject({
 
     const projectDirectory = tmpProjPath();
     copySync(`${tmpBackupProjPath()}`, `${projectDirectory}`);
+
+    if (unsetProjectNameAndRootFormat) {
+      console.warn(
+        'ATTENTION: The workspace generated for this e2e test does not use the new as-provided project name/root format. Please update this test'
+      );
+      createFile('apps/.gitkeep');
+      createFile('libs/.gitkeep');
+    }
+
+    if (!packages) {
+      console.warn(
+        'ATTENTION: All packages are installed into the new workspace. To make this test faster, please pass the subset of packages that this test needs by passing a packages array in the options'
+      );
+    }
+    const packageInstallStart = performance.mark('packageInstall:start');
 
     const dependencies = readJsonFile(
       `${projectDirectory}/package.json`
@@ -163,6 +157,12 @@ export function newProject({
         encoding: 'utf-8',
       });
     }
+    const packageInstallEnd = performance.mark('packageInstall:end');
+    packageInstallMeasure = performance.measure(
+      'packageInstall',
+      packageInstallStart.name,
+      packageInstallEnd.name
+    );
 
     const newProjectEnd = performance.mark('new-project:end');
     const perfMeasure = performance.measure(
@@ -171,12 +171,11 @@ export function newProject({
       newProjectEnd.name
     );
 
-    if (isVerbose()) {
-      logInfo(
-        `NX`,
-        `E2E created a project: ${projectDirectory} in ${
-          perfMeasure.duration / 1000
-        } seconds
+    logInfo(
+      `NX`,
+      `E2E created a project: ${projectDirectory} in ${
+        perfMeasure.duration / 1000
+      } seconds
 ${
   createNxWorkspaceMeasure
     ? `create-nx-workspace: ${
@@ -184,14 +183,11 @@ ${
       } seconds\n`
     : ''
 }${
-          packageInstallMeasure
-            ? `packageInstall: ${
-                packageInstallMeasure.duration / 1000
-              } seconds\n`
-            : ''
-        }`
-      );
-    }
+        packageInstallMeasure
+          ? `packageInstall: ${packageInstallMeasure.duration / 1000} seconds\n`
+          : ''
+      }`
+    );
 
     if (process.env.NX_E2E_EDITOR) {
       const editor = process.env.NX_E2E_EDITOR;
