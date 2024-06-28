dc470cbca02955e8ea9e27e91ae0f1318d1e9448
FrozenPandaz <jasonjean1993@gmail.com>
2024-06-28 16:40:42 -0400

    Revert "chore(repo): update nx to 19.4.0-rc.0 (#26728)"
    This reverts commit a8742539e6263c30dcbd02bc77eb5d14523a7292.

--- jest.preset.js
+++ jest.preset.js
@@ -1,5 +1,4 @@
-// Ensure that the preset loads from node_modules rather than our local typescript source
-const nxPreset = require('./node_modules/@nx/jest/preset').default;
+const nxPreset = require('@nx/jest/preset').default;
 
 module.exports = {
   ...nxPreset,
