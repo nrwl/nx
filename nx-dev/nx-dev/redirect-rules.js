07c87df3ac92e2c9e1576b21783460998d14e11f
Rares Matei <matei.rar@gmail.com>
2024-07-10 13:39:06 +0100

    docs(nx-cloud): use ci redirect

--- nx-dev/nx-dev/redirect-rules.js
+++ nx-dev/nx-dev/redirect-rules.js
@@ -418,9 +418,9 @@ const nxCloudUrls = {
   '/nx-cloud/set-up/github':
     '/nx-cloud/recipes/source-control-integration/github',
   '/nx-cloud/set-up/bitbucket-cloud':
-    '/nx-cloud/recipes/source-control-integration/bitbucket',
+    '/ci/recipes/source-control-integration/bitbucket',
   '/nx-cloud/recipes/source-control-integration/bitbucket-cloud':
-    '/nx-cloud/recipes/source-control-integration/bitbucket',
+    '/ci/recipes/source-control-integration/bitbucket',
   '/nx-cloud/set-up/gitlab':
     '/nx-cloud/recipes/source-control-integration/gitlab',
   '/core-features/remote-cache': '/ci/features/remote-cache',
