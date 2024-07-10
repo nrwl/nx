e01caf16ef83db520a30c0a56da3b5cc9634b3c1
Nicholas Cunningham <ndcunningham@gmail.com>
2024-07-09 18:45:18 -0600

    feat(nx-dev): Add additional logos

--- nx-dev/ui-customers/src/lib/enterprise-customers.tsx
+++ nx-dev/ui-customers/src/lib/enterprise-customers.tsx
@@ -11,6 +11,26 @@ import {
   FicoIcon,
   HiltonIcon,
   FedExIcon,
+  ParamountIcon,
+  AdobeIcon,
+  AddidasIcon,
+  IntelIcon,
+  IkeaIcon,
+  DeloitteIcon,
+  VodafoneIcon,
+  TMobileIcon,
+  MailChimpIcon,
+  ModernaIcon,
+  ClickUpIcon,
+  SharpIcon,
+  RedBullIcon,
+  LegoIcon,
+  PhilipsIcon,
+  BloombergIcon,
+  SainsburysIcon,
+  TideIcon,
+  UkgIcon,
+  SpliceIcon,
 } from '@nx/nx-dev/ui-common';
 
 export function EnterpriseCustomers(): JSX.Element {
@@ -114,6 +134,166 @@ export function EnterpriseCustomers(): JSX.Element {
               className="flex items-center justify-center border-b border-r border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
             >
               <ShopifyIcon aria-hidden="true" className="h-12 w-12" />
+            </a>
+            <a
+              href="https://www.paramountplus.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-x border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <ParamountIcon aria-hidden="true" className="h-14 w-14" />
+            </a>
+            <a
+              href="https://www.adobe.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <AdobeIcon aria-hidden="true" className="h-14 w-14" />
+            </a>
+            <a
+              href="https://www.adidas.com"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-x border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <AddidasIcon aria-hidden="true" className="h-14 w-14" />
+            </a>
+            <a
+              href="https://www.intel.com"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-b border-r border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <IntelIcon aria-hidden="true" className="h-16 w-16" />
+            </a>
+            <a
+              href="https://www.ikea.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-x border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <IkeaIcon aria-hidden="true" className="h-20 w-20" />
+            </a>
+            <a
+              href="https://www.deloitte.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <DeloitteIcon aria-hidden="true" className="h-28 w-28" />
+            </a>
+            <a
+              href="https://www.vodafone.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-x border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <VodafoneIcon aria-hidden="true" className="h-12 w-12" />
+            </a>
+            <a
+              href="https://www.t-mobile.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-b border-r border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <TMobileIcon aria-hidden="true" className="h-10 w-10" />
+            </a>
+            <a
+              href="https://mailchimp.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-x border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <MailChimpIcon aria-hidden="true" className="h-14 w-14" />
+            </a>
+            <a
+              href="https://www.modernatx.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <ModernaIcon aria-hidden="true" className="h-32 w-32" />
+            </a>
+            <a
+              href="https://clickup.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-x border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <ClickUpIcon aria-hidden="true" className="h-12 w-12" />
+            </a>
+            <a
+              href="https://global.sharp/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-b border-r border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <SharpIcon aria-hidden="true" className="h-28 w-28" />
+            </a>
+            <a
+              href="https://www.redbull.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-x border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <RedBullIcon aria-hidden="true" className="h-24 w-24" />
+            </a>
+            <a
+              href="https://www.lego.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <LegoIcon aria-hidden="true" className="h-24 w-24" />
+            </a>
+            <a
+              href="https://www.philips.ca/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-x border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <PhilipsIcon aria-hidden="true" className="h-24 w-24" />
+            </a>
+            <a
+              href="https://www.bloomberg.com"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-b border-r border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <BloombergIcon aria-hidden="true" className="h-10 w-10" />
+            </a>
+            <a
+              href="https://www.sainsburys.co.uk/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-x border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <SainsburysIcon aria-hidden="true" className="h-32 w-32" />
+            </a>
+            <a
+              href="https://splice.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <SpliceIcon aria-hidden="true" className="h-14 w-14" />
+            </a>
+            <a
+              href="https://www.tide.com/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-x border-b border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <TideIcon aria-hidden="true" className="h-20 w-20" />
+            </a>
+            <a
+              href="https://www.ukg.ca/"
+              rel="noreferrer"
+              target="_blank"
+              className="flex items-center justify-center border-b border-r border-slate-200/20 p-12 transition hover:bg-slate-100/20 dark:border-slate-800/20 dark:hover:border-slate-600/20 dark:hover:bg-slate-600/10"
+            >
+              <UkgIcon aria-hidden="true" className="h-20 w-20" />
             </a>
           </div>
         </div>
