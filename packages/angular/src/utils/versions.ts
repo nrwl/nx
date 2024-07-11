4de4ca9de593a985068d2a3bde48aed2cbc16804
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-07-11 12:24:52 +0200

    feat(angular): support typescript 5.5 (#26897)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    <!-- This is the behavior we have today -->
    
    <!-- This is the behavior we should expect with the changes in this PR
    -->
    
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    <!-- Fixes NXP-850 -->
    
    Fixes #

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -18,7 +18,7 @@ export const moduleFederationNodeVersion = '^2.3.0';
 export const moduleFederationEnhancedVersion = '~0.2.3';
 
 export const angularEslintVersion = '^18.0.1';
-export const typescriptEslintVersion = '^8.0.0-alpha.28';
+export const typescriptEslintVersion = '^7.16.0';
 export const tailwindVersion = '^3.0.2';
 export const postcssVersion = '^8.4.5';
 export const postcssUrlVersion = '~10.1.3';
