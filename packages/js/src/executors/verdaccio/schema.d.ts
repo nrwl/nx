382b90f2d99020da0ac463a208050e33a21e1652
William van der Vegt <william.van.der.vegt@moba.net>
2024-07-18 13:09:30 +0200

    feat(js): added a verdaccio listen address to configuration
    Added a Verdaccio listen address to the configuration so that you are able to configure the listen address of Verdaccio.

--- packages/js/src/executors/verdaccio/schema.d.ts
+++ packages/js/src/executors/verdaccio/schema.d.ts
@@ -2,6 +2,7 @@ export interface VerdaccioExecutorSchema {
   location: 'global' | 'user' | 'project' | 'none';
   storage?: string;
   port?: number;
+  listenAddress?: string;
   config?: string;
   clear?: boolean;
 }
