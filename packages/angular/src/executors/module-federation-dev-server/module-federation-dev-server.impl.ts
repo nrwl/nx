77cdd77c47f682a32a04923ea71c7f820604f4aa
Jack Hsu <jack.hsu@gmail.com>
2024-07-19 10:39:32 -0400

    fix(module-federation): do not cache assets from static serve (#27005)
    We're currently caching files for an hour when serving the host with
    static remotes. This PR fixes the issue by setting `cacheSeconds: -1` on
    the static server (disables cache).
    
    This affects development only.
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    ## Current Behavior
    <!-- This is the behavior we have today -->
    
    ## Expected Behavior
    <!-- This is the behavior we should expect with the changes in this PR
    -->
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #
    
    (cherry picked from commit 8b1c78caa56e391f7e795798be4d0626a5a06630)

--- packages/angular/src/executors/module-federation-dev-server/module-federation-dev-server.impl.ts
+++ packages/angular/src/executors/module-federation-dev-server/module-federation-dev-server.impl.ts
@@ -56,6 +56,7 @@ export async function* moduleFederationDevServerExecutor(
           spa: false,
           withDeps: false,
           cors: true,
+          cacheSeconds: -1,
         },
         context
       )
