184e83ad5897811a626af2c0f5679a77496bf378
MaxKless <34165455+MaxKless@users.noreply.github.com>
2024-07-23 16:52:14 +0200

    feat(core): error when running atomized tasks outside of DTE (#26898)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    ## Current Behavior
    Nx allows running atomized tasks (like `e2e-ci`) anywhere.
    
    ## Expected Behavior
    Nx allows running atomized tasks (like `e2e-ci`) only in distributed
    environments like Nx Agents or DTE. We'll leave that actual check to nx
    cloud but in the default runner, we'll throw an error.
    It's possible to escape from this with an env var.

--- packages/nx/src/tasks-runner/task-graph-utils.spec.ts
+++ packages/nx/src/tasks-runner/task-graph-utils.spec.ts
@@ -1,4 +1,19 @@
-import { findCycle, makeAcyclic } from './task-graph-utils';
+import '../internal-testing-utils/mock-fs';
+
+import { vol } from 'memfs';
+
+import { join } from 'path';
+import {
+  findCycle,
+  makeAcyclic,
+  validateNoAtomizedTasks,
+} from './task-graph-utils';
+import { ensureDirSync, removeSync, writeFileSync } from 'fs-extra';
+import { tmpdir } from 'os';
+import { workspaceRoot } from '../utils/workspace-root';
+import { cacheDir } from '../utils/cache-directory';
+import { Task } from '../config/task-graph';
+import { ProjectGraph } from '../config/project-graph';
 
 describe('task graph utils', () => {
   describe('findCycles', () => {
@@ -55,6 +70,134 @@ describe('task graph utils', () => {
         e: [],
       });
       expect(graph.roots).toEqual(['d', 'e']);
+    });
+  });
+
+  describe('validateNoAtomizedTasks', () => {
+    let mockProcessExit: jest.SpyInstance;
+    let env: NodeJS.ProcessEnv;
+
+    beforeEach(() => {
+      env = process.env;
+      process.env = {};
+
+      mockProcessExit = jest
+        .spyOn(process, 'exit')
+        .mockImplementation((code: number) => {
+          return undefined as any as never;
+        });
+    });
+
+    afterEach(() => {
+      process.env = env;
+      vol.reset();
+      mockProcessExit.mockRestore();
+    });
+
+    it('should do nothing if no tasks are atomized', () => {
+      const taskGraph = {
+        tasks: {
+          'e2e:e2e': {
+            id: 'e2e:e2e',
+            target: {
+              project: 'e2e',
+              target: 'e2e',
+            },
+          },
+        },
+      };
+      const projectGraph = {
+        nodes: {
+          e2e: {
+            data: {
+              targets: {
+                e2e: {},
+              },
+            },
+          },
+        },
+      };
+      validateNoAtomizedTasks(taskGraph as any, projectGraph as any);
+      expect(mockProcessExit).not.toHaveBeenCalled();
+    });
+
+    it('should exit if atomized task is present', () => {
+      const taskGraph = {
+        tasks: {
+          'e2e:e2e-ci': {
+            id: 'e2e:e2e-ci',
+            target: {
+              project: 'e2e',
+              target: 'e2e-ci',
+            },
+          },
+        },
+      };
+      const projectGraph = {
+        nodes: {
+          e2e: {
+            data: {
+              targets: {
+                'e2e-ci': {
+                  metadata: {
+                    nonAtomizedTarget: 'e2e',
+                  },
+                },
+              },
+            },
+          },
+        },
+      };
+      validateNoAtomizedTasks(taskGraph as any, projectGraph as any);
+      expect(mockProcessExit).toHaveBeenCalledWith(1);
+    });
+    it('should exit if multiple atomized tasks are present', () => {
+      const taskGraph = {
+        tasks: {
+          'e2e:e2e-ci': {
+            id: 'e2e:e2e-ci',
+            target: {
+              project: 'e2e',
+              target: 'e2e-ci',
+            },
+          },
+          'gradle:test-ci': {
+            id: 'gradle:test-ci',
+            target: {
+              project: 'gradle',
+              target: 'test-ci',
+            },
+          },
+        },
+      };
+      const projectGraph = {
+        nodes: {
+          e2e: {
+            data: {
+              targets: {
+                'e2e-ci': {
+                  metadata: {
+                    nonAtomizedTarget: 'e2e',
+                  },
+                },
+              },
+            },
+          },
+          gradle: {
+            data: {
+              targets: {
+                'test-ci': {
+                  metadata: {
+                    nonAtomizedTarget: 'test',
+                  },
+                },
+              },
+            },
+          },
+        },
+      };
+      validateNoAtomizedTasks(taskGraph as any, projectGraph as any);
+      expect(mockProcessExit).toHaveBeenCalledWith(1);
     });
   });
 });
