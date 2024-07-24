450a528825222c0f8f38873c62fa80bd07cfc76b
Phillip Barta <barta.phillip@gmail.com>
2024-07-24 17:56:27 +0200

    cleanup(core): refactor nx list command to remove unused code and code duplication (#21748)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    ## Current Behavior
    <!-- This is the behavior we have today -->
    the code for the `nx list` command has unused and duplicated code.
    
    ## Expected Behavior
    <!-- This is the behavior we should expect with the changes in this PR
    -->
    no unused code and code duplication
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #
    
    ---------
    
    Co-authored-by: Craigory Coppola <craigorycoppola@gmail.com>
    (cherry picked from commit aeec5cc31a85d7698a2cd7d8bf2016a7185b9377)

--- packages/nx/src/utils/plugins/installed-plugins.ts
+++ packages/nx/src/utils/plugins/installed-plugins.ts
@@ -1,15 +1,14 @@
-import * as chalk from 'chalk';
-import { output } from '../output';
-import type { PluginCapabilities } from './models';
-import { getPluginCapabilities } from './plugin-capabilities';
-import { hasElements } from './shared';
-import { readJsonFile } from '../fileutils';
-import { PackageJson, readModulePackageJson } from '../package-json';
-import { workspaceRoot } from '../workspace-root';
 import { join } from 'path';
 import { readNxJson } from '../../config/nx-json';
-import { getNxRequirePaths } from '../installation-directory';
 import { ProjectConfiguration } from '../../config/workspace-json-project-json';
+import { readJsonFile } from '../fileutils';
+import { getNxRequirePaths } from '../installation-directory';
+import { PackageJson, readModulePackageJson } from '../package-json';
+import { workspaceRoot } from '../workspace-root';
+import {
+  PluginCapabilities,
+  getPluginCapabilities,
+} from './plugin-capabilities';
 
 export function findInstalledPlugins(): PackageJson[] {
   const packageJsonDeps = getDependenciesFromPackageJson();
@@ -91,32 +90,4 @@ export async function getInstalledPluginsAndCapabilities(
   }
 
   return result;
-}
-
-export function listInstalledPlugins(
-  installedPlugins: Map<string, PluginCapabilities>
-) {
-  const bodyLines: string[] = [];
-
-  for (const [, p] of installedPlugins) {
-    const capabilities = [];
-    if (hasElements(p.executors)) {
-      capabilities.push('executors');
-    }
-    if (hasElements(p.generators)) {
-      capabilities.push('generators');
-    }
-    if (p.projectGraphExtension) {
-      capabilities.push('graph-extensions');
-    }
-    if (p.projectInference) {
-      capabilities.push('project-inference');
-    }
-    bodyLines.push(`${chalk.bold(p.name)} (${capabilities.join()})`);
-  }
-
-  output.log({
-    title: `Installed plugins:`,
-    bodyLines: bodyLines,
-  });
 }
