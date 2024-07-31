548b689fa16301cda2ea68e8c364d7a263f28ffe
Nicholas Cunningham <ndcunningham@gmail.com>
2024-07-31 07:48:42 -0600

    fix(nx-dev): OG image meta tag (#27195)

--- nx-dev/data-access-documents/src/lib/blog.api.ts
+++ nx-dev/data-access-documents/src/lib/blog.api.ts
@@ -1,5 +1,5 @@
-import { readFileSync } from 'fs';
-import { join, basename } from 'path';
+import { readFileSync, readdirSync, accessSync, constants } from 'fs';
+import { join, basename, parse, resolve } from 'path';
 import { extractFrontmatter } from '@nx/nx-dev/ui-markdoc';
 import { sortPosts } from './blog.util';
 import { BlogPostDataEntry } from './blog.model';
@@ -48,6 +48,8 @@ export class BlogApi {
       const content = await readFile(filePath, 'utf8');
       const frontmatter = extractFrontmatter(content);
       const slug = this.calculateSlug(filePath, frontmatter);
+      const { image, type } = this.determineOgImage(frontmatter.cover_image);
+
       const post = {
         content,
         title: frontmatter.title ?? null,
@@ -62,6 +64,8 @@ export class BlogApi {
         tags: frontmatter.tags ?? [],
         reposts: frontmatter.reposts ?? [],
         pinned: frontmatter.pinned ?? false,
+        ogImage: image,
+        ogImageType: type,
         filePath,
         slug,
       };
@@ -73,6 +77,59 @@ export class BlogApi {
       }
     }
     return sortPosts(allPosts);
+  }
+
+  getBlogPosts(): BlogPostDataEntry[] {
+    const files: string[] = readdirSync(this.options.blogRoot);
+    const authors = JSON.parse(
+      readFileSync(join(this.options.blogRoot, 'authors.json'), 'utf8')
+    );
+    const allPosts: BlogPostDataEntry[] = [];
+
+    for (const file of files) {
+      const filePath = join(this.options.blogRoot, file);
+      // filter out directories (e.g. images)
+      if (!filePath.endsWith('.md')) continue;
+
+      const content = readFileSync(filePath, 'utf8');
+      const frontmatter = extractFrontmatter(content);
+      const slug = this.calculateSlug(filePath, frontmatter);
+      const { image, type } = this.determineOgImage(frontmatter.cover_image);
+      const post = {
+        content,
+        title: frontmatter.title ?? null,
+        description: frontmatter.description ?? null,
+        authors: authors.filter((author) =>
+          frontmatter.authors.includes(author.name)
+        ),
+        date: this.calculateDate(file, frontmatter),
+        cover_image: frontmatter.cover_image
+          ? `/documentation${frontmatter.cover_image}` // Match the prefix used by markdown parser
+          : null,
+        tags: frontmatter.tags ?? [],
+        reposts: frontmatter.reposts ?? [],
+        pinned: frontmatter.pinned ?? false,
+        ogImage: image,
+        ogImageType: type,
+        filePath,
+        slug,
+      };
+
+      if (!frontmatter.draft || process.env.NODE_ENV === 'development') {
+        allPosts.push(post);
+      }
+    }
+
+    return sortPosts(allPosts);
+  }
+
+  getBlogPost(slug: string): BlogPostDataEntry {
+    const blogs = this.getBlogPosts();
+    const blog = blogs.find((b) => b.slug === slug);
+    if (!blog) {
+      throw new Error(`Could not find blog post with slug: ${slug}`);
+    }
+    return blog;
   }
 
   // Optimize this so we don't read the FS multiple times
@@ -102,5 +159,51 @@ export class BlogApi {
         throw new Error(`Could not parse date from filename: ${filename}`);
       }
     }
+  }
+
+  private fileExists(filePath: string): boolean {
+    try {
+      accessSync(filePath, constants.F_OK);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  private determineOgImage(imagePath: string): {
+    image: string;
+    type: string;
+  } {
+    const allowedExtensions = ['.png', '.webp', '.jpg', '.jpeg'];
+    const defaultImage = 'https://nx.dev/socials/nx-media.png';
+    const defaultType = 'png';
+
+    if (!imagePath) {
+      return { image: defaultImage, type: defaultType };
+    }
+    const { ext } = parse(imagePath);
+
+    if (!allowedExtensions.includes(ext)) {
+      const foundExt = allowedExtensions.find((allowedExt) => {
+        const ogImagePath = imagePath.replace(ext, allowedExt);
+        return this.fileExists(
+          join(
+            'public',
+            'documentation',
+            resolve(this.options.blogRoot, ogImagePath)
+          )
+        );
+      });
+
+      if (!foundExt) {
+        return { image: defaultImage, type: defaultType };
+      }
+
+      return {
+        image: imagePath.replace(ext, foundExt),
+        type: foundExt.replace('.', ''),
+      };
+    }
+    return { image: imagePath, type: ext.replace('.', '') };
   }
 }
