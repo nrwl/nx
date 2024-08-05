197db569aea77b57f370503c860216a39b38adcc
Younes Jaaidi <younes@marmicode.io>
2024-08-06 00:10:42 +0200

    chore(angular): generate test-setup.ts

--- packages/angular/src/generators/application/application.spec.ts
+++ packages/angular/src/generators/application/application.spec.ts
@@ -706,12 +706,14 @@ describe('app', () => {
         ).toMatchSnapshot();
       });
 
-      it.skip('🚧 should generate src/test-setup.ts', async () => {
+      it('should generate src/test-setup.ts', async () => {
         await generateApp(appTree, 'my-app', {
           unitTestRunner: UnitTestRunner.Vitest,
         });
 
-        expect(appTree.read('my-app/src/test-setup.ts')).toMatchSnapshot();
+        expect(
+          appTree.read('my-app/src/test-setup.ts', 'utf-8')
+        ).toMatchSnapshot();
       });
 
       it.skip('🚧 should add tsconfig.spec.json', async () => {
