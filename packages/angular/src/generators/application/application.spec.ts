d612b606cd26b98750a25b80a49a15f437924450
Younes Jaaidi <younes@marmicode.io>
2024-08-06 00:02:07 +0200

    chore(angular): generate vite.config.ts

--- packages/angular/src/generators/application/application.spec.ts
+++ packages/angular/src/generators/application/application.spec.ts
@@ -696,20 +696,14 @@ describe('app', () => {
     });
 
     describe('vitest', () => {
-      it.skip('🚧 should generate vite.config.ts', async () => {
+      it('should generate vite.config.ts', async () => {
         await generateApp(appTree, 'my-app', {
           unitTestRunner: UnitTestRunner.Vitest,
         });
 
-        expect(appTree.read('my-app/vite.config.ts')).toMatchSnapshot();
-      });
-
-      it.skip('🚧 should generate vitest.config.ts', async () => {
-        await generateApp(appTree, 'my-app', {
-          unitTestRunner: UnitTestRunner.Vitest,
-        });
-
-        expect(appTree.read('my-app/vitest.config.ts')).toMatchSnapshot();
+        expect(
+          appTree.read('my-app/vite.config.ts', 'utf-8')
+        ).toMatchSnapshot();
       });
 
       it.skip('🚧 should generate src/test-setup.ts', async () => {
@@ -725,7 +719,9 @@ describe('app', () => {
           unitTestRunner: UnitTestRunner.Vitest,
         });
 
-        expect(appTree.read('my-app/tsconfig.spec.json')).toMatchSnapshot();
+        expect(
+          appTree.read('my-app/tsconfig.spec.json', 'utf-8')
+        ).toMatchSnapshot();
       });
 
       it.skip('🚧 should add a reference to tsconfig.spec.json in tsconfig.json', async () => {
