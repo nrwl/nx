df388700cb02889ba657dbec82cf01bace59800c
Craigory Coppola <craigorycoppola@gmail.com>
2024-08-06 13:53:16 -0400

    fix(core): allow isolated plugins to shut themselves down

--- packages/nx/src/project-graph/plugins/isolation/plugin-pool.ts
+++ packages/nx/src/project-graph/plugins/isolation/plugin-pool.ts
@@ -53,8 +53,8 @@ export async function loadRemoteNxPlugin(
 
   const cleanupFunction = () => {
     worker.off('exit', exitHandler);
+    shutdownPluginWorker(socket);
     socket.destroy();
-    shutdownPluginWorker(worker);
     nxPluginWorkerCache.delete(cacheKey);
   };
 
@@ -94,13 +94,8 @@ export async function loadRemoteNxPlugin(
   return [pluginPromise, cleanupFunction];
 }
 
-function shutdownPluginWorker(worker: ChildProcess) {
-  // Clears the plugin cache so no refs to the workers are held
-  nxPluginCache.clear();
-
-  // logger.verbose(`[plugin-pool] starting worker shutdown`);
-
-  worker.kill('SIGINT');
+function shutdownPluginWorker(socket: Socket) {
+  sendMessageOverSocket(socket, { type: 'shutdown', payload: {} });
 }
 
 /**
@@ -249,6 +244,7 @@ function createWorkerExitHandler(
 
 let cleanedUp = false;
 const exitHandler = () => {
+  nxPluginCache.clear();
   for (const fn of cleanupFunctions) {
     fn();
   }
