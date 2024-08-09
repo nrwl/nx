f64c775c8bc22fd8866dc08c48eb07798a06ad99
Wei Liang <liang2010@hotmail.my>
2024-08-09 18:17:17 +0800

    feat(core): add shutdown lifecycle hook to node executor
    To execute application before shutdown events with asynchronous functions and prevent nx shutdown immediately. Espically for NestJS lifecycle events https://docs.nestjs.com/fundamentals/lifecycle-events.
    
    fixes #9237 and #18037

--- packages/js/src/executors/node/node-with-require-overrides.ts
+++ packages/js/src/executors/node/node-with-require-overrides.ts
@@ -1,6 +1,40 @@
 const Module = require('module');
 const url = require('node:url');
 const originalLoader = Module._load;
+const { logger } = require('@nx/devkit');
+const readline = require('readline');
+
+// To overrides the `process.on` method to prepend the app listener to the event listeners list.
+process.on = (eventName, listener) => {
+  process.prependListener(eventName, listener);
+  return process;
+};
+
+// To overrides the `process.emit` method to execute the listeners in asynchronously.
+// @ts-expect-error To ensure process.emit function is running in asynchronously
+process.emit = async (eventName, ...args) => {
+  const listeners = process.listeners(eventName);
+
+  for (const listener of listeners) {
+    // @ts-expect-error Making async listener to execute sequentially
+    await listener(...args);
+  }
+
+  return true;
+};
+
+readline.emitKeypressEvents(process.stdin);
+
+if (process.stdin.isTTY) process.stdin.setRawMode(true);
+
+process.stdin.on('keypress', async (chunk, key) => {
+  if (key && key.ctrl && key.name === 'c') {
+    process.stdin.setRawMode(false); // To ensure nx terminal is not stuck in raw mode
+    process.emit('SIGINT', 'SIGINT');
+  }
+});
+
+logger.info('To exit the process with SIGINT, press Ctrl+C');
 
 const dynamicImport = new Function('specifier', 'return import(specifier)');
 
