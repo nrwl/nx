c0f5c157abfe07d15ffcc056739236673a69bdf6
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-08-14 22:33:33 +0200

    feat(angular): support angular v18.2.0 (#27379)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    ## Current Behavior
    <!-- This is the behavior we have today -->
    
    ## Expected Behavior
    <!-- This is the behavior we should expect with the changes in this PR
    -->
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #

--- packages/angular/src/executors/utilities/esbuild-extensions.ts
+++ packages/angular/src/executors/utilities/esbuild-extensions.ts
@@ -1,6 +1,12 @@
 import { registerTsProject } from '@nx/js/src/internal';
-import type { Plugin } from 'esbuild';
 import { loadModule } from './module-loader';
+
+// This is a workaround to make sure we use the same esbuild version as the
+// Angular DevKit uses. This is only used internally to load the plugins and
+// forward them to the Angular DevKit builders.
+type Plugin = Parameters<
+  typeof import('@angular-devkit/build-angular').buildApplication
+>[2]['codePlugins'][number];
 
 export type PluginSpec = {
   path: string;
