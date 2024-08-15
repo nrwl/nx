2f4fc5ef9a9ffa6a222d62ba3eb46824e77feb19
Colum Ferry <cferry09@gmail.com>
2024-08-15 12:25:24 +0100

    feat(vite): add util for determining the e2e web server info

---
+++ packages/vite/src/utils/e2e-web-server-info-utils.ts
@@ -0,0 +1,26 @@
+import { type Tree, getE2EWebServerInfo } from '@nx/devkit';
+
+export async function getViteWebServerInfo(
+  tree: Tree,
+  projectName: string,
+  configFilePath: string,
+  isPluginBeingAdded: boolean
+) {
+  return getE2EWebServerInfo(
+    tree,
+    projectName,
+    {
+      plugin: '@nx/vite/plugin',
+      serveTargetName: 'serveTargetName',
+      serveStaticTargetName: 'previewTargetName',
+      configFilePath,
+    },
+    {
+      defaultServeTargetName: 'serve',
+      defaultServeStaticTargetName: 'preview',
+      defaultE2EWebServerAddress: 'http://localhost:4200',
+      defaultE2ECiBaseUrl: 'http://localhost:4300',
+    },
+    isPluginBeingAdded
+  );
+}
