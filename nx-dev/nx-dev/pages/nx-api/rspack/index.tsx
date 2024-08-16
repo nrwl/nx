9dca7c7025e7e1271bd4c2fef8f1e4c6d2aae560
Jack Hsu <jack.hsu@gmail.com>
2024-08-16 12:25:20 -0400

    docs(core): add scroll_25, scroll_50, scroll_75, and scroll_90 events to track engagement (#27461)
    This PR adds events to track engagement in our docs. Since we use a
    scrollable `<div>` in our docs, the normal `scroll` events in GA do not
    work.
    
    A new `<ScrollableContent>` component is added that will do two things:
    - Send `scroll_25`, `scroll_50`, `scroll_75`, and `scroll_90` events
    whenever the user scrolls to 25%, 50%, 75%, of 90% of the content
    - Optionally reset scroll top to zero whenever router changes (existing
    behavior)
    
    All of the places where we have content in a scrollable `<div>` is
    replaced with `<ScrollableContent>`.
    
    Note: 90% means user has reached the bottom, since it's not usually
    possible to get to 100%.

--- nx-dev/nx-dev/pages/nx-api/rspack/index.tsx
+++ nx-dev/nx-dev/pages/nx-api/rspack/index.tsx
@@ -4,12 +4,11 @@ import { sortCorePackagesFirst } from '@nx/nx-dev/data-access-packages';
 import { Menu, MenuItem, MenuSection } from '@nx/nx-dev/models-menu';
 import { ProcessedPackageMetadata } from '@nx/nx-dev/models-package';
 import { DocumentationHeader, SidebarContainer } from '@nx/nx-dev/ui-common';
-import { useRouter } from 'next/router';
-import { useEffect, useRef } from 'react';
 import { menusApi } from '../../../lib/menus.api';
 import { useNavToggle } from '../../../lib/navigation-toggle.effect';
 import { content } from '../../../lib/rspack/content/overview';
 import { pkg } from '../../../lib/rspack/pkg';
+import { ScrollableContent } from '@nx/ui-scrollable-content';
 
 export default function RspackIndex({
   overview,
@@ -20,25 +19,7 @@ export default function RspackIndex({
   overview: string;
   pkg: ProcessedPackageMetadata;
 }): JSX.Element {
-  const router = useRouter();
   const { toggleNav, navIsOpen } = useNavToggle();
-  const wrapperElement = useRef(null);
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      if (url.includes('#')) return;
-      if (!wrapperElement) return;
-
-      (wrapperElement as any).current.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth',
-      });
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => router.events.off('routeChangeComplete', handleRouteChange);
-  }, [router, wrapperElement]);
 
   const vm: { menu: Menu; package: ProcessedPackageMetadata } = {
     menu: {
@@ -71,14 +52,9 @@ export default function RspackIndex({
           navIsOpen={navIsOpen}
           toggleNav={toggleNav}
         />
-        <div
-          ref={wrapperElement}
-          id="wrapper"
-          data-testid="wrapper"
-          className="relative flex flex-grow flex-col items-stretch justify-start overflow-y-scroll"
-        >
+        <ScrollableContent resetScrollOnNavigation={true}>
           <PackageSchemaList pkg={vm.package} overview={overview} />
-        </div>
+        </ScrollableContent>
       </main>
     </div>
   );
