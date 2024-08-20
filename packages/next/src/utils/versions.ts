142d0a28fc1e618dd08410701e499f8ef7893881
Nicholas Cunningham <ndcunningham@gmail.com>
2024-08-20 11:53:24 -0600

    fix(nextjs): Should be able to run custom server targets with swc (#27526)
    ## Currently
    When we installed swc from the Next.js generator it installed an older
    version that expected version of swc this caused the node & build
    generator to not work.
    
    ## Expected
    With these changes the version will be in sync with what the
    `@nx/js:swc` executor is expecting. With that in mind the build should
    now work as expected and by extension the serve target for custom server
    as well.
    
    fixes: #27222
    (cherry picked from commit 554e979abfaef3045f98d5bae55843df87776a4d)

--- packages/next/src/utils/versions.ts
+++ packages/next/src/utils/versions.ts
@@ -7,8 +7,3 @@ export const lessLoader = '11.1.0';
 export const emotionServerVersion = '11.11.0';
 export const babelPluginStyledComponentsVersion = '1.10.7';
 export const tsLibVersion = '^2.3.0';
-
-export const swcCliVersion = '~0.1.62';
-export const swcCoreVersion = '~1.3.85';
-export const swcHelpersVersion = '~0.5.2';
-export const swcNodeVersion = '~1.8.0';
