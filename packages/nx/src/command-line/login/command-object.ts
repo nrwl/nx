a6cad5cd321f9d0bf640257a759ff6080f4c36c8
lourw <56288712+lourw@users.noreply.github.com>
2024-08-20 07:56:03 -0700

    chore(nx-cloud): move login handler to another file

--- packages/nx/src/command-line/login/command-object.ts
+++ packages/nx/src/command-line/login/command-object.ts
@@ -1,33 +1,15 @@
-import { verifyOrUpdateNxCloudClient } from '../../nx-cloud/update-manager';
 import { Argv, CommandModule } from 'yargs';
-import { handleErrors } from '../../utils/params';
-import { nxVersion } from '../../utils/versions';
-import { getCloudOptions } from '../../nx-cloud/utilities/get-cloud-options';
 
 export const yargsLoginCommand: CommandModule = {
   command: 'login',
   describe: false,
-  builder: (yargs) => withLoginOptions(yargs),
-  handler: async (args) => {
-    args._ = args._.slice(1);
-
+  builder: (yargs) =>
+    yargs.positional('nxCloudUrl', {
+      describe: 'The Nx Cloud URL of the instance you are trying to connect to.',
+      type: 'string',
+      required: false,
+    }),
+  handler: async (args: any) => {
     process.exit(await (await import('./login')).loginHandler(args));
   },
 };
-
-function withLoginOptions(yargs: Argv) {
-  const loginWillShowHelp = process.argv[3] && !process.argv[3].startsWith('-');
-  const res = yargs.positional('nxCloudUrl', {
-    describe: 'The Nx Cloud URL of the instance you are trying to connect to.',
-    type: 'string',
-    required: false,
-  });
-
-  if (loginWillShowHelp) {
-    return res.help(false);
-  } else {
-    return res.epilog(
-      `Run "nx login --help" to see information about this command`
-    );
-  }
-}
--- packages/nx/src/command-line/login/login.ts
+++ packages/nx/src/command-line/login/login.ts
@@ -2,16 +2,16 @@ import { verifyOrUpdateNxCloudClient } from '../../nx-cloud/update-manager';
 import { getCloudOptions } from '../../nx-cloud/utilities/get-cloud-options';
 import { handleErrors } from '../../utils/params';
 
-export interface LoginOptions {
+export interface LoginArgs {
   nxCloudUrl?: string;
-  verbose?: boolean;
 }
 
-export function loginHandler(options: LoginOptions): Promise<number> {
-  if (options.verbose) {
-    process.env.NX_VERBOSE_LOGGING = 'true';
-  }
+export function loginHandler(args: LoginArgs): Promise<number> {
   const isVerbose = process.env.NX_VERBOSE_LOGGING === 'true';
+
+  if (args.nxCloudUrl) {
+    process.env.NX_CLOUD_LOGIN_URL = args.nxCloudUrl
+  }
 
   return handleErrors(isVerbose, async () => {
     const nxCloudClient = (
