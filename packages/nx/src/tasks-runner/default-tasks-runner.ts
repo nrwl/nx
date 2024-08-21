d6fa71ec763f5b5f8f83d2c8ebf8cddcfe014efd
FrozenPandaz <jasonjean1993@gmail.com>
2024-08-21 17:13:14 -0400

    fix(core): fix compat for old remote caches

--- packages/nx/src/tasks-runner/default-tasks-runner.ts
+++ packages/nx/src/tasks-runner/default-tasks-runner.ts
@@ -7,7 +7,8 @@ import { NxJsonConfiguration } from '../config/nx-json';
 import { Task, TaskGraph } from '../config/task-graph';
 import { NxArgs } from '../utils/command-line-utils';
 import { DaemonClient } from '../daemon/client/client';
-import { readFile, writeFile } from 'fs/promises';
+import { cacheDir } from '../utils/cache-directory';
+import { readFile, writeFile, mkdir } from 'fs/promises';
 import { join } from 'path';
 import { CachedResult } from '../native';
 
@@ -17,18 +18,28 @@ export interface RemoteCache {
 }
 
 export abstract class RemoteCacheV2 {
-  static fromCacheV1(cache: RemoteCache): RemoteCacheV2 {
+  static async fromCacheV1(cache: RemoteCache): Promise<RemoteCacheV2> {
+    await mkdir(join(cacheDir, 'terminalOutputs'), { recursive: true });
+
     return {
       retrieve: async (hash, cacheDirectory) => {
-        const res = cache.retrieve(hash, cacheDirectory);
-        const [terminalOutput, code] = await Promise.all([
-          readFile(join(cacheDirectory, hash, 'terminalOutputs'), 'utf-8'),
-          readFile(join(cacheDirectory, hash, 'code'), 'utf-8').then((s) => +s),
-        ]);
+        const res = await cache.retrieve(hash, cacheDirectory);
         if (res) {
+          const [terminalOutput, oldTerminalOutput, code] = await Promise.all([
+            readFile(
+              join(cacheDirectory, hash, 'terminalOutputs'),
+              'utf-8'
+            ).catch(() => null),
+            readFile(join(cacheDir, 'terminalOutputs', hash), 'utf-8').catch(
+              () => null
+            ),
+            readFile(join(cacheDirectory, hash, 'code'), 'utf-8').then(
+              (s) => +s
+            ),
+          ]);
           return {
             outputsPath: cacheDirectory,
-            terminalOutput,
+            terminalOutput: terminalOutput ?? oldTerminalOutput,
             code,
           };
         } else {
