3518cd0175d37bec7499f743512d5cf47d306b37
Colum Ferry <cferry09@gmail.com>
2024-08-22 12:13:14 +0100

    fix(remix): e2e project generation

--- packages/remix/src/generators/application/lib/add-e2e.ts
+++ packages/remix/src/generators/application/lib/add-e2e.ts
@@ -7,6 +7,8 @@ import {
   ensurePackage,
   getPackageManagerCommand,
   readNxJson,
+  E2EWebServerDetails,
+  getE2EWebServerInfo,
 } from '@nx/devkit';
 import { type NormalizedSchema } from './normalize-options';
 import { getPackageVersion } from '../../../utils/versions';
@@ -19,6 +21,14 @@ export async function addE2E(tree: Tree, options: NormalizedSchema) {
       ? p === '@nx/remix/plugin'
       : p.plugin === '@nx/remix/plugin'
   );
+
+  let e2eWebsServerInfo = await getRemixE2EWebServerInfo(
+    tree,
+    options.projectName,
+    joinPathFragments(options.projectRoot, 'remix.config.js'),
+    options.addPlugin ?? Boolean(hasRemixPlugin)
+  );
+
   if (options.e2eTestRunner === 'cypress') {
     const { configurationGenerator } = ensurePackage<
       typeof import('@nx/cypress')
@@ -37,8 +47,18 @@ export async function addE2E(tree: Tree, options: NormalizedSchema) {
       project: options.e2eProjectName,
       directory: 'src',
       skipFormat: true,
-      devServerTarget: `${options.projectName}:${options.e2eWebServerTarget}:development`,
-      baseUrl: options.e2eWebServerAddress,
+      devServerTarget: e2eWebsServerInfo.e2eDevServerTarget,
+      baseUrl: e2eWebsServerInfo.e2eWebServerAddress,
+      webServerCommands: hasRemixPlugin
+        ? {
+            default: e2eWebsServerInfo.e2eWebServerCommand,
+            production: e2eWebsServerInfo.e2eCiWebServerCommand,
+          }
+        : undefined,
+      ciWebServerCommand: hasRemixPlugin
+        ? e2eWebsServerInfo.e2eCiWebServerCommand
+        : undefined,
+      ciBaseUrl: e2eWebsServerInfo.e2eCiBaseUrl,
       addPlugin: options.addPlugin,
     });
 
@@ -97,10 +117,8 @@ export async function addE2E(tree: Tree, options: NormalizedSchema) {
       js: false,
       linter: options.linter,
       setParserOptionsProject: false,
-      webServerCommand: `${getPackageManagerCommand().exec} nx ${
-        options.e2eWebServerTarget
-      } ${options.name}`,
-      webServerAddress: options.e2eWebServerAddress,
+      webServerCommand: e2eWebsServerInfo.e2eCiWebServerCommand,
+      webServerAddress: e2eWebsServerInfo.e2eCiBaseUrl,
       rootProject: options.rootProject,
       addPlugin: options.addPlugin,
     });
@@ -138,4 +156,42 @@ export async function addE2E(tree: Tree, options: NormalizedSchema) {
   } else {
     return () => {};
   }
+}
+
+async function getRemixE2EWebServerInfo(
+  tree: Tree,
+  projectName: string,
+  configFilePath: string,
+  isPluginBeingAdded: boolean
+) {
+  const nxJson = readNxJson(tree);
+  let e2ePort = isPluginBeingAdded ? 3000 : 4200;
+
+  const defaultServeTarget = isPluginBeingAdded ? 'dev' : 'serve';
+
+  if (
+    nxJson.targetDefaults?.[defaultServeTarget] &&
+    nxJson.targetDefaults?.[defaultServeTarget].options?.port
+  ) {
+    e2ePort = nxJson.targetDefaults?.[defaultServeTarget].options?.port;
+  }
+
+  return getE2EWebServerInfo(
+    tree,
+    projectName,
+    {
+      plugin: '@nx/remix/plugin',
+      serveTargetName: 'serveTargetName',
+      serveStaticTargetName: 'serveStaticTargetName',
+      configFilePath,
+    },
+    {
+      defaultServeTargetName: defaultServeTarget,
+      defaultServeStaticTargetName: 'serve-static',
+      defaultE2EWebServerAddress: `http://localhost:${e2ePort}`,
+      defaultE2ECiBaseUrl: 'http://localhost:3000',
+      defaultE2EPort: e2ePort,
+    },
+    isPluginBeingAdded
+  );
 }
