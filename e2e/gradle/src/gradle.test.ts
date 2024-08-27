3ae12ab84bb0b63545631e6068dacc1a8877bf1f
Emily Xiong <xiongemi@gmail.com>
2024-08-27 02:09:46 -0400

    feat(core): detect gradle plugin for any settings.gradle

--- e2e/gradle/src/gradle.test.ts
+++ e2e/gradle/src/gradle.test.ts
@@ -2,17 +2,13 @@ import {
   checkFilesExist,
   cleanupProject,
   createFile,
-  e2eConsoleLogger,
-  isWindows,
   newProject,
   runCLI,
-  runCommand,
-  tmpProjPath,
   uniq,
   updateFile,
 } from '@nx/e2e/utils';
-import { execSync } from 'child_process';
-import { resolve } from 'path';
+
+import { createGradleProject } from './utils/create-gradle-project';
 
 describe('Gradle', () => {
   describe.each([{ type: 'kotlin' }, { type: 'groovy' }])(
@@ -22,6 +18,7 @@ describe('Gradle', () => {
       beforeAll(() => {
         newProject();
         createGradleProject(gradleProjectName, type);
+        runCLI(`add @nx/gradle`);
       });
       afterAll(() => cleanupProject());
 
@@ -101,33 +98,3 @@ dependencies {
     }
   );
 });
-
-function createGradleProject(
-  projectName: string,
-  type: 'kotlin' | 'groovy' = 'kotlin'
-) {
-  e2eConsoleLogger(`Using java version: ${execSync('java -version')}`);
-  const gradleCommand = isWindows()
-    ? resolve(`${__dirname}/../gradlew.bat`)
-    : resolve(`${__dirname}/../gradlew`);
-  e2eConsoleLogger(
-    'Using gradle version: ' +
-      execSync(`${gradleCommand} --version`, {
-        cwd: tmpProjPath(),
-      })
-  );
-  e2eConsoleLogger(
-    execSync(`${gradleCommand} help --task :init`, {
-      cwd: tmpProjPath(),
-    }).toString()
-  );
-  e2eConsoleLogger(
-    runCommand(
-      `${gradleCommand} init --type ${type}-application --dsl ${type} --project-name ${projectName} --package gradleProject --no-incubating --split-project`,
-      {
-        cwd: tmpProjPath(),
-      }
-    )
-  );
-  runCLI(`add @nx/gradle`);
-}
--- packages/gradle/src/utils/exec-gradle.ts
+++ packages/gradle/src/utils/exec-gradle.ts
@@ -1,36 +1,35 @@
-import { workspaceRoot } from '@nx/devkit';
+import { AggregateCreateNodesError, workspaceRoot } from '@nx/devkit';
 import { ExecFileOptions, execFile } from 'node:child_process';
 import { existsSync } from 'node:fs';
-import { join } from 'node:path';
+import { dirname, join } from 'node:path';
 
-export function getGradleBinaryPath(): string {
-  const gradleFile = process.platform.startsWith('win')
-    ? 'gradlew.bat'
-    : 'gradlew';
-  const gradleBinaryPath = join(workspaceRoot, gradleFile);
-  if (!existsSync(gradleBinaryPath)) {
-    throw new Error('Gradle is not setup. Run "gradle init"');
-  }
-
-  return gradleBinaryPath;
-}
-
+/**
+ * For gradle command, it needs to be run from the directory of the gradle binary
+ * @returns gradle binary file name
+ */
 export function getGradleExecFile(): string {
   return process.platform.startsWith('win') ? '.\\gradlew.bat' : './gradlew';
 }
 
+/**
+ * This function executes gradle with the given arguments
+ * @param gradleBinaryPath absolute path to gradle binary
+ * @param args args passed to gradle
+ * @param execOptions exec options
+ * @returns promise with the stdout buffer
+ */
 export function execGradleAsync(
+  gradleBinaryPath: string,
   args: ReadonlyArray<string>,
   execOptions: ExecFileOptions = {}
 ): Promise<Buffer> {
-  const gradleBinaryPath = getGradleBinaryPath();
-
   return new Promise<Buffer>((res, rej) => {
     const cp = execFile(gradleBinaryPath, args, {
-      ...execOptions,
+      cwd: dirname(gradleBinaryPath),
       shell: true,
       windowsHide: true,
       env: process.env,
+      ...execOptions,
     });
 
     let stdout = Buffer.from('');
@@ -52,4 +51,47 @@ export function execGradleAsync(
       }
     });
   });
+}
+
+/**
+ * This function recursively finds the nearest gradlew file in the workspace
+ * @param originalFileToSearch the original file to search for
+ * @param wr workspace root
+ * @param currentSearchPath the path to start searching for gradlew file
+ * @returns the relative path of the gradlew file to workspace root, throws an error if gradlew file is not found
+ * It will return gradlew.bat file on windows and gradlew file on other platforms
+ */
+export function findGraldewFile(
+  originalFileToSearch: string,
+  wr: string = workspaceRoot,
+  currentSearchPath?: string
+): string {
+  currentSearchPath ??= originalFileToSearch;
+  const parent = dirname(currentSearchPath);
+  if (currentSearchPath === parent) {
+    throw new AggregateCreateNodesError(
+      [
+        [
+          originalFileToSearch,
+          new Error('No Gradlew file found. Run "gradle init"'),
+        ],
+      ],
+      []
+    );
+  }
+
+  const gradlewPath = join(parent, 'gradlew');
+  const gradlewBatPath = join(parent, 'gradlew.bat');
+
+  if (process.platform.startsWith('win')) {
+    if (existsSync(join(wr, gradlewBatPath))) {
+      return gradlewBatPath;
+    }
+  } else {
+    if (existsSync(join(wr, gradlewPath))) {
+      return gradlewPath;
+    }
+  }
+
+  return findGraldewFile(originalFileToSearch, wr, parent);
 }
