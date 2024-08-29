faffd30271916114986fddf2d4dba8f62b14c943
Colum Ferry <cferry09@gmail.com>
2024-08-29 11:34:33 +0100

    fix(react): migration test

--- packages/react/src/migrations/update-19-6-0/update-ssr-server-port.spec.ts
+++ packages/react/src/migrations/update-19-6-0/update-ssr-server-port.spec.ts
@@ -20,6 +20,7 @@ describe('update-19-6-0 update-ssr-server-port migration', () => {
       projectNameAndRootFormat: 'as-provided',
       style: 'css',
       remotes: ['product'],
+      bundler: 'webpack',
     });
     const remotePort = readProjectConfiguration(tree, 'product').targets.serve
       .options.port;
@@ -104,28 +105,39 @@ describe('update-19-6-0 update-ssr-server-port migration', () => {
 
     updateSsrServerPort(tree);
     expect(tree.read('shell/server.ts', 'utf-8')).toContain(
-      `port = process.env.PORT || ${shellPort}`
+      `port = process.env['PORT'] || ${shellPort}`
     );
     expect(tree.read('shell/server.ts', 'utf-8')).toMatchInlineSnapshot(`
-        "import * as path from 'path';
-        import express from 'express';
-        import cors from 'cors';
-        import { handleRequest } from './src/main.server';
-        const port = process.env.PORT || 4200;
-        const app = express();
-        const browserDist = path.join(process.cwd(), 'dist/shell/browser');
-        const indexPath = path.join(browserDist, 'index.html');
-        app.use(cors());
-        app.get('*.*', express.static(browserDist, {
-            maxAge: '1y',
-        }));
-        app.use('*', handleRequest(indexPath));
-        const server = app.listen(port, () => {
-            console.log(\`Express server listening on http://localhost:\${port}\`);
-        });
-        server.on('error', console.error);
-        "
-      `);
+      "import * as path from 'path';
+      import express from 'express';
+      import cors from 'cors';
+
+      import { handleRequest } from './src/main.server';
+
+      const port = process.env['PORT'] || 4200;
+      const app = express();
+
+      const browserDist = path.join(process.cwd(), 'dist/shell/browser');
+      const indexPath = path.join(browserDist, 'index.html');
+
+      app.use(cors());
+
+      app.get(
+        '*.*',
+        express.static(browserDist, {
+          maxAge: '1y',
+        })
+      );
+
+      app.use('*', handleRequest(indexPath));
+
+      const server = app.listen(port, () => {
+        console.log(\`Express server listening on http://localhost:\${port}\`);
+      });
+
+      server.on('error', console.error);
+      "
+    `);
   });
 
   it('should update a host project server file', async () => {
@@ -137,6 +149,7 @@ describe('update-19-6-0 update-ssr-server-port migration', () => {
       linter: Linter.EsLint,
       projectNameAndRootFormat: 'as-provided',
       style: 'css',
+      bundler: 'webpack',
     });
 
     const hostPort = readProjectConfiguration(tree, 'host').targets.serve
@@ -155,28 +168,39 @@ describe('update-19-6-0 update-ssr-server-port migration', () => {
     updateSsrServerPort(tree);
 
     expect(tree.read('host/server.ts', 'utf-8')).toContain(
-      `port = process.env.PORT || ${hostPort}`
+      `port = process.env['PORT'] || ${hostPort}`
     );
     expect(tree.read('host/server.ts', 'utf-8')).toMatchInlineSnapshot(`
-        "import * as path from 'path';
-        import express from 'express';
-        import cors from 'cors';
-        import { handleRequest } from './src/main.server';
-        const port = process.env.PORT || 4200;
-        const app = express();
-        const browserDist = path.join(process.cwd(), 'dist/host/browser');
-        const indexPath = path.join(browserDist, 'index.html');
-        app.use(cors());
-        app.get('*.*', express.static(browserDist, {
-            maxAge: '1y',
-        }));
-        app.use('*', handleRequest(indexPath));
-        const server = app.listen(port, () => {
-            console.log(\`Express server listening on http://localhost:\${port}\`);
-        });
-        server.on('error', console.error);
-        "
-      `);
+      "import * as path from 'path';
+      import express from 'express';
+      import cors from 'cors';
+
+      import { handleRequest } from './src/main.server';
+
+      const port = process.env['PORT'] || 4200;
+      const app = express();
+
+      const browserDist = path.join(process.cwd(), 'dist/host/browser');
+      const indexPath = path.join(browserDist, 'index.html');
+
+      app.use(cors());
+
+      app.get(
+        '*.*',
+        express.static(browserDist, {
+          maxAge: '1y',
+        })
+      );
+
+      app.use('*', handleRequest(indexPath));
+
+      const server = app.listen(port, () => {
+        console.log(\`Express server listening on http://localhost:\${port}\`);
+      });
+
+      server.on('error', console.error);
+      "
+    `);
   });
 
   it('should not update a mfe project that is not ssr', async () => {
@@ -188,6 +212,7 @@ describe('update-19-6-0 update-ssr-server-port migration', () => {
       linter: Linter.EsLint,
       projectNameAndRootFormat: 'as-provided',
       style: 'css',
+      bundler: 'webpack',
     });
 
     tree.write('shell-not-ssr/server.ts', 'const port = 9999;');
