543ff2704465d4da946b16e05e12c50f76ff5ffd
Colum Ferry <cferry09@gmail.com>
2024-08-30 11:31:40 +0100

    feat(remix): add createnodesv2

--- packages/remix/src/plugins/plugin.ts
+++ packages/remix/src/plugins/plugin.ts
@@ -1,44 +1,26 @@
 import { workspaceDataDirectory } from 'nx/src/utils/cache-directory';
+import { hashObject } from 'nx/src/hasher/file-hasher';
 import {
   type CreateDependencies,
   type CreateNodes,
   type CreateNodesContext,
+  createNodesFromFiles,
+  CreateNodesV2,
   detectPackageManager,
   joinPathFragments,
+  logger,
+  ProjectConfiguration,
   readJsonFile,
   type TargetConfiguration,
   writeJsonFile,
 } from '@nx/devkit';
 import { calculateHashForCreateNodes } from '@nx/devkit/src/utils/calculate-hash-for-create-nodes';
 import { getNamedInputs } from '@nx/devkit/src/utils/get-named-inputs';
+import { loadConfigFile } from '@nx/devkit/src/utils/config-utils';
 import { getLockFileName } from '@nx/js';
 import { type AppConfig } from '@remix-run/dev';
 import { dirname, join } from 'path';
 import { existsSync, readdirSync } from 'fs';
-import { loadConfigFile } from '@nx/devkit/src/utils/config-utils';
-
-const cachePath = join(workspaceDataDirectory, 'remix.hash');
-const targetsCache = readTargetsCache();
-
-function readTargetsCache(): Record<
-  string,
-  Record<string, TargetConfiguration>
-> {
-  return existsSync(cachePath) ? readJsonFile(cachePath) : {};
-}
-
-function writeTargetsToCache() {
-  const oldCache = readTargetsCache();
-  writeJsonFile(cachePath, {
-    ...oldCache,
-    ...targetsCache,
-  });
-}
-
-export const createDependencies: CreateDependencies = () => {
-  writeTargetsToCache();
-  return [];
-};
 
 export interface RemixPluginOptions {
   buildTargetName?: string;
@@ -51,49 +33,109 @@ export interface RemixPluginOptions {
   staticServeTargetName?: string;
   serveStaticTargetName?: string;
 }
+type RemixTargets = Pick<ProjectConfiguration, 'targets' | 'metadata'>;
 
-export const createNodes: CreateNodes<RemixPluginOptions> = [
-  '**/remix.config.{js,cjs,mjs}',
-  async (configFilePath, options, context) => {
-    const projectRoot = dirname(configFilePath);
-    const fullyQualifiedProjectRoot = join(context.workspaceRoot, projectRoot);
-    // Do not create a project if package.json and project.json isn't there
-    const siblingFiles = readdirSync(fullyQualifiedProjectRoot);
-    if (
-      !siblingFiles.includes('package.json') &&
-      !siblingFiles.includes('project.json') &&
-      !siblingFiles.includes('vite.config.ts') &&
-      !siblingFiles.includes('vite.config.js')
-    ) {
-      return {};
+function readTargetsCache(
+  cachePath: string
+): Record<string, Record<string, TargetConfiguration>> {
+  return existsSync(cachePath) ? readJsonFile(cachePath) : {};
+}
+
+function writeTargetsToCache(
+  cachePath: string,
+  results: Record<string, RemixTargets>
+) {
+  writeJsonFile(cachePath, results);
+}
+
+/**
+ * @deprecated The 'createDependencies' function is now a no-op. This functionality is included in 'createNodesV2'.
+ */
+export const createDependencies: CreateDependencies = () => {
+  return [];
+};
+
+const remixConfigGlob = '**/{remix|vite}.config.{js,cjs,mjs}';
+
+export const createNodesV2: CreateNodesV2<RemixPluginOptions> = [
+  remixConfigGlob,
+  async (configFilePaths, options, context) => {
+    const optionsHash = hashObject(options);
+    const cachePath = join(workspaceDataDirectory, `remix-${optionsHash}.hash`);
+    const targetsCache = readTargetsCache(cachePath);
+    try {
+      return await createNodesFromFiles(
+        (configFile, options, context) =>
+          createNodesInternal(configFile, options, context, targetsCache),
+        configFilePaths,
+        options,
+        context
+      );
+    } finally {
+      writeTargetsToCache(cachePath, targetsCache);
     }
-
-    options = normalizeOptions(options);
-
-    const hash = await calculateHashForCreateNodes(
-      projectRoot,
-      options,
-      context,
-      [getLockFileName(detectPackageManager(context.workspaceRoot))]
-    );
-    targetsCache[hash] ??= await buildRemixTargets(
-      configFilePath,
-      projectRoot,
-      options,
-      context,
-      siblingFiles
-    );
-
-    return {
-      projects: {
-        [projectRoot]: {
-          root: projectRoot,
-          targets: targetsCache[hash],
-        },
-      },
-    };
   },
 ];
+
+export const createNodes: CreateNodes<RemixPluginOptions> = [
+  remixConfigGlob,
+  async (configFilePath, options, context) => {
+    logger.warn(
+      '`createNodes` is deprecated. Update your plugin to utilize createNodesV2 instead. In Nx 20, this will change to the createNodesV2 API.'
+    );
+    return createNodesInternal(configFilePath, options, context, {});
+  },
+];
+
+async function createNodesInternal(
+  configFilePath: string,
+  options: RemixPluginOptions,
+  context: CreateNodesContext,
+  targetsCache: Record<string, RemixTargets>
+) {
+  const projectRoot = dirname(configFilePath);
+  const fullyQualifiedProjectRoot = join(context.workspaceRoot, projectRoot);
+  // Do not create a project if package.json and project.json isn't there
+  const siblingFiles = readdirSync(fullyQualifiedProjectRoot);
+  if (
+    !siblingFiles.includes('package.json') &&
+    !siblingFiles.includes('project.json') &&
+    !siblingFiles.includes('vite.config.ts') &&
+    !siblingFiles.includes('vite.config.js')
+  ) {
+    return {};
+  }
+
+  options = normalizeOptions(options);
+
+  const hash = await calculateHashForCreateNodes(
+    projectRoot,
+    options,
+    context,
+    [getLockFileName(detectPackageManager(context.workspaceRoot))]
+  );
+  targetsCache[hash] ??= await buildRemixTargets(
+    configFilePath,
+    projectRoot,
+    options,
+    context,
+    siblingFiles
+  );
+
+  const { targets, metadata } = targetsCache[hash];
+
+  const project: ProjectConfiguration = {
+    root: projectRoot,
+    targets,
+    metadata,
+  };
+
+  return {
+    projects: {
+      [projectRoot]: project,
+    },
+  };
+}
 
 async function buildRemixTargets(
   configFilePath: string,
