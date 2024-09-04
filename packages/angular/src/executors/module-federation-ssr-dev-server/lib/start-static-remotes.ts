d4af3d98bbcc3c3e314eba78585b67cda83711f2
Colum Ferry <cferry09@gmail.com>
2024-09-04 16:33:24 +0100

    fix(module-federation): ssr uses async-node with runtime plugin (#27492)
    - fix(module-federation): SSR should work correctly
    - fix(module-federation): ssr-dev-servers should wait for remotes before
    starting final executor
    
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    ## Current Behavior
    <!-- This is the behavior we have today -->
    
    ## Expected Behavior
    <!-- This is the behavior we should expect with the changes in this PR
    -->
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #

--- packages/angular/src/executors/module-federation-ssr-dev-server/lib/start-static-remotes.ts
+++ packages/angular/src/executors/module-federation-ssr-dev-server/lib/start-static-remotes.ts
@@ -4,6 +4,7 @@ import fileServerExecutor from '@nx/web/src/executors/file-server/file-server.im
 import { join } from 'path';
 import { cpSync, rmSync } from 'fs';
 import type { StaticRemotesConfig } from '@nx/webpack/src/utils/module-federation/parse-static-remotes-config';
+import { createAsyncIterable } from '@nx/devkit/src/utils/async-iterable';
 
 export function startStaticRemotes(
   ssrStaticRemotesConfig: StaticRemotesConfig,
@@ -11,7 +12,10 @@ export function startStaticRemotes(
   options: Schema
 ) {
   if (ssrStaticRemotesConfig.remotes.length === 0) {
-    return;
+    return createAsyncIterable(({ next, done }) => {
+      next({ success: true });
+      done();
+    });
   }
 
   // The directories are usually generated with /browser and /server suffixes so we need to copy them to a common directory
