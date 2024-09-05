7aa361b2f0f401f54ba77a7f9a6a4b76bf28efbd
lourw <56288712+lourw@users.noreply.github.com>
2024-09-05 08:36:32 -0700

    chore(nx-cloud): remove env variable for login

--- packages/nx/src/command-line/logout/command-object.ts
+++ packages/nx/src/command-line/logout/command-object.ts
@@ -3,7 +3,7 @@ import { withVerbose } from '../../command-line/yargs-utils/shared-options';
 
 export const yargsLogoutCommand: CommandModule = {
   command: 'logout',
-  describe: false,
+  describe: 'Logout from Nx Cloud.',
   builder: (yargs) => withVerbose(yargs),
   handler: async (args: any) => {
     process.exit(await (await import('./logout')).logoutHandler(args));
