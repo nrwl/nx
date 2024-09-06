ac9da418bd00858d8f4d50b009c8f17fc67c734f
Craigory Coppola <craigorycoppola@gmail.com>
2024-09-06 17:07:30 -0400

    fix(core): optimize daemon output glob matching (#27775)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    ## Current Behavior
    - Nonexistant outputs are considered globs
    - Glob outputs are walked from workspace root, which is slow on large
    repos
    
    [NX Daemon Server] - 2024-09-06T19:44:39.674Z - Done responding to the
    client outputsHashesMatch
    [NX Daemon Server] - 2024-09-06T19:44:39.674Z - Handled
    OUTPUTS_HASHES_MATCH. Handling time: 94. Response time: 0.
    [NX Daemon Server] - 2024-09-06T19:44:39.775Z - [REQUEST]: Responding to
    the client. recordOutputsHash
    [NX Daemon Server] - 2024-09-06T19:44:39.775Z - Done responding to the
    client recordOutputsHash
    [NX Daemon Server] - 2024-09-06T19:44:39.775Z - Handled
    RECORD_OUTPUTS_HASH. Handling time: 100. Response time: 0.
    [NX Daemon Server] - 2024-09-06T19:44:39.818Z - [REQUEST]: Responding to
    the client. PROCESS_IN_BACKGROUND
    [NX Daemon Server] - 2024-09-06T19:44:39.818Z - Done responding to the
    client PROCESS_IN_BACKGROUND
    [NX Daemon Server] - 2024-09-06T19:44:39.818Z - Handled
    PROCESS_IN_BACKGROUND. Handling time: 14. Response time: 0.
    
    ## Expected Behavior
    - Nonexistant outputs are only globs if they should be
    - Globs are a bit faster
    
    [NX Daemon Server] - 2024-09-06T19:43:36.899Z - Handled
    OUTPUTS_HASHES_MATCH. Handling time: 0. Response time: 0.
    [NX Daemon Server] - 2024-09-06T19:43:36.900Z - [REQUEST]: Responding to
    the client. recordOutputsHash
    [NX Daemon Server] - 2024-09-06T19:43:36.900Z - Done responding to the
    client recordOutputsHash
    [NX Daemon Server] - 2024-09-06T19:43:36.900Z - Handled
    RECORD_OUTPUTS_HASH. Handling time: 0. Response time: 0.
    [NX Daemon Server] - 2024-09-06T19:43:36.944Z - [REQUEST]: Responding to
    the client. PROCESS_IN_BACKGROUND
    [NX Daemon Server] - 2024-09-06T19:43:36.944Z - Done responding to the
    client PROCESS_IN_BACKGROUND
    [NX Daemon Server] - 2024-09-06T19:43:36.944Z - Handled
    PROCESS_IN_BACKGROUND. Handling time: 13. Response time: 0.
    [NX Daemon Server] - 2024-09-06T19:43:36.949Z - Uploading file artifacts
    [NX Daemon Server] - 2024-09-06T19:43:36.949Z - Done uploading file
    artifacts
    
    > Note timings are from Nx repo, close enough to be comparable. No real
    improvement was expected here, mainly checking that things didn't get
    worse.
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #
    
    ---------
    
    Co-authored-by: FrozenPandaz <jasonjean1993@gmail.com>

--- packages/nx/src/native/cache/mod.rs
+++ packages/nx/src/native/cache/mod.rs
@@ -1,3 +1,4 @@
+pub mod cache;
 pub mod expand_outputs;
 pub mod file_ops;
-pub mod cache;
+pub mod validate_outputs;
--- packages/nx/src/native/index.d.ts
+++ packages/nx/src/native/index.d.ts
@@ -150,6 +150,8 @@ export declare export function getBinaryTarget(): string
  */
 export declare export function getFilesForOutputs(directory: string, entries: Array<string>): Array<string>
 
+export declare export function getTransformableOutputs(outputs: Array<string>): Array<string>
+
 export declare export function hashArray(input: Array<string>): string
 
 export interface HashDetails {
@@ -262,6 +264,8 @@ export interface UpdatedWorkspaceFiles {
   fileMap: FileMap
   externalReferences: NxWorkspaceFilesExternals
 }
+
+export declare export function validateOutputs(outputs: Array<string>): void
 
 export interface WatchEvent {
   path: string
