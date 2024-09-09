eacc143d98325e984c3065de3d905e6ef49a00ea
Nicholas Cunningham <ndcunningham@gmail.com>
2024-09-09 09:05:57 -0600

    fix(react-native): remove derived format

--- packages/react-native/src/generators/component/schema.d.ts
+++ packages/react-native/src/generators/component/schema.d.ts
@@ -5,22 +5,10 @@ import type { NameAndDirectoryFormat } from '@nx/devkit/src/generators/artifact-
  */
 export interface Schema {
   name: string;
-  /**
-   * @deprecated Provide the `directory` option instead and use the `as-provided` format. The project will be determined from the directory provided. It will be removed in Nx v20.
-   */
-  project: string;
   skipTests?: boolean;
   directory?: string;
   export?: boolean;
-  /**
-   * @deprecated Provide the `directory` option instead and use the `as-provided` format. This option will be removed in Nx v19.
-   */
-  pascalCaseFiles?: boolean;
   classComponent?: boolean;
   js?: boolean;
-  /**
-   * @deprecated Provide the `name` in pascal-case and use the `as-provided` format. This option will be removed in Nx v19.
-   */
-  flat?: boolean;
   nameAndDirectoryFormat?: NameAndDirectoryFormat;
 }
