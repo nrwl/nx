72cd1c15e6e0a8037888eb86ca620cb30c157613
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-09-24 15:24:09 +0200

    feat(js): add the setup-prettier generator (#27996)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    ## Current Behavior
    <!-- This is the behavior we have today -->
    
    ## Expected Behavior
    <!-- This is the behavior we should expect with the changes in this PR
    -->
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #

---
+++ packages/js/src/generators/setup-prettier/generator.ts
@@ -0,0 +1,31 @@
+import {
+  ensurePackage,
+  formatFiles,
+  type GeneratorCallback,
+  type Tree,
+} from '@nx/devkit';
+import { generatePrettierSetup } from '../../utils/prettier';
+import { prettierVersion } from '../../utils/versions';
+import type { GeneratorOptions } from './schema';
+
+export async function setupPrettierGenerator(
+  tree: Tree,
+  options: GeneratorOptions
+): Promise<GeneratorCallback> {
+  const prettierTask = generatePrettierSetup(tree, {
+    skipPackageJson: options.skipPackageJson,
+  });
+
+  if (!options.skipFormat) {
+    if (!options.skipPackageJson) {
+      ensurePackage('prettier', prettierVersion);
+    }
+    // even if skipPackageJson === true, we can safely run formatFiles, prettier might
+    // have been installed earlier and if not, the formatFiles function still handles it
+    await formatFiles(tree);
+  }
+
+  return prettierTask;
+}
+
+export default setupPrettierGenerator;
