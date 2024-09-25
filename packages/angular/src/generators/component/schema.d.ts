3f6dc573bac698f931fa4aafe297f9e337c34c12
Colum Ferry <cferry09@gmail.com>
2024-09-25 18:20:08 +0100

    fix(angular): revert schema change for component

--- packages/angular/src/generators/component/schema.d.ts
+++ packages/angular/src/generators/component/schema.d.ts
@@ -1,8 +1,8 @@
 import type { NameAndDirectoryFormat } from '@nx/devkit/src/generators/artifact-name-and-directory-utils';
 
 export interface Schema {
-  directory: string;
-  name?: string;
+  name: string;
+  directory?: string;
   nameAndDirectoryFormat?: NameAndDirectoryFormat;
   displayBlock?: boolean;
   inlineStyle?: boolean;
