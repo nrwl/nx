ff23fb0c0728beca01c913a677f147f232c961b9
Jack Hsu <jack.hsu@gmail.com>
2024-10-15 14:48:00 -0400

    fix(react): patch fix for 88f163e9d5 to normalize project name without messing up directory

--- packages/react/src/generators/host/host.ts
+++ packages/react/src/generators/host/host.ts
@@ -67,7 +67,6 @@ export async function hostGeneratorInternal(
 
   const initTask = await applicationGenerator(host, {
     ...options,
-    directory: options.appProjectRoot,
     name: options.projectName,
     // The target use-case is loading remotes as child routes, thus always enable routing.
     routing: true,
