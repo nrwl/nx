7c12eb6ec960649ae3d4271b3541bd9ef027e35b
demonicattack <demonicattacks@outlook.com>
2024-10-17 23:42:27 +0400

    fix(eslint): formating

--- packages/eslint-plugin/src/flat-configs/javascript.ts
+++ packages/eslint-plugin/src/flat-configs/javascript.ts
@@ -80,7 +80,7 @@ export default tseslint.config(
        */
       '@typescript-eslint/no-require-imports': 'off',
     },
-  }
+  },
 /**
  * With the new ESLint “flat config” format, you can control what things override what yourself. 
  * One way of solving the above conflict is to reorder the config objects so that eslint-config-prettier is last:
