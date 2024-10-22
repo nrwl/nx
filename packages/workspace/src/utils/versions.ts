6e3157c2cd074d9b64c23ee6f1ea9a4f8c4e0625
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-10-22 11:23:50 +0200

    feat(angular): support angular v19.0.0 (#28953)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    <!-- This is the behavior we have today -->
    
    <!-- This is the behavior we should expect with the changes in this PR
    -->
    
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -4,4 +4,4 @@ export const typescriptVersion = '~5.5.2';
 
 // TODO: remove when preset generation is reworked and
 // deps are not installed from workspace
-export const angularCliVersion = '~18.2.0';
+export const angularCliVersion = '~19.0.0';
