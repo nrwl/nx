9be4a25f01fc8e5acdf83afecb2193771a5e39ff
Colum Ferry <cferry09@gmail.com>
2024-11-02 11:28:22 +0000

    chore(react): separate module federation tests into separate test files for better distribution

---
+++ e2e/react/src/module-federation/utils.ts
@@ -0,0 +1,12 @@
+import { readJson } from '@nx/e2e/utils';
+import { join } from 'path';
+
+export function readPort(appName: string): number {
+  let config;
+  try {
+    config = readJson(join('apps', appName, 'project.json'));
+  } catch {
+    config = readJson(join(appName, 'project.json'));
+  }
+  return config.targets.serve.options.port;
+}
