2cc84053b9da84370cf94f622d31d7e03e7022eb
Colum Ferry <cferry09@gmail.com>
2024-11-04 13:34:53 +0000

    feat(rspack): make target property optional

--- packages/rspack/src/executors/rspack/schema.d.ts
+++ packages/rspack/src/executors/rspack/schema.d.ts
@@ -1,7 +1,7 @@
 import type { Mode } from '@rspack/core';
 
 export interface RspackExecutorSchema {
-  target: 'web' | 'node';
+  target?: 'web' | 'node';
   main: string;
   index?: string;
   tsConfig: string;
