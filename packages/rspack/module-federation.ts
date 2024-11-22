840d935d2ef03a509a69352361d4a8791fbbd534
Colum Ferry <cferry09@gmail.com>
2024-11-22 13:26:00 -0500

    feat(module-federation): move withModuleFederation for rspack to new package (#29045)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    ## Current Behavior
    <!-- This is the behavior we have today -->
    The `withModuleFederation` helper currently lives in the `@nx/rspack`
    package.
    With the goal of consolidating the module federation support into a
    single package, this introduces a divergence in where module-federation
    support lies
    
    
    ## Expected Behavior
    <!-- This is the behavior we should expect with the changes in this PR
    -->
    Move `withModuleFederation` helper for angular into the
    `@nx/module-federation` package, exposed via
    `@nx/module-federation/rspack`.
    Adds a migration to migrate existing projects to use the new package
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #

--- packages/rspack/module-federation.ts
+++ packages/rspack/module-federation.ts
@@ -1,1 +1,7 @@
-export * from './src/utils/module-federation/public-api';
+/**
+ * @deprecated Use `@nx/module-federation/rspack` instead. This will be removed in Nx v22.
+ */
+export {
+  withModuleFederation,
+  withModuleFederationForSSR,
+} from '@nx/module-federation/rspack';
