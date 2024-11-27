e4d2130bd93c3b8c529ebadd8c30e0fe41a570ca
Miroslav Jonas <missing.manual@gmail.com>
2024-11-27 14:39:15 +0100

    fix(bundling): rspack should allow ES config module imports

--- packages/rspack/src/utils/resolve-user-defined-rspack-config.ts
+++ packages/rspack/src/utils/resolve-user-defined-rspack-config.ts
@@ -18,7 +18,7 @@ export function resolveUserDefinedRspackConfig(
   // Don't transpile non-TS files. This prevents workspaces libs from being registered via tsconfig-paths.
   // There's an issue here with Nx workspace where loading plugins from source (via tsconfig-paths) can lead to errors.
   if (!/\.(ts|mts|cts)$/.test(path)) {
-    return require(path);
+    return import(path);
   }
 
   const cleanupTranspiler = registerTsProject(tsConfig);
