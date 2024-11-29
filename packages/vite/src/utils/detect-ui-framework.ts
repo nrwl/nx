3eb73c25937ff412539feab691b91339fdfc4e22
Younes Jaaidi <younes@marmicode.io>
2024-11-29 21:01:04 +0100

    feat(vite): add angular detection to vitest configuration generator

---
+++ packages/vite/src/utils/detect-ui-framework.ts
@@ -0,0 +1,19 @@
+import { createProjectGraphAsync } from '@nx/devkit';
+
+export async function detectUiFramework(
+  project: string
+): Promise<'angular' | 'react' | 'none'> {
+  const graph = await createProjectGraphAsync();
+
+  for (const dep of graph.dependencies[project]) {
+    if (dep.source !== project) {
+      continue;
+    }
+
+    if (dep.target.startsWith('npm:@angular/')) {
+      return 'angular';
+    }
+  }
+
+  return 'none';
+}
