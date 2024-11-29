22be047262918c511f513b72bb7905d8371885a1
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-11-29 10:28:53 +0100

    feat(react-native): update react-native generators

--- packages/react-native/src/generators/component/component.ts
+++ packages/react-native/src/generators/component/component.ts
@@ -5,7 +5,6 @@ import {
   generateFiles,
   getProjects,
   joinPathFragments,
-  toJS,
   Tree,
 } from '@nx/devkit';
 import { NormalizedSchema, normalizeOptions } from './lib/normalize-options';
@@ -26,25 +25,23 @@ export async function reactNativeComponentGenerator(
 }
 
 function createComponentFiles(host: Tree, options: NormalizedSchema) {
-  generateFiles(host, join(__dirname, './files'), options.directory, {
-    ...options,
-    tmpl: '',
-  });
-
-  for (const c of host.listChanges()) {
-    let deleteFile = false;
-
-    if (options.skipTests && /.*spec.tsx/.test(c.path)) {
-      deleteFile = true;
+  generateFiles(
+    host,
+    join(__dirname, 'files', options.fileExtensionType),
+    options.directory,
+    {
+      ...options,
+      ext: options.fileExtension,
     }
+  );
 
-    if (deleteFile) {
-      host.delete(c.path);
-    }
-  }
-
-  if (options.js) {
-    toJS(host);
+  if (options.skipTests) {
+    host.delete(
+      join(
+        options.directory,
+        `${options.fileName}.spec.${options.fileExtension}`
+      )
+    );
   }
 }
 
@@ -61,27 +58,30 @@ function addExportsToBarrel(host: Tree, options: NormalizedSchema) {
   if (options.export && !isApp) {
     const indexFilePath = joinPathFragments(
       options.projectSourceRoot,
-      options.js ? 'index.js' : 'index.ts'
+      options.fileExtensionType === 'js' ? 'index.js' : 'index.ts'
     );
-    const indexSource = host.read(indexFilePath, 'utf-8');
-    if (indexSource !== null) {
-      const indexSourceFile = tsModule.createSourceFile(
-        indexFilePath,
-        indexSource,
-        tsModule.ScriptTarget.Latest,
-        true
-      );
 
-      const relativePathFromIndex = getRelativeImportToFile(
-        indexFilePath,
-        options.filePath
-      );
-      const changes = applyChangesToString(
-        indexSource,
-        addImport(indexSourceFile, `export * from '${relativePathFromIndex}';`)
-      );
-      host.write(indexFilePath, changes);
+    if (!host.exists(indexFilePath)) {
+      return;
     }
+
+    const indexSource = host.read(indexFilePath, 'utf-8');
+    const indexSourceFile = tsModule.createSourceFile(
+      indexFilePath,
+      indexSource,
+      tsModule.ScriptTarget.Latest,
+      true
+    );
+
+    const relativePathFromIndex = getRelativeImportToFile(
+      indexFilePath,
+      options.filePath
+    );
+    const changes = applyChangesToString(
+      indexSource,
+      addImport(indexSourceFile, `export * from '${relativePathFromIndex}';`)
+    );
+    host.write(indexFilePath, changes);
   }
 }
 
