bd94a09032bff3e7be556b54059dc6ceb0895341
Younes Jaaidi <younes@marmicode.io>
2024-11-29 14:48:13 +0100

    chore(angular): remove old mentions of analogVitestAngular version

--- packages/angular/src/utils/backward-compatible-versions.ts
+++ packages/angular/src/utils/backward-compatible-versions.ts
@@ -16,7 +16,7 @@ export type VersionMap = {
   angularV17: Record<
     Exclude<
       CompatPackageVersionNames,
-      'analogVitestAngular' | 'typescriptEslintVersion'
+     'typescriptEslintVersion'
     >,
     string
   >;
@@ -80,7 +80,6 @@ export const backwardCompatibleVersions: VersionMap = {
     jestPresetAngularVersion: '~14.1.0',
     typesNodeVersion: '18.16.9',
     jasmineMarblesVersion: '^0.9.2',
-    analogVitestAngular: '~1.9.1',
     jsoncEslintParserVersion: '^2.1.0',
   },
 };
