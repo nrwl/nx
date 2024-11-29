31b04b2c9ce6b2ddfef81474147e24b82828c1e6
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-11-29 14:12:33 +0100

    feat(angular): update angular generators

--- packages/angular/src/generators/scam/lib/normalize-options.ts
+++ packages/angular/src/generators/scam/lib/normalize-options.ts
@@ -19,6 +19,8 @@ export async function normalizeOptions(
     name: options.name,
     path: options.path,
     suffix: options.type ?? 'component',
+    allowedFileExtensions: ['ts'],
+    fileExtension: 'ts',
   });
 
   const { className } = names(name);
