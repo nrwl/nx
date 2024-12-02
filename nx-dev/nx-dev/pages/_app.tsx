1eeb213898dd83ff58de93c83f9af6f378bc6071
Benjamin Cabanes <3447705+bcabanes@users.noreply.github.com>
2024-12-02 15:37:50 -0500

    docs(nx-dev): add rb2b script to _app.tsx
    This commit integrates the rb2b tracking script into the application by injecting it into the page with the `afterInteractive` strategy.

--- nx-dev/nx-dev/pages/_app.tsx
+++ nx-dev/nx-dev/pages/_app.tsx
@@ -153,6 +153,15 @@ export default function CustomApp({
         `,
         }}
       />
+      <Script
+        id="rb2b-script-loader"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        !function () {var reb2b = window.reb2b = window.reb2b || [];if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"];reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments);args.unshift(method);reb2b.push(args);return reb2b;};};for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);}reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true;script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/0NW1GH7YJ4O4.js.gz";var first = document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script, first);};reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("0NW1GH7YJ4O4");}();
+        `,
+        }}
+      />
     </>
   );
 }
