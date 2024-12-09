9f5b630d9d646eb9325393f1f83ad76d533a5569
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-12-09 18:07:07 +0100

    fix(vite)!: generate config with esm by default

--- packages/react/src/generators/library/library.ts
+++ packages/react/src/generators/library/library.ts
@@ -75,6 +75,7 @@ export async function libraryGeneratorInternal(host: Tree, schema: Schema) {
         : undefined;
     writeJson(host, `${options.projectRoot}/package.json`, {
       name: options.importPath,
+      version: '0.0.1',
       main: sourceEntry,
       types: sourceEntry,
       nx: {
