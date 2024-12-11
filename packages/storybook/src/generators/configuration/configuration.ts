b4aadccac82d2b18d6d56587aabe80602e61b442
Emily Xiong <xiongemi@gmail.com>
2024-12-11 12:01:17 -0800

    fix(react-native): fix react native storybook for lib (#29210)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    ## Current Behavior
    <!-- This is the behavior we have today -->
    
    ## Expected Behavior
    <!-- This is the behavior we should expect with the changes in this PR
    -->
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes https://github.com/nrwl/nx/issues/28802

--- packages/storybook/src/generators/configuration/configuration.ts
+++ packages/storybook/src/generators/configuration/configuration.ts
@@ -25,9 +25,9 @@ import {
   createProjectStorybookDir,
   createStorybookTsconfigFile,
   editTsconfigBaseJson,
-  findMetroConfig,
   findNextConfig,
   findViteConfig,
+  isUsingReactNative,
   projectIsRootProjectInStandaloneWorkspace,
   updateLintConfig,
 } from './lib/util-functions';
@@ -82,7 +82,6 @@ export async function configurationGeneratorInternal(
   const viteConfigFilePath = viteConfig?.fullConfigPath;
   const viteConfigFileName = viteConfig?.viteConfigFileName;
   const nextConfigFilePath = findNextConfig(tree, root);
-  const metroConfigFilePath = findMetroConfig(tree, root);
 
   if (viteConfigFilePath) {
     if (schema.uiFramework === '@storybook/react-webpack5') {
@@ -133,7 +132,7 @@ export async function configurationGeneratorInternal(
 
   const usesVite =
     !!viteConfigFilePath || schema.uiFramework?.endsWith('-vite');
-  const useReactNative = !!metroConfigFilePath;
+  const usesReactNative = isUsingReactNative(schema.project);
 
   createProjectStorybookDir(
     tree,
@@ -152,7 +151,7 @@ export async function configurationGeneratorInternal(
     viteConfigFilePath,
     hasPlugin,
     viteConfigFileName,
-    useReactNative
+    usesReactNative
   );
 
   if (schema.uiFramework !== '@storybook/angular') {
