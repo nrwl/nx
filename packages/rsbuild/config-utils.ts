32f6ed5b5c167821201dd301173ad9734bc5f324
Colum Ferry <cferry09@gmail.com>
2024-12-17 18:15:25 +0000

    chore(rsbuild): refactor exports

---
+++ packages/rsbuild/config-utils.ts
@@ -0,0 +1,8 @@
+export { addBuildPlugin } from './src/utils/add-build-plugin';
+export {
+  addCopyAssets,
+  addHtmlTemplatePath,
+  addExperimentalSwcPlugin,
+} from './src/utils/ast-utils';
+export * as versions from './src/utils/versions';
+export { getRsbuildE2EWebServerInfo } from './src/utils/e2e-web-server-info-utils';
