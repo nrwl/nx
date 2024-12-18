6ebe258f59901b3ae4e31e82a2cb7de39a7b02d6
JamesHenry <james@henry.sc>
2024-12-18 16:36:31 +0400

    chore(repo): fix resolver to not special case powerpack packages

--- tools/workspace-plugin/jest.config.ts
+++ tools/workspace-plugin/jest.config.ts
@@ -1,8 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'workspace-plugin',
-  // TODO: For some reason our patched jest resolve cannot work with @nx/powerpack-conformance
-  // preset: '../../jest.preset.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
