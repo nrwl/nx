8976f1c56f18b7ecc09637a2836a6fe9c16101ae
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2025-01-13 10:50:36 +0100

    feat(angular): support angular v19.1.0-rc.0

--- packages/angular/src/migrations/update-20-4-0/update-angular-cli.ts
+++ packages/angular/src/migrations/update-20-4-0/update-angular-cli.ts
@@ -1,6 +1,6 @@
 import { formatFiles, Tree, updateJson } from '@nx/devkit';
 
-export const angularCliVersion = '~19.1.0-next.2';
+export const angularCliVersion = '~19.1.0-rc.0';
 
 export default async function (tree: Tree) {
   let shouldFormat = false;
