ffe15c32ad70c7386083a1d8c12b71e9daf769ea
Benjamin Cabanes <3447705+bcabanes@users.noreply.github.com>
2025-01-23 15:21:29 -0500

    docs(nx-dev): remove webinar notifier & event link (#29737)
    Commented out the `WebinarNotifier` in `_app.tsx` and the live event link section in `hero.tsx`.

--- nx-dev/nx-dev/pages/_app.tsx
+++ nx-dev/nx-dev/pages/_app.tsx
@@ -106,7 +106,7 @@ export default function CustomApp({
       </Link>
       <Component {...pageProps} />
       {/* <LiveStreamNotifier /> */}
-      <WebinarNotifier />
+      {/*<WebinarNotifier />*/}
 
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
