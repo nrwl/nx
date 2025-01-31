31f01bc02384a246e6441c2ab24343f6eeb8ba3c
Juri <juri.strumpflohner@gmail.com>
2025-01-31 15:54:58 +0100

    fix(nx-dev): revert show alt text as label below markdown images
    This reverts commit a4f07dbb641d655d65128acd6ff2330b1068ec62.

--- nx-dev/ui-markdoc/src/lib/nodes/image.schema.ts
+++ nx-dev/ui-markdoc/src/lib/nodes/image.schema.ts
@@ -4,7 +4,7 @@ import markdoc from '@markdoc/markdoc';
 const { Tag } = markdoc;
 
 export const getImageSchema = (documentFilePath: string): Schema => ({
-  render: 'figure',
+  render: 'img',
   attributes: {
     src: { type: 'String', required: true },
     alt: { type: 'String', required: true },
@@ -13,15 +13,11 @@ export const getImageSchema = (documentFilePath: string): Schema => ({
     const attributes = node.transformAttributes(config);
     const children = node.transformChildren(config);
     const src = transformImagePath(documentFilePath)(attributes['src']);
-    const alt = attributes['alt'];
 
-    return new Tag(this.render, { className: 'not-prose my-8 text-center' }, [
-      new Tag('img', { src, alt, loading: 'lazy', className: 'mx-auto !my-0' }),
-      new Tag(
-        'figcaption',
-        { className: 'mt-2 text-sm text-slate-600 dark:text-slate-400' },
-        [alt]
-      ),
-    ]);
+    return new Tag(
+      this.render,
+      { ...attributes, src, loading: 'lazy' },
+      children
+    );
   },
 });
