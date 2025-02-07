33352bc970c05274ab5263e354c912926956a85b
Isaac Mann <isaacplmann@users.noreply.github.com>
2025-02-07 14:22:52 -0500

    feat(nx-dev): webinar page (#29913)
    Adds a webinar page and a script to pull the webinar content from Notion

--- nx-dev/ui-common/src/lib/headers/menu-items.ts
+++ nx-dev/ui-common/src/lib/headers/menu-items.ts
@@ -203,7 +203,7 @@ export const learnItems: MenuItem[] = [
   {
     name: 'Webinars',
     description: null,
-    href: 'https://go.nx.dev/jan2025-webinar',
+    href: '/webinar',
     icon: ComputerDesktopIcon,
     isNew: false,
     isHighlight: false,
