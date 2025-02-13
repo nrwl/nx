05ac00f02f059983c0fe3914abdffebe3232fbca
Stefan Haas <stefan.haas.privat@gmail.com>
2025-02-14 00:48:30 +0100

    formatting

--- packages/devkit/src/utils/semver.ts
+++ packages/devkit/src/utils/semver.ts
@@ -1,25 +1,25 @@
 import { valid } from 'semver';
 
 export function checkAndCleanWithSemver(
-    pkgName: string,
-    version: string
+  pkgName: string,
+  version: string
 ): string {
-    let newVersion = version;
-    const isPnpmCatalog = newVersion.startsWith('catalog:');
+  let newVersion = version;
+  const isPnpmCatalog = newVersion.startsWith('catalog:');
 
-    if (valid(newVersion) || isPnpmCatalog) {
-        return newVersion;
-    }
-
-    if (version.startsWith('~') || version.startsWith('^')) {
-        newVersion = version.substring(1);
-    }
-
-    if (!valid(newVersion)) {
-        throw new Error(
-            `The package.json lists a version of ${pkgName} that Nx is unable to validate - (${version})`
-        );
-    }
-
+  if (valid(newVersion) || isPnpmCatalog) {
     return newVersion;
+  }
+
+  if (version.startsWith('~') || version.startsWith('^')) {
+    newVersion = version.substring(1);
+  }
+
+  if (!valid(newVersion)) {
+    throw new Error(
+      `The package.json lists a version of ${pkgName} that Nx is unable to validate - (${version})`
+    );
+  }
+
+  return newVersion;
 }
