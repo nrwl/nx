608a369e6ef67436b6df4f8ed0e8a10231ac4e74
Stefan Haas <stefan.haas.privat@gmail.com>
2025-02-14 11:05:29 +0100

    fix(devkit): support pnpm catalogs

--- packages/devkit/src/utils/semver.ts
+++ packages/devkit/src/utils/semver.ts
@@ -11,6 +11,10 @@ export function checkAndCleanWithSemver(
     return newVersion;
   }
 
+  if (version.startsWith('~') || version.startsWith('^')) {
+    newVersion = version.substring(1);
+  }
+
   if (!valid(newVersion)) {
     throw new Error(
       `The package.json lists a version of ${pkgName} that Nx is unable to validate - (${version})`
