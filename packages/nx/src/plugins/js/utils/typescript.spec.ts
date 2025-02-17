8c6c53dedd0344a3a3ebb6ed2b5cb621ac61e221
Ethan Koenig <ethantkoenig@gmail.com>
2025-02-17 18:24:05 +0000

    fix(core): third-party test

--- packages/nx/src/plugins/js/utils/typescript.spec.ts
+++ packages/nx/src/plugins/js/utils/typescript.spec.ts
@@ -10,13 +10,29 @@ describe('readTsConfigOptions', () => {
   afterEach(() => {
     fs.cleanup();
   });
-  it('should handle extends', async () => {
+
+  it('should handle extending local configs', async () => {
     await fs.createFiles({
       'a.json': JSON.stringify({ extends: './b.json' }),
       'b.json': JSON.stringify({ compilerOptions: { strict: true } }),
     });
 
     expect(readTsConfigOptions(join(fs.tempDir, 'a.json'))).toEqual({
+      configFilePath: undefined,
+      strict: true,
+    });
+  });
+
+  it('should handle extending third-party configs', async () => {
+    await fs.createFiles({
+      'tsconfig.json': JSON.stringify({
+        extends: '@fake-third-party/some-package/tsconfig.json',
+      }),
+      'node_modules/@fake-third-party/some-package/tsconfig.json':
+        JSON.stringify({ compilerOptions: { strict: true } }),
+    });
+
+    expect(readTsConfigOptions(join(fs.tempDir, 'tsconfig.json'))).toEqual({
       configFilePath: undefined,
       strict: true,
     });
