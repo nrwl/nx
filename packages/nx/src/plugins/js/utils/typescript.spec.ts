nrwl/nx

commit 9b70596272de3efdcbfa6a2d15517c28fa3111c8

fix(core): reduce number of processed files when creating in-memory transpiler (#29774)
## Current Behavior

All of the TS source trees are unnecessarily traversed when a local
plugin is being registered.

## Expected Behavior

TS source trees are not unnecessarily traversed.

## Related Issue(s)

Fixes #29102 (see issue for more context)

packages/nx/src/plugins/js/utils/typescript.ts
@@ -23,7 +23,7 @@ export function readTsConfig(tsConfigPath: string) {
   );
 }
 
-function readTsConfigOptions(tsConfigPath: string) {
+export function readTsConfigOptions(tsConfigPath: string) {
   if (!tsModule) {
     tsModule = require('typescript');
   }

packages/nx/src/plugins/js/utils/register.ts
@@ -293,8 +293,7 @@ export function getTranspiler(
  * @returns cleanup method
  */
 export function registerTranspiler(
-  compilerOptions: CompilerOptions,
-  tsConfigRaw?: unknown
+  compilerOptions: CompilerOptions
 ): () => void {
   // Function to register transpiler that returns cleanup function
   const transpiler = getTranspiler(compilerOptions);

packages/nx/src/project-graph/plugins/transpiler.ts
@@ -5,7 +5,7 @@ import {
   registerTranspiler,
   registerTsConfigPaths,
 } from '../../plugins/js/utils/register';
-import { readTsConfig } from '../../plugins/js/utils/typescript';
+import { readTsConfigOptions } from '../../plugins/js/utils/typescript';
 import type * as ts from 'typescript';
 
 export let unregisterPluginTSTranspiler: (() => void) | null = null;
@@ -25,19 +25,16 @@ export function registerPluginTSTranspiler() {
     return;
   }
 
-  const tsConfig: Partial<ts.ParsedCommandLine> = tsConfigName
-    ? readTsConfig(tsConfigName)
+  const tsConfigOptions: Partial<ts.CompilerOptions> = tsConfigName
+    ? readTsConfigOptions(tsConfigName)
     : {};
   const cleanupFns = [
     registerTsConfigPaths(tsConfigName),
-    registerTranspiler(
-      {
-        experimentalDecorators: true,
-        emitDecoratorMetadata: true,
-        ...tsConfig.options,
-      },
-      tsConfig.raw
-    ),
+    registerTranspiler({
+      experimentalDecorators: true,
+      emitDecoratorMetadata: true,
+      ...tsConfigOptions,
+    }),
   ];
   unregisterPluginTSTranspiler = () => {
     cleanupFns.forEach((fn) => fn?.());

commit 9cdc1ccba4ae948c9735de47c2a4c2db86eba9e4

fix(core): respect `extends` when reading tsconfig options (#30062)
Fix a bug introduced in https://github.com/nrwl/nx/pull/29774.

## Current Behavior

If a local plugin's `tsconfig.json` uses `extends`, the referenced base
config is not loaded.

## Expected Behavior

If a local plugin's `tsconfig.json` uses `extends`, the referenced base
config is loaded.

## Related Issue(s)

Fixes #30007

---------

Co-authored-by: Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>

packages/nx/src/plugins/js/utils/typescript.ts
@@ -33,16 +33,16 @@ export function readTsConfigOptions(tsConfigPath: string) {
     tsModule.sys.readFile
   );
 
-  // we don't need to scan the files, we only care about options
-  const host: Partial<ts.ParseConfigHost> = {
+  // We only care about options, so we don't need to scan source files, and thus
+  // `readDirectory` is stubbed for performance.
+  const host = {
+    ...tsModule.sys,
     readDirectory: () => [],
-    readFile: () => '',
-    fileExists: tsModule.sys.fileExists,
   };
 
   return tsModule.parseJsonConfigFileContent(
     readResult.config,
-    host as ts.ParseConfigHost,
+    host,
     dirname(tsConfigPath)
   ).options;
 }

packages/nx/src/plugins/js/utils/typescript.spec.ts
@@ -1,0 +1,40 @@
+import { readTsConfigOptions } from './typescript';
+import { join } from 'path';
+import { TempFs } from '../../../internal-testing-utils/temp-fs';
+
+describe('readTsConfigOptions', () => {
+  let fs: TempFs;
+  beforeEach(() => {
+    fs = new TempFs('Workspaces');
+  });
+  afterEach(() => {
+    fs.cleanup();
+  });
+
+  it('should handle extending local configs', async () => {
+    await fs.createFiles({
+      'a.json': JSON.stringify({ extends: './b.json' }),
+      'b.json': JSON.stringify({ compilerOptions: { strict: true } }),
+    });
+
+    expect(readTsConfigOptions(join(fs.tempDir, 'a.json'))).toEqual({
+      configFilePath: undefined,
+      strict: true,
+    });
+  });
+
+  it('should handle extending third-party configs', async () => {
+    await fs.createFiles({
+      'tsconfig.json': JSON.stringify({
+        extends: '@fake-third-party/some-package/tsconfig.json',
+      }),
+      'node_modules/@fake-third-party/some-package/tsconfig.json':
+        JSON.stringify({ compilerOptions: { strict: true } }),
+    });
+
+    expect(readTsConfigOptions(join(fs.tempDir, 'tsconfig.json'))).toEqual({
+      configFilePath: undefined,
+      strict: true,
+    });
+  });
+});