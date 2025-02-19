a473fa277aa575abc1fbec7614e75ba112057c4c
JamesHenry <james@henry.sc>
2025-02-19 12:28:59 +0400

    chore(core): remove dead code, try updating SHELL

--- packages/expo/src/generators/application/lib/create-application-files.ts
+++ packages/expo/src/generators/application/lib/create-application-files.ts
@@ -2,40 +2,21 @@ import {
   detectPackageManager,
   generateFiles,
   offsetFromRoot,
-  PackageManager,
   toJS,
   Tree,
 } from '@nx/devkit';
-import { execSync } from 'node:child_process';
 import {
   createNxCloudOnboardingURLForWelcomeApp,
   getNxCloudAppOnBoardingUrl,
 } from 'nx/src/nx-cloud/utilities/onboarding';
 import { join } from 'path';
-import { gte } from 'semver';
 import { NormalizedSchema } from './normalize-options';
 
 export async function createApplicationFiles(
   host: Tree,
   options: NormalizedSchema
 ) {
-  const packageManagerLockFile: Record<PackageManager, string> = {
-    npm: 'package-lock.json',
-    yarn: 'yarn.lock',
-    pnpm: 'pnpm-lock.yaml',
-    bun: (() => {
-      try {
-        // In version 1.2.0, bun switched to a text based lockfile format by default
-        return gte(execSync('bun --version').toString().trim(), '1.2.0')
-          ? 'bun.lock'
-          : 'bun.lockb';
-      } catch {
-        return 'bun.lockb';
-      }
-    })(),
-  };
   const packageManager = detectPackageManager(host.root);
-  const packageLockFile = packageManagerLockFile[packageManager];
 
   const onBoardingStatus = await createNxCloudOnboardingURLForWelcomeApp(
     host,
@@ -54,7 +35,6 @@ export async function createApplicationFiles(
       ...options,
       offsetFromRoot: offsetFromRoot(options.appProjectRoot),
       packageManager,
-      packageLockFile,
     }
   );
 
@@ -67,7 +47,6 @@ export async function createApplicationFiles(
       connectCloudUrl,
       offsetFromRoot: offsetFromRoot(options.appProjectRoot),
       packageManager,
-      packageLockFile,
     }
   );
 
