d536959968e6d5eccbbe452b151e9bea9895bc80
Isaac Mann <isaacplmann@gmail.com>
2025-02-19 14:29:22 -0500

    docs(core): update module boundaries step

--- nx-dev/tutorial/src/content/tutorial/3-angular-monorepo/3a-fast-ci/1-welcome/_files/eslint.config.mjs
+++ nx-dev/tutorial/src/content/tutorial/3-angular-monorepo/3a-fast-ci/1-welcome/_files/eslint.config.mjs
@@ -17,8 +17,28 @@ export default [
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
           depConstraints: [
             {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
+              sourceTag: "type:feature",
+              onlyDependOnLibsWithTags: ["type:feature", "type:ui"]
+            },
+            {
+              sourceTag: "type:ui",
+              onlyDependOnLibsWithTags: ["type:ui"]
+            },
+            {
+              sourceTag: "scope:orders",
+              onlyDependOnLibsWithTags: [
+                "scope:orders",
+                "scope:products",
+                "scope:shared"
+              ]
+            },
+            {
+              sourceTag: "scope:products",
+              onlyDependOnLibsWithTags: ["scope:products", "scope:shared"]
+            },
+            {
+              sourceTag: "scope:shared",
+              onlyDependOnLibsWithTags: ["scope:shared"]
             },
           ],
         },
