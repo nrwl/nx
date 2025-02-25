e6676eb45506949325c8ec706cf5a2722dbec773
Joshua Carter <carter.joshua.603@gmail.com>
2025-02-25 07:00:45 -0800

    fix(js): Use correctly resolved path to tsconfig in readTsConfigWithRemappedPaths()

--- packages/js/src/utils/buildable-libs-utils.ts
+++ packages/js/src/utils/buildable-libs-utils.ts
@@ -211,7 +211,7 @@ function readTsConfigWithRemappedPaths(
     normalizedTsConfig
   );
   generatedTsConfig.compilerOptions.paths = computeCompilerOptionsPaths(
-    originalTsconfigPath,
+    normalizedTsConfig,
     dependencies
   );
 
