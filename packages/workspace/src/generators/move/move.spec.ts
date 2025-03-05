e21db8382f0f61794036db5d26d0b7897340d7b5
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2025-03-05 12:59:43 +0100

    fix(core): handle nx config in package.json in move generator

--- packages/workspace/src/generators/move/move.spec.ts
+++ packages/workspace/src/generators/move/move.spec.ts
@@ -4,13 +4,10 @@ import {
   readJson,
   readProjectConfiguration,
   Tree,
-  updateJson,
   updateProjectConfiguration,
 } from '@nx/devkit';
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
 import { moveGenerator } from './move';
-// nx-ignore-next-line
-const { applicationGenerator } = require('@nx/react');
 
 // nx-ignore-next-line
 const { libraryGenerator } = require('@nx/js');
@@ -72,8 +69,9 @@ describe('move', () => {
       destination: 'shared/two',
     });
 
+    // check that the project.json file is present
+    expect(tree.exists('shared/two/project.json')).toBeTruthy();
     const myLibNewConfig = readProjectConfiguration(tree, 'two');
-
     expect(myLibNewConfig.targets.custom.options.buildTarget).toEqual(
       'two:build:production'
     );
@@ -83,6 +81,8 @@ describe('move', () => {
     expect(myLibNewConfig.targets.custom.options.irrelevantTarget).toEqual(
       'my-lib:build:production'
     );
+    // check that the package.json does not have nx config
+    expect(readJson(tree, 'shared/two/package.json').nx).toBeUndefined();
   });
 
   it('should update jest config when moving up directories', async () => {
@@ -144,5 +144,55 @@ describe('move', () => {
     });
 
     expect(tree.exists('tsconfig.base.json')).toBeFalsy();
+  });
+
+  it('should correctly update the nx configuration when it is located in the package.json file', async () => {
+    await libraryGenerator(tree, {
+      directory: 'libs/lib1',
+      useProjectJson: false,
+      skipFormat: true,
+    });
+    updateProjectConfiguration(tree, 'lib1', {
+      root: 'libs/lib1',
+      targets: {
+        foo: {
+          command: 'echo "foo"',
+          options: {
+            cwd: 'libs/lib1',
+          },
+        },
+      },
+    });
+
+    await moveGenerator(tree, {
+      projectName: 'lib1',
+      destination: 'packages/lib1',
+      updateImportPath: true,
+      skipFormat: true,
+    });
+
+    // check that the nx config in the package.json is correct
+    const packageJson = readJson(tree, 'packages/lib1/package.json');
+    expect(packageJson.nx).toMatchInlineSnapshot(`
+      {
+        "name": "lib1",
+        "projectType": "library",
+        "sourceRoot": "packages/lib1/src",
+        "targets": {
+          "foo": {
+            "command": "echo "foo"",
+            "options": {
+              "cwd": "packages/lib1",
+            },
+          },
+        },
+      }
+    `);
+    // check that we read the project configuration with the updated paths from the package.json
+    const myLibNewConfig = readProjectConfiguration(tree, 'lib1');
+    expect(myLibNewConfig.sourceRoot).toEqual('packages/lib1/src');
+    expect(myLibNewConfig.targets.foo.options.cwd).toEqual('packages/lib1');
+    // check that the project.json file is not present
+    expect(tree.exists('packages/lib1/project.json')).toBeFalsy();
   });
 });
