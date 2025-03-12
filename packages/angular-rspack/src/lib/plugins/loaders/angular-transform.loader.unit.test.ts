ab49f89f8fc2dc2586ba4b64f1c4649e37453f67
Michael Hladky <10064416+BioPhoton@users.noreply.github.com>
2025-03-12 15:28:25 +0100

    test(angular-rspack): add unit tests for angular loader (#17)
    * test(angular-rspack): add unit tests for angular-transform-loader
    
    * test(angular-rspack): add unit tests for angular-transform-loader
    
    * fix: format
    
    * fix: wip
    
    * fix: test change
    
    * fix: test change 2

---
+++ packages/angular-rspack/src/lib/plugins/loaders/angular-transform.loader.unit.test.ts
@@ -0,0 +1,125 @@
+import { beforeEach, describe, expect, vi } from 'vitest';
+import { default as angularTransformLoader } from './angular-transform.loader';
+import { NG_RSPACK_SYMBOL_NAME, type NgRspackCompilation } from '../../models';
+import type { LoaderContext } from '@rspack/core';
+
+class StyleUrlsResolverMock {
+  constructor(private cb: (content: string) => string) {}
+
+  resolve(content: string, _: string): string[] {
+    return [this.cb(content)];
+  }
+}
+
+class TemplateUrlsResolverMock {
+  constructor(private cb: (content: string) => string) {}
+
+  resolve(content: string, _: string): string[] {
+    return [this.cb(content)];
+  }
+}
+
+describe('angular-transform.loader', () => {
+  const callback = vi.fn();
+  const addDependency = vi.fn();
+  const typescriptFileCache = new Map<string, string | Buffer>();
+  const _compilation = {
+    [NG_RSPACK_SYMBOL_NAME]: () => ({
+      typescriptFileCache,
+    }),
+  } as unknown as NgRspackCompilation;
+  const thisValue = {
+    async: vi.fn(() => callback),
+    _compilation: {},
+  } as unknown as LoaderContext<unknown>;
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should return content when NG_RSPACK_SYMBOL_NAME is undefined', () => {
+    angularTransformLoader.call(thisValue, 'content');
+    expect(callback).toHaveBeenCalledWith(null, 'content');
+  });
+
+  it('should add dependencies for resolved template and style URLs', () => {
+    angularTransformLoader.call(
+      {
+        ...thisValue,
+        _compilation,
+        resourcePath: '/home/projects/analog/src/app/app.component.ts',
+        addDependency,
+      },
+      '@Component()',
+      {
+        templateUrlsResolver: new TemplateUrlsResolverMock(
+          () =>
+            './app.component.html|/home/projects/analog/src/app/app.component.html'
+        ),
+        styleUrlsResolver: new StyleUrlsResolverMock(
+          () =>
+            './app.component.css|/home/projects/analog/src/app/app.component.css'
+        ),
+      } as any
+    );
+
+    expect(addDependency).toHaveBeenCalledTimes(2);
+    expect(addDependency).toHaveBeenNthCalledWith(
+      1,
+      '/home/projects/analog/src/app/app.component.html'
+    );
+    expect(addDependency).toHaveBeenNthCalledWith(
+      2,
+      '/home/projects/analog/src/app/app.component.css'
+    );
+  });
+
+  it('should return content when typescriptFileCache does not contain normalizedRequest', () => {
+    angularTransformLoader.call(
+      {
+        ...thisValue,
+        _compilation,
+        resourcePath: '/path/to/file.ts',
+      },
+      'content'
+    );
+
+    expect(callback).toHaveBeenCalledWith(null, 'content');
+  });
+
+  it('should return content when typescriptFileCache does contain string', () => {
+    typescriptFileCache.set(
+      '/home/projects/analog/src/app/app.component.ts',
+      'cached content'
+    );
+
+    angularTransformLoader.call(
+      {
+        ...thisValue,
+        _compilation,
+        resourcePath: '/home/projects/analog/src/app/app.component.ts',
+      },
+      '@Component()'
+    );
+
+    expect(callback).toHaveBeenCalledWith(null, 'cached content');
+  });
+
+  it('should return content when typescriptFileCache does contain Buffer', () => {
+    typescriptFileCache.set(
+      '/home/projects/analog/src/app/app.component.ts',
+      Buffer.from('cached content')
+    );
+
+    angularTransformLoader.call(
+      {
+        ...thisValue,
+        _compilation,
+        resourcePath: '/home/projects/analog/src/app/app.component.ts',
+      },
+      '@Component()'
+    );
+
+    expect(callback).toHaveBeenCalledWith(null, Buffer.from('cached content'));
+  });
+});
