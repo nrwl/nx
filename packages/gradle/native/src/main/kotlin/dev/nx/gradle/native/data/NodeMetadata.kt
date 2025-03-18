b1b0f85a10d0dcab720b2ad212ad53d720009336
Emily Xiong <xiongemi@gmail.com>
2025-03-18 01:04:47 -0400

    feat(gradle): rename plugin

--- packages/gradle/native/src/main/kotlin/dev/nx/gradle/native/data/NodeMetadata.kt
+++
@@ -1,9 +0,0 @@
-package dev.nx.gradle.native.data
-
-import java.io.Serializable
-
-data class NodeMetadata(
-    val targetGroups: TargetGroups,
-    val technologies: List<String>,
-    val description: String?
-) : Serializable
