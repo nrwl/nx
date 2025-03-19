e0cae539d6ff70d6968ca87af7ed3fd8d866435e
Jack Hsu <jack.hsu@gmail.com>
2025-03-19 09:20:18 -0400

    fix(js): update tinyglobby to speed up shallow file matching (#30415)
    `tinyglobby` at `0.2.10` (what we use now) is slow on shallow files, but
    the latest version `0.2.12` is fast due to this PR
    https://github.com/SuperchupuDev/tinyglobby/pull/69/files.
    
    This PR updates both the js and esbuild plugins to use the newest
    versions, but also adds `tinyglobby@^0.2.12` to our root `package.json`
    so we get the speed increase right away. I removed `fast-glob` in our
    repo scripts and replaced it with `tinyglobby`.
    
    ## Current Behavior
    Asset handling is slow for shallow files like `LICENSE` but is fine for
    scoped patterns like `src/**/*.ts`.
    
    ## Expected Behavior
    Asset handling should be fast for shallow files.
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #

--- scripts/copy-local-native.js
+++ scripts/copy-local-native.js
@@ -1,10 +1,10 @@
 //@ts-check
 const fs = require('fs');
-const glob = require('fast-glob');
+const glob = require('tinyglobby');
 
 const p = process.argv[2];
 
-const nativeFiles = glob.sync(`packages/${p}/**/*.{node,wasm,js,mjs,cjs}`);
+const nativeFiles = glob.globSync(`packages/${p}/**/*.{node,wasm,js,mjs,cjs}`);
 
 console.log({ nativeFiles });
 
