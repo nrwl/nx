38da70478ed3ed72610a2fd711a2a3e85317fbcd
JamesHenry <james@henry.sc>
2025-03-19 19:33:14 +0400

    fix(devkit): add check for NODE_ENV=test

--- packages/devkit/src/generators/format-files.ts
+++ packages/devkit/src/generators/format-files.ts
@@ -25,12 +25,16 @@ export async function formatFiles(
      * Even after we discovered prettier in node_modules, we need to be sure that the user is intentionally using prettier
      * before proceeding to format with it. Therefore we attempt to resolve a config file from the root of the workspace.
      * The promise will reject if there is an issue processing the config file, and it will resolve to null if there is no file.
+     *
+     * To avoid a breaking change for unit tests in Nx v20, we do not apply this check when NODE_ENV=test.
      */
-    const resolvedConfig = await prettier.resolveConfig(
-      path.join(tree.root, 'package.json')
-    );
-    if (!resolvedConfig) {
-      return;
+    if (process.env.NODE_ENV !== 'test') {
+      const resolvedConfig = await prettier.resolveConfig(
+        path.join(tree.root, 'package.json')
+      );
+      if (!resolvedConfig) {
+        return;
+      }
     }
   } catch {}
 
