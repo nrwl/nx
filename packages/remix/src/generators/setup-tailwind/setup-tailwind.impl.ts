93b2be74ab583da4d73951885709899e9594735a
Nicholas Cunningham <ndcunningham@gmail.com>
2025-04-02 14:35:40 -0600

    fix(remix): update setup-tailwind generator to correct import statements:
    closes: #30343

--- packages/remix/src/generators/setup-tailwind/setup-tailwind.impl.ts
+++ packages/remix/src/generators/setup-tailwind/setup-tailwind.impl.ts
@@ -5,39 +5,34 @@ import {
   installPackagesTask,
   joinPathFragments,
   readProjectConfiguration,
+  stripIndents,
   type Tree,
 } from '@nx/devkit';
 
-import { upsertLinksFunction } from '../../utils/upsert-links-function';
 import {
   autoprefixerVersion,
   postcssVersion,
   tailwindVersion,
 } from '../../utils/versions';
 import type { SetupTailwindSchema } from './schema';
+import { insertStatementAfterImports } from '../../utils/insert-statement-after-imports';
 
 export default async function setupTailwind(
   tree: Tree,
   options: SetupTailwindSchema
 ) {
   const project = readProjectConfiguration(tree, options.project);
-  if (project.projectType !== 'application') {
-    throw new Error(
-      `Project "${options.project}" is not an application. Please ensure the project is an application.`
-    );
-  }
 
   generateFiles(tree, joinPathFragments(__dirname, 'files'), project.root, {
     tpl: '',
   });
 
   const pathToRoot = joinPathFragments(project.root, 'app/root.tsx');
-  upsertLinksFunction(
+
+  insertStatementAfterImports(
     tree,
     pathToRoot,
-    'twStyles',
-    './tailwind.css',
-    `{ rel: "stylesheet", href: twStyles }`
+    stripIndents`import './tailwind.css';`
   );
 
   addDependenciesToPackageJson(
