8f67c63ca00e29b8d96f01d335f1cf56081810c0
Colum Ferry <cferry09@gmail.com>
2025-04-02 11:11:03 +0100

    feat(storybook): migrate to latest storybook 8 version

--- packages/storybook/src/utils/versions.ts
+++ packages/storybook/src/utils/versions.ts
@@ -1,13 +1,13 @@
 export const nxVersion = require('../../package.json').version;
-export const storybookTestRunnerVersion = '^0.19.0';
+export const storybookTestRunnerVersion = '^0.22.0';
 export const storybookTestingLibraryVersion = '^0.2.2';
 export const storybookJestVersion = '^0.2.3';
 export const litVersion = '^2.6.1';
 export const tsNodeVersion = '10.9.1';
 export const tsLibVersion = '^2.3.0';
 
-export const storybookVersion = '^8.4.6';
+export const storybookVersion = '^8.6.11';
 export const reactVersion = '^18.2.0';
-export const viteVersion = '^5.0.0';
+export const viteVersion = '^6.0.0';
 
 export const coreJsVersion = '^3.36.1';
