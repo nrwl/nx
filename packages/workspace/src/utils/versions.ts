94172a516d8bc25e40a8752b0d7c4aa885b4939e
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2025-04-16 15:03:24 +0200

    feat(angular): support angular 20.0.0-next.5

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -1,8 +1,8 @@
 export const nxVersion = require('../../package.json').version;
 
-export const typescriptVersion = '~5.7.2';
+export const typescriptVersion = '~5.8.2';
 
 // TODO: remove when preset generation is reworked and
 // deps are not installed from workspace
-export const angularCliVersion = '~19.2.0';
+export const angularCliVersion = '~20.0.0-next.5';
 export const angularRspackVersion = '^21.0.1';
