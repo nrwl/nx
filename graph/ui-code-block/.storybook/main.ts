ba621b65aee2089f0d31b4d61ea3b26e8f7159d5
Jack Hsu <jack.hsu@gmail.com>
2025-04-16 15:02:44 -0400

    chore(repo): remove vite paths plugin from our own Storybook files

--- graph/ui-code-block/.storybook/main.ts
+++ graph/ui-code-block/.storybook/main.ts
@@ -1,8 +1,6 @@
 /* eslint-disable @nx/enforce-module-boundaries */
 import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
-// nx-ignore-next-line
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 const config: StorybookConfig = {
   stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
@@ -15,7 +13,7 @@ const config: StorybookConfig = {
 
   viteFinal: async (config) =>
     mergeConfig(config, {
-      plugins: [nxViteTsPaths()],
+      plugins: [],
     }),
 };
 
