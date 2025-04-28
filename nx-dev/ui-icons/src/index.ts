d08775f168ea578f08c7b74d5fa1ae1cd4885106
Jordan Powell <jordan@jpdesigning.com>
2025-04-28 11:10:52 -0400

    docs(core): add briebug cloud and update pushbased

--- nx-dev/ui-icons/src/index.ts
+++ nx-dev/ui-icons/src/index.ts
@@ -95,6 +95,7 @@ export * from './lib/partners/callstack';
 export * from './lib/partners/hero-devs';
 export * from './lib/partners/push-based';
 export * from './lib/partners/e-square';
+export * from './lib/partners/briebug';
 
 // PODCASTS
 export * from './lib/podcasts/amazon-music';
