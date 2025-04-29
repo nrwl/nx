e21c1a60102765b15e303c3312b43d96c1623c52
Jack Hsu <jack.hsu@gmail.com>
2025-04-29 09:57:16 -0400

    feat(react): replace deprecated babel classProperties.loose option in .babelrc files (#30912)
    This PR updates all `.babelrc` file that still uses the deprecated
    `classProperties.loose` (deprecated since v18). The top-level `loose`
    option does the same thing -- previously the two options handled
    different cases because `@babel/preset-env` did not include class
    properties.

---
+++ packages/react/src/migrations/update-21-0-0/update-babel-loose.ts
@@ -0,0 +1,31 @@
+import {
+  formatFiles,
+  type Tree,
+  updateJson,
+  visitNotIgnoredFiles,
+} from '@nx/devkit';
+
+export default async function updateBabelLoose(tree: Tree) {
+  visitNotIgnoredFiles(tree, '', (path) => {
+    if (!path.endsWith('.babelrc')) return;
+    try {
+      updateJson(tree, path, (babelConfig) => {
+        if (!Array.isArray(babelConfig.presets)) return;
+        const ourPreset = babelConfig.presets.find(
+          (p) => Array.isArray(p) && p[0] === '@nx/react/babel'
+        );
+        if (!ourPreset || !ourPreset[1]) return;
+        const options = ourPreset[1];
+        if (options['classProperties']?.loose !== undefined) {
+          options.loose = options['classProperties'].loose;
+          delete options['classProperties'];
+        }
+        return babelConfig;
+      });
+    } catch {
+      // Skip if JSON does not parse for whatever reason
+      return;
+    }
+  });
+  await formatFiles(tree);
+}
