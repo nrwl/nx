eb9f8f46d3febf587f5ea0010e57601761c92127
Colum Ferry <cferry09@gmail.com>
2025-05-01 10:13:13 +0100

    feat(module-federation): bump @module-federation/enhanced to 0.13.0 to fix koa vulnerability

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -16,7 +16,7 @@ export const expressVersion = '^4.21.2';
 export const typesExpressVersion = '^4.17.21';
 export const browserSyncVersion = '^3.0.0';
 export const moduleFederationNodeVersion = '^2.6.26';
-export const moduleFederationEnhancedVersion = '^0.9.0';
+export const moduleFederationEnhancedVersion = '^0.13.0';
 
 export const angularEslintVersion = '^19.2.0';
 export const typescriptEslintVersion = '^7.16.0';
