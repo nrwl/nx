a612a92df921a60bf6e5454daa77b530dbae01e4
Tony133 <a.tripodi133@gmail.com>
2025-05-01 15:34:20 +0200

    chore: update nestjs version 11.1.x

--- packages/nest/src/utils/versions.ts
+++ packages/nest/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = require('../../package.json').version;
 
-export const nestJsVersion = '^11.0.10';
+export const nestJsVersion = '^11.1.0';
 export const nestJsSchematicsVersion = '^11.0.0';
 export const rxjsVersion = '^7.8.1';
 export const reflectMetadataVersion = '^0.2.2';
