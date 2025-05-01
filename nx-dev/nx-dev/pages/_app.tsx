533523fa1ef1972c423809900b7b41bc2ec7c121
Benjamin Cabanes <3447705+bcabanes@users.noreply.github.com>
2025-05-01 15:25:05 -0400

    docs(nx-dev): add Google Tag Manager script
    Integrated Google Tag Manager (GTM) with both script and noscript tags for analytics tracking. Ensured the script uses `afterInteractive` loading strategy and includes dynamic `gaMeasurementId` for proper configuration.

--- nx-dev/nx-dev/pages/_app.tsx
+++ nx-dev/nx-dev/pages/_app.tsx
@@ -131,6 +131,29 @@ export default function CustomApp({
           `,
         }}
       />
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gaMeasurementId}');
+          `,
+        }}
+      />
+      {/* Google Tag Manager - NoScript */}
+      <noscript>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=${gaMeasurementId}`}
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
+
       {/* Apollo.io Embed Code */}
       <Script
         type="text/javascript"
