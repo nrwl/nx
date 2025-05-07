980256d89103910f146e793eb96e43d0b60f352a
Colum Ferry <cferry09@gmail.com>
2025-05-07 15:23:58 +0100

    feat(angular-rspack,angular-rspack-compiler): reuse existing compilation if exists (#77)
    * feat(angular-rspack,angular-rspack-compiler): reuse existing compilation if exists
    
    * feat(angular-rspack,angular-rspack-compiler): ensure only affected file is rebuilt

--- packages/angular-rspack/src/lib/models/augmented-compilation.ts
+++ packages/angular-rspack/src/lib/models/augmented-compilation.ts
@@ -1,12 +1,15 @@
 import type { Compilation } from '@rspack/core';
-import type { JavaScriptTransformer } from '@nx/angular-rspack-compiler';
+import type {
+  JavaScriptTransformer,
+  SourceFileCache,
+} from '@nx/angular-rspack-compiler';
 import { I18nOptions } from './i18n';
 
 export const NG_RSPACK_SYMBOL_NAME = 'NG_RSPACK_BUILD';
 
 export type NG_RSPACK_COMPILATION_STATE = {
   javascriptTransformer: JavaScriptTransformer;
-  typescriptFileCache: Map<string, string | Buffer>;
+  typescriptFileCache: SourceFileCache['typeScriptFileCache'];
   i18n?: I18nOptions;
 };
 export type NgRspackCompilation = Compilation & {
