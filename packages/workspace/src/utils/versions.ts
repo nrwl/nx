9aee5b7adf1defa15082f9a2a6689b0d634ff283
Colum Ferry <cferry09@gmail.com>
2025-05-14 15:26:41 +0100

    feat(angular): handle prerender and appshell in covert to rspack (#31210)
    ## Current Behavior
    The `convert-to-rspack` generator does not handle projects that use
    Prerendering or App Shell
    
    ## Expected Behavior
    The generator sets up Prerendering and App Shell
    
    Fixes https://github.com/nrwl/angular-rspack/issues/88

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -5,3 +5,4 @@ export const typescriptVersion = '~5.7.2';
 // TODO: remove when preset generation is reworked and
 // deps are not installed from workspace
 export const angularCliVersion = '~19.2.0';
+export const angularRspackVersion = '^20.9.0';
