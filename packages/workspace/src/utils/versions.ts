25020ee336c108062a65f658703ea406170ee2fc
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2025-05-16 14:58:19 +0200

    feat(angular): support angular 20.0.0-rc.2 (#31247)
    Update Angular packages to their latest prerelease versions.

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -4,4 +4,4 @@ export const typescriptVersion = '~5.8.2';
 
 // TODO: remove when preset generation is reworked and
 // deps are not installed from workspace
-export const angularCliVersion = '20.0.0-rc.0';
+export const angularCliVersion = '20.0.0-rc.2';
