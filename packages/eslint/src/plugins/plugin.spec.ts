d78782da492e2838828b7edebf615d9d2600aefc
Miroslav Jonaš <missing.manual@gmail.com>
2025-05-22 15:59:25 +0200

    fix(linter): speed up inferred plugin node processing (#31281)
    This PR improves the **createNodes** function of eslint's inferred
    plugin by making two pragmatic choices:
    - reusing the ESLint between config file's runs instead of recreating
    the new one every time
    - skipping ignored files checks for projects that already have eslint
    config file
    
    ## Results of benchmarks on customer's repo:
    
    ### Without ESLint plugin
    - create-project-graph-async - avg. 11739.1326225 -> 11 seconds
    ### With current ESLint plugin
    - create-project-graph-async - avg. 98005.0965135 -> 98 seconds
    ### With modified ESLint plugin
    - create-project-graph-async - avg. 13225.073817  -> 13 seconds
      - (@nx/eslint/plugin:createNodes - 2206.96497, 16.69%)
    
    
    ## Current Behavior
    <!-- This is the behavior we have today -->
    
    ## Expected Behavior
    <!-- This is the behavior we should expect with the changes in this PR
    -->
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #

--- packages/eslint/src/plugins/plugin.spec.ts
+++ packages/eslint/src/plugins/plugin.spec.ts
@@ -518,43 +518,45 @@ describe('@nx/eslint/plugin', () => {
       `);
     });
 
-    it('should not create nodes for nested projects without a root level eslint config when all files are ignored (.eslintignore)', async () => {
-      createFiles({
-        'apps/my-app/.eslintrc.json': `{}`,
-        'apps/my-app/.eslintignore': `**/*`,
-        'apps/my-app/project.json': `{}`,
-        'apps/my-app/index.ts': `console.log('hello world')`,
-        'libs/my-lib/.eslintrc.json': `{}`,
-        'libs/my-lib/.eslintignore': `**/*`,
-        'libs/my-lib/project.json': `{}`,
-        'libs/my-lib/index.ts': `console.log('hello world')`,
-      });
-      expect(
-        await invokeCreateNodesOnMatchingFiles(context, { targetName: 'lint' })
-      ).toMatchInlineSnapshot(`
-        {
-          "projects": {},
-        }
-      `);
-    });
+    // This is intentionally disabled, since we should always create a node for project that contains eslint config
+    // it('should not create nodes for nested projects without a root level eslint config when all files are ignored (.eslintignore)', async () => {
+    //   createFiles({
+    //     'apps/my-app/.eslintrc.json': `{}`,
+    //     'apps/my-app/.eslintignore': `**/*`,
+    //     'apps/my-app/project.json': `{}`,
+    //     'apps/my-app/index.ts': `console.log('hello world')`,
+    //     'libs/my-lib/.eslintrc.json': `{}`,
+    //     'libs/my-lib/.eslintignore': `**/*`,
+    //     'libs/my-lib/project.json': `{}`,
+    //     'libs/my-lib/index.ts': `console.log('hello world')`,
+    //   });
+    //   expect(
+    //     await invokeCreateNodesOnMatchingFiles(context, { targetName: 'lint' })
+    //   ).toMatchInlineSnapshot(`
+    //     {
+    //       "projects": {},
+    //     }
+    //   `);
+    // });
 
-    it('should not create nodes for nested projects without a root level eslint config when all files are ignored (ignorePatterns in .eslintrc.json)', async () => {
-      createFiles({
-        'apps/my-app/.eslintrc.json': `{ "ignorePatterns": ["**/*"] }`,
-        'apps/my-app/project.json': `{}`,
-        'apps/my-app/index.ts': `console.log('hello world')`,
-        'libs/my-lib/.eslintrc.json': `{ "ignorePatterns": ["**/*"] }`,
-        'libs/my-lib/project.json': `{}`,
-        'libs/my-lib/index.ts': `console.log('hello world')`,
-      });
-      expect(
-        await invokeCreateNodesOnMatchingFiles(context, { targetName: 'lint' })
-      ).toMatchInlineSnapshot(`
-        {
-          "projects": {},
-        }
-      `);
-    });
+    // This is intentionally disabled, since we should always create a node for project that contains eslint config
+    // it('should not create nodes for nested projects without a root level eslint config when all files are ignored (ignorePatterns in .eslintrc.json)', async () => {
+    //   createFiles({
+    //     'apps/my-app/.eslintrc.json': `{ "ignorePatterns": ["**/*"] }`,
+    //     'apps/my-app/project.json': `{}`,
+    //     'apps/my-app/index.ts': `console.log('hello world')`,
+    //     'libs/my-lib/.eslintrc.json': `{ "ignorePatterns": ["**/*"] }`,
+    //     'libs/my-lib/project.json': `{}`,
+    //     'libs/my-lib/index.ts': `console.log('hello world')`,
+    //   });
+    //   expect(
+    //     await invokeCreateNodesOnMatchingFiles(context, { targetName: 'lint' })
+    //   ).toMatchInlineSnapshot(`
+    //     {
+    //       "projects": {},
+    //     }
+    //   `);
+    // });
   });
 
   describe('root eslint config and nested eslint configs', () => {
@@ -721,7 +723,6 @@ describe('@nx/eslint/plugin', () => {
     it('should handle multiple levels of nesting and ignored files correctly', async () => {
       createFiles({
         '.eslintrc.json': '{ "root": true, "ignorePatterns": ["**/*"] }',
-        'apps/myapp/.eslintrc.json': '{ "extends": "../../.eslintrc.json" }', // no lintable files, don't create task
         'apps/myapp/project.json': '{}',
         'apps/myapp/index.ts': 'console.log("hello world")',
         'apps/myapp/nested/mylib/.eslintrc.json': JSON.stringify({
