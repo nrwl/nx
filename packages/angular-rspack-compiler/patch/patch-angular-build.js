4a4f1d02a5c3e15c040c198ef912eb7b7cccff78
Colum Ferry <cferry09@gmail.com>
2025-05-30 12:35:35 +0200

    feat(angular-rspack,angular-rspack-compiler): support angular 20 (#99)
    * feat(angular-rspack,angular-rspack-compiler): support angular 20
    
    * chore(repo): integration test output updaet
    
    * chore(repo): rsbuild-ssr-csr server entry update
    
    * feat(angular-rspack): match changes in v20
    
    * feat(angular-rspack): update nx and angular versions
    
    ---------
    
    Co-authored-by: Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>

--- packages/angular-rspack-compiler/patch/patch-angular-build.js
+++ packages/angular-rspack-compiler/patch/patch-angular-build.js
@@ -10,9 +10,6 @@ function main() {
   fileContentsJson.exports['./src/tools/esbuild/javascript-transformer'] =
     './src/tools/esbuild/javascript-transformer.js';
   fileContentsJson.exports[
-    './src/tools/esbuild/angular/file-reference-tracker'
-  ] = './src/tools/esbuild/angular/file-reference-tracker.js';
-  fileContentsJson.exports[
     './src/tools/angular/compilation/parallel-compilation'
   ] = './src/tools/angular/compilation/parallel-compilation.js';
   fileContentsJson.exports[
