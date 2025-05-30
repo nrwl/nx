28e61e7c2beb7757d47ee79f254f60964ba60bae
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2025-05-30 12:57:27 +0200

    feat(angular): update `@nx/angular-rspack` package

--- packages/workspace/src/utils/versions.ts
+++ packages/workspace/src/utils/versions.ts
@@ -5,4 +5,4 @@ export const typescriptVersion = '~5.8.2';
 // TODO: remove when preset generation is reworked and
 // deps are not installed from workspace
 export const angularCliVersion = '~20.0.0';
-export const angularRspackVersion = '^21.0.1';
+export const angularRspackVersion = '^21.1.0';
