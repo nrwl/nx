43e28d0f77a1e1a35e9a99e9ed97045f1d148099
Nicholas Cunningham <ndcunningham@gmail.com>
2025-06-01 10:20:26 -0600

    fix(core): improve e2e matrix configuration and add support for golden projects

--- .github/workflows/nightly/process-matrix.ts
+++ .github/workflows/nightly/process-matrix.ts
@@ -1,6 +1,7 @@
 type MatrixDataProject = {
   name: string,
   codeowners: string,
+  isGolden?: boolean, // true if this is a golden project, false otherwise
 };
 
 type MatrixDataOS = {
@@ -22,12 +23,12 @@ type MatrixData = {
 // TODO: Extract Slack groups into named groups for easier maintenance
 const matrixData: MatrixData = {
   coreProjects: [
-    { name: 'e2e-lerna-smoke-tests', codeowners: 'S04TNCVEETS' },
-    { name: 'e2e-js', codeowners: 'S04SJ6HHP0X' },
-    { name: 'e2e-nx-init', codeowners: 'S04SYHYKGNP' },
-    { name: 'e2e-nx', codeowners: 'S04SYHYKGNP' },
-    { name: 'e2e-release', codeowners: 'S04SYHYKGNP' },
-    { name: 'e2e-workspace-create', codeowners: 'S04SYHYKGNP' }
+    { name: 'e2e-lerna-smoke-tests', codeowners: 'S04TNCVEETS', isGolden: true },
+    { name: 'e2e-js', codeowners: 'S04SJ6HHP0X', isGolden: true },
+    { name: 'e2e-nx-init', codeowners: 'S04SYHYKGNP', isGolden: true },
+    { name: 'e2e-nx', codeowners: 'S04SYHYKGNP', isGolden: true },
+    { name: 'e2e-release', codeowners: 'S04SYHYKGNP', isGolden: true },
+    { name: 'e2e-workspace-create', codeowners: 'S04SYHYKGNP', isGolden: true },
   ],
   projects: [
     { name: 'e2e-angular', codeowners: 'S04SS457V38' },
@@ -58,7 +59,8 @@ const matrixData: MatrixData = {
   setup: [
     { os: 'ubuntu-latest', os_name: 'Linux', os_timeout: 60, package_managers: ['npm', 'pnpm', 'yarn'], node_versions: ['20.19.0', "22.12.0"], excluded: ['e2e-detox', 'e2e-react-native', 'e2e-expo'] },
     { os: 'macos-latest', os_name: 'MacOS', os_timeout: 90, package_managers: ['npm'], node_versions: ['20.19.0'] },
-    { os: 'windows-latest', os_name: 'WinOS', os_timeout: 180, package_managers: ['npm'], node_versions: ['20.19.0'], excluded: ['e2e-detox', 'e2e-react-native', 'e2e-expo'] }
+    // TODO: Fix Windows support as gradle fails when running nx build https://staging.nx.app/runs/LgD4vxGn8w?utm_source=pull-request&utm_medium=comment
+    // { os: 'windows-latest', os_name: 'WinOS', os_timeout: 180, package_managers: ['npm'], node_versions: ['20.19.0'], excluded: ['e2e-detox', 'e2e-react-native', 'e2e-expo'] }
   ]
 };
 
@@ -69,19 +71,23 @@ const matrix: Array<{
   package_manager: string,
   os: string,
   os_name: string,
-  os_timeout: number
+  os_timeout: number,
+  isGolden?: boolean,
 }> = [];
 
 function addMatrixCombo(project: MatrixDataProject, nodeVersion: number | string, pm: number, os: number) {
-  matrix.push({
-    project: project.name,
-    codeowners: project.codeowners,
-    node_version: nodeVersion,
-    package_manager: matrixData.setup[os].package_managers[pm],
-    os: matrixData.setup[os].os,
-    os_name: matrixData.setup[os].os_name,
-    os_timeout: matrixData.setup[os].os_timeout,
-  });
+  if(!!project.isGolden) {
+    matrix.push({
+      project: project.name,
+      codeowners: project.codeowners,
+      node_version: nodeVersion,
+      package_manager: matrixData.setup[os].package_managers[pm],
+      os: matrixData.setup[os].os,
+      os_name: matrixData.setup[os].os_name,
+      os_timeout: matrixData.setup[os].os_timeout,
+      isGolden: true,
+    });
+  }
 }
 
 function processProject(project: MatrixDataProject, nodeVersion?: number) {
