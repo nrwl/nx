207cbe97bdc5479dfafdc0765226c663d6ef3559
ian <yihengz@outlook.com>
2025-06-13 21:43:56 +0800

    style: format

--- packages/nx/src/command-line/yargs-utils/shared-options.ts
+++ packages/nx/src/command-line/yargs-utils/shared-options.ts
@@ -412,7 +412,7 @@ const coerceTuiAutoExit = (value: string) => {
 function concurrency(val: string | number) {
   let parallel = typeof val === 'number' ? val : stringToNumber(val);
   if (parallel >= 1) {
-    return Math.floor(parallel)
+    return Math.floor(parallel);
   }
 
   const maxCores = availableParallelism?.() ?? cpus().length;
@@ -423,12 +423,12 @@ function concurrency(val: string | number) {
 function stringToNumber(input: string): number {
   const percentMatch = input.match(/^(\d+\.?\d*)%$/);
   if (percentMatch) {
-      return parseFloat(percentMatch[1]) / 100;
+    return parseFloat(percentMatch[1]) / 100;
   }
 
   const numericMatch = input.match(/^-?\d+\.?\d*$/);
   if (numericMatch) {
-      return parseFloat(input);
+    return parseFloat(input);
   }
 
   return 1;
