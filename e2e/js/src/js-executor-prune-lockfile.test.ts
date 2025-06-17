b5d24f2be2b79b2e66b9c8e060f5b5d540407d99
Colum Ferry <cferry09@gmail.com>
2025-06-17 12:16:10 +0100

    chore(js): use packageManager api from newProject

--- e2e/js/src/js-executor-prune-lockfile.test.ts
+++ e2e/js/src/js-executor-prune-lockfile.test.ts
@@ -15,54 +15,52 @@ describe('js:prune-lockfile executor', () => {
     ['pnpm', 'pnpm-lock.yaml'],
     ['yarn', 'yarn.lock'],
     ['npm', 'package-lock.json'],
-  ])('package manager %s', (packageManager, lockfile) => {
-    let scope;
-    let previousPackageManager: string;
+  ])(
+    'package manager %s',
+    (packageManager: 'pnpm' | 'yarn' | 'npm', lockfile) => {
+      let scope;
 
-    beforeAll(() => {
-      previousPackageManager = process.env.SELECTED_PM;
-      // Use pnpm to properly test workspace: protocol dependencies
-      process.env.SELECTED_PM = packageManager;
-
-      scope = newProject({
-        packages: ['@nx/node', '@nx/js'],
-        preset: 'ts',
+      beforeAll(() => {
+        scope = newProject({
+          packages: ['@nx/node', '@nx/js'],
+          preset: 'ts',
+          packageManager,
+        });
       });
-    });
-    afterAll(() => {
-      cleanupProject();
-      process.env.SELECTED_PM = previousPackageManager;
-    });
-
-    it('should prune lockfile with workspace module', () => {
-      const nodeapp = uniq('nodeapp');
-      const nodelib = uniq('nodelib');
-
-      runCLI(
-        `generate @nx/node:app ${nodeapp} --linter=eslint --unitTestRunner=jest`
-      );
-      runCLI(
-        `generate @nx/js:lib ${nodelib} --bundler=tsc --linter=eslint --unitTestRunner=jest`
-      );
-
-      updateJson(`${nodeapp}/package.json`, (json) => {
-        json.dependencies = {
-          ...json.dependencies,
-          [`@${scope}/${nodelib}`]: `file:../${nodelib}`,
-        };
-        json.nx.targets['prune-lockfile'] = {
-          executor: '@nx/js:prune-lockfile',
-          options: {
-            buildTarget: 'build',
-          },
-        };
-        return json;
+      afterAll(() => {
+        cleanupProject();
       });
-      runCommand(`${packageManager} install`);
 
-      runCLI(`build ${nodeapp}`);
-      runCLI(`prune-lockfile ${nodeapp}`);
-      checkFilesExist(`${nodeapp}/dist/${lockfile}`);
-    });
-  });
+      it('should prune lockfile with workspace module', () => {
+        const nodeapp = uniq('nodeapp');
+        const nodelib = uniq('nodelib');
+
+        runCLI(
+          `generate @nx/node:app ${nodeapp} --linter=eslint --unitTestRunner=jest`
+        );
+        runCLI(
+          `generate @nx/js:lib ${nodelib} --bundler=tsc --linter=eslint --unitTestRunner=jest`
+        );
+
+        updateJson(`${nodeapp}/package.json`, (json) => {
+          json.dependencies = {
+            ...json.dependencies,
+            [`@${scope}/${nodelib}`]: `file:../${nodelib}`,
+          };
+          json.nx.targets['prune-lockfile'] = {
+            executor: '@nx/js:prune-lockfile',
+            options: {
+              buildTarget: 'build',
+            },
+          };
+          return json;
+        });
+        runCommand(`${packageManager} install`);
+
+        runCLI(`build ${nodeapp}`);
+        runCLI(`prune-lockfile ${nodeapp}`);
+        checkFilesExist(`${nodeapp}/dist/${lockfile}`);
+      });
+    }
+  );
 });
