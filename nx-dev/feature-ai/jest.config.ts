794d92756f45816df144a4e290b31bc5da3eff7d
Nicholas Cunningham <ndcunningham@gmail.com>
2025-06-26 00:44:14 -0600

    feat(core): enable tesing for ts solution
    - Updated the custom Jest resolver to support TypeScript source resolution for specific Nx workspace packages.
    
    - Added module name mappings in Jest configurations for tools and workspace plugins to point to the correct package paths.

--- nx-dev/feature-ai/jest.config.ts
+++ nx-dev/feature-ai/jest.config.ts
@@ -1,10 +1,18 @@
 /* eslint-disable */
 export default {
-  displayName: 'nx-dev-feature-search',
+  displayName: 'nx-dev-feature-ai',
   transform: {
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/nx-dev/feature-ai',
   preset: '../../jest.preset.js',
+  moduleNameMapper: {
+    // Override for nx-dev packages - point to packages directory
+    '^@nx/devkit$': '<rootDir>/../../packages/devkit/index.ts',
+    '^@nx/devkit/testing$': '<rootDir>/../../packages/devkit/testing.ts',
+    '^@nx/devkit/internal-testing-utils$':
+      '<rootDir>/../../packages/devkit/internal-testing-utils.ts',
+    '^@nx/devkit/src/(.*)$': '<rootDir>/../../packages/devkit/src/$1',
+  },
 };
