83d2d3b9bf75eb43733cb83c94f4e5494f84487a
Julien Marcou <julien.marcou@convelio.com>
2025-07-15 15:04:25 +0200

    feat(module-federation): bump @module-federation/node & @module-federation/enhanced to fix esbuild vulnerability (#31924)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    ## Current Behavior
    
    NX has a dependency to a [vulnerable version of
    esbuild](https://github.com/evanw/esbuild/security/advisories/GHSA-67mh-4wv8-2f99)
    
    ## Expected Behavior
    
    Updating several `@module-federation` dependencies to update esbuild to
    v0.25.5 and fix the vulnerability
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #31923

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -15,8 +15,8 @@ export const typesCorsVersion = '~2.8.5';
 export const expressVersion = '^4.21.2';
 export const typesExpressVersion = '^4.17.21';
 export const browserSyncVersion = '^3.0.0';
-export const moduleFederationNodeVersion = '^2.6.26';
-export const moduleFederationEnhancedVersion = '^0.15.0';
+export const moduleFederationNodeVersion = '^2.7.9';
+export const moduleFederationEnhancedVersion = '^0.17.0';
 export const webpackMergeVersion = '^5.8.0';
 
 export const angularEslintVersion = '^20.0.0';
