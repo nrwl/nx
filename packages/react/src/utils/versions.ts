45c7cd4dd39aa25b03635c8c77bf156676172e6b
Julien Marcou <julien.marcou@convelio.com>
2025-07-15 15:04:25 +0200

    feat(module-federation): bump @module-federation/node & @module-federation/enhanced to fix esbuild vulnerability (#31924)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    NX has a dependency to a [vulnerable version of
    esbuild](https://github.com/evanw/esbuild/security/advisories/GHSA-67mh-4wv8-2f99)
    
    Updating several `@module-federation` dependencies to update esbuild to
    v0.25.5 and fix the vulnerability
    
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #31923

--- packages/react/src/utils/versions.ts
+++ packages/react/src/utils/versions.ts
@@ -61,8 +61,8 @@ export const typesExpressVersion = '^4.17.21';
 export const isbotVersion = '^3.6.5';
 export const corsVersion = '~2.8.5';
 export const typesCorsVersion = '~2.8.12';
-export const moduleFederationNodeVersion = '^2.6.26';
-export const moduleFederationEnhancedVersion = '^0.15.0';
+export const moduleFederationNodeVersion = '^2.7.9';
+export const moduleFederationEnhancedVersion = '^0.17.0';
 
 // style preprocessors
 export const lessVersion = '3.12.2';
