da873b23613b57c4842eb33c59cf1a5241afa5ad
Nicholas Cunningham <ndcunningham@gmail.com>
2025-07-16 08:57:12 -0600

    fix(react): e2e test should work out of the box for react-router

--- e2e/react/src/playwright.test.ts
+++ e2e/react/src/playwright.test.ts
@@ -5,7 +5,6 @@ import {
   newProject,
   uniq,
   updateFile,
-  readFile,
 } from '@nx/e2e/utils';
 
 describe('React Playwright e2e tests', () => {
@@ -32,50 +31,6 @@ describe('React Playwright e2e tests', () => {
         `Successfully ran target e2e for project ${appName}-e2e`
       );
     }
-  });
-
-  it('should generate a playwright config with the default options', () => {
-    const testAppName = uniq('pw-test-app');
-    runCLI(
-      `generate @nx/react:app ${testAppName} --e2eTestRunner=playwright --bundler=vite --no-interactive`
-    );
-
-    const configContent = readFile(`${testAppName}-e2e/playwright.config.ts`);
-
-    //  imports
-    expect(configContent).toContain(
-      "import { defineConfig, devices } from '@playwright/test';"
-    );
-    expect(configContent).toContain(
-      "import { nxE2EPreset } from '@nx/playwright/preset';"
-    );
-    expect(configContent).toContain(
-      "import { workspaceRoot } from '@nx/devkit';"
-    );
-
-    //  baseURL configuration
-    expect(configContent).toContain(
-      "const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';"
-    );
-
-    // defineConfig
-    expect(configContent).toContain('export default defineConfig({');
-    expect(configContent).toContain(
-      "...nxE2EPreset(__filename, { testDir: './src' })"
-    );
-
-    //  webServer configuration
-    expect(configContent).toContain('use: {');
-    expect(configContent).toContain('baseURL,');
-    expect(configContent).toContain("trace: 'on-first-retry',");
-
-    expect(configContent).toContain('webServer: {');
-    expect(configContent).toContain(
-      `command: 'npx nx run ${testAppName}:preview'`
-    );
-    expect(configContent).toContain("url: 'http://localhost:4200'");
-    expect(configContent).toContain('reuseExistingServer: true');
-    expect(configContent).toContain('cwd: workspaceRoot');
   });
 
   it('should execute e2e tests using playwright with a library used in the app', () => {
