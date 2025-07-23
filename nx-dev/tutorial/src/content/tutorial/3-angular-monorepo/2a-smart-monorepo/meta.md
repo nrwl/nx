074b75b06e13f78bde8fb1b13ffd947b252d8245
Jack Hsu <jack.hsu@gmail.com>
2025-07-23 12:40:56 -0400

    docs(misc): remove unused tutorialkit code

--- nx-dev/tutorial/src/content/tutorial/3-angular-monorepo/2a-smart-monorepo/meta.md
+++
@@ -1,9 +0,0 @@
----
-type: chapter
-title: Smart Monorepo
-editor:
-  fileTree:
-    allowEdits: true
-filesystem:
-  watch: ['/*.json', '/libs', '/(apps|libs)/**']
----
--- nx-dev/tutorial/uno.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from '@tutorialkit/theme';
-
-const config: any = defineConfig({
-  // add your UnoCSS config here: https://unocss.dev/guide/config-file
-});
-export default config;
