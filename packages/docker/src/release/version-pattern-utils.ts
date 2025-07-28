d5bc1c7ec24959451b41ca60c6d1eeb1acb2b90d
Colum Ferry <cferry09@gmail.com>
2025-07-28 14:06:06 +0100

    fix(docker): format current date to UTC

--- packages/docker/src/release/version-pattern-utils.ts
+++ packages/docker/src/release/version-pattern-utils.ts
@@ -18,12 +18,12 @@ export interface PatternTokens {
 const tokenRegex = /\{([^|{}]+)(?:\|([^{}]+))?\}/g;
 
 function formatDate(date: Date, format: string) {
-  const year = String(date.getFullYear());
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
+  const year = String(date.getUTCFullYear());
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  const hours = String(date.getUTCHours()).padStart(2, '0');
+  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+  const seconds = String(date.getUTCSeconds()).padStart(2, '0');
 
   return format
     .replace(/YYYY/g, year)
