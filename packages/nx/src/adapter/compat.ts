cf39165cb5b22aab11825ccb2a43708e990a347b
zack-derose <zack.derose@gmail.com>
2025-08-08 11:41:38 -0400

    fix(core): adding to allowed options

--- packages/nx/src/adapter/compat.ts
+++ packages/nx/src/adapter/compat.ts
@@ -77,6 +77,7 @@ export const allowedWorkspaceExtensions = [
   'nxCloudId',
   'nxCloudUrl',
   'nxCloudEncryptionKey',
+  'customProxyConfigPath',
   'parallel',
   'cacheDirectory',
   'useDaemonProcess',
