a3c54e8df73d60fc0671e4b38b34bc9937c1429b
Jack Baker <jack.baker@directlinegroup.co.uk>
2025-08-11 15:29:47 +0100

    cleanup(angular): remove CommonModule from component generators (#32215)
    ## Current Behavior
    When generating a new Angular component it imports `CommonModule` by
    default.
    
    ## Expected Behavior
    The `CommonModule` is not needed as often in modern angular because of
    the new control flow syntax and the move away from `[ngClass]` and
    `[ngStyle]` in favour of `[class]` and `[style]`. It is also not
    included by the official angular component generator.

--- packages/angular/src/generators/host/host.spec.ts
+++ packages/angular/src/generators/host/host.spec.ts
@@ -585,11 +585,10 @@ describe('Host App Generator', () => {
         tree.read('remote1/src/app/remote-entry/entry.component.ts', 'utf-8')
       ).toMatchInlineSnapshot(`
         "import { Component } from '@angular/core';
-        import { CommonModule } from '@angular/common';
         import { NxWelcomeComponent } from './nx-welcome.component';
 
         @Component({
-          imports: [CommonModule, NxWelcomeComponent],
+          imports: [NxWelcomeComponent],
           selector: 'app-remote1-entry',
           template: \`<app-nx-welcome></app-nx-welcome>\`
         })
