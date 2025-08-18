c300d014a0d3c305d93b6141c0cb73d69ba29a4e
FrozenPandaz <jasonjean1993@gmail.com>
2025-08-18 14:52:05 -0400

    Refactor to use qualified names and run-commands executor
    - Update project names to use qualified Maven coordinates (groupId.artifactId)
    - Replace custom executors with @nx/workspace:run-commands
    - Use Maven -pl flag to run commands from workspace root
    - Remove custom executors directory and implementation
    - Remove project generator (not needed)
    - All targets now use actual Maven reactor project data with proper Maven commands

--- packages/maven/src/index.ts
+++ packages/maven/src/index.ts
@@ -1,6 +1,2 @@
 export * from './generators/init/generator';
-export * from './generators/project/generator';
-export * from './executors/compile/executor';
-export * from './executors/test/executor';
-export * from './executors/package/executor';
 export { createNodesV2, createDependencies } from './plugin';
