7aa7cbe927d1f976d23ad6c4cac140ccb5b242fe
Louie Weng <56288712+lourw@users.noreply.github.com>
2025-08-22 08:56:16 -0700

    fix(gradle): use AST parsing to migrate updates to version catalogs (#32463)
    <!-- Please make sure you have read the submission guidelines before
    posting an PR -->
    <!--
    https://github.com/nrwl/nx/blob/master/CONTRIBUTING.md#-submitting-a-pr
    -->
    
    <!-- Please make sure that your commit message follows our format -->
    <!-- Example: `fix(nx): must begin with lowercase` -->
    
    <!-- If this is a particularly complex change or feature addition, you
    can request a dedicated Nx release for this pull request branch. Mention
    someone from the Nx team or the `@nrwl/nx-pipelines-reviewers` and they
    will confirm if the PR warrants its own release for testing purposes,
    and generate it for you if appropriate. -->
    
    ## Current Behavior
    <!-- This is the behavior we have today -->
    
    When we migrate version catalogs to use new versions of the gradle
    project graph plugin, we lose the original formatting of the version
    catalogue file.
    
    ## Expected Behavior
    <!-- This is the behavior we should expect with the changes in this PR
    -->
    
    We use AST parsing on the version catalogue so that we can make version
    edits on the specific portion of the file.
    
    ## Related Issue(s)
    <!-- Please link the issue being fixed so it gets closed when this is
    merged. -->
    
    Fixes #

--- packages/gradle/src/migrations/21-4-0/change-plugin-version-0-1-5.ts
+++ packages/gradle/src/migrations/21-4-0/change-plugin-version-0-1-5.ts
@@ -1,7 +1,7 @@
 import { Tree, readNxJson } from '@nx/devkit';
 import { hasGradlePlugin } from '../../utils/has-gradle-plugin';
 import { addNxProjectGraphPlugin } from '../../generators/init/gradle-project-graph-plugin-utils';
-import { updateNxPluginVersionInCatalogs } from '../../utils/version-catalog-utils';
+import { updateNxPluginVersionInCatalogsAst } from '../../utils/version-catalog-ast-utils';
 
 /* Change the plugin version to 0.1.5
  */
@@ -16,8 +16,8 @@ export default async function update(tree: Tree) {
 
   const gradlePluginVersionToUpdate = '0.1.5';
 
-  // Update version in version catalogs first
-  await updateNxPluginVersionInCatalogs(tree, gradlePluginVersionToUpdate);
+  // Update version in version catalogs using AST-based approach to preserve formatting
+  await updateNxPluginVersionInCatalogsAst(tree, gradlePluginVersionToUpdate);
 
   // Then update in build.gradle(.kts) files
   await addNxProjectGraphPlugin(tree, gradlePluginVersionToUpdate);
