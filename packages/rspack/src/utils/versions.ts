e799d875f76f99a33b02823a618acb8a6433b4f1
Colum Ferry <cferry09@gmail.com>
2025-08-29 10:48:26 +0100

    fix(rspack): do not pin dev server

--- packages/rspack/src/utils/versions.ts
+++ packages/rspack/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = require('../../package.json').version;
 export const rspackCoreVersion = '^1.5.0';
-export const rspackDevServerVersion = '1.1.4';
+export const rspackDevServerVersion = '^1.1.4';
 
 export const rspackPluginReactRefreshVersion = '^1.0.0';
 export const lessLoaderVersion = '~11.1.3';
