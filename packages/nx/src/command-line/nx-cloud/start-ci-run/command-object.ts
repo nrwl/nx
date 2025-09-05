b5f795670ef5dd9cfa0b89dda172456a5737610b
Rares Matei <matei.rar@gmail.com>
2025-09-05 13:34:19 +0300

    fix(nx-cloud): delcare help arg for start-ci-run

--- packages/nx/src/command-line/nx-cloud/start-ci-run/command-object.ts
+++ packages/nx/src/command-line/nx-cloud/start-ci-run/command-object.ts
@@ -7,9 +7,9 @@ export const yargsStartCiRunCommand: CommandModule = {
     'Starts a new CI run for distributed task execution. This command is an alias for [`nx-cloud start-ci-run`](/ci/reference/nx-cloud-cli#npx-nxcloud-start-ci-run).',
   builder: (yargs) =>
     withVerbose(yargs)
-      .option('help', { describe: 'Show help.', type: 'boolean', alias: 'h', global: false })
       .help(false)
-      .showHelpOnFail(false),
+      .showHelpOnFail(false)
+      .option('help', { describe: 'Show help.', type: 'boolean' }),
   handler: async (args: any) => {
     process.exit(
       await (await import('./start-ci-run')).startCiRunHandler(args)
