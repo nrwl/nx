25efd2507ebb97b730742c154ef87bb75106ee76
Max Kless <maxk@nrwl.io>
2025-09-10 18:00:04 +0200

    mention NX_SKIP_PROVENANCE_CHECK env var wip

--- packages/nx/src/utils/provenance.ts
+++ packages/nx/src/utils/provenance.ts
@@ -113,7 +113,7 @@ export const noProvenanceError = (
   packageVersion: string,
   error?: string
 ) =>
-  `An error occurred while checking the provenance of ${packageName}@${packageVersion}. This could indicate a security risk. Please double check https://www.npmjs.com/package/${packageName} to see if the package is published correctly or file an issue at https://github.com/nrwl/nx/issues \n Error: ${
+  `An error occurred while checking the provenance of ${packageName}@${packageVersion}. This could indicate a security risk. Please double check https://www.npmjs.com/package/${packageName} to see if the package is published correctly or file an issue at https://github.com/nrwl/nx/issues. To disable this check at your own risk, you can set the NX_SKIP_PROVENANCE_CHECK environment variable to true \n Error: ${
     error ?? ''
   }`;
 
