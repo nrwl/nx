b24443e1da4a096a72cd4e29b2193d062c2af103
Philip Fulcher <philip@nrwl.io>
2025-09-11 10:54:50 -0400

    docs(nx-dev): add september webinar (#32694)

--- nx-dev/ui-common/src/lib/webinar-notifier.tsx
+++ nx-dev/ui-common/src/lib/webinar-notifier.tsx
@@ -11,7 +11,7 @@ import {
 export function WebinarNotifier(): ReactElement | null {
   const [isMounted, setIsMounted] = useState(false);
   const [isVisible, setIsVisible] = useState<boolean>(true);
-  const localStorageKey = 'workshop-august-26-2025--notifier-closed';
+  const localStorageKey = 'workshop-september-24-2025--notifier-closed';
 
   useEffect(() => {
     setIsMounted(true);
@@ -61,18 +61,17 @@ export function WebinarNotifier(): ReactElement | null {
               aria-hidden="true"
               className="size-8 flex-shrink-0"
             />
-            <span>Join our webinar on August 26th</span>
+            <span>Join our webinar on September 24th</span>
           </motion.h3>
           <motion.div key="live-event" className="mt-4 space-y-4">
             <p className="mb-2 text-sm">
-              Learn how Nx’s AI-powered self-healing CI can reduce your
-              development bottlenecks and free you from hours of manual PR
-              oversight.
+              Learn what modern monorepo development looks like in our September
+              webinar.
             </p>
             <div className="flex flex-wrap items-center justify-end gap-1 sm:gap-4">
               <a
                 title="Signup"
-                href="https://bit.ly/4maTaFI"
+                href="https://bit.ly/3JX0gzB"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-flex items-center justify-center gap-2 rounded-lg bg-pink-600 px-2 py-2 text-sm font-semibold text-white transition hover:bg-pink-700 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:text-black/70 md:px-4"
