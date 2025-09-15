d672eeca6bec1043471a0474923be95db764c2f3
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2025-09-15 13:08:30 +0200

    feat(angular): update angular eslint packages to 20.3.0

--- packages/angular/src/utils/versions.ts
+++ packages/angular/src/utils/versions.ts
@@ -18,7 +18,7 @@ export const moduleFederationNodeVersion = '^2.7.11';
 export const moduleFederationEnhancedVersion = '^0.18.0';
 export const webpackMergeVersion = '^5.8.0';
 
-export const angularEslintVersion = '^20.2.0';
+export const angularEslintVersion = '^20.3.0';
 export const typescriptEslintVersion = '^7.16.0';
 export const tailwindVersion = '^3.0.2';
 export const postcssVersion = '^8.4.5';
