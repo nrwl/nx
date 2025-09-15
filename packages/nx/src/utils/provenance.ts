799494ed85ef3a6d468caedcd6e42f14687a89fa
Max Kless <maxk@nrwl.io>
2025-09-15 17:40:56 +0200

    cleanup wip

--- packages/nx/src/utils/provenance.ts
+++ packages/nx/src/utils/provenance.ts
@@ -17,7 +17,6 @@ export async function ensurePackageHasProvenance(
 ): Promise<void> {
   // this is used for locally released versions without provenance
   // do not set this for other reasons or you might be exposed to security risks
-  // also skip this check during e2e tests because those rely on local packages without provenance
   if (process.env.NX_SKIP_PROVENANCE_CHECK) {
     return;
   }
