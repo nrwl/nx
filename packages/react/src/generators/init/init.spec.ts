7d816e7becacaaf67684d06e0e35857499719ebf
Colum Ferry <cferry09@gmail.com>
2025-09-22 21:09:06 +0100

    fix(react): only add react router plugin when using react router #32525

--- packages/react/src/generators/init/init.spec.ts
+++ packages/react/src/generators/init/init.spec.ts
@@ -1,4 +1,4 @@
-import { readJson, Tree } from '@nx/devkit';
+import { readJson, readNxJson, Tree } from '@nx/devkit';
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
 import reactInitGenerator from './init';
 
@@ -15,5 +15,50 @@ describe('init', () => {
     expect(packageJson.dependencies['react']).toBeDefined();
     expect(packageJson.dependencies['react-dom']).toBeDefined();
     expect(packageJson.devDependencies['@nx/react']).toBeDefined();
+  });
+
+  it('should NOT add react-router plugin when useReactRouterPlugin is false', async () => {
+    await reactInitGenerator(tree, {
+      skipFormat: true,
+      addPlugin: true,
+      useReactRouterPlugin: false,
+    });
+    const nxJson = readNxJson(tree);
+    const hasRouterPlugin = nxJson.plugins?.some(
+      (p) =>
+        (typeof p === 'string' && p === '@nx/react/router-plugin') ||
+        (typeof p === 'object' && p.plugin === '@nx/react/router-plugin')
+    );
+    expect(hasRouterPlugin).toBeFalsy();
+  });
+
+  it('should add react-router plugin when useReactRouterPlugin is true', async () => {
+    await reactInitGenerator(tree, {
+      skipFormat: true,
+      addPlugin: true,
+      useReactRouterPlugin: true,
+    });
+    const nxJson = readNxJson(tree);
+    const hasRouterPlugin = nxJson.plugins?.some(
+      (p) =>
+        (typeof p === 'string' && p === '@nx/react/router-plugin') ||
+        (typeof p === 'object' && p.plugin === '@nx/react/router-plugin')
+    );
+    expect(hasRouterPlugin).toBeTruthy();
+  });
+
+  it('should NOT add react-router plugin when addPlugin is false even if useReactRouterPlugin is true', async () => {
+    await reactInitGenerator(tree, {
+      skipFormat: true,
+      addPlugin: false,
+      useReactRouterPlugin: true,
+    });
+    const nxJson = readNxJson(tree);
+    const hasRouterPlugin = nxJson.plugins?.some(
+      (p) =>
+        (typeof p === 'string' && p === '@nx/react/router-plugin') ||
+        (typeof p === 'object' && p.plugin === '@nx/react/router-plugin')
+    );
+    expect(hasRouterPlugin).toBeFalsy();
   });
 });
