4144ec8cec042ac2788922d7dbe6b65771f04567
Sean Sanker <seanjr@sanker.com>
2025-09-26 15:09:57 -0400

    chore(expo): bump versions

--- packages/react-native/src/utils/versions.ts
+++ packages/react-native/src/utils/versions.ts
@@ -1,29 +1,29 @@
 export const nxVersion = require('../../package.json').version;
 
-export const reactNativeVersion = '~0.79.3';
+export const reactNativeVersion = '~0.81.4';
 
 export const typesNodeVersion = '20.19.9';
 
-export const reactNativeCommunityCliVersion = '~18.0.0';
-export const reactNativeCommunityCliPlatformAndroidVersion = '~18.0.0';
-export const reactNativeCommunityCliPlatformIosVersion = '~18.0.0';
+export const reactNativeCommunityCliVersion = '~20.0.0';
+export const reactNativeCommunityCliPlatformAndroidVersion = '~20.0.0';
+export const reactNativeCommunityCliPlatformIosVersion = '~20.0.0';
 
-export const reactNativeBabelPresetVersion = '~0.79.3';
-export const reactNativeMetroConfigVersion = '~0.79.3';
-export const reactNativeWebVersion = '~0.20.0';
+export const reactNativeBabelPresetVersion = '~0.81.4';
+export const reactNativeMetroConfigVersion = '~0.81.4';
+export const reactNativeWebVersion = '~0.21.1';
 
-export const metroVersion = '~0.82.4';
+export const metroVersion = '~0.83.1';
 
-export const reactVersion = '19.0.0';
-export const reactDomVersion = '19.0.0';
-export const typesReactVersion = '~19.0.10';
-export const typesReactDomVersion = '~19.0.6';
+export const reactVersion = '19.1.0';
+export const reactDomVersion = '19.1.0';
+export const typesReactVersion = '~19.1.14';
+export const typesReactDomVersion = '~19.1.0';
 
-export const testingLibraryReactNativeVersion = '~13.2.0';
-export const reactTestRendererVersion = '~19.0.0';
+export const testingLibraryReactNativeVersion = '~13.3.3';
+export const reactTestRendererVersion = '~19.1.0';
 
 export const reactNativeSvgTransformerVersion = '~1.5.1';
-export const reactNativeSvgVersion = '~15.11.2';
+export const reactNativeSvgVersion = '~15.12.1';
 export const reactNativeSvgWebVersion = '~1.0.9';
 
-export const babelRuntimeVersion = '~7.27.6';
+export const babelRuntimeVersion = '~7.28.4';
