5f4a0fe852c982d80280ac9df6d7df2cbf3859e1
Jack Hsu <jack.hsu@gmail.com>
2025-09-29 16:35:12 -0400

    Revert "feat(webpack)!: remove SVGR option and provide withSvgr composable function (#32843) (#32882)
    This reverts commit 2e9b5430bb4d7d790befebad35753209f4b6a4f2. This is
    meant for v22 not 21.6.

--- packages/react/plugins/nx-react-webpack-plugin/nx-react-webpack-plugin.ts
+++ packages/react/plugins/nx-react-webpack-plugin/nx-react-webpack-plugin.ts
@@ -2,7 +2,7 @@ import { Compiler } from 'webpack';
 import { applyReactConfig } from './lib/apply-react-config';
 
 export class NxReactWebpackPlugin {
-  constructor(private options: Record<string, any> = {}) {}
+  constructor(private options: { svgr?: boolean } = {}) {}
 
   apply(compiler: Compiler): void {
     applyReactConfig(this.options, compiler.options);
