2b3bd40b44d48e0d5db5186a81cb3119add6923a
Rares Matei <matei.rar@gmail.com>
2025-09-30 18:07:09 +0100

    chore(repo): correctly setup cypress components tests

--- e2e/angular/src/cypress-component-tests-implicit-dep.test.ts
+++ e2e/angular/src/cypress-component-tests-implicit-dep.test.ts
@@ -3,11 +3,13 @@ import {
   checkFilesExist,
   runCLI,
   runE2ETests,
+  updateFile,
 } from '@nx/e2e-utils';
 import { join } from 'path';
 import {
   setupCypressComponentTests,
   cleanupCypressComponentTests,
+  updateTestToAssertTailwindIsNotApplied,
   useBuildableLibInLib,
   updateBuilableLibTestsToAssertAppStyles,
   useRootLevelTailwindConfig,
@@ -19,6 +21,31 @@ describe('Angular Cypress Component Tests - Implicit Dep', () => {
 
   beforeAll(async () => {
     setup = setupCypressComponentTests();
+
+    // Setup cypress component testing for the buildable lib
+    // This is needed for the tests in this file to work
+    const { appName, buildableLibName } = setup;
+    updateTestToAssertTailwindIsNotApplied(buildableLibName);
+    runCLI(
+      `generate @nx/angular:cypress-component-configuration --project=${buildableLibName} --generate-tests --build-target=${appName}:build --no-interactive`
+    );
+
+    // Add tailwind to the buildable lib
+    runCLI(`generate @nx/angular:setup-tailwind --project=${buildableLibName}`);
+    updateFile(
+      `${buildableLibName}/src/lib/input/input.component.cy.ts`,
+      (content) => {
+        // text-green-500 should now apply
+        return content.replace('rgb(0, 0, 0)', 'rgb(34, 197, 94)');
+      }
+    );
+    updateFile(
+      `${buildableLibName}/src/lib/input-standalone/input-standalone.cy.ts`,
+      (content) => {
+        // text-green-500 should now apply
+        return content.replace('rgb(0, 0, 0)', 'rgb(34, 197, 94)');
+      }
+    );
   });
 
   afterAll(() => cleanupCypressComponentTests());
