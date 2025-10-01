0ef9be82956cc224ae722d4123f1acb03966799c
Jack Hsu <jack.hsu@gmail.com>
2025-10-01 09:54:56 -0400

    feat(webpack)!: remove SVGR option and provide withSvgr composable function (#32917)
    This reverts commit 5f4a0fe852c982d80280ac9df6d7df2cbf3859e1.
    
    The previous PR was erroneously merged into `master` for 21.6.x, so we
    had to revert it. This PR brings it back for 22.

--- packages/react/plugins/nx-react-webpack-plugin/lib/apply-react-config.ts
+++ packages/react/plugins/nx-react-webpack-plugin/lib/apply-react-config.ts
@@ -1,43 +1,12 @@
 import { Configuration, WebpackOptionsNormalized } from 'webpack';
-import { SvgrOptions } from '../../with-react';
 
 export function applyReactConfig(
-  options: { svgr?: boolean | SvgrOptions },
+  options: Record<string, any> = {},
   config: Partial<WebpackOptionsNormalized | Configuration> = {}
 ): void {
   if (!process.env['NX_TASK_TARGET_PROJECT']) return;
 
   addHotReload(config);
-
-  if (options.svgr !== false || typeof options.svgr === 'object') {
-    removeSvgLoaderIfPresent(config);
-
-    const defaultSvgrOptions = {
-      svgo: false,
-      titleProp: true,
-      ref: true,
-    };
-
-    const svgrOptions =
-      typeof options.svgr === 'object' ? options.svgr : defaultSvgrOptions;
-
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: /\.(js|ts|md)x?$/,
-      use: [
-        {
-          loader: require.resolve('@svgr/webpack'),
-          options: svgrOptions,
-        },
-        {
-          loader: require.resolve('file-loader'),
-          options: {
-            name: '[name].[hash].[ext]',
-          },
-        },
-      ],
-    });
-  }
 
   // enable webpack node api
   config.node = {
@@ -77,19 +46,4 @@ function addHotReload(
     const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
     config.plugins.push(new ReactRefreshPlugin({ overlay: false }));
   }
-}
-
-// We remove potentially conflicting rules that target SVGs because we use @svgr/webpack loader
-// See https://github.com/nrwl/nx/issues/14383
-function removeSvgLoaderIfPresent(
-  config: Partial<WebpackOptionsNormalized | Configuration>
-) {
-  const svgLoaderIdx = config.module.rules.findIndex(
-    (rule) =>
-      typeof rule === 'object' &&
-      typeof rule.test !== 'undefined' &&
-      rule.test.toString().includes('svg')
-  );
-  if (svgLoaderIdx === -1) return;
-  config.module.rules.splice(svgLoaderIdx, 1);
 }
