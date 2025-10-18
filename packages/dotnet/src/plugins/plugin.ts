5d384d3920b1abbcbb04235168dfff7b85ecc6aa
Craigory Coppola <craigorycoppola@gmail.com>
2025-10-17 22:37:47 -0400

    fix(dotnet): disable analyzer build and provide opt out for vercel

--- packages/dotnet/src/plugins/plugin.ts
+++ packages/dotnet/src/plugins/plugin.ts
@@ -2,11 +2,18 @@ import { createNodesV2, type DotNetPluginOptions } from './create-nodes';
 import { createDependencies } from './create-dependencies';
 import { NxPlugin } from '@nx/devkit';
 
-const plugin: NxPlugin<DotNetPluginOptions> = {
+const regularPlugin: NxPlugin<DotNetPluginOptions> = {
   name: '@nx/dotnet',
   createNodes: createNodesV2,
   createNodesV2,
   createDependencies,
 };
+
+const noopPlugin: NxPlugin = {
+  name: '@nx/dotnet [disabled]',
+};
+
+const plugin: NxPlugin<DotNetPluginOptions> =
+  process.env.NX_DOTNET_DISABLE === 'true' ? noopPlugin : regularPlugin;
 
 export = plugin;
