833c0f8b12ab4b46f739c45dd3d8a9b24e092924
Rares Matei <matei.rar@gmail.com>
2025-10-24 17:25:42 +0100

    chore(repo): revert e2e test splits for e2e-react, e2e-webpack, and e2e-nx
    This reverts the test splitting changes for the following projects:
    - e2e-react: Restored cypress-component-tests.test.ts and module-federation tests
    - e2e-webpack: Restored monolithic webpack.test.ts, removed split test files
    - e2e-nx: Restored original tsconfig.spec.json
    
    These projects were causing slower CI runs, so reverting to their original structure.

--- e2e/react/src/cypress-component-tests.test.ts
+++ e2e/react/src/cypress-component-tests.test.ts
@@ -192,6 +192,69 @@ export default Input;
     }
   }, 300_000);
 
+  it('should test buildable lib not being used in app', () => {
+    createFile(
+      `libs/${buildableLibName}/src/lib/input/input.cy.tsx`,
+      `
+import * as React from 'react'
+import Input from './input'
+
+
+describe(Input.name, () => {
+  it('renders', () => {
+    cy.mount(<Input readOnly={false} />)
+    cy.get('label').should('have.css', 'color', 'rgb(0, 0, 0)');
+  })
+  it('should be read only', () => {
+    cy.mount(<Input readOnly={true}/>)
+    cy.get('input').should('have.attr', 'readonly');
+  })
+});
+`
+    );
+
+    runCLI(
+      `generate @nx/react:cypress-component-configuration --project=${buildableLibName} --generate-tests --build-target=${appName}:build`
+    );
+
+    if (runE2ETests()) {
+      expect(runCLI(`component-test ${buildableLibName} --no-watch`)).toContain(
+        'All specs passed!'
+      );
+    }
+
+    // add tailwind
+    runCLI(`generate @nx/react:setup-tailwind --project=${buildableLibName}`);
+    updateFile(
+      `libs/${buildableLibName}/src/styles.css`,
+      `
+@tailwind components;
+@tailwind base;
+@tailwind utilities;
+`
+    );
+    updateFile(
+      `libs/${buildableLibName}/src/lib/input/input.cy.tsx`,
+      (content) => {
+        // text-green-500 should now apply
+        return content.replace('rgb(0, 0, 0)', 'rgb(34, 197, 94)');
+      }
+    );
+    updateFile(
+      `libs/${buildableLibName}/src/lib/input/input.tsx`,
+      (content) => {
+        return `import '../../styles.css';
+${content}`;
+      }
+    );
+
+    if (runE2ETests()) {
+      expect(runCLI(`component-test ${buildableLibName} --no-watch`)).toContain(
+        'All specs passed!'
+      );
+    }
+  }, 300_000);
+
   it('should work with async webpack config', async () => {
     // TODO: (caleb) for whatever reason the MF webpack config + CT is running, but cypress is not starting up?
     // are they overriding some option on top of each other causing cypress to not see it's running?
--- e2e/webpack/src/webpack-setup.ts
+++
@@ -1,8 +0,0 @@
-import { cleanupProject, newProject } from '@nx/e2e-utils';
-
-export function setupWebpackTest() {
-  beforeAll(() =>
-    newProject({ packages: ['@nx/webpack', '@nx/js', '@nx/react', '@nx/web'] })
-  );
-  afterAll(() => cleanupProject());
-}
