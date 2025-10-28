4b6009764ae46f1e4e87bb98b32478d024343e8d
Juri Strumpflohner <juri@nrwl.io>
2025-10-28 16:12:39 +0100

    feat(nx-dev): add downloadable resources page and React book blog post
    (cherry picked from commit 160b4cce34c9e51e2ab3fdfe60f40022c43ae42a)

---
+++ nx-dev/ui-resources/src/index.ts
@@ -0,0 +1,6 @@
+export * from './lib/types';
+export * from './lib/resource-types';
+export * from './lib/filters';
+export * from './lib/resource-card';
+export * from './lib/resource-grid';
+export * from './lib/resource-container';
