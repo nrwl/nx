f2072f01f2a71c925fd599fdd05cd13f58f02eff
Colum Ferry <cferry09@gmail.com>
2025-11-06 14:28:18 +0000

    feat(storybook): generate ai instructions for converting from CJS to ESM after migration (#33395)
    ## Current Behavior
    Storybook 10 Changelog states that CJS is no longer supported.
    The migration does not do this automatically.
    
    https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#the-storybookmain-file-and-other-presets-must-be-valid-esm
    
    ## Expected Behavior
    Generate instructions for an AI Agent to find and convert CJS Storybook
    Config files to ESM.
    
    ## Related Issue(s)
    
    Fixes NXC-3409
    Fixes NXC-3336

--- packages/storybook/src/migrations/update-22-1-0/migrate-to-storybook-10.ts
+++ packages/storybook/src/migrations/update-22-1-0/migrate-to-storybook-10.ts
@@ -2,6 +2,8 @@ import { Tree } from '@nx/devkit';
 import { output } from 'nx/src/utils/output';
 import migrate10Generator from '../../generators/migrate-10/migrate-10';
 import { storybookMajorVersion } from '../../utils/utilities';
+import { join } from 'path';
+import { existsSync, readFileSync } from 'fs';
 
 export default async function migrateToStorybook10(tree: Tree) {
   output.log({
@@ -13,7 +15,23 @@ export default async function migrateToStorybook10(tree: Tree) {
       `https://nx.dev/nx-api/storybook/generators/migrate-10`,
     ],
   });
-  return migrate10Generator(tree, {
+  await migrate10Generator(tree, {
     autoAcceptAllPrompts: true,
   });
+
+  const pathToAiInstructions = join(
+    __dirname,
+    'files',
+    'ai-instructions-for-cjs-esm.md'
+  );
+  if (!existsSync(pathToAiInstructions)) {
+    return;
+  }
+
+  const contents = readFileSync(pathToAiInstructions);
+  tree.write('MIGRATE_STORYBOOK_10.md', contents);
+  return [
+    `Storybook 10 requires Storybook Configs to use ESM.`,
+    `We created 'MIGRATE_STORYBOOK_10.md' with instructions for an AI Agent to convert CJS Storybook Configs to ESM in your workspace.`,
+  ];
 }
