00a247be3fa48a8ca71e0b1a160803924ea86aa6
Colum Ferry <cferry09@gmail.com>
2025-11-11 15:20:14 +0000

    feat(vitest): support vitest 4 (#33424)
    ## Current Behavior
    We currently only generate vitest projects using Vitest 3
    
    ## Expected Behavior
    Use Vitest 4 when generating new projects
    
    ## Related Issue(s)
    
    Closes NXC-3343
    Closes NXC-3379
    
    ---------
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- packages/vitest/src/executors/test/lib/nx-reporter.ts
+++ packages/vitest/src/executors/test/lib/nx-reporter.ts
@@ -1,4 +1,5 @@
-import type { File, Reporter } from 'vitest';
+import type { RunnerTestFile } from 'vitest/node';
+import type { Reporter } from 'vitest/reporters';
 
 export class NxReporter implements Reporter {
   deferred: {
@@ -32,8 +33,9 @@ export class NxReporter implements Reporter {
   onTestRunEnd(files: any[], errors?: any) {
     this._handleFinished(files, errors);
   }
+
   /** Vitest ≤ 0.28 */
-  onFinished(files: File[], errors?: unknown[]) {
+  onFinished(files: RunnerTestFile[], errors?: unknown[]) {
     this._handleFinished(files, errors);
   }
 
