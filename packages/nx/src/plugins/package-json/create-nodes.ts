898448f8af6b3e2f4a877f58cdba4d4236e886ee
Miroslav Jonas <missing.manual@gmail.com>
2025-12-19 17:29:32 +0100

    fix(core): improve performance of package-json:create-nodes

--- packages/nx/src/plugins/package-json/create-nodes.ts
+++ packages/nx/src/plugins/package-json/create-nodes.ts
@@ -28,22 +28,34 @@ import {
   readPackageJsonConfigurationCache,
 } from '../../../plugins/package-json';
 
+const globPatterns = combineGlobPatterns(
+  'package.json',
+  '**/package.json',
+  'project.json',
+  '**/project.json'
+);
+
 export const createNodesV2: CreateNodesV2 = [
-  combineGlobPatterns(
-    'package.json',
-    '**/package.json',
-    'project.json',
-    '**/project.json'
-  ),
+  globPatterns,
   (configFiles, _, context) => {
     const { packageJsons, projectJsonRoots } = splitConfigFiles(configFiles);
 
     const readJson = (f) => readJsonFile(join(context.workspaceRoot, f));
-    const isInPackageJsonWorkspaces =
-      process.env.NX_INFER_ALL_PACKAGE_JSONS === 'true' &&
-      !configFiles.includes('package.json')
-        ? () => true
-        : buildPackageJsonWorkspacesMatcher(context.workspaceRoot, readJson);
+    let isInPackageJsonWorkspaces = (p: string) => true;
+
+    if (
+      process.env.NX_INFER_ALL_PACKAGE_JSONS !== 'true' ||
+      configFiles.includes('package.json')
+    ) {
+      const { positivePatterns, negativePatterns } = buildPackageJsonPatterns(
+        context.workspaceRoot,
+        readJson
+      );
+      isInPackageJsonWorkspaces = buildPackageJsonWorkspacesMatcher(
+        positivePatterns,
+        negativePatterns
+      );
+    }
     const isNextToProjectJson = (packageJsonPath: string) => {
       return projectJsonRoots.has(dirname(packageJsonPath));
     };
@@ -93,8 +105,7 @@ function splitConfigFiles(configFiles: readonly string[]): {
 
   return { packageJsons, projectJsonRoots };
 }
-
-export function buildPackageJsonWorkspacesMatcher(
+export function buildPackageJsonPatterns(
   workspaceRoot: string,
   readJson: (path: string) => any
 ) {
@@ -103,9 +114,20 @@ export function buildPackageJsonWorkspacesMatcher(
     readJson
   );
 
-  const negativePatterns = patterns.filter((p) => p.startsWith('!'));
-  const positivePatterns = patterns.filter((p) => !p.startsWith('!'));
+  const negativePatterns: string[] = [];
+  const positivePatterns: string[] = [];
+  const positivePatternLookup: Record<string, boolean> = {};
+  const negativePatternLookup: Record<string, boolean> = {};
 
+  for (const pattern of patterns) {
+    if (pattern.startsWith('!')) {
+      negativePatterns.push(pattern);
+      negativePatternLookup[pattern.slice(1)] = true;
+    } else {
+      positivePatterns.push(pattern);
+      positivePatternLookup[pattern] = true;
+    }
+  }
   if (
     // There are some negative patterns
     negativePatterns.length > 0 &&
@@ -117,8 +139,26 @@ export function buildPackageJsonWorkspacesMatcher(
     positivePatterns.push('**/package.json');
   }
 
-  return (p: string) =>
-    positivePatterns.some((positive) => minimatch(p, positive)) &&
+  return {
+    positive: positivePatterns,
+    positiveLookup: positivePatternLookup,
+    negative: negativePatterns,
+    negativeLookup: negativePatternLookup,
+  };
+}
+type PackageJsonPatterns = {
+  positive: string[];
+  positiveLookup: Record<string, boolean>;
+  negative: string[];
+  negativeLookup: Record<string, boolean>;
+};
+export function buildPackageJsonWorkspacesMatcher(
+  patterns: PackageJsonPatterns
+) {
+  return (p) =>
+    // use lookup to avoid unnecessary minimatch calls
+    (patterns.positiveLookup[p] ||
+      patterns.positive.some((positive) => minimatch(p, positive))) &&
     /**
      * minimatch will return true if the given p is NOT excluded by the negative pattern.
      *
@@ -128,7 +168,8 @@ export function buildPackageJsonWorkspacesMatcher(
      * Therefore, we need to ensure that every negative pattern returns true to validate that the given p is not
      * excluded by any of the negative patterns.
      */
-    negativePatterns.every((negative) => minimatch(p, negative));
+    !patterns.negativeLookup[p] &&
+    patterns.negative.every((negative) => minimatch(p, negative));
 }
 
 export function createNodeFromPackageJson(
