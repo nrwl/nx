25442271ab8bd1f0ddbc423bb61a08b8f0a66ad5
Jason Jean <jasonjean1993@gmail.com>
2026-01-16 09:28:53 -0500

    chore(repo): update nx to 22.4.0-beta.3 (#34108)
    Updating Nx from 22.4.0-beta.1 to 22.4.0-beta.3
    
    ---------
    
    Co-authored-by: nx-cloud[bot] <71083854+nx-cloud[bot]@users.noreply.github.com>

--- packages/web/src/generators/application/application.spec.ts
+++ packages/web/src/generators/application/application.spec.ts
@@ -182,7 +182,6 @@ describe('app', () => {
         .toMatchInlineSnapshot(`
         "import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
         import { defineConfig } from 'cypress';
-
         export default defineConfig({
           e2e: {
             ...nxE2EPreset(__filename, {
@@ -214,7 +213,6 @@ describe('app', () => {
         .toMatchInlineSnapshot(`
         "import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
         import { defineConfig } from 'cypress';
-
         export default defineConfig({
           e2e: {
             ...nxE2EPreset(__filename, {
