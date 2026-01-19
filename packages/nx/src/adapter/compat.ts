399dc6689ecd24fb36912fe894210dd60e3af70a
Colum Ferry <cferry09@gmail.com>
2026-01-19 10:38:25 +0000

    feat(core): add analytics boolean option to NxConfiguration

--- packages/nx/src/adapter/compat.ts
+++ packages/nx/src/adapter/compat.ts
@@ -82,6 +82,7 @@ export const allowedWorkspaceExtensions = [
   'useDaemonProcess',
   'useInferencePlugins',
   'neverConnectToCloud',
+  'analytics',
   'sync',
   'useLegacyCache',
   'maxCacheSize',
