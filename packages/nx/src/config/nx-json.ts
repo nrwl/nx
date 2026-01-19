040c3e87fc28c6aa8e281f081adbe3cb4b463207
Colum Ferry <cferry09@gmail.com>
2026-01-19 10:38:25 +0000

    feat(core): add analytics boolean option to NxConfiguration

--- packages/nx/src/config/nx-json.ts
+++ packages/nx/src/config/nx-json.ts
@@ -880,6 +880,11 @@ export interface NxJsonConfiguration<T = '*' | string[]> {
   neverConnectToCloud?: boolean;
 
   /**
+   * Set this to false to disable Nx from collecting anonymous usage analytics.
+   */
+  analytics?: boolean;
+
+  /**
    * Configuration for the `nx sync` command.
    */
   sync?: NxSyncConfiguration;
