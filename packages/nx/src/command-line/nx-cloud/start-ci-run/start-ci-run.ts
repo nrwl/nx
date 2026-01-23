0e37963f6771bcd5317c5268ac6a6329ab0c23a6
Jack Hsu <jack.hsu@gmail.com>
2026-01-23 12:29:41 -0500

    fix(core): cloud commands are noop when not connected rather than errors (#34193)
    This PR makes it so Cloud commands like `npx nx record` and `npx nx
    fix-ci` still work without `nxCloudId`. We'll log a warning so that
    `ci.yml` using these commands will still work. The warning let's users
    know that these do not work without being connected.
    
    Closes #NXC-3753
    
    (cherry picked from commit 687357c82ec07f8e3f06470df7c7e802fe0e7229)

--- packages/nx/src/command-line/nx-cloud/start-ci-run/start-ci-run.ts
+++ packages/nx/src/command-line/nx-cloud/start-ci-run/start-ci-run.ts
@@ -1,9 +1,15 @@
-import { executeNxCloudCommand } from '../utils';
+import { readNxJson } from '../../../config/nx-json';
+import { isNxCloudUsed } from '../../../utils/nx-cloud-utils';
+import { executeNxCloudCommand, warnNotConnectedToCloud } from '../utils';
 
 export interface StartCiRunArgs {
   verbose?: boolean;
 }
 
 export function startCiRunHandler(args: StartCiRunArgs): Promise<number> {
+  if (!isNxCloudUsed(readNxJson())) {
+    warnNotConnectedToCloud();
+    return Promise.resolve(0);
+  }
   return executeNxCloudCommand('start-ci-run', args.verbose);
 }
