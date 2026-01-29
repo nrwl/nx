4ba8c44077e45b23705998cbf3f4a946c8a1d185
Caleb Ukle <caleb@nrwl.io>
2026-01-29 12:06:04 -0600

    WIP: improve sidebar highlighting for non exact matches
    for subpages that aren't directly referenced in the sidebar we still
    want to highlight the main page where the information is from.
    there is probs a better way to do this, e.g. dynamically changing the
    sidebar only in highlight as well. but trying this for now

--- astro-docs/astro.config.mjs
+++ astro-docs/astro.config.mjs
@@ -87,6 +87,8 @@ export default defineConfig({
         // since the sidebar doesn't auto generate w/ dynamic routes from src/pages/reference
         // only the src/content/docs/reference files
         './src/plugins/sidebar-reference-updater.middleware.ts',
+        // Highlights parent sidebar items when viewing sub-routes not listed in the sidebar
+        './src/plugins/sidebar-subroute-highlighter.middleware.ts',
         // './src/plugins/sidebar-icons.middleware.ts',
         './src/plugins/og.middleware.ts',
         './src/plugins/github-stars.middleware.ts',
