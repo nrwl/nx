3b3a34f3334b5282ef2ed07c2adc6496f62ebb7d
Louie Weng <56288712+lourw@users.noreply.github.com>
2026-02-05 13:49:45 -0800

    chore(gradle): remove resulting space
    Co-authored-by: Jason Jean <jasonjean1993@gmail.com>

--- packages/gradle/src/executors/gradle/gradle-batch.impl.ts
+++ packages/gradle/src/executors/gradle/gradle-batch.impl.ts
@@ -192,8 +192,8 @@ async function runTasksInBatch(
   const usePseudoTerminal =
     process.env.NX_NATIVE_COMMAND_RUNNER !== 'false' &&
     PseudoTerminal.isSupported();
-  const debugOptions = process.env.NX_GRADLE_BATCH_DEBUG ?? '';
-  const command = `java ${debugOptions} -jar ${batchRunnerPath} --tasks='${JSON.stringify(
+  const debugOptions = ' ' + process.env.NX_GRADLE_BATCH_DEBUG ?? '';
+  const command = `java${debugOptions} -jar ${batchRunnerPath} --tasks='${JSON.stringify(
     gradlewTasksToRun
   )}' --workspaceRoot=${root} --args='${args
     .join(' ')
