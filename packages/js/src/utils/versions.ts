b85ac155cf2fdd4c77c5f2c0eb5b855bf9ab5243
Colum Ferry <cferry09@gmail.com>
2026-02-06 13:47:56 +0000

    feat(js): update swc/cli to 0.8.0 (#34365)
    Update `@swc/cli` to 0.8.0 which uses chokidar v5

--- packages/js/src/utils/versions.ts
+++ packages/js/src/utils/versions.ts
@@ -2,7 +2,7 @@ export const nxVersion = require('../../package.json').version;
 
 export const esbuildVersion = '^0.19.2';
 export const prettierVersion = '~3.6.2';
-export const swcCliVersion = '~0.7.2';
+export const swcCliVersion = '~0.8.0';
 export const swcCoreVersion = '~1.15.5';
 export const swcHelpersVersion = '~0.5.18';
 export const swcNodeVersion = '~1.11.1';
