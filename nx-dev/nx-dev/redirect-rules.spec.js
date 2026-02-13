cbb6f735b7a4f53ddc52b972ab5bba2eec681d51
Caleb Ukle <caleb@nrwl.io>
2026-02-13 16:43:56 -0600

    chore(nx-dev): reduce redirect count

--- nx-dev/nx-dev/redirect-rules.spec.js
+++ nx-dev/nx-dev/redirect-rules.spec.js
@@ -3,76 +3,62 @@ import redirectRules from './redirect-rules';
 describe('Redirect rules configuration', () => {
   describe('Safety checks', () => {
     it('should not redirect to itself', () => {
-      const rules = {
-        ...redirectRules.cliUrls,
-        ...redirectRules.diataxis,
-        ...redirectRules.guideUrls,
-        ...redirectRules.overviewUrls,
-        ...redirectRules.schemaUrls,
-        ...redirectRules.tutorialRedirects,
-      };
+      const allRules = {};
+      for (const section of Object.keys(redirectRules)) {
+        Object.assign(allRules, redirectRules[section]);
+      }
 
-      for (let k of Object.keys(rules)) {
-        expect(k).not.toEqual(rules[k]);
+      for (const k of Object.keys(allRules)) {
+        expect(k).not.toEqual(allRules[k]);
+      }
+    });
+
+    it('should not have empty destinations', () => {
+      for (const section of Object.keys(redirectRules)) {
+        for (const [source, dest] of Object.entries(redirectRules[section])) {
+          expect(dest).toBeTruthy();
+        }
       }
     });
   });
 
   describe('Tutorial redirects', () => {
-    test('old tutorial links', () => {
-      const oldReactUrls = [
-        '/react-tutorial/01-create-application',
-        '/react-tutorial/02-add-e2e-test',
-        '/react-tutorial/03-display-todos',
-        '/react-tutorial/04-connect-to-api',
-        '/react-tutorial/05-add-node-app',
-        '/react-tutorial/06-proxy',
-        '/react-tutorial/07-share-code',
-        '/react-tutorial/08-create-libs',
-        '/react-tutorial/09-dep-graph',
-        '/react-tutorial/10-computation-caching',
-        '/react-tutorial/11-test-affected-projects',
-        '/react-tutorial/12-summary',
-      ];
-
-      for (const url of oldReactUrls) {
-        expect(redirectRules.tutorialRedirects[url]).toEqual(
-          '/react-tutorial/1-code-generation'
-        );
-      }
+    test('react tutorial wildcard covers old step URLs', () => {
+      // Individual react tutorial step URLs are now handled by
+      // '/react-tutorial/:path*' -> '/docs/getting-started/tutorials/react-monorepo-tutorial'
+      expect(redirectRules.tutorialRedirects['/react-tutorial/:path*']).toEqual(
+        '/docs/getting-started/tutorials/react-monorepo-tutorial'
+      );
     });
 
-    test('old angular tutorial links', () => {
-      const oldAngularUrls = [
-        '/angular-tutorial/01-create-application',
-        '/angular-tutorial/02-add-e2e-test',
-        '/angular-tutorial/03-display-todos',
-        '/angular-tutorial/04-connect-to-api',
-        '/angular-tutorial/05-add-node-app',
-        '/angular-tutorial/06-proxy',
-        '/angular-tutorial/07-share-code',
-        '/angular-tutorial/08-create-libs',
-        '/angular-tutorial/09-dep-graph',
-        '/angular-tutorial/10-computation-caching',
-        '/angular-tutorial/11-test-affected-projects',
-        '/angular-tutorial/12-summary',
-      ];
-
-      for (const url of oldAngularUrls) {
-        expect(redirectRules.tutorialRedirects[url]).toEqual(
-          '/angular-tutorial/1-code-generation'
-        );
-      }
+    test('angular tutorial wildcard covers old step URLs', () => {
+      // Individual angular tutorial step URLs are now handled by
+      // '/angular-tutorial/:path*' -> '/docs/getting-started/tutorials/angular-monorepo-tutorial'
+      expect(
+        redirectRules.tutorialRedirects['/angular-tutorial/:path*']
+      ).toEqual('/docs/getting-started/tutorials/angular-monorepo-tutorial');
     });
 
-    test('old tutorial links', () => {
-      const oldTutorialUrls = ['/tutorials/integrated-repo-tutorial'];
+    test('old integrated-repo tutorial link', () => {
+      expect(
+        redirectRules.nested5minuteTutorialUrls[
+          '/tutorials/integrated-repo-tutorial'
+        ]
+      ).toEqual('/docs/getting-started/tutorials/react-monorepo-tutorial');
+    });
+  });
 
-      for (const url of oldTutorialUrls) {
-        expect(redirectRules.nested5minuteTutorialUrls[url]).toEqual(
-          '/getting-started' + url
-        );
-      }
+  describe('Wildcard consolidation', () => {
+    test('cli wildcard exists', () => {
+      expect(redirectRules.cliUrls['/cli/:path*']).toEqual(
+        '/packages/nx/documents/:path*'
+      );
+    });
+
+    test('concept wildcard exists in docsToAstroRedirects', () => {
+      expect(redirectRules.docsToAstroRedirects['/concepts/:path*']).toEqual(
+        '/docs/concepts/:path*'
+      );
     });
   });
 });
