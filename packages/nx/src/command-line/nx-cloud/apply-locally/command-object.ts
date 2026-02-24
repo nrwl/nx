5e9529021115b79e98dcfbf2f2a6a2335cb105df
MaxKless <34165455+MaxKless@users.noreply.github.com>
2026-02-24 11:42:41 +0100

    feat(core): add passthrough for nx-cloud apply-locally command (#34557)
    ## Current Behavior
    folks had to type in `nx-cloud apply-locally`
    
    ## Expected Behavior
    now `nx apply-locally` works
    
    ---------
    
    Co-authored-by: nx-cloud[bot] <71083854+nx-cloud[bot]@users.noreply.github.com>
    (cherry picked from commit 832355081b8d9b8e91884953a8b87a6ba6ebaec3)

---
+++ packages/nx/src/command-line/nx-cloud/apply-locally/command-object.ts
@@ -0,0 +1,18 @@
+import { CommandModule } from 'yargs';
+import { withVerbose } from '../../yargs-utils/shared-options';
+
+export const yargsApplyLocallyCommand: CommandModule = {
+  command: 'apply-locally [options]',
+  describe:
+    'Applies a self-healing CI fix locally. This command is an alias for `nx-cloud apply-locally`.',
+  builder: (yargs) =>
+    withVerbose(yargs)
+      .help(false)
+      .showHelpOnFail(false)
+      .option('help', { describe: 'Show help.', type: 'boolean' }),
+  handler: async (args: any) => {
+    process.exit(
+      await (await import('./apply-locally')).applyLocallyHandler(args)
+    );
+  },
+};
