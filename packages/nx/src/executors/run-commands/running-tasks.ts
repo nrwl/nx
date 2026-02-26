730f7a5931ee8f1665c3e88f3dd3d4ec3c15e1af
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2026-02-26 15:05:41 +0100

    fix(core): ensure postTasksExecution fires on SIGINT for continuous tasks
    Three issues prevented postTasksExecution from firing or having complete
    task results when pressing Ctrl+C on continuous tasks:
    
    - Initiating continuous task's onExit called process.exit() before the
      orchestrator's SIGINT handler could run cleanup
    - run-commands SIGINT handlers called process.exit(130), killing the
      process before orchestrator cleanup
    - cleanup() used a boolean guard causing concurrent signal handlers
      (SIGINT then SIGTERM from pnpm/npm) to resolve prematurely

--- packages/nx/src/executors/run-commands/running-tasks.ts
+++ packages/nx/src/executors/run-commands/running-tasks.ts
@@ -525,8 +525,6 @@ class RunningNodeProcess implements RunningTask {
     });
     process.on('SIGINT', () => {
       this.kill('SIGTERM');
-      // we exit here because we don't need to write anything to cache.
-      process.exit(signalToCode('SIGINT'));
     });
     process.on('SIGTERM', () => {
       this.kill('SIGTERM');
@@ -710,8 +708,6 @@ function registerProcessListener(
   });
   process.on('SIGINT', () => {
     runningTask.kill('SIGTERM');
-    // we exit here because we don't need to write anything to cache.
-    process.exit(signalToCode('SIGINT'));
   });
   process.on('SIGTERM', () => {
     runningTask.kill('SIGTERM');
