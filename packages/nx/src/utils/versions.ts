5bec5002986fbf1beeffdc1361bc7259f8f5a83f
Colum Ferry <cferry09@gmail.com>
2026-03-09 15:50:27 +0000

    chore(core): get package.json relative path via extname

--- packages/nx/src/utils/versions.ts
+++ packages/nx/src/utils/versions.ts
@@ -1,1 +1,12 @@
-export const nxVersion = require('nx/package.json').version;
+import { extname } from 'path';
+
+const isTS = extname(__filename) === '.ts';
+const pathToPackageJson = isTS ? '../../package.json' : '../../../package.json';
+const getNxVersion = () => {
+  try {
+    return require(pathToPackageJson).version;
+  } catch {
+    return require('nx/package.json').version;
+  }
+};
+export const nxVersion = getNxVersion();
