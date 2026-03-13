6a75be79c29d82dc806bf5f16510c0d263c4e89e
FrozenPandaz <jasonjean1993@gmail.com>
2026-03-13 18:39:03 -0400

    feat(core): add project count to createNodes telemetry

--- packages/nx/src/project-graph/plugins/loaded-nx-plugin.ts
+++ packages/nx/src/project-graph/plugins/loaded-nx-plugin.ts
@@ -1,5 +1,6 @@
 import type { ProjectGraph } from '../../config/project-graph';
 import { type PluginConfiguration } from '../../config/nx-json';
+import { customDimensions } from '../../analytics';
 import {
   AggregateCreateNodesError,
   isAggregateCreateNodesError,
@@ -89,8 +90,13 @@ export class LoadedNxPlugin {
       const inner = this.createNodes[1];
       this.createNodes[1] = async (...args) => {
         performance.mark(`${plugin.name}:createNodes - start`);
+        let projectCount = 0;
         try {
-          return await inner(...args);
+          const result = await inner(...args);
+          for (const [, , r] of result) {
+            projectCount += Object.keys(r.projects ?? {}).length;
+          }
+          return result;
         } catch (e) {
           if (isAggregateCreateNodesError(e)) {
             throw e;
@@ -102,7 +108,12 @@ export class LoadedNxPlugin {
           performance.measure(`${plugin.name}:createNodes`, {
             start: `${plugin.name}:createNodes - start`,
             end: `${plugin.name}:createNodes - end`,
-            detail: { track: true },
+            detail: {
+              track: true,
+              ...(customDimensions && {
+                [customDimensions.projectCount]: projectCount,
+              }),
+            },
           });
         }
       };
