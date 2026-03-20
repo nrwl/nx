fc5c84cfb2844a457064aa735f39fccdf5a63cd5
FrozenPandaz <jasonjean1993@gmail.com>
2026-03-20 15:57:09 -0400

    fix(core): use splitTargetFromNodes instead of faking ProjectGraph

--- packages/nx/src/tasks-runner/utils.ts
+++ packages/nx/src/tasks-runner/utils.ts
@@ -22,7 +22,7 @@ import { findMatchingProjects } from '../utils/find-matching-projects';
 import { isGlobPattern } from '../utils/globs';
 import { joinPathFragments } from '../utils/path';
 import { serializeOverridesIntoCommandLine } from '../utils/serialize-overrides-into-command-line';
-import { splitTarget } from '../utils/split-target';
+import { splitTargetFromNodes } from '../utils/split-target';
 import { workspaceRoot } from '../utils/workspace-root';
 import { isTuiEnabled } from './is-tui-enabled';
 
@@ -169,9 +169,9 @@ export function readProjectAndTargetFromTargetString(
   currentProject?: string
 ): { projects?: string[]; target: string } {
   // Support for both `project:target` and `target:with:colons` syntax
-  const [maybeProject, ...segments] = splitTarget(
+  const [maybeProject, ...segments] = splitTargetFromNodes(
     targetString,
-    { nodes: projects } as ProjectGraph,
+    projects,
     { silent: true, currentProject }
   );
 
