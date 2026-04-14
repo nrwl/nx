01a1db54ff0fc83db907c4faa34f66d744ccab2c
Miroslav Jonas <missing.manual@gmail.com>
2026-04-14 13:34:49 -0400

    chore(repo): improve docs based on review notes

--- packages/nx/src/config/nx-json.ts
+++ packages/nx/src/config/nx-json.ts
@@ -875,9 +875,9 @@ export interface NxJsonConfiguration<T = '*' | string[]> {
   useInferencePlugins?: boolean;
 
   /**
-   * Setting this to true will cause all attempts to connect your workspace to Nx Cloud to fail.
+   * Setting this to true will cause all attempts to setup your workspace to Nx Cloud to fail.
    * This value does not prevent using Nx Cloud if already connected.
-   * Use NX_NO_CLOUD=true env var to prevent using Nx Cloud when running commands.
+   * Use NX_NO_CLOUD=true env var or the `--no-cloud` arg to prevent using Nx Cloud when running commands.
    */
   neverConnectToCloud?: boolean;
 
