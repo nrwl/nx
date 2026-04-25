54672cd7c7f739121d2d21c809fd94e6dac86030
Jason Jean <jasonjean1993@gmail.com>
2026-04-25 01:50:06 -0400

    chore(module-federation): re-enable webpack module federation e2e suites
    Reverts the temporary describe.skip blocks added in #35214. The
    @module-federation/enhanced dependency has been bumped (now 2.3.3) and
    upstream webpack compatibility is expected to be restored, so we let CI
    verify whether the suites pass again.
    
    NXC-4220

--- e2e/react/src/module-federation/misc-rspack-convert-to-rspack.test.ts
+++ e2e/react/src/module-federation/misc-rspack-convert-to-rspack.test.ts
@@ -11,10 +11,7 @@ import {
 import { readPort, runCLI } from './utils';
 import { stripIndents } from 'nx/src/utils/strip-indents';
 
-// TODO: re-enable when @module-federation/enhanced supports webpack 5.106.0+
-// webpack 5.106.0 removed lib/util/create-schema-validation.js which @module-federation/enhanced@2.3.1 depends on
-// eslint-disable-next-line jest/no-disabled-tests
-describe.skip('React Rspack Module Federation Misc - Convert To Rspack', () => {
+describe('React Rspack Module Federation Misc - Convert To Rspack', () => {
   beforeAll(() => {
     process.env.NX_ADD_PLUGINS = 'false';
     newProject({ packages: ['@nx/react', '@nx/rspack'] });
