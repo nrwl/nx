0ee48524711e4d724d5741c30dd68fb56f2cfe29
FrozenPandaz <jasonjean1993@gmail.com>
2026-04-28 12:39:15 -0400

    fix(core): import getInstalledNxVersion directly from utils

--- packages/nx/src/daemon/is-nx-version-mismatch.ts
+++ packages/nx/src/daemon/is-nx-version-mismatch.ts
@@ -1,8 +1,6 @@
 import { nxVersion } from '../utils/versions';
 import { getInstalledNxVersion } from '../utils/installed-nx-version';
 
-export { getInstalledNxVersion };
-
 export function isNxVersionMismatch(): boolean {
   return getInstalledNxVersion() !== nxVersion;
 }
