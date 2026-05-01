0a614c9154837bdaa3a70a0ca85b3c490d67eedb
FrozenPandaz <jasonjean1993@gmail.com>
2026-05-01 18:52:28 -0400

    fix(testing): point cypress deprecation messages at general convert-to-inferred guide and drop cypress-specific docs

--- packages/cypress/src/utils/deprecation.ts
+++ packages/cypress/src/utils/deprecation.ts
@@ -3,7 +3,7 @@ import { logger } from '@nx/devkit';
 // TODO(v24): Remove the @nx/cypress:cypress executor. The inferred plugin
 // (@nx/cypress/plugin) and the convert-to-inferred generator stay supported.
 export const CYPRESS_EXECUTOR_DEPRECATION_MESSAGE =
-  'The `@nx/cypress:cypress` executor is deprecated and will be removed in Nx v24. Run `nx g @nx/cypress:convert-to-inferred` to migrate to the `@nx/cypress/plugin` inferred targets. See https://nx.dev/technologies/test-tools/cypress/guides/convert-to-inferred for details.';
+  'The `@nx/cypress:cypress` executor is deprecated and will be removed in Nx v24. Run `nx g @nx/cypress:convert-to-inferred` to migrate to the `@nx/cypress/plugin` inferred targets. See https://nx.dev/docs/guides/tasks--caching/convert-to-inferred for details.';
 
 export function warnCypressExecutorDeprecation(): void {
   logger.warn(CYPRESS_EXECUTOR_DEPRECATION_MESSAGE);
@@ -16,6 +16,6 @@ export function warnCypressExecutorDeprecation(): void {
 // generated target.
 export function warnCypressExecutorScaffolding(): void {
   logger.warn(
-    'Scaffolding a target that uses the deprecated `@nx/cypress:cypress` executor. The executor will be removed in Nx v24. Run `nx g @nx/cypress:convert-to-inferred` after this scaffold to migrate the generated target to the `@nx/cypress/plugin` inferred plugin. To skip emitting executor targets entirely on future scaffolds, register `@nx/cypress/plugin` in `nx.json` first (`nx add @nx/cypress` or a manual plugin entry). See https://nx.dev/technologies/test-tools/cypress/guides/convert-to-inferred for details.'
+    'Scaffolding a target that uses the deprecated `@nx/cypress:cypress` executor. The executor will be removed in Nx v24. Run `nx g @nx/cypress:convert-to-inferred` after this scaffold to migrate the generated target to the `@nx/cypress/plugin` inferred plugin. To skip emitting executor targets entirely on future scaffolds, register `@nx/cypress/plugin` in `nx.json` first (`nx add @nx/cypress` or a manual plugin entry). See https://nx.dev/docs/guides/tasks--caching/convert-to-inferred for details.'
   );
 }
