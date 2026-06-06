e143035b07801a432b25ec7bd020b73ea6199ff2
Jason Jean <jasonjean1993@gmail.com>
2026-06-06 11:20:13 -0400

    chore(plugin): build to local dist with nodenext module resolution
    Migrate @nx/plugin to build locally to packages/plugin/dist with a nodenext
    tsconfig, exports map (@nx/nx-source condition, ./src/* kept), files field, and
    release config. Self-reference the package.json version lookups in versions.ts
    and the plugin generator.

--- packages/plugin/src/utils/versions.ts
+++ packages/plugin/src/utils/versions.ts
@@ -1,3 +1,5 @@
-export const nxVersion = require('../../package.json').version;
+import { join } from 'path';
+
+export const nxVersion = require(join('@nx/plugin', 'package.json')).version;
 
 export const jsoncEslintParserVersion = '^2.1.0';
