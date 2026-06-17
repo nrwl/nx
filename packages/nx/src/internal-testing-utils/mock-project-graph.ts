352f3ea0baf2db2aeffe8e2b663dbdc188e6007c
FrozenPandaz <jasonjean1993@gmail.com>
2026-06-17 10:19:05 -0400

    chore(repo): use @jest/globals import in mock-project-graph

--- packages/nx/src/internal-testing-utils/mock-project-graph.ts
+++ packages/nx/src/internal-testing-utils/mock-project-graph.ts
@@ -1,4 +1,5 @@
-/// <reference types="jest" />
+import { jest } from '@jest/globals';
+
 jest.doMock('@nx/devkit', () => ({
   ...jest.requireActual<any>('@nx/devkit'),
   createProjectGraphAsync: jest.fn().mockImplementation(async () => {
