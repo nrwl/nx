ewok-pulse/saber-e2e-nx

feat(linter): default to ESLint v10 for new workspaces and projects
New workspaces and projects now scaffold with ESLint v10 and its ecosystem
(typescript-eslint, @eslint/js, @eslint/eslintrc, @eslint/compat, and the
Vue/Angular lint stacks). Existing workspaces continue to work with their
installed ESLint version: dependency additions keep installed versions
untouched and new ecosystem packages are pinned to a range compatible with
the workspace's ESLint major.

Version pins are now resolved through per-package compat maps keyed on the
installed ESLint major, mirroring the @nx/angular approach, so future
majors can be added by updating a single data structure.

packages/eslint/src/utils/versions.ts
@@ -1,12 +1,14 @@
 export const nxVersion = require('../../package.json').version;
 
-export const eslintVersion = '~8.57.0';
-export const eslintrcVersion = '^2.1.1';
+// ESLint core
+export const eslintVersion = '^10.0.0';
+export const eslintJsVersion = '^10.0.0';
+export const eslintrcVersion = '^3.3.0';
 export const eslintConfigPrettierVersion = '^10.0.0';
-export const typescriptESLintVersion = '^7.16.0';
-export const jsoncEslintParserVersion = '^2.1.0';
+export const eslintCompatVersion = '^2.0.5';
 
-// Updated linting stack for ESLint v9, typescript-eslint v8
-export const eslint9__typescriptESLintVersion = '^8.40.0';
-export const eslint9__eslintVersion = '^9.8.0';
-export const eslintCompat = '^1.1.1';
+// typescript-eslint
+export const typescriptESLintVersion = '^8.57.0';
+
+// parsers
+export const jsoncEslintParserVersion = '^2.1.0';

nrwl/nx

packages/eslint/src/utils/versions.ts
@@ -11,12 +11,12 @@ export const nxVersion = require(join('@nx/eslint', 'package.json')).version;
 export const minSupportedEslintVersion = '9.0.0';
 
 export const eslintConfigPrettierVersion = '^10.0.0';
-export const eslintrcVersion = '^3.0.0';
+export const eslintrcVersion = '^3.3.0';
 export const jsoncEslintParserVersion = '^2.1.0';
-export const eslintCompat = '^1.1.1';
+export const eslintCompat = '^2.0.5';
 
-export const eslintVersion = '^9.8.0';
-export const typescriptESLintVersion = '^8.40.0';
+export const eslintVersion = '^10.0.0';
+export const typescriptESLintVersion = '^8.57.0';
 
 type EslintVersions = {
   eslintVersion: string;
@@ -29,8 +29,7 @@ const latestVersions: EslintVersions = {
 };
 
 // Pins dependency versions for supported ESLint majors that aren't the default
-// (latest) stack so existing workspaces aren't force-bumped. Only v9 is mapped
-// for now; the v10 entry will be added alongside full v10 support.
+// (latest) stack so existing workspaces aren't force-bumped to v10.
 type CompatVersions = 9;
 const versionMap: Record<CompatVersions, EslintVersions> = {
   9: { eslintVersion: '^9.8.0', typescriptESLintVersion: '^8.40.0' },