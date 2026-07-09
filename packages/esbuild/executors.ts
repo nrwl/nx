544670cc008774265adbbd1d26a7998fec60a442
Steffen Neubauer <stefreak@users.noreply.github.com>
2026-07-09 19:28:12 +0200

    fix(esbuild): use type-only options export

--- packages/esbuild/executors.ts
+++ packages/esbuild/executors.ts
@@ -1,2 +1,2 @@
-export { EsBuildExecutorOptions } from './src/executors/esbuild/schema';
+export type { EsBuildExecutorOptions } from './src/executors/esbuild/schema';
 export { esbuildExecutor } from './src/executors/esbuild/esbuild.impl';
