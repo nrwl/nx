97634ec4fde057212a14810b1e9484c2cdcdcfa7
Ryan Hefner <hi@ryanhefner.com>
2026-07-30 18:42:44 -0400

    fix(core): align create-nx-workspace style options

--- packages/create-nx-workspace/bin/create-nx-workspace.ts
+++ packages/create-nx-workspace/bin/create-nx-workspace.ts
@@ -1228,7 +1228,7 @@ async function determineNoneOptions(
   }
 }
 
-async function determineReactOptions(
+export async function determineReactOptions(
   parsedArgs: yargs.Arguments<ReactArguments>
 ): Promise<Partial<ReactArguments>> {
   let preset: Preset;
@@ -1335,23 +1335,8 @@ async function determineReactOptions(
             message: 'SASS(.scss)       [ https://sass-lang.com   ]',
           },
           {
-            name: 'less',
-            message: 'LESS              [ https://lesscss.org     ]',
-          },
-          {
-            name: 'styled-components',
-            message:
-              'styled-components [ https://styled-components.com            ]',
-          },
-          {
-            name: '@emotion/styled',
-            message:
-              'emotion           [ https://emotion.sh                       ]',
-          },
-          {
-            name: 'styled-jsx',
-            message:
-              'styled-jsx        [ https://www.npmjs.com/package/styled-jsx ]',
+            name: 'none',
+            message: 'None',
           },
         ],
       },
@@ -1493,7 +1478,7 @@ async function determineVueOptions(
   };
 }
 
-async function determineAngularOptions(
+export async function determineAngularOptions(
   parsedArgs: yargs.Arguments<AngularArguments>
 ): Promise<Partial<AngularArguments>> {
   let preset: Preset;
@@ -1601,6 +1586,10 @@ async function determineAngularOptions(
           {
             name: 'scss',
             message: 'SASS(.scss)       [ https://sass-lang.com   ]',
+          },
+          {
+            name: 'sass',
+            message: 'SASS(.sass)       [ https://sass-lang.com   ]',
           },
           {
             name: 'less',
