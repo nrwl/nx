68e820ead45168b7f94cd2e053caf53f37c22828
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2026-08-11 15:18:51 +0200

    fix(testing): support Angular 22.1 CT with Cypress 15.20.1

--- packages/angular/src/utils/assert-cypress-component-testing-support.ts
+++ packages/angular/src/utils/assert-cypress-component-testing-support.ts
@@ -2,6 +2,8 @@ import { getDependencyVersionFromPackageJson, type Tree } from '@nx/devkit';
 import { getDeclaredPackageVersion } from '@nx/devkit/internal';
 import { coerce, lt } from 'semver';
 
+const minCypressVersion = '15.20.1';
+
 /**
  * Asserts the workspace's Cypress version can run Component Testing against its
  * Angular version. Must be called after `@nx/cypress` has been ensured.
@@ -21,11 +23,14 @@ export function assertCypressComponentTestingSupport(tree: Tree): void {
   // A dist tag (`latest`, `next`) can't be compared, so it's not gated
   const installedCypressVersion = coerce(declaredCypressVersion)?.version;
 
-  if (installedCypressVersion && lt(installedCypressVersion, '16.0.0')) {
+  if (
+    installedCypressVersion &&
+    lt(installedCypressVersion, minCypressVersion)
+  ) {
     throw new Error(
-      `Cypress Component Testing doesn't support Angular 22.1 and higher for Cypress version ${installedCypressVersion}. ` +
-        `Angular 22.1 moved @angular-devkit/build-angular to Babel 8, which Cypress can't load. ` +
-        `Cypress 16 should be the first release to support it. ` +
+      `Cypress Component Testing with Angular 22.1 and higher requires Cypress ${minCypressVersion} or higher. ` +
+        `Found Cypress ${installedCypressVersion}. Earlier Cypress versions can't load Angular's Babel 8 dependencies. ` +
+        `Please upgrade Cypress. ` +
         `See https://github.com/cypress-io/cypress/issues/34461.`
     );
   }
