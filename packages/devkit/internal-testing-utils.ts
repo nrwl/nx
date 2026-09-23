455cfd38858fa1076e1896467c15191ba4e7afab
Jason Jean <jasonjean1993@gmail.com>
2026-09-23 16:55:15 -0400

    chore(devkit): export the CJS mock helpers from internal testing utils

--- packages/devkit/internal-testing-utils.ts
+++ packages/devkit/internal-testing-utils.ts
@@ -5,5 +5,6 @@ export * from 'nx/src/internal-testing-utils/run-migration-against-this-workspac
 export * from 'nx/src/internal-testing-utils/with-environment';
 export * from 'nx/src/internal-testing-utils/with-pnpm';
 export * from 'nx/src/internal-testing-utils/temp-fs';
+export * from 'nx/src/internal-testing-utils/cjs-mock';
 
 export { setCwd } from './src/generators/artifact-name-and-directory-utils';
