fb656afde178123863cac650284032ae145448ed
Jason Jean <jasonjean1993@gmail.com>
2026-09-24 18:05:57 -0400

    chore(remix): codemod jest.* to vi.* in remix specs

--- packages/remix/src/generators/route/route.impl.spec.ts
+++ packages/remix/src/generators/route/route.impl.spec.ts
@@ -1,6 +1,7 @@
+import type { Mock } from 'vitest';
 import '@nx/devkit/internal-testing-utils/mock-project-graph';
 
-jest.mock('../../utils/remix-config');
+vi.mock('../../utils/remix-config');
 import * as remixConfigUtils from '../../utils/remix-config';
 import { Tree } from '@nx/devkit';
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
@@ -20,7 +21,7 @@ describe('route', () => {
     tree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
     tree.write('.gitignore', `/node_modules/dist`);
 
-    (remixConfigUtils.getRemixConfigValues as jest.Mock) = jest.fn(() =>
+    (remixConfigUtils.getRemixConfigValues as Mock) = vi.fn(() =>
       Promise.resolve({
         ignoredRouteFiles: ['**/.*'],
       })
