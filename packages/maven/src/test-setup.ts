e3c719925b8b6a024e12a199af546dfe342c38d1
Jason Jean <jasonjean1993@gmail.com>
2026-09-25 00:53:07 -0400

    chore(maven): remove unused jest test-setup file

--- packages/maven/src/test-setup.ts
+++
@@ -1,9 +0,0 @@
-// Test setup file for Maven plugin tests
-
-// Mock console methods to reduce test output noise
-global.console = {
-  ...console,
-  log: jest.fn(),
-  warn: jest.fn(),
-  error: jest.fn(),
-};
